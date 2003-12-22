826e45df029577d9e26168e872c427f1fdc0c8b9
Rob Awles <rta@open-mpi-git-mirror.example.com>
2003-12-22 19:27:22 +0000

    Getting free list to compile.  Added stubs for global fixed shared mem pools.

--- src/lam/mem/mem_globals.c
+++ src/lam/mem/mem_globals.c
@@ -17,7 +17,7 @@
  * COMPLETENESS, OR USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR
  * PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY
  * OWNED RIGHTS.
-                                                                                                     
+ 
  * Additionally, this program is free software; you can distribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2 of the License,
@@ -28,49 +28,27 @@
  */
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-#include "lam/mem/seg_list.h"
+#include <unistd.h>
+#include "lam_constants.h"
+#include "lam/base/object.h"
+#include "lam/mem/mem_globals.h"
 
-lam_class_info_t seg_list_cls = {"lam_seg_list_t", &object_cls, 
-    (class_init_t)lam_sgl_init, (class_destroy_t)lam_sgl_destroy};
+lam_fixed_mpool_t    lam_shmem_pools;
+lam_fixed_mpool_t    lam_per_proc_shmem_pools;
 
-void lam_sgl_init(lam_seg_list_t *slist)
+int lam_setup_per_proc_shmem_pools(int npools)
 {
-    SUPER_INIT(slist, seg_list_cls.cls_parent);
-    STATIC_INIT(slist->sgl_list, &lam_dbl_list_cls);
-    lam_mtx_init(&slist->sgl_lock);
-    slist->sgl_min_bytes_pushed = 0;
-    slist->sgl_max_bytes_pushed = 0;
-    slist->sgl_bytes_pushed = 0;
-    slist->sgl_max_consec_fail = 0;
-    slist->sgl_consec_fail = 0;
-}
-
-void lam_sgl_destroy(lam_seg_list_t *slist)
-{
-    lam_dbl_empty_list(&(slist->sgl_list));
-    SUPER_DESTROY(slist, seg_list_cls.cls_parent);
-}
-
-
-void lam_sgl_append_elt_chunk(
-    lam_seg_list_t *slist, 
-    void *chunk, 
-    size_t chunk_size,
-    int n_elts, 
-    size_t elt_size)
-{
-    /* Since this function could be called frequently, we do not
-        want to compute the number of elements each time, so we
-        require it to be passed as an arg.
-    */
-    int     i;
-    char    *ptr;
+    int ret = LAM_SUCCESS;
+    ssize_t initial_alloc = 0;
+    ssize_t min_alloc_size = 4 * getpagesize();
+    int n_array_elts_add = 10;
     
-    ptr = (char *)chunk;
-    slist->sgl_bytes_pushed += chunk_size;
-    for ( i = 0; i < n_elts; i++ )
-    {
-        lam_dbl_append(&(slist->sgl_list), (lam_dbl_item_t *)ptr);
-        ptr += elt_size;
-    }
+    STATIC_INIT(lam_per_proc_shmem_pools, &fixed_mem_pool_cls);
+    lam_fmp_init_with(&lam_per_proc_shmem_pools,
+                                  initial_alloc, min_alloc_size,
+                                  npools, n_array_elts_add, 1);
+    
+    return ret;
+    
 }
+
--- src/lam/mem/mem_globals.h
+++ src/lam/mem/mem_globals.h
@@ -17,7 +17,7 @@
  * COMPLETENESS, OR USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR
  * PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY
  * OWNED RIGHTS.
-                                                                                                     
+ 
  * Additionally, this program is free software; you can distribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2 of the License,
@@ -28,49 +28,20 @@
  */
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-#include "lam/mem/seg_list.h"
+#ifndef MEM_GLOBALS_H
+#define MEM_GLOBALS_H
 
-lam_class_info_t seg_list_cls = {"lam_seg_list_t", &object_cls, 
-    (class_init_t)lam_sgl_init, (class_destroy_t)lam_sgl_destroy};
+#include "lam/mem/mem_pool.h"
 
-void lam_sgl_init(lam_seg_list_t *slist)
-{
-    SUPER_INIT(slist, seg_list_cls.cls_parent);
-    STATIC_INIT(slist->sgl_list, &lam_dbl_list_cls);
-    lam_mtx_init(&slist->sgl_lock);
-    slist->sgl_min_bytes_pushed = 0;
-    slist->sgl_max_bytes_pushed = 0;
-    slist->sgl_bytes_pushed = 0;
-    slist->sgl_max_consec_fail = 0;
-    slist->sgl_consec_fail = 0;
-}
+/* shared memory pool for use before fork.
+    should be initialized during prefork init.
+*/
+extern lam_fixed_mpool_t    lam_shmem_pools;
 
-void lam_sgl_destroy(lam_seg_list_t *slist)
-{
-    lam_dbl_empty_list(&(slist->sgl_list));
-    SUPER_DESTROY(slist, seg_list_cls.cls_parent);
-}
+extern lam_fixed_mpool_t    lam_per_proc_shmem_pools;
+
+int lam_setup_per_proc_shmem_pools(int npools);
+
+#endif
 
 
-void lam_sgl_append_elt_chunk(
-    lam_seg_list_t *slist, 
-    void *chunk, 
-    size_t chunk_size,
-    int n_elts, 
-    size_t elt_size)
-{
-    /* Since this function could be called frequently, we do not
-        want to compute the number of elements each time, so we
-        require it to be passed as an arg.
-    */
-    int     i;
-    char    *ptr;
-    
-    ptr = (char *)chunk;
-    slist->sgl_bytes_pushed += chunk_size;
-    for ( i = 0; i < n_elts; i++ )
-    {
-        lam_dbl_append(&(slist->sgl_list), (lam_dbl_item_t *)ptr);
-        ptr += elt_size;
-    }
-}
