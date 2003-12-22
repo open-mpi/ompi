e5929aeb8ea2ea528994fc090179aa8b250c64e0
Rob Awles <rta@lanl.gov>
2003-12-22 20:46:53 +0000

    Started porting ctnetwork classes.
    This commit was SVN r12.

---
+++ src/lam/ctnetwork/ctmessage.h
@@ -0,0 +1,17 @@
+/*
+ *  ctmessage.h
+ *  LAM-MPI
+ *
+ *  Created by Rob Aulwes on Sun Dec 21 2003.
+ *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef CT_MESSAGE_H
+#define CT_MESSAGE_H
+
+#include "lam/lfc/object.h"
+
+#endif  /* CT_MESSAGE_H */
+
+
---
+++ src/lam/ctnetwork/ctpeer.c
@@ -0,0 +1,11 @@
+/*
+ *  ctpeer.c
+ *  LAM-MPI
+ *
+ *  Created by Rob Aulwes on Sun Dec 21 2003.
+ *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "ctpeer.h"
+
