d32fa062d7c5cda4926031d8c2697e1bfa9a1354
Rob Awles <rta@open-mpi-git-mirror.example.com>
2003-12-23 18:21:32 +0000

    Added channel and controller classes. Added more function prototypes.

---
+++ src/lam/ctnetwork/ctchannel.c
@@ -0,0 +1,11 @@
+/*
+ *  ctchannel.c
+ *  LAM-MPI
+ *
+ *  Created by Rob Aulwes on Tue Dec 23 2003.
+ *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "ctchannel.h"
+
