dbac4d0a2c428505eace000c22d1c5c3b843b1bc
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-01-08 13:40:30 +0000

    Clearly mark the example files

--- src/mpi/interface/c/comm_get_name.c
+++ src/mpi/interface/c/comm_get_name.c
@@ -2,3 +2,4 @@
  * $HEADER$
  */
 
+/* Nothing here for now -- example is in src/mpi/c/comm_set_name.c */
--- src/mpi/interface/f77/datatype_get_name_f.c
+++ src/mpi/interface/f77/datatype_get_name_f.c
@@ -1,3 +1,5 @@
 /*
  * $HEADER$
  */
+
+/* Nothing here for now -- example is in src/mpi/f77/comm_set_name_f.c */
