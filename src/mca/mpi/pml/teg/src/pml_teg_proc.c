e874133d2202db1c8ec8d28fa39909a67fcfd295
Tim Woodall <twoodall@lanl.gov>
2004-01-09 23:20:23 +0000

    move to new pml instance
    This commit was SVN r189.

---
+++ src/mca/mpi/pml/teg/src/pml_teg_proc.c
@@ -0,0 +1,34 @@
+/*
+ * $HEADER$
+ */
+
+#include "mca/mpi/pml/teg/proc.h"
+
+
+lam_class_info_t mca_pml_teg_proc_cls = { 
+    "mca_pml_teg_proc_t", 
+    &lam_list_item_cls,
+    (class_init_t) mca_pml_teg_proc_init, 
+    (class_destroy_t) mca_pml_teg_proc_destroy 
+};
+
+static int mca_pml_teg_procs_init = 0;
+lam_list_t mca_pml_teg_procs;
+
+
+void mca_pml_teg_proc_init(mca_pml_proc_t* proc)
+{
+    if(fetchNset(&mca_pml_teg_procs_init,1) == 0) 
+        lam_list_init(&mca_pml_teg_procs);
+    SUPER_INIT(proc, &lam_list_item_cls);
+    mca_pml_teg_array_init(&proc->proc_ptl_first);
+    mca_pml_teg_array_init(&proc->proc_ptl_next);
+    lam_list_append(&mca_pml_teg_procs, &proc->super);
+}
+
+
+void mca_pml_teg_proc_destroy(mca_pml_proc_t* proc)
+{
+    SUPER_DESTROY(proc, &lam_list_item_cls);
+}
+
---
+++ src/mca/mpi/pml/teg/src/pml_teg_proc.h
@@ -0,0 +1,31 @@
+/*
+ * $HEADER$
+ */
+
+#ifndef MCA_PML_PROC_H
+#define MCA_PML_PROC_H
+
+#include "lam/proc.h"
+#include "mca/mpi/pml/teg/ptl_array.h"
+
+extern lam_class_info_t mca_pml_teg_proc_cls;
+
+/*
+ *  Structure associated w/ lam_proc_t that contains data specific
+ *  to the PML. Note that this name is not PML specific.
+ */
+
+struct mca_pml_proc_t {
+   lam_list_item_t super;
+   lam_proc_t *proc_lam;
+   mca_ptl_array_t proc_ptl_first;
+   mca_ptl_array_t proc_ptl_next;
+};
+typedef struct mca_pml_proc_t mca_pml_proc_t;
+
+
+void mca_pml_teg_proc_init(mca_pml_proc_t*);
+void mca_pml_teg_proc_destroy(mca_pml_proc_t*);
+
+#endif
+
