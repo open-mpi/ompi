2620d1fd223af7af706161738c5eb0e483c8d86d
Jeff Squyres <jsquyres@cisco.com>
2004-01-09 08:29:57 +0000

    Initial versions of functions
    This commit was SVN r163.

---
+++ src/mpi/interface/f77/finalize_f.c
@@ -0,0 +1,30 @@
+/*
+ * $HEADER$
+ */
+
+#include "lam_config.h"
+
+#include <stdio.h>
+
+#include "mpi.h"
+#include "mpi/f77/bindings.h"
+
+#if LAM_HAVE_WEAK_SYMBOLS
+#pragma weak MPI_FINALIZE = mpi_finalize_f
+#pragma weak mpi_finalize = mpi_finalize_f
+#pragma weak mpi_finalize_ = mpi_finalize_f
+#pragma weak mpi_finalize__ = mpi_finalize_f
+#if LAM_WANT_MPI_PROFILING
+#pragma weak PMPI_FINALIZE = mpi_finalize_f
+#pragma weak pmpi_finalize = mpi_finalize_f
+#pragma weak pmpi_finalize_ = mpi_finalize_f
+#pragma weak pmpi_finalize__ = mpi_finalize_f
+#endif
+#endif
+
+
+void
+mpi_finalize_f(int *ierror)
+{
+  *ierror = MPI_Finalize();
+}
---
+++ src/mpi/interface/f77/init_f.c
@@ -0,0 +1,30 @@
+/*
+ * $HEADER$
+ */
+
+#include "lam_config.h"
+
+#include <stdio.h>
+
+#include "mpi.h"
+#include "mpi/f77/bindings.h"
+
+#if LAM_HAVE_WEAK_SYMBOLS
+#pragma weak MPI_INIT = mpi_init_f
+#pragma weak mpi_init = mpi_init_f
+#pragma weak mpi_init_ = mpi_init_f
+#pragma weak mpi_init__ = mpi_init_f
+#if LAM_WANT_MPI_PROFILING
+#pragma weak PMPI_INIT = mpi_init_f
+#pragma weak pmpi_init = mpi_init_f
+#pragma weak pmpi_init_ = mpi_init_f
+#pragma weak pmpi_init__ = mpi_init_f
+#endif
+#endif
+
+
+void
+mpi_init_f(int *ierror)
+{
+  *ierror = MPI_Init(NULL, NULL);
+}
