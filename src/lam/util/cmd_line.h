67a26a1d657e7e4f1700a98ffa696ef7383fee72
Jeff Squyres <jsquyres@cisco.com>
2004-01-09 18:39:35 +0000

    Oops -- didn't mean to commit all the OPT* stuff.
    This commit was SVN r175.

--- src/lam/util/cmd_line.h
+++ src/lam/util/cmd_line.h
@@ -13,27 +13,29 @@ typedef struct lam_cmd_line {
 extern "C" {
 #endif
 
-  char *lam_cmd_line_argv0(OPT *aod);
-  char *lam_cmd_line_chosen(OPT *aod, const char *opt);
-  OPT *lam_cmd_line_init(void);
-  int lam_cmd_line_exam_isopt(OPT *aod, const char *opt);
-  int lam_cmd_line_exam_nparams(OPT *aod, const char *opt, int *nparams);
-  void lam_cmd_line_free(OPT *aod);
-  int lam_cmd_line_intparam(OPT *aod, const char *opt, int inst, 
-			 int idx, int *iparam);
-  int lam_cmd_line_ninsts(OPT *aod, const char *opt);
-  int lam_cmd_line_nparams(OPT *aod, const char *opt, int inst);
-  int lam_cmd_line_ntaken(OPT *aod);
-  char *lam_cmd_line_param(OPT *aod, const char *opt, int inst, int idx);
-  int lam_cmd_line_parse(OPT *aod, int *argc, char **argv);
-  int lam_cmd_line_setflags(OPT *aod, int flags);
-  int lam_cmd_line_setopt(OPT *aod, const char *opt, const char *mutex, 
-		       int np, int flags);
-  int lam_cmd_line_setopt1(OPT *aod, const char *opt, const char *mutex, 
-			int np, int flags);
-  int lam_cmd_line_tail(OPT *aod, int *tailc, char ***tailv);
-  int lam_cmd_line_taken(OPT *aod, const char *opt);
-  int lam_cmd_line_unused(OPT *aod, int *unusedc, char ***unusedv);
+  char *lam_cmd_line_argv0(lam_cmd_line_t *aod);
+  char *lam_cmd_line_chosen(lam_cmd_line_t *aod, const char *opt);
+  lam_cmd_line_t *lam_cmd_line_init(void);
+  int lam_cmd_line_exam_isopt(lam_cmd_line_t *aod, const char *opt);
+  int lam_cmd_line_exam_nparams(lam_cmd_line_t *aod, const char *opt, 
+                                int *nparams);
+  void lam_cmd_line_free(lam_cmd_line_t *aod);
+  int lam_cmd_line_intparam(lam_cmd_line_t *aod, const char *opt, int inst, 
+                            int idx, int *iparam);
+  int lam_cmd_line_ninsts(lam_cmd_line_t *aod, const char *opt);
+  int lam_cmd_line_nparams(lam_cmd_line_t *aod, const char *opt, int inst);
+  int lam_cmd_line_ntaken(lam_cmd_line_t *aod);
+  char *lam_cmd_line_param(lam_cmd_line_t *aod, const char *opt, int inst, 
+                           int idx);
+  int lam_cmd_line_parse(lam_cmd_line_t *aod, int *argc, char **argv);
+  int lam_cmd_line_setflags(lam_cmd_line_t *aod, int flags);
+  int lam_cmd_line_setopt(lam_cmd_line_t *aod, const char *opt, 
+                          const char *mutex, int np, int flags);
+  int lam_cmd_line_setopt1(lam_cmd_line_t *aod, const char *opt, 
+                           const char *mutex, int np, int flags);
+  int lam_cmd_line_tail(lam_cmd_line_t *aod, int *tailc, char ***tailv);
+  int lam_cmd_line_taken(lam_cmd_line_t *aod, const char *opt);
+  int lam_cmd_line_unused(lam_cmd_line_t *aod, int *unusedc, char ***unusedv);
 
 #ifdef __cplusplus
 }
