1d15b70dc3ed3662e5fbc136d35b0d03770a45c8
Tim Woodall <twoodall@open-mpi-git-mirror.example.com>
2004-01-09 05:11:33 +0000

    - added initial i/f functions

--- src/mca/mpi/pml/pml.h
+++ src/mca/mpi/pml/pml.h
@@ -12,63 +12,85 @@
 
 
 /*
- * Types for each API function pointer.
- *
- * JMS: This is an example -- there will need to be a query function,
- * but I have no idea what the arguments will be.
+ * PML module functions
  */
 
-typedef int (*mca_pml_query_fn_t)(int *priority, int *thread_min, 
-                                  int *thread_max);
-
-/*
- * Action functions
- *
- * JMS: Again, this is an example.
- */
-
-typedef int (*mci_pml_addprocs_fn_t)(lam_proc_t **procs, int nprocs);
-typedef const struct mca_pml_actions_1_0_0 *
-  (*mca_pml_init_1_0_0_fn_t)(struct _proc **procs, int nprocs,
-                             int *max_tag, int *max_cid);
+typedef int (*mca_pml_query_fn_t)(int *priority);
+typedef struct mca_pml_1_0_0 * (*mca_pml_init_1_0_0_fn_t)(
+    struct lam_proc **procs, 
+    int nprocs, 
+    int *max_tag, 
+    int *max_cid
+);
 
 
 /*
- * Struct used to pass pml MCA information from the each pml instance
- * back to the MCA framework.
+ * PML interface functions
  */
 
-typedef struct mca_pml_1_0_0 {
+typedef enum {
+    MCA_PTM_REQUEST_TYPE_RECV,
+    MCA_PTM_REQUEST_TYPE_SEND_STANDARD,
+    MCA_PTM_REQUEST_TYPE_SEND_BUFFERED,
+    MCA_PTM_REQUEST_TYPE_SEND_SYNCHRONOUS,
+    MCA_PTM_REQUEST_TYPE_SEND_READY
+} mca_pml_request_type_t;
+
+
+typedef int (*mca_pml_progress_fn_t)(lam_time_t);
+
+typedef int (*mca_pml_isend_fn_t)(
+    void *buf,
+    size_t size,
+    lam_datatype_t *datatype,
+    int dest,
+    int tag,
+    lam_communicator_t* comm,
+    mca_pml_request_type_t req_type,
+    lam_request_t **request
+);
+
+typedef int (*mci_pma_addprocs_fn_t)(lam_proc_t **procs, int nprocs);
+
+
+/*
+ * PML module definition.
+ */
+
+typedef struct mca_pml_module_1_0_0 {
   mca_1_0_0_t mp_meta_info;
 
   /* pml API function pointers */
 
   mca_pml_query_fn_t mp_query;
   mca_pml_init_1_0_0_fn_t mp_init;
+} mca_pml_module_1_0_0_t;
+
+
+/*
+ * Struct that represents the common state and interface functions
+ * provided by a PML.
+ */
+                                                                                                         
+typedef struct mca_pml_1_0_0 {
+
+  mca_pml_addprocs_fn_t pml_addprocs;
+  mca_pml_isend_fn_t pml_isend;
+  mca_pml_progress_fn_t pml_progress;
+
 } mca_pml_1_0_0_t;
 
-typedef struct mca_pml_actions_1_0_0 {
-
-  /* pml API action function pointers */
-
-  mca_pml_addprocs_fn_t mpa_addprocs;
-
-  /* Flags */
-
-  bool mp_tv_queue_support;
-} mca_pml_actions_1_0_0_t;
-
 
 /*
- * Set the default type to use version 1.1.0 of the SSI RPI struct 
+ * Set the default type to use version 1.1.0 of the PML
  */
 
+typedef mca_pml_module_1_1_0_t mca_pml_t;
 typedef mca_pml_1_1_0_t mca_pml_t;
-typedef mca_pml_actions_1_1_0_t mca_pml_actions_t;
 
 
 /*
- * Global functions for MCA: overall pml open and close
+ * Global functions for MCA: overall PML open and close
  */
 
 #if defined(c_plusplus) || defined(__cplusplus)
@@ -86,8 +108,8 @@ extern "C" {
  * Public variables
  */
 
-extern lam_list_t *lam_ssi_rpi_base_opened;
-extern lam_list_t *lam_ssi_rpi_base_available;
+extern lam_list_t *mca_base_opened;
+extern lam_list_t *mca_base_available;
 
 /*
  * Global instance of array of pointers to lam_ssi_rpi_t.  Will
