3b2c1b6dd7061d43cd6e7a6d2f4a3b6e273648a8
Prabhanjan Kambadur <pkambadu@osl.iu.edu>
2004-01-09 18:47:52 +0000

    Fixing STRDIR in path.c and prototyping errors
    This commit was SVN r177.

--- src/lam/util/path.h
+++ src/lam/util/path.h
@@ -5,9 +5,9 @@
 #ifndef LAM_PATH_H
 #define LAM_PATH_H
 
-char   *lam_path_find (char *, char **, int);
-char   *lam_path_env_find (char *, int);
-char   *lam_path_findv (char *, char **, int, char **);
-char   *lam_path_env_findv (char *, int, char **, char *);
+char   *lam_path_find (char *fname, char **pathv, int mode);
+char   *lam_path_env_find (char *fname, int mode);
+char   *lam_path_findv (char *fname, char **pathv, int mode, char **envv);
+char   *lam_path_env_findv (char *fname, int mode, char **envv, char *wrkdir);
 
 #endif /* LAM_PATH_H */
