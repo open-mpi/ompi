eee3abd2f9223083034b249801d9655de23d8799
Tim Woodall <twoodall@lanl.gov>
2004-01-09 05:11:33 +0000

    - added initial i/f functions
    This commit was SVN r144.

--- src/mca/mpi/ptl/ptl.h
+++ src/mca/mpi/ptl/ptl.h
@@ -12,63 +12,62 @@
 
 
 /*
- * Types for each API function pointer.
- *
- * JMS: This is an example -- there will need to be a query function,
- * but I have no idea what the arguments will be.
+ * PTL module functions.
  */
 
-typedef int (*mca_ptl_query_fn_t)(int *priority, int *thread_min, 
-                                  int *thread_max);
-
+typedef int (*mca_ptl_query_fn_t)(int *priority, int *thread_min, int *thread_max);
+typedef struct mca_ptl_1_0_0* (*mca_ptl_init_1_0_0_fn_t)();
+                                                                                                         
 /*
- * Action functions
- *
- * JMS: Again, this is an example.
+ * PTL action functions.
  */
 
-typedef int (*mci_ptl_addprocs_fn_t)(lam_proc_t **procs, int nprocs);
-typedef const struct mca_ptl_actions_1_0_0 *
-  (*mca_ptl_init_1_0_0_fn_t)(struct _proc **procs, int nprocs,
-                             int *max_tag, int *max_cid);
-
+typedef int (*mca_ptl_fragment_fn_t)(mca_ptl_send_request_t*, size_t);
+typedef int (*mca_ptl_progress_fn_t)(mca_time_t);
 
 /*
- * Struct used to pass ptl MCA information from the each ptl instance
- * back to the MCA framework.
+ * Struct used to pass PTL module information from the each PTL 
+ * instance back to the MCA framework.
+ */
+
+typedef struct mca_ptl_module_1_0_0 {
+  mca_1_0_0_t mp_meta_info;
+  mca_ptl_query_fn_t mp_query;
+  mca_ptl_init_1_0_0_fn_t mp_init;
+} mca_ptl_module_1_0_0_t;
+
+/*
+ * Struct that represents the common state and interface functions
+ * provided by a PTL.
  */
 
 typedef struct mca_ptl_1_0_0 {
-  mca_1_0_0_t mp_meta_info;
 
-  /* ptl API function pointers */
+    /* PTL common attributes */
+    size_t       ptl_frag_first_size;     /* maximum size of first fragment */
+    size_t       ptl_frag_min_size;       /* threshold below which the CDI will not fragment */
+    size_t       ptl_frag_max_size;       /* maximum fragment size supported by the CDI */
+    uint32_t     ptl_endpoint_latency;    /* relative/absolute measure of latency */
+    uint64_t     ptl_endpoint_bandwidth;  /* bandwidth (bytes/sec) supported by each endpoint */
+    size_t       ptl_endpoint_count;      /* number endpoints supported by this CDI */
 
-  mca_ptl_query_fn_t mp_query;
-  mca_ptl_init_1_0_0_fn_t mp_init;
+    /* PTL function table */
+    mca_ptl_fragment_fn_t  ptl_fragment;
+    mca_ptl_progress_fn_t  ptl_progress;
+
 } mca_ptl_1_0_0_t;
 
-typedef struct mca_ptl_actions_1_0_0 {
-
-  /* ptl API action function pointers */
-
-  mca_ptl_addprocs_fn_t mpa_addprocs;
-
-  /* Flags */
-
-  bool mp_tv_queue_support;
-} mca_ptl_actions_1_0_0_t;
-
 
 /*
- * Set the default type to use version 1.1.0 of the SSI RPI struct 
+ * Set the default type to use version 1.1.0 of the PTL 
  */
 
+typedef mca_ptl_module_1_1_0_t mca_ptl_module_t;
 typedef mca_ptl_1_1_0_t mca_ptl_t;
-typedef mca_ptl_actions_1_1_0_t mca_ptl_actions_t;
 
 
 /*
- * Global functions for MCA: overall ptl open and close
+ * Global functions for MCA: overall PTL open and close
  */
 
 #if defined(c_plusplus) || defined(__cplusplus)
@@ -86,11 +85,11 @@ extern "C" {
  * Public variables
  */
 
-extern lam_list_t *lam_ssi_rpi_base_opened;
-extern lam_list_t *lam_ssi_rpi_base_available;
+extern lam_list_t *mca_base_opened;
+extern lam_list_t *mca_base_available;
 
 /*
- * Global instance of array of pointers to lam_ssi_rpi_t.  Will
+ * Global instance of array of pointers to mca_t.  Will
  * effectively be filled in by configure.
  */
 
