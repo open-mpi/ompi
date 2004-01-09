db3517165dccea5054c54fc93d780275753a0277
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-01-09 08:27:58 +0000

    More skeleton infrastructure for MCA

---
+++ src/mca/mpi/io/io.h
@@ -0,0 +1,10 @@
+/*
+ * $HEADER
+ */
+
+#ifndef MCA_IO_H
+#define MCA_IO_H
+
+/* JMS Need to fill in here */
+
+#endif /* MCA_IO_H */
---
+++ src/mca/mpi/one/one.h
@@ -0,0 +1,10 @@
+/*
+ * $HEADER
+ */
+
+#ifndef MCA_ONE_H
+#define MCA_ONE_H
+
+/* JMS Need to fill in here */
+
+#endif /* MCA_ONE_H */
