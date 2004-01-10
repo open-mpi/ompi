2bfdc3d7ca1a7ad25befdb4d0172a07918a6e229
Jeff Squyres <jsquyres@cisco.com>
2004-01-10 14:59:06 +0000

    Add C++ protection
    This commit was SVN r202.

--- src/lam/util/argv.h
+++ src/lam/util/argv.h
@@ -5,12 +5,18 @@
 #ifndef LAM_ARGV_H
 #define LAM_ARGV_H
 
-int lam_argv_add(int *argc, char ***argv, char *arg);
-void lam_argv_free(char **argv);
-char **lam_argv_split(char *src_string, int delimiter);
-int lam_argv_count(char **argv);
-char *lam_argv_join(char **argv, int delimiter);
-size_t lam_argv_len(char **argv);
-char **lam_argv_copy(char **argv);
+#ifdef __cplusplus
+extern "C" {
+#endif
+  int lam_argv_add(int *argc, char ***argv, char *arg);
+  void lam_argv_free(char **argv);
+  char **lam_argv_split(char *src_string, int delimiter);
+  int lam_argv_count(char **argv);
+  char *lam_argv_join(char **argv, int delimiter);
+  size_t lam_argv_len(char **argv);
+  char **lam_argv_copy(char **argv);
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LAM_ARGV_H */
