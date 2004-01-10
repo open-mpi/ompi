47d7109d5b6668cc7bec25e018dc24c9297c2e95
Jeff Squyres <jsquyres@cisco.com>
2004-01-10 08:20:15 +0000

    - Make the context ID be uint32_t - Make the coll type correct - Change to make the type "struct lam_communicator" so that mpi.h is   happy.  This may end up changing; we should name all the MPI opaque   object structs the same way.
    This commit was SVN r199.

--- src/mpi/communicator/communicator.h
+++ src/mpi/communicator/communicator.h
@@ -2,48 +2,42 @@
  * $HEADER$
  */
 
-#ifndef LAM_COMMUNICATOR
-#define LAM_COMMUNICATOR
+#ifndef LAM_COMMUNICATOR_H
+#define LAM_COMMUNICATOR_H
 
 #include "mpi.h"
 #include "lam/group.h"
+#include "lam/stdint.h"
 #include "lam/threads/mutex.h"
 #include "mca/mpi/coll/coll.h"
 
-struct lam_communicator_t {
+typedef struct lam_communicator {
     char c_name[MPI_MAX_OBJECT_NAME];
-    int  c_contextid;
-    int  c_refcount;
-    int  c_flags;
+    uint32_t c_contextid;
+    int c_refcount;
+    int c_flags;
 
     lam_group_t c_local_group;
     lam_group_t c_remote_group;
-
   
-  /* Queues */
+    /* Queues */
 
-  /* Attributes */
+    /* Attributes */
 
-  /* Topology information */
+    /* Topology information */
 
-  /* Error handling */
+    /* Error handling */
 
-#if 0
-  MPI_Errhandler c_error_handler;
-#endif
+    MPI_Errhandler c_error_handler;
 
-   /* Hooks for PML to hang things */
+    /* Hooks for PML to hang things */
+
     struct mca_pml_comm_t* c_pml_comm;
 
-#if 0
-   /* Hooks for collectives to hang things */
-    mca_coll_t c_coll;
+    /* Hooks for collectives to hang things */
+
+    mca_coll_1_0_0_t c_coll;
     struct mca_coll_comm_t* c_coll_comm;
-#endif
-};
-
-typedef struct lam_communicator_t lam_communicator_t;
-
+} lam_communicator_t;
 
 #endif /* LAM_COMMUNICATOR_H */
-
