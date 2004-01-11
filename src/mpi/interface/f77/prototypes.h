57584d36c37a316512eb01d1fce88e7e58112831
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-01-11 21:33:35 +0000

    Add some missing prototypes

--- src/mpi/interface/f77/prototypes.h
+++ src/mpi/interface/f77/prototypes.h
@@ -26,34 +26,54 @@
 /*
  * Zeroth, the LAM wrapper functions, with a "_f" suffix.
  */
+void mpi_alloc_mem_f(int *size, int *info, char *baseptr, int *ierr);
+void mpi_comm_get_name_f(int *comm, char *name, int *l, int *ierror, 
+                         int charlen);
 void mpi_comm_set_name_f(int *comm, char *name, int *ierror, int charlen);
 void mpi_init_f(int *ierror);
 void mpi_finalize_f(int *ierror);
+void mpi_free_mem_f(char *baseptr, int *ierr);
 
 /*
  * First, all caps.
  */
+void MPI_ALLOC_MEM(int *size, int *info, char *baseptr, int *ierr);
+void MPI_COMM_GET_NAME(int *comm, char *name, int *l, int *ierror, 
+                       int charlen);
 void MPI_COMM_SET_NAME(int *comm, char *name, int *ierror, int charlen);
 void MPI_INIT(void *ierror);
 void MPI_FINALIZE(int *ierror);
+void MPI_FREE_MEM(char *baseptr, int *ierr);
 
 /*
  * Second, all lower case.
  */
+void mpi_alloc_mem(int *size, int *info, char *baseptr, int *ierr);
+void mpi_comm_get_name(int *comm, char *name, int *l, int *ierror, 
+                       int charlen);
 void mpi_comm_set_name(int *comm, char *name, int *ierror, int charlen);
 void mpi_init(int *ierror);
 void mpi_finalize(int *ierror);
+void mpi_free_mem(char *baseptr, int *ierr);
 
 /*
  * Third, one trailing underscore.
  */
+void mpi_alloc_mem_(int *size, int *info, char *baseptr, int *ierr);
+void mpi_comm_get_name_(int *comm, char *name, int *l, int *ierror, 
+                        int charlen);
 void mpi_comm_set_name_(int *comm, char *name, int *ierror, int charlen);
 void mpi_init_(int *ierror);
 void mpi_finalize_(int *ierror);
+void mpi_free_mem_(char *baseptr, int *ierr);
 
 /*
  * Fourth, two trailing underscores.
  */
+void mpi_alloc_mem__(int *size, int *info, char *baseptr, int *ierr);
+void mpi_comm_get_name__(int *comm, char *name, int *l, int *ierror, 
+                         int charlen);
 void mpi_comm_set_name__(int *comm, char *name, int *ierror, int charlen);
 void mpi_init__(int *ierror);
 void mpi_finalize__(int *ierror);
+void mpi_free_mem__(char *baseptr, int *ierr);
