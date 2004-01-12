59d1aef3ee484806b62906a9c942f673bbbc6104
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-01-12 02:07:42 +0000

    preventing C++ name mangling

--- src/lam/util/path.h
+++ src/lam/util/path.h
@@ -5,9 +5,16 @@
 #ifndef LAM_PATH_H
 #define LAM_PATH_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char   *lam_path_find (char *fname, char **pathv, int mode);
 char   *lam_path_env_find (char *fname, int mode);
 char   *lam_path_findv (char *fname, char **pathv, int mode, char **envv);
 char   *lam_path_env_findv (char *fname, int mode, char **envv, char *wrkdir);
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* LAM_PATH_H */
