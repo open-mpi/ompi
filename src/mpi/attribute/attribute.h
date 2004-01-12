f4896b6ed9369abbcbf6922f2896fadb18a55f02
Jeff Squyres <jsquyres@cisco.com>
2004-01-12 16:08:46 +0000

    Add empty defintions for MPI errhandlers and attributes; they'll likely be shared across several MPI opaque datatypes.
    This commit was SVN r297.

---
+++ src/mpi/attribute/attribute.h
@@ -0,0 +1,16 @@
+/*
+ * $HEADER$
+ */
+
+#ifndef LAM_ATTRIBUTE_H
+#define LAM_ATTRIBUTE_H
+
+#include "lam_config.h"
+
+
+struct lam_attribute_t {
+  /* JMS Put stuff here */
+};
+typedef struct lam_attribute_t lam_attribute_t;
+
+#endif /* LAM_ATTRIBUTE_H */
---
+++ src/mpi/errhandler/errhandler.h
@@ -0,0 +1,16 @@
+/*
+ * $HEADER$
+ */
+
+#ifndef LAM_ERRHANDLER_H
+#define LAM_ERRHANDLER_H
+
+#include "lam_config.h"
+
+
+struct lam_errhandler_t {
+  /* JMS Put stuff here */
+};
+typedef struct lam_errhandler_t lam_errhandler_t;
+
+#endif /* LAM_ERRHANDLER_H */
