f68f52fdda70a0e4e667cf767e406268b25466da
Prabhanjan Kambadur <pkambadu@osl.iu.edu>
2004-01-12 02:07:42 +0000

    preventing C++ name mangling
    This commit was SVN r289.

--- src/lam/util/few.h
+++ src/lam/util/few.h
@@ -4,7 +4,13 @@
 
 #ifndef LAM_FEW_H
 #define LAM_FEW_H
-
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
 int lam_few(char *argv[], int *status);
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* LAM_FEW_H */
