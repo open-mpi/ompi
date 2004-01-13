5bfddeed7e5815fc24909c7810186cf89d870a10
Tim Woodall <twoodall@lanl.gov>
2004-01-13 02:31:38 +0000

    rename mtx to mutex
    This commit was SVN r317.

--- src/lam/threads/mutex_spinlock.h
+++ src/lam/threads/mutex_spinlock.h
@@ -8,10 +8,10 @@
 #include "lam/os/atomic.h"
 typedef lam_lock_data_t lam_mutex_t;
 
-#define lam_mtx_init(m)    spinunlock(m)
-#define lam_mtx_lock(m)    spinlock(m)
-#define lam_mtx_trylock(m) spintrylock(m)
-#define lam_mtx_unlock(m)  spinunlock(m)
+#define lam_mutex_init(m)    spinunlock(m)
+#define lam_mutex_lock(m)    spinlock(m)
+#define lam_mutex_trylock(m) spintrylock(m)
+#define lam_mutex_unlock(m)  spinunlock(m)
 
 #endif
 
