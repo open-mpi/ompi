63355808d7b1fda2949a0fa3d5f03c89687618a9
Tim Woodall <twoodall@open-mpi-git-mirror.example.com>
2004-01-14 03:44:11 +0000

    specific to PTL interface

---
+++ src/mca/mpi/ptl/base/ptl_base_comm.h
@@ -0,0 +1,47 @@
+#ifndef MCA_PML_COMM_H
+#define MCA_PML_COMM_H
+
+#include "lam/threads/mutex.h"
+#include "mca/mpi/ptl/ptl.h"
+#include "lam/lfc/list.h"
+
+/*
+ *  Structure associated w/ lam_communicator_t that contains data 
+ *  specific to the PML. 
+ */
+
+extern lam_class_info_t mca_ptl_comm_cls;
+
+struct mca_pml_comm_t {
+    
+    /* send message sequence-number support - sender side */
+    mca_ptl_base_sequence_t *c_msg_seq_num;
+
+    /* send message sequence-number support - receiver side */
+    mca_ptl_base_sequence_t *c_next_msg_seq_num;
+
+    /* matching lock */
+    lam_mutex_t *c_matching_lock;
+
+    /* unexpected fragments queues */
+    lam_list_t *unexpected_frags;
+    /* these locks are needed to avoid a probe interfering with a match
+     */
+    lam_mutex_t *unexpected_frags_lock;
+
+    /* out-of-order fragments queues */
+    lam_list_t *frags_cant_match;
+
+    /* queues of unmatched specific (source process specified) receives
+     * sorted by source process */
+    lam_list_t *specific_receives;
+
+    /* queue of unmatched wild (source process not specified) receives
+     * */
+    lam_list_t wild_receives;
+};
+typedef struct mca_pml_comm_t mca_pml_comm_t;
+
+
+#endif
+
