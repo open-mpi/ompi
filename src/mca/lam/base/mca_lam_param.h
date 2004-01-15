6a3d4011a437875f4367e9a708ced59b30141de5
Jeff Squyres <jsquyres@cisco.com>
2004-01-15 21:47:30 +0000

    rename param.h -> mca_lam_param.h
    This commit was SVN r434.

--- src/mca/lam/base/mca_lam_param.h
+++ src/mca/lam/base/mca_lam_param.h
@@ -14,42 +14,12 @@
  * Types for MCA parameters
  */
 
-typedef enum {
-  MCA_BASE_PARAM_TYPE_INT,
-  MCA_BASE_PARAM_TYPE_STRING,
-
-  MCA_BASE_PARAM_TYPE_MAX
-} mca_base_param_type_t;
-
 typedef union {
   int intval;
   char *stringval;
 } mca_base_param_storage_t;
 
 #define MCA_BASE_PARAM_INFO ((void*) -1)
-
-struct mca_base_param_t {
-  lam_array_item_t super;
-
-  mca_base_param_type_t mbp_type;
-  char *mbp_type_name;
-  char *mbp_module_name;
-  char *mbp_param_name;
-  char *mbp_full_name;
-
-  int mbp_keyval;
-  char *mbp_env_var_name;
-
-  mca_base_param_storage_t mbp_default_value;
-};
-typedef struct mca_base_param_t mca_base_param_t;
-
-
-/*
- * Variable holding the array of registered MCA parameters
- */
-
-extern lam_array_t mca_base_params;
 
 
 /*
@@ -59,16 +29,20 @@ extern lam_array_t mca_base_params;
 #if defined(c_plusplus) || defined(__cplusplus)
 extern "C" {
 #endif
-  int mca_base_param_register_int(const char *type_name, const char *module_name,
-                                  const char *param_name, const char *mca_param_name,
+  int mca_base_param_register_int(const char *type_name, 
+                                  const char *module_name,
+                                  const char *param_name, 
+                                  const char *mca_param_name,
                                   int default_value);
-  int mca_base_param_register_string(const char *type_name, const char *module_name,
+  int mca_base_param_register_string(const char *type_name, 
+                                     const char *module_name,
                                      const char *param_name, 
                                      const char *mca_param_name,
                                      const char *default_value);
   int mca_base_param_lookup_int(int index, int *value);
   int mca_base_param_lookup_string(int index, char **value);
-  int mca_base_param_find(const char *type, const char *module, const char *param);
+  int mca_base_param_find(const char *type, const char *module, 
+                          const char *param);
   int mca_base_param_finalize(void);
 
 #if 0
