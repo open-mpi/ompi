d2f7c543e97d824b73293daabef025f42f2191f4
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-01-15 06:10:28 +0000

    Call back-end finalize function

--- src/mpi/interface/c/finalize.c
+++ src/mpi/interface/c/finalize.c
@@ -6,14 +6,16 @@
 
 #include "mpi.h"
 #include "mpi/interface/c/bindings.h"
+#include "mpi/runtime/runtime.h"
 
 #if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
 #pragma weak MPI_Finalize = PMPI_Finalize
 #endif
 
 
-int
-MPI_Finalize(void)
+int MPI_Finalize(void)
 {
-  return MPI_SUCCESS;
+  /* Pretty simple */
+
+  return lam_mpi_finalize();
 }
