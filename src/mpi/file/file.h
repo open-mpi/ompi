e20c037b5660c5a6b5d3f4b8e30b86902ed335aa
Mark Taylor <mt@lanl.gov>
2004-01-16 21:59:05 +0000

    modified file.h so that lam_file_t is handled in the same was as lam_request_t (modules add to the struct and do the malloc)
    This commit was SVN r450.

--- src/mpi/file/file.h
+++ src/mpi/file/file.h
@@ -8,13 +8,21 @@
 #include "mpi.h"
 #include "mca/mpi/io/io.h"
 
+typedef enum {
+    LAM_IO_1_0_0,
+    LAM_IO_2_0_0
+} lam_io_version_t;
+
+
 struct lam_file_t {
    char f_name[MPI_MAX_OBJECT_NAME];
+    lam_io_version_t lam_io_version;
 
     /* Hooks for io modules to hang things */
+    union {
+        mca_io_1_0_0_t f_io;
+    } mca_io_functions;
 
-    mca_io_1_0_0_t f_io;
-    struct mca_io_file_t* f_io_file;
 
 };
 typedef struct lam_file_t lam_file_t;
