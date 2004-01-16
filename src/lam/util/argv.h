a3316f27129e977974e9244c61912249a95b87fa
Jeff Squyres <jsquyres@cisco.com>
2004-01-16 01:33:12 +0000

    - Use "const" for regular strings in judicious places - Use LAM_MALLOC and LAM_FREE - Use NULL instead of 0
    This commit was SVN r445.

--- src/lam/util/argv.h
+++ src/lam/util/argv.h
@@ -10,9 +10,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-  int lam_argv_add(int *argc, char ***argv, char *arg);
+  int lam_argv_add(int *argc, char ***argv, const char *arg);
   void lam_argv_free(char **argv);
-  char **lam_argv_split(char *src_string, int delimiter);
+  char **lam_argv_split(const char *src_string, int delimiter);
   int lam_argv_count(char **argv);
   char *lam_argv_join(char **argv, int delimiter);
   size_t lam_argv_len(char **argv);
