6a78265b2f012c93ecdcffd70e8cae2ea14d1e0d
Prabhanjan Kambadur <pkambadu@osl.iu.edu>
2004-01-20 06:30:33 +0000

    Adding the shell functions for barrier, bcast, bsend, bsend_init, buffer_attach and buffer_detach
    This commit was SVN r492.

---
+++ src/mpi/interface/c/barrier.c
@@ -0,0 +1,16 @@
+/*
+ * $HEADERS$
+ */
+#include "lam_config.h"
+#include <stdio.h>
+
+#include "mpi.h"
+#include "mpi/interface/c/bindings.h"
+
+#if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
+#pragma weak MPI_Barrier = PMPI_Barrier
+#endif
+
+int MPI_Barrier(MPI_Comm comm) {
+    return MPI_SUCCESS;
+}
---
+++ src/mpi/interface/c/buffer_attach.c
@@ -0,0 +1,17 @@
+/*
+ * $HEADERS$
+ */
+#include "lam_config.h"
+#include <stdio.h>
+
+#include "mpi.h"
+#include "mpi/interface/c/bindings.h"
+
+#if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
+#pragma weak MPI_Buffer_attach = PMPI_Buffer_attach
+#endif
+
+
+int MPI_Buffer_attach(void *buffer, int size) {
+    return MPI_SUCCESS;
+}
