3986e79323cc340951a9fb3cf381c3ea99937b31
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-01-25 23:03:40 +0000

    adding new functions:k and l

---
+++ src/mpi/interface/c/keyval_free.c
@@ -0,0 +1,16 @@
+/*
+ * $HEADER$
+ */
+
+#include "lam_config.h"
+
+#include "mpi.h"
+#include "mpi/interface/c/bindings.h"
+
+#if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
+#pragma weak MPI_Keyval_free = PMPI_Keyval_free
+#endif
+
+int MPI_Keyval_free(int *keyval) {
+    return MPI_SUCCESS;
+}
