87271e3db012a979ec6e66cc27cc7badf25e09b9
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-01-29 19:36:34 +0000

    Add instances of lam_mpi_comm_world and lam_mpi_comm_self

---
+++ src/mpi/communicator/mpi_comm_globals.c
@@ -0,0 +1,18 @@
+/*
+ * $HEADER$
+ */
+
+#include "lam_config.h"
+
+#include <stdio.h>
+
+#include "mpi.h"
+#include "mpi/communicator/communicator.h"
+
+
+/*
+ * Global variables
+ */
+
+lam_communicator_t lam_mpi_comm_world;
+lam_communicator_t lam_mpi_comm_self;
