ede4459f9af7e9aac5ad213602e319f0a24e03db
Jeff Squyres <jsquyres@cisco.com>
2004-01-29 02:49:32 +0000

    Add ptl base open and close functions; ensure that ptl base is compiled into the rollup MCA MPI convenience library
    This commit was SVN r573.

---
+++ src/mca/mpi/ptl/base/ptl_base_close.c
@@ -0,0 +1,29 @@
+/*
+ * $HEADER$
+ */
+
+#include "lam_config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lam/constants.h"
+#include "mca/mca.h"
+#include "mca/lam/base/base.h"
+#include "mca/mpi/ptl/ptl.h"
+
+
+int mca_ptl_base_close(void)
+{
+  extern lam_list_t mca_ptl_base_modules_available;
+
+  /* Close all remaining available modules (may be one if this is a
+     LAM RTE program, or [possibly] multiple if this is laminfo) */
+
+  mca_base_modules_close(mca_ptl_base_output, 
+                         &mca_ptl_base_modules_available, NULL);
+
+  /* All done */
+
+  return LAM_SUCCESS;
+}
