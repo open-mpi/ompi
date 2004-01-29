4e0c6ceef7722dbadfa83818fb596852d3304443
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-01-29 02:35:33 +0000

    Add empty shell functions so that we can link

---
+++ src/mca/lam/base/mca_base_module_exchange.c
@@ -0,0 +1,24 @@
+/*
+ * $HEADER$
+ */
+
+#include "lam_config.h"
+
+#include "mpi/proc/proc.h"
+#include "mca/mca.h"
+#include "mca/lam/base/mca_base_module_exchange.h"
+
+
+int mca_base_modex_send(mca_base_module_t *source_module, 
+                        void *buffer, size_t size, int count)
+{
+  return LAM_ERR_NOT_IMPLEMENTED;
+}
+
+
+int mca_base_modex_recv(mca_base_module_t *dest_module,
+                        lam_proc_t *source_proc,
+                        void **buffer, size_t *size, int *count)
+{
+  return LAM_ERR_NOT_IMPLEMENTED;
+}
