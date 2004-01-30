161e021562db3ed7bc0031b9ccf68a74555eb6fd
Mark Taylor <mt@lanl.gov>
2004-01-30 23:14:19 +0000

    ROMIO MCA component:
        completed all MPI-IO MCA v1 prototypes
        completed all MPI-IO MCA v1 typedefs
        add all functions to MCA v1 action struct
    
        completed about 50% of the wrapper functions.
    
    This commit was SVN r621.

--- src/mca/mpi/io/romio/src/file_open.c
+++ src/mca/mpi/io/romio/src/file_open.c
@@ -32,5 +32,218 @@ int mca_io_romio_File_open(MPI_Comm comm, char *filename, int amode,
 }
     
 
+int mca_io_romio_File_close(MPI_File *fh) {
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *)(*fh);
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_close(&romio_fh);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    LAM_FREE(*fh);
+
+    return ret;
+}
+
+
+
+int mca_io_romio_File_delete(char *filename, MPI_Info info) {
+    int ret;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_delete(filename, info);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+
+    return ret;
+}
+
+
+int mca_io_romio_File_set_size(MPI_File fh, MPI_Offset size){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_set_size(romio_fh, size);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+
+
+}
+
+int mca_io_romio_File_preallocate(MPI_File fh, MPI_Offset size){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_preallocate(romio_fh,size);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_File_get_size(MPI_File fh, MPI_Offset *size){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_get_size(romio_fh,size);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_File_get_group(MPI_File fh, MPI_Group *group){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_get_group(romio_fh,group);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_File_get_amode(MPI_File fh, int *amode){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_get_amode(romio_fh, amode);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_File_set_info(MPI_File fh, MPI_Info info){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_set_info(romio_fh,info);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_File_get_info(MPI_File fh, MPI_Info *info_used){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_get_info(romio_fh,info_used);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_File_set_view(MPI_File fh, MPI_Offset disp, MPI_Datatype etype,
+                               MPI_Datatype filetype, char *datarep, MPI_Info info){
+
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_set_view(romio_fh,disp,etype,filetype,datarep,info);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+
+
+}
+int mca_io_romio_File_get_view(MPI_File fh, MPI_Offset *disp, MPI_Datatype *etype, MPI_Datatype *filetype, char *datarep){
+    int ret;
+    mca_io_romio_MPI_File romio_fh;
+    mca_io_romio_file_t *mca_romio_fh;  
+
+    /* extract the ROMIO file handle: */
+    mca_romio_fh = (mca_io_romio_file_t *) fh;
+    romio_fh = mca_romio_fh->romio_fh;
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPI_File_get_view(romio_fh,disp,etype,filetype,datarep);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+
+}
+
+
+
+
+int mca_io_romio_File_get_type_extent(MPI_File fh, MPI_Datatype datatype, 
+                                     MPI_Aint *extent){ }
+
+
+int mca_io_romio_File_set_atomicity(MPI_File fh, int flag){ }
+int mca_io_romio_File_get_atomicity(MPI_File fh, int *flag){ }
+int mca_io_romio_File_sync(MPI_File fh){ }
+
+
+
+
+
+int mca_io_romio_File_seek_shared(MPI_File fh, MPI_Offset offset, int whence){ }
+int mca_io_romio_File_get_position_shared(MPI_File fh, MPI_Offset *offset){ }
+
+int mca_io_romio_File_seek(MPI_File fh, MPI_Offset offset, int whence){ }
+int mca_io_romio_File_get_position(MPI_File fh, MPI_Offset *offset){ }
+int mca_io_romio_File_get_byte_offset(MPI_File fh, MPI_Offset offset, 
+                                     MPI_Offset *disp){ }
+
+
+int mca_io_romio_File_set_errhandler(MPI_File fh, MPI_Errhandler eh){ }
+int mca_io_romio_File_get_errhandler(MPI_File fh, MPI_Errhandler *eh ){ }
 
 
--- src/mca/mpi/io/romio/src/romio-1.2.5.1/include/mpio.h
+++ src/mca/mpi/io/romio/src/romio-1.2.5.1/include/mpio.h
@@ -1,21 +1,147 @@
 /*
  * $HEADER
+ *
+ * The actual ROMIO routines.
+ * all of these routines have been renamed: prefix with mca_io_romio
+ *
+ * ROMIO specific types have also been renamed:  (cant do this with #define
+ * since this file is included by LAM's I/O module)
+ * all ROMIO references to:
+ *    MPIO_Request changed to:  mca_io_romio_MPIO_Request
+ *    MPI_File     changed to:  mca_io_romio_MPI_File
+ *
+ *  All other types have been deleted, since they appear in LAM's mpi.h
+ *  
  */
 
 #ifndef MCA_IO_ROMIO_DUMMY_H
 #define MCA_IO_ROMIO_DUMMY_H
 
-
-typedef char *mca_io_romio_MPIO_Request;
-typedef char *mca_io_romio_MPI_File;
-
-
-int mca_io_romio_MPI_File_iwrite(mca_io_romio_MPI_File, void *,
-                             int, MPI_Datatype, mca_io_romio_MPIO_Request);
+typedef struct ADIOI_FileD *mca_io_romio_MPI_File;
+typedef struct ADIOI_RequestD *mca_io_romio_MPIO_Request;  
 
 
+
+
+
+/* Section 9.2 */
 int mca_io_romio_MPI_File_open(MPI_Comm comm, char *filename, int amode,
-                         MPI_Info info, mca_io_romio_MPI_File *fh);
+                          MPI_Info info, mca_io_romio_MPI_File *fh);
+int mca_io_romio_MPI_File_close(mca_io_romio_MPI_File *fh);
+int mca_io_romio_MPI_File_delete(char *filename, MPI_Info info);
+int mca_io_romio_MPI_File_set_size(mca_io_romio_MPI_File fh, MPI_Offset size);
+int mca_io_romio_MPI_File_preallocate(mca_io_romio_MPI_File fh, MPI_Offset size);
+int mca_io_romio_MPI_File_get_size(mca_io_romio_MPI_File fh, MPI_Offset *size);
+int mca_io_romio_MPI_File_get_group(mca_io_romio_MPI_File fh, MPI_Group *group);
+int mca_io_romio_MPI_File_get_amode(mca_io_romio_MPI_File fh, int *amode);
+int mca_io_romio_MPI_File_set_info(mca_io_romio_MPI_File fh, MPI_Info info);
+int mca_io_romio_MPI_File_get_info(mca_io_romio_MPI_File fh, MPI_Info *info_used);
+
+
+
+
+/* Section 9.3 */
+int mca_io_romio_MPI_File_set_view(mca_io_romio_MPI_File fh, MPI_Offset disp, MPI_Datatype etype,
+	         MPI_Datatype filetype, char *datarep, MPI_Info info);
+int mca_io_romio_MPI_File_get_view(mca_io_romio_MPI_File fh, MPI_Offset *disp, 
+                 MPI_Datatype *etype, MPI_Datatype *filetype, char *datarep);
+
+/* Section 9.4.2 */
+int mca_io_romio_MPI_File_read_at(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+	      int count, MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_read_at_all(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+	      int count, MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_write_at(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+	      int count, MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_write_at_all(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+	      int count, MPI_Datatype datatype, MPI_Status *status);
+
+
+int mca_io_romio_MPI_File_iread_at(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+	      int count, MPI_Datatype datatype, mca_io_romio_MPIO_Request *request);
+int mca_io_romio_MPI_File_iwrite_at(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+	      int count, MPI_Datatype datatype, mca_io_romio_MPIO_Request *request);
+
+
+/* Section 9.4.3 */
+int mca_io_romio_MPI_File_read(mca_io_romio_MPI_File fh, void *buf, int count, MPI_Datatype
+	     datatype, MPI_Status *status); 
+int mca_io_romio_MPI_File_read_all(mca_io_romio_MPI_File fh, void *buf, int count, MPI_Datatype
+	     datatype, MPI_Status *status); 
+int mca_io_romio_MPI_File_write(mca_io_romio_MPI_File fh, void *buf, int count, MPI_Datatype
+	      datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_write_all(mca_io_romio_MPI_File fh, void *buf, int count, MPI_Datatype
+	      datatype, MPI_Status *status);
+
+
+int mca_io_romio_MPI_File_iread(mca_io_romio_MPI_File fh, void *buf, int count, MPI_Datatype
+	     datatype, mca_io_romio_MPIO_Request *request); 
+int mca_io_romio_MPI_File_iwrite(mca_io_romio_MPI_File fh, void *buf, int count, 
+                             MPI_Datatype datatype, mca_io_romio_MPIO_Request *request);
+
+
+int mca_io_romio_MPI_File_seek(mca_io_romio_MPI_File fh, MPI_Offset offset, int whence);
+int mca_io_romio_MPI_File_get_position(mca_io_romio_MPI_File fh, MPI_Offset *offset);
+int mca_io_romio_MPI_File_get_byte_offset(mca_io_romio_MPI_File fh, MPI_Offset offset, 
+                                     MPI_Offset *disp);
+
+/* Section 9.4.4 */
+int mca_io_romio_MPI_File_read_shared(mca_io_romio_MPI_File fh, void *buf, int count, 
+                         MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_write_shared(mca_io_romio_MPI_File fh, void *buf, int count, 
+                          MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_iread_shared(mca_io_romio_MPI_File fh, void *buf, int count, 
+                          MPI_Datatype datatype, mca_io_romio_MPIO_Request *request);
+int mca_io_romio_MPI_File_iwrite_shared(mca_io_romio_MPI_File fh, void *buf, int count, 
+                           MPI_Datatype datatype, mca_io_romio_MPIO_Request *request);
+int mca_io_romio_MPI_File_read_ordered(mca_io_romio_MPI_File fh, void *buf, int count, 
+                          MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_write_ordered(mca_io_romio_MPI_File fh, void *buf, int count, 
+                           MPI_Datatype datatype, MPI_Status *status);
+int mca_io_romio_MPI_File_seek_shared(mca_io_romio_MPI_File fh, MPI_Offset offset, int whence);
+int mca_io_romio_MPI_File_get_position_shared(mca_io_romio_MPI_File fh, MPI_Offset *offset);
+
+/* Section 9.4.5 */
+int mca_io_romio_MPI_File_read_at_all_begin(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+                               int count, MPI_Datatype datatype);
+int mca_io_romio_MPI_File_read_at_all_end(mca_io_romio_MPI_File fh, void *buf, MPI_Status *status);
+int mca_io_romio_MPI_File_write_at_all_begin(mca_io_romio_MPI_File fh, MPI_Offset offset, void *buf,
+                                int count, MPI_Datatype datatype);
+int mca_io_romio_MPI_File_write_at_all_end(mca_io_romio_MPI_File fh, void *buf, MPI_Status *status);
+int mca_io_romio_MPI_File_read_all_begin(mca_io_romio_MPI_File fh, void *buf, int count, 
+                            MPI_Datatype datatype);
+int mca_io_romio_MPI_File_read_all_end(mca_io_romio_MPI_File fh, void *buf, MPI_Status *status);
+int mca_io_romio_MPI_File_write_all_begin(mca_io_romio_MPI_File fh, void *buf, int count, 
+                             MPI_Datatype datatype);
+int mca_io_romio_MPI_File_write_all_end(mca_io_romio_MPI_File fh, void *buf, MPI_Status *status);
+int mca_io_romio_MPI_File_read_ordered_begin(mca_io_romio_MPI_File fh, void *buf, int count, 
+                                MPI_Datatype datatype);
+int mca_io_romio_MPI_File_read_ordered_end(mca_io_romio_MPI_File fh, void *buf, MPI_Status *status);
+int mca_io_romio_MPI_File_write_ordered_begin(mca_io_romio_MPI_File fh, void *buf, int count, 
+                                 MPI_Datatype datatype);
+int mca_io_romio_MPI_File_write_ordered_end(mca_io_romio_MPI_File fh, void *buf, MPI_Status *status);
+
+/* Section 9.5.1 */
+int mca_io_romio_MPI_File_get_type_extent(mca_io_romio_MPI_File fh, MPI_Datatype datatype, 
+                                     MPI_Aint *extent);
+
+/* Section 9.6.1 */
+int mca_io_romio_MPI_File_set_atomicity(mca_io_romio_MPI_File fh, int flag);
+int mca_io_romio_MPI_File_get_atomicity(mca_io_romio_MPI_File fh, int *flag);
+int mca_io_romio_MPI_File_sync(mca_io_romio_MPI_File fh);
+
+/* Section 4.13.3 */
+int mca_io_romio_MPI_File_set_errhandler(mca_io_romio_MPI_File, MPI_Errhandler );
+int mca_io_romio_MPI_File_get_errhandler(mca_io_romio_MPI_File, MPI_Errhandler * );
+/* End Prototypes */
+
+
+int mca_io_romio_MPIO_Test(mca_io_romio_MPIO_Request *request, int *flag, MPI_Status *status);
+int mca_io_romio_MPIO_Wait(mca_io_romio_MPIO_Request *request, MPI_Status *status);
+MPI_Fint mca_io_romio_MPIO_Request_c2f(mca_io_romio_MPIO_Request request);
+mca_io_romio_MPIO_Request mca_io_romio_MPIO_Request_f2c(MPI_Fint request);
+
+
 
 
 #endif
