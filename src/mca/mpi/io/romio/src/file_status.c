eec1c9344fe608b469c115e98cabd65092c4ad98
Mark Taylor <mt@open-mpi-git-mirror.example.com>
2004-01-31 06:05:28 +0000

    more romio MCA wrapper functions

---
+++ src/mca/mpi/io/romio/src/file_status.c
@@ -0,0 +1,47 @@
+/*
+ *  $HEADER$
+ */
+
+#include "mpi.h"
+#include "mpi/file/file.h"
+#include "io_romio.h"
+#include "mpi/request/request.h"
+#include "lam/mem/malloc.h"
+#include <string.h>
+
+
+int mca_io_romio_Test(MPI_Request *request, int *flag, MPI_Status *status){ 
+    int ret;
+    mca_io_romio_request_t *rq;
+    mca_io_romio_MPIO_Request romio_rq;
+
+    /* extract the ROMIO request */
+    rq=(mca_io_romio_request_t *)(*request);
+    romio_rq = rq->romio_rq;
+
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPIO_Test(&romio_rq, flag,status);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
+
+int mca_io_romio_Wait(MPI_Request *request, MPI_Status *status){ 
+    int ret;
+    mca_io_romio_request_t *rq;
+    mca_io_romio_MPIO_Request romio_rq;
+
+    /* extract the ROMIO request */
+    rq=(mca_io_romio_request_t *)(*request);
+    romio_rq = rq->romio_rq;
+
+
+    THREAD_LOCK(&mca_io_romio_mutex);
+    ret=mca_io_romio_MPIO_Wait(&romio_rq, status);
+    THREAD_UNLOCK(&mca_io_romio_mutex);
+  
+    return ret;
+}
+
