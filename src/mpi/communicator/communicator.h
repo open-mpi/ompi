65e32a8514a29b16fd23d2dc5f1a004bf28b1ac3
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-02-05 01:52:56 +0000

    Move global variables into files with functions in them.  POSIX says that linkers don't have to pull in .o files unless they have functions in them, so having global symbols in files by themselves does not guarantee that a program trying to use them will be able to link successfully.

--- src/mpi/communicator/communicator.h
+++ src/mpi/communicator/communicator.h
@@ -69,4 +69,12 @@ static inline lam_proc_t* lam_comm_lookup_peer(lam_communicator_t* comm, size_t 
     return comm->c_remote_group->g_procs[peer_id];
 }
 
+#if defined(c_plusplus) || defined(__cplusplus)
+extern "C" {
+#endif
+  int lam_comm_init(lam_communicator_t *comm);
+#if defined(c_plusplus) || defined(__cplusplus)
+}
+#endif
+
 #endif /* LAM_COMMUNICATOR_H */
