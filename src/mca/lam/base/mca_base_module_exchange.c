50d1f847710a6ecb4d0a770be9ac82830a9ad47b
David Daniel <ddd@open-mpi-git-mirror.example.com>
2004-02-10 21:52:00 +0000

    Making count size_t instead of int

--- src/mca/lam/base/mca_base_module_exchange.c
+++ src/mca/lam/base/mca_base_module_exchange.c
@@ -10,7 +10,7 @@
 
 
 int mca_base_modex_send(mca_base_module_t *source_module, 
-                        void *buffer, size_t size, int count)
+                        void *buffer, size_t size, size_t count)
 {
   return LAM_ERR_NOT_IMPLEMENTED;
 }
@@ -18,7 +18,7 @@ int mca_base_modex_send(mca_base_module_t *source_module,
 
 int mca_base_modex_recv(mca_base_module_t *dest_module,
                         lam_proc_t *source_proc,
-                        void **buffer, size_t *size, int *count)
+                        void **buffer, size_t *size, size_t *count)
 {
   return LAM_ERR_NOT_IMPLEMENTED;
 }
