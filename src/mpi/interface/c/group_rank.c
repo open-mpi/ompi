771d7efbd7a27ca47eed02ed2fa5a8938a333635
Rich Graham <rlgraham@open-mpi-git-mirror.example.com>
2004-02-13 16:41:53 +0000

    implement mpi_group_rank mpi_group_size.

--- src/mpi/interface/c/group_rank.c
+++ src/mpi/interface/c/group_rank.c
@@ -6,11 +6,13 @@
 
 #include "mpi.h"
 #include "mpi/interface/c/bindings.h"
+#include "mpi/group/group.h"
 
 #if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
 #pragma weak MPI_Group_rank = PMPI_Group_rank
 #endif
 
 int MPI_Group_rank(MPI_Group group, int *rank) {
+    *rank=lam_group_rank((lam_group_t *)group);
     return MPI_SUCCESS;
 }
