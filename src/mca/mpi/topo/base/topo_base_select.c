900a39d2eb53fdf417e6cbcc8f462eba23a3d4de
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-03-07 23:53:21 +0000

    making changes to topo_base_select and topo.h so that they are more in conformance with the MCA framework

--- src/mca/mpi/topo/base/topo_base_select.c
+++ src/mca/mpi/topo/base/topo_base_select.c
@@ -22,19 +22,27 @@
 struct opened_module_t {
     lam_list_item_t super;
     mca_topo_base_module_t *om_module;
+    mca_topo_t *om_actions;
 };
 typedef struct opened_module_t opened_module_t;
 
 /*
  * Only one topo module can be attached to each communicator.
  *
- * This function calls the init function on all the modules which
- * were opened by mca_base_modules_open. init function for each 
- * module returns the priority for each module and hence this
- * function is able to select on particular module from a plethora
- * of modules available to implement topology. The selected module 
- * will have all its function pointers saved and returned to the
- * caller
+ * This module calls the query funtion on all the modules 
+ * that were detected by topo_base_open. This function is 
+ * called on a per-communicator basis. This function has the 
+ * following function. 
+ *
+ * 1. Iterate over the list of available_modules
+ * 2. Call the query function on each of these modules.
+ * 3. query function returns the structure containing pointers
+ * to its functions and the priority of this module. 
+ * 4. Select the module with the highest priority
+ * 5. Call the init function on its actions so that it does the
+ *    right setup for the communicator
+ * 6. Call finalize on all the other modules which returned 
+ * their actions but were unfortunate to not get selected
  */  
 
 int mca_topo_base_select (mca_topo_t *selected, 
@@ -57,6 +65,8 @@ int mca_topo_base_select (mca_topo_t *selected,
      */
     best_module = NULL;
     best_priority = -1;
+    OBJ_CONSTRUCT(&opened, lam_list_t);
+
     for (item = lam_list_get_first(&mca_topo_base_modules_available);
          item != lam_list_get_end(&mca_topo_base_modules_available);
          item = lam_list_get_next(item)) {
@@ -71,27 +81,33 @@ int mca_topo_base_select (mca_topo_t *selected,
                           module->topom_version.mca_type_name,
                           module->topom_version.mca_module_name);
 
-       if (NULL == module->topom_int) {
-          /*
-           * there is no initialisation function in this module
-           */
+       /*
+        * we can call the query function only if there is a function :-)
+        */
+       if (NULL == module->topom_query) {
           lam_output_verbose(10, mca_topo_base_output,
-                             "select: no init, ignoring the module");
+                             "select: no query, ignoring the module");
        } else {
-           actions = module->topom_init (&priority,
-                                         &user_threads,
-                                         &hidden_threads);
+           /*
+            * call the query function and see what it returns
+            */ 
+           actions = module->topom_query (&priority,
+                                          &user_threads,
+                                          &hidden_threads);
            if (NULL == actions) {
+               /*
+                * query did not return any action which can be used
+                */ 
                lam_output_verbose(10, mca_topo_base_module,
-                                  "select: init returned failure");
+                                  "select: query returned failure");
            } else {
                lam_output_verbose(10, mca_topo_base_module,
-                                  "select: init returned priority &d",
+                                  "select: query returned priority &d",
                                   priority);
+               /* 
+                * is this the best module we have found till now
+                */
                if (priority > best_priority) {
-                   /* 
-                    * this module is the best we have found till now
-                    */
                    best_priority = priority;
                    best_user_threads = user_threads;
                    best_hidden_threads = hidden_threads;
@@ -99,14 +115,15 @@ int mca_topo_base_select (mca_topo_t *selected,
                }
 
                om = (opened_module_t *) malloc(sizeof(opened_module_t)); 
+               /*
+                * check if we have run out of space
+                */
                if (NULL == om) {
-                   /*
-                    * out of space I guess
-                    */
                    return LAM_ERR_OUT_OF_RESOURCE;
                }
                OBJ_CONSTRUCT(om, lam_list_item_t);
                om->om_module = module;
+               om->om_actions = actions; 
                lam_list_append(&opened, (lam_list_item_t *)om); 
            } /* end else of if (NULL == actions) */
        } /* end else of if (NULL == module->topom_init) */
@@ -123,19 +140,47 @@ int mca_topo_base_select (mca_topo_t *selected,
        /*
         * This typically means that there was no module which was able
         * to run properly this time. So, we need to abort
-        *
         * JMS replace with show_help
         */
        lam_abort(1, "No topo module avaliable. This should not happen");
     }
 
+    /*
+     * We now have a list of modules which have successfully returned
+     * their priorities from the query. We now have to finalize() those
+     * modules which have not been selected and init() the module which
+     * was selected
+     */ 
     for (item = lam_list_remove_first(&opened);
          NULL != item;
          item = lam_list_remove_first(&opened)) {
             om = (opened_module_t *) item;
-            if (om->om_module != best_module) {
+            if (om->om_module == best_module) {
+                /*
+                 * this is the chosen module, we have to initialise
+                 * the actions of this module. Also, save the actions
+                 * in selected since we are going to deallocate om
+                 * at the end of the loop. For now I am ignoring the init
+                 * function call of the module since I dont know what
+                 * it is supposed to do as of now. Someday this will have
+                 * arguments.
+                 *
+                 * ANJU: a module might not have all the functions defined.
+                 * Whereever a function pointer is null in the actions 
+                 * structure we need to fill it in with the base structure
+                 * function pointers. This is yet to be done 
+                 */ 
+                if (NULL != om->om_actions->init) { 
+                    (void)om->om_actions->init();
+                }
+                mca_topo_base_selected_module = *best_module;
+                mca_topo = *(om->om_actions);
+                *selected = *(om->om_actions);
+                *allow_multi_user_threads = best_user_threads;
+                *have_hidden_threads = best_hidden_threads;
+            } else {
               /*
-               * this is not the "choosen one", discard this   
+               * this is not the "choosen one", finalize
                */
                if (NULL != om->om_module->topom_finalize) {
                   /* finalise the module only if they have some
@@ -153,26 +198,11 @@ int mca_topo_base_select (mca_topo_t *selected,
     } /* traversing through the entire list */
 
     /*
-     * Now we have to unload all the modules which were not selected 
-     * through mca_base_modules_close. After this function call, the
-     * mca_topo_base_available_modules will contain only the selected
-     * module
-     */
-    mca_base_modules_close(mca_topo_base_output,
-                           &mca_topo_base_modules_available,
-                           (mca_base_module_t *) best_module);
-
-    /*
      * save the selected module
      */
-   mca_topo_base_selected_module = *best_module;
-   mca_topo = *actions;
-   *selected = *actions;
-   *allow_multi_user_threads = best_user_threads;
-   *have_hidden_threads = best_hidden_threads;
-   lam_output_verbose(10, mca_topo_base_output,
-                      "select: module %s selected",
-                      module->topom_version.mca_module_name);
+     lam_output_verbose(10, mca_topo_base_output,
+                       "select: module %s selected",
+                        module->topom_version.mca_module_name);
 
    /*
     * I think we are done :-)
