65902c8cf6a0dd5fa7e164bb74e88ef5f794d0a0
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-03-08 00:34:16 +0000

    changing the base header file

--- src/mca/mpi/topo/base/base.h
+++ src/mca/mpi/topo/base/base.h
@@ -15,57 +15,83 @@
 extern "C" {
 #endif
     int mca_topo_base_open(void);
+    int mca_topo_base_close(void);
     int mca_topo_base_select(lam_list_t *selected,
                              bool *allow_multi_user_threads,
                              bool *have_hidden_threads);
 
-    const mca_topo_1_0_0_t *
-        mca_topo_unity_query(lam_comm_t *comm, int priority);
-    int mca_topo_base_close(void);
 
     int mca_topo_base_init_comm (lam_comm_t *comm);
     int mca_topo_base_get_param (lam_comm_t *comm, int keyval);
 
+    const mca_topo_1_0_0_t *
+        mca_topo_unity_query(int *priority,
+                             bool *allow_multi_user_threads,
+                             bool *have_hidden_threads);
     /*
      * All the glue functions which we will provide to the users
      * by default. The users need to only write back-end functions
      * for graph_map() and cart_map() for their topology modules.
      * But they can implement these glue functions if they want.
      */
-    int topo_base_cart_coords (lam_comm_t *comm, int rank, int maxdims,
+    int topo_base_cart_coords (lam_comm_t *comm, 
+                               int rank, 
+                               int maxdims,
                                int *coords);
 
-    int topo_base_cart_create (lam_comm_t *old_comm, int ndims, int *dims,
-                               int *peroids, int reorder, 
+    int topo_base_cart_create (lam_comm_t *old_comm, 
+                               int ndims, 
+                               int *dims,
+                               int *peroids, 
+                               int reorder, 
                                lam_comm_t *comm_cart);
 
-    int topo_base_cartdim_get (lam_comm_t *comm, int *ndims);
+    int topo_base_cartdim_get (lam_comm_t *comm, 
+                               int *ndims);
 
-    int topo_base_cart_get (lam_comm_t *comm, int maxdims, int *dims,
-                            int *periods, int *coords);
+    int topo_base_cart_get (lam_comm_t *comm, 
+                            int maxdims, 
+                            int *dims,
+                            int *periods, 
+                            int *coords);
 
-    int topo_base_cart_rank (lam_comm_t *comm, int *coords, int *rank);
+    int topo_base_cart_rank (lam_comm_t *comm, 
+                             int *coords, 
+                             int *rank);
 
-    int topo_base_cart_shift (lam_comm_t *comm, int direction, int disp,
-                              int *rank_source, int *rank_dest);
+    int topo_base_cart_shift (lam_comm_t *comm, 
+                              int direction, 
+                              int disp,
+                              int *rank_source, 
+                              int *rank_dest);
 
-    int topo_base_cart_sub (lam_comm_t *comm, int *remain_dims,
+    int topo_base_cart_sub (lam_comm_t *comm, 
+                            int *remain_dims,
                             lam_comm_t **new_comm);
 
-    int topo_base_graph_create (lam_comm_t *comm_old, int nnodes,
-                                int *index, int reorder,
+    int topo_base_graph_create (lam_comm_t *comm_old, 
+                                int nnodes,
+                                int *index, 
+                                int reorder,
                                 lam_comm_t **comm_graph);
 
-    int topo_base_graph_dims_get (lam_comm_t *comm, int *nodes,
+    int topo_base_graph_dims_get (lam_comm_t *comm, 
+                                  int *nodes,
                                   int *nedges);
 
-    int topo_base_graph_get (lam_comm_t *comm, int maxindex, 
-                             int maxedges, int *index, int *edges);
+    int topo_base_graph_get (lam_comm_t *comm, 
+                             int maxindex, 
+                             int maxedges, 
+                             int *index, 
+                             int *edges);
 
-    int topo_base_graph_neighbors (lam_comm_t *comm, int rank,
-                                   int maxneighbors, int *neighbors);
+    int topo_base_graph_neighbors (lam_comm_t *comm, 
+                                   int rank,
+                                   int maxneighbors, 
+                                   int *neighbors);
 
-    int topo_base_graph_neighbors_count (lam_comm_t *comm, int rank,
+    int topo_base_graph_neighbors_count (lam_comm_t *comm, 
+                                         int rank,
                                          int *nneighbors);
 
 
