6e96ca6a78b33509cb1c490dd67e02d36c4a58d3
Tim Woodall <twoodall@lanl.gov>
2004-03-17 22:13:01 +0000

    added request free
    This commit was SVN r898.

---
+++ src/mca/pml/teg/src/pml_teg_free.c
@@ -0,0 +1,11 @@
+#include "pml_teg.h"
+#include "mca/ptl/base/ptl_base_comm.h"
+#include "mca/pml/base/pml_base_request.h"
+
+
+
+int mca_pml_teg_free(lam_request_t** request)
+{
+    return LAM_SUCCESS;
+}
+
