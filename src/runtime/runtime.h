5bd69674f7d1974297b44649363e8a0a8d92c799
David Daniel <ddd@open-mpi-git-mirror.example.com>
2004-03-17 20:00:24 +0000

    More fixes from directory reorg

--- src/runtime/runtime.h
+++ src/runtime/runtime.h
@@ -8,6 +8,18 @@
 #include "lam_config.h"
 
 
+/*
+ * Global variables and symbols for the MPI layer
+ */
+
+extern bool lam_mpi_initialized;
+extern bool lam_mpi_finalized;
+
+extern bool lam_mpi_thread_multiple;
+extern int lam_mpi_thread_requested;
+extern int lam_mpi_thread_provided;
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -17,6 +29,9 @@ extern "C" {
   int lam_finalize(void);
   int lam_rte_init(bool *allow_multi_user_threads, bool *have_hidden_threads);
   int lam_rte_finalize(void);
+
+  int lam_mpi_init(int argc, char **argv, int requested, int *provided);
+  int lam_mpi_finalize(void);
 
 #ifdef __cplusplus
 }
