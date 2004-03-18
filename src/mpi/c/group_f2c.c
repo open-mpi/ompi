dfdd30969b58e0243bfd89d06ee0f4e6663f2043
David Daniel <ddd@lanl.gov>
2004-03-18 14:17:23 +0000

    Miscellaneous fixes for a clean build
    This commit was SVN r913.

--- src/mpi/c/group_f2c.c
+++ src/mpi/c/group_f2c.c
@@ -1,9 +1,8 @@
 /*
  * $HEADERS$
  */
-#include "lam_config.h"
-#include <stdio.h>
 
+#include "lam_config.h"
 #include "mpi.h"
 #include "mpi/c/bindings.h"
 #include "group/group.h"
@@ -12,24 +11,25 @@
 #pragma weak MPI_Group_f2c = PMPI_Group_f2c
 #endif
 
-MPI_Group MPI_Group_f2c(MPI_Fint group_f) {
-    /* local variable */
+MPI_Group MPI_Group_f2c(MPI_Fint group_f)
+{
+    /* local variables */
     lam_group_t *group_c;
     int group_index;
 
-    group_index=(int)group_f;
+    group_index = (int) group_f;
 
     /* error checks */
-    if( MPI_PARAM_CHECK ) {
-        if( 0 > group_index ) {
+    if (MPI_PARAM_CHECK) {
+        if (0 > group_index) {
             return NULL;
         }
-        if( group_index >= lam_group_f_to_c_table->size ) {
+        if (group_index >= lam_group_f_to_c_table->size) {
             return NULL;
         }
     }
 
-    group_c=lam_group_f_to_c_table->addr[group_index];
+    group_c = lam_group_f_to_c_table->addr[group_index];
 
     return (MPI_Group) group_c;
 }
--- src/mpi/f77/group_compare_f.c
+++ src/mpi/f77/group_compare_f.c
@@ -4,8 +4,6 @@
 
 #include "lam_config.h"
 
-#include <stdio.h>
-
 #include "mpi.h"
 #include "mpi/f77/bindings.h"
 #include "group/group.h"
@@ -18,47 +16,48 @@
 #pragma weak pmpi_group_compare__ = mpi_group_compare_f
 #elif LAM_PROFILE_LAYER
 LAM_GENERATE_F77_BINDINGS (PMPI_GROUP_COMPARE,
-        pmpi_group_compare,
-        pmpi_group_compare_,
-        pmpi_group_compare__,
-        pmpi_group_compare_f,
-        (MPI_Fint *group1, MPI_Fint *group2, MPI_Fint *result, MPI_Fint *ierror),
-        (group1,group2,result,ierror))
+                           pmpi_group_compare,
+                           pmpi_group_compare_,
+                           pmpi_group_compare__,
+                           pmpi_group_compare_f,
+                           (MPI_Fint *group1, MPI_Fint *group2,
+                            MPI_Fint *result, MPI_Fint *ierror),
+                           (group1,group2,result,ierror))
 #endif
 
 #if LAM_HAVE_WEAK_SYMBOLS
-#pragma weak MPI_INIT = mpi_group_compare_f
-#pragma weak mpi_init = mpi_group_compare_f
-#pragma weak mpi_init_ = mpi_group_compare_f
-#pragma weak mpi_init__ = mpi_group_compare_f
+#pragma weak MPI_GROUP_COMPARE = mpi_group_compare_f
+#pragma weak mpi_group_compare = mpi_group_compare_f
+#pragma weak mpi_group_compare_ = mpi_group_compare_f
+#pragma weak mpi_group_compare__ = mpi_group_compare_f
 #endif
 
 #if ! LAM_PROFILE_LAYER && ! LAM_HAVE_WEAK_SYMBOLS
-LAM_GENERATE_F77_BINDINGS (PMPI_GROUP_COMPARE,
-        pmpi_group_compare,
-        pmpi_group_compare_,
-        pmpi_group_compare__,
-        pmpi_group_compare_f,
-        (MPI_Fint *group1, MPI_Fint *group2, MPI_Fint *result, MPI_Fint *ierror),
-        (group1,group2,result,ierror))
+LAM_GENERATE_F77_BINDINGS (MPI_GROUP_COMPARE,
+                           mpi_group_compare,
+                           mpi_group_compare_,
+                           mpi_group_compare__,
+                           mpi_group_compare_f,
+                           (MPI_Fint *group1, MPI_Fint *group2,
+                            MPI_Fint *result, MPI_Fint *ierror),
+                           (group1,group2,result,ierror))
 #endif
 
 
-void
-mpi_group_compare_f(MPI_Fint *group1, MPI_Fint *group2, MPI_Fint *result,
-        MPI_Fint *ierror)
+void mpi_group_compare_f(MPI_Fint *group1, MPI_Fint *group2,
+                         MPI_Fint *result, MPI_Fint *ierror)
 {
     /* local variables */
     int c_result, c_error;
     lam_group_t *c_group1, *c_group2;
 
     /* make the fortran to c representation conversion */
-    c_group1=group_f2c(*group1);
-    c_group2=group_f2c(*group2);
+    c_group1 = MPI_Group_f2c(*group1);
+    c_group2 = MPI_Group_f2c(*group2);
 
     c_error = MPI_Group_compare(c_group1, c_group2, &c_result);
 
     /* translate the results from c to fortran */
-    *ierror=(MPI_Fint)c_error;
-    *result=(MPI_Fint)c_result;
+    *ierror = (MPI_Fint) c_error;
+    *result = (MPI_Fint) c_result;
 }
