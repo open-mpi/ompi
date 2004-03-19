e4b821cc7c2304dbcdac75f07d80dee3ce7d1804
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-03-19 19:01:09 +0000

    Implemented MPI_Errhandler_f2c and MPI_Errhandler_c2f

--- src/mpi/c/errhandler_f2c.c
+++ src/mpi/c/errhandler_f2c.c
@@ -6,11 +6,27 @@
 
 #include "mpi.h"
 #include "mpi/c/bindings.h"
+#include "errhandler/errhandler.h"
+#include "communicator/communicator.h"
 
 #if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
 #pragma weak MPI_Errhandler_f2c = PMPI_Errhandler_f2c
 #endif
 
-MPI_Errhandler MPI_Errhandler_f2c(MPI_Fint errhandler) {
-    return (MPI_Errhandler)0;
+
+MPI_Errhandler MPI_Errhandler_f2c(MPI_Fint errhandler_f)
+{
+  int eh_index = (int) errhandler_f;
+
+  /* Error checking */
+
+  if (MPI_PARAM_CHECK) {
+    if (0 > eh_index || eh_index >= lam_errhandler_f_to_c_table->size) {
+      return MPI_ERRHANDLER_NULL;
+    }
+  }
+
+  /* All done */
+
+  return lam_errhandler_f_to_c_table->addr[eh_index];
 }
