a3cf78a4d569069d261c6818f5dc77c9513b2fcb
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-04-20 23:12:34 +0000

    Add top-level interface functions for the MPI_OP* calls

--- src/mpi/c/op_f2c.c
+++ src/mpi/c/op_f2c.c
@@ -6,6 +6,9 @@
 
 #include "mpi.h"
 #include "mpi/c/bindings.h"
+#include "op/op.h"
+#include "errhandler/errhandler.h"
+#include "communicator/communicator.h"
 
 #if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
 #pragma weak MPI_Op_f2c = PMPI_Op_f2c
@@ -15,6 +18,20 @@
 #include "mpi/c/profile/defines.h"
 #endif
 
-MPI_Op MPI_Op_f2c(MPI_Fint op) {
-    return (MPI_Op)0;
+MPI_Op MPI_Op_f2c(MPI_Fint op_f)
+{
+  size_t o_index = (size_t) op_f;
+
+  /* Error checking */
+
+  if (MPI_PARAM_CHECK) {
+    if (0 > o_index || 
+        o_index >= lam_pointer_array_get_size(lam_op_f_to_c_table)) {
+      return MPI_OP_NULL;
+    }
+  }
+
+  /* All done */
+
+  return lam_op_f_to_c_table->addr[o_index];
 }
--- src/mpi/c/op_free.c
+++ src/mpi/c/op_free.c
@@ -6,6 +6,9 @@
 
 #include "mpi.h"
 #include "mpi/c/bindings.h"
+#include "op/op.h"
+#include "errhandler/errhandler.h"
+#include "communicator/communicator.h"
 
 #if LAM_HAVE_WEAK_SYMBOLS && LAM_PROFILING_DEFINES
 #pragma weak MPI_Op_free = PMPI_Op_free
@@ -15,6 +18,24 @@
 #include "mpi/c/profile/defines.h"
 #endif
 
-int MPI_Op_free(MPI_Op *op) {
-    return MPI_SUCCESS;
+int MPI_Op_free(MPI_Op *op) 
+{
+  /* Error checking */
+
+  if (MPI_PARAM_CHECK) {
+    if (NULL == op ||
+        lam_op_is_intrinsic(*op)) {
+      return LAM_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_ARG,
+                                   "MPI_Op_free");
+    }
+  }
+
+  /* We have a valid op, release it */
+
+  OBJ_RELEASE(*op);
+  *op = MPI_OP_NULL;
+
+  /* All done */
+
+  return MPI_SUCCESS;
 }
