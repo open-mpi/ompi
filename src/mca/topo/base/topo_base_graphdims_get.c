bcc8a53e31a90dd1a81ac21abe298ed5bb60699d
Prabhanjan Kambadur <pkambadu@osl.iu.edu>
2004-04-21 21:08:11 +0000

    This is the right names
    This commit was SVN r1078.

---
+++ src/mca/topo/base/topo_base_graphdims_get.c
@@ -0,0 +1,30 @@
+/*
+ * $HEADER$
+ */
+
+#include "mca/topo/base/base.h"
+#include "communicator/communicator.h"
+#include "mca/topo/topo.h"
+
+/*
+ * function - Retrieves graph topology information associated with a
+ *            communicator
+ *            
+ * @param comm - communicator for group with graph structure (handle)
+ * @param nodes - number of nodes in graph (integer)
+ * @param nedges - number of edges in graph (integer)
+ * 
+ * @retval MPI_SUCCESS
+ * @retval MPI_ERR_TOPOLOGY
+ * @retval MPI_ERR_COMM
+ * @retval MPI_ERR_ARG
+ */
+int topo_base_graphdims_get (MPI_Comm comm,
+                              int *nodes,
+                              int *nedges){
+
+    *nodes = comm->c_topo_comm->mtc_nprocs;
+    *nedges = comm->c_topo_comm->mtc_nedges;
+
+    return MPI_SUCCESS;
+}
