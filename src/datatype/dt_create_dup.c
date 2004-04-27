e8237b95d307504ad454657ed5829c7ce4dbb1b1
George Bosilca <bosilca@icl.utk.edu>
2004-04-27 18:07:18 +0000

    If the duplicated datatype was already commited, copy the last fake entry in the data description.
    This commit was SVN r1091.

--- src/datatype/dt_create_dup.c
+++ src/datatype/dt_create_dup.c
@@ -1,19 +1,26 @@
 /* -*- Mode: C; c-basic-offset:4 ; -*- */
 
 #include "datatype.h"
+#include "datatype_internal.h"
 
 int lam_ddt_duplicate( dt_desc_t* oldType, dt_desc_t** newType )
 {
-   dt_desc_t* pdt = lam_ddt_create( oldType->desc.used );
-   void* temp = pdt->desc.desc; /* temporary copy of the desc pointer */
+    dt_desc_t* pdt;
+    void* temp;
+    int desc_size = oldType->desc.used;
 
-   memcpy( pdt, oldType, sizeof(dt_desc_t) );
-   pdt->desc.desc = temp;
-   memcpy( pdt->desc.desc, oldType->desc.desc, sizeof(dt_elem_desc_t) * oldType->desc.used );
-   pdt->id  = 0;
-   pdt->args = NULL;
-   *newType = pdt;
-   return 0;
+    if( oldType->flags & DT_FLAG_COMMITED ) desc_size++;
+
+    pdt = lam_ddt_create( desc_size );
+    temp = pdt->desc.desc; /* temporary copy of the desc pointer */
+    memcpy( pdt, oldType, sizeof(dt_desc_t) );
+    pdt->desc.desc = temp;
+    memcpy( pdt->desc.desc, oldType->desc.desc, sizeof(dt_elem_desc_t) * desc_size );
+    pdt->id  = 0;
+    pdt->args = NULL;
+    /* TODO: if the data was commited update the opt_desc field */
+    *newType = pdt;
+    return 0;
 }
 
 int lam_ddt_create_contiguous( int count, dt_desc_t* oldType, dt_desc_t** newType )
