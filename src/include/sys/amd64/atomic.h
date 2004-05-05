c174b2ca7cc007f1591937deffd75b663a34b859
David Daniel <ddd@lanl.gov>
2004-05-05 23:19:32 +0000

    Adding more atomic stuff before I vanish...
    This commit was SVN r1116.

--- src/include/sys/amd64/atomic.h
+++ src/include/sys/amd64/atomic.h
@@ -5,65 +5,103 @@
 #ifndef LAM_SYS_ATOMIC_H_INCLUDED
 #define LAM_SYS_ATOMIC_H_INCLUDED
 
-#ifdef SMP
-#define LOCK "lock; "
-#else
-#define LOCK
-#endif
-
 /*
  * On amd64, we use cmpxchg.
  */
 
+
+#ifdef HAVE_SMP
+#define LOCK "lock; "
+#define MB() __asm__ __volatile__("": : :"memory")
+#else
+#define LOCK
+#define MB()
+#endif
+
+
+static inline lam_atomic_mb(void)
+{
+    MB();
+}
+
+
+static inline lam_atomic_rmb(void)
+{
+    MB();
+}
+
+
+static inline lam_atomic_wmb(void)
+{
+    MB();
+}
+
+
+static inline int lam_atomic_cmpset_32(volatile uint32_t *addr,
+                                       uint32_t old,
+                                       uint32_t new)
+{
+    uint32_t ret = old;
+
+    __asm__ __volatile (
+LOCK "cmpxchgl %1,%2   \n\
+      setz     %%al    \n\
+      movzbl   %%al,%0 \n"
+    : "+a" (ret)
+    : "r" (new), "m" (*(addr))
+    : "memory");
+
+    return (ret == old);
+}
+
+
 static inline int lam_atomic_cmpset_acq_32(volatile uint32_t *addr,
-                                            uint32_t cmp,
+                                            uint32_t old,
                                             uint32_t new)
 {
-    uint32_t ret = cmp;
-
-    __asm __volatile (
-        LOCK "cmpxchgl %1,%2;   "
-        "     setz     %%al; "
-        "     movzbl   %%al,%0; "
-        : "+a" (ret)
-        : "r" (new), "m" (*(addr))
-        : "memory");
-
-    return (ret == cmp);
+    return lam_atomic_cmpset_32(addr, old, new);
 }
 
 
 static inline int lam_atomic_cmpset_rel_32(volatile uint32_t *addr,
-                                            uint32_t cmp,
+                                            uint32_t old,
                                             uint32_t new)
 {
-    return lam_atomic_cmpset_acq_32(addr, cmp, new);
+    return lam_atomic_cmpset_32(addr, old, new);
+}
+
+
+static inline int lam_atomic_cmpset_64(volatile uint64_t *addr,
+                                       uint64_t old,
+                                       uint64_t new)
+{
+    uint64_t ret = old;
+    
+    __asm__ __volatile (
+LOCK "cmpxchgq %1,%2   \n\
+      setz     %%al    \n\
+      movzbl   %%al,%0 \n"
+    : "+a" (ret)
+    : "r" (new), "m" (*(addr))
+    : "memory");
+
+    return (ret == old);
 }
 
 
 static inline int lam_atomic_cmpset_acq_64(volatile uint64_t *addr,
-                                            uint64_t cmp,
-                                            uint64_t new)
+                                           uint64_t old,
+                                           uint64_t new)
 {
-    uint64_t ret = cmp;
-
-    __asm __volatile (
-        LOCK "cmpxchgq %1,%2; "
-        "     setz     %%al; "
-        "     movzbl   %%al,%0; "
-        : "+a" (ret)
-        : "r" (new), "m" (*(addr))
-        : "memory");
-
-    return (ret == cmp);
+    return lam_atomic_cpmset_64(addr, old, new);
 }
 
 
 static inline int lam_atomic_cmpset_rel_64(volatile uint64_t *addr,
-                                            uint64_t cmp,
-                                            uint64_t new)
+                                           uint64_t old,
+                                           uint64_t new)
 {
-    return lam_atomic_cpmset_acq_64(addr, cmp, new);
+    return lam_atomic_cpmset_64(addr, old, new);
 }
 
 #endif /* ! LAM_SYS_ATOMIC_H_INCLUDED */
--- src/include/sys/ia64/atomic.h
+++ src/include/sys/ia64/atomic.h
@@ -9,64 +9,113 @@
  * On ia64, we use cmpxchg, which supports acquire/release semantics natively.
  */
 
+
+#ifdef HAVE_SMP
+#define MB() __asm__ __volatile__("": : :"memory")
+#else
+#define MB()
+#endif
+
+
+static inline lam_atomic_mb(void)
+{
+    MB();
+}
+
+
+static inline lam_atomic_rmb(void)
+{
+    MB();
+}
+
+
+static inline lam_atomic_wmb(void)
+{
+    MB();
+}
+
+
 static inline int lam_atomic_cmpset_acq_32(volatile uint32_t *addr,
-                                           uint32_t cmp,
+                                           uint32_t old,
                                            uint32_t new)
 {
     uint32_t ret;
 
-    __asm __volatile("mov ar.ccv=%2; "
-                     "cmpxchg4.acq %0=%4,%3,ar.ccv; "
-                     : "=r"(ret), "=m"(*addr)
-                     : "r"(cmp), "r"(new), "m"(*addr)
-                     : "memory");
-    return (ret == cmp);
+    __asm__ __volatile(
+"   mov ar.ccv=%2                \n\
+    cmpxchg4.acq %0=%4,%3,ar.ccv \n"
+    : "=r"(ret), "=m"(*addr)
+    : "r"(old), "r"(new), "m"(*addr)
+    : "memory");
+
+    return (ret == old);
 }
 
 
 static inline int lam_atomic_cmpset_rel_32(volatile uint32_t *addr,
-                                           uint32_t cmp,
+                                           uint32_t old,
                                            uint32_t new)
 {
     uint32_t ret;
 
-    __asm __volatile("mov ar.ccv=%2; "
-                     "cmpxchg4.rel %0=%4,%3,ar.ccv; "
-                     : "=r"(ret), "=m"(*addr)
-                     : "r"(cmp), "r"(new), "m"(*addr)
-                     : "memory");
-    return (ret == cmp);
+    __asm__ __volatile(
+"   mov ar.ccv=%2                \n\
+    cmpxchg4.rel %0=%4,%3,ar.ccv \n"
+    : "=r"(ret), "=m"(*addr)
+    : "r"(old), "r"(new), "m"(*addr)
+    : "memory");
+
+    return (ret == old);
+}
+
+
+static inline int lam_atomic_cmpset_32(volatile uint32_t *addr,
+                                       uint32_t old,
+                                       uint32_t new)
+{
+    return lam_atomic_cmpset_acq_32(addr, old, new);
 }
 
 
 static inline int lam_atomic_cmpset_acq_64(volatile uint64_t *addr,
-                                           uint64_t cmp,
+                                           uint64_t old,
                                            uint64_t new)
 {
     uint64_t ret;
 
-    __asm __volatile("mov ar.ccv=%2; "
-                     "cmpxchg8.acq %0=%4,%3,ar.ccv; "
-                     : "=r"(ret), "=m"(*addr)
-                     : "r"(cmp), "r"(new), "m"(*addr)
-                     : "memory");
+    __asm__ __volatile(
+"   mov ar.ccv=%2                \n\
+    cmpxchg8.acq %0=%4,%3,ar.ccv \n"
+    : "=r"(ret), "=m"(*addr)
+    : "r"(old), "r"(new), "m"(*addr)
+    : "memory");
 
-    return (ret == cmp);
+    return (ret == old);
 }
 
 
 static inline int lam_atomic_cmpset_rel_64(volatile uint64_t *addr,
-                                           uint64_t cmp,
+                                           uint64_t old,
                                            uint64_t new)
 {
     uint64_t ret;
 
-    __asm __volatile("mov ar.ccv=%2; "
-                     "cmpxchg8.rel %0=%4,%3,ar.ccv; "
-                     : "=r"(ret), "=m"(*addr)
-                     : "r"(cmp), "r"(new), "m"(*addr)
-                     : "memory");
+    __asm__ __volatile(
+"   mov ar.ccv=%2                \n\
+    cmpxchg8.rel %0=%4,%3,ar.ccv \n"
+    : "=r"(ret), "=m"(*addr)
+    : "r"(old), "r"(new), "m"(*addr)
+    : "memory");
     return (ret);
 }
+
+
+static inline int lam_atomic_cmpset_64(volatile uint64_t *addr,
+                                       uint64_t old,
+                                       uint64_t new)
+{
+    return lam_atomic_cmpset_acq_64(addr, old, new);
+}
+
 
 #endif /* ! LAM_SYS_ATOMIC_H_INCLUDED */
