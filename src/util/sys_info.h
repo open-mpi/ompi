35de46bac6d88d3c8bc8d371c1ca3b8894993295
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2004-05-20 17:38:54 +0000

    Remove the runtime-environment directory RTE/universe from the makefile list since it only exists on my machine! Update the sys_info header file

--- src/util/sys_info.h
+++ src/util/sys_info.h
@@ -4,6 +4,23 @@
 
 /** @file **/
 
+/* sys_info populates a global variable with information about the system
+ * upon which the program is executing. The structure contains the following
+ * fields:
+ *
+ * char sysname[] - the name of the operating system in use
+ *
+ * char release[] - current release level of the operating system
+ *
+ * char version[] - current version level of the operating system
+ *
+ * char machine[] - type of hardware in use
+ *
+ * char nodename[] - the network node name of the host, same as that
+ *                   returned by gethostname()
+ */
+
+
 #include <stdio.h>
 #include <sys/utsname.h>
 
