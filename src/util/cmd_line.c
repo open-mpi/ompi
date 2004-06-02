4aa88ea29e5cb5e6723406db3f0f2b4f2c4e89f1
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-06-02 17:26:43 +0000

    I can't believe this typo slipped by -- I know I checked this before I commited.  :-(

--- src/util/cmd_line.c
+++ src/util/cmd_line.c
@@ -937,7 +937,7 @@ static cmd_line_option_t *find_option(lam_cmd_line_t *cmd,
        item = lam_list_get_next(item)) {
     option = (cmd_line_option_t *) item;
     if ((NULL != option->clo_long_name &&
-         0 == strcmp(option_name, option->clo_long_name) ||
+         0 == strcmp(option_name, option->clo_long_name)) ||
         (strlen(option_name) == 1 &&
          option_name[0] == option->clo_short_name)) {
       return option;
