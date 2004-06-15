c0c70e626a4bc5d23f1f8ab1cb7db83f447ef6bb
David Daniel <ddd@lanl.gov>
2004-06-15 22:41:41 +0000

    Adding some atomic tests.  Work on MacOS X, but not yet included in the build system (I hope).
    This commit was SVN r1289.

--- test/support/support.c
+++ test/support/support.c
@@ -24,103 +24,99 @@ void test_init(char *a)
     size_t len;
 
     /* save the descriptive string */
-    len=strlen(a);
-    ompi_description=(char *)malloc(len+1);
+    len = strlen(a);
+    ompi_description = (char *) malloc(len + 1);
     assert(ompi_description);
 
-    strcpy(ompi_description,a);
+    strcpy(ompi_description, a);
 
     /* initialize counters */
-    ompi_n_tests=0;
-    ompi_n_success=0;
-    ompi_n_failures=0;
+    ompi_n_tests = 0;
+    ompi_n_success = 0;
+    ompi_n_failures = 0;
 
     return;
 
 }
 
-void test_success(void){
 
+void test_success(void)
+{
     ompi_n_tests++;
     ompi_n_success++;
-
-    return;
 }
 
-void test_failure(char *a){
+
+void test_failure(char *a)
+{
     ompi_n_tests++;
     ompi_n_failures++;
 
-    fprintf(stderr," Failure : %s \n",a);
+    fprintf(stderr, " Failure : %s\n", a);
     fflush(stderr);
 }
 
-int test_verify_str(const char *expected_result, const char *test_result){
 
-    /* local variables */
-    size_t len_expect,len_result;
+int test_verify_str(const char *expected_result, const char *test_result)
+{
+    size_t len_expect, len_result;
     int return_value;
 
     return_value = 1;
     len_expect = expected_result ? strlen(expected_result) : 0;
     len_result = test_result ? strlen(test_result) : 0;
 
-    if( (!(len_expect == len_result)) ||
-            ( 0 != strcmp(expected_result,test_result)) ) {
+    if ((!(len_expect == len_result)) ||
+        (0 != strcmp(expected_result, test_result))) {
         test_failure("Comparison failure");
-        fprintf(stderr," Expected result: %s \n", expected_result);
-        fprintf(stderr," Test result: %s \n", test_result);
+        fprintf(stderr, " Expected result: %s\n", expected_result);
+        fprintf(stderr, " Test result: %s\n", test_result);
         fflush(stderr);
         return_value = 0;
-    }
-    else
-    {
+    } else {
         test_success();
     }
 
     return return_value;
 }
+
 
 int test_verify_int(int expected_result, int test_result)
 {
-    /* local variables */
     int return_value;
-    
+
     return_value = 1;
-    if( expected_result != test_result ) 
-    {
+    if (expected_result != test_result) {
         test_failure("Comparison failure");
-        fprintf(stderr," Expected result: %d \n", expected_result);
-        fprintf(stderr," Test result: %d \n", test_result);
+        fprintf(stderr, " Expected result: %d\n", expected_result);
+        fprintf(stderr, " Test result: %d\n", test_result);
         fflush(stderr);
         return_value = 0;
-    }
-    else
-    {
-        test_success();
-    }
-        
-    return return_value;    
-}
-
-int test_finalize(void)
-{
-
-    int return_value;
-
-    return_value=1;
-
-    if( ompi_n_tests == ompi_n_success) {
-        fprintf(stderr," SUPPORT :: OMPI Test Passed :: %s %d tests \n",
-                ompi_description,ompi_n_tests);
-        fflush(stderr);
     } else {
-        fprintf(stderr," SUPPORT :: OMPI Test failed :: %s :: %d of %d failed\n"
-                ,ompi_description,ompi_n_failures,ompi_n_tests);
-        fflush(stderr);
-        return_value=0;
+        test_success();
     }
 
     return return_value;
 }
 
+
+int test_finalize(void)
+{
+    int return_value;
+
+    return_value = 1;
+
+    if (ompi_n_tests == ompi_n_success) {
+        fprintf(stderr, "SUPPORT: OMPI Test Passed: %s: (%d tests)\n",
+                ompi_description, ompi_n_tests);
+        fflush(stderr);
+    } else {
+        fprintf(stderr,
+                "SUPPORT: OMPI Test failed: %s (%d of %d failed)\n",
+                ompi_description, ompi_n_failures, ompi_n_tests);
+        fflush(stderr);
+        return_value = 0;
+    }
+
+    return return_value;
+}
--- test/support/support.h
+++ test/support/support.h
@@ -5,12 +5,32 @@
 #ifndef OMPI_SUPPORT_H
 #define OMPI_SUPPORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 void test_init(char *a);
 void test_success(void);
 void test_failure(char *a);
 int test_verify_str(const char *expected_result, const char *test_result);
 int test_verify_int(int expected_result, int test_result);
 int test_finalize(void);
+
+
+/*
+ * test_verify: Non-fatal assertion macro.
+ */
+
+#define test_verify(MESSAGE, EXPR)                               \
+    do {                                                         \
+        if (!(EXPR)) {                                           \
+            char s[256];                                         \
+            sprintf(s, "%s:%d: %s: %s\n",                        \
+                    __FILE__, __LINE__, MESSAGE, # EXPR);        \
+            test_failure(s);                                     \
+        } else {                                                 \
+            test_success();                                      \
+        }                                                        \
+    } while (0)
 
 #endif /* OMPI_SUPPORT_H */
 
