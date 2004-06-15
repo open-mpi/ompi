ca30b00d188b703ad5e1b7112364f7faf06f56ee
Edgar Gabriel <gabriel@cs.uh.edu>
2004-06-15 00:06:33 +0000

    bug fixes, removing typos and removing the grp_ok_to_free flag
    This commit was SVN r1256.

--- src/mpi/c/group_translate_ranks.c
+++ src/mpi/c/group_translate_ranks.c
@@ -35,8 +35,7 @@ int MPI_Group_translate_ranks(MPI_Group group1, int n_ranks, int *ranks1,
             return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_GROUP, 
                         "MPI_Group_translate_ranks");
         }
-        if( (n_ranks > group1_pointer->grp_proc_count) ||
-                ( 0 >= n_ranks ) ){
+        if( 0 >= n_ranks ){
             return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_GROUP, 
                         "MPI_Group_translate_ranks - II ");
         }
