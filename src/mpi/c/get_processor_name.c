9dbdc3f210360905bc96a847a8892131dd6bdad1
Edgar Gabriel <edgar@open-mpi-git-mirror.example.com>
2004-06-16 02:21:00 +0000

    adding a simple version of get_processor_name based on gethostname, as discussed tonight with Jeff.

--- src/mpi/c/get_processor_name.c
+++ src/mpi/c/get_processor_name.c
@@ -3,9 +3,13 @@
  */
 #include "ompi_config.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "mpi.h"
 #include "mpi/c/bindings.h"
+#include "runtime/runtime.h"
+#include "communicator/communicator.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILING_DEFINES
 #pragma weak MPI_Get_processor_name = PMPI_Get_processor_name
@@ -15,6 +19,36 @@
 #include "mpi/c/profile/defines.h"
 #endif
 
-int MPI_Get_processor_name(char *name, int *resultlen) {
+int MPI_Get_processor_name(char *name, int *resultlen) 
+{
+    char tmp[MPI_MAX_PROCESSOR_NAME];
+    int len;
+
+    if ( MPI_PARAM_CHECK) {
+        if ( ompi_mpi_finalized )
+            return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_INTERN, 
+                                          "MPI_Get_processor_name");
+        if ( NULL == name  )
+            return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_ARG, 
+                                         "MPI_Get_processor_name");
+        if ( NULL == resultlen  )
+            return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_ARG, 
+                                         "MPI_Get_processor_name");
+    }
+    
+    /* A simple implementation of this function using gethostname*/
+    gethostname (tmp, MPI_MAX_PROCESSOR_NAME);
+    len = strlen (tmp);
+    strncpy ( name, tmp, len);
+
+    if ( MPI_MAX_PROCESSOR_NAME > len ) {
+        *resultlen = len;
+        name[len] = '\0';
+    }
+    else {
+        *resultlen = MPI_MAX_PROCESSOR_NAME-1;
+        name[MPI_MAX_PROCESSOR_NAME-1] = '\0';
+    }
+
     return MPI_SUCCESS;
 }
