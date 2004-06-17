62e46c1d7c9a78bc167befde0219c7aa14c9dd00
George Bosilca <bosilca@icl.utk.edu>
2004-06-17 18:30:30 +0000

    Indentation, replacing tabs with spaces.
    This commit was SVN r1373.

--- src/mpi/c/type_delete_attr.c
+++ src/mpi/c/type_delete_attr.c
@@ -21,18 +21,18 @@ static char FUNC_NAME[] = "MPI_Type_delete_attr";
 int
 MPI_Type_delete_attr (MPI_Datatype type, int type_keyval)
 {
-    int ret;
+   int ret;
 
-    if (MPI_PARAM_CHECK) {
-	if (MPI_DATATYPE_NULL == type) {
-	    return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, 
-					 MPI_ERR_TYPE, 
-					 FUNC_NAME);
-	}
-    }
+   if (MPI_PARAM_CHECK) {
+      if (MPI_DATATYPE_NULL == type) {
+         return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, 
+                                       MPI_ERR_TYPE, 
+                                       FUNC_NAME);
+      }
+   }
   
-    ret = ompi_attr_delete(TYPE_ATTR, type, type->d_keyhash, type_keyval, 0);
+   ret = ompi_attr_delete(TYPE_ATTR, type, type->d_keyhash, type_keyval, 0);
 
-    OMPI_ERRHANDLER_RETURN(ret, MPI_COMM_WORLD,
+   OMPI_ERRHANDLER_RETURN(ret, MPI_COMM_WORLD,
 			  MPI_ERR_OTHER, FUNC_NAME);  
 }
