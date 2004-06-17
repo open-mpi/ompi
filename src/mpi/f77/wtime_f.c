ca10a19e8c10d1fa947eed0c56ac1e2246a4f48e
George Bosilca <bosilca@icl.utk.edu>
2004-06-17 21:10:39 +0000

    Remove the profiling interface for those 2 functions.
    This commit was SVN r1389.

--- src/mpi/f77/wtime_f.c
+++ src/mpi/f77/wtime_f.c
@@ -9,54 +9,31 @@
 #include "mpi.h"
 #include "mpi/f77/bindings.h"
 
-#if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
-#pragma weak PMPI_WTIME = mpi_wtime_f
-#pragma weak pmpi_wtime = mpi_wtime_f
-#pragma weak pmpi_wtime_ = mpi_wtime_f
-#pragma weak pmpi_wtime__ = mpi_wtime_f
-#elif OMPI_PROFILE_LAYER
-
-    double pmpi_wtime(void) {
-        return pmpi_wtime_f();
-    }
-
-    double pmpi_wtime_(void) {
-        return pmpi_wtime_f();
-    }
-
-    double pmpi_wtime__(void) {
-        return pmpi_wtime_f();
-    }
-
-    double PMPI_WTIME(void) {
-        return pmpi_wtime_f();
-    }
-
-#endif
-
+/* As the standard allow us to implement this function as a macro
+ * we cannot have profiling interface.
+ */
 #if OMPI_HAVE_WEAK_SYMBOLS
 #pragma weak MPI_WTIME = mpi_wtime_f
 #pragma weak mpi_wtime = mpi_wtime_f
 #pragma weak mpi_wtime_ = mpi_wtime_f
 #pragma weak mpi_wtime__ = mpi_wtime_f
-#endif
 
-#if ! OMPI_HAVE_WEAK_SYMBOLS && ! OMPI_PROFILE_LAYER
+#else
 
     double mpi_wtime(void) {
-        return mpi_wtime_f();
+        return MPI_Wtime();
     }
 
     double mpi_wtime_(void) {
-        return mpi_wtime_f();
+        return MPI_Wtime();
     }
 
     double mpi_wtime__(void) {
-        return mpi_wtime_f();
+        return MPI_Wtime();
     }
 
     double MPI_WTIME(void) {
-        return mpi_wtime_f();
+        return MPI_Wtime();
     }
 
 #endif
