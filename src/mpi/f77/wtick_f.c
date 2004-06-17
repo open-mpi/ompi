ffe0a6f365088464c5e086d7033a6832dd4ebeda
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2004-06-17 21:10:39 +0000

    Remove the profiling interface for those 2 functions.

--- src/mpi/f77/wtick_f.c
+++ src/mpi/f77/wtick_f.c
@@ -9,54 +9,31 @@
 #include "mpi.h"
 #include "mpi/f77/bindings.h"
 
-#if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
-#pragma weak PMPI_WTICK = mpi_wtick_f
-#pragma weak pmpi_wtick = mpi_wtick_f
-#pragma weak pmpi_wtick_ = mpi_wtick_f
-#pragma weak pmpi_wtick__ = mpi_wtick_f
-#elif OMPI_PROFILE_LAYER
-
-    double pmpi_wtick(void) {
-        return pmpi_wtick_f();
-    }
-
-    double pmpi_wtick_(void) {
-        return pmpi_wtick_f();
-    }
-
-    double pmpi_wtick__(void) {
-        return pmpi_wtick_f();
-    }
-
-    double PMPI_WTICK(void) {
-        return pmpi_wtick_f();
-    }
-
-#endif
-
+/* As the standard allow us to define this function as a macro, it cannot
+ * have a profiling interface.
+ */
 #if OMPI_HAVE_WEAK_SYMBOLS
 #pragma weak MPI_WTICK = mpi_wtick_f
 #pragma weak mpi_wtick = mpi_wtick_f
 #pragma weak mpi_wtick_ = mpi_wtick_f
 #pragma weak mpi_wtick__ = mpi_wtick_f
-#endif
 
-#if ! OMPI_HAVE_WEAK_SYMBOLS && ! OMPI_PROFILE_LAYER
+#else
 
     double mpi_wtick(void) {
-        return mpi_wtick_f();
+        return MPI_Wtick();
     }
 
     double mpi_wtick_(void) {
-        return mpi_wtick_f();
+        return MPI_Wtick();
     }
 
     double mpi_wtick__(void) {
-        return mpi_wtick_f();
+        return MPI_Wtick();
     }
 
     double MPI_WTICK(void) {
-        return mpi_wtick_f();
+        return MPI_Wtick();
     }
 
 #endif
