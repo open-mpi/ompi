ecb0385e10d1d0f556c53fa5edf6ed1fff950e29
Weikuan Yu <yuw@lanl.gov>
2004-06-19 17:02:52 +0000

    Add req.[hc] and frag.[hc] for compilation
    This commit was SVN r1421.

---
+++ src/mca/ptl/elan/src/ptl_elan_req.c
@@ -0,0 +1,36 @@
+/*
+ * $HEADER$
+ */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/errno.h>
+#include "types.h"
+#include "mca/ptl/base/ptl_base_sendreq.h"
+#include "ptl_elan_req.h"
+#include "ptl_elan.h"
+
+static void
+            mca_ptl_elan_send_request_construct (mca_ptl_elan_send_request_t *);
+static void mca_ptl_elan_send_request_destruct (mca_ptl_elan_send_request_t
+                                                *);
+
+ompi_class_t mca_ptl_elan_send_request_t_class = {
+    "mca_ptl_elan_send_request_t",
+    OBJ_CLASS (mca_ptl_base_send_request_t),
+    (ompi_construct_t) mca_ptl_elan_send_request_construct,
+    (ompi_destruct_t) mca_ptl_elan_send_request_destruct
+};
+
+
+void
+mca_ptl_elan_send_request_construct (mca_ptl_elan_send_request_t * request)
+{
+    OBJ_CONSTRUCT (&request->req_frag, mca_ptl_elan_send_frag_t);
+}
+
+
+void
+mca_ptl_elan_send_request_destruct (mca_ptl_elan_send_request_t * request)
+{
+    OBJ_DESTRUCT (&request->req_frag);
+}
