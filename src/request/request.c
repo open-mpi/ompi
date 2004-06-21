4e224f010d0472fa817cdce0b89da7b9a38773c3
Tim Woodall <twoodall@lanl.gov>
2004-06-21 15:24:40 +0000

    added init/fini stubs
    This commit was SVN r1424.

--- src/request/request.c
+++ src/request/request.c
@@ -4,22 +4,22 @@
 
 #include "request/request.h"
 
-ompi_class_t ompi_request_t_class = { 
-    "ompi_request_t", 
-    OBJ_CLASS(ompi_object_t),
-    (ompi_construct_t) ompi_request_construct,
-    (ompi_destruct_t) ompi_request_destruct,
-};
 
-
-void ompi_request_construct(ompi_request_t* req)
+static void ompi_request_construct(ompi_request_t* req)
 {
     req->req_mode = OMPI_REQUEST_INVALID;
 }
 
 
-void ompi_request_destruct(ompi_request_t* req)
+static void ompi_request_destruct(ompi_request_t* req)
 {
 }
+
+
+OBJ_CLASS_INSTANCE(
+    ompi_request_t,
+    ompi_object_t,
+    ompi_request_construct,
+    ompi_request_destruct);
 
 
