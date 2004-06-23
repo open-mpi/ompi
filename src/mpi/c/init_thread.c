9aa5cc21328c8b8e9018ff3b5a3f733cc591e7c0
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2004-06-23 01:32:03 +0000

    More fixes for errhandler, particularly in the case where MPI_INIT* is the one throwing the exception.

--- src/mpi/c/init_thread.c
+++ src/mpi/c/init_thread.c
@@ -5,7 +5,11 @@
 #include "ompi_config.h"
 
 #include "mpi.h"
+#include "include/constants.h"
 #include "mpi/c/bindings.h"
+#include "runtime/runtime.h"
+#include "errhandler/errhandler.h"
+#include "communicator/communicator.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILING_DEFINES
 #pragma weak MPI_Init_thread = PMPI_Init_thread
@@ -15,7 +19,19 @@
 #include "mpi/c/profile/defines.h"
 #endif
 
+static char FUNC_NAME[] = "MPI_Init_thread";
+
+
 int MPI_Init_thread(int *argc, char ***argv, int required,
-                    int *provided) {
-    return MPI_SUCCESS;
+                    int *provided) 
+{
+  int err;
+  MPI_Comm null = NULL;
+
+  /* Call the back-end initialization function (we need to put as
+     little in this function as possible so that if it's profiled, we
+     don't lose anything) */
+
+  err = ompi_mpi_init(*argc, *argv, required, provided);
+  OMPI_ERRHANDLER_RETURN(err, null, err, FUNC_NAME);
 }
