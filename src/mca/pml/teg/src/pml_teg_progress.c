50d7e8b2059906cbf0e0f9f8cce699c624a350be
Tim Woodall <twoodall@open-mpi-git-mirror.example.com>
2004-06-24 20:58:40 +0000

    check for valid progress function

--- src/mca/pml/teg/src/pml_teg_progress.c
+++ src/mca/pml/teg/src/pml_teg_progress.c
@@ -10,8 +10,11 @@ int mca_pml_teg_progress(void)
     /*
      * Progress each of the PTL modules
      */
-    for(i=0; i<mca_pml_teg.teg_num_ptl_modules; i++)
-        mca_pml_teg.teg_ptl_modules[i]->ptlm_progress(tstamp);
+    for(i=0; i<mca_pml_teg.teg_num_ptl_modules; i++) {
+        mca_ptl_base_module_progress_fn_t progress = mca_pml_teg.teg_ptl_modules[i]->ptlm_progress;
+        if(NULL != progress)
+            progress(tstamp);
+    }
     return OMPI_SUCCESS;
 }
 
