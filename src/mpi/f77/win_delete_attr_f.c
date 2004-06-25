3e5b973bd7a2fa0a247bb956827510c36078a9bc
Ginger Young <gingery@lanl.gov>
2004-06-25 15:45:24 +0000

    A finished wrapper that I neglected to commit several days ago.
    This commit was SVN r1483.

--- src/mpi/f77/win_delete_attr_f.c
+++ src/mpi/f77/win_delete_attr_f.c
@@ -8,6 +8,7 @@
 
 #include "mpi.h"
 #include "mpi/f77/bindings.h"
+#include "attribute/attribute.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
 #pragma weak PMPI_WIN_DELETE_ATTR = mpi_win_delete_attr_f
@@ -45,8 +46,27 @@ OMPI_GENERATE_F77_BINDINGS (MPI_WIN_DELETE_ATTR,
 #if OMPI_PROFILE_LAYER && ! OMPI_HAVE_WEAK_SYMBOLS
 #include "mpi/c/profile/defines.h"
 #endif
+static const char FUNC_NAME[] = "MPI_Win_delete_attr_f";
 
 void mpi_win_delete_attr_f(MPI_Fint *win, MPI_Fint *win_keyval, MPI_Fint *ierr)
 {
+    MPI_Win c_win = MPI_Win_f2c( *win );
+    int ret; 
+
+    if (MPI_PARAM_CHECK) {
+        if (MPI_WIN_NULL == c_win) {
+            *ierr = OMPI_ERRHANDLER_INVOKE(c_win, MPI_ERR_WIN, 
+                                         FUNC_NAME);
+        }
+    }
+  
+    ret = ompi_attr_delete(WIN_ATTR, c_win, c_win->w_keyhash, *win_keyval, 
+                           OMPI_KEYVAL_F77);
+
+    if (ret != OMPI_SUCCESS) {
+        OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_OTHER, FUNC_NAME);  
+    } else {
+        *ierr = MPI_SUCCESS;
+    }
 
 }
