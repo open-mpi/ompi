c83ddc15d06bbb2c04ffd57e32a49e631dd4cb97
Tim Prins <tprins@lanl.gov>
2004-06-29 20:36:34 +0000

    Checking in the pack and unpack functions. These only support int16 and int32 as well as strings and byte streams.
    This commit was SVN r1505.

--- src/mca/oob/base/base.h
+++ src/mca/oob/base/base.h
@@ -12,22 +12,86 @@
 #include "mca/mca.h"
 #include "mca/oob/oob.h"
 
+/**
+ * the different types one can use to describe the data
+ */
+typedef enum { 
+    MCA_OOB_BASE_BYTE, /**< a byte of data */
+    MCA_OOB_BASE_INT16, /**< a 16 bit integer */
+    MCA_OOB_BASE_INT32, /**< a 32 bit integer */
+    MCA_OOB_BASE_PACKED,/**< already packed data. */
+    MCA_OOB_BASE_BYTE_BY_REF /**< indicates to try to use the data directly without copying */
+} mca_oob_base_types_t;
+
 /*
  * Global functions for MCA overall collective open and close
  */
 #if defined(c_plusplus) || defined(__cplusplus)
 extern "C" {
 #endif
-  int mca_oob_base_open(void);
-  int mca_oob_base_select(bool *allow_multi_user_threads, 
-                          bool *have_hidden_threads);
-  int mca_oob_base_close(void);
+    int mca_oob_base_open(void);
+    int mca_oob_base_select(bool *allow_multi_user_threads,
+                            bool *have_hidden_threads);
+    int mca_oob_base_close(void);
 
-  bool mca_oob_base_is_checkpointable(void);
+    bool mca_oob_base_is_checkpointable(void);
 
-  int mca_oob_base_checkpoint(void);
-  int mca_oob_base_continue(void);
-  int mca_oob_base_restart(void);
+    int mca_oob_base_checkpoint(void);
+    int mca_oob_base_continue(void);
+    int mca_oob_base_restart(void);
+
+/*
+ * functions for pack and unpack routines
+ */
+/**
+ * This function packs the passed data according to the type enum.
+ *
+ * @param dest the destination for the packed data
+ * @param src the source of the data
+ * @param n the number of elements in the src
+ * @param type the type of data 
+ *
+ * @retval OMPI_SUCCESS
+ * @retval OMPI_ERROR
+ */
+    int mca_oob_base_pack(void * dest, void * src, size_t n, mca_oob_base_types_t type);
+
+/**
+ * This function unpacks the passed data according to the type enum.
+ *
+ * @param dest the destination for the unpacked data
+ * @param src the source of the packed data
+ * @param n the number of elements in the src
+ * @param type the type of the data to unpack
+ * 
+ * @retval OMPI_SUCCESS
+ * @retval OMPI_ERROR
+ */
+    int mca_oob_base_unpack(void * dest, void * src, size_t n, mca_oob_base_types_t type);
+
+/**
+ * This function packs null terminated strings
+ *
+ * @param dest the destination for the packed information
+ * @param src the source of the information
+ * @param maxlen the maximum length available in dest. the string will
+ *               be truncated and a NULL added if it is longer then maxlen.
+ * 
+ * @retval OMPI_SUCCESS
+ */
+    int mca_oob_base_pack_string(void * dest, void * src, size_t maxlen);
+/**
+ * This function unpacks strings
+ *
+ * @param dest the destination for the packed information
+ * @param src the source of the information
+ * @param maxlen the maximum length available in dest. the string will
+ *               be truncated and a NULL added if it is longer then maxlen.
+ *  
+ * @retval OMPI_SUCCESS
+ */
+    int mca_oob_base_unpack_string(void * dest, void * src, size_t maxlen);
+
 #if defined(c_plusplus) || defined(__cplusplus)
 }
 #endif
