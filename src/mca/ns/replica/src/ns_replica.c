7eb8f4968ba86aa923b277e580c08de2e02f0b77
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2004-07-11 02:31:30 +0000

    Fix location of class instance.

--- src/mca/ns/replica/src/ns_replica.c
+++ src/mca/ns/replica/src/ns_replica.c
@@ -13,24 +13,6 @@
 /**
  * globals
  */
-/* constructor - used to initialize state of name instance */
-static void ompi_name_construct(ompi_process_name_t* name)
-{
-    name->cellid = 0;
-    name->jobid = 0;
-    name->vpid = 0;
-}
-
-/* destructor - used to free any resources held by instance */
-static void ompi_name_destructor(ompi_process_name_t* name)
-{
-}
-
-OBJ_CLASS_INSTANCE(
-		   ompi_process_name_t,   /* type name */
-		   ompi_object_t,         /* parent "class" name */
-		   ompi_name_construct,   /* constructor */
-		   ompi_name_destructor);  /* destructor */
 
 /*
  * functions
