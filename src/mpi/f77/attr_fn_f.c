768006f867c2dc74210c330dfa065c1a0d50db4e
George Bosilca <bosilca@icl.utk.edu>
2004-07-14 19:29:47 +0000

    Add more Fortran wrappers... Still in progress.
    This commit was SVN r1727.

--- src/mpi/f77/attr_fn_f.c
+++ src/mpi/f77/attr_fn_f.c
@@ -88,7 +88,90 @@ void mpi_comm_dup_fn_f( MPI_Fint* comm, int* comm_keyval, void* extra_state,
 #pragma weak mpi_dup_fn__ = mpi_dup_fn_f
 
 #else
-#warning Still to be done
+OMPI_GENERATE_F77_BINDINGS( MPI_TYPE_NULL_DELETE_FN,
+                            mpi_type_null_delete_fn,
+                            mpi_type_null_delete_fn_,
+                            mpi_type_null_delete_fn__,
+                            mpi_type_null_delete_fn_f,
+                            (MPI_Fint* type, int* type_keyval,void* attribute_val_out, int* flag, int* ierr),
+                            (type, type_keyval, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_TYPE_NULL_COPY_FN,
+                            mpi_type_null_copy_fn,
+                            mpi_type_null_copy_fn_,
+                            mpi_type_null_copy_fn__,
+                            mpi_type_null_copy_fn_f,
+                            (MPI_Fint* type, int* type_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (type, type_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_TYPE_DUP_FN,
+                            mpi_type_dup_fn,
+                            mpi_type_dup_fn_,
+                            mpi_type_dup_fn__,
+                            mpi_type_dup_fn_f,
+                            (MPI_Fint* type, int* type_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (type, type_keyval, extra_state, attribute_val_in, attribute_val_out, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_COMM_NULL_DELETE_FN,
+                            mpi_comm_null_delete_fn,
+                            mpi_comm_null_delete_fn_,
+                            mpi_comm_null_delete_fn__,
+                            mpi_comm_null_delete_fn_f,
+                            (MPI_Fint* comm, int* comm_keyval,void* attribute_val_out, int* flag, int* ierr ),
+                            (comm, comm_keyval, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_COMM_NULL_COPY_FN,
+                            mpi_comm_null_copy_fn,
+                            mpi_comm_null_copy_fn_,
+                            mpi_comm_null_copy_fn__,
+                            mpi_comm_null_copy_fn_f,
+                            (MPI_Fint* comm, int* comm_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (type, comm_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_COMM_DUP_FN,
+                            mpi_comm_dup_fn,
+                            mpi_comm_dup_fn_,
+                            mpi_comm_dup_fn__,
+                            mpi_comm_dup_fn_f,
+                            (MPI_Fint* comm, int* comm_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (type, comm_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_NULL_DELETE_FN,
+                            mpi_null_delete_fn,
+                            mpi_null_delete_fn_,
+                            mpi_null_delete_fn__,
+                            mpi_null_delete_fn_f,
+                            (MPI_Fint* comm, int* comm_keyval,void* attribute_val_out, int* flag, int* ierr ),
+                            (comm, comm_keyval, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_NULL_COPY_FN,
+                            mpi_null_copy_fn,
+                            mpi_null_copy_fn_,
+                            mpi_null_copy_fn__,
+                            mpi_null_copy_fn_f,
+                            (MPI_Fint* comm, int* comm_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (comm, comm_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_DUP_FN,
+                            mpi_dup_fn,
+                            mpi_dup_fn_,
+                            mpi_dup_fn__,
+                            mpi_dup_fn_f,
+                            (MPI_Fint* comm, int* comm_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (comm, comm_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_WIN_NULL_DELETE_FN,
+                            mpi_win_null_delete_fn,
+                            mpi_win_null_delete_fn_,
+                            mpi_win_null_delete_fn__,
+                            mpi_win_null_delete_fn_f,
+                            (MPI_Fint* type, int* type_keyval,void* attribute_val_out, int* flag, int* ierr ),
+                            (type, type_keyval, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_WIN_NULL_COPY_FN,
+                            mpi_win_null_copy_fn,
+                            mpi_win_null_copy_fn_,
+                            mpi_win_null_copy_fn__,
+                            mpi_win_null_copy_fn_f,
+                            (MPI_Fint* window, int* win_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (window, win_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
+OMPI_GENERATE_F77_BINDINGS( MPI_WIN_DUP_FN,
+                            mpi_win_dup_fn,
+                            mpi_win_dup_fn_,
+                            mpi_win_dup_fn__,
+                            mpi_win_dup_fn_f,
+                            (MPI_Fint* window, int* win_keyval, void* extra_state, void* attribute_val_in, void* attribute_val_out, int* flag, int* ierr),
+                            (window, win_keyval, extra_state, attribute_val_in, attribute_val_out, flag, ierr) )
 #endif
 
 void mpi_type_null_delete_fn_f( MPI_Fint* type, int* type_keyval,
