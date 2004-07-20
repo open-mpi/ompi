c45ea2c6a5e95fa523f837d68f345581385080dc
Prabhanjan Kambadur <pkambadu@osl.iu.edu>
2004-07-20 22:55:48 +0000

    Sorry, this is probably breaking the trunk
    This commit was SVN r1791.

--- src/mca/topo/unity/src/topo_unity_module.c
+++ src/mca/topo/unity/src/topo_unity_module.c
@@ -35,79 +35,13 @@ const mca_topo_base_module_1_0_0_t mca_topo_unity_module = {
         MCA_topo_unity_MAJOR_VERSION, /* major version */
         MCA_topo_unity_MINOR_VERSION, /* minor version */
         MCA_topo_unity_RELEASE_VERSION, /* release version */
-        mca_topo_unity_module_open,   /* fp to open the module */
-        mca_topo_unity_module_close   /* fp to close the module */
+        NULL,   /* fp to open the module */
+        NULL    /* fp to close the module */
     },
     {
       false /* whether checkpoint/restart is enabled */
     },
-    mca_topo_unity_module_query,      /* get priority and actions */
-    mca_topo_unity_module_finalize    /* undo actions of query */
+    mca_topo_unity_module_init_query,      /* get thread level */
+    mca_topo_unity_module_comm_query,      /* get priority and actions */
+    mca_topo_unity_module_comm_unquery     /* undo what was done by previous function */
 };
-/*
- * *******************************************************************
- * ******************** structure definition ends ********************
- * *******************************************************************
- */ 
-
-/*
- * *******************************************************************
- * ************************ actions structure ************************
- * *******************************************************************
- */
-static mca_topo_t unity =  {
-    mca_topo_unity_init, /* initalise after being selected */
-    NULL, /* topo_cart_coords */
-    NULL, /* topo_cart_create */
-    NULL, /* topo_cart_get */
-    NULL, /* topo_cartdim_get */
-    mca_topo_unity_cart_map,
-    NULL, /* topo_cart_rank */
-    NULL, /* topo_cart_shift */
-    NULL, /* topo_cart_sub */
-    NULL, /* topo_graph_create */
-    NULL, /* topo_graph_get */
-    mca_topo_unity_graph_map,
-    NULL, /* topo_graphdims_get */
-    NULL, /* topo_graph_neighbors */
-    NULL /* topo_graph_neighbors_count */
-};
-/*
- * *******************************************************************
- * ************************* structure ends **************************
- * *******************************************************************
- */
-
-int mca_topo_unity_module_open (void) {
-    /*
-     * As of now do nothing
-     */
-    return OMPI_SUCCESS;
-} 
-    
-int mca_topo_unity_module_close (void) {
-    /*
-     * As of now do nothing
-     */
-    return OMPI_SUCCESS;
-}
-
-mca_topo_t* mca_topo_unity_module_query(int* priority,
-                                        bool *allow_multi_user_threads,
-                                        bool *have_hidden_threads)
-{
-        *priority = 0;
-        *allow_multi_user_threads = true;
-        *have_hidden_threads = false;
-        /*
-         * return the structure of function pointers
-         */ 
-        return &unity;
-}      
-
-int mca_topo_unity_module_finalize (void) {
-    /*
-     * do nothing as of now
-     */
-     return OMPI_SUCCESS;
-} 
