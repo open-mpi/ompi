212116621d8b7782a12645319c13e5afd2e46a94
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-07-30 22:06:09 +0000

    These two functions were unimplemented .. (oops)

--- src/mpi/c/cartdim_get.c
+++ src/mpi/c/cartdim_get.c
@@ -23,11 +23,37 @@ static const char FUNC_NAME[] = "MPI_Cartdim_get";
 
 int MPI_Cartdim_get(MPI_Comm comm, int *ndims) 
 {
-  if (MPI_PARAM_CHECK) {
-    OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
-  }
+    mca_topo_base_cartdim_get_fn_t func;
+    int err;
 
-  /* This function is not yet implemented */
+    if (MPI_PARAM_CHECK) {
+       OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
+       if (ompi_comm_invalid(comm)) {
+           return OMPI_ERRHANDLER_INVOKE (MPI_COMM_WORLD, MPI_ERR_COMM,
+                                        FUNC_NAME);
+       }
+       if (OMPI_COMM_IS_INTER(comm)) { 
+           return OMPI_ERRHANDLER_INVOKE (comm, MPI_ERR_COMM,
+                                         FUNC_NAME);
+       }
+       if (!OMPI_COMM_IS_CART(comm)) {
+           return OMPI_ERRHANDLER_INVOKE (comm, MPI_ERR_TOPOLOGY,
+                                          FUNC_NAME);
+       }
+       if (NULL == ndims) {
+           return OMPI_ERRHANDLER_INVOKE (comm, MPI_ERR_ARG,
+                                          FUNC_NAME);
+       }
+    }
 
-  return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_INTERN, FUNC_NAME);
+    /* get the function pointer on this communicator */
+    func = comm->c_topo->topo_cartdim_get;
+
+    /* call the function */
+    if ( MPI_SUCCESS != 
+              (err = func(comm, ndims))) {
+         return OMPI_ERRHANDLER_INVOKE(comm, err, FUNC_NAME);
+    }
+
+    return MPI_SUCCESS;
 }
