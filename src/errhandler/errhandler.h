34311ef01d3add0aa5a098aeaedf9444963e56e9
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2004-08-08 05:20:32 +0000

    * As requested, move mpiruntime to mpi/runtime

--- src/errhandler/errhandler.h
+++ src/errhandler/errhandler.h
@@ -11,7 +11,7 @@
 #include "mpi.h"
 #include "class/ompi_object.h"
 #include "class/ompi_pointer_array.h"
-#include "mpiruntime/mpiruntime.h"
+#include "mpi/runtime/mpiruntime.h"
 #include "errhandler/errhandler_predefined.h"
 
 /*
