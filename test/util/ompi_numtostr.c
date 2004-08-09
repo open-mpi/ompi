33c1952c7954559f988d97ff50cc587dc76d5753
Laura Casswell <lcasswell@lanl.gov>
2004-08-09 21:24:20 +0000

    add common test support routines and reporting
    This commit was SVN r1981.

--- test/util/ompi_numtostr.c
+++ test/util/ompi_numtostr.c
@@ -4,19 +4,26 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "support.h"
+
 int
 main(int argc, char *argv[])
 {
     char * tst;
     char * expected;
+
+    test_init("ompi_numtostr_t");
     
     tst = ltostr(10);
     expected = malloc(sizeof(long) * 8);
     snprintf(expected, sizeof(long) * 8, "%d", 10);
     if (strcmp(tst, expected) != 0) {
-        exit(1);
+      test_failure("ltostr test failed");
     }
-    
+    else {
+      test_success();
+    }
+
     free(tst);
     free(expected);
     
@@ -24,8 +31,13 @@ main(int argc, char *argv[])
     expected = malloc(sizeof(long) * 8);
     snprintf(expected, sizeof(long) * 8, "%f", 5.32);
     if (strcmp(tst, expected) != 0) {
-        exit(1);
+      test_failure("dtostr test failed");
     }
+    else {
+      test_success();
+    }
+
+    test_finalize();
         
     return 0;
 }
