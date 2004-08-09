07065f968ac06b29285e1d7c58bcfa662ea4fa25
Ginger Young <gingery@lanl.gov>
2004-08-09 20:33:24 +0000

    change from c++ to regular c
    This commit was SVN r1979.

--- src/tools/ompid/ompid.h
+++ src/tools/ompid/ompid.h
@@ -1,99 +1,58 @@
-//
-// $HEADER$
-//
+/*
+ * $HEADER$
+ */
 
 #ifndef OMPID_H
 #define OMPID_H
 
-#include <string>
-#include <vector>
-#include <map>
+#include <string.h>
 
 #include "class/ompi_list.h"
 #include "util/cmd_line.h"
 #include "mca/mca.h"
 
 
-namespace ompid {
+  /*
+   * Globals
+   */
 
-  //
-  // Globals
-  //
-
-  typedef std::vector<std::string> type_vector_t;
+  typedef char *type_vector_t;
 
   extern bool pretty;
   extern ompi_cmd_line_t *cmd_line;
 
-  extern const std::string type_all;
-  extern const std::string type_ompi;
-  extern const std::string type_base;
+  extern const char *type_all;
+  extern const char *type_ompi;
+  extern const char *type_base;
   extern type_vector_t mca_types;
 
-  //
-  // Version-related strings and functions
-  //
+  /*
+   * Version-related strings and functions
+   */
 
-  extern const std::string ver_full;
-  extern const std::string ver_major;
-  extern const std::string ver_minor;
-  extern const std::string ver_release;
-  extern const std::string ver_alpha;
-  extern const std::string ver_beta;
-  extern const std::string ver_svn;
+  extern const char *ver_full;
+  extern const char *ver_major;
+  extern const char *ver_minor;
+  extern const char *ver_release;
+  extern const char *ver_alpha;
+  extern const char *ver_beta;
+  extern const char *ver_svn;
 
   void do_version(bool want_all, ompi_cmd_line_t *cmd_line);
-  void show_ompi_version(const std::string& scope);
-  void show_component_version(const std::string& type_name, 
-                           const std::string& component_name,
-                           const std::string& scope, 
-                           const std::string& ver_type);
+  void show_ompi_version(const char *scope);
 
-  //
-  // Parameter/configuration-related functions
-  //
+  /*
+   * Parameter/configuration-related functions
+   */
 
-  extern std::string component_all;
-  extern std::string param_all;
+  extern char *param_all;
 
-  extern std::string path_prefix;
-  extern std::string path_bindir;
-  extern std::string path_libdir;
-  extern std::string path_incdir;
-  extern std::string path_pkglibdir;
-  extern std::string path_sysconfdir;
+  extern char *path_prefix;
+  extern char *path_bindir;
+  extern char *path_libdir;
+  extern char *path_incdir;
+  extern char *path_pkglibdir;
+  extern char *path_sysconfdir;
 
-  void do_params();
-  void show_mca_params(const std::string& type, const std::string& component, 
-                       const std::string& param);
-
-  void do_path(bool want_all, ompi_cmd_line_t *cmd_line);
-  void show_path(const std::string& type, const std::string& value);
-
-  void do_arch(ompi_cmd_line_t *cmd_line);
-  void do_config(bool want_all);
-
-  //
-  // Output-related functions
-  //
-  void out(const std::string& pretty_message, 
-           const std::string &plain_message, 
-           int value);
-  void out(const std::string& pretty_message, 
-           const std::string &plain_message,
-           const std::string& value);
-
-  //
-  // Component-related functions
-  //
-
-  typedef std::map<std::string, ompi_list_t *> component_map_t;
-
-  extern component_map_t component_map;
-
-  //void open_components();
-  //void close_components();
-
-}
 
 #endif /* OMPID_H */
