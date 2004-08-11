0c7308d0feb690eedb0a3fa673f0b34a6f4bb9ca
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2004-08-11 20:50:55 +0000

    Remove the memory leak. Keep trace of the allocated array of ptl pointers.

--- src/mca/ptl/prof/ptl_prof.h
+++ src/mca/ptl/prof/ptl_prof.h
@@ -14,17 +14,19 @@
 #include "mca/pml/base/pml_base_recvreq.h"
 #include "mca/ptl/base/ptl_base_recvfrag.h"
 
+typedef struct mca_ptl_prof mca_ptl_prof_t;
+typedef struct mca_ptl_prof_module_1_0_0 mca_ptl_prof_module_1_0_0_t;
+
 /**
  * PROF PTL module.
  */
 struct mca_ptl_prof_module_1_0_0 {
    mca_ptl_base_component_t super;          /**< base PTL module */
-   struct mca_ptl_t**       prof_ptls;      /**< array of available PTLs */
+   mca_ptl_prof_t**         prof_ptls;      /**< array of available PTLs */
    u_int32_t                prof_num_ptls;  /**< number of ptls actually used */
    u_int32_t                prof_max_ptls;  /**< maximum number of ptls - available kernel ifs */
    u_int32_t                prof_buf_size;  /**< the size of the internal buffer used to profile each PTL */
 };
-typedef struct mca_ptl_prof_module_1_0_0 mca_ptl_prof_module_1_0_0_t;
 /**
  * Profiling module.
  */
@@ -35,7 +37,6 @@ struct mca_ptl_prof {
    u_int32_t             prof_start_recv_id;
    u_int32_t             prof_complete_id;
 };
-typedef struct mca_ptl_prof mca_ptl_prof_t;
 OBJ_CLASS_DECLARATION(mca_ptl_prof_t);
 
 #endif  /* PTL_PROF_H_HAS_BEEN_INCLUDED */
