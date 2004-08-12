620ccad3177c797cf495050731c6a132b6e26b21
Jeff Squyres <jsquyres@cisco.com>
2004-08-12 01:14:45 +0000

    If there's an error message, print it instead of the error code int value
    This commit was SVN r2080.

--- src/errhandler/errhandler_predefined.c
+++ src/errhandler/errhandler_predefined.c
@@ -12,6 +12,7 @@
 #include "util/output.h"
 #include "errhandler/errhandler.h"
 #include "errhandler/errhandler_predefined.h"
+#include "errhandler/errcode.h"
 #include "communicator/communicator.h"
 #include "runtime/runtime.h"
 
@@ -46,9 +47,14 @@ void ompi_mpi_errors_are_fatal_handler(struct ompi_communicator_t **comm,
   }
 
   if (NULL != error_code) {
-    ompi_output(0, "*** error code: %d\n", *error_code);
+    char *tmp = ompi_mpi_errcode_get_string(*error_code);
+    if (NULL != tmp) {
+      ompi_output(0, "*** %s\n", tmp);
+    } else {
+      ompi_output(0, "*** Error code: %d (no associated error message)\n",
+                  *error_code);
+    }
   }
-  /* JMS: Should print the error string as well */
   ompi_output(0, "*** MPI_ERRORS_ARE_FATAL (goodbye)");
   va_end(arglist);
 
