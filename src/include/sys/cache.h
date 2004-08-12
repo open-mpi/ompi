fa6abf032e18101d53aa24342de4edb3a2facaed
Rich Graham <richardg@mellanox.com>
2004-08-12 23:30:16 +0000

    header with parameters to describe the system's cache bahavior. right now, parameters are hard coded.
    This commit was SVN r2099.

---
+++ src/include/sys/cache.h
@@ -0,0 +1,19 @@
+/*
+ * $HEADER$
+ */
+
+/** @file
+ *
+ *  Define the cache caracteristics of the platform
+ *  
+ */
+
+#ifndef CACHE
+#define CACHE 1
+
+/* size of cache line  - for now hardwire this to a reasonable
+ * value, and automate later - RLG */
+#define CACHE_LINE_SIZE 128
+
+
+#endif /* CACHE */
