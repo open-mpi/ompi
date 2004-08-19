6bf4dc4478c15b18e62e516a68212c4a7d0f8d6f
David Daniel <ddd@lanl.gov>
2004-08-19 00:34:00 +0000

    more cleanup of threads, atomic stuff etc.
    This commit was SVN r2224.

--- src/threads/mutex.c
+++ src/threads/mutex.c
@@ -2,7 +2,9 @@
  * $HEADER$
  */
 
+#ifdef HAVE_CONFIG_H
 #include "ompi_config.h"
+#endif
 
 #include "threads/mutex.h"
 
@@ -11,3 +13,42 @@
  */
 bool ompi_uses_threads = (bool) OMPI_HAVE_THREADS;
 
+
+#ifdef __WINDOWS__
+
+#include <windows.h>
+
+static void ompi_mutex_construct(ompi_mutex_t *m)
+{
+    InterlockedExchange(&m->m_lock, 0);
+}
+
+static void ompi_mutex_destruct(ompi_mutex_t *m)
+{
+}
+
+#else
+
+static void ompi_mutex_construct(ompi_mutex_t *m)
+{
+#if OMPI_HAVE_POSIX_THREADS
+    pthread_mutex_init(&m->m_lock.thread, 0);
+#endif
+#if OMPI_SYS_ARCH_ATOMIC_H
+    ompi_atomic_unlock(&m->m_lock.atomic);
+#endif
+}
+
+static void ompi_mutex_destruct(ompi_mutex_t *m)
+{
+#if OMPI_HAVE_POSIX_THREADS
+    pthread_mutex_destroy(&m->m_lock);
+#endif
+}
+
+#endif
+
+OBJ_CLASS_INSTANCE(ompi_mutex_t,
+                   ompi_object_t,
+                   ompi_mutex_construct,
+                   ompi_mutex_destruct);
