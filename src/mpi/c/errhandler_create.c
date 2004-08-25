15c01284e6e82e95ea29e2a61c6c880d87b756f2
Jeff Squyres <jsquyres@cisco.com>
2004-08-25 11:49:19 +0000

    Remove unused variables to eliminate compiler warnings
    This commit was SVN r2296.

--- src/mpi/c/errhandler_create.c
+++ src/mpi/c/errhandler_create.c
@@ -15,14 +15,12 @@
 #include "mpi/c/profile/defines.h"
 #endif
 
-static const char FUNC_NAME[] = "MPI_Errhandler_create";
-
 
 int MPI_Errhandler_create(MPI_Handler_function *function,
                           MPI_Errhandler *errhandler)
 {
-  /* This is a deprecated -- just turn around and call the real
-     function */
+    /* This is a deprecated -- just turn around and call the real
+       function */
 
-  return MPI_Comm_create_errhandler(function, errhandler);
+    return MPI_Comm_create_errhandler(function, errhandler);
 }
