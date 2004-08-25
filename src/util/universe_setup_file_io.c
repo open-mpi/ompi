b0fd705821e354e4f804d8cbd3a73b655ea0a10c
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2004-08-25 02:01:35 +0000

    Reserves a spot for the console program - no code in it yet. Attempts to fix earlier problem when compiling/linking the universe_setup_file_io routines. Will test further on new checkout - appears to work fine under OSX right now.

--- src/util/universe_setup_file_io.c
+++ src/util/universe_setup_file_io.c
@@ -21,12 +21,8 @@
 
 char *ompi_getline_buffer(FILE *fp);
 
-int ompi_write_universe_setup_file(char *filename)
+int ompi_write_universe_setup_file(char *filename, ompi_universe_t *universe)
 {
-#if 1
-    ompi_output(0, "WARNING: Called non-functional ompi_write_universe_setup_file\n");
-    return OMPI_ERROR;
-#else
     FILE *fp;
 
     fp = fopen(filename, "w");
@@ -34,38 +30,33 @@ int ompi_write_universe_setup_file(char *filename)
 	ompi_output(0, "cannot open file to save contact info");
 	return OMPI_ERROR;
     }
-    fprintf(fp, "%ld name: %s\n", strlen(ompi_universe.name), ompi_universe.name);
-    fprintf(fp, "%ld host: %s\n", strlen(ompi_universe.host), ompi_universe.host);
-    fprintf(fp, "%ld user: %s\n", strlen(ompi_universe.uid), ompi_universe.uid);
-    if (ompi_universe.persistence) {
+    fprintf(fp, "%ld name: %s\n", strlen(universe->name), universe->name);
+    fprintf(fp, "%ld host: %s\n", strlen(universe->host), universe->host);
+    fprintf(fp, "%ld user: %s\n", strlen(universe->uid), universe->uid);
+    if (universe->persistence) {
 	fprintf(fp, "state: persistent\n");
     } else {
 	fprintf(fp, "state: non-persistent\n");
     }
-    if (ompi_universe.silent_mode) {
+    if (universe->silent_mode) {
 	fprintf(fp, "mode: silent\n");
     } else {
 	fprintf(fp, "mode: console\n");
     }
-    if (ompi_universe.web_server) {
-	fprintf(fp, "%ld socket: %s\n", strlen(ompi_universe.socket_contact_info),
-		ompi_universe.socket_contact_info);
+    if (universe->web_server) {
+	fprintf(fp, "%ld socket: %s\n", strlen(universe->socket_contact_info),
+		universe->socket_contact_info);
     } else {
 	fprintf(fp, "0\n");
     }
-    fprintf(fp, "%ld oob: %s\n", strlen(ompi_universe.oob_contact_info),
-	    ompi_universe.oob_contact_info);
+    fprintf(fp, "%ld oob: %s\n", strlen(universe->oob_contact_info),
+	    universe->oob_contact_info);
     fclose(fp);
     return OMPI_SUCCESS;
-#endif
 }
 
-int ompi_read_universe_setup_file(char *filename)
+int ompi_read_universe_setup_file(char *filename, ompi_universe_t *universe)
 {
-#if 1
-    ompi_output(0, "WARNING: Called non-functional ompi_read_universe_setup_file\n");
-    return OMPI_ERROR;
-#else
     char persist[20], mode[10];
     FILE *fp;
 
@@ -78,48 +69,47 @@ int ompi_read_universe_setup_file(char *filename)
 	}
     }
 
-    ompi_universe.name = ompi_getline_buffer(fp);
-    fscanf(fp, "name: %s\n", ompi_universe.name);
+    universe->name = ompi_getline_buffer(fp);
+    fscanf(fp, "name: %s\n", universe->name);
 
-    ompi_universe.host = ompi_getline_buffer(fp);
-    fscanf(fp, "host: %s\n", ompi_universe.host);
+    universe->host = ompi_getline_buffer(fp);
+    fscanf(fp, "host: %s\n", universe->host);
 
-    ompi_universe.uid = ompi_getline_buffer(fp);
-    fscanf(fp, "user: %s\n", ompi_universe.uid);
+    universe->uid = ompi_getline_buffer(fp);
+    fscanf(fp, "user: %s\n", universe->uid);
 
     fscanf(fp, "state: %s", persist);
     if (0 == strncmp(persist, "persistent", strlen("persistent"))) {
-	ompi_universe.persistence = true;
+	universe->persistence = true;
     } else if (0 == strncmp(persist, "non-persistent", strlen("non-persistent"))) {
-	ompi_universe.persistence = false;
+	universe->persistence = false;
     } else {
 	return OMPI_ERROR;
     }
 
     fscanf(fp, "mode: %s", mode);
     if (0 == strncmp(mode, "silent", strlen("silent"))) {
-	ompi_universe.silent_mode = true;
+	universe->silent_mode = true;
     } else if (0 == strncmp(mode, "console", strlen("console"))) {
-	ompi_universe.silent_mode = false;
+	universe->silent_mode = false;
     } else {
 	return OMPI_ERROR;
     }
 
-    ompi_universe.socket_contact_info = ompi_getline_buffer(fp);
-    if (NULL != ompi_universe.socket_contact_info) {
-	fscanf(fp, "socket: %s", ompi_universe.socket_contact_info);
-	ompi_universe.web_server = true;
+    universe->socket_contact_info = ompi_getline_buffer(fp);
+    if (NULL != universe->socket_contact_info) {
+	fscanf(fp, "socket: %s", universe->socket_contact_info);
+	universe->web_server = true;
     } else {
-	ompi_universe.web_server = false;
+	universe->web_server = false;
     }
 
-    ompi_universe.oob_contact_info = ompi_getline_buffer(fp);
-    fscanf(fp, "oob: %s", ompi_universe.oob_contact_info);
+    universe->oob_contact_info = ompi_getline_buffer(fp);
+    fscanf(fp, "oob: %s", universe->oob_contact_info);
 
     fclose(fp);
 
     return OMPI_SUCCESS;
-#endif
 }
 
 char *ompi_getline_buffer(FILE *fp)
