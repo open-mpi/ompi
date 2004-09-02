7223524e27d6f9f4c8991215d66435d0b4ec59e4
Weikuan Yu <yuw@lanl.gov>
2004-09-02 05:16:59 +0000

    -- Save the orginal test file as check.c -- Simplify the basic ping-pong latency program.
    This commit was SVN r2442.

--- src/mca/ptl/elan/tests/check.c
+++ src/mca/ptl/elan/tests/check.c
@@ -8,6 +8,8 @@
 #include "test_util.h"
 
 #define MYBUFSIZE (4*1024*1024)
+#define CHECK 1
+#define PONG 1
 
 char        s_buf[MYBUFSIZE];
 char        r_buf[MYBUFSIZE];
@@ -20,11 +22,15 @@ main (int argc, char *argv[])
     double      startwtime = 0.0, endwtime;
     int         namelen;
     int         size;
+    int         loop;
     MPI_Status  stat;
-    int         loop = 1;
     int         sleep = 1;
 
+    /*while (sleep > 0);*/
+
     struct timeval t_start, t_end;
+
+    loop = 2;
 
     if (argc < 2) {
         fprintf (stderr, "Usage: %s msg_size\n", argv[0]);
@@ -42,19 +48,46 @@ main (int argc, char *argv[])
     MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank (MPI_COMM_WORLD, &myid);
 
+    /* touch the data */
+    for (i = 0; i < size; i++) {
+        s_buf[i] = i;
+    }
+
+    fprintf(stderr, "[proc%d:%s:%d] done with init, to loop %d \n",
+	    myid, __FUNCTION__, __LINE__, loop);
+    fflush(stderr);
+
     for (i = 0; i < loop + skip; i++) {
 	if (i == skip)
 	    gettimeofday (&t_start, 0);
 	if (myid == 0) {
             MPI_Send (s_buf, size, MPI_CHAR, 1, i, MPI_COMM_WORLD);
+	    if (PONG)
             MPI_Recv (r_buf, size, MPI_CHAR, 1, i, MPI_COMM_WORLD, &stat);
 	} else {
             MPI_Recv (r_buf, size, MPI_CHAR, 0, i, MPI_COMM_WORLD, &stat);
+	    if (PONG)
             MPI_Send (s_buf, size, MPI_CHAR, 0, i, MPI_COMM_WORLD);
         }
+
+	if (CHECK && myid != 0) {
+	    for (j=0; j < size; j ++) {
+		if (r_buf[j] != j) {
+		    fprintf(stderr, "[proc%d:%s] byte %d error, %02X \n",
+			    myid, __FUNCTION__, j, r_buf[j]);
+		    break;
+		} else {
+		    r_buf[j] = '0';
+		}
+	    } 
+	}
     }
     gettimeofday (&t_end, 0);
 
+    fprintf(stderr, "[proc%d:%s:%d] pingpong\n",
+	    myid, __FUNCTION__, __LINE__);
+    fflush(stderr);
+ 
     if (myid == 0) {
         double      latency;
         latency = ((1.0e6 * t_end.tv_sec + t_end.tv_usec) 
@@ -65,5 +98,6 @@ main (int argc, char *argv[])
     }
 
     MPI_Finalize ();
+
     return 0;
 }
