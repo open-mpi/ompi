985b219c6c33b8a6bc4ef09f9455349dc89a046c
Jeff Squyres <jsquyres@cisco.com>
2004-09-07 20:07:31 +0000

    Fix typo (sorry folks, this'll cause a lot of recompiling... :-\ )
    This commit was SVN r2526.

--- src/mpi/c/bindings.h
+++ src/mpi/c/bindings.h
@@ -36,7 +36,7 @@ do { \
    if( NULL == (DDT) || MPI_DATATYPE_NULL == (DDT) ) (RC) = MPI_ERR_TYPE; \
    else if( (COUNT) < 0 ) (RC) = MPI_ERR_COUNT; \
    else if( !ompi_ddt_is_committed((DDT)) ) (RC) = MPI_ERR_TYPE; \
-   else if( ompi_ddt_is_overerlapped((DDT)) ) (RC) = MPI_ERR_TYPE; \
+   else if( ompi_ddt_is_overlapped((DDT)) ) (RC) = MPI_ERR_TYPE; \
 } while (0)
 
 #define OMPI_CHECK_DATATYPE_FOR_ONE_SIDED( RC, DDT, COUNT ) \
