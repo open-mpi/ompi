7dafd4ed893b0eeaa84619ea4da44b336c9f10dd
Tim Woodall <twoodall@lanl.gov>
2004-09-08 21:07:19 +0000

    stubs for stdio forwarding service
    This commit was SVN r2549.

---
+++ src/mca/svc/stdio/svc_stdio.c
@@ -0,0 +1,50 @@
+#include "ompi_config.h"
+#include "mca/oob/oob.h"
+#include "mca/oob/base/base.h"
+#include "svc_stdio.h"
+
+
+mca_svc_base_module_t mca_svc_stdio_module = {
+    mca_svc_stdio_module_init,
+    mca_svc_stdio_module_fini
+};
+
+
+/**
+ *  Process an OOB request.
+ */
+
+static void mca_svc_stdio_recv(
+    int status,
+    ompi_process_name_t* peer,
+    ompi_buffer_t buffer,
+    int tag,
+    void* cbdata)
+{
+}
+
+
+/**
+ * Register a callback to receive OOB requests.
+ */
+
+int mca_svc_stdio_module_init(mca_svc_base_module_t* module)
+{
+    return mca_oob_recv_packed_nb(
+        MCA_OOB_NAME_ANY,  
+        MCA_OOB_TAG_STDIO, 
+        MCA_OOB_ALLOC, 
+        mca_svc_stdio_recv,
+        NULL);
+}
+
+
+/**
+ *  Cleanup
+ */
+
+int mca_svc_stdio_module_fini(mca_svc_base_module_t* module)
+{
+    return OMPI_SUCCESS;
+}
+
---
+++ src/mca/svc/stdio/svc_stdio.h
@@ -0,0 +1,30 @@
+/*
+ * $HEADER$
+ */
+/**
+ * @file
+ */
+#ifndef _MCA_SVC_STDIO_
+#define _MCA_SVC_STDIO_
+
+#include "mca/svc/svc.h"
+
+
+/**
+ * Component open/close/init
+ */
+int mca_svc_stdio_component_open(void);
+int mca_svc_stdio_component_close(void);
+mca_svc_base_module_t* mca_svc_stdio_component_init(void);
+
+/**
+ * Module init/fini
+ */
+int mca_svc_stdio_module_init(mca_svc_base_module_t*);
+int mca_svc_stdio_module_fini(mca_svc_base_module_t*);
+
+extern mca_svc_base_module_t mca_svc_stdio_module;
+extern mca_svc_base_component_t mca_svc_stdio_component;
+
+#endif
+
