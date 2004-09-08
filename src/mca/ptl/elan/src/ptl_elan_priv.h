24684bd66abae3591cef20c39ad0646498babcb7
Weikuan Yu <yuw@lanl.gov>
2004-09-08 21:36:02 +0000

    -- Remove a bug on completion Queue support to get.
    This commit was SVN r2551.

--- src/mca/ptl/elan/src/ptl_elan_priv.h
+++ src/mca/ptl/elan/src/ptl_elan_priv.h
@@ -102,7 +102,7 @@ do {                                                           \
 #define  OMPI_PTL_ELAN_NUM_PUTGET    (8)
 #define  OMPI_PTL_ELAN_ZERO_FFRAG    (0)
 
-#define  OMPI_PTL_ELAN_ENABLE_GET    (1)
+#define  OMPI_PTL_ELAN_ENABLE_GET    (0)
 #define  OMPI_PTL_ELAN_COMP_QUEUE    (0) 
 #define  OMPI_PTL_ELAN_THREADING      \
     (OMPI_PTL_ELAN_COMP_QUEUE && OMPI_HAVE_POSIX_THREADS)
