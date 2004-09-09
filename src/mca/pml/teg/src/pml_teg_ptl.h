8de416c25d7c79d111c384f9eadf15207806cd28
Tim Woodall <twoodall@lanl.gov>
2004-09-09 21:31:49 +0000

    Modified per ptl request cache to honor the request limit - However, if the cache limit is exceeded - a request is allocated from the global pool. A flag mca_pml_base_send_request.req_cached has been added to the request so that the ptl can determine wether the request came from the cache and hence has been initialized via the ptls ptl_request_init method. If not - the ptl is responsible for allocating the resources required to handle the request - or potentially queue the request until resources become available.
    This commit was SVN r2570.

--- src/mca/pml/teg/src/pml_teg_ptl.h
+++ src/mca/pml/teg/src/pml_teg_ptl.h
@@ -2,10 +2,13 @@
 #define _MCA_PML_BASE_PTL_
 
 #include "mca/ptl/ptl.h"
+#include "threads/condition.h"
+
 
 struct mca_pml_base_ptl_t {
     ompi_list_t       ptl_cache;       /**< cache of send requests */
     size_t            ptl_cache_size;  /**< maximum size of cache */
+    size_t            ptl_cache_alloc; /**< current number of allocated items */
     ompi_mutex_t      ptl_cache_lock;  /**< lock for queue access */
     struct mca_ptl_base_module_t* ptl; /**< back pointer to ptl */
 };
