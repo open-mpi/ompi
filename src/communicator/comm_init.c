2a71e4abace120a0197081efb0984950d221d833
Tim Woodall <twoodall@lanl.gov>
2004-09-09 22:57:53 +0000

    fixed the local rank in comm world to be the relative rank in the grouprather than the proceses vpid
    This commit was SVN r2580.

--- src/communicator/comm_init.c
+++ src/communicator/comm_init.c
@@ -55,9 +55,9 @@ int ompi_comm_init(void)
     OBJ_CONSTRUCT(&ompi_mpi_comm_world, ompi_communicator_t);
     group = OBJ_NEW(ompi_group_t);
     group->grp_proc_pointers = ompi_proc_world(&size);
-    group->grp_my_rank       = ompi_name_server.get_vpid(&(ompi_proc_local()->proc_name));
     group->grp_proc_count    = size;
     group->grp_flags        |= OMPI_GROUP_INTRINSIC;
+    ompi_set_group_rank(group, ompi_proc_local());
     OBJ_RETAIN(group); /* bump reference count for remote reference */
 
     ompi_mpi_comm_world.c_contextid    = 0;
