0cc555218758c1fbef7bf7c4ab5849a30bb6cbfe
Weikuan Yu <yuw@open-mpi-git-mirror.example.com>
2004-09-10 00:18:57 +0000

    -- Add bw check program

---
+++ src/mca/ptl/elan/tests/check_bw.c
@@ -0,0 +1,82 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <sys/time.h>
+#include "test_util.h"
+
+#define MYBUFSIZE (1<<22)
+#define MAX_REQ_NUM 1024
+
+MPI_Request request[MAX_REQ_NUM];
+MPI_Status  tmp_stat[MAX_REQ_NUM];
+
+int         skip = 0;
+
+int
+main (int argc,
+      char *argv[])
+{
+    int         myid, numprocs, i, j;
+    int         size=0, loop, page_size;
+    struct timeval t_start, t_end;
+    char        *s_buf, *r_buf;
+
+    s_buf = (char *) malloc (sizeof(char)*MYBUFSIZE);
+    r_buf = (char *) malloc (sizeof(char)*MYBUFSIZE);
+
+    /* Get some environmental variables set for Open MPI, OOB */
+    env_init_for_elan();
+
+    MPI_Init (&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank (MPI_COMM_WORLD, &myid);
+
+    if (argc < 5) {
+        fprintf (stderr, "Usage: %s loop size \n", argv[0]);
+        MPI_Finalize ();
+        return 0;
+    }
+    size = atoi (argv[2]);
+    loop= atoi (argv[1]);
+    page_size = getpagesize ();
+
+    for (i = 0; i < size ; i++) {
+        s_buf[i] = 'a';
+        r_buf[i] = 'b';
+    }
+
+    if (myid == 0) {
+	/* Start time */
+	if ( i == skip )
+	    gettimeofday (&t_start, 0);
+
+	for (j = 0; j < loop; j++) {
+	    MPI_Isend (s_buf, size, MPI_CHAR, 1, 100, MPI_COMM_WORLD,
+		       request + j);
+	}
+	MPI_Waitall (loop, request, tmp_stat);
+	MPI_Recv (r_buf, 4, MPI_CHAR, 1, 101, MPI_COMM_WORLD, &tmp_stat[0]);
+    } else {
+	for (j = 0; j < loop; j++) {
+	    MPI_Irecv (r_buf, size, MPI_CHAR, 0, 100, MPI_COMM_WORLD,
+		       request + j);
+	}
+	MPI_Waitall (loop, request, tmp_stat);
+	MPI_Send (s_buf, 4, MPI_CHAR, 0, 101, MPI_COMM_WORLD);
+    }
+
+    if (myid == 0) {
+	double      latency;
+	gettimeofday (&t_end, 0);
+	latency = ((1.0e6 * t_end.tv_sec + t_end.tv_usec) 
+		- (1.0e6 * t_start.tv_sec + t_start.tv_usec)) 
+	    / (loop);
+	fprintf (stdout, "%8d  %8.2f\n", size, size / latency);
+	fflush (stdout);
+    }
+    MPI_Finalize ();
+    return 0;
+}
