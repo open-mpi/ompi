52df687aaa1a05c90b862934245f07adc5441a1e
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2004-09-10 16:41:25 +0000

    * reorg the RSH pcm to not have any static data structure (keeping   everything in the "this" structure) so that multiple RSH pcm   modules can be loaded at once

--- src/mca/pcm/rsh/src/pcm_rsh_allocate.c
+++ src/mca/pcm/rsh/src/pcm_rsh_allocate.c
@@ -14,18 +14,22 @@
 
 
 ompi_list_t *
-mca_pcm_rsh_allocate_resources(struct mca_pcm_base_module_1_0_0_t* me,
+mca_pcm_rsh_allocate_resources(struct mca_pcm_base_module_1_0_0_t* me_super,
                                mca_ns_base_jobid_t jobid,
                                int nodes, int procs)
 {
-    return mca_pcm_rsh_llm.llm_allocate_resources(jobid, nodes, procs);
+    mca_pcm_rsh_module_t *me = (mca_pcm_rsh_module_t*) me_super;
+
+    return me->llm.llm_allocate_resources(jobid, nodes, procs);
 }
 
 
 int
-mca_pcm_rsh_deallocate_resources(struct mca_pcm_base_module_1_0_0_t* me,
+mca_pcm_rsh_deallocate_resources(struct mca_pcm_base_module_1_0_0_t* me_super,
                                  mca_ns_base_jobid_t jobid,
                                  ompi_list_t *nodelist)
 {
-    return mca_pcm_rsh_llm.llm_deallocate_resources(jobid, nodelist);
+    mca_pcm_rsh_module_t *me = (mca_pcm_rsh_module_t*) me_super;
+
+    return me->llm.llm_deallocate_resources(jobid, nodelist);
 }
