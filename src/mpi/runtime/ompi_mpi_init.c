5265b084a30241db8f984ed8d5e20bd6796bd0c4
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2004-09-14 14:28:20 +0000

    Oops....forgot to take out the diagnostic printouts. Sorry.

--- src/mpi/runtime/ompi_mpi_init.c
+++ src/mpi/runtime/ompi_mpi_init.c
@@ -134,7 +134,6 @@ int ompi_mpi_init(int argc, char **argv, int requested, int *provided)
 	goto error;
     }
 
-    ompi_output(0, "proc sessiondir(1): %s", ompi_process_info.proc_session_dir);
     /*
      *  Register my process info with my replica.
      */
@@ -143,7 +142,6 @@ int ompi_mpi_init(int argc, char **argv, int requested, int *provided)
 	goto error;
     }
 
-    ompi_output(0, "proc sessiondir(2): %s", ompi_process_info.proc_session_dir);
 
     /* finalize the rte startup */
     if (OMPI_SUCCESS != (ret = ompi_rte_init_finalstage(&allow_multi_user_threads,
@@ -152,7 +150,6 @@ int ompi_mpi_init(int argc, char **argv, int requested, int *provided)
         goto error;
     }
 
-    ompi_output(0, "proc sessiondir(3): %s", ompi_process_info.proc_session_dir);
 
     /* Once we've joined the RTE, see if any MCA parameters were
        passed to the MPI level */
@@ -161,7 +158,6 @@ int ompi_mpi_init(int argc, char **argv, int requested, int *provided)
         error = "mca_mpi_register_params() failed";
         goto error;
     }
-    ompi_output(0, "proc sessiondir(4): %s", ompi_process_info.proc_session_dir);
 
     /* initialize ompi procs */
     if (OMPI_SUCCESS != (ret = ompi_proc_init())) {
@@ -341,7 +337,6 @@ int ompi_mpi_init(int argc, char **argv, int requested, int *provided)
     }
 
     /* All done */
-    ompi_output(0, "proc sessiondir(end): %s", ompi_process_info.proc_session_dir);
 
     ompi_mpi_initialized = true;
     ompi_mpi_finalized = false;
