b3aca01d1a92c1ae5cbef17c7692ae3e6d804bff
Brian Barrett <brbarret@open-mpi.org>
2004-09-15 18:54:57 +0000

    * commit .ompi_ignored shell of a bproc component for the pcm
    This commit was SVN r2680.

---
+++ src/mca/pcm/bproc/src/pcm_bproc.h
@@ -0,0 +1,100 @@
+/* -*- C -*-
+ * 
+ * $HEADER$
+ *
+ * BWB: COMPONENT TODO
+ *
+ *  - add process reaping code
+ *  - trigger status change events on process death
+ *
+ */
+
+#include "ompi_config.h"
+
+#include "mca/pcm/pcm.h"
+#include "include/types.h"
+#include "class/ompi_list.h"
+
+#include <sys/types.h>
+
+#ifndef MCA_PCM_RMS_H_
+#define MCA_PCM_RMS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /*
+     * Module open / close
+     */
+    int mca_pcm_rms_component_open(void);
+    int mca_pcm_rms_component_close(void);
+
+    /*
+     * Startup / Shutdown
+     */
+    struct mca_pcm_base_module_1_0_0_t* mca_pcm_rms_init(int *priority, 
+                                                         bool *allow_multi_user_threads,
+                                                         bool *have_hidden_threads,
+                                                         int constraints);
+    int mca_pcm_rms_finalize(struct mca_pcm_base_module_1_0_0_t* me);
+
+    /*
+     * Interface
+     */
+    ompi_list_t* mca_pcm_rms_allocate_resources(struct mca_pcm_base_module_1_0_0_t* me,
+                                                mca_ns_base_jobid_t jobid,
+                                                int nodes, int procs);
+    bool mca_pcm_rms_can_spawn(struct mca_pcm_base_module_1_0_0_t* me);
+    int mca_pcm_rms_spawn_procs(struct mca_pcm_base_module_1_0_0_t* me,
+                                mca_ns_base_jobid_t jobid, ompi_list_t *schedule_list);
+    int mca_pcm_rms_kill_proc(struct mca_pcm_base_module_1_0_0_t* me,
+                              ompi_process_name_t *name, int flags);
+    int mca_pcm_rms_kill_job(struct mca_pcm_base_module_1_0_0_t* me,
+                             mca_ns_base_jobid_t jobid, int flags);
+    int mca_pcm_rms_deallocate_resources(struct mca_pcm_base_module_1_0_0_t* me,
+                                         mca_ns_base_jobid_t jobid,
+                                         ompi_list_t *nodelist);
+
+    /*
+     * Job management code
+     */
+    void mca_pcm_rms_job_list_init(void);
+    void mca_pcm_rms_job_list_fini(void);
+
+    int mca_pcm_rms_add_started_pids(mca_ns_base_jobid_t jobid, pid_t child_pid,
+                         mca_ns_base_vpid_t lower, mca_ns_base_vpid_t upper);
+    pid_t mca_pcm_rms_get_started_pid(mca_ns_base_jobid_t jobid, mca_ns_base_vpid_t vpid,
+                          bool remove_started_pid);
+    int mca_pcm_rms_get_started_pid_list(mca_ns_base_jobid_t jobid, pid_t **pids, size_t *len,
+                             bool remove_started_pids);
+    int mca_pcm_rms_remove_job(mca_ns_base_jobid_t jobid);
+
+    struct mca_pcm_rms_pids_t {
+        ompi_list_item_t super;
+        mca_ns_base_vpid_t lower;
+        mca_ns_base_vpid_t upper;
+        pid_t child;
+    };
+    typedef struct mca_pcm_rms_pids_t mca_pcm_rms_pids_t;
+    OBJ_CLASS_DECLARATION(mca_pcm_rms_pids_t);
+
+    struct mca_pcm_rms_job_item_t {
+        ompi_list_item_t super;
+        mca_ns_base_jobid_t jobid;
+        ompi_list_t *pids;
+    };
+    typedef struct mca_pcm_rms_job_item_t mca_pcm_rms_job_item_t;
+    OBJ_CLASS_DECLARATION(mca_pcm_rms_job_item_t);
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * Module variables
+ */
+extern int mca_pcm_rms_output;
+extern int mca_pcm_rms_use_ns;
+
+#endif /* MCA_PCM_RMS_H_ */
---
+++ src/mca/pcm/bproc/src/pcm_bproc_component.c
@@ -0,0 +1,132 @@
+/* -*- C -*-
+ * 
+ * $HEADER$
+ *
+ */
+
+#include "ompi_config.h"
+
+#include "include/constants.h"
+#include "include/types.h"
+#include "class/ompi_list.h"
+#include "mca/mca.h"
+#include "mca/base/mca_base_param.h"
+#include "mca/pcm/pcm.h"
+#include "mca/pcm/base/base.h"
+#include "mca/pcm/rms/pcm_rms.h"
+#include "mca/llm/base/base.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*
+ * Struct of function pointers and all that to let us be initialized
+ */
+mca_pcm_base_component_1_0_0_t mca_pcm_bproc_component = {
+  {
+    MCA_PCM_BASE_VERSION_1_0_0,
+
+    "bproc", /* MCA component name */
+    1,  /* MCA component major version */
+    0,  /* MCA component minor version */
+    0,  /* MCA component release version */
+    mca_pcm_bproc_component_open,  /* component open */
+    mca_pcm_bproc_component_close /* component close */
+  },
+  {
+    false /* checkpoint / restart */
+  },
+  mca_pcm_bproc_init,    /* component init */
+  NULL                 /* unique name */
+};
+
+
+struct mca_pcm_base_module_1_0_0_t mca_pcm_bproc_1_0_0 = {
+    mca_pcm_bproc_allocate_resources,
+    mca_pcm_bproc_can_spawn,
+    mca_pcm_bproc_spawn_procs,
+    mca_pcm_bproc_kill_proc,
+    mca_pcm_bproc_kill_job,
+    mca_pcm_bproc_deallocate_resources,
+    mca_pcm_bproc_finalize
+};
+
+
+/* need to create output stream to dump in file */
+ompi_output_stream_t mca_pcm_bproc_output_stream = {
+    false, /* lds_is_debugging  BWB - change me for release */
+    0,     /* lds_verbose_level */
+    false, /* lds_want_syslog */
+    0,     /* lds_syslog_priority */
+    NULL,  /* lds_syslog_ident */
+    "pcm: bproc: ", /* lds_prefix */
+    true,  /* lds_want_stdout */
+    false, /* lds_want_stderr */
+    true,  /* lds_want_file */
+    true,  /* lds_want_file_append */
+    "pcm_bproc" /* lds_file_suffix */
+};
+
+
+/*
+ * Module variables handles
+ */
+static int mca_pcm_bproc_param_priority;
+static int mca_pcm_bproc_param_debug;
+
+/*
+ * Component variables.  All of these are shared among the module
+ * instances, so they don't need to go in a special structure or
+ * anything.
+ */
+int mca_pcm_bproc_output = 0;
+
+int
+mca_pcm_bproc_component_open(void)
+{
+    mca_pcm_bproc_param_debug =
+        mca_base_param_register_int("pcm", "bproc", "debug", NULL, 100);
+
+  mca_pcm_bproc_param_priority =
+    mca_base_param_register_int("pcm", "bproc", "priority", NULL, 5);
+
+  return OMPI_SUCCESS;
+}
+
+
+int
+mca_pcm_bproc_component_close(void)
+{
+    return OMPI_SUCCESS;
+}
+
+
+mca_pcm_base_module_t*
+mca_pcm_bproc_init(int *priority, 
+                 bool *allow_multi_user_threads, 
+                 bool *have_hidden_threads,
+                 int constraints)
+{
+    int debug;
+
+    mca_base_param_lookup_int(mca_pcm_bproc_param_debug, &debug);
+    mca_pcm_bproc_output = ompi_output_open(&mca_pcm_bproc_output_stream);
+    ompi_output_set_verbosity(mca_pcm_bproc_output, debug);
+
+    mca_base_param_lookup_int(mca_pcm_bproc_param_priority, priority);
+
+    *allow_multi_user_threads = true;
+    *have_hidden_threads = false;
+
+    return NULL;
+}
+
+
+int
+mca_pcm_bproc_finalize(struct mca_pcm_base_module_1_0_0_t* me)
+{
+    return OMPI_SUCCESS;
+}
+
