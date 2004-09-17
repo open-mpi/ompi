54bdb20351987c2dbc50348c8fbb39b1933bae97
Edgar Gabriel <gabriel@cs.uh.edu>
2004-09-17 10:10:24 +0000

    removing the second lock. By introducing a new function proc_find_and_add we can work with a single lock. Actually, this makes the stuff for the dynamic process management easier.
    This commit was SVN r2740.

--- src/proc/proc.h
+++ src/proc/proc.h
@@ -68,6 +68,12 @@ ompi_proc_t * ompi_proc_find ( const ompi_process_name_t* name );
 
 
 /**
+ * Returns the proc instance for a given name. If process is not known,
+ * we add it to the process list.
+*/
+ompi_proc_t * ompi_proc_find_and_add ( const ompi_process_name_t* name );
+
+/**
  * INPUT: ompi_proc_t **proclist : list of process pointers
  * INPUT: int proclistsize       : lenght of the proclist array
  * IN/OUT: ompi_buffer_t *buf    : an ompi_buffer containing the packed names
