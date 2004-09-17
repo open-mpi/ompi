e14bbf5fc439190688b50e3fc12eb34ea3b8d135
Edgar Gabriel <gabriel@cs.uh.edu>
2004-09-17 10:11:22 +0000

    adaping some routines to use proc_find_and_add activating name_publishing (also tested, testcode will be checked in soon). adaping the port-stuff to the new format.
    This commit was SVN r2741.

--- src/communicator/comm_publish.c
+++ src/communicator/comm_publish.c
@@ -12,23 +12,58 @@
 #include "include/constants.h"
 #include "mca/pcm/pcm.h"
 #include "mca/pml/pml.h"
-
-#ifndef HAVE_REGISTRY
-/* just to keep the linker happy */
-int ompi_comm_namepublish ( char *service_name, char *port_name ) 
-{
-    return OMPI_SUCCESS;
-}
-char* ompi_comm_namelookup ( char *service_name )
-{
-    return NULL;
-}
-int ompi_comm_nameunpublish ( char *service_name )
-{
-    return OMPI_SUCCESS;
-}
-#else
+#include "mca/ns/ns.h"
+#include "mca/gpr/base/base.h"
 #include "mca/gpr/gpr.h"
+
+static ompi_mutex_t ompi_port_lock;
+static int port_id=0;
+
+int ompi_open_port(char *port_name)
+{
+    ompi_proc_t **myproc=NULL;
+    char *name=NULL;
+    size_t size=0;
+    int lport_id=-1;
+
+    /*
+     * The port_name is equal to the OOB-contact information
+     * and an integer. The reason for adding the integer is
+     * to make the port unique for multi-threaded scenarios.
+     */
+  
+    myproc = ompi_proc_self (&size);
+    name = ompi_name_server.get_proc_name_string (&(myproc[0]->proc_name));
+
+    OMPI_THREAD_LOCK(&ompi_port_lock);
+    lport_id = port_id++;
+    OMPI_THREAD_UNLOCK(&ompi_port_lock);
+
+    sprintf (port_name, "%s:%d", name, lport_id);
+    free ( myproc );
+    free ( name );
+    
+    return OMPI_SUCCESS;
+}
+
+/* takes a port_name and separates it into the process_name 
+   and the tag
+*/
+char *ompi_parse_port (char *port_name, int *tag) 
+{
+    char tmp_port[MPI_MAX_PORT_NAME], *tmp_string;
+
+    tmp_string = (char *) malloc (MPI_MAX_PORT_NAME);
+    if (NULL ==  tmp_string ) {
+	return NULL;
+    }
+
+    strncpy (tmp_port, port_name, MPI_MAX_PORT_NAME);
+    strncpy (tmp_string, strtok(tmp_port, ":"), MPI_MAX_PORT_NAME);
+    sscanf( strtok(NULL, ":"),"%d", tag);
+    
+    return tmp_string;
+}
 
 /* 
  * publish the port_name using the service_name as a token
@@ -37,21 +72,38 @@ int ompi_comm_nameunpublish ( char *service_name )
  */
 int ompi_comm_namepublish ( char *service_name, char *port_name ) 
 {
-    return (ompi_registry_put((uint8_t *)port_name, strlen(service_name), 
-                              "universe", service_name, NULL));
+
+    char *key[2];
+    
+    key[0] = service_name;
+    key[1] = NULL;
+    return ompi_registry.put(OMPI_REGISTRY_OVERWRITE, "ompi_name_publish", 
+			     key, port_name, (strlen(port_name)+1));
 }
 
 char* ompi_comm_namelookup ( char *service_name )
 {
-    ompi_registry_value_t *tmp;
-    char *stmp=NULL;
+    char *key[2];
+    ompi_list_t *tmp=NULL;
+    ompi_registry_value_t *vtmp=NULL;
+    char *stmp=NULL, *stmp2=NULL;
 
-    tmp = ompi_registry_get("universe", service_name, NULL);
+    key[0] = service_name;
+    key[1] = NULL;
+    tmp = ompi_registry.get(OMPI_REGISTRY_NONE, "ompi_name_publish", key);
     if ( NULL != tmp ) {
-        stmp = (char*)tmp->object;
+        vtmp = (ompi_registry_value_t *) ompi_list_get_first(tmp);
+	if (NULL != vtmp) {
+	    stmp  = (char *)vtmp->object;
+	    if ( NULL != stmp) {
+		stmp2 = strdup(stmp);
+		OBJ_RELEASE(vtmp);
+	    }
+}
+	OBJ_RELEASE(tmp);
     }
 
-    return (stmp);
+    return (stmp2);
 }
 
 /* 
@@ -62,7 +114,8 @@ char* ompi_comm_namelookup ( char *service_name )
  */
 int ompi_comm_nameunpublish ( char *service_name )
 {
-    return (ompi_registry_del("universe", service_name, NULL)); 
+    char *key[2];
+    key[0] = service_name;
+    key[1] = NULL;
+    return ompi_registry.delete_object(OMPI_REGISTRY_NONE, "ompi_name_publish", key); 
 }
-
-#endif
