d54429151c37ff46fcd5bb6a83e7cb051878e4c7
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2004-09-26 17:43:35 +0000

    * code to deal with getting callbacks / waiting for SIGCHLD.  These should   only be used if the RTE init functions have been called.  Not quite as   flexible as the real waitpid() function (no -1 support), but all I need   for the SSH / BProc / RMS pcms.  This code is not yet turned on by   default (need to add the init / finalize calls to ompi_rte_init?? and   ompi_rte_finalize()

---
+++ src/runtime/ompi_rte_wait.h
@@ -0,0 +1,69 @@
+/*
+ * $HEADER$
+ */
+
+/**
+ * @file
+ *
+ * Interface for waitpid / async notification of child death with the
+ * libevent runtime system.
+ */
+#ifndef OMPI_RTE_WAIT_H
+#define OMPI_RTE_WAIT_H
+
+#include "ompi_config.h"
+
+#include <sys/types.h>
+
+/** typedef for callback function used in \c ompi_rte_wait_cb */
+typedef void (*ompi_rte_wait_fn_t)(pid_t wpid, int status, void *data);
+
+
+/**
+ * Wait for process terminiation
+ *
+ * Similar to \c waitpid, \c ompi_rte_waitpid utilizes the run-time
+ * event library for process terminiation notification.  The \c
+ * WUNTRACED option is not supported, but the \c WNOHANG option is
+ * supported.
+ *
+ * \note A \c wpid value of \c -1 is not currently supported and will
+ * return an error.
+ */
+pid_t ompi_rte_waitpid(pid_t wpid, int *status, int options);
+
+
+/**
+ * Register a callback for process termination
+ *
+ * Register a callback for notification when \c wpid causes a SIGCHLD.
+ * \c waitpid() will have already been called on the process at this
+ * time.  
+ *
+ * If a thread is already blocked in \c ompi_rte_waitpid for \c wpid,
+ * this function will return \c OMPI_ERR_EXISTS.  It is legal for
+ * multiple callbacks to be registered for a single \c wpid.
+ *
+ * \warning It is not legal for \c wpid to be -1 when registering a
+ * callback.
+ */
+int ompi_rte_wait_cb(pid_t wpid, ompi_rte_wait_fn_t callback, void *data);
+
+
+/**
+ * \internal
+ *
+ * Initialize the wait system (allocate mutexes, etc.)
+ */
+int ompi_rte_wait_init(void);
+
+
+/**
+ * \internal
+ *
+ * Finalize the wait system (deallocate mutexes, etc.)
+ */
+int ompi_rte_wait_finalize(void);
+
+
+#endif
---
+++ test/rte/sigchld.c
@@ -0,0 +1,69 @@
+/*
+ * $HEADER$
+ */
+
+#include "mpi.h"
+#include "runtime/ompi_rte_wait.h"
+#include "runtime/ompi_progress.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int count = 0;
+
+static void
+callback(pid_t pid, int status, void *data)
+{
+    printf("callback for %d, %d\n", pid, status);
+    count--;
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    pid_t pid, ret;
+    int status = -1;
+
+    MPI_Init(&argc, &argv);
+    ompi_rte_wait_init();
+
+    pid = fork();
+    if (pid > 0) {
+        count++;
+        ompi_rte_wait_cb(pid, callback, NULL);
+    } else {
+        sleep(10);
+        printf("pid %d exiting\n", getpid());
+        exit(0);
+    }
+
+    pid = fork();
+    if (pid > 0) {
+        ret = ompi_rte_waitpid(pid, &status, 0);
+        printf("pid %d waitpid, status %d\n", ret, status);
+    } else {
+        sleep(5);
+        printf("pid %d exiting\n", getpid());
+        exit(0);
+    }
+
+    pid = fork();
+    if (pid > 0) {
+        count++;
+        ompi_rte_wait_cb(pid, callback, NULL);
+    } else {
+        printf("pid %d exiting\n", getpid());
+        exit(0);
+    }
+    
+    while (count > 0) { ompi_progress(); }
+
+    ompi_rte_wait_finalize();
+    MPI_Finalize();
+
+
+    return 0;
+}
