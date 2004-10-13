98c158c0d3525706a944a6d90c1f1c74bf3cc55e
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-10-13 20:21:07 +0000

    This directory contains the functions windows functions for handling some system calls. Has not been integrated ... simply committing the directory into the tree, so it will not get commpiled

---
+++ src/win32/win_compat.h
@@ -0,0 +1,49 @@
+/*
+ $HEADER&
+ */
+
+#ifndef OMPI_WIN_COMPAT_H
+#define OMPI_WIN_COMPAT_H
+
+/* It is always better to include windows.h with the lean and mean option. 
+   So, include it with that option and then include some which are required 
+   for us in ompi. Note: this file is included only on windows */
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#   include <windows.h>
+#undef WIN32_LEAN_AND_MEAN
+
+/* other utility header files */
+#include <cderr.h>
+#include <dde.h>
+#include <ddeml.h>
+#include <dlgs.h>
+#include <imm.h>
+#include <lzexpand.h>
+#include <mmsystem.h>
+#include <nb30.h>
+#include <rpc.h>
+#include <shellapi.h>
+#include <winperf.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <io.h>
+#include "win32/ompi_uio.h"
+#include "win32/ompi_time.h"
+#include "win32/ompi_utsname.h"
+#include "win32/ompi_util.h"
+
+#define MAXPATHLEN MAX_PATH
+#define MAXHOSTNAMELEN MAX_PATH
+typedef unsigned short mode_t;
+typedef unsigned long size_t;
+typedef long ssize_t;
+#define OMPI_WINDOWS 
+
+/* Anju: some random #defines which I know offhand, but need to configure it */
+#define OMPI_ALIGNMENT_CXX_BOOL OMPI_ALIGNMENT_INT
+#define SIZEOF_BOOL SIZEOF_INT
+#define __func__ __FUNCTION__
+ 
+#endif /* compat */
