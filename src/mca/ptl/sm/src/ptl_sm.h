ed9a3bf48fb835b1cc01d7bc14382037bc0762fb
Rich Graham <rlgraham@open-mpi-git-mirror.example.com>
2004-10-21 22:40:25 +0000

    starting to debug the shared memory implmentation.  Changing process count references to int's rather than size_t's. use asprintf to store the name of the shared memory backing file.

--- src/mca/ptl/sm/src/ptl_sm.h
+++ src/mca/ptl/sm/src/ptl_sm.h
@@ -46,8 +46,8 @@ struct mca_ptl_sm_component_t {
     int sm_second_frag_free_list_num;       /**< initial size of free lists */
     int sm_second_frag_free_list_max;       /**< maximum size of free lists */
     int sm_second_frag_free_list_inc;       /**< number of elements to alloc when growing free lists */
-    size_t sm_max_procs;                      /**< upper limit on the number of processes using the shared memory pool */
-    size_t sm_extra_procs;                  /**< number of extra procs to allow */
+    int sm_max_procs;                      /**< upper limit on the number of processes using the shared memory pool */
+    int sm_extra_procs;                  /**< number of extra procs to allow */
     char* sm_mpool_name;                  /**< name of shared memory pool module */
     mca_mpool_base_module_t* sm_mpool; /**< shared memory pool */
     void* sm_mpool_base;                  /**< base address of shared memory pool */
@@ -134,8 +134,9 @@ struct mca_ptl_sm_t {
                                                     fragments that are
                                                     awaiting resources */
     ompi_mutex_t sm_pending_ack_lock;
-    ompi_list_t sm_pending_ack; /* list of fragmnent that need to be
+    ompi_list_t sm_pending_ack; /**< list of fragmnent that need to be
                                    acked */
+    bool ptl_inited;  /**< flag indicating if ptl has been inited */
 };
 typedef struct mca_ptl_sm_t mca_ptl_sm_t;
 
