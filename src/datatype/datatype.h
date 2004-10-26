a83389702e7a05d2b93ac74948ae23ce6a677f32
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2004-10-26 06:09:38 +0000

    Remove useless function.

--- src/datatype/datatype.h
+++ src/datatype/datatype.h
@@ -147,8 +147,6 @@ static inline int ompi_ddt_type_size ( dt_desc_t* pData, int *size )
 static inline int ompi_ddt_type_extent( dt_desc_t* pData, long* extent )
 { *extent = (pData->ub - pData->lb); return 0; }
 
-static inline int ompi_ddt_type_resize( dt_desc_t* pOld, long lb, long extent, dt_desc_t** pNew )
-{ /* empty function */ return -1; }
 static inline int ompi_ddt_get_extent( dt_desc_t* pData, long* lb, long* extent)
 { *lb = pData->lb; *extent = pData->ub - pData->lb; return 0; }
 static inline int ompi_ddt_get_true_extent( dt_desc_t* pData, long* true_lb, long* true_extent)
