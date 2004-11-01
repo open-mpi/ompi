b2841e176c6d8f69a21e3d9ccab9bce1ccf02e1e
Craig E Rasmussen <rasmus@cas.uoregon.edu>
2004-11-01 17:41:16 +0000

    Changed to declare interfaces only, procedures defined elsewhere.
    This commit was SVN r3461.

--- src/mpi/f90/mpi.f90
+++ src/mpi/f90/mpi.f90
@@ -1,9 +1,23 @@
 module mpi_kinds
 
-  !
-  ! kind for 4 byte integer (selected_int_kind(18) for 8 byte integer)
-  !
+!
+! kind for 4 byte integer (selected_int_kind(18) for 8 byte integer)
+! (WARNING, Compiler dependent)
+!
+  integer :: MPI_ADDRESS_KIND
   parameter(MPI_ADDRESS_KIND = selected_int_kind(9))
+
+!
+! integer kinds
+!
+
+  integer :: MPI_INTEGER1_KIND, MPI_INTEGER2_KIND, MPI_INTEGER4_KIND
+  integer :: MPI_INTEGER8_KIND, MPI_INTEGER16_KIND
+  parameter(MPI_INTEGER1_KIND  = selected_int_kind(2))
+  parameter(MPI_INTEGER2_KIND  = selected_int_kind(4))
+  parameter(MPI_INTEGER4_KIND  = selected_int_kind(9))
+  parameter(MPI_INTEGER8_KIND  = selected_int_kind(18))
+  parameter(MPI_INTEGER16_KIND = selected_int_kind(19))
 
 end module mpi_kinds
 
@@ -12,55 +26,44 @@ module mpi
 
   use mpi_kinds
 
+  include "mpif.h"
+
   interface mpi_send
-    module procedure mpi_send_1DI
-    module procedure mpi_send_1DR
-  end interface
 
+!
+! Integer array types
+!
 
-contains
-
-
-subroutine mpi_send_1DI(a, count, datatype, dest, tag, comm, ierr)
-  implicit none
-  interface
-    subroutine mpi_send_f(buf, count, datatype, dest, tag, comm, ierr)
-      use mpi_kinds
-      integer(kind=MPI_ADDRESS_KIND), intent(in) :: buf
+    subroutine mpi_send_1DI(a, count, datatype, dest, tag, comm, ierr)
+      integer :: a(:)
       integer, intent(in) :: count, datatype, dest, tag, comm
-      integer, intent(out) :: ierr
-    end subroutine mpi_send_f
-  end interface
-  integer :: a(:)
-  integer, intent(in) :: count, datatype, dest, tag, comm
-  integer, intent(out), optional :: ierr
-  integer(kind=MPI_ADDRESS_KIND) :: buf
+      integer, intent(out), optional :: ierr
+    end subroutine mpi_send_1DI
 
-  call MPI_GET_ADDRESS(a(1), buf, ierr)
-  call mpi_send_f(buf, count, datatype, dest, tag, comm, ierr)
-
-end subroutine mpi_send_1DI
-
-
-subroutine mpi_send_1DR(a, count, datatype, dest, tag, comm, ierr)
-  implicit none
-  interface
-    subroutine mpi_send_f(buf, count, datatype, dest, tag, comm, ierr)
+    subroutine mpi_send_1DI1(a, count, datatype, dest, tag, comm, ierr)
       use mpi_kinds
-      integer(kind=MPI_ADDRESS_KIND), intent(in) :: buf
+      integer(kind=MPI_INTEGER1_KIND) :: a(:)
       integer, intent(in) :: count, datatype, dest, tag, comm
-      integer, intent(out) :: ierr
-    end subroutine mpi_send_f
+      integer, intent(out), optional :: ierr
+    end subroutine mpi_send_1DI1
+
+    subroutine mpi_send_1DI2(a, count, datatype, dest, tag, comm, ierr)
+      use mpi_kinds
+      integer(kind=MPI_INTEGER2_KIND) :: a(:)
+      integer, intent(in) :: count, datatype, dest, tag, comm
+      integer, intent(out), optional :: ierr
+    end subroutine mpi_send_1DI2
+
+!
+! Real array types
+!
+
+    subroutine mpi_send_1DR(a, count, datatype, dest, tag, comm, ierr)
+      real :: a(:)
+      integer, intent(in) :: count, datatype, dest, tag, comm
+      integer, intent(out), optional :: ierr
+    end subroutine mpi_send_1DR
+
   end interface
-  real :: a(:)
-  integer, intent(in) :: count, datatype, dest, tag, comm
-  integer, intent(out), optional :: ierr
-  integer(kind=MPI_ADDRESS_KIND) :: buf
-  
-  call MPI_GET_ADDRESS(a(1), buf, ierr)
-  call mpi_send_f(buf, count, datatype, dest, tag, comm, ierr)
-
-end subroutine mpi_send_1DR
-
 
 end module mpi
