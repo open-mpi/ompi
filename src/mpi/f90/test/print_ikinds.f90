d6525e17c087576f0ddb551290ba7c048ecb1358
Craig E Rasmussen <rasmus@cas.uoregon.edu>
2004-11-01 17:57:32 +0000

    Initial version(s).
    This commit was SVN r3462.

---
+++ src/mpi/f90/test/print_ikinds.f90
@@ -0,0 +1,13 @@
+!
+! print_ikinds.f90
+!   - prints out values of selected_int_kind to determine Fortran compiler
+!     dependent values
+!
+program main
+  integer :: i
+
+  do i = 1, 24
+    print*, "i = ", i, "selected_int_kind(i) = ", selected_int_kind(i)
+  end do
+
+end program main
---
+++ src/mpi/f90/test/test_send.f90
@@ -0,0 +1,37 @@
+!
+! test_send.f90 - tests mpi_send variants (calls functions in send_t.c)
+!
+! Actual tests do not use MPI, they just test parameters and types.
+!
+
+program main
+  use mpi
+
+  implicit none
+
+  integer(kind=MPI_INTEGER1_KIND) :: ai1(10) = (/1,2,3,4,5,6,7,8,9,10/)
+  integer, target  :: ai(10) = (/10,9,8,7,6,5,4,3,2,1/)
+  integer, pointer :: pai(:)
+  integer :: count, dest, tag, ierr
+
+  count = 10
+  dest = 59
+  tag = 999
+
+  call mpi_send(ai, count, MPI_INTEGER, dest, tag, MPI_COMM_WORLD, ierr)
+  if (ierr /= MPI_SUCCESS) then
+    print *, "FAILURE test_send: ierr = ", ierr
+  end if
+
+  pai => ai
+  call mpi_send(pai, count, MPI_INTEGER, dest, tag, MPI_COMM_WORLD, ierr)
+  if (ierr /= MPI_SUCCESS) then
+    print *, "FAILURE test_send: ierr = ", ierr
+  end if
+
+  call mpi_send(ai1, count, MPI_INTEGER1, dest, tag, MPI_COMM_SELF, ierr)
+  if (ierr /= MPI_SUCCESS) then
+    print *, "FAILURE test_send: ierr = ", ierr
+  end if
+
+end program
