076b3676b8c59f0a7a0ddb818a92780e0ad78e06
Brian Barrett <brbarret@open-mpi.org>
2004-11-01 16:16:05 +0000

    * forgot to do file add with last commit (Bad Brian)
    This commit was SVN r3459.

---
+++ src/mpi/runtime/ompi_mpi_abort.c
@@ -0,0 +1,54 @@
+/*
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include "communicator/communicator.h"
+#include "util/show_help.h"
+#include "util/proc_info.h"
+#include "runtime/runtime.h"
+#include "communicator/communicator.h"
+#include "mca/ns/ns.h"
+#include "mca/ns/base/base.h"
+#include "event/event.h"
+
+#if HAVE_SIGNAL_H
+#include <signal.h>
+#endif
+
+int
+ompi_mpi_abort(struct ompi_communicator_t* comm,
+               int errcode,
+               bool kill_remote_of_intercomm)
+{
+    mca_ns_base_jobid_t jobid;
+    int ret;
+    
+    /* Kill everyone in the job.  We may make this better someday to
+       actually loop over ompi_rte_kill_proc() to only kill the procs
+       in comm, and additionally to somehow use errorcode. */
+
+    jobid = ompi_name_server.get_jobid(ompi_rte_get_self());
+    ret = ompi_rte_kill_job(jobid, SIGTERM, errcode, 0);
+
+    if (1 /* BWB - fix me */) {
+        while (1) {
+            /* we successfully started the kill.  Just sit around and
+               wait to be slaughtered.  run the event loop if we
+               should */
+            if (!OMPI_HAVE_THREADS || ompi_event_progress_thread()) {
+                ompi_event_loop(0);
+            } else {
+                sleep(1000);
+            }
+        }
+    } else {
+        /* If we return from this, then the selected PCM was unable to
+           kill the job (and the rte printed an error message).  So
+           just die die die. */
+        abort();
+    }
+
+    return OMPI_SUCCESS;
+}
