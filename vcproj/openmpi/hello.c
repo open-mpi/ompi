8b4797ea2c70d4d30544551a076e6e013254075b
Prabhanjan Kambadur <pkambadu@open-mpi-git-mirror.example.com>
2004-11-03 00:42:41 +0000

    adding my first program

---
+++ vcproj/openmpi/hello.c
@@ -0,0 +1,14 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int rank;
+    int size;
+    MPI_Init(&argc, &argv);
+    printf("Hello World!!\n");
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf ("I am rank %d of %d\n", rank, size);
+    MPI_Finalize();
+    return 0;
+}
