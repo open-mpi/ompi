357891b26bfd906bf0057de1d7c064333f387385
Prabhanjan Kambadur <pkambadu@osl.iu.edu>
2004-11-03 18:57:20 +0000

    simple event test
    This commit was SVN r3495.

---
+++ vcproj/console/test_event.c
@@ -0,0 +1,15 @@
+#include "ompi_config.h"
+#include "event.h"
+
+int main(int argc, char **argv) {
+
+	WSADATA winsockdata;
+	WSAStartup (MAKE_WORD(2,2), &winsockdata);
+
+	ompi_init();
+	ompi_event_init();
+	WSACleanup();
+
+
+	return 0;
+}
