a1e7233dd1876c9d972d00dd80930849f66d4f32
Jeff Squyres <jsquyres@cisco.com>
2004-11-08 05:04:54 +0000

    Per MPI-2:88-89 -- if we are comm_free'ing the parent, then subsequent calls to comm_get_parent shuld return comm_null.  Additionally, if it's freed, we don't want to free it again during ompi_comm_finalize. So if we're freeing the user's handle to the parent comm (i.e., we're not calling ompi_comm_free on ompi_mpi_comm_parent itself), then explicitly set ompi_mpi_comm_parent to &ompi_mpi_comm_null.  This does two things:
    1. subsequent calls to comm_get_parent will return comm_null
    2. we won't try to ompi_comm_free the ompi_mpi_comm_parent in
       ompi_comm_finalize() 
    
    This commit was SVN r3543.

--- src/communicator/comm.c
+++ src/communicator/comm.c
@@ -706,6 +706,14 @@ int ompi_comm_free ( ompi_communicator_t **comm )
         OBJ_RELEASE((*comm)->c_keyhash);
     }
 
+    /* Special case: if we are freeing the parent handle, then we need
+       to set our internal handle to the parent to be equal to
+       COMM_NULL.  This is according to MPI-2:88-89. */
+
+    if (*comm == ompi_mpi_comm_parent && comm != &ompi_comm_parent) {
+        ompi_mpi_comm_parent = &ompi_mpi_comm_null;
+    }
+
     /* Release the communicator */
 
     OBJ_RELEASE ( (*comm) );
