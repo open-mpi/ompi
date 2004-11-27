0c05204c134f306eae6fb0545e9f45b259ee1d50
Jeff Squyres <jsquyres@cisco.com>
2004-11-27 12:43:19 +0000

    Arf -- didn't test the OMPI_DEBUG_ZERO with an optimized build, and it caused the nightlies to fail.  Move the definition of OMPI_DEBUG_ZERO up to ompi_config_bottom.h and now all is well.
    This commit was SVN r3664.

--- src/util/malloc.h
+++ src/util/malloc.h
@@ -36,15 +36,6 @@
 #define OMPI_MALLOC_DEBUG_LEVEL 2
 #endif
 
-/*
- * If we're mem debugging, make the OMPI_DEBUG_ZERO resolve to memset
- */
-#if defined(OMPI_ENABLE_MEM_DEBUG) && OMPI_ENABLE_MEM_DEBUG
-#define OMPI_DEBUG_ZERO(obj) memset(&obj, 0, sizeof(obj))
-#else
-#define OMPI_DEBUG_ZERO(obj)
-#endif
-
 #if defined(c_plusplus) || defined(__cplusplus)
 extern "C" {
 #endif
