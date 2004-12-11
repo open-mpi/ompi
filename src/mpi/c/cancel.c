9f6cbfde9b6bb9d86c9e695bfa14dc44ceecc9bb
Jeff Squyres <jsquyres@cisco.com>
2004-12-11 01:32:17 +0000

    Remove duplicate copyright
    This commit was SVN r3781.

--- src/mpi/c/cancel.c
+++ src/mpi/c/cancel.c
@@ -5,14 +5,14 @@
  *                         All rights reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
  *                         University of Stuttgart.  All rights reserved.
- * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
- *                         University of Stuttgart.  All rights reserved.
+ *
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
  * 
  * $HEADER$
  */
+
 #include "ompi_config.h"
 #include <stdio.h>
 
