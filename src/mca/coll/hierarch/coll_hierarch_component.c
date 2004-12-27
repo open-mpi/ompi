f5abd20a5767668e1f33000e70e7a737c87df601
Edgar Gabriel <gabriel@cs.uh.edu>
2004-12-27 08:56:33 +0000

    adding the basic framework for the hierarchical collective module.
    This commit was SVN r3880.

---
+++ src/mca/coll/hierarch/coll_hierarch_component.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ *
+ * These symbols are in a file by themselves to provide nice linker
+ * semantics.  Since linkers generally pull in symbols by object
+ * files, keeping these symbols as the only symbols in this file
+ * prevents utility programs such as "ompi_info" from having to import
+ * entire components just to query their version and parameters.
+ */
+
+#include "ompi_config.h"
+#include "coll_hierarch.h"
+#include "coll-hierarch-version.h"
+
+#include "mpi.h"
+#include "mca/coll/coll.h"
+#include "coll_hierarch.h"
+
+/*
+ * Public string showing the coll ompi_hierarch component version number
+ */
+const char *mca_coll_hierarch_component_version_string =
+  "OMPI/MPI hierarch collective MCA component version " MCA_coll_hierarch_VERSION;
+
+/*
+ * Global variable
+ */
+int mca_coll_hierarch_priority_param = -1;
+int mca_coll_hierarch_verbose_param = -1;
+int mca_coll_hierarch_verbose = 0;
+
+/*
+ * Local function
+ */
+static int hierarch_open(void);
+
+/*
+ * Instantiate the public struct with all of our public information
+ * and pointers to our public functions in it
+ */
+
+const mca_coll_base_component_1_0_0_t mca_coll_hierarch_component = {
+
+  /* First, the mca_component_t struct containing meta information
+     about the component itself */
+
+  {
+    /* Indicate that we are a coll v1.0.0 component (which also implies a
+       specific MCA version) */
+
+    MCA_COLL_BASE_VERSION_1_0_0,
+
+    /* Component name and version */
+
+    "hierarch",
+    MCA_coll_hierarch_MAJOR_VERSION,
+    MCA_coll_hierarch_MINOR_VERSION,
+    MCA_coll_hierarch_RELEASE_VERSION,
+
+    /* Component open and close functions */
+
+    hierarch_open,
+    NULL
+  },
+
+  /* Next the MCA v1.0.0 component meta data */
+
+  {
+   /* Whether the component is checkpointable or not */
+
+   true
+  },
+
+  /* Initialization / querying functions */
+
+  mca_coll_hierarch_init_query,
+  mca_coll_hierarch_comm_query,
+  NULL
+};
+
+
+static int hierarch_open(void)
+{
+    /* Use a low priority, but allow other components to be lower */
+    
+    mca_coll_hierarch_priority_param = 
+        mca_base_param_register_int("coll", "hierarch", "priority", NULL, 20);
+    mca_coll_hierarch_verbose_param = 
+        mca_base_param_register_int("coll", "hierarch", "verbose", NULL, 
+                                    mca_coll_hierarch_verbose);
+
+    return OMPI_SUCCESS;
+}
---
+++ src/mca/coll/hierarch/coll_hierarch_reduce.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+#include "coll_hierarch.h"
+
+#include <stdio.h>
+
+#include "mpi.h"
+#include "include/constants.h"
+#include "util/output.h"
+#include "mca/coll/coll.h"
+#include "mca/coll/base/base.h"
+#include "coll_hierarch.h"
+
+
+/*
+ *	reduce_intra
+ *
+ *	Function:	- reduction using O(N) algorithm
+ *	Accepts:	- same as MPI_Reduce()
+ *	Returns:	- MPI_SUCCESS or error code
+ */
+int mca_coll_hierarch_reduce_intra(void *sbuf, void *rbuf, int count,
+                               struct ompi_datatype_t *dtype, 
+                               struct ompi_op_t *op,
+                               int root, struct ompi_communicator_t *comm)
+{
+  ompi_output_verbose(10, mca_coll_base_output, "In hierarch reduce_intra");
+  return comm->c_coll_basic_module->coll_reduce(sbuf, rbuf, count, dtype,
+                                                op, root, comm);
+}
+
+
+/*
+ *	reduce_log_inter
+ *
+ *	Function:	- reduction using O(N) algorithm
+ *	Accepts:	- same as MPI_Reduce()
+ *	Returns:	- MPI_SUCCESS or error code
+ */
+int mca_coll_hierarch_reduce_inter(void *sbuf, void *rbuf, int count,
+                               struct ompi_datatype_t *dtype, 
+                               struct ompi_op_t *op,
+                               int root, struct ompi_communicator_t *comm)
+{
+  ompi_output_verbose(10, mca_coll_base_output, "In hierarch reduce_inter");
+  return comm->c_coll_basic_module->coll_reduce(sbuf, rbuf, count, dtype,
+                                                op, root, comm);
+}
