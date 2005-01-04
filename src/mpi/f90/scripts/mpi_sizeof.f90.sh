3d922d89473dafdbad9d85d4a31dce747630831d
Craig E Rasmussen <rasmus@cas.uoregon.edu>
2005-01-04 17:46:10 +0000

    Initial version.
    This commit was SVN r3897.

---
+++ src/mpi/f90/scripts/mpi_sizeof.f90.sh
@@ -0,0 +1,109 @@
+#! /bin/sh
+
+. fortran_kinds.sh
+
+procedure='MPI_Sizeof'
+
+rank=0
+for kind in $ikinds
+do
+    proc="${procedure}${rank}DI${kind}"
+    echo "subroutine ${proc}(x, size, ierr)"
+    echo "  use mpi_kinds"
+    echo "  integer(kind=MPI_INTEGER${kind}_KIND), intent(in) :: x"
+    echo "  integer, intent(out) :: size"
+    echo "  integer, intent(out) :: ierr"
+    echo "  size = sizeof(x)"
+    echo "  ierr = 0"
+    echo "end subroutine ${proc}"
+    echo
+done
+
+for kind in $rkinds
+do
+    proc="${procedure}${rank}DR${kind}"
+    echo "subroutine ${proc}(x, size, ierr)"
+    echo "  use mpi_kinds"
+    echo "  real(kind=MPI_REAL${kind}_KIND), intent(in) :: x"
+    echo "  integer, intent(out) :: size"
+    echo "  integer, intent(out) :: ierr"
+    echo "  size = sizeof(x)"
+    echo "  ierr = 0"
+    echo "end subroutine ${proc}"
+    echo
+done
+
+for kind in $ckinds
+do
+    proc="${procedure}${rank}DC${kind}"
+    echo "subroutine ${proc}(x, size, ierr)"
+    echo "  use mpi_kinds"
+    echo "  complex(kind=MPI_REAL${kind}_KIND), intent(in) :: x"
+    echo "  integer, intent(out) :: size"
+    echo "  integer, intent(out) :: ierr"
+    echo "  size = sizeof(x)"
+    echo "  ierr = 0"
+    echo "end subroutine ${proc}"
+    echo
+done
+
+
+for rank in $ranks
+do
+  case "$rank" in  1)  dim=':'  ;  esac
+  case "$rank" in  2)  dim=':,:'  ;  esac
+  case "$rank" in  3)  dim=':,:,:'  ;  esac
+  case "$rank" in  4)  dim=':,:,:,:'  ;  esac
+  case "$rank" in  5)  dim=':,:,:,:,:'  ;  esac
+  case "$rank" in  6)  dim=':,:,:,:,:,:'  ;  esac
+  case "$rank" in  7)  dim=':,:,:,:,:,:,:'  ;  esac
+
+  for kind in $ikinds
+  do
+    proc="${procedure}${rank}DI${kind}"
+    echo "subroutine ${proc}(x, size, ierr)"
+    echo "  use mpi_kinds"
+    echo "  integer(kind=MPI_INTEGER${kind}_KIND), dimension(${dim}), intent(in) :: x"
+    echo "  integer, intent(out) :: size"
+    echo "  integer, intent(out) :: ierr"
+    echo "  integer(kind=MPI_INTEGER${kind}_KIND) :: type"
+    echo "  size = sizeof(type)"
+    echo "  ierr = 0"
+    echo "end subroutine ${proc}"
+    echo
+  done
+
+  for kind in $rkinds
+  do
+    proc="${procedure}${rank}DR${kind}"
+    echo "subroutine ${proc}(x, size, ierr)"
+    echo "  use mpi_kinds"
+    echo "  real(kind=MPI_REAL${kind}_KIND), dimension(${dim}), intent(in) :: x"
+    echo "  integer, intent(out) :: size"
+    echo "  integer, intent(out) :: ierr"
+    echo "  real(kind=MPI_REAL${kind}_KIND) :: type"
+    echo "  size = sizeof(type)"
+    echo "  ierr = 0"
+    echo "end subroutine ${proc}"
+    echo
+  done
+
+  for kind in $ckinds
+  do
+    proc="${procedure}${rank}DC${kind}"
+    echo "subroutine ${proc}(x, size, ierr)"
+    echo "  use mpi_kinds"
+    echo "  complex(kind=MPI_REAL${kind}_KIND), dimension(${dim}), intent(in) :: x"
+    echo "  integer, intent(out) :: size"
+    echo "  integer, intent(out) :: ierr"
+    echo "  complex(kind=MPI_REAL${kind}_KIND) :: type"
+    echo "  size = sizeof(type)"
+    echo "  ierr = 0"
+    echo "end subroutine ${proc}"
+    echo
+  done
+  echo
+done
+echo
+echo
+echo
