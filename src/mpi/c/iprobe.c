80184b67c26bb44b7fc63f992e8e3baa3d1d3867
Rainer Keller <rusraink@open-mpi-git-mirror.example.com>
2005-01-24 22:23:15 +0000

    Allow MPI_ANY_TAG to probe for when MPI_PARAM_CHECKing.

--- src/mpi/c/iprobe.c
+++ src/mpi/c/iprobe.c
@@ -46,7 +46,7 @@ int MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status *status
     if ( MPI_PARAM_CHECK ) {
         rc = MPI_SUCCESS;
         OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
-        if (tag < 0 || tag > MPI_TAG_UB_VALUE) {
+        if (((tag < 0) && (tag != MPI_ANY_TAG)) || (tag > MPI_TAG_UB_VALUE)) {
             rc = MPI_ERR_TAG;
         } else if (ompi_comm_invalid(comm)) {
             rc = MPI_ERR_COMM;
