e00782ed7dfbda165f001809cac2d44e59d15029
Brian Barrett <brbarret@open-mpi.org>
2005-03-30 01:31:30 +0000

    * add missing string.h include and copyright header
    This commit was SVN r5098.

--- src/mca/iof/svc/iof_svc_sub.c
+++ src/mca/iof/svc/iof_svc_sub.c
@@ -1,4 +1,23 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
 #include "ompi_config.h"
+
+#include <string.h>
+
 #include "util/output.h"
 #include "mca/oob/oob.h"
 #include "mca/iof/base/iof_base_header.h"
