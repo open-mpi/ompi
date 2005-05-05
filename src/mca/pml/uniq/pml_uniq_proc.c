9b62069ad62be1382eb0b154ac4047fa17bdc621
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2005-05-05 04:17:52 +0000

    More fixes. Remove useless files. Decrease the memory footprint for this PML.

--- src/mca/pml/uniq/pml_uniq_proc.c
+++ src/mca/pml/uniq/pml_uniq_proc.c
@@ -27,12 +27,14 @@ static void mca_pml_uniq_proc_construct(mca_pml_proc_t* proc)
     proc->proc_ompi = NULL;
     proc->proc_ptl_flags = 0;
     OBJ_CONSTRUCT(&proc->proc_lock, ompi_mutex_t);
-#ifdef TOTO
-    OBJ_CONSTRUCT(&proc->proc_ptl_first, mca_pml_uniq_ptl_array_t);
-    OBJ_CONSTRUCT(&proc->proc_ptl_next,  mca_pml_uniq_ptl_array_t);
-#endif  /* TOTO */
-    proc->proc_ptl_first = NULL;
-    proc->proc_ptl_next = NULL;
+
+    proc->proc_ptl_first.ptl_peer = NULL;
+    proc->proc_ptl_first.ptl_base = NULL;
+    proc->proc_ptl_first.ptl = NULL;
+
+    proc->proc_ptl_next.ptl_peer = NULL;
+    proc->proc_ptl_next.ptl_base = NULL;
+    proc->proc_ptl_next.ptl = NULL;
 
     OMPI_THREAD_LOCK(&mca_pml_uniq.uniq_lock);
     ompi_list_append(&mca_pml_uniq.uniq_procs, (ompi_list_item_t*)proc);
@@ -47,10 +49,6 @@ static void mca_pml_uniq_proc_destruct(mca_pml_proc_t* proc)
     OMPI_THREAD_UNLOCK(&mca_pml_uniq.uniq_lock);
 
     OBJ_DESTRUCT(&proc->proc_lock);
-#ifdef TOTO
-    OBJ_DESTRUCT(&proc->proc_ptl_first);
-    OBJ_DESTRUCT(&proc->proc_ptl_next);
-#endif  /* TOTO */
 }
 
 OBJ_CLASS_INSTANCE(
