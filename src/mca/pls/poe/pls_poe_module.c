55538e100d2cdbea5cc374fa60159863e57ceb21
Thara Angskun <angskun@cs.utk.edu>
2005-05-11 09:09:55 +0000

    - another check point - able to launch job (sort of)...but not correctly clean up, mpirun hang, blowup terminal etc.
    This commit was SVN r5685.

--- src/mca/pls/poe/pls_poe_module.c
+++ src/mca/pls/poe/pls_poe_module.c
@@ -31,6 +31,7 @@
 #include "mca/base/mca_base_param.h"
 #include "mca/ns/ns.h"
 #include "mca/rml/rml.h"
+#include "mca/errmgr/errmgr.h"
 
 #include "util/argv.h"
 #include "util/ompi_environ.h"
@@ -56,127 +57,209 @@ orte_pls_base_module_1_0_0_t orte_pls_poe_module = {
     pls_poe_finalize
 };
 
-
-static int pls_poe_launch(orte_jobid_t jobid)
+int __poe_argv_append_int(int *argc, char ***argv, int varname, int min, char *argname) 
 {
-    char **argv;
-    int argc;
-    int rc;
-    int pid;
     char *tmp_string;
-    ompi_list_t map;
-    char* param;
-    char* uri;
-    char **new_env, **environ_copy;
-    orte_vpid_t vpid_start;
-    orte_vpid_t vpid_range;
+    if(varname >= min) {
+        ompi_argv_append(argc, argv, argname);
+        asprintf(&tmp_string, "%d", varname);
+        ompi_argv_append(argc, argv, tmp_string);
+        free(tmp_string);
+    } else {
+        return ORTE_ERR_BAD_PARAM;
+    } 
+    return ORTE_SUCCESS;
+}
+
+int pls_poe_launch_interactive(orte_jobid_t jobid)
+{
+    ompi_list_t nodes;
     ompi_list_item_t* item;
+    size_t num_nodes;
+    orte_vpid_t vpid;
+    int node_name_index1;
+    int node_name_index2;
+    int proc_name_index;
+    char *tmp_string;
+    char *uri, *param;
+    char* name_string;
+    char** argv;
+    int argc;
+    int pid;
+    int rc;
     int i;
-    orte_rmaps_base_map_t* map2;
+    char *hostfile, *cmdfile;
+    FILE *hfp, *cfp;
+                                                                                                       
+    /* query the list of nodes allocated to the job - don't need the entire
+     * mapping - as the daemon/proxy is responsibe for determining the apps
+     * to launch on each node.
+     */
 
-    OBJ_CONSTRUCT(&map, ompi_list_t);
+    if((hostfile=tempnam(NULL,NULL))==NULL) return ORTE_ERR_OUT_OF_RESOURCE;
+    if((cmdfile=tempnam(NULL,NULL))==NULL) return ORTE_ERR_OUT_OF_RESOURCE;
+    if((hfp=fopen(hostfile,"w"))==NULL) return ORTE_ERR_OUT_OF_RESOURCE;
+    if((cfp=fopen(cmdfile,"w"))==NULL) return ORTE_ERR_OUT_OF_RESOURCE;
 
-    rc = orte_rmaps_base_get_map(jobid,&map);
-    if (ORTE_SUCCESS != rc) {
-        /* ORTE_ERROR_LOG(rc); */
+    OBJ_CONSTRUCT(&nodes, ompi_list_t);
+    rc = orte_ras_base_node_query_alloc(&nodes, jobid);
+    if(ORTE_SUCCESS != rc) {
         goto cleanup;
     }
-
-    rc = orte_rmaps_base_get_vpid_range(jobid, &vpid_start, &vpid_range);
-    if (ORTE_SUCCESS != rc) {
-        /* ORTE_ERROR_LOG(rc); */
+                                                                                                       
+    /*
+     * Allocate a range of vpids for the daemons.
+     */
+     
+    num_nodes = ompi_list_get_size(&nodes);
+    if(num_nodes == 0) {
+        return ORTE_ERR_BAD_PARAM;
+    }
+    rc = orte_ns.reserve_range(0, num_nodes, &vpid);
+    if(ORTE_SUCCESS != rc) {
         goto cleanup;
     }
-
-    /* attempt to launch each of the apps */
-    for(item =  ompi_list_get_first(&map);
-        item != ompi_list_get_end(&map);
+     
+    /* need integer value for command line parameter - NOT hex */
+    asprintf(&tmp_string, "%lu", (unsigned long)jobid);
+     
+    /* application */
+    argv = ompi_argv_copy(ompi_argv_split(mca_pls_poe_component.orted, ' '));
+    argc = ompi_argv_count(argv);
+    if (mca_pls_poe_component.debug) {
+         ompi_argv_append(&argc, &argv, "--debug");
+    }
+     
+   
+    ompi_argv_append(&argc, &argv, "--bootproxy");
+    ompi_argv_append(&argc, &argv, tmp_string);
+    ompi_argv_append(&argc, &argv, "--name");
+    proc_name_index = argc;
+    ompi_argv_append(&argc, &argv, "");
+    ompi_argv_append(&argc, &argv, "--nodename");
+    node_name_index2 = argc;
+    ompi_argv_append(&argc, &argv, "");
+                                        
+    /* setup ns contact info */
+    ompi_argv_append(&argc, &argv, "--nsreplica");
+    if(NULL != orte_process_info.ns_replica_uri) {
+        uri = strdup(orte_process_info.ns_replica_uri);
+    } else {
+        uri = orte_rml.get_uri();
+    }
+    asprintf(&param, "\"%s\"", uri);
+    ompi_argv_append(&argc, &argv, param);
+    free(uri);
+                                           
+    /* setup gpr contact info */
+    ompi_argv_append(&argc, &argv, "--gprreplica");
+    if(NULL != orte_process_info.gpr_replica_uri) {
+        uri = strdup(orte_process_info.gpr_replica_uri);
+    } else {
+        uri = orte_rml.get_uri();
+    }
+    asprintf(&param, "\"%s\"", uri);
+    ompi_argv_append(&argc, &argv, param);
+    free(uri);
+     
+    /*
+     * Iterate through each of the nodes and spin
+     * up a daemon.
+     */
+     
+    for(item =  ompi_list_get_first(&nodes);
+        item != ompi_list_get_end(&nodes);
         item =  ompi_list_get_next(item)) {
-        map2 = (orte_rmaps_base_map_t*)item; 
+        orte_ras_base_node_t* node = (orte_ras_base_node_t*)item;
+        orte_process_name_t* name;
+        pid_t pid;
+     
+        /* setup node name */
+        argv[node_name_index2] = node->node_name;
 
+        fprintf(hfp,"%s\n",node->node_name); 
+     
+        /* initialize daemons process name */
+        rc = orte_ns.create_process_name(&name, node->node_cellid, 0, vpid);
+        if(ORTE_SUCCESS != rc) {
+            ORTE_ERROR_LOG(rc);
+            goto cleanup;
+        }
 
-       /* set working directory */
-       if(chdir((map2->app)->cwd) != 0) {
-          /* ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM); */
-       }
-
-       /* setup base environment */
-       environ_copy = ompi_argv_copy(environ);
-       param = mca_base_param_environ_variable("rmgr","bootproxy","jobid");
-       ompi_unsetenv(param, &environ_copy);
-
-       /* setup ns contact info */
-       if(NULL != orte_process_info.ns_replica_uri) {
-          uri = strdup(orte_process_info.ns_replica_uri);
-       } else {
-          uri = orte_rml.get_uri();
-       }
-       param = mca_base_param_environ_variable("ns","replica","uri");
-       ompi_setenv(param, uri, true, &environ_copy);
-       free(param);
-       free(uri);
-
-       /* setup gpr contact info */
-       if(NULL != orte_process_info.gpr_replica_uri) {
-          uri = strdup(orte_process_info.gpr_replica_uri);
-       } else {
-          uri = orte_rml.get_uri();
-       }
-       param = mca_base_param_environ_variable("gpr","replica","uri");
-       ompi_setenv(param, uri, true, &environ_copy);
-       free(param);
-       free(uri);
-
-       /* push name into environment */
-       orte_ns_nds_env_put(map2->procs[0]->proc_name, vpid_start, vpid_range, &environ_copy);
-
-       new_env = ompi_environ_merge(map2->app->env, environ_copy);
-       ompi_argv_free(environ_copy);
-
-       /* POE command */
-       argv = ompi_argv_copy(mca_pls_poe_component.argv);
-       argc = mca_pls_poe_component.argc;
-
-       /* POE argument */
-       ompi_argv_append(&argc, &argv, "-procs");
-       asprintf(&tmp_string, "%d", map2->num_procs);
-       ompi_argv_append(&argc, &argv, tmp_string);
-       free(tmp_string);
-
-       /* Application */
-       ompi_argv_append(&argc, &argv, map2->app->app);
-       i=0;
-       while(map2->app->argv[i]!=NULL) {
-          ompi_argv_append(&argc, &argv, map2->app->argv[i++]);
-       }
-
-       for(i=0;i<argc;i++) printf("%s ", argv[i]); fflush(stdout);
-
-       pid = fork();
-       if(pid < 0) {
-          /* ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE); */
-          return ORTE_ERR_OUT_OF_RESOURCE;
-       }
-
-       if(pid == 0) {
-           execve(mca_pls_poe_component.path, argv, new_env);
-           ompi_output(0, "orte_pls_poe: %s - %s\n", map2->app->app,
-           ompi_argv_join(map2->app->argv, ' '));
-           ompi_output(0, "orte_pls_poe: execv failed with errno=%d\n", errno);
-           exit(-1);
-       } 
-
+        /* setup process name */
+        rc = orte_ns.get_proc_name_string(&name_string, name);
+        if(ORTE_SUCCESS != rc) {
+            ompi_output(0, "orte_pls_poe: unable to create process name");
+            exit(-1);
+        }
+        argv[proc_name_index] = name_string;
+     
+        for(i=0;i<argc;i++) {
+           printf("%s ",argv[i]);
+           fprintf(cfp,"%s ",argv[i]); 
+        }
+        printf("\n"); fflush(stdout); 
+        fprintf(cfp,"\n");
+        vpid++;
+        free(name);
     }
+   
+    fclose(cfp);
+    fclose(hfp);
 
+    argv = ompi_argv_copy(mca_pls_poe_component.argv);
+    argc = mca_pls_poe_component.argc;
+    ompi_argv_append(&argc, &argv, "-hostfile");
+    ompi_argv_append(&argc, &argv, hostfile);
+    ompi_argv_append(&argc, &argv, "-cmdfile");
+    ompi_argv_append(&argc, &argv, cmdfile);
+    ompi_argv_append(&argc, &argv, "-procs");
+    asprintf(&tmp_string, "%d", num_nodes);
+    ompi_argv_append(&argc, &argv, tmp_string);
+    ompi_argv_append(&argc, &argv, "-pgmmodel");
+    ompi_argv_append(&argc, &argv, "mpmd");
+    ompi_argv_append(&argc, &argv, "-resd");
+    ompi_argv_append(&argc, &argv, "no");
+    rc=__poe_argv_append_int(&argc, &argv, mca_pls_poe_component.retry, 0, "-retry");
+    if(ORTE_SUCCESS!=rc) { ORTE_ERROR_LOG(rc); goto cleanup; }
+    rc=__poe_argv_append_int(&argc, &argv, mca_pls_poe_component.retrycount, 0, "-retrycount");
+    if(ORTE_SUCCESS!=rc) { ORTE_ERROR_LOG(rc); goto cleanup; }
+    ompi_output(0, "%s", ompi_argv_join(argv, ' '));
+
+    pid = fork();
+    if(pid < 0) {
+        rc = ORTE_ERR_OUT_OF_RESOURCE;
+        goto cleanup;
+    }
+     
+    /* child */
+    if(pid == 0) {
+       execv(mca_pls_poe_component.path, argv);
+       ompi_output(0, "orte_pls_poe: execv failed with errno=%d\n", errno);
+       exit(-1);
+    }
+     
+             
+     
 cleanup:
-    while(NULL != (item = ompi_list_remove_first(&map))) {
+    while(NULL != (item = ompi_list_remove_first(&nodes))) {
         OBJ_RELEASE(item);
     }
-    OBJ_DESTRUCT(&map);
-    printf("EXIT FROM POE_LAUNCH\n"); fflush(stdout);
+    OBJ_DESTRUCT(&nodes);
     return rc;
 }
 
+
+
+
+
+static int pls_poe_launch(orte_jobid_t jobid)
+{
+    if(!strncmp(mca_pls_poe_component.class,"interactive",11)) {
+        return pls_poe_launch_interactive(jobid);
+    }
+    return ORTE_ERR_NOT_IMPLEMENTED;
+}
 
 static int pls_poe_terminate_job(orte_jobid_t jobid)
 {
