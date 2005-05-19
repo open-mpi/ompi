de82142ab4406c0d2ae3519d26daeb998e7a78e2
Jeff Squyres <jsquyres@cisco.com>
2005-05-19 13:33:14 +0000

    Submitted by: Jeff Squyres Reviewed by: Brian Barrett, Tim Woodall, Ralph Castain
    Reset certain signal handlers after the fork but before the exec when
    launching new processes so that they get all the default unix
    behaviors (i.e., become killable, and don't accidentally have some
    signals blocked -- left over from the event library).
    
    This commit was SVN r5756.

--- src/mca/pls/rsh/pls_rsh_module.c
+++ src/mca/pls/rsh/pls_rsh_module.c
@@ -29,6 +29,7 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <signal.h>
 
 #include "include/orte_constants.h"
 #include "util/argv.h"
@@ -78,6 +79,7 @@ typedef struct rsh_daemon_info_t rsh_daemon_info_t;
 static OBJ_CLASS_INSTANCE(rsh_daemon_info_t,
                           ompi_object_t,
                           NULL, NULL);
+static void set_handler_default(int sig);
 
 /**
  * Callback on daemon exit.
@@ -241,6 +243,7 @@ int orte_pls_rsh_launch(orte_jobid_t jobid)
     char** argv;
     int argc;
     int rc;
+    sigset_t sigs;
 
     /* query the list of nodes allocated to the job - don't need the entire
      * mapping - as the daemon/proxy is responsibe for determining the apps
@@ -367,6 +370,30 @@ int orte_pls_rsh_launch(orte_jobid_t jobid)
                 dup2(fd, 0);
                 close(fd);
             }
+
+            /* Set signal handlers back to the default.  Do this close
+               to the exev() because the event library may (and likely
+               will) reset them.  If we don't do this, the event
+               library may have left some set that, at least on some
+               OS's, don't get reset via fork() or exec().  Hence, the
+               orted could be unkillable (for example). */
+
+            set_handler_default(SIGTERM);
+            set_handler_default(SIGINT);
+            set_handler_default(SIGHUP);
+            set_handler_default(SIGCHLD);
+            set_handler_default(SIGPIPE);
+
+            /* Unblock all signals, for many of the same reasons that
+               we set the default handlers, above.  This is noticable
+               on Linux where the event library blocks SIGTERM, but we
+               don't want that blocked by the orted (or, more
+               specifically, we don't want it to be blocked by the
+               orted and then inherited by the ORTE processes that it
+               forks, making them unkillable by SIGTERM). */
+
+            sigprocmask(0, 0, &sigs);
+            sigprocmask(SIG_UNBLOCK, &sigs, 0);
 
             /* exec the daemon */
             execv(mca_pls_rsh_component.path, argv);
@@ -637,3 +664,14 @@ static int orte_pls_rsh_launch_threaded(orte_jobid_t jobid)
 
 #endif
 
+
+static void set_handler_default(int sig)
+{
+    struct sigaction act;
+
+    act.sa_handler = SIG_DFL;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+
+    sigaction(sig, &act, (struct sigaction *)0);
+}
