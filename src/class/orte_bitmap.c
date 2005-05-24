ab4a742e305b20f6184b363cde0f24fc2218f1cf
Jeff Squyres <jsquyres@cisco.com>
2005-05-24 13:41:26 +0000

    Submitted by: Ralph Castain Reviewed by: Jeff Squyres
    After yesterday's workaround in the fork pls, more conversations
    ensured about this topic.  This commit:
    
    - rolls back the workaround from yesterday
    - fixes a bug in the GPR
    
    Here's Ralph's description of the bug:
    
    -----
    Note that there is a bug currently in the GPR that I need to fix -
    this bug causes the GPR to NOT return data from containers where ALL
    of the specified fields are present, but one or more of the fields has
    multiple copies present. So, in other words, if you ask for keys "foo"
    AND "bar", and the container holds "foo" and two keyvals with key
    "bar", then the GPR will say it fails the AND test.
    -----
    
    Note that this was not the bug that we were seeing (i.e., none of the
    keys that we were searching for were duplicated in the container), but
    Ralph feels that the logic is now much simpler and less complicated,
    and is likely to have fixed whatever the real problem was.
    
    Here's a portion of Ralph's commit message from the trunk:
    
    -----
    1. Fixed the GPR search engine so that keys AND worked, and so that
    multiple objects with the same key didn't mess up the search.
    
    2. Added an orte_bitmap function based on the existing ompi_bitmap
    one, but minus the fortran "pollution"
    ----
    
    This commit was SVN r5843.

---
+++ src/class/orte_bitmap.c
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "orte_config.h"
+
+#include <stdio.h>
+
+#include "include/orte_constants.h"
+#include "mca/errmgr/errmgr.h"
+
+#include "class/orte_bitmap.h"
+
+
+#define SIZE_OF_CHAR (sizeof(char) * 8)
+
+static void orte_bitmap_construct(orte_bitmap_t *bm);
+static void orte_bitmap_destruct(orte_bitmap_t *bm);
+
+OBJ_CLASS_INSTANCE(orte_bitmap_t, ompi_object_t, 
+                   orte_bitmap_construct, orte_bitmap_destruct);
+
+
+static void 
+orte_bitmap_construct(orte_bitmap_t *bm) 
+{
+    bm->legal_numbits = 0;
+    bm->array_size = 0;
+    bm->bitmap = NULL;
+}
+
+
+static void
+orte_bitmap_destruct(orte_bitmap_t *bm)
+{
+    if (NULL != bm->bitmap) {
+        free(bm->bitmap);
+    }
+}
+
+
+int
+orte_bitmap_init(orte_bitmap_t *bm, size_t size)
+{
+    size_t actual_size;
+
+    if (NULL == bm) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+	    return ORTE_ERR_BAD_PARAM;
+    }
+
+    bm->legal_numbits = size;
+    actual_size = size / SIZE_OF_CHAR;
+  
+    actual_size += (size % SIZE_OF_CHAR == 0) ? 0 : 1;
+    bm->bitmap = (unsigned char *) malloc(actual_size);
+    if (NULL == bm->bitmap) {
+        ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE);
+	    return ORTE_ERR_OUT_OF_RESOURCE;
+    }
+
+    bm->array_size = actual_size;
+    orte_bitmap_clear_all_bits(bm);
+    return ORTE_SUCCESS;
+}
+  
+
+int
+orte_bitmap_set_bit(orte_bitmap_t *bm, size_t bit)
+{
+    size_t index, offset, new_size, i;
+
+    if (NULL == bm) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+
+    index = bit / SIZE_OF_CHAR; 
+    offset = bit % SIZE_OF_CHAR;
+
+    if (index >= bm->array_size) {
+
+        	/* We need to allocate more space for the bitmap, since we are
+        	   out of range. We dont throw any error here, because this is
+        	   valid and we simply expand the bitmap */
+        
+        	new_size = (index / bm->array_size + 1 ) * bm->array_size;
+
+        	/* New size is just a multiple of the original size to fit in
+        	   the index. */
+        
+        	bm->bitmap = (unsigned char *) realloc(bm->bitmap, new_size);
+        	if (NULL == bm->bitmap) {
+            ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE);
+        	    return ORTE_ERR_OUT_OF_RESOURCE;
+        	}
+        
+        	/* zero out the new elements */
+        	for (i = bm->array_size; i < new_size; ++i) {
+        	    bm->bitmap[i] = 0;
+        	}
+        
+        	/* Update the array_size */
+        	bm->array_size = new_size;
+        	bm->legal_numbits = bit + 1;
+    }
+    
+    /* Now set the bit */
+    bm->bitmap[index] |= (1 << offset);
+
+    return ORTE_SUCCESS;
+}
+
+
+int
+orte_bitmap_clear_bit(orte_bitmap_t *bm, size_t bit)
+{
+    size_t index, offset;
+
+    if ((bit > bm->legal_numbits - 1) || (NULL == bm)) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+	    return ORTE_ERR_BAD_PARAM;
+    }
+
+    index = bit / SIZE_OF_CHAR; 
+    offset = bit % SIZE_OF_CHAR;
+  
+    if (index >= bm->array_size) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+
+    bm->bitmap[index] &= ~(1 << offset);
+    return ORTE_SUCCESS;
+}
+
+
+int
+orte_bitmap_is_set_bit(orte_bitmap_t *bm, size_t bit)
+{
+    size_t index, offset;
+  
+    if ((bit > bm->legal_numbits - 1) || (NULL == bm)) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+
+    index = bit / SIZE_OF_CHAR; 
+    offset = bit % SIZE_OF_CHAR;
+  
+    if (index >= bm->array_size) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+  
+    if (0 != (bm->bitmap[index] & (1 << offset))) {
+        return (int) true;
+    }
+
+    return (int) false;
+}
+
+
+int
+orte_bitmap_clear_all_bits(orte_bitmap_t *bm)
+{
+    if (NULL == bm) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+
+    memset(bm->bitmap, 0, bm->array_size);
+    return ORTE_SUCCESS;
+}
+
+
+int
+orte_bitmap_set_all_bits(orte_bitmap_t *bm)
+{
+    size_t i;
+    
+    if (NULL == bm) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+     
+    for (i = 0; i < bm->array_size; ++i) {
+        bm->bitmap[i] = ~((char) 0);
+    }
+    return ORTE_SUCCESS;
+}
+
+
+int
+orte_bitmap_find_and_set_first_unset_bit(orte_bitmap_t *bm, size_t *position)
+{
+    size_t i = 0;
+    unsigned char temp;
+    unsigned char all_ones = 0xff;
+
+    if (NULL == bm) {
+        ORTE_ERROR_LOG(ORTE_ERR_BAD_PARAM);
+        return ORTE_ERR_BAD_PARAM;
+    }
+
+    /* Neglect all which dont have an unset bit */
+    *position = 0;
+    while((i < bm->array_size) && (bm->bitmap[i] == all_ones)) {
+        ++i;
+    }
+
+    if (i == bm->array_size) {
+        /* increase the bitmap size then */
+        *position = bm->array_size * SIZE_OF_CHAR;
+        return orte_bitmap_set_bit(bm, *position);
+    }
+
+    /* This one has an unset bit, find its bit number */
+
+    temp = bm->bitmap[i];
+    while (temp & 0x1) {
+        ++(*position);
+        temp >>= 1;
+    }
+      
+    /* Now set the bit number */
+    bm->bitmap[i] |= (bm->bitmap[i] + 1);
+
+    (*position) += i * SIZE_OF_CHAR;
+    return ORTE_SUCCESS;
+}
