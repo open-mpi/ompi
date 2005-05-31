5ccaaf55e28ba50c9bef2a09abd4731a085694c0
Galen Shipman <gshipman@lanl.gov>
2005-05-31 17:08:41 +0000

    Initial checkin of VAPI allocator.
    This commit was SVN r5896.

---
+++ src/mca/mpool/vapi/mpool_vapi.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+/**
+ * @file
+ */
+#ifndef MCA_MPOOL_VAPI_H
+#define MCA_MPOOL_VAPI_H
+
+#include "class/ompi_list.h"
+#include "class/ompi_free_list.h"
+#include "event/event.h"
+#include "mca/mpool/mpool.h"
+#include "mca/allocator/allocator.h"
+#include "mca/common/vapi/vapi_mem_reg.h" 
+
+#if defined(c_plusplus) || defined(__cplusplus)
+extern "C" {
+#endif
+
+struct mca_mpool_vapi_component_t {
+  mca_mpool_base_component_t super;
+  char*  vapi_allocator_name;
+};
+
+typedef struct mca_mpool_vapi_component_t mca_mpool_vapi_component_t;
+
+OMPI_COMP_EXPORT extern mca_mpool_vapi_component_t mca_mpool_vapi_component;
+
+struct mca_mpool_vapi_module_t {
+  mca_mpool_base_module_t super;
+  mca_allocator_base_module_t * vapi_allocator; 
+  mca_common_vapi_mem_reg_t mem_reg;
+  mca_common_vapi_memhandle_t mem_hndl; 
+}; typedef struct mca_mpool_vapi_module_t mca_mpool_vapi_module_t; 
+
+/* 
+ *  Initializes the mpool module. 
+ */ 
+void mca_mpool_vapi_module_init(mca_mpool_vapi_module_t* mpool); 
+
+
+/*
+ *  Returns base address of shared memory mapping.
+ */
+void* mca_mpool_vapi_base(mca_mpool_base_module_t*);
+
+/**
+  *  Allocate block of shared memory.
+  */
+void* mca_mpool_vapi_alloc( mca_mpool_base_module_t* mpool, size_t size, size_t align, void** user_out);
+
+/**
+  * realloc function typedef
+  */
+void* mca_mpool_vapi_realloc(mca_mpool_base_module_t* mpool, void* addr, size_t size, void** user_out);
+
+/**
+  * free function typedef
+  */
+void mca_mpool_vapi_free(mca_mpool_base_module_t* mpool, void *);
+
+
+#if defined(c_plusplus) || defined(__cplusplus)
+}
+#endif
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/mca/mpool/vapi/mpool_vapi_module.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+#include <string.h>
+#include "util/output.h"
+#include "mca/mpool/vapi/mpool_vapi.h"
+#include "mca/common/vapi/common/vapi_mem_reg.h"
+
+
+/* 
+ *  Initializes the mpool module.
+ */ 
+void mca_mpool_vapi_module_init(mca_mpool_vapi_module_t* mpool)
+{
+  mpool->super.mpool_component = &mca_mpool_vapi_component.super; 
+  mpool->super.mpool_base = NULL; /* no base .. */ 
+  mpool->super.mpool_alloc = mca_mpool_vapi_alloc; 
+  mpool->super.mpool_realloc = mca_mpool_vapi_realloc; 
+  mpool->super.mpool_free = mca_mpool_vapi_free; 
+  mpool->super.mpool_register = NULL; 
+  mpool->super.mpool_deregister = NULL; 
+  mpool->super.mpool_finalize = NULL; 
+}
+
+
+/**
+  * allocate function 
+  */
+void* mca_mpool_vapi_alloc(mca_mpool_base_module_t* mpool, size_t size, size_t align, void** user_out)
+{
+  mca_mpool_vapi_module_t* mpool_vapi = (mca_mpool_vapi_module_t*)mpool; 
+  return mpool_vapi->vapi_allocator->alc_alloc(mpool_vapi->vapi_allocator, size, align);
+}
+
+/**
+  * realloc function 
+  */
+void* mca_mpool_vapi_realloc(mca_mpool_base_module_t* mpool, void* addr, size_t size, void** user_out)
+{
+  mca_mpool_vapi_module_t* mpool_vapi = (mca_mpool_vapi_module_t*)mpool; 
+  return mpool_vapi->vapi_allocator->alc_realloc(mpool_vapi->vapi_allocator, addr, size);
+}
+
+/**
+  * free function 
+  */
+void mca_mpool_vapi_free(mca_mpool_base_module_t* mpool, void * addr)
+{
+  mca_mpool_vapi_module_t* mpool_vapi = (mca_mpool_vapi_module_t*)mpool; 
+  mpool_vapi->vapi_allocator->alc_free(mpool_vapi->vapi_allocator, addr);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
