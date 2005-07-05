465e206216d789707d403d7d50488f95d29c02a3
Brian Barrett <brbarret@open-mpi.org>
2005-07-05 22:15:35 +0000

    * fill in some more of the chunk mamagement code (chunks == array of memory   used for the rolling recv buffers for non-RDMA messages)
    This commit was SVN r6355.

--- ompi/mca/btl/portals/src/btl_portals_recv.c
+++ ompi/mca/btl/portals/src/btl_portals_recv.c
@@ -44,3 +44,58 @@ mca_btl_portals_process_recv(mca_btl_portals_module_t *module,
 {
     return OMPI_SUCCESS;
 }
+
+
+mca_btl_portals_recv_chunk_t* 
+mca_btl_portals_recv_chunk_init(mca_btl_portals_module_t *module)
+{
+    mca_btl_portals_recv_chunk_t *chunk;
+
+    chunk = OBJ_NEW(mca_btl_portals_recv_chunk_t);
+    chunk->btl = module;
+    chunk->length = module->portals_recv_mds_size;
+    chunk->start = malloc(chunk->length);
+    if (chunk->start == NULL) return NULL;
+
+    chunk->me_h = PTL_INVALID_HANDLE;
+    chunk->md_h = PTL_INVALID_HANDLE;
+
+    chunk->full = false;
+    chunk->pending = 0;
+
+    return chunk;
+}
+
+
+int
+mca_btl_portals_recv_chunk_free(mca_btl_portals_recv_chunk_t *chunk)
+{
+
+    if (PTL_INVALID_HANDLE != chunk->me_h) {
+        PtlMEUnlink(chunk->me_h);
+        chunk->me_h = PTL_INVALID_HANDLE;
+    }
+
+    while (chunk->pending != 0) {
+        mca_btl_portals_component_progress();
+    }
+
+    if (PTL_INVALID_HANDLE != chunk->md_h) {
+        PtlMDUnlink(chunk->md_h);
+        chunk->md_h = PTL_INVALID_HANDLE;
+    }
+
+    if (NULL != chunk->start) {
+        free(chunk->start);
+        chunk->start = NULL;
+    }
+    chunk->length = 0;
+    chunk->full = false;
+
+    return OMPI_SUCCESS;
+}
+
+
+OBJ_CLASS_INSTANCE(mca_btl_portals_recv_chunk_t,
+                   opal_list_item_t,
+                   NULL, NULL);
