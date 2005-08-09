bd4a0c4ee0bed909b7beed885e8f4d93e7efc5da
Brian Barrett <brbarret@open-mpi.org>
2005-08-09 19:32:49 +0000

    * start memory manager code.  Not activated yet, since configure changes   aren't included yet
    This commit was SVN r6784.

---
+++ opal/memory/memory_internal.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#ifndef OPAL_MEMORY_MEMORY_INTERNAL_H
+#define OPAL_MEMORY_MEMORY_INTERNAL_H
+
+void opal_mem_free_release_hook(void *buf, size_t length);
+
+#endif /* OPAL_MEMORY_MEMORY_INTERNAL_H */
