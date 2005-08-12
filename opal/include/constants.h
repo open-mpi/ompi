dc9b04fc209505a100a0590f8d900ef206105403
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-08-12 18:54:09 +0000

    Starting to use this...

---
+++ opal/include/constants.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#ifndef OPAL_CONSTANTS_H
+#define OPAL_CONSTANTS_H
+
+/* error codes */
+
+#define OPAL_SUCCESS 0
+#define OPAL_ERROR -1
+#define OPAL_ERR_OUT_OF_RESOURCE -2
+#define OPAL_ERR_IN_ERRNO -3
+#define OPAL_ERR_NOT_FOUND -4
+
+#endif /* OPAL_CONSTANTS_H */
+
