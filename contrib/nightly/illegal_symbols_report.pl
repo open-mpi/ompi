78cba1f70fa65cea76511da54497be7bc8c1b6ff
Jeff Squyres <jsquyres@cisco.com>
2005-08-14 11:49:37 +0000

    Update the illegal symbol report to allow opal_ prefixes
    This commit was SVN r6869.

--- contrib/nightly/illegal_symbols_report.pl
+++ contrib/nightly/illegal_symbols_report.pl
@@ -33,8 +33,8 @@ my @comp_suffixes = (".so");
 
 # Acceptable public symbol prefixes
 
-my @lib_acceptable = ("ompi_" , "mpi_", "MPI_", "OMPI_", "MPI::", "PMPI_", "PMPI::", "mca_", "lt_", "orte_", "epoll");
-my @comp_acceptable = ("mca_", "orte_");
+my @lib_acceptable = ("ompi_" , "mpi_", "MPI_", "OMPI_", "MPI::", "PMPI_", "PMPI::", "mca_", "lt_", "orte_", "epoll", "opal_");
+my @comp_acceptable = ("mca_", "orte_", "opal_");
 
 # Subject line for e-mail
 
