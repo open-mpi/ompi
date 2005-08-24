e3dc9313efe160ef2de1d3c0a11e2309ece134f0
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-08-24 19:15:11 +0000

    First cut of the SLURM ras.  Seems to be working!  Now need to write SLURM pls...

---
+++ orte/mca/ras/slurm/ras_slurm_component.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "orte_config.h"
+
+#include "opal/mca/base/base.h"
+#include "opal/mca/base/mca_base_param.h"
+#include "orte/include/orte_constants.h"
+#include "ras_slurm.h"
+
+
+/*
+ * Local functions
+ */
+static orte_ras_base_module_t *ras_slurm_init(int*);
+
+
+orte_ras_base_component_1_0_0_t mca_ras_slurm_component = {
+    /* First, the mca_base_component_t struct containing meta
+       information about the component itself */
+
+    {
+        /* Indicate that we are a iof v1.0.0 component (which also
+           implies a specific MCA version) */
+        
+        ORTE_RAS_BASE_VERSION_1_0_0,
+        
+        /* Component name and version */
+        
+        "slurm",
+        ORTE_MAJOR_VERSION,
+        ORTE_MINOR_VERSION,
+        ORTE_RELEASE_VERSION,
+        
+        /* Component open and close functions */
+        
+        NULL,
+        NULL
+    },
+    
+    /* Next the MCA v1.0.0 component meta data */
+    {
+        /* Whether the component is checkpointable or not */
+        false
+    },
+    
+    ras_slurm_init
+};
+
+
+static orte_ras_base_module_t *ras_slurm_init(int* priority)
+{
+    /* Are we running under a SLURM job? */
+
+    mca_base_param_reg_int(&mca_ras_slurm_component.ras_version, "priority",
+                           "Priority of the slurm ras component",
+                           false, false, 100, priority);
+
+    if (NULL != getenv("SLURM_JOBID")) {
+        opal_output(orte_ras_base.ras_output,
+                    "ras:slurm: available for selection");
+        return &orte_ras_slurm_module;
+    }
+
+    /* Sadly, no */
+
+    opal_output(orte_ras_base.ras_output,
+                "ras:slurm: NOT available for selection");
+    return NULL;
+}
