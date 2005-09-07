4b7023498088a7136cfc80daa4d6d08aacd1c01f
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-09-07 13:33:43 +0000

    simplify the bcast code by taking abstract actions and making them macros -- will help with the other algorithms

--- ompi/mca/coll/sm/coll_sm_bcast.c
+++ ompi/mca/coll/sm/coll_sm_bcast.c
@@ -46,17 +46,15 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
                             struct ompi_communicator_t *comm)
 {
     struct iovec iov;
-    uint32_t iov_size = 1;
     mca_coll_base_comm_t *data = comm->c_coll_selected_data;
-    int i, ret, rank, size, num_children;
+    int i, ret, rank, size, num_children, src_rank;
     int flag_num, segment_num, max_segment_num;
-    int parent_rank, child_rank;
+    int parent_rank;
     size_t total_size, max_data, bytes;
-    volatile uint32_t *my_control;
     mca_coll_sm_in_use_flag_t *flag;
     ompi_convertor_t convertor;
     mca_coll_sm_tree_node_t *me, *parent, **children;
-    int32_t bogus_free_after = 0;
+    mca_coll_base_mpool_index_t *index;
 
     /* Setup some identities */
 
@@ -106,27 +104,9 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
             flag_num = (data->mcb_operation_count++ % 
                         mca_coll_sm_component.sm_comm_num_in_use_flags);
 
-            /* Wait for the set of segments to become available */
-            flag = (mca_coll_sm_in_use_flag_t*)
-                (((char *) data->mcb_in_use_flags) +
-                 (flag_num * mca_coll_sm_component.sm_control_size));
-            D(("root waiting for in_use flag %d (value %d), %p\n", 
-               flag_num, flag->mcsiuf_num_procs_using, flag));
-            while (0 != flag->mcsiuf_num_procs_using) {
-                continue;
-            }
-            D(("root got in_use flag %d (value %d), %p\n", 
-               flag_num, flag->mcsiuf_num_procs_using, flag));
-
-            /* Now that the set of segments is availble, mark it as
-               used.  No need to include the root in the count (we'd
-               only have to decrement it later).  Don't need a write
-               barrier here -- we have another later that will
-               guarantee that the write has completed, if
-               necessary. */
-
-            flag->mcsiuf_num_procs_using = size - 1;
-            flag->mcsiuf_operation_count = data->mcb_operation_count - 1;
+            FLAG_SETUP(flag_num, flag, data);
+            FLAG_WAIT_FOR_IDLE(flag);
+            FLAG_RETAIN(flag, size - 1, data->mcb_operation_count - 1);
 
             /* Loop over all the segments in this set */
             
@@ -135,40 +115,19 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
             max_segment_num = (flag_num + 1) * 
                 mca_coll_sm_component.sm_comm_num_in_use_flags; 
             do {
+                index = &(data->mcb_mpool_index[segment_num]);
 
                 /* Copy the fragment from the user buffer to my fragment
                    in the current segment */
-                iov.iov_base = 
-                    data->mcb_mpool_index[segment_num].mcbmi_data +
-                    (rank * mca_coll_sm_component.sm_fragment_size);
-                max_data = iov.iov_len;
-                D(("root copying %lu bytes to data fan out, seg %d: %p\n",
-                   (unsigned long) iov.iov_len, segment_num, iov.iov_base));
-                ompi_convertor_pack(&convertor, &iov, &iov_size,
-                                    &max_data, &bogus_free_after);
+                COPY_FRAGMENT_IN(convertor, index, iov, max_data);
                 bytes += max_data;
                 
                 /* Wait for the write to absolutely complete */
                 opal_atomic_wmb();
                 
-                /* Tell my children that this fragment is ready (be
-                   sure to normalize the child's ID based on the shift
-                   we did above to calculate the "me" node in the
-                   tree) */
-                for (i = 0; i < num_children; ++i) {
-                    child_rank = (children[i]->mcstn_id + root) % size;
-                    *((size_t*) 
-                      (((char*) 
-                        data->mcb_mpool_index[segment_num].mcbmi_control) +
-                       (mca_coll_sm_component.sm_control_size * 
-                        child_rank))) = max_data;
-                    D(("root sent notice to child %d (vrank %d), control to %p\n",
-                       i, children[i]->mcstn_id,
-                       (((char*) 
-                         data->mcb_mpool_index[segment_num].mcbmi_control) +
-                        (mca_coll_sm_component.sm_control_size * 
-                         child_rank))));
-                }
+                /* Tell my children that this fragment is ready */
+                PARENT_NOTIFY_CHILDREN(children, num_children, index,
+                                       max_data);
 
                 ++segment_num;
             } while (bytes < total_size && segment_num < max_segment_num);
@@ -207,13 +166,8 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
 
             /* Wait for the root to mark this set of segments as
                ours */
-            flag = (mca_coll_sm_in_use_flag_t*)
-                (((char *) data->mcb_in_use_flags) +
-                 (flag_num * mca_coll_sm_component.sm_control_size));
-            D(("rank %d waiting for root to claim in-use flag %d, %p (op count %d)\n", rank, flag_num, flag, data->mcb_operation_count));
-            while (data->mcb_operation_count != flag->mcsiuf_operation_count) {
-                continue;
-            }
+            FLAG_SETUP(flag_num, flag, data);
+            FLAG_WAIT_FOR_OP(flag, data->mcb_operation_count);
             ++data->mcb_operation_count;
 
             /* Loop over all the segments in this set */
@@ -224,71 +178,32 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
                 mca_coll_sm_component.sm_comm_num_in_use_flags; 
             do {
 
-                /* Pre-calculate some pointers */
-                
+                /* Pre-calculate some values */
                 parent_rank = (parent->mcstn_id + root) % size;
-                D(("rank %d parent rank is %d\n", rank, parent_rank));
-                my_control = (uint32_t *)
-                    (((char*) 
-                      data->mcb_mpool_index[segment_num].mcbmi_control) +
-                     (rank * mca_coll_sm_component.sm_control_size));
+                index = &(data->mcb_mpool_index[segment_num]);
 
-                /* Wait for the fragment: the parent will mark the segment
-                   as ready */
-                D(("rank %d waiting for fragment in segment %d (control %p)\n",
-                   rank, segment_num, (char*) my_control));
-                while (0 == *my_control) {
-                    continue;
-                }
-                max_data = *my_control;
-                D(("rank %d: fragment ready in segment %d\n", rank, segment_num));
+                /* Wait for my parent to tell me that the segment is ready */
+                CHILD_WAIT_FOR_NOTIFY(rank, index, max_data);
                 
                 /* If I have children, send the data to them */
                 if (num_children > 0) {
-                    /* No need to wait for the segment to become
-                       available -- the root has already claimed it
-                       and we're all already using it.  So copy the
-                       fragment from the parent's portion in the
-                       segment to my portion in the segment.  This is
-                       a simply memcpy because it's already been
-                       packed into the parent's segment. */
-                    memcpy(/* my data fan out section in the segment */
-                           (data->mcb_mpool_index[segment_num].mcbmi_data +
-                            (rank * mca_coll_sm_component.sm_fragment_size)),
-                           /* parent's fan out section in the segment */
-                           (data->mcb_mpool_index[segment_num].mcbmi_data +
-                            (parent_rank *
-                             mca_coll_sm_component.sm_fragment_size)),
-                           /* length */
-                           *my_control);
-                    D(("rank %d memcopy'ed fragment (%p) to my data fan out (%lu bytes)\n", rank, 
-                       (data->mcb_mpool_index[segment_num].mcbmi_data +
-                        (parent_rank * mca_coll_sm_component.sm_fragment_size)),
-                       (unsigned long) *my_control));
+                    /* Copy the fragment from the parent's portion in
+                       the segment to my portion in the segment. */
+                    COPY_FRAGMENT_BETWEEN(parent_rank, rank, index, max_data);
                     
                     /* Wait for the write to absolutely complete */
                     opal_atomic_wmb();
                     
                     /* Tell my children that this fragment is ready */
-                    for (i = 0; i < num_children; ++i) {
-                        child_rank = (children[i]->mcstn_id + root) % size;
-                        *((size_t*) 
-                          (((char*) 
-                            data->mcb_mpool_index[segment_num].mcbmi_control) +
-                           (mca_coll_sm_component.sm_control_size * 
-                            child_rank))) = *my_control;
-                        D(("rank %d notifying child %d (vrank %d, rank %d)\n",
-                           rank, i, children[i]->mcstn_id, child_rank));
-                    }
+                    PARENT_NOTIFY_CHILDREN(children, num_children, index,
+                                           max_data);
                     
                     /* Set the "copy from buffer" to be my local
                        segment buffer so that we don't potentially
                        incur a non-local memory copy from the parent's
                        fan out data segment [again] when copying to
                        the user's buffer */
-                    iov.iov_base =
-                        data->mcb_mpool_index[segment_num].mcbmi_data +
-                        (rank * mca_coll_sm_component.sm_fragment_size);
+                    src_rank = rank;
                 }
                 
                 /* If I don't have any children, set the "copy from
@@ -296,20 +211,12 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
                    directly from my parent */
                 
                 else {
-                    iov.iov_base =
-                        (((char*) 
-                          data->mcb_mpool_index[segment_num].mcbmi_data) +
-                         (parent_rank * 
-                          mca_coll_sm_component.sm_fragment_size));
+                    src_rank = parent_rank;
                 }
                 
                 /* Copy to my output buffer */
-                D(("rank %d convertor copied from parent data %p to user buffer (%lu bytes)\n",
-                   rank, iov.iov_base, (unsigned long) iov.iov_len));
-                ompi_convertor_unpack(&convertor, &iov, &iov_size,
-                                      &max_data, &bogus_free_after);
+                COPY_FRAGMENT_OUT(convertor, src_rank, index, iov, max_data);
 
-                *my_control = 0;
                 bytes += max_data;
                 ++segment_num;
             } while (bytes < total_size && segment_num < max_segment_num);
@@ -319,10 +226,7 @@ int mca_coll_sm_bcast_intra(void *buff, int count,
             opal_atomic_wmb();
 
             /* We're finished with this set of segments */
-
-            D(("rank %d done with in-use flag %d (value %d)\n",
-               rank, flag_num, flag->mcsiuf_num_procs_using));
-            opal_atomic_add(&flag->mcsiuf_num_procs_using, -1);
+            FLAG_RELEASE(flag);
         } while (bytes < total_size);
     }
 
