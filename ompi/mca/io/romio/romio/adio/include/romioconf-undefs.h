c6e0a51267041f7dfeafa64cbd6b86f90af86b7a
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-09-09 10:33:04 +0000

    Prior fix for the ROMIO re-#define problem didn't really fix the problem because Autoconf replaced the "#undef ..." with "#define ...".  Fix this by not putting the "#undef ..." statement directly in romioconf.h[.in] -- but rather having romioconf.h[.in] #include romioconf-undefs.h, which has the #undef statements.

---
+++ ompi/mca/io/romio/romio/adio/include/romioconf-undefs.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                          All rights reserved.
+ *  Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                          All rights reserved.
+ *  Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                          University of Stuttgart.  All rights reserved.
+ *  Copyright (c) 2004-2005 The Regents of the University of California.
+ *                          All rights reserved.
+ *  $COPYRIGHT$
+ *  
+ *  Additional copyrights may follow
+ *  
+ *  $HEADER$
+ */
+
+#ifndef ROMIO_CONF_UNDEFS_H
+#define ROMIO_CONF_UNDEFS_H
+
+/* Need to add some undefs here so that we don't conflict with the
+ * main ompi_config.h.  Arrgh.  Stupid autoconf not giving us the option
+ * to not define these macros... grumble... 
+ */
+#if defined(PACKAGE_BUGREPORT)
+#undef PACKAGE_BUGREPORT
+#endif
+#if defined(PACKAGE_NAME)
+#undef PACKAGE_NAME
+#endif
+#if defined(PACKAGE_STRING)
+#undef PACKAGE_STRING
+#endif
+#if defined(PACKAGE_TARNAME)
+#undef PACKAGE_TARNAME
+#endif
+#if defined(PACKAGE_VERSION)
+#undef PACKAGE_VERSION
+#endif
+
+#endif /* ROMIOCONF_UNDEFS_H */
