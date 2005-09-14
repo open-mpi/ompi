8b146cec13dc7f837367b97ff26602e6195e244f
Galen Shipman <gshipman@lanl.gov>
2005-09-14 13:27:09 +0000

    Callback shouldn't be static as it is used in more than one .c file.
    This commit was SVN r7369.

--- ompi/mca/mpool/base/mpool_base_mem_cb.h
+++ ompi/mca/mpool/base/mpool_base_mem_cb.h
@@ -35,47 +35,7 @@ extern "C" {
 /*
  *  memory hook callback, called when memory is free'd out from under us
  */
-static void mca_mpool_base_mem_cb(void* base, size_t size, void* cbdata){ 
-    uint32_t i, cnt;
-    ompi_pointer_array_t regs;
-    mca_mpool_base_registration_t* reg;
-    mca_mpool_base_selected_module_t* current;
-    int rc;
-    opal_list_item_t* item;
-
-    for(item = opal_list_get_first(&mca_mpool_base_modules);
-        item != opal_list_get_end(&mca_mpool_base_modules);
-        item = opal_list_get_next(item)) {
-        
-        current = (mca_mpool_base_selected_module_t*) item; 
-        if(NULL != current->mpool_module->mpool_find) { 
-            rc = current->mpool_module->mpool_find(
-                                                   current->mpool_module, 
-                                                   base,
-                                                   size,
-                                                   &regs, 
-                                                   &cnt 
-                                                   ); 
-            if(OMPI_SUCCESS != rc) { 
-                return;
-            }
-            if(0 < cnt) { 
-                for(i = 0; i < cnt; i++) { 
-                    reg = (mca_mpool_base_registration_t*) 
-                        ompi_pointer_array_get_item(&regs, i);
-                    
-                    rc = current->mpool_module->mpool_deregister(
-                                                                 current->mpool_module, 
-                                                                 reg
-                                                                 ); 
-                    if(OMPI_SUCCESS != rc) { 
-                        return; 
-                    }
-                }
-            }
-        }    
-    }    
-}
+    void mca_mpool_base_mem_cb(void* base, size_t size, void* cbdata);
 
 #if defined(c_plusplus) || defined(__cplusplus)
 }
