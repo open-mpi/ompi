9611ba19c3cd24317e736ce1b6bf7a7c63db1b1c
Andrew Friedley <afriedle@open-mpi-git-mirror.example.com>
2005-10-21 21:06:20 +0000

    Complete George's fix - this is a problem I caused :(
    This takes care of Troy's first segfault problem, and compile errors that will likely happen as soon as Ken applies George's patch and runs make again.

--- orte/runtime/orte_init_stage1.c
+++ orte/runtime/orte_init_stage1.c
@@ -537,7 +537,7 @@ error:
     if (ret != ORTE_SUCCESS) {
         opal_show_help("help-orte-runtime",
                        "orte_init:startup:internal-failure",
-                       error, ret);
+                       true, error, ret);
     }
 
     return ret;
