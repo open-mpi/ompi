26003bc952399b95b8c995d51be77ee23cb33d5d
Tim Woodall <twoodall@lanl.gov>
2005-11-03 20:52:56 +0000

    fix from release branch - don't use get protocol if more than one btl is available
    This commit was SVN r7984.

--- ompi/mca/pml/ob1/pml_ob1_sendreq.c
+++ ompi/mca/pml/ob1/pml_ob1_sendreq.c
@@ -392,7 +392,7 @@ int mca_pml_ob1_send_request_start_copy(
     int rc;
 
     /* allocate descriptor */
-    MCA_PML_OB1_DES_ALLOC(bml_btl, descriptor, sizeof(mca_pml_ob1_match_hdr_t) + size);
+    mca_bml_base_alloc(bml_btl, &descriptor, sizeof(mca_pml_ob1_match_hdr_t) + size);
     if(NULL == descriptor) {
         return OMPI_ERR_OUT_OF_RESOURCE;
     } 
@@ -524,8 +524,10 @@ int mca_pml_ob1_send_request_start_rdma(
     size_t i;
     int rc;
 
+
     bml_btl = sendreq->req_rdma[0].bml_btl;
-    if(bml_btl->btl_flags & MCA_BTL_FLAGS_GET) {
+    if(sendreq->req_rdma_cnt == 1 &&
+       bml_btl->btl_flags & MCA_BTL_FLAGS_GET) {
 
          /* prepare source descriptor/segment(s) */
          size = sendreq->req_send.req_bytes_packed;
