020079e3068713e7160ffcbd943b061fd5d92bd0
Jeff Squyres <jsquyres@cisco.com>
2005-11-05 20:27:07 +0000

    Update IU and UTK copyrights.
    No review necessary.
    
    This commit was SVN r8001.

--- ompi/mca/pml/example/pml_example_cancel.c
+++ ompi/mca/pml/example/pml_example_cancel.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
- *                         All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
