499d33fd73347c3c91a7d3ffd087b69d9e7f523e
Jeff Squyres <jsquyres@cisco.com>
2005-11-07 22:46:41 +0000

    Submitted by: George Bosilca, Jeff Squyres Reviewed by: Jeff Squyres, Brian Barrett
    r8016:
    The OMPI_GENERATE_F77_BINDINGS work only for the most common F77
    bindings, the one that does not return any value. There are 2
    exceptions MPI_Wtick and MPI_Wtime.  For these 2 we can insert the
    bindings manually.
    
    r8017:
    Remove extrameous comments
    
    This commit was SVN r8018.
    
    The following SVN revision numbers were found above:
      r8016 --> open-mpi/ompi@9832d5d883287485aef0742ae736e9fe6abe78bf
      r8017 --> open-mpi/ompi@a1ba3168d9d0a5e0e874fa5b8ef6195bcc65be69

--- ompi/mpi/f77/wtick_f.c
+++ ompi/mpi/f77/wtick_f.c
@@ -20,19 +20,20 @@
 
 #include "mpi/f77/bindings.h"
 
+/* The OMPI_GENERATE_F77_BINDINGS work only for the most common F77 bindings, the
+ * one that does not return any value. There are 2 exceptions MPI_Wtick and MPI_Wtime.
+ * For these 2 we can insert the bindings manually.
+ */
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
 #pragma weak PMPI_WTICK = mpi_wtick_f
 #pragma weak pmpi_wtick = mpi_wtick_f
 #pragma weak pmpi_wtick_ = mpi_wtick_f
 #pragma weak pmpi_wtick__ = mpi_wtick_f
 #elif OMPI_PROFILE_LAYER
-OMPI_GENERATE_F77_BINDINGS (PMPI_WTICK,
-                           pmpi_wtick,
-                           pmpi_wtick_,
-                           pmpi_wtick__,
-                           pmpi_wtick_f,
-                           (),
-                           () )
+double PMPI_WTICK(void) { return pmpi_wtick_f(); }
+double pmpi_wtick(void) { return pmpi_wtick_f(); }
+double pmpi_wtick_(void) { return pmpi_wtick_f(); }
+double pmpi_wtick__(void) { return pmpi_wtick_f(); }
 #endif
 
 #if OMPI_HAVE_WEAK_SYMBOLS
@@ -43,13 +44,10 @@ OMPI_GENERATE_F77_BINDINGS (PMPI_WTICK,
 #endif
 
 #if ! OMPI_HAVE_WEAK_SYMBOLS && ! OMPI_PROFILE_LAYER
-OMPI_GENERATE_F77_BINDINGS (MPI_WTICK,
-                           mpi_wtick,
-                           mpi_wtick_,
-                           mpi_wtick__,
-                           mpi_wtick_f,
-                           (),
-                           () )
+double MPI_WTICK(void) { return mpi_wtick_f(); }
+double mpi_wtick(void) { return mpi_wtick_f(); }
+double mpi_wtick_(void) { return mpi_wtick_f(); }
+double mpi_wtick__(void) { return mpi_wtick_f(); }
 #endif
 
 
