586a9be55759e667ab0532fa89a19b2341e35970
Brian Barrett <brbarret@open-mpi.org>
2005-11-09 18:20:08 +0000

    * make it easier to compare free timings with / without memory hooks enabled
    This commit was SVN r8059.

--- test/memory/opal_memory_speed.c
+++ test/memory/opal_memory_speed.c
@@ -26,26 +26,40 @@
 #include "opal/runtime/opal.h"
 #include "opal/memory/memory.h"
 
-#define iters 10000
+#define iters 100000
+#define mask 0xfff
 
 static void* ptrs[iters];
+
+static void
+callback(void *buf, size_t length, void *cbdata)
+{
+}
 
 int
 main(int argc, char *argv[])
 {
-    int i;
+    int i, retval;
     struct timeval start, end;
     long time;
 
+    opal_init();
+    if (!opal_mem_free_is_supported()) {
+        printf("no memory registration supported.  skipping\n");
+        return 77;
+    }
+
+    printf("speed without a handler:\n");
+
     gettimeofday(&start, NULL);
     for (i = 0 ; i < iters ; ++i) {
-        /* malloc out a random size from 1 to 256 bytes */
-        ptrs[i] = malloc((rand() & 0xff) + 1);
+        /* malloc out a random size */
+        ptrs[i] = malloc((rand() & mask) + 1);
     }
     gettimeofday(&end, NULL);
     time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
         (end.tv_usec - start.tv_usec);
-    printf("malloc: %d calls in %ld microseconds.  %lf microseconds/call\n",
+    printf("  malloc: %d calls in %ld microseconds.  %lf microseconds/call\n",
            iters, time, (double) time / iters);
     gettimeofday(&start, NULL);
     for (i = 0 ; i < iters ; ++i) {
@@ -54,8 +68,88 @@ main(int argc, char *argv[])
     gettimeofday(&end, NULL);
     time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
         (end.tv_usec - start.tv_usec);
-    printf("free: %d calls in %ld microseconds.  %lf microseconds/call\n",
+    printf("  free: %d calls in %ld microseconds.  %lf microseconds/call\n",
            iters, time, (double) time / iters);
+
+    printf("speed with empty handler:\n");
+    retval = opal_mem_free_register_handler(callback, NULL);
+    if (retval != OMPI_SUCCESS) {
+        printf("handler registration failed\n");
+        return retval;
+    }
+
+    gettimeofday(&start, NULL);
+    for (i = 0 ; i < iters ; ++i) {
+        /* malloc out a random size */
+        ptrs[i] = malloc((rand() & mask) + 1);
+    }
+    gettimeofday(&end, NULL);
+    time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
+        (end.tv_usec - start.tv_usec);
+    printf("  malloc: %d calls in %ld microseconds.  %lf microseconds/call\n",
+           iters, time, (double) time / iters);
+    gettimeofday(&start, NULL);
+    for (i = 0 ; i < iters ; ++i) {
+        free(ptrs[i]);
+    }
+    gettimeofday(&end, NULL);
+    time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
+        (end.tv_usec - start.tv_usec);
+    printf("  free: %d calls in %ld microseconds.  %lf microseconds/call\n",
+           iters, time, (double) time / iters);
+    opal_mem_free_unregister_handler(callback);
+
+    printf("speed without a handler:\n");
+
+    gettimeofday(&start, NULL);
+    for (i = 0 ; i < iters ; ++i) {
+        /* malloc out a random size */
+        ptrs[i] = malloc((rand() & mask) + 1);
+    }
+    gettimeofday(&end, NULL);
+    time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
+        (end.tv_usec - start.tv_usec);
+    printf("  malloc: %d calls in %ld microseconds.  %lf microseconds/call\n",
+           iters, time, (double) time / iters);
+    gettimeofday(&start, NULL);
+    for (i = 0 ; i < iters ; ++i) {
+        free(ptrs[i]);
+    }
+    gettimeofday(&end, NULL);
+    time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
+        (end.tv_usec - start.tv_usec);
+    printf("  free: %d calls in %ld microseconds.  %lf microseconds/call\n",
+           iters, time, (double) time / iters);
+
+    printf("speed with empty handler:\n");
+    retval = opal_mem_free_register_handler(callback, NULL);
+    if (retval != OMPI_SUCCESS) {
+        printf("handler registration failed\n");
+        return retval;
+    }
+
+    gettimeofday(&start, NULL);
+    for (i = 0 ; i < iters ; ++i) {
+        /* malloc out a random size */
+        ptrs[i] = malloc((rand() & mask) + 1);
+    }
+    gettimeofday(&end, NULL);
+    time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
+        (end.tv_usec - start.tv_usec);
+    printf("  malloc: %d calls in %ld microseconds.  %lf microseconds/call\n",
+           iters, time, (double) time / iters);
+    gettimeofday(&start, NULL);
+    for (i = 0 ; i < iters ; ++i) {
+        free(ptrs[i]);
+    }
+    gettimeofday(&end, NULL);
+    time = ((end.tv_sec - start.tv_sec) * 1000 * 1000) +
+        (end.tv_usec - start.tv_usec);
+    printf("  free: %d calls in %ld microseconds.  %lf microseconds/call\n",
+           iters, time, (double) time / iters);
+    opal_mem_free_unregister_handler(callback);
+
+    opal_finalize();
 
     return 0;
 }
