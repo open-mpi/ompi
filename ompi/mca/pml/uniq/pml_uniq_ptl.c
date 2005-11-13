a8d2b70d63bac4d6c226a02aac6bd99415d019c9
George Bosilca <bosilca@icl.utk.edu>
2005-11-13 23:03:36 +0000

    Small fix fot the collision between the TEG and UNIQ PML.
    This commit was SVN r8145.

--- ompi/mca/pml/uniq/pml_uniq_ptl.c
+++ ompi/mca/pml/uniq/pml_uniq_ptl.c
@@ -20,8 +20,7 @@
 
 #include "pml_uniq_ptl.h"
 
-
-static void mca_pml_base_ptl_construct(mca_pml_base_ptl_t* ptl)
+static void mca_pml_uniq_ptl_construct(mca_pml_base_ptl_t* ptl)
 {
     OBJ_CONSTRUCT(&ptl->ptl_cache, opal_list_t);
     OBJ_CONSTRUCT(&ptl->ptl_cache_lock, opal_mutex_t);
@@ -30,7 +29,7 @@ static void mca_pml_base_ptl_construct(mca_pml_base_ptl_t* ptl)
     ptl->ptl_cache_alloc = 0;
 }
 
-static void mca_pml_base_ptl_destruct(mca_pml_base_ptl_t* ptl)
+static void mca_pml_uniq_ptl_destruct(mca_pml_base_ptl_t* ptl)
 {
     OBJ_DESTRUCT(&ptl->ptl_cache);
     OBJ_DESTRUCT(&ptl->ptl_cache_lock);
@@ -39,7 +38,7 @@ static void mca_pml_base_ptl_destruct(mca_pml_base_ptl_t* ptl)
 OBJ_CLASS_INSTANCE(
     mca_pml_base_ptl_t,
     opal_list_t,
-    mca_pml_base_ptl_construct,
-    mca_pml_base_ptl_destruct
+    mca_pml_uniq_ptl_construct,
+    mca_pml_uniq_ptl_destruct
 );
 
