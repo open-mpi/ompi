3f15a6fb9d662aa25e7f77bdc64e92e32ab43792
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2005-12-10 23:27:20 +0000

    The lb should be extracted from the datatype not from the convertor.

--- ompi/mca/btl/mvapi/btl_mvapi.c
+++ ompi/mca/btl/mvapi/btl_mvapi.c
@@ -467,7 +467,7 @@ mca_btl_base_descriptor_t* mca_btl_mvapi_prepare_dst(
         return NULL; 
     }
     
-    ompi_ddt_type_lb(convertor, &lb);
+    ompi_ddt_type_lb(convertor->pDesc, &lb);
     frag->segment.seg_len = *size; 
     frag->segment.seg_addr.pval = convertor->pBaseBuf + lb + convertor->bConverted; 
     frag->base.des_flags = 0; 
