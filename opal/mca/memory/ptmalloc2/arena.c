01e7191c3ecb350537c4139625c2a15b3e49ff4b
Brian Barrett <brbarret@open-mpi.org>
2005-12-13 16:04:48 +0000

    * use our memory barriers instead of ptmalloc2's, since it only does the   right thing with gcc on x86.
    This commit was SVN r8490.

--- opal/mca/memory/ptmalloc2/arena.c
+++ opal/mca/memory/ptmalloc2/arena.c
@@ -739,7 +739,10 @@ arena_get2(a_tsd, size) mstate a_tsd; size_t size;
   /* Add the new arena to the global list.  */
   (void)mutex_lock(&list_lock);
   a->next = main_arena.next;
+/* OMPI: use our barriers 
   atomic_write_barrier ();
+*/
+  opal_atomic_wmb();
   main_arena.next = a;
   (void)mutex_unlock(&list_lock);
 
