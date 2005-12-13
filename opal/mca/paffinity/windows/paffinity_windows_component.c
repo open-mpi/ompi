1d75ebd4a5e2b0342efd978bf43983abca8cfc0a
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2005-12-13 06:04:41 +0000

    First version of the paffinity for Windows. We detect correctly if the functions are available in the .h file as well as the kernel32.dll but we generate empty functions.

---
+++ opal/mca/paffinity/windows/paffinity_windows_component.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ *
+ * These symbols are in a file by themselves to provide nice linker
+ * semantics.  Since linkers generally pull in symbols by object
+ * files, keeping these symbols as the only symbols in this file
+ * prevents utility programs such as "ompi_info" from having to import
+ * entire components just to query their version and parameters.
+ */
+
+#include "ompi_config.h"
+
+#include "opal/include/constants.h"
+#include "opal/mca/paffinity/paffinity.h"
+#include "paffinity_windows.h"
+
+/*
+ * Public string showing the paffinity ompi_windows component version number
+ */
+const char *opal_paffinity_windows_component_version_string =
+    "OPAL windows paffinity MCA component version " OMPI_VERSION;
+
+/*
+ * Local function
+ */
+static int windows_open(void);
+
+/*
+ * Instantiate the public struct with all of our public information
+ * and pointers to our public functions in it
+ */
+
+const opal_paffinity_base_component_1_0_0_t mca_paffinity_windows_component = {
+
+    /* First, the mca_component_t struct containing meta information
+       about the component itself */
+
+    {
+        /* Indicate that we are a paffinity v1.0.0 component (which also
+           implies a specific MCA version) */
+        
+        OPAL_PAFFINITY_BASE_VERSION_1_0_0,
+
+        /* Component name and version */
+
+        "windows",
+        OMPI_MAJOR_VERSION,
+        OMPI_MINOR_VERSION,
+        OMPI_RELEASE_VERSION,
+
+        /* Component open and close functions */
+
+        windows_open,
+        NULL
+    },
+
+    /* Next the MCA v1.0.0 component meta data */
+
+    {
+        /* Whether the component is checkpointable or not */
+        
+        true
+    },
+
+    /* Query function */
+
+    opal_paffinity_windows_component_query
+};
+
+
+static int windows_open(void)
+{
+    mca_base_param_reg_int(&mca_paffinity_windows_component.paffinityc_version,
+                           "priority",
+                           "Priority of the windows paffinity component",
+                           false, false, 10, NULL);
+
+    return OPAL_SUCCESS;
+}
---
+++ opal/mca/paffinity/windows/paffinity_windows_module.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include "opal/include/constants.h"
+#include "opal/mca/base/mca_base_param.h"
+#include "opal/mca/paffinity/paffinity.h"
+#include "opal/mca/paffinity/base/base.h"
+#include "paffinity_windows.h"
+
+
+/*
+ * Local functions
+ */
+static int windows_module_init(void);
+static int windows_module_get_num_procs(int *num_procs);
+static int windows_module_set(int id);
+static int windows_module_get(int *id);
+
+/*
+ * Linux paffinity module
+ */
+static const opal_paffinity_base_module_1_0_0_t module = {
+
+    /* Initialization function */
+
+    windows_module_init,
+
+    /* Module function pointers */
+
+    windows_module_get_num_procs,
+    windows_module_set,
+    windows_module_get
+};
+
+
+const opal_paffinity_base_module_1_0_0_t *
+opal_paffinity_windows_component_query(int *query)
+{
+    int param;
+
+    param = mca_base_param_find("paffinity", "windows", "priority");
+    mca_base_param_lookup_int(param, query);
+
+    return &module;
+}
+
+
+static int windows_module_init(void)
+{
+    /* Nothing to do */
+
+    return OPAL_SUCCESS;
+}
+
+
+static int windows_module_get_num_procs(int *num_procs)
+{
+    *num_procs = 1;
+    return OPAL_SUCCESS;
+}
+
+static int windows_module_set(int id)
+{
+    int num_procs;
+
+    windows_module_get_num_procs(&num_procs);
+    if (id >= num_procs || id < 0) {
+        return OPAL_ERR_BAD_PARAM;
+    }
+
+    return OPAL_SUCCESS;
+}
+
+
+static int windows_module_get(int *id)
+{
+    unsigned int i;
+
+    return OPAL_SUCCESS;
+}
+
