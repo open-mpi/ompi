d290c861344a6e0c17066b52f801696939d3200f
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2005-12-31 12:35:24 +0000

    Add more protections around the headers.

--- orte/mca/pls/bproc/pls_bproc.c
+++ orte/mca/pls/bproc/pls_bproc.c
@@ -23,13 +23,23 @@
  */
 
 #include "orte_config.h"
+#if HAVE_SYS_TYPES_H
 #include <sys/types.h>
+#endif  /* HAVE_SYS_TYPES_H */
+#ifdef HAVE_SYS_STAT_H
 #include <sys/stat.h>
+#endif  /* HAVE_SYS_STAT_H */
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#include <sys/errno.h>
+#endif  /* HAVE_UNISTD_H */
+#include <errno.h>
 #include <signal.h>
+#ifdef HAVE_FCNTL_H
 #include <fcntl.h>
+#endif  /* HAVE_FCNTL_H */
+#ifdef HAVE_STRING_H
 #include <string.h>
+#endif  /* HAVE_STRING_H */
 
 #include "opal/event/event.h"
 #include "opal/mca/base/mca_base_param.h"
