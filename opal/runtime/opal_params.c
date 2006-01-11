234c6b96c3685f4a19922ac0abf470102f10cfcf
Brian Barrett <brbarret@open-mpi.org>
2006-01-11 05:02:15 +0000

    commit missing file from r8672
    This commit was SVN r8674.
    
    The following SVN revision numbers were found above:
      r8672 --> open-mpi/ompi@60ac1cb5f425988bd0e9a6efda97add34f5eee65

---
+++ opal/runtime/opal_params.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include <time.h>
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
+
+#include "opal/include/constants.h"
+#include "opal/runtime/opal.h"
+#include "opal/util/output.h"
+#include "opal/util/show_help.h"
+#include "opal/mca/base/mca_base_param.h"
+
+int opal_register_params(void)
+{
+    /*
+     * This string is going to be used in opal/util/stacktrace.c
+     */
+    {
+        char *string = NULL;
+        int j;
+        int signals[] = {
+#ifdef SIGABRT
+            SIGABRT,
+#endif
+#ifdef SIGBUS
+            SIGBUS,
+#endif
+#ifdef SIGFPE
+            SIGFPE,
+#endif
+#ifdef SIGSEGV
+            SIGSEGV,
+#endif
+            -1
+        };
+        for (j = 0 ; signals[j] != -1 ; ++j) {
+            if (j == 0) {
+                asprintf(&string, "%d", signals[j]);
+            } else {
+                char *tmp;
+                asprintf(&tmp, "%s,%d", string, signals[j]);
+                free(string);
+                string = tmp;
+            }
+        }
+
+        mca_base_param_reg_string_name("opal", "signal", 
+                                       "If a signal is received, display the stack trace frame",
+                                       false, false, string, NULL);
+    }
+
+    return OPAL_SUCCESS;
+}
