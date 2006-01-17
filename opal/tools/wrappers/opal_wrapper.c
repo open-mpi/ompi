bd5bcdc8a3e636593edcc3d4ff6da75b9c2c2adf
George Bosilca <bosilca@icl.utk.edu>
2006-01-17 13:22:08 +0000

    Add missing include files for x86 builds.
    This commit was SVN r8710.

--- opal/tools/wrappers/opal_wrapper.c
+++ opal/tools/wrappers/opal_wrapper.c
@@ -20,6 +20,9 @@
 
 #include <stdio.h>
 #include <errno.h>
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif  /*  HAVE_STDLIB_H */
 #ifdef HAVE_SYS_STAT_H
 #include <sys/stat.h>
 #endif
@@ -32,6 +35,9 @@
 #ifdef HAVE_SYS_WAIT_H
 #include <sys/wait.h>
 #endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif  /* HAVE_STRING_H */
 
 #include "opal/runtime/opal.h"
 #include "opal/include/constants.h"
