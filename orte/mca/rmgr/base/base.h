4b4b70cb0fb8d099a5b1f93e3b2e7bac4085b274
George Bosilca <bosilca@icl.utk.edu>
2006-02-08 19:52:57 +0000

    Remove compilation warning.
    This commit was SVN r8942.

--- orte/mca/rmgr/base/base.h
+++ orte/mca/rmgr/base/base.h
@@ -125,7 +125,8 @@ int orte_rmgr_base_spawn_not_available(
     orte_app_context_t** app_context,
     size_t num_context,
     orte_jobid_t* jobid,
-    orte_rmgr_cb_fn_t cbfn);
+    orte_rmgr_cb_fn_t cbfn,
+    orte_proc_state_t cb_conditions);
 int orte_rmgr_base_finalize_not_available(void);
 
 /*
