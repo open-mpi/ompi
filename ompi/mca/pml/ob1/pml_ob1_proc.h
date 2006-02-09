269fc0c13ab4ebe1bb9fe7448db93227494c6eca
George Bosilca <bosilca@icl.utk.edu>
2006-02-09 06:59:54 +0000

    Cleanup: 1. remove all useless macros from the proc header file 2. merge 2 of the match macros (they share the same logic except one list)
    This commit was SVN r8946.

--- ompi/mca/pml/ob1/pml_ob1_proc.h
+++ ompi/mca/pml/ob1/pml_ob1_proc.h
@@ -38,59 +38,7 @@ struct mca_pml_ob1_proc_t {
     mca_pml_proc_t base; 
 };
 typedef struct mca_pml_ob1_proc_t mca_pml_ob1_proc_t;
-    OMPI_COMP_EXPORT extern opal_class_t mca_pml_ob1_proc_t_class; 
-
-/**
- * Return the mca_pml_proc_t instance cached in the communicators local group.
- * 
- * @param comm   Communicator
- * @param rank   Peer rank
- * @return       mca_pml_proc_t instance
- */
-
-/* static inline mca_pml_ob1_proc_t* mca_pml_ob1_proc_lookup_local(ompi_communicator_t* comm, int rank) */
-/* { */
-/*     return (mca_pml_ob1_proc_t*) comm->c_local_group->grp_proc_pointers[rank]->proc_pml; */
-/* } */
-
-/* /\** */
-/*  * Return the mca_pml_proc_t instance cached on the communicators remote group. */
-/*  *  */
-/*  * @param comm   Communicator */
-/*  * @param rank   Peer rank */
-/*  * @return       mca_pml_proc_t instance */
-/*  *\/ */
-
-/* static inline mca_pml_ob1_proc_t* mca_pml_ob1_proc_lookup_remote(ompi_communicator_t* comm, int rank) */
-/* { */
-/*     return (mca_pml_ob1_proc_t*)  comm->c_pml_procs[rank]; */
-/* } */
-
-/* /\** */
-/*  * Return the mca_btl_peer_t instance corresponding to the process/btl combination. */
-/*  *  */
-/*  * @param comm   Communicator */
-/*  * @param rank   Peer rank */
-/*  * @return       mca_pml_proc_t instance */
-/*  *\/ */
-
-/* static inline struct mca_btl_base_endpoint_t* mca_pml_ob1_proc_lookup_remote_endpoint( */
-/*     ompi_communicator_t* comm,  */
-/*     int rank,  */
-/*     struct mca_btl_base_module_t* btl) */
-/* { */
-/*     mca_pml_ob1_proc_t* proc = (mca_pml_ob1_proc_t*) comm->c_pml_procs[rank]; */
-/*     size_t i, size = mca_pml_ob1_ep_array_get_size(&proc->btl_eager); */
-/*     mca_pml_ob1_endpoint_t* endpoint = proc->btl_eager.arr_endpoints; */
-/*     for(i = 0; i < size; i++) { */
-/*         if(endpoint->btl == btl) { */
-/*             return endpoint->btl_endpoint; */
-/*         } */
-/*         endpoint++; */
-/*     } */
-/*     return NULL; */
-/* } */
-
+OMPI_COMP_EXPORT extern opal_class_t mca_pml_ob1_proc_t_class; 
 
 #if defined(c_plusplus) || defined(__cplusplus)
 }
