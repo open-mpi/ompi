8568bb98927f3e7d78c35740b0efff72f091b5ce
Rainer Keller <rainer.keller@hlrs.de>
2006-02-28 10:16:42 +0000

    - Allow MPI_ERRCODES_IGNORE when checking is on.
    This commit was SVN r9180.

--- ompi/mpi/c/comm_spawn.c
+++ ompi/mpi/c/comm_spawn.c
@@ -63,10 +63,6 @@ int MPI_Comm_spawn(char *command, char **argv, int maxprocs, MPI_Info info,
             return OMPI_ERRHANDLER_INVOKE(comm, MPI_ERR_ARG,
                                           FUNC_NAME);
         }
-        if ( NULL == array_of_errcodes ) {
-            return OMPI_ERRHANDLER_INVOKE(comm, MPI_ERR_ARG,
-                                          FUNC_NAME);
-        }
         if (NULL == info || ompi_info_is_freed(info)) {
           return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_INFO,
                                         FUNC_NAME);
