c5a8870f6bf4e8ea04e181882d1a5ce7bfc99022
Mohamad Chaarawi <mschaara@cs.uh.edu>
2006-03-14 21:01:46 +0000

    move the implementation of the group creation functions into according back-end functions.
    This commit was SVN r9281.

--- ompi/mpi/c/group_excl.c
+++ ompi/mpi/c/group_excl.c
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      University of Houston. All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -36,16 +37,14 @@ static const char FUNC_NAME[] = "MPI_Group_excl";
 
 
 int MPI_Group_excl(MPI_Group group, int n, int *ranks,
-                   MPI_Group *new_group) {
+                   MPI_Group *new_group) 
+{
 
-    /* local variables */
-    int return_value,proc,i_excl,found,excl_proc,cnt,my_group_rank;
-    ompi_group_t *group_pointer, *new_group_pointer;
-    ompi_proc_t *my_proc_pointer;
-
-    return_value = MPI_SUCCESS;
+    ompi_group_t *group_pointer;
     group_pointer = (ompi_group_t *)group;
+    int i, err, group_size;
 
+    group_size = ompi_group_size ( group_pointer);
     if( MPI_PARAM_CHECK ) {
         OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
 
@@ -57,66 +56,29 @@ int MPI_Group_excl(MPI_Group group, int n, int *ranks,
         }
 
         /* check that new group is no larger than old group */
-        if ( n > group_pointer->grp_proc_count) {
+        if ( n > group_size) {
             return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_GROUP,
                                           FUNC_NAME);
         }
 
-    }  /* end if( MPI_CHECK_ARGS) */
+	/* check to see if procs are within range */
+        for( i=0 ; i  < n ; i++ ) {
+            if( ( 0 > ranks[i] ) || (ranks[i] >= group_size)){
+		return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_RANK,
+					      FUNC_NAME );
+	    }
+	}
 
-    if ( n == group_pointer->grp_proc_count ) {
+    }  /* end if( MPI_CHECK_ARGS) */
+    
+    if ( n == group_size ) {
         *new_group = MPI_GROUP_EMPTY;
         OBJ_RETAIN(MPI_GROUP_EMPTY);
         return MPI_SUCCESS;
     }
 
-    /*
-     * pull out elements
-     */
+    err = ompi_group_excl ( group, n, ranks, new_group );
+    OMPI_ERRHANDLER_RETURN(err, MPI_COMM_WORLD, err, FUNC_NAME );
 
-    /* get new group struct */
-    new_group_pointer=ompi_group_allocate(group_pointer->grp_proc_count-n);
-    if( NULL == new_group_pointer ) {
-        return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_GROUP,
-                                      FUNC_NAME);
-    }
-
-    /* put group elements in the list */
-    cnt=0;
-    for (proc = 0; proc < group_pointer->grp_proc_count; proc++) {
-        found=0;
-        /* check to see if this proc is in the exclusion list */
-        for( i_excl=0 ; i_excl < n ; ++i_excl ) {
-            excl_proc=ranks[i_excl];
-            /* check to see if this proc is within range */
-            if( ( 0 > excl_proc ) ||
-                (excl_proc >= group_pointer->grp_proc_count)){
-                return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_RANK,
-                                              FUNC_NAME);
-                }
-            if(excl_proc == proc ){
-                found=1;
-                break;
-            }
-
-        } /* end i_excl loop */
-        if( !found ) {
-            new_group_pointer->grp_proc_pointers[cnt] =
-                group_pointer->grp_proc_pointers[proc];
-            cnt++;
-        }
-
-    }   /* end proc loop */
-
-    /* increment proc reference counters */
-    ompi_group_increment_proc_count(new_group_pointer);
-
-    /* find my rank */
-    my_group_rank=group_pointer->grp_my_rank;
-    my_proc_pointer=group_pointer->grp_proc_pointers[my_group_rank];
-    ompi_set_group_rank(new_group_pointer,my_proc_pointer);
-
-    *new_group = (MPI_Group)new_group_pointer;
-
-    return MPI_SUCCESS;
+   
 }
