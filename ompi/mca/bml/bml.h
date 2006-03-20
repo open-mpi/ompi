7ce7baff1525ef7039235e0e1a1504c9b69cb97a
Galen Shipman <gshipman@lanl.gov>
2006-03-20 21:58:20 +0000

    more bml work
    This commit was SVN r9341.

--- ompi/mca/bml/bml.h
+++ ompi/mca/bml/bml.h
@@ -240,66 +240,11 @@ static inline void mca_bml_base_free(mca_bml_base_btl_t* bml_btl, mca_btl_base_d
 
 #if OMPI_ENABLE_DEBUG_RELIABILITY
 
-
-struct mca_bml_base_context_t {
-    size_t index;
-    mca_btl_base_completion_fn_t cbfunc;
-    void* cbdata;
-};
-typedef struct mca_bml_base_context_t mca_bml_base_context_t;
-
-
-static inline void mca_bml_base_completion(
-                                           struct mca_btl_base_module_t* btl,
-                                           struct mca_btl_base_endpoint_t* ep,
-                                           struct mca_btl_base_descriptor_t* des,
-                                           int status)
-{
-    mca_bml_base_context_t* ctx = (mca_bml_base_context_t*) des->des_cbdata;
-    /* restore original state */
-    ((unsigned char*)des->des_src[0].seg_addr.pval)[ctx->index] ^= ~0;
-    des->des_cbdata = ctx->cbdata;
-    des->des_cbfunc = ctx->cbfunc;
-    free(ctx);
-    /* invoke original callback */
-    des->des_cbfunc(btl,ep,des,status);
-}
-
-
-static inline int mca_bml_base_send(
+int mca_bml_base_send(
     mca_bml_base_btl_t* bml_btl, 
     mca_btl_base_descriptor_t* des, 
-    mca_btl_base_tag_t tag) 
-{ 
-    static int count;
-    if(count <= 0) {
-        count = (int) ((1000.0 * rand())/(RAND_MAX+1.0));
-        if(count % 2) {
-            /* local completion - network "drops" packet */
-            des->des_cbfunc(bml_btl->btl, bml_btl->btl_endpoint, des, OMPI_SUCCESS);
-            return OMPI_SUCCESS;
-        } else {
-            /* corrupt data */
-            mca_bml_base_context_t* ctx = (mca_bml_base_context_t*) 
-                malloc(sizeof(mca_bml_base_context_t));
-            if(NULL != ctx) {
-                ctx->index = (size_t) ((des->des_src[0].seg_len * rand() * 1.0) / (RAND_MAX + 1.0));
-                ctx->cbfunc = des->des_cbfunc;
-                ctx->cbdata = des->des_cbdata;
-                ((unsigned char*)des->des_src[0].seg_addr.pval)[ctx->index] ^= ~0;
-                des->des_cbdata = ctx;
-                des->des_cbfunc = mca_bml_base_completion;
-            }
-        }
-    }
-    count--;
-    des->des_context = (void*) bml_btl; 
-    return bml_btl->btl_send(
-                             bml_btl->btl,
-                             bml_btl->btl_endpoint, 
-                             des,
-                             tag);
-}
+    mca_btl_base_tag_t tag); 
+
 
 #else
 
