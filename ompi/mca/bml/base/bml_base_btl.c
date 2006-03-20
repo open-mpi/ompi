8276234e0ebb6881c50354ba1a9c51ab3beb6fef
Galen Shipman <gshipman@open-mpi-git-mirror.example.com>
2006-03-20 21:58:20 +0000

    more bml work

--- ompi/mca/bml/base/bml_base_btl.c
+++ ompi/mca/bml/base/bml_base_btl.c
@@ -22,7 +22,7 @@
 
 #include "ompi/mca/bml/bml.h"
 #include "bml_base_btl.h"
-
+#include "opal/util/crc.h"
 
 static void mca_bml_base_btl_array_construct(mca_bml_base_btl_array_t* array)
 {
@@ -62,3 +62,83 @@ int mca_bml_base_btl_array_reserve(mca_bml_base_btl_array_t* array, size_t size)
 }
 
 
+#if OMPI_ENABLE_DEBUG_RELIABILITY
+
+struct mca_bml_base_context_t {
+    size_t index;
+    mca_btl_base_completion_fn_t cbfunc;
+    void* cbdata;
+    uint32_t csum;
+};
+typedef struct mca_bml_base_context_t mca_bml_base_context_t;
+
+static void mca_bml_base_completion(
+                                    struct mca_btl_base_module_t* btl,
+                                    struct mca_btl_base_endpoint_t* ep,
+                                    struct mca_btl_base_descriptor_t* des,
+                                    int status)
+{
+    mca_bml_base_context_t* ctx = (mca_bml_base_context_t*) des->des_cbdata;
+    uint32_t csum;
+    /* restore original state */
+    ((unsigned char*)des->des_src[0].seg_addr.pval)[ctx->index] ^= ~0;
+    des->des_cbdata = ctx->cbdata;
+    des->des_cbfunc = ctx->cbfunc;
+    csum = OPAL_CSUM(des->des_src[0].seg_addr.pval, des->des_src[0].seg_len);
+    assert(csum == ctx->csum);
+    if(ctx->index == 40) { 
+        unsigned char temp = ((unsigned char*)des->des_src[0].seg_addr.pval)[ctx->index];
+        opal_output(0, "in completion, before %d, after %d\n", ~temp, temp);
+    }
+    
+    free(ctx);
+    /* invoke original callback */
+    des->des_cbfunc(btl,ep,des,status);
+}
+
+int mca_bml_base_send(
+    mca_bml_base_btl_t* bml_btl, 
+    mca_btl_base_descriptor_t* des, 
+    mca_btl_base_tag_t tag) 
+{ 
+    static int count;
+    des->des_context = bml_btl;
+    if(0 && count <= 0) {
+        count = (int) ((1000.0 * rand())/(RAND_MAX+1.0));
+        if(1 || count % 2) {
+            /* local completion - network "drops" packet */
+            des->des_cbfunc(bml_btl->btl, bml_btl->btl_endpoint, des, OMPI_SUCCESS);
+            return OMPI_SUCCESS;
+        } else {
+            /* corrupt data */
+            mca_bml_base_context_t* ctx = (mca_bml_base_context_t*) 
+                malloc(sizeof(mca_bml_base_context_t));
+            if(NULL != ctx) {
+                ctx->csum = OPAL_CSUM(des->des_src[0].seg_addr.pval, des->des_src[0].seg_len);
+                ctx->index = (size_t) ((des->des_src[0].seg_len * rand() * 1.0) / (RAND_MAX + 1.0));
+                if(des->des_src[0].seg_len > 40 ) { 
+                    unsigned char temp;
+                    ctx->index =  40;
+                    temp =  ((unsigned char*)des->des_src[0].seg_addr.pval)[ctx->index];
+                    opal_output(0, "corrupting index 40, before %d, after %d\n",  temp, ~temp);
+                    
+                }
+                ctx->cbfunc = des->des_cbfunc;
+                ctx->cbdata = des->des_cbdata;
+                ((unsigned char*)des->des_src[0].seg_addr.pval)[ctx->index] ^= ~0;
+                des->des_cbdata = ctx;
+                des->des_cbfunc = mca_bml_base_completion;
+               
+            }
+        }
+    }
+    count--;
+    des->des_context = (void*) bml_btl; 
+    return bml_btl->btl_send(
+                             bml_btl->btl,
+                             bml_btl->btl_endpoint, 
+                             des,
+                             tag);
+}
+
+#endif
