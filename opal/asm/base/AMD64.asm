252d5e36dcbda7622c4d43d5082b7d691d2d1ae9
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2006-04-06 01:23:33 +0000

    * sync opal_atomic_cmpset_{32,64} for AMD64 with the x86 32 bit version,   as I understand how that one works and don't really understand what   was in the amd64 code (which was copied from before I started working   on the inline assembly).  This fixes the race condition we were   seeing on PGI causing test failures * sync non-inline assembly with inline assembly version
    This needs to go to the v1.0 branch

--- opal/asm/base/AMD64.asm
+++ opal/asm/base/AMD64.asm
@@ -26,47 +26,20 @@ END_FUNC(opal_atomic_wmb)
 
 
 START_FUNC(opal_atomic_cmpset_32)
-	pushq	%rbp
-	movq	%rsp, %rbp
-	movq	%rdi, -8(%rbp)
-	movl	%esi, -12(%rbp)
-	movl	%edx, -16(%rbp)
-	movl	-16(%rbp), %ecx
-	movq	-8(%rbp), %rdx
-	movl	-12(%rbp), %eax
-	cmpxchgl %ecx,(%rdx)
-	movq	%rax, -24(%rbp)
-	movl	-24(%rbp), %eax
-	movl	%eax, -28(%rbp)
-	movl	-28(%rbp), %eax
-	cmpl	-12(%rbp), %eax
-	sete	%al
-	movzbl	%al, %eax
-	movl	%eax, -28(%rbp)
-	movl	-28(%rbp), %eax
-	leave
-	ret
+        movl    %esi, %eax
+        lock; cmpxchgl %edx,(%rdi)   
+        sete     %dl
+        movzbl  %dl, %eax
+        ret
 END_FUNC(opal_atomic_cmpset_32)
 
 
 START_FUNC(opal_atomic_cmpset_64)
-	pushq	%rbp
-	movq	%rsp, %rbp
-	movq	%rdi, -8(%rbp)
-	movq	%rsi, -16(%rbp)
-	movq	%rdx, -24(%rbp)
-	movq	-24(%rbp), %rcx
-	movq	-8(%rbp), %rdx
-	movq	-16(%rbp), %rax
-	cmpxchgq %rcx,(%rdx)   
-	
-	movq	%rax, -32(%rbp)
-	movq	-32(%rbp), %rax
-	cmpq	-16(%rbp), %rax
-	sete	%al
-	movzbl	%al, %eax
-	leave
-	ret
+        movq    %rsi, %rax
+        lock; cmpxchgq %rdx,(%rdi)   
+        sete     %dl
+        movzbl  %dl, %eax
+        ret
 END_FUNC(opal_atomic_cmpset_64)
 
 
