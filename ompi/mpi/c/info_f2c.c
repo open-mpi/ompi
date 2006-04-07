bdde2236b34b0a8e9e8f4e73530e825bfcd185ab
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2006-04-07 03:49:58 +0000

    Commit 1 of 2: be consistent in the MPI_*_F2C functions for the handling of invalid Fortran handles.  Per MPI-2:4.12.4, if we get an invalid Fortran handle, we should return an invalid C handle.  Before this commit, we checked if the fortran handle index was out of range (i.e., <0 or >sizeof(array)).  If so, we used to return MPI_*_NULL (i.e., a valid C handle).  But to be faithful to MPI-2:4.12.4, we now return NULL (an invalid C handle).
    If the fortran index is in bounds but is an index for an MPI object
    that has already been freed, the code already returns NULL because the
    entry in the array will be NULL (i.e., we already did what
    MPI-2:4.12.4 said for this case).
    
    Hence, this commit makes the handling of invalid fortran handles in
    the MPI_*_F2C functions be uniform: we always return NULL.
    
    Commit 2 of this will be to edit just about every C interface function
    (!) to ensure that MPI handles are not NULL.  Otherwise, if the user
    calls a fortran interface function with an invalid handle, the fortran
    interface function will call MPI_*_F2C and blindly pass the result to
    the back-end C function.  The C function will eventually end up trying
    to dereference it -- segv.  Having a run-time check for NULL and
    invoking an MPI exception is far more social (e.g., the user can get a
    stack trace out of MPI_ABORT) and consistent (i.e., we're already
    checking for MPI_*_NULL in the C interface functions).
    
    Since all the C interface functions have all the machinery for
    run-time parameter checking, and they all already check for
    MPI_*_NULL, it's easy enough to add another check for NULL.

--- ompi/mpi/c/info_f2c.c
+++ ompi/mpi/c/info_f2c.c
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -50,12 +51,13 @@ MPI_Info MPI_Info_f2c(MPI_Fint info)
     }
 
     /* Per MPI-2:4.12.4, do not invoke an error handler if we get an
-       invalid fortran handle */
+       invalid fortran handle.  If we get an invalid fortran handle,
+       return an invalid C handle. */
     
     if (info_index < 0 || 
         info_index >= 
         ompi_pointer_array_get_size(&ompi_info_f_to_c_table)) {
-        return MPI_INFO_NULL;
+        return NULL;
     }
 
     return ompi_pointer_array_get_item(&ompi_info_f_to_c_table, info_index);
