4f64f87b3b7021e5f3051b0437b1b556f5deac3f
Jeff Squyres <jsquyres@cisco.com>
2006-05-03 03:32:43 +0000

    Submitted by: Brian Barrett Reveiwed by: Jeff Squyres
    * do proper fortran string handling for the MPI-IO functions that take
      string arguments.  Thanks to Bernard Knaepen for bringing this to
      our attention.
    
    This commit was SVN r9797.

--- ompi/mpi/f77/file_open_f.c
+++ ompi/mpi/f77/file_open_f.c
@@ -19,6 +19,8 @@
 #include "ompi_config.h"
 
 #include "ompi/mpi/f77/bindings.h"
+#include "ompi/mpi/f77/strings.h"
+#include "ompi/file/file.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
 #pragma weak PMPI_FILE_OPEN = mpi_file_open_f
@@ -31,8 +33,8 @@ OMPI_GENERATE_F77_BINDINGS (PMPI_FILE_OPEN,
                            pmpi_file_open_,
                            pmpi_file_open__,
                            pmpi_file_open_f,
-                           (MPI_Fint *comm, char *filename, MPI_Fint *amode, MPI_Fint *info, MPI_Fint *fh, MPI_Fint *ierr),
-                           (comm, filename, amode, info, fh, ierr) )
+                           (MPI_Fint *comm, char *filename, MPI_Fint *amode, MPI_Fint *info, MPI_Fint *fh, MPI_Fint *ierr, int name_len),
+                           (comm, filename, amode, info, fh, ierr, name_len) )
 #endif
 
 #if OMPI_HAVE_WEAK_SYMBOLS
@@ -48,8 +50,8 @@ OMPI_GENERATE_F77_BINDINGS (MPI_FILE_OPEN,
                            mpi_file_open_,
                            mpi_file_open__,
                            mpi_file_open_f,
-                           (MPI_Fint *comm, char *filename, MPI_Fint *amode, MPI_Fint *info, MPI_Fint *fh, MPI_Fint *ierr),
-                           (comm, filename, amode, info, fh, ierr) )
+                           (MPI_Fint *comm, char *filename, MPI_Fint *amode, MPI_Fint *info, MPI_Fint *fh, MPI_Fint *ierr, int name_len),
+                           (comm, filename, amode, info, fh, ierr, name_len) )
 #endif
 
 
@@ -58,16 +60,28 @@ OMPI_GENERATE_F77_BINDINGS (MPI_FILE_OPEN,
 #endif
 
 void mpi_file_open_f(MPI_Fint *comm, char *filename, MPI_Fint *amode,
-		     MPI_Fint *info, MPI_Fint *fh, MPI_Fint *ierr)
+		     MPI_Fint *info, MPI_Fint *fh, MPI_Fint *ierr, int name_len)
 {
     MPI_Comm c_comm = MPI_Comm_f2c(*comm);
     MPI_Info c_info = MPI_Info_f2c(*info);
     MPI_File c_fh;
+    char *c_filename;
+    int c_err, ret;
 
-    *ierr = OMPI_INT_2_FINT(MPI_File_open(c_comm, filename, 
+    /* Convert the fortran string */
+    if (OMPI_SUCCESS != (ret = ompi_fortran_string_f2c(filename, name_len,
+                                                       &c_filename))) {
+        c_err = OMPI_ERRHANDLER_INVOKE(MPI_FILE_NULL, ret, "MPI_FILE_OPEN");
+        *ierr = OMPI_INT_2_FINT(c_err);
+        return;
+    }
+
+    *ierr = OMPI_INT_2_FINT(MPI_File_open(c_comm, c_filename, 
 					  OMPI_FINT_2_INT(*amode),
 					  c_info, &c_fh));
-   if (MPI_SUCCESS == OMPI_FINT_2_INT(*ierr)) {
+    if (MPI_SUCCESS == OMPI_FINT_2_INT(*ierr)) {
        *fh = MPI_File_c2f(c_fh);
-   }
+    }
+
+    free(c_filename);
 }
--- ompi/mpi/f77/register_datarep_f.c
+++ ompi/mpi/f77/register_datarep_f.c
@@ -19,6 +19,8 @@
 #include "ompi_config.h"
 
 #include "ompi/mpi/f77/bindings.h"
+#include "ompi/mpi/f77/strings.h"
+#include "ompi/file/file.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
 #pragma weak PMPI_REGISTER_DATAREP = mpi_register_datarep_f
@@ -31,8 +33,8 @@ OMPI_GENERATE_F77_BINDINGS (PMPI_REGISTER_DATAREP,
                            pmpi_register_datarep_,
                            pmpi_register_datarep__,
                            pmpi_register_datarep_f,
-                           (char *datarep, void *read_conversion_fn, void *write_conversion_fn, void *dtype_file_extent_fn, MPI_Fint *extra_state, MPI_Fint *ierr),
-                           (datarep, read_conversion_fn, write_conversion_fn, dtype_file_extent_fn, extra_state, ierr) )
+                           (char *datarep, void *read_conversion_fn, void *write_conversion_fn, void *dtype_file_extent_fn, MPI_Fint *extra_state, MPI_Fint *ierr, int datarep_len),
+                           (datarep, read_conversion_fn, write_conversion_fn, dtype_file_extent_fn, extra_state, ierr, datarep_len) )
 #endif
 
 #if OMPI_HAVE_WEAK_SYMBOLS
@@ -48,8 +50,8 @@ OMPI_GENERATE_F77_BINDINGS (MPI_REGISTER_DATAREP,
                            mpi_register_datarep_,
                            mpi_register_datarep__,
                            mpi_register_datarep_f,
-                           (char *datarep, void *read_conversion_fn, void *write_conversion_fn, void *dtype_file_extent_fn, MPI_Fint *extra_state, MPI_Fint *ierr),
-                           (datarep, read_conversion_fn, write_conversion_fn, dtype_file_extent_fn, extra_state, ierr) )
+                           (char *datarep, void *read_conversion_fn, void *write_conversion_fn, void *dtype_file_extent_fn, MPI_Fint *extra_state, MPI_Fint *ierr, int datarep_len),
+                           (datarep, read_conversion_fn, write_conversion_fn, dtype_file_extent_fn, extra_state, ierr, datarep_len) )
 #endif
 
 
@@ -66,18 +68,29 @@ union local_type_convert {
 void mpi_register_datarep_f(char *datarep, void *read_conversion_fn,
 			    void *write_conversion_fn,
 			    void *dtype_file_extent_fn, MPI_Fint *extra_state,
-			    MPI_Fint *ierr)
+			    MPI_Fint *ierr, int datarep_len)
 {
+   char *c_datarep;
+   int c_err, ret;
+   union local_type_convert a, b, c;
+
+    /* Convert the fortran string */
+    if (OMPI_SUCCESS != (ret = ompi_fortran_string_f2c(datarep, datarep_len,
+                                                       &c_datarep))) {
+        c_err = OMPI_ERRHANDLER_INVOKE(MPI_FILE_NULL, ret, "MPI_FILE_SET_VIEW");
+        *ierr = OMPI_INT_2_FINT(c_err);
+        return;
+    }
+
     /* Do a little type shifting so that we don't get compiler
        warnings */
-
-    union local_type_convert a, b, c;
-
     a.voidp = read_conversion_fn;
     b.voidp = write_conversion_fn;
     c.voidp = dtype_file_extent_fn;
 
-    *ierr = OMPI_INT_2_FINT(MPI_Register_datarep(datarep, a.convertp, 
+    *ierr = OMPI_INT_2_FINT(MPI_Register_datarep(c_datarep, a.convertp, 
                                                  b.convertp, c.extentp,
                                                  extra_state));
+
+    free(c_datarep);
 }
