0973292878e9aaddcc43585a9b54c182a6bb5c8b
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2006-05-07 20:51:43 +0000

    The last commit wans't supposed to bring this function in. It's not yet ready for primetime...

--- ompi/mca/pml/base/pml_base_request.c
+++ ompi/mca/pml/base/pml_base_request.c
@@ -36,39 +36,3 @@ opal_class_t mca_pml_base_request_t_class = {
     (opal_destruct_t) mca_pml_base_request_destruct 
 };
 
-typedef struct {
-    opal_list_item_t* last_memory;
-    opal_list_item_t* last_request;
-} ompi_debug_request_pos_t;
-
-int mca_pml_base_get_next_request( ompi_free_list_t* list,
-                                   struct ompi_free_list_pos_t* position,
-                                   ompi_request_state_t req_state,
-                                   mca_pml_base_request_t** base_req )
-{
-    opal_list_item_t* item;
-
- dig_for_the_requests:
-    (void)ompi_free_list_parse( list, position, &item );
-    if( item == NULL ) {
-        *base_req = NULL;
-        return 0;
-    }
-    *base_req = (mca_pml_base_request_t*)item;
-    /* Now that we have a pointer to a request, let's find if the request
-     * match the user requirements.
-     */
-    if( (*base_req)->req_ompi.req_state == OMPI_REQUEST_INVALID )
-        goto dig_for_the_requests;
-
-    /* If the req_state is set to INVALID, then the user does not care about
-     * the status of the request (except it should be valid !!!)
-     */
-    if( req_state == OMPI_REQUEST_INVALID )
-        return 0;
-
-    if( (*base_req)->req_ompi.req_state != req_state )
-        goto dig_for_the_requests;
-
-    return 0;
-}
