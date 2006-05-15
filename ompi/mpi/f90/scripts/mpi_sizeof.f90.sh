171585ff34c02633252aa860f384e81e9e5fe101
Jeff Squyres <jsquyres@cisco.com>
2006-05-15 19:31:01 +0000

    Ensure to put implicit none *before* the header file includes
    This commit was SVN r9923.

--- ompi/mpi/f90/scripts/mpi_sizeof.f90.sh
+++ ompi/mpi/f90/scripts/mpi_sizeof.f90.sh
@@ -1,4 +1,22 @@
 #! /bin/sh
+#
+# Copyright (c) 2004-2006 The Trustees of Indiana University and Indiana
+#                         University Research and Technology
+#                         Corporation.  All rights reserved.
+# Copyright (c) 2004-2005 The University of Tennessee and The University
+#                         of Tennessee Research Foundation.  All rights
+#                         reserved.
+# Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+#                         University of Stuttgart.  All rights reserved.
+# Copyright (c) 2004-2005 The Regents of the University of California.
+#                         All rights reserved.
+# Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
+# $COPYRIGHT$
+# 
+# Additional copyrights may follow
+# 
+# $HEADER$
+#
 
 . "$1/fortran_kinds.sh"
 
@@ -9,8 +27,8 @@ for kind in $lkinds
 do
     proc="${procedure}${rank}DL${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  logical*${kind}, intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -25,8 +43,8 @@ for kind in $ikinds
 do
     proc="${procedure}${rank}DI${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  integer*${kind}, intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -41,8 +59,8 @@ for kind in $rkinds
 do
     proc="${procedure}${rank}DR${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  real*${kind}, intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -60,8 +78,8 @@ do
     case "$kind" in  16)  size_kind='32'  ;  esac
     proc="${procedure}${rank}DC${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  complex*${kind}, intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -87,8 +105,8 @@ do
   do
     proc="${procedure}${rank}DL${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  logical*${kind}, dimension(${dim}), intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -103,8 +121,8 @@ do
   do
     proc="${procedure}${rank}DI${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  integer*${kind}, dimension(${dim}), intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -119,8 +137,8 @@ do
   do
     proc="${procedure}${rank}DR${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  real*${kind}, dimension(${dim}), intent(in) :: x"
     echo "  integer, intent(out) :: size"
@@ -138,8 +156,8 @@ do
     case "$kind" in  16)  size_kind='32'  ;  esac
     proc="${procedure}${rank}DC${kind}"
     echo "subroutine ${proc}(x, size, ierr)"
-    echo "  include 'mpif.h'"
     echo "  implicit none"
+    echo "  include 'mpif.h'"
     echo "  include 'fortran_sizes.h'"
     echo "  complex*${kind}, dimension(${dim}), intent(in) :: x"
     echo "  integer, intent(out) :: size"
