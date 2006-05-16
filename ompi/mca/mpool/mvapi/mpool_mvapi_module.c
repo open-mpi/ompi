deb2254c91268ddda6bd0bdb3abc35f46fe94446
Galen Shipman <gshipman@lanl.gov>
2006-05-16 22:04:31 +0000

    1. mpool_free changes to allow null registrations
    2. fix for MPI_Free_mem, was calling deregister but never called mpool_free.. so
    we leaked memory. Still an open issue here though, if the memory is alloc'd
    and the mpool doesn't create and cache a registration, we will never find the
    mpool to free with. 
    
    This commit was SVN r9944.

--- ompi/mca/mpool/mvapi/mpool_mvapi_module.c
+++ ompi/mca/mpool/mvapi/mpool_mvapi_module.c
@@ -197,7 +197,9 @@ void* mca_mpool_mvapi_realloc(
 void mca_mpool_mvapi_free(mca_mpool_base_module_t* mpool, void * addr,
                          mca_mpool_base_registration_t* registration)
 {
-    mpool->mpool_deregister(mpool, registration); 
+    if(registration){     
+        mpool->mpool_deregister(mpool, registration); 
+    }
     free(registration->alloc_base); 
 }
 
