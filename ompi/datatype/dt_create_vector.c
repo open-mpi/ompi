c32a61129716d4521fc1e314a97d71e6269bfe60
George Bosilca <bosilca@icl.utk.edu>
2006-06-06 18:24:09 +0000

    Minor cleanups and add the same consistent behavior as the one described on the commit 10225.
    This commit was SVN r10227.

--- ompi/datatype/dt_create_vector.c
+++ ompi/datatype/dt_create_vector.c
@@ -32,21 +32,25 @@ int32_t ompi_ddt_create_vector( int count, int bLength, long stride,
     long extent = oldType->ub - oldType->lb;
     ompi_datatype_t *pTempData, *pData;
 
-    pTempData = ompi_ddt_create( oldType->desc.used + 2 );
+    if( 0 == count ) {
+        *newType = ompi_ddt_create( 0 );
+        ompi_ddt_add( *newType, &ompi_mpi_datatype_null, 0, 0, 0);
+        return OMPI_SUCCESS;
+    }
+
+    pData = ompi_ddt_create( oldType->desc.used + 2 );
     if( (bLength == stride) || (1 >= count) ) {  /* the elements are contiguous */
-        pData = pTempData;
         ompi_ddt_add( pData, oldType, count * bLength, 0, extent );
     } else {
         if( 1 == bLength ) {
-            pData = pTempData;
             ompi_ddt_add( pData, oldType, count, 0, extent * stride );
         } else {
-            ompi_ddt_add( pTempData, oldType, bLength, 0, extent );
+            ompi_ddt_add( pData, oldType, bLength, 0, extent );
+            pTempData = pData;
             pData = ompi_ddt_create( oldType->desc.used + 2 + 2 );
             ompi_ddt_add( pData, pTempData, count, 0, extent * stride );
-        }
-        if( 1 != bLength )
             OBJ_RELEASE( pTempData );
+        }
     }
     *newType = pData;
     return OMPI_SUCCESS;
@@ -57,6 +61,12 @@ int32_t ompi_ddt_create_hvector( int count, int bLength, long stride,
 {
     long extent = oldType->ub - oldType->lb;
     ompi_datatype_t *pTempData, *pData;
+
+    if( 0 == count ) {
+        *newType = ompi_ddt_create( 0 );
+        ompi_ddt_add( *newType, &ompi_mpi_datatype_null, 0, 0, 0);
+        return OMPI_SUCCESS;
+    }
 
     pTempData = ompi_ddt_create( oldType->desc.used + 2 );
     if( ((extent * bLength) == stride) || (1 >= count) ) {  /* contiguous */
@@ -70,9 +80,8 @@ int32_t ompi_ddt_create_hvector( int count, int bLength, long stride,
             ompi_ddt_add( pTempData, oldType, bLength, 0, extent );
             pData = ompi_ddt_create( oldType->desc.used + 2 + 2 );
             ompi_ddt_add( pData, pTempData, count, 0, stride );
-        }
-        if( 1 != bLength )
             OBJ_RELEASE( pTempData );
+        }
     }
     *newType = pData;
     return OMPI_SUCCESS;
