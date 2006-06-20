1e4199ee612c43a2d8129c87dbae8e1179b7e505
George Bosilca <bosilca@icl.utk.edu>
2006-06-20 14:19:44 +0000

    Add more datatype tests. One to check all communications to self, it is used to compute the overhead of the convertor (and all convertor related operations). The second will check the position setting on the convertor. Not yet completed ...
    This commit was SVN r10432.

---
+++ test/datatype/position.c
@@ -0,0 +1,20 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+#include "ompi/datatype/convertor.h"
+#include "ompi/datatype/datatype.h"
+
+int main( int argc, char* argv[] )
+{
+    return 0;
+}
