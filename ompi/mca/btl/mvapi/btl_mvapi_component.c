bfcfc6fe1619356bdaad9a7880742af845d85849
Jeff Squyres <jsquyres@cisco.com>
2006-06-29 23:43:45 +0000

    Submitted by: Galen Shipman Reviewed by: Brian Barrett
    fix for stupid casting.. addresses issue on PPC64 where sizes get set
    improperly and badness ensues.. 
    
    This commit was SVN r10581.

--- ompi/mca/btl/mvapi/btl_mvapi_component.c
+++ ompi/mca/btl/mvapi/btl_mvapi_component.c
@@ -223,15 +223,20 @@ int mca_btl_mvapi_component_open(void)
     mca_btl_mvapi_param_register_int ("exclusivity", "BTL exclusivity", 
                                       MCA_BTL_EXCLUSIVITY_DEFAULT, (int*) &mca_btl_mvapi_module.super.btl_exclusivity);
     mca_btl_mvapi_param_register_int ("eager_limit", "eager send limit", 
-                                      (12*1024),(int*) &mca_btl_mvapi_module.super.btl_eager_limit);  
+                                      (12*1024), &tmp_int); 
+    mca_btl_mvapi_module.super.btl_eager_limit = tmp_int;  
     mca_btl_mvapi_param_register_int ("min_send_size", "minimum send size", 
-                                      (32*1024),(int*)  &mca_btl_mvapi_module.super.btl_min_send_size);
+                                      (32*1024), &tmp_int);
+    mca_btl_mvapi_module.super.btl_min_send_size = tmp_int;
     mca_btl_mvapi_param_register_int ("max_send_size", "maximum send size", 
-                                      (64*1024), (int*) &mca_btl_mvapi_module.super.btl_max_send_size); 
+                                      (64*1024), &tmp_int);
+    mca_btl_mvapi_module.super.btl_max_send_size = tmp_int; 
     mca_btl_mvapi_param_register_int("min_rdma_size", "minimum rdma size", 
-                                     1024*1024, (int*) &mca_btl_mvapi_module.super.btl_min_rdma_size);
+                                     1024*1024, &tmp_int);
+    mca_btl_mvapi_module.super.btl_min_rdma_size = tmp_int;
     mca_btl_mvapi_param_register_int("max_rdma_size", "maximium rdma size", 
-                                     1024*1024, (int*) &mca_btl_mvapi_module.super.btl_max_rdma_size); 
+                                     1024*1024, &tmp_int);
+    mca_btl_mvapi_module.super.btl_max_rdma_size = tmp_int; 
     mca_btl_mvapi_param_register_int("flags", "BTL flags, SEND=1, PUT=2, GET=4", 
                                      MCA_BTL_FLAGS_PUT | MCA_BTL_FLAGS_GET, (int*) &mca_btl_mvapi_module.super.btl_flags); 
     mca_btl_mvapi_param_register_int("bandwidth", "Approximate maximum bandwidth of interconnect", 
