open-mpi/ompi

commit 54327323501626a3c6dac47e701acf07125f50b8

Per the analysis in bug #184, move some assignments around to effect thread safety.  This is likely to be only the first of multiple steps for complete thread safety in the MPI attribute code.  All tests [continue to] pass the intel and ibm attribute tests.
Also renamed a variable from "attr" to "attrkey" to reflect that it's
a keyval, not an attribute.


git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@10661 63e3feb5-37d5-0310-a306-e8a459e722fe

ompi/attribute/attribute.c
@@ -516,7 +516,7 @@ int ompi_attr_create_keyval(ompi_attribute_type_t type,
                             ompi_attribute_fn_ptr_union_t delete_attr_fn,
                             int *key, void *extra_state, int flags)
 {
-    ompi_attrkey_item_t *attr;
+    ompi_attrkey_item_t *attrkey;
     int ret;
 
     /* Protect against the user calling ompi_attr_destroy and then
@@ -527,34 +527,34 @@ int ompi_attr_create_keyval(ompi_attribute_type_t type,
 
     /* Allocate space for the list item */
 
-    attr = OBJ_NEW(ompi_attrkey_item_t);
-    if (NULL == attr) {
+    attrkey = OBJ_NEW(ompi_attrkey_item_t);
+    if (NULL == attrkey) {
 	return MPI_ERR_SYSRESOURCE;
     }
+
+    /* Fill in the list item (must be done before we set the attrkey
+       on the keyval_hash in case some other thread immediately reads
+       it from the keyval_hash) */
+  
+    attrkey->copy_attr_fn = copy_attr_fn;
+    attrkey->delete_attr_fn = delete_attr_fn;
+    attrkey->extra_state = extra_state;
+    attrkey->attr_type = type;
+    attrkey->key = *key;
+    attrkey->attr_flag = flags;
 
     /* Create a new unique key and fill the hash */
   
     OPAL_THREAD_LOCK(&alock);
     ret = CREATE_KEY(key);
     if (OMPI_SUCCESS == ret) {
-        ret = opal_hash_table_set_value_uint32(keyval_hash, *key, attr);
+        ret = opal_hash_table_set_value_uint32(keyval_hash, *key, attrkey);
     }
     OPAL_THREAD_UNLOCK(&alock);
     if (OMPI_SUCCESS != ret) {
 	return ret;
     }
 
-    /* Fill in the list item */
-  
-    attr->copy_attr_fn = copy_attr_fn;
-    attr->delete_attr_fn = delete_attr_fn;
-    attr->extra_state = extra_state;
-    attr->attr_type = type;
-    attr->key = *key;
-    attr->attr_flag = flags;
-
-    /* Other fields will be filled in by the create_attr function */
-  
     return MPI_SUCCESS;
 }
 

commit 0e337431de0e3e3a80e846013a1f4eb013b4627a

Fix for bug #176.
 * Fix for two problems introduced by r10661:
   1. ensure to use the key ''after'' it is initialized (sigh).
   1. handle the case where we free the attrkey before it is fully
      initialized (i.e., some other error causes us to free it).  In
      this case, don't try to remove the key from the hash map,
      because it won't exist.
 * More accurate zeroing in the keyval constructor
   (ompi_attrkey_item_constructor)
 * Widen the scope of the alock such that the attrkey destructor does
   not need to acquire it.  Instead, assume that the caller already
   has it.
 * Add a comment about why the keyval may get destroyed as the result
   of deleting an attribute (so that I don't have to figure it out
   again the next time I read this code :-) )

ompi/attribute/attribute.c
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -435,20 +436,32 @@ static void attribute_value_construct(attribute_value_t *item)
 static void
 ompi_attrkey_item_construct(ompi_attrkey_item_t *item) 
 {
-    memset(&(item->attr_type), 0, 
-	   sizeof(ompi_attrkey_item_t) - sizeof(opal_object_t));
+    item->attr_type = 0;
+    item->attr_flag = 0;
+    item->copy_attr_fn.attr_communicator_copy_fn = NULL;
+    item->delete_attr_fn.attr_communicator_copy_fn = NULL;
+    item->extra_state = NULL;
+
+    /* Set the item->key value to an invalid value so that we can know
+       if it has been initialized with a proper value or not.
+       Specifically, the destructor may get invoked if we weren't able
+       to assign a key properly.  So we don't want to try to remove it
+       from the table if it wasn't there. */
+    item->key = -1;
 }
 
 
 static void 
 ompi_attrkey_item_destruct(ompi_attrkey_item_t *item) 
 {
-    /* Remove the key entry from the hash and free the key */
+    /* THIS FUNCTION ASSUMES THAT THE CALLER ALREADY HAS OBTAINED THE
+       alock MUTEX!  Remove the key entry from the hash and free the
+       key. */
 
-    OPAL_THREAD_LOCK(&alock);
-    opal_hash_table_remove_value_uint32(keyval_hash, item->key);
-    FREE_KEY(item->key);
-    OPAL_THREAD_UNLOCK(&alock);
+    if (-1 != item->key) {
+        opal_hash_table_remove_value_uint32(keyval_hash, item->key);
+        FREE_KEY(item->key);
+    }
 }
 
 
@@ -540,21 +553,24 @@ int ompi_attr_create_keyval(ompi_attribute_type_t type,
     attrkey->delete_attr_fn = delete_attr_fn;
     attrkey->extra_state = extra_state;
     attrkey->attr_type = type;
-    attrkey->key = *key;
     attrkey->attr_flag = flags;
+    attrkey->key = -1;
 
     /* Create a new unique key and fill the hash */
   
     OPAL_THREAD_LOCK(&alock);
     ret = CREATE_KEY(key);
     if (OMPI_SUCCESS == ret) {
+        attrkey->key = *key;
         ret = opal_hash_table_set_value_uint32(keyval_hash, *key, attrkey);
     }
-    OPAL_THREAD_UNLOCK(&alock);
     if (OMPI_SUCCESS != ret) {
+        OBJ_RELEASE(attrkey);
+        OPAL_THREAD_UNLOCK(&alock);
 	return ret;
     }
 
+    OPAL_THREAD_UNLOCK(&alock);
     return MPI_SUCCESS;
 }
 
@@ -576,11 +592,11 @@ int ompi_attr_free_keyval(ompi_attribute_type_t type, int *key,
     OPAL_THREAD_LOCK(&alock);
     ret = opal_hash_table_get_value_uint32(keyval_hash, *key, 
 					   (void **) &key_item);
-    OPAL_THREAD_UNLOCK(&alock);
   
     if ((OMPI_SUCCESS != ret) || (NULL == key_item) || 
         (key_item->attr_type != type) ||
 	((!predefined) && (key_item->attr_flag & OMPI_KEYVAL_PREDEFINED))) {
+        OPAL_THREAD_UNLOCK(&alock);
 	return OMPI_ERR_BAD_PARAM;
     }
 
@@ -593,6 +609,7 @@ int ompi_attr_free_keyval(ompi_attribute_type_t type, int *key,
        the last attribute is deleted, this object gets deleted too */
 
     OBJ_RELEASE(key_item);
+    OPAL_THREAD_UNLOCK(&alock);
 
     return MPI_SUCCESS;
 }
@@ -674,15 +691,20 @@ int ompi_attr_delete(ompi_attribute_type_t type, void *object,
 
 
  exit:
+    /* Decrement the ref count for the keyval.  If ref count goes to
+       0, destroy the keyval (the destructor deletes the key
+       implicitly for this object).  The ref count will only go to 0
+       here if MPI_*_FREE_KEYVAL was previously invoked and we just
+       freed the last attribute that was using the keyval. */
+
+    if (OMPI_SUCCESS == ret) {
+        OBJ_RELEASE(key_item);
+    }
+
     if (need_lock) {
 	OPAL_THREAD_UNLOCK(&alock);
     }	
 
-    /* Decrement the ref count for the key, and if ref count is 0,
-       remove the key (the destructor deletes the key implicitly for
-       this object */
-    
-    OBJ_RELEASE(key_item);
     return ret;
 }
 
@@ -1040,6 +1062,9 @@ static int set_value(ompi_attribute_type_t type, void *object,
 	    break;
 
 	default:
+            if (need_lock) {
+                OPAL_THREAD_UNLOCK(&alock);
+            }
             return MPI_ERR_INTERN;
 	}
 	had_old = true;
@@ -1048,6 +1073,15 @@ static int set_value(ompi_attribute_type_t type, void *object,
 
     ret = opal_hash_table_set_value_uint32(*keyhash, key, new_attr);
 
+    /* Increase the reference count of the object, only if there was no
+       old atribute/no old entry in the object's key hash */
+
+    if (OMPI_SUCCESS == ret && !had_old) {
+	OBJ_RETAIN(key_item);
+    }
+
+    /* Release the lock if we grabbed it */
+
     if (need_lock) {
         OPAL_THREAD_UNLOCK(&alock);
     }
@@ -1055,12 +1089,6 @@ static int set_value(ompi_attribute_type_t type, void *object,
 	return ret;
     }
 
-    /* Increase the reference count of the object, only if there was no
-       old atribute/no old entry in the object's key hash */
-
-    if (!had_old) {
-	OBJ_RETAIN(key_item);
-    }
     return MPI_SUCCESS;
 }
 