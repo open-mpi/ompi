31c66d92aa607d2098152075d0c93197c63e1269
Rainer Keller <rainer.keller@hlrs.de>
2006-07-24 16:59:01 +0000

    Minor fixes to match standard -- and run strict test of mpi_test_suite:  - bsend_init: use *request after error-checking  - Always reset the status->cancelled  - cancel, wait: need to check *request for MPI_REQUEST_NULL, not    NULL...    (actually ompi_request_wait handles MPI_PROC_NULL, so no need    to check&set of status_empty in wait.c)
    This commit was SVN r10972.

--- ompi/mpi/c/iprobe.c
+++ ompi/mpi/c/iprobe.c
@@ -5,7 +5,7 @@
  * Copyright (c) 2004-2005 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
- * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ * Copyright (c) 2004-2006 High Performance Computing Center Stuttgart, 
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
@@ -41,6 +41,7 @@ int MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status *status
             status->MPI_TAG = MPI_ANY_TAG;
             status->MPI_ERROR = MPI_SUCCESS;
             status->_count = 0;
+            status->_cancelled = 0;
         }
         return MPI_SUCCESS;
     }
--- ompi/mpi/c/sendrecv.c
+++ ompi/mpi/c/sendrecv.c
@@ -5,7 +5,7 @@
  * Copyright (c) 2004-2005 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
- * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ * Copyright (c) 2004-2006 High Performance Computing Center Stuttgart, 
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
@@ -77,10 +77,11 @@ int MPI_Sendrecv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
         rc = ompi_request_wait(&req, status);
     } else {
         if (MPI_STATUS_IGNORE != status) {
-            status->MPI_ERROR = MPI_SUCCESS;
             status->MPI_SOURCE = MPI_PROC_NULL;
             status->MPI_TAG = MPI_ANY_TAG;
+            status->MPI_ERROR = MPI_SUCCESS;
             status->_count = 0;
+            status->_cancelled = 0;
         }
         rc = MPI_SUCCESS;
     }
