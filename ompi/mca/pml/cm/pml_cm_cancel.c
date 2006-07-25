785a773829ee7c1a114a25a2867cd8bf8c81d516
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2006-07-25 21:46:04 +0000

    Remove compiler warning.  Remove this when CM cancel is fully implemented.

--- ompi/mca/pml/cm/pml_cm_cancel.c
+++ ompi/mca/pml/cm/pml_cm_cancel.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -20,11 +21,17 @@
 int
 mca_pml_cm_cancel(struct ompi_request_t *request, int flag)
 {
-    int ret;
+    /* Temporarily silence a compiler warning.  Remove this when the
+       cancel is fully implemented */
+#if 1
+    return OMPI_SUCCESS;
+#else
+    int ret
    /*  mca_pml_cm_request_t *base_request =  */
 /*         (mca_pml_cm_request_t*) request; */
 /*     ret = OMPI_MTL_CALL(cancel(ompi_mtl, */
 /*                                &base_request->req_mtl, */
 /*                                flag)); */
     return ret;
+#endif
 }
