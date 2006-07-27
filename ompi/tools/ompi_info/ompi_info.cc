cef6fdbca3988b766ae327a0ea3a664c238d97f1
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2006-07-27 03:48:12 +0000

    * add listing of which backtrace component got compiled in

--- ompi/tools/ompi_info/ompi_info.cc
+++ ompi/tools/ompi_info/ompi_info.cc
@@ -176,6 +176,7 @@ int main(int argc, char *argv[])
   ompi_info::mca_types.push_back("orte");
   ompi_info::mca_types.push_back("opal");
 
+  ompi_info::mca_types.push_back("backtrace");
   ompi_info::mca_types.push_back("memory");
   ompi_info::mca_types.push_back("paffinity");
   ompi_info::mca_types.push_back("maffinity");
