open-mpi/ompi

commit dc57e851399d8c9b335f18f51b250ead92fcc841

Per LANL's stated need, add functionality that runs a.out across ALL available process slots if no num_proc is specified on the command line. However, please note the following limitation: we ONLY allow ONE application to be specified on the command line when this feature is invoked. If multiple apps are specified, the user MUST also specify the number to be launched for each and every one of them.
Update the help text to report errors when not following that rule.

Also updated the RMAPS help text to reflect the reorganization of some of the round-robin code into the base.

The new functionality has been tested under Mac OS-X and on Odin using an MPI program. Both byslot and bynode mapping have been checked and verified. Operational support for other systems needs to be verified - I respectfully request people's help in doing so.


git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@10708 63e3feb5-37d5-0310-a306-e8a459e722fe

orte/mca/rmgr/urm/rmgr_urm.c
@@ -150,13 +150,6 @@ static int orte_rmgr_urm_create(
         return rc;
     }
 
-    /* setup the launch stage gate counters and subscriptions */
-    if (ORTE_SUCCESS !=
-        (rc = orte_rmgr_base_proc_stage_gate_init(*jobid))) {
-        ORTE_ERROR_LOG(rc);
-        return rc;
-    }
-
     return ORTE_SUCCESS;
 }
 
@@ -465,6 +458,14 @@ static int orte_rmgr_urm_spawn(
         ORTE_ERROR_LOG(rc);
         return rc;
     }
+     
+     /* setup the launch system's stage gate counters and subscriptions */
+     if (ORTE_SUCCESS !=
+         (rc = orte_rmgr_base_proc_stage_gate_init(*jobid))) {
+         ORTE_ERROR_LOG(rc);
+         return rc;
+     }
+          
     /** setup the subscription so we can complete the wireup when all processes reach LAUNCHED */
     rc = orte_rmgr_base_proc_stage_gate_subscribe(*jobid, orte_rmgr_urm_wireup_callback, NULL, ORTE_PROC_STATE_LAUNCHED);
     if(ORTE_SUCCESS != rc) {

orte/include/orte/orte_constants.h
@@ -86,7 +86,8 @@ enum {
     ORTE_ERR_PROC_STATE_MISSING             = (ORTE_ERR_BASE - 23),
     ORTE_ERR_PROC_EXIT_STATUS_MISSING       = (ORTE_ERR_BASE - 24),
     ORTE_ERR_INDETERMINATE_STATE_INFO       = (ORTE_ERR_BASE - 25),
-    ORTE_ERR_NODE_FULLY_USED				= (ORTE_ERR_BASE - 26)
+    ORTE_ERR_NODE_FULLY_USED				= (ORTE_ERR_BASE - 26),
+    ORTE_ERR_INVALID_NUM_PROCS              = (ORTE_ERR_BASE - 27)
 };
 
 #define ORTE_ERR_MAX                      (ORTE_ERR_BASE - 100)

orte/mca/rmaps/base/rmaps_base_map.c
@@ -347,12 +347,20 @@ int orte_rmaps_base_get_map(orte_jobid_t jobid, opal_list_t* mapping_list)
         orte_rmaps_base_map_t* map = OBJ_NEW(orte_rmaps_base_map_t);
         orte_app_context_t* app = app_context[i];
         map->app = app;
-        map->procs = malloc(sizeof(orte_rmaps_base_proc_t*) * app->num_procs);
-        if(NULL == map->procs) {
-            OBJ_RELEASE(map);
-            rc = ORTE_ERR_OUT_OF_RESOURCE;
-            ORTE_ERROR_LOG(rc);
-            goto cleanup;
+        if (0 < app->num_procs) {
+            map->procs = malloc(sizeof(orte_rmaps_base_proc_t*) * app->num_procs);
+            if(NULL == map->procs) {
+                OBJ_RELEASE(map);
+                rc = ORTE_ERR_OUT_OF_RESOURCE;
+                ORTE_ERROR_LOG(rc);
+                goto cleanup;
+            }
+        } else {
+            if (1 < num_context) {  /** can't have multiple contexts if zero num_procs */
+                ORTE_ERROR_LOG(ORTE_ERR_INVALID_NUM_PROCS);
+                rc = ORTE_ERR_INVALID_NUM_PROCS;
+                goto cleanup;
+            }
         }
         map->num_procs = 0;
         mapping[i] = map;

orte/mca/rmaps/base/rmaps_base_node.c
@@ -96,12 +96,16 @@ static bool is_mapped(opal_list_item_t *item,
 /*
  * Query the registry for all nodes allocated to a specified job
  */
-int orte_rmaps_base_get_target_nodes(opal_list_t* nodes, orte_jobid_t jobid)
+int orte_rmaps_base_get_target_nodes(opal_list_t* nodes, orte_jobid_t jobid, size_t *total_num_slots)
 {
     opal_list_item_t *item, *next;
     orte_ras_node_t *node;
     int id, rc, nolocal;
+    size_t num_slots=0;
 
+    /** set default answer */
+    *total_num_slots = 0;
+    
     if(ORTE_SUCCESS != (rc = orte_ras_base_node_query_alloc(nodes, jobid))) {
         ORTE_ERROR_LOG(rc);
         return rc;
@@ -136,6 +140,8 @@ int orte_rmaps_base_get_target_nodes(opal_list_t* nodes, orte_jobid_t jobid)
         node = (orte_ras_node_t*)item;
         if (0 != node->node_slots_max && node->node_slots_inuse > node->node_slots_max) {
             opal_list_remove_item(nodes, item);
+        } else { /** otherwise, add its slots to the total */
+            num_slots += node->node_slots;
         }
 
         /** go on to next item */
@@ -148,6 +154,8 @@ int orte_rmaps_base_get_target_nodes(opal_list_t* nodes, orte_jobid_t jobid)
         return ORTE_ERR_TEMP_OUT_OF_RESOURCE;
     }
     
+    *total_num_slots = num_slots;
+    
     return ORTE_SUCCESS;
 }
 
@@ -157,15 +165,19 @@ int orte_rmaps_base_get_target_nodes(opal_list_t* nodes, orte_jobid_t jobid)
  */
 int orte_rmaps_base_get_mapped_targets(opal_list_t *mapped_node_list,
                                        orte_app_context_t *app,
-                                       opal_list_t *master_node_list)
+                                       opal_list_t *master_node_list,
+                                       size_t *total_num_slots)
 {
     orte_app_context_map_t** loc_map = app->map_data;
     opal_list_item_t *item;
     orte_ras_node_t *node, *new_node;
     char **mapped_nodes = NULL;
     int num_mapped_nodes = 0;
-    size_t j, k;
+    size_t j, k, num_slots=0;
     int rc;
+    
+    /** set default answer */
+    *total_num_slots = 0;
     
     /* Accumulate all of the host name mappings */
     for(k = 0; k < app->num_map; ++k) {
@@ -193,7 +205,7 @@ int orte_rmaps_base_get_mapped_targets(opal_list_t *mapped_node_list,
      * for our use - if not, then that's an error
      */
     if( !are_all_mapped_valid(mapped_nodes, num_mapped_nodes, master_node_list) ) {
-        opal_show_help("help-orte-rmaps-rr.txt", "orte-rmaps-rr:not-all-mapped-alloc",
+        opal_show_help("help-orte-rmaps-base.txt", "orte-rmaps-base:not-all-mapped-alloc",
                        true, app->app, opal_argv_join(mapped_nodes, ','));
         ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE);
         return ORTE_ERR_OUT_OF_RESOURCE;
@@ -212,21 +224,24 @@ int orte_rmaps_base_get_mapped_targets(opal_list_t *mapped_node_list,
             /** we can't just add this item to the mapped_node_list as it cannot be
              * on two lists at the same time, so we need to copy it first
              */
-            if (ORTE_SUCCESS != (rc = orte_dss.copy(&new_node, node, ORTE_RAS_NODE))) {
+            if (ORTE_SUCCESS != (rc = orte_dss.copy((void**)&new_node, node, ORTE_RAS_NODE))) {
                 ORTE_ERROR_LOG(rc);
                 return rc;
             }
             opal_list_append(mapped_node_list, &new_node->super);
+            num_slots += new_node->node_slots_alloc;
         }
     }
 
     /** check that anything is left! */
     if (0 == opal_list_get_size(mapped_node_list)) {
-        opal_show_help("help-orte-rmaps-rr.txt", "orte-rmaps-rr:alloc-error",
+        opal_show_help("help-orte-rmaps-base.txt", "orte-rmaps-base:no-mapped-node",
                        true, app->num_procs, app->app);
         ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE);
         return ORTE_ERR_OUT_OF_RESOURCE;
     }
+
+    *total_num_slots = num_slots;
 
     return ORTE_SUCCESS;
 }

orte/mca/rmaps/base/rmaps_base_node.h
@@ -45,11 +45,12 @@ extern "C" {
  * RMAPS 
  */
 
-int orte_rmaps_base_get_target_nodes(opal_list_t* node_list, orte_jobid_t jobid);
+int orte_rmaps_base_get_target_nodes(opal_list_t* node_list, orte_jobid_t jobid, size_t *total_num_slots);
 int orte_rmaps_base_update_node_usage(opal_list_t *nodes);
 int orte_rmaps_base_get_mapped_targets(opal_list_t *mapped_node_list,
                                        orte_app_context_t *app,
-                                       opal_list_t *master_node_list);
+                                       opal_list_t *master_node_list,
+                                       size_t *total_num_slots);
 int orte_rmaps_base_claim_slot(orte_rmaps_base_map_t *map,
                                orte_ras_node_t *current_node,
                                orte_jobid_t jobid, orte_vpid_t vpid,

orte/mca/rmaps/base/rmaps_base_open.c
@@ -97,8 +97,8 @@ int orte_rmaps_base_open(void)
     mca_base_param_reg_int_name("rmaps", "base_no_oversubscribe",
                                 "If nonzero, then do not allow oversubscription of nodes - mpirun will return an error if there aren't enough nodes to launch all processes without oversubscribing",
                                 false, false, 0, &value);
-    if (0 == value) {
-        orte_rmaps_base.oversubscribe = true;  /** default condition */
+    if ((int)false == value) {
+        orte_rmaps_base.oversubscribe = true;  /** default condition that allows oversubscription */
     } else {
         orte_rmaps_base.oversubscribe = false;
     }

orte/mca/rmaps/round_robin/rmaps_rr.c
@@ -252,10 +252,10 @@ static int orte_rmaps_rr_map(orte_jobid_t jobid)
     opal_list_item_t *item, *item2;
     orte_ras_node_t *node, *node2;
     orte_vpid_t vpid_start;
-    size_t num_procs = 0;
+    size_t num_procs = 0, total_num_slots, mapped_num_slots;
     int rank = 0;
     int rc;
-    bool bynode = true;
+    bool bynode = true, modify_app_context = false;
 
     /* query for the application context and allocated nodes */
     if(ORTE_SUCCESS != (rc = orte_rmgr_base_get_app_context(jobid, &context, &num_context))) {
@@ -275,7 +275,7 @@ static int orte_rmaps_rr_map(orte_jobid_t jobid)
      * mappings from the user
      */
     OBJ_CONSTRUCT(&master_node_list, opal_list_t);
-    if(ORTE_SUCCESS != (rc = orte_rmaps_base_get_target_nodes(&master_node_list, jobid))) {
+    if(ORTE_SUCCESS != (rc = orte_rmaps_base_get_target_nodes(&master_node_list, jobid, &total_num_slots))) {
         ORTE_ERROR_LOG(rc);
         OBJ_DESTRUCT(&master_node_list);
         return rc;
@@ -288,7 +288,25 @@ static int orte_rmaps_rr_map(orte_jobid_t jobid)
      */
     for(i=0; i<num_context; i++) {
         app = context[i];
-        num_procs += app->num_procs;
+        if (0 == app->num_procs) {
+            /** if the number of processes wasn't specified, then we know there can be only
+             * one app_context allowed in the launch, and that we are to launch it across
+             * all available slots. We'll double-check the single app_context rule first
+             */
+            if (1 < num_context) {
+                opal_show_help("help-orte-rmaps-rr.txt", "orte-rmaps-rr:multi-apps-and-zero-np",
+                               true, num_context, NULL);
+                ORTE_ERROR_LOG(ORTE_ERR_INVALID_NUM_PROCS);
+                return ORTE_ERR_INVALID_NUM_PROCS;
+            }
+            /** okay, only one app_context - so set the num_procs to equal the number of slots */
+            app->num_procs = total_num_slots;
+            num_procs = total_num_slots;
+            modify_app_context = true;
+        } else {
+        /** have to add up all the num_procs to get the total being launched */
+            num_procs += app->num_procs;
+        }
     }
 
     /* allocate a vpid range for the job */
@@ -350,7 +368,7 @@ static int orte_rmaps_rr_map(orte_jobid_t jobid)
             /** If the user has specified a mapping for this app_context, then we
             * create a working node list that contains only those nodes.
             */
-            if (ORTE_SUCCESS != (rc = orte_rmaps_base_get_mapped_targets(&mapped_node_list, app, &master_node_list))) {
+            if (ORTE_SUCCESS != (rc = orte_rmaps_base_get_mapped_targets(&mapped_node_list, app, &master_node_list, &mapped_num_slots))) {
                 ORTE_ERROR_LOG(rc);
                 goto cleanup;
             }
@@ -380,7 +398,11 @@ static int orte_rmaps_rr_map(orte_jobid_t jobid)
             goto cleanup;
         }
         
-        rank += app->num_procs;
+        if (0 < app->num_procs) {
+            rank += app->num_procs;
+        } else {
+            rank += total_num_slots;
+        }
         
         /** cleanup the mapped_node_list, if necessary */
         if (0 < app->num_map) {
@@ -475,6 +497,14 @@ static int orte_rmaps_rr_map(orte_jobid_t jobid)
     */
     if (ORTE_SUCCESS != (rc = orte_rmaps_base_update_node_usage(&master_node_list))) {
         ORTE_ERROR_LOG(rc);
+        goto cleanup;
+    }
+    
+    /** if the app_context was modified, update that information too */
+    if (modify_app_context) {
+        if (ORTE_SUCCESS != (rc = orte_rmgr_base_put_app_context(jobid, context, 1))) {
+            ORTE_ERROR_LOG(rc);
+        }
     }
     
 

orte/tools/orterun/orterun.c
@@ -91,6 +91,8 @@ static int max_display_aborted = 1;
 static int num_aborted = 0;
 static int num_killed = 0;
 static char **global_mca_env = NULL;
+static bool have_zero_np = false;
+static size_t total_num_apps = 0;
 
 /*
  * setup globals for catching orterun command line options
@@ -104,6 +106,7 @@ struct globals_t {
     bool no_wait_for_job_completion;
     bool by_node;
     bool by_slot;
+    bool no_oversubscribe;
     bool debugger;
     bool no_local_schedule;
     size_t num_procs;
@@ -153,7 +156,7 @@ opal_cmd_line_init_t cmd_line_init[] = {
     { NULL, NULL, NULL, '\0', "n", "n", 1,
       &orterun_globals.num_procs, OPAL_CMD_LINE_TYPE_SIZE_T,
       "Number of processes to run" },
-
+    
     /* Set a hostfile */
     { "rds", "hostfile", "path", '\0', "hostfile", "hostfile", 1,
       NULL, OPAL_CMD_LINE_TYPE_STRING,
@@ -191,6 +194,9 @@ opal_cmd_line_init_t cmd_line_init[] = {
     { NULL, NULL, NULL, '\0', "byslot", "byslot", 0,
       &orterun_globals.by_slot, OPAL_CMD_LINE_TYPE_BOOL,
       "Whether to allocate/map processes round-robin by slot (the default)" },
+    { NULL, NULL, NULL, '\0', "no_oversubscribe", "no_oversubscribe", 0,
+      &orterun_globals.no_oversubscribe, OPAL_CMD_LINE_TYPE_BOOL,
+      "Nodes are not to be oversubscribed, even if the system supports such operation"},
 
     /* mpiexec-like arguments */
     { NULL, NULL, NULL, '\0', "wdir", "wdir", 1,
@@ -758,23 +764,24 @@ static void  signal_forward_callback(int fd, short event, void *arg)
 static int init_globals(void)
 {
     struct globals_t tmp = {
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        0,
-        0,
-        NULL,
-        NULL,
-        NULL,
-        NULL,
-        NULL
+        /* help =           */  false,
+        /* version =        */  false,
+        /* verbose =        */  false,
+        /* quiet =          */  false,
+        /* exit =           */  false,
+        /* no_wait =        */  false,
+        /* by_node =        */  false,
+        /* by_slot =        */  false,
+        /* no_oversub =     */  false,
+        /* debugger =       */  false,
+        /* no_local =       */  false,
+        /* num_procs =      */  0,
+        /* exit_status =    */  0,
+        /* hostfile =       */  NULL,
+        /* env_val =        */  NULL,
+        /* appfile =        */  NULL,
+        /* wdir =           */  NULL,
+        /* path =           */  NULL
     };
 
     /* Only CONSTRUCT things once */
@@ -784,7 +791,7 @@ static int init_globals(void)
         OBJ_CONSTRUCT(&orterun_globals.cond, opal_condition_t);
     }
 
-    /* Reset this every time */
+    /* Reset the other fields every time */
 
     orterun_globals = tmp;
 
@@ -878,6 +885,16 @@ static int parse_globals(int argc, char* argv[])
     else {
         /* Default */
         orterun_globals.by_slot = true;
+    }
+    
+    /** Do we want to disallow oversubscription of nodes? */
+    id = mca_base_param_reg_int_name("rmaps", "base_no_oversubscribe",
+                                     "If nonzero, do not allow oversubscription of processes on nodes. If zero (default), oversubscription is allowed.",
+                                     false, false, 0, &ret);
+    if (orterun_globals.no_oversubscribe) {
+        mca_base_param_set_int(id, (int)true);
+    } else {
+        mca_base_param_set_int(id, (int)false);
     }
 
     /* Do we want to allow MPI applications on the same node as
@@ -1348,16 +1365,32 @@ static int create_app(int argc, char* argv[], orte_app_context_t **app_ptr,
 
     app->num_procs = orterun_globals.num_procs;
 
-    /* If the user didn't specify a num procs or any map data, then we
-       really have no idea what the launch... */
-
-    if (app->num_procs == 0 && !map_data) {
-        opal_show_help("help-orterun.txt", "orterun:num-procs-unspecified",
-                       true, orterun_basename, app->argv[0]);
-        rc = ORTE_ERR_BAD_PARAM;
-        goto cleanup;
+    /* If the user didn't specify the number of processes to run, then we
+       default to launching an app process using every slot. We can't do
+       anything about that here - we leave it to the RMAPS framework's
+       components to note this and deal with it later.
+        
+       HOWEVER, we ONLY support this mode of operation if the number of
+       app_contexts is equal to ONE. If the user provides multiple applications,
+       we simply must have more information - in this case, generate an
+       error.
+    */
+    if (app->num_procs == 0) {
+        have_zero_np = true;  /** flag that we have a zero_np situation */
     }
-
+    if (0 < total_num_apps && have_zero_np) {
+        /** we have more than one app and a zero_np - that's no good.
+         * note that we have to do this as a two step logic check since
+         * the user may fail to specify num_procs for the first app, but
+         * then give us another application.
+         */
+        opal_show_help("help-orterun.txt", "orterun:multi-apps-and-zero-np",
+                       true, orterun_basename, NULL);
+        return ORTE_ERR_FATAL;
+    }
+    
+    total_num_apps++;
+    
     /* Do not try to find argv[0] here -- the starter is responsible
        for that because it may not be relevant to try to find it on
        the node where orterun is executing.  So just strdup() argv[0]

commit 07b3cf0dc7854381eed2101eac6a9cde328be07e

It seems that MPI_Comm_spawn{_multiple} has been broken since r10708
By reverting this file (changeset from commit r10708) to its previous
version fixes the problem.

This should be moved to the v1.1 branch where it is also broken.

orte/mca/rmgr/urm/rmgr_urm.c
@@ -150,6 +150,14 @@ static int orte_rmgr_urm_create(
         return rc;
     }
 
+     /* setup the launch system's stage gate counters and subscriptions */
+     if (ORTE_SUCCESS !=
+         (rc = orte_rmgr_base_proc_stage_gate_init(*jobid))) {
+         ORTE_ERROR_LOG(rc);
+         return rc;
+     }
+          
+
     return ORTE_SUCCESS;
 }
 
@@ -459,13 +467,6 @@ static int orte_rmgr_urm_spawn(
         return rc;
     }
      
-     /* setup the launch system's stage gate counters and subscriptions */
-     if (ORTE_SUCCESS !=
-         (rc = orte_rmgr_base_proc_stage_gate_init(*jobid))) {
-         ORTE_ERROR_LOG(rc);
-         return rc;
-     }
-          
     /** setup the subscription so we can complete the wireup when all processes reach LAUNCHED */
     rc = orte_rmgr_base_proc_stage_gate_subscribe(*jobid, orte_rmgr_urm_wireup_callback, NULL, ORTE_PROC_STATE_LAUNCHED);
     if(ORTE_SUCCESS != rc) {