aaf31c6adef93c9d9de13e35044a110afebc3fdd
Brian Barrett <brbarret@open-mpi.org>
2006-07-27 02:56:02 +0000

    * Make the backtrace printing functionality a framework * Copy Linux and Solaris backtrace support from util/stacktrace.c * Added backtrace support for Mac OS X.
    This commit was SVN r11023.

---
+++ opal/mca/backtrace/printstack/backtrace_printstack_component.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2006 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "opal_config.h"
+
+#include "opal/mca/backtrace/backtrace.h"
+
+const opal_backtrace_base_component_1_0_0_t mca_backtrace_printstack_component = {
+    /* First, the mca_component_t struct containing meta information
+       about the component itself */
+    {
+        /* Indicate that we are a backtrace v1.0.0 component (which also
+           implies a specific MCA version) */
+        OPAL_BACKTRACE_BASE_VERSION_1_0_0,
+
+        /* Component name and version */
+        "printstack",
+        OPAL_MAJOR_VERSION,
+        OPAL_MINOR_VERSION,
+        OPAL_RELEASE_VERSION,
+
+        /* Component open and close functions */
+        NULL,
+        NULL
+    },
+
+    /* Next the MCA v1.0.0 component meta data */
+    {
+        /* Whether the component is checkpointable or not */
+        true
+    },
+};
