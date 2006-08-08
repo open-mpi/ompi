71839a0fdb7df852101011f108ee9a69942aee76
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2006-08-08 13:01:17 +0000

    - Convert MPI_Init(NULL, NULL) to MPI_Init(&argc, &argv) because   apparently some people are using these examples to test other MPI's,   and some of those MPI's don't handle MPI_Init(NULL, NULL) properly.   :-) - Add comments and a print statement to the f90 example to make it   like the others.

--- examples/ring_c.c
+++ examples/ring_c.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     /* Start up MPI */
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
  
