13dd02a9f44f818bc32afb6509af03b0570a73f8
Jeff Squyres <jsquyres@cisco.com>
2006-08-08 13:01:17 +0000

    - Convert MPI_Init(NULL, NULL) to MPI_Init(&argc, &argv) because   apparently some people are using these examples to test other MPI's,   and some of those MPI's don't handle MPI_Init(NULL, NULL) properly.   :-) - Add comments and a print statement to the f90 example to make it   like the others.
    This commit was SVN r11123.

--- examples/hello_c.c
+++ examples/hello_c.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 {
     int rank, size;
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     printf("Hello, world, I am %d of %d\n", rank, size);
