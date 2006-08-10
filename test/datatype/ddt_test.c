76cd49f5f3df8ccf3771abfcf4b89d93f3d553b8
Terry Dontje <tdd@open-mpi-git-mirror.example.com>
2006-08-10 14:25:45 +0000

    Removed include of stdbool.h since it was not being used and was causing the Sun compilers to abort when make check was done.

--- test/datatype/ddt_test.c
+++ test/datatype/ddt_test.c
@@ -10,6 +10,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Sun Microsystems Inc. All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -18,7 +19,6 @@
  */
 
 #include "ompi_config.h"
-#include <stdbool.h>
 #include "ompi/datatype/datatype.h"
 #include "ompi/datatype/datatype_internal.h"
 #include "ompi/datatype/convertor.h"
