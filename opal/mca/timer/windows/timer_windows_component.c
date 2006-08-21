328be72072eff0e3e96f2107a37202a6b013bc41
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2006-08-21 21:28:56 +0000

    Support C++ compiler.

--- opal/mca/timer/windows/timer_windows_component.c
+++ opal/mca/timer/windows/timer_windows_component.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
@@ -19,7 +19,7 @@
 #include "opal_config.h"
 
 #include "opal/mca/timer/timer.h"
-#include "opal/mca/timer/windows/timer_windows.h"
+#include "opal/mca/timer/windows/timer_windows_component.h"
 #include "opal/constants.h"
 
 opal_timer_t opal_timer_windows_freq;
