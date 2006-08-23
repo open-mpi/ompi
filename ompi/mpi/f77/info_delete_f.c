b51ec46b5d117f1107b563b1c6fab7deea3c90ce
Dan Lacher <dan.lacher@sun.com>
2006-08-23 14:17:14 +0000

    Ticket: #302
    Move Fortran string fixes to v1.2
    
    Submitted by: Jeff Squyres
    Reviewed by: Rolf Vandevaart
    
    This commit was SVN r11365.

--- ompi/mpi/f77/info_delete_f.c
+++ ompi/mpi/f77/info_delete_f.c
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -19,6 +20,9 @@
 #include "ompi_config.h"
 
 #include "ompi/mpi/f77/bindings.h"
+#include "ompi/constants.h"
+#include "ompi/communicator/communicator.h"
+#include "ompi/mpi/f77/strings.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
 #pragma weak PMPI_INFO_DELETE = mpi_info_delete_f
@@ -27,12 +31,12 @@
 #pragma weak pmpi_info_delete__ = mpi_info_delete_f
 #elif OMPI_PROFILE_LAYER
 OMPI_GENERATE_F77_BINDINGS (PMPI_INFO_DELETE,
-                           pmpi_info_delete,
-                           pmpi_info_delete_,
-                           pmpi_info_delete__,
-                           pmpi_info_delete_f,
-                           (MPI_Fint *info, char *key, MPI_Fint *ierr),
-                           (info, key, ierr) )
+                            pmpi_info_delete,
+                            pmpi_info_delete_,
+                            pmpi_info_delete__,
+                            pmpi_info_delete_f,
+                            (MPI_Fint *info, char *key, MPI_Fint *ierr, int key_len),
+                            (info, key, ierr, key_len) )
 #endif
 
 #if OMPI_HAVE_WEAK_SYMBOLS
@@ -44,12 +48,12 @@ OMPI_GENERATE_F77_BINDINGS (PMPI_INFO_DELETE,
 
 #if ! OMPI_HAVE_WEAK_SYMBOLS && ! OMPI_PROFILE_LAYER
 OMPI_GENERATE_F77_BINDINGS (MPI_INFO_DELETE,
-                           mpi_info_delete,
-                           mpi_info_delete_,
-                           mpi_info_delete__,
-                           mpi_info_delete_f,
-                           (MPI_Fint *info, char *key, MPI_Fint *ierr),
-                           (info, key, ierr) )
+                            mpi_info_delete,
+                            mpi_info_delete_,
+                            mpi_info_delete__,
+                            mpi_info_delete_f,
+                            (MPI_Fint *info, char *key, MPI_Fint *ierr, int key_len),
+                            (info, key, ierr, key_len) )
 #endif
 
 
@@ -57,11 +61,26 @@ OMPI_GENERATE_F77_BINDINGS (MPI_INFO_DELETE,
 #include "ompi/mpi/f77/profile/defines.h"
 #endif
 
-void mpi_info_delete_f(MPI_Fint *info, char *key, MPI_Fint *ierr)
-{
-    MPI_Info c_info;
+static const char FUNC_NAME[] = "MPI_INFO_DELETE";
 
+/* Note that the key_len parameter is silently added by the Fortran
+   compiler, and will be filled in with the actual length of the
+   character array from the caller.  Hence, it's the max length of the
+   string that we can use. */
+
+void mpi_info_delete_f(MPI_Fint *info, char *key, MPI_Fint *ierr, int key_len)
+{
+    int c_err, ret;
+    MPI_Info c_info;
+    char *c_key;
+
+    if (OMPI_SUCCESS != (ret = ompi_fortran_string_f2c(key, key_len, &c_key))) {
+        c_err = OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, ret, FUNC_NAME);
+        *ierr = OMPI_INT_2_FINT(c_err);
+        return;
+    }
     c_info = MPI_Info_f2c(*info);
     
-    *ierr = OMPI_INT_2_FINT(MPI_Info_delete(c_info, key));
+    *ierr = OMPI_INT_2_FINT(MPI_Info_delete(c_info, c_key));
+    free(c_key);
 }
--- ompi/mpi/f77/info_set_f.c
+++ ompi/mpi/f77/info_set_f.c
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -19,6 +20,9 @@
 #include "ompi_config.h"
 
 #include "ompi/mpi/f77/bindings.h"
+#include "ompi/constants.h"
+#include "ompi/communicator/communicator.h"
+#include "ompi/mpi/f77/strings.h"
 
 #if OMPI_HAVE_WEAK_SYMBOLS && OMPI_PROFILE_LAYER
 #pragma weak PMPI_INFO_SET = mpi_info_set_f
@@ -27,12 +31,12 @@
 #pragma weak pmpi_info_set__ = mpi_info_set_f
 #elif OMPI_PROFILE_LAYER
 OMPI_GENERATE_F77_BINDINGS (PMPI_INFO_SET,
-                           pmpi_info_set,
-                           pmpi_info_set_,
-                           pmpi_info_set__,
-                           pmpi_info_set_f,
-                           (MPI_Fint *info, char *key, char *value, MPI_Fint *ierr),
-                           (info, key, value, ierr) )
+                            pmpi_info_set,
+                            pmpi_info_set_,
+                            pmpi_info_set__,
+                            pmpi_info_set_f,
+                            (MPI_Fint *info, char *key, char *value, MPI_Fint *ierr, int key_len, int value_len),
+                            (info, key, value, ierr, key_len, value_len) )
 #endif
 
 #if OMPI_HAVE_WEAK_SYMBOLS
@@ -44,12 +48,12 @@ OMPI_GENERATE_F77_BINDINGS (PMPI_INFO_SET,
 
 #if ! OMPI_HAVE_WEAK_SYMBOLS && ! OMPI_PROFILE_LAYER
 OMPI_GENERATE_F77_BINDINGS (MPI_INFO_SET,
-                           mpi_info_set,
-                           mpi_info_set_,
-                           mpi_info_set__,
-                           mpi_info_set_f,
-                           (MPI_Fint *info, char *key, char *value, MPI_Fint *ierr),
-                           (info, key, value, ierr) )
+                            mpi_info_set,
+                            mpi_info_set_,
+                            mpi_info_set__,
+                            mpi_info_set_f,
+                            (MPI_Fint *info, char *key, char *value, MPI_Fint *ierr, int key_len, int value_len),
+                            (info, key, value, ierr, key_len, value_len) )
 #endif
 
 
@@ -57,11 +61,34 @@ OMPI_GENERATE_F77_BINDINGS (MPI_INFO_SET,
 #include "ompi/mpi/f77/profile/defines.h"
 #endif
 
-void mpi_info_set_f(MPI_Fint *info, char *key, char *value, MPI_Fint *ierr)
-{
-    MPI_Info c_info;
+static const char FUNC_NAME[] = "MPI_INFO_SET";
 
+/* Note that the key_len and value_len parameters are silently added
+   by the Fortran compiler, and will be filled in with the actual
+   length of the character array from the caller.  Hence, it's the max
+   length of the string that we can use. */
+
+void mpi_info_set_f(MPI_Fint *info, char *key, char *value, MPI_Fint *ierr,
+                    int key_len, int value_len)
+{
+    int ret, c_err;
+    MPI_Info c_info;
+    char *c_key = NULL, *c_value = NULL;
+
+    if (OMPI_SUCCESS != (ret = ompi_fortran_string_f2c(key, key_len, &c_key)) ||
+        OMPI_SUCCESS != (ret = ompi_fortran_string_f2c(value, value_len, 
+                                                       &c_value))) {
+        c_err = OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, ret, FUNC_NAME);
+        *ierr = OMPI_INT_2_FINT(c_err);
+        if (NULL != c_key) {
+            free(c_key);
+        }
+        return;
+    }
     c_info = MPI_Info_f2c(*info);
 
-    *ierr = OMPI_INT_2_FINT(MPI_Info_set(c_info, key, value));
+    *ierr = OMPI_INT_2_FINT(MPI_Info_set(c_info, c_key, c_value));
+
+    free(c_key);
+    free(c_value);
 }
