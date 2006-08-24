e04032ca2fab0a863cacd353c259ce7c40e81b4a
George Bosilca <bosilca@icl.utk.edu>
2006-08-24 16:18:42 +0000

    Correct a comment and protect the usage of the environ variable against Windows.
    This commit was SVN r11397.

--- orte/tools/orted/orted.c
+++ orte/tools/orted/orted.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -68,7 +68,9 @@
 
 #include "orte/tools/orted/orted.h"
 
-extern char**environ;
+#if !defined(__WINDOWS__)
+extern char** environ;
+#endif   /* !defined(__WINDOWS__) */
 
 orted_globals_t orted_globals;
 
