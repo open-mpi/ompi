open-mpi/ompi

commit f52034d95e03bc55f103ee4be2f5a43278b38064

And ORTE is ready for prime-time. All Windows tricks are in: - use the OPAL functions for PATH and environment variables - make all headers C++ friendly - no unamed structures - no implicit cast.
Plus a full implementation for the orte_wait functions.


git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@11347 63e3feb5-37d5-0310-a306-e8a459e722fe

orte/mca/pls/rsh/pls_rsh_component.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
@@ -40,8 +40,9 @@
 #include "opal/mca/base/mca_base_param.h"
 #include "orte/mca/rml/rml.h"
 
+#if !defined(__WINDOWS__)
 extern char **environ;
-
+#endif  /* !defined(__WINDOWS__) */
 
 /*
  * Local function
@@ -118,7 +119,7 @@ int orte_pls_rsh_component_open(void)
     mca_base_param_reg_int(c, "debug",
                            "Whether or not to enable debugging output for the rsh pls component (0 or 1)",
                            false, false, false, &tmp);
-    mca_pls_rsh_component.debug = tmp;
+    mca_pls_rsh_component.debug = (tmp != 0 ? true : false);
     mca_base_param_reg_int(c, "num_concurrent",
                            "How many pls_rsh_agent instances to invoke concurrently (must be > 0)",
                            false, false, 128, &tmp);
@@ -127,12 +128,12 @@ int orte_pls_rsh_component_open(void)
                        true, tmp);
         tmp = 1;
     }
-    mca_pls_rsh_component.num_concurrent = tmp;
+    mca_pls_rsh_component.num_concurrent = (tmp != 0 ? true : false);
     if (mca_pls_rsh_component.debug == 0) {
         mca_base_param_reg_int_name("orte", "debug",
                                     "Whether or not to enable debugging output for all ORTE components (0 or 1)",
                                     false, false, false, &tmp);
-        mca_pls_rsh_component.debug = tmp;
+        mca_pls_rsh_component.debug = (tmp != 0 ? true : false);
     }
 
     mca_base_param_reg_string(c, "orted",
@@ -150,11 +151,11 @@ int orte_pls_rsh_component_open(void)
     mca_base_param_reg_int(c, "reap",
                            "If set to 1, wait for all the processes to complete before exiting.  Otherwise, quit immediately -- without waiting for confirmation that all other processes in the job have completed.",
                            false, false, 1, &tmp);
-    mca_pls_rsh_component.reap = tmp;
+    mca_pls_rsh_component.reap = (tmp != 0 ? true : false);
     mca_base_param_reg_int(c, "assume_same_shell",
                            "If set to 1, assume that the shell on the remote node is the same as the shell on the local node.  Otherwise, probe for what the remote shell.",
                            false, false, 1, &tmp);
-    mca_pls_rsh_component.assume_same_shell = tmp;
+    mca_pls_rsh_component.assume_same_shell = (tmp != 0 ? true : false);
 
     mca_base_param_reg_string(c, "agent",
                               "The command used to launch executables on remote nodes (typically either \"ssh\" or \"rsh\")",
@@ -165,11 +166,14 @@ int orte_pls_rsh_component_open(void)
 }
 
 
+#if !defined(__WINDOWS__)
+extern char **environ;
+#endif  /* !defined(__WINDOWS__) */
+
 orte_pls_base_module_t *orte_pls_rsh_component_init(int *priority)
 {
     char *bname;
     size_t i;
-    extern char **environ;
 
     /* Take the string that was given to us by the pla_rsh_agent MCA
        param and search for it */

orte/mca/pls/rsh/pls_rsh_module.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -58,7 +58,7 @@
 #include "opal/install_dirs.h"
 #include "opal/mca/base/mca_base_param.h"
 #include "opal/util/if.h"
-#include "opal/util/if.h"
+#include "opal/util/os_path.h"
 #include "opal/util/path.h"
 #include "opal/event/event.h"
 #include "opal/util/show_help.h"
@@ -83,8 +83,9 @@
 #include "orte/mca/pls/rsh/pls_rsh.h"
 #include "orte/util/sys_info.h"
 
+#if !defined(__WINDOWS__)
 extern char **environ;
-
+#endif  /* !defined(__WINDOWS__) */
 
 #if OMPI_HAVE_POSIX_THREADS && OMPI_THREADS_HAVE_DIFFERENT_PIDS && OMPI_ENABLE_PROGRESS_THREADS
 static int orte_pls_rsh_launch_threaded(orte_jobid_t jobid);
@@ -764,8 +765,7 @@ int orte_pls_rsh_launch(orte_jobid_t jobid)
                         }
                     } else {
                         if (NULL != prefix_dir) {
-                            asprintf(&exec_path, "%s/%s/orted",
-                                     prefix_dir, bin_base);
+                            exec_path = opal_os_path( false, prefix_dir, bin_base, "orted", NULL );
                         }
                         /* If we yet did not fill up the execpath, do so now */
                         if (NULL == exec_path) {
@@ -784,12 +784,13 @@ int orte_pls_rsh_launch(orte_jobid_t jobid)
                         char *oldenv, *newenv;
 
                         /* Reset PATH */
+                        newenv = opal_os_path( false, prefix_dir, bin_base, NULL );
                         oldenv = getenv("PATH");
                         if (NULL != oldenv) {
-                            asprintf(&newenv, "%s/%s:%s", prefix_dir,
-                                     bin_base, oldenv);
-                        } else {
-                            asprintf(&newenv, "%s/%s", prefix_dir, bin_base);
+                            char *temp;
+                            asprintf(&temp, "%s:%s", newenv, oldenv );
+                            free( newenv );
+                            newenv = temp;
                         }
                         opal_setenv("PATH", newenv, true, &environ);
                         if (mca_pls_rsh_component.debug) {
@@ -798,12 +799,13 @@ int orte_pls_rsh_launch(orte_jobid_t jobid)
                         free(newenv);
 
                         /* Reset LD_LIBRARY_PATH */
+                        newenv = opal_os_path( false, prefix_dir, lib_base, NULL );
                         oldenv = getenv("LD_LIBRARY_PATH");
                         if (NULL != oldenv) {
-                            asprintf(&newenv, "%s/%s:%s", prefix_dir,
-                                     lib_base, oldenv);
-                        } else {
-                            asprintf(&newenv, "%s/%s", prefix_dir, lib_base);
+                            char* temp;
+                            asprintf(&temp, "%s:%s", newenv, lib_base, oldenv, NULL);
+                            free(newenv);
+                            newenv = temp;
                         }
                         opal_setenv("LD_LIBRARY_PATH", newenv, true, &environ);
                         if (mca_pls_rsh_component.debug) {

commit 160120b4c570f27373cd873e83e941a7e194801b

Fix a cut-n-paste error that causes the 'num_concurrent' to be set to 1 or 0 instead of the user defined number or default (128).
This caused the PLS to deadlock when using '--debug-daemons' with
more than 2 processes. :(

svn blame says that it was broken in r11347

It is *not* a problem on v1.1 or v1.2 branches.

Bug spotted by Tim Mattox and myself.

This commit was SVN r11575.

The following SVN revision numbers were found above:
  r11347 --> open-mpi/ompi@f52c10d18e701764980a1fc5548a500cb2ebe5c7

orte/mca/pls/rsh/pls_rsh_component.c
@@ -128,7 +128,8 @@ int orte_pls_rsh_component_open(void)
                        true, tmp);
         tmp = 1;
     }
-    mca_pls_rsh_component.num_concurrent = (tmp != 0 ? true : false);
+    mca_pls_rsh_component.num_concurrent = tmp;
+
     if (mca_pls_rsh_component.debug == 0) {
         mca_base_param_reg_int_name("orte", "debug",
                                     "Whether or not to enable debugging output for all ORTE components (0 or 1)",

orte/mca/pls/rsh/pls_rsh_module.c
@@ -961,6 +961,17 @@ int orte_pls_rsh_launch(orte_jobid_t jobid)
                 rsh_daemon_info_t *daemon_info;
 
                 OPAL_THREAD_LOCK(&mca_pls_rsh_component.lock);
+                /* JJH Bug:
+                 * If we are in '--debug-daemons' we keep the ssh connection 
+                 * alive for the span of the run. If we use this option 
+                 * AND we launch on more than "num_concurrent" machines
+                 * then we will deadlock. No connections are terminated 
+                 * until the job is complete, no job is started
+                 * since all the orteds are waiting for all the others
+                 * to come online, and the others ore not launched because
+                 * we are waiting on those that have started to terminate
+                 * their ssh tunnels. :(
+                 */
                 if (mca_pls_rsh_component.num_children++ >=
                     mca_pls_rsh_component.num_concurrent) {
                     opal_condition_wait(&mca_pls_rsh_component.cond, &mca_pls_rsh_component.lock);