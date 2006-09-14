4718dd18f28efa4441d07762cf3fdba9db1b118e
Galen Shipman <gshipman@open-mpi-git-mirror.example.com>
2006-09-14 15:27:17 +0000

    Push a generated "key" out to all the processes. This is necessary for some interconnect wireup in which all processes must agree on a "key" to initialize the interconnect with.

---
+++ orte/util/pre_condition_transports.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+/** @file:
+ *
+ * Pre-condition transports
+ *
+ *
+ */
+
+#ifndef _ORTE_PRE_CONDITION_TRANSPORTS_H_
+#define _ORTE_PRE_CONDITION_TRANSPORTS_H_
+
+#define ORTE_TRANSPORT_KEY_FMT "%016x-%016x"
+#define ORTE_TRANSPORT_KEY_LEN 33 /* 16 + sep + 16 */
+
+#include "orte_config.h"
+
+#include "orte/mca/rmgr/rmgr_types.h"
+
+int orte_pre_condition_transports(orte_app_context_t **app_context, size_t num_context);
+
+#if defined(c_plusplus) || defined(__cplusplus)
+}
+#endif
+
+#endif
