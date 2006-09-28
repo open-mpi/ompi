a3eb6788107629c68c3539dc5ae64cc66c63252b
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2006-09-28 23:57:49 +0000

    We don't use the send and receive request destructor.

--- ompi/mca/pml/ob1/pml_ob1_recvreq.c
+++ ompi/mca/pml/ob1/pml_ob1_recvreq.c
@@ -122,16 +122,11 @@ static void mca_pml_ob1_recv_request_construct(mca_pml_ob1_recv_request_t* reque
     request->req_rdma_cnt = 0;
 }
 
-static void mca_pml_ob1_recv_request_destruct(mca_pml_ob1_recv_request_t* request)
-{
-}
-
-
 OBJ_CLASS_INSTANCE(
     mca_pml_ob1_recv_request_t,
     mca_pml_base_recv_request_t,
     mca_pml_ob1_recv_request_construct,
-    mca_pml_ob1_recv_request_destruct);
+    NULL);
 
 
 /*
