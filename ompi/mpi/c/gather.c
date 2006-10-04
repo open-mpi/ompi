e08c6e81f5e85b1cc32056af7dec3b43852b83bc
Jeff Squyres <jsquyres@cisco.com>
2006-10-04 22:36:01 +0000

    * Fixes trac:338: Only look at root-significant values at the root (e.g.,    recvbuf in MPI_GATHER).  * Minor style updates (constants on the left of == and !=)  * Fix a minor buglet that crept in r11904: had a recvbuf where it    should have been recvcount.  Thankfully, this would have only    affected erroneous programs.  ;-)
    This commit was SVN r11980.
    
    The following SVN revision numbers were found above:
      r11904 --> open-mpi/ompi@17539dc154ab3a6f0cd64d66c499c3aad675e54d
    
    The following Trac tickets were found above:
      Ticket 338 --> https://svn.open-mpi.org/trac/ompi/ticket/338

--- ompi/mpi/c/gather.c
+++ ompi/mpi/c/gather.c
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -45,7 +46,7 @@ int MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
             return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_COMM, 
                                           FUNC_NAME);
 	} else if ((ompi_comm_rank(comm) != root && MPI_IN_PLACE == sendbuf) ||
-                   MPI_IN_PLACE == recvbuf) {
+                   (ompi_comm_rank(comm) == root && MPI_IN_PLACE == recvbuf)) {
             return OMPI_ERRHANDLER_INVOKE(comm, MPI_ERR_ARG, FUNC_NAME);
         }
 
@@ -118,7 +119,7 @@ int MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
          (ompi_comm_rank(comm) != root ||
           (ompi_comm_rank(comm) == root && MPI_IN_PLACE != sendbuf))) ||
         (ompi_comm_rank(comm) == root && MPI_IN_PLACE == sendbuf && 
-         0 == recvbuf)) {
+         0 == recvcount)) {
         return MPI_SUCCESS;
     }
 
