ec0bb9ffda628f94bb56e3785167f5a1f44af3de
Ralph Castain <rhc@open-mpi.org>
2006-10-20 18:05:16 +0000

    Fix the bookmark system - we now have children being correctly spawned where they should!
    Also, I am no longer seeing any issue with the child job spawning its own daemons - this appears to be fixed. We still don't reuse the existing daemons, however, but that will come.
    
    This commit was SVN r12229.

--- orte/test/mpi/simple_spawn.c
+++ orte/test/mpi/simple_spawn.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/types.h>
+
 #include <mpi.h>
 
 int main(int argc, char* argv[])
@@ -7,12 +9,14 @@ int main(int argc, char* argv[])
     MPI_Comm parent, child;
     int rank, size;
     char hostname[512];
+    pid_t pid;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_get_parent(&parent);
     /* If we get COMM_NULL back, then we're the parent */
     if (MPI_COMM_NULL == parent) {
-        printf("Parent about to spawn!\n");
+        pid = getpid();
+        printf("Parent [pid %ld] about to spawn!\n", (long)pid);
         MPI_Comm_spawn(argv[0], MPI_ARGV_NULL, 1, MPI_INFO_NULL, 
                        0, MPI_COMM_WORLD, &child, MPI_ERRCODES_IGNORE);
         printf("Parent done with spawn\n");
@@ -27,7 +31,8 @@ int main(int argc, char* argv[])
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         gethostname(hostname, 512);
-        printf("Hello from the child %d of %d on host %s\n", rank, size, hostname);
+        pid = getpid();
+        printf("Hello from the child %d of %d on host %s pid %ld\n", rank, size, hostname, (long)pid);
         MPI_Recv(&msg, 1, MPI_INT, 0, 1, parent, MPI_STATUS_IGNORE);
         printf("Child %d received msg: %d\n", rank, msg);
         MPI_Comm_disconnect(&parent);
