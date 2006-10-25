7124fbb676633a76a712d24e77343bd6ec9f2f03
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2006-10-25 14:33:06 +0000

    First step on supporting full external32 conversion on both operations pack and unpack.

--- ompi/mpi/c/pack_external_size.c
+++ ompi/mpi/c/pack_external_size.c
@@ -52,7 +52,9 @@ int MPI_Pack_external_size(char *datarep, int incount,
 
     /* the resulting convertor will be set to the position ZERO */
     ompi_convertor_copy_and_prepare_for_send( ompi_mpi_external32_convertor,
-                                              datatype, incount, NULL, 0, &local_convertor );
+                                              datatype, incount, NULL,
+                                              CONVERTOR_SEND_CONVERSION,
+                                              &local_convertor );
 
     ompi_convertor_get_packed_size( &local_convertor, &length );
     *size = (MPI_Aint)length;
