b53a31390487c1a852766caed396ae7ef06512b5
George Bosilca <bosilca@icl.utk.edu>
2006-11-08 16:49:34 +0000

    These functions dont have to be exported. The outside world access them only via the pointer in the convertor structure.
    This commit was SVN r12492.

--- ompi/datatype/datatype_prototypes.h
+++ ompi/datatype/datatype_prototypes.h
@@ -15,51 +15,51 @@
 
 #include "ompi_config.h"
 
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_pack_homogeneous_contig( ompi_convertor_t* pConv,
                           struct iovec* iov, uint32_t* out_size,
                           size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_pack_homogeneous_contig_checksum( ompi_convertor_t* pConv,
                                    struct iovec* iov, uint32_t* out_size,
                                    size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_pack_homogeneous_contig_with_gaps( ompi_convertor_t* pConv,
                                     struct iovec* iov, uint32_t* out_size,
                                     size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_pack_homogeneous_contig_with_gaps_checksum( ompi_convertor_t* pConv,
                                              struct iovec* iov, uint32_t* out_size,
                                              size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_generic_simple_pack( ompi_convertor_t* pConvertor,
                           struct iovec* iov, uint32_t* out_size,
                           size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_generic_simple_pack_checksum( ompi_convertor_t* pConvertor,
                                    struct iovec* iov, uint32_t* out_size,
                                    size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_unpack_general( ompi_convertor_t* pConvertor,
                      struct iovec* iov, uint32_t* out_size,
                      size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_unpack_general_checksum( ompi_convertor_t* pConvertor,
                               struct iovec* iov, uint32_t* out_size,
                               size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_unpack_homogeneous_contig( ompi_convertor_t* pConv,
                                 struct iovec* iov, uint32_t* out_size,
                                 size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_unpack_homogeneous_contig_checksum( ompi_convertor_t* pConv,
                                          struct iovec* iov, uint32_t* out_size,
                                          size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_generic_simple_unpack( ompi_convertor_t* pConvertor,
                             struct iovec* iov, uint32_t* out_size,
                             size_t* max_data );
-OMPI_DECLSPEC int32_t
+int32_t
 ompi_generic_simple_unpack_checksum( ompi_convertor_t* pConvertor,
                                      struct iovec* iov, uint32_t* out_size,
                                      size_t* max_data );
