597598b712491d397920f9ad1df017213aa72791
Brian Barrett <brbarret@open-mpi.org>
2006-12-15 17:46:53 +0000

    Move the req_mtl structure back to the end of each of the structures in the CM PML.  The req_mtl structure is cast into a mtl_*_request_structure for each MTL, which is larger than the req_mtl itself.  The cast will cause the *_request to overwrite parts of the heavy requests if the req_mtl isn't the *LAST* thing on each structure (hence the comment).  This was moved as an optimization at some point, which caused buffer sends to fail...
    Refs trac:669
    
    This commit was SVN r12871.
    
    The following Trac tickets were found above:
      Ticket 669 --> https://svn.open-mpi.org/trac/ompi/ticket/669

--- ompi/runtime/ompi_mpi_preconnect.c
+++ ompi/runtime/ompi_mpi_preconnect.c
@@ -29,35 +29,42 @@ int ompi_init_do_preconnect(void)
 {
     int comm_size = ompi_comm_size(MPI_COMM_WORLD);
     int my_rank =  ompi_comm_rank(MPI_COMM_WORLD);
-    int i, j, ret;
-    struct ompi_request_t **requests; 
-
-    requests = (ompi_request_t**)malloc(comm_size * sizeof(struct ompi_request_t *));
-    if (NULL == requests) {
-        return OMPI_ERR_OUT_OF_RESOURCE;
-    }
-
-    for (i = j = 0; i < comm_size; ++i) {
-        if (i == my_rank) {
-            continue;
-        } else if (my_rank < i) {
-            ret = MCA_PML_CALL(isend(MPI_BOTTOM, 0, MPI_BYTE,
-                                     i, 1,
-                                     MCA_PML_BASE_SEND_STANDARD,
-                                     MPI_COMM_WORLD,
-                                     &requests[j++]));
+    int i, ret;
+    int next,prev;
+    struct ompi_request_t * requests[2];
+    if(comm_size == 2) {
+        if(my_rank){ 
+            ret = MCA_PML_CALL(send(MPI_BOTTOM, 0, MPI_BYTE,
+                                     0, 1,
+                                     MCA_PML_BASE_SEND_SYNCHRONOUS,
+                                     MPI_COMM_WORLD));
         } else {
-            ret = MCA_PML_CALL(irecv(MPI_BOTTOM,0, MPI_BYTE, i, 
-                                     1, MPI_COMM_WORLD, 
-                                     &requests[j++]));
+            ret = MCA_PML_CALL(recv(MPI_BOTTOM,0, MPI_BYTE, 1, 
+                                    1, MPI_COMM_WORLD, 
+                                    MPI_STATUS_IGNORE));
+        }   
+    } else { 
+        for (i = 1; i < comm_size/2; ++i) {
+            next = (my_rank + i) % comm_size;
+            prev = (my_rank - i + comm_size) % comm_size;
+            ret = MCA_PML_CALL(irecv(MPI_BOTTOM,0, MPI_BYTE,
+                                     prev, 1,
+                                     MPI_COMM_WORLD, 
+                                     &requests[0]));
+            
+            ret = MCA_PML_CALL(isend(MPI_BOTTOM, 0, MPI_BYTE,
+                                     next, 1,
+                                     MCA_PML_BASE_SEND_STANDARD,
+                                     MPI_COMM_WORLD, 
+                                     &requests[1]));
+            ret = ompi_request_wait_all(2, requests, MPI_STATUSES_IGNORE);
+            
         }
         if (OMPI_SUCCESS != ret) {
             return ret;
         }
     }
-    ret = ompi_request_wait_all(j, requests, MPI_STATUSES_IGNORE);
-    free(requests);
-
+    
     return ret;
 }
     
