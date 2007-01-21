3f397f38480a50c374e5e3e423af1286ec3fb843
Rainer Keller <rainer.keller@hlrs.de>
2007-01-21 13:00:23 +0000

    - Do not convert the cart_comm -- its intent is out.
    This commit was SVN r13224.

--- ompi/mpi/f77/cart_create_f.c
+++ ompi/mpi/f77/cart_create_f.c
@@ -5,7 +5,7 @@
  * Copyright (c) 2004-2005 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
- * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ * Copyright (c) 2004-2007 High Performance Computing Center Stuttgart, 
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
@@ -67,7 +67,6 @@ void mpi_cart_create_f(MPI_Fint *old_comm, MPI_Fint *ndims, MPI_Fint *dims,
     OMPI_LOGICAL_ARRAY_NAME_DECL(periods);
 
     c_comm1 = MPI_Comm_f2c(*old_comm);
-    c_comm2 = MPI_Comm_f2c(*comm_cart);
 
     size = OMPI_FINT_2_INT(*ndims);
     OMPI_ARRAY_FINT_2_INT(dims, size);
