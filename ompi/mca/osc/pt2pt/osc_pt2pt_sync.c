3a22799ca329954029d59ae1901fd823b24e3413
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2007-02-25 01:03:19 +0000

    Make some debugging output only looked at when debugging is enabled

--- ompi/mca/osc/pt2pt/osc_pt2pt_sync.c
+++ ompi/mca/osc/pt2pt/osc_pt2pt_sync.c
@@ -143,10 +143,10 @@ ompi_osc_pt2pt_module_fence(int assert, ompi_win_t *win)
         OPAL_THREAD_ADD32(&(P2P_MODULE(win)->p2p_num_pending_out), 
                           opal_list_get_size(&(P2P_MODULE(win)->p2p_copy_pending_sendreqs)));
 
-        opal_output_verbose(50, ompi_osc_base_output,
-                            "fence: waiting on %d in and %d out",
-                            P2P_MODULE(win)->p2p_num_pending_in,
-                            P2P_MODULE(win)->p2p_num_pending_out);
+        OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                             "fence: waiting on %d in and %d out",
+                             P2P_MODULE(win)->p2p_num_pending_in,
+                             P2P_MODULE(win)->p2p_num_pending_out));
 
         /* try to start all the requests.  We've copied everything we
            need out of pending_sendreqs, so don't need the lock
@@ -429,9 +429,9 @@ ompi_osc_pt2pt_module_lock(int lock_type,
     ompi_win_remove_mode(win, OMPI_WIN_FENCE);
     ompi_win_append_mode(win, OMPI_WIN_ACCESS_EPOCH | OMPI_WIN_LOCK_ACCESS);
 
-    opal_output_verbose(50, ompi_osc_base_output,
-                        "%d sending lock request to %d", 
-                        P2P_MODULE(win)->p2p_comm->c_my_rank, target);
+    OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                         "%d sending lock request to %d", 
+                         P2P_MODULE(win)->p2p_comm->c_my_rank, target));
     /* generate a lock request */
     ompi_osc_pt2pt_control_send(P2P_MODULE(win), 
                                 proc,
@@ -471,9 +471,9 @@ ompi_osc_pt2pt_module_unlock(int target,
     OPAL_THREAD_ADD32(&(P2P_MODULE(win)->p2p_num_pending_out), out_count + 1);
 
     /* send the unlock request */
-    opal_output_verbose(50, ompi_osc_base_output,
-                        "%d sending unlock request to %d", 
-                        P2P_MODULE(win)->p2p_comm->c_my_rank, target);
+    OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                         "%d sending unlock request to %d", 
+                         P2P_MODULE(win)->p2p_comm->c_my_rank, target));
     ompi_osc_pt2pt_control_send(P2P_MODULE(win), 
                                 proc,
                                 OMPI_OSC_PT2PT_HDR_UNLOCK_REQ,
@@ -524,9 +524,9 @@ ompi_osc_pt2pt_passive_lock(ompi_osc_pt2pt_module_t *module,
             ompi_win_append_mode(module->p2p_win, OMPI_WIN_EXPOSE_EPOCH);
             send_ack = true;
         } else {
-            opal_output_verbose(50, ompi_osc_base_output,
-                                "%d queuing lock request from %d (%d)", 
-                                module->p2p_comm->c_my_rank, origin, lock_type);
+            OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                                 "%d queuing lock request from %d (%d)", 
+                                 module->p2p_comm->c_my_rank, origin, lock_type));
             new_pending = OBJ_NEW(ompi_osc_pt2pt_pending_lock_t);
             new_pending->proc = proc;
             new_pending->lock_type = lock_type;
@@ -539,9 +539,9 @@ ompi_osc_pt2pt_passive_lock(ompi_osc_pt2pt_module_t *module,
             ompi_win_append_mode(module->p2p_win, OMPI_WIN_EXPOSE_EPOCH);
             send_ack = true;
         } else {
-            opal_output_verbose(50, ompi_osc_base_output,
-                                "queuing lock request from %d (%d) lock_type:%d", 
-                                module->p2p_comm->c_my_rank, origin, lock_type);
+            OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                                 "queuing lock request from %d (%d) lock_type:%d", 
+                                 module->p2p_comm->c_my_rank, origin, lock_type));
             new_pending = OBJ_NEW(ompi_osc_pt2pt_pending_lock_t);
             new_pending->proc = proc;
             new_pending->lock_type = lock_type;
@@ -553,9 +553,9 @@ ompi_osc_pt2pt_passive_lock(ompi_osc_pt2pt_module_t *module,
     OPAL_THREAD_UNLOCK(&(module->p2p_lock));
 
     if (send_ack) {
-        opal_output_verbose(50, ompi_osc_base_output,
-                            "%d sending lock ack to %d", 
-                            module->p2p_comm->c_my_rank, origin);
+        OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                             "%d sending lock ack to %d", 
+                             module->p2p_comm->c_my_rank, origin));
         ompi_osc_pt2pt_control_send(module, proc,
                                     OMPI_OSC_PT2PT_HDR_LOCK_REQ,
                                     module->p2p_comm->c_my_rank,
@@ -604,8 +604,8 @@ ompi_osc_pt2pt_passive_unlock(ompi_osc_pt2pt_module_t *module,
     OPAL_THREAD_UNLOCK(&(module->p2p_lock));
 
     if (NULL != new_pending) {
-        opal_output_verbose(50, ompi_osc_base_output,
-                            "sending lock request to proc");
+        OPAL_OUTPUT_VERBOSE((50, ompi_osc_base_output,
+                             "sending lock request to proc"));
         ompi_win_append_mode(module->p2p_win, OMPI_WIN_EXPOSE_EPOCH);
         /* set lock state and generate a lock request */
         module->p2p_lock_status = new_pending->lock_type;
