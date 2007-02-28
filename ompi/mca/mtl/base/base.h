870740efe2f9696f8434a38c05a453e25f122c8b
Sven Stork <stork@hlrs.de>
2007-02-28 12:51:55 +0000

    - proper export symbols that are required by other components.
    This commit was SVN r13841.

--- ompi/mca/mtl/base/base.h
+++ ompi/mca/mtl/base/base.h
@@ -33,6 +33,8 @@
 extern "C" {
 #endif
 
+OMPI_DECLSPEC mca_mtl_base_component_t* ompi_mtl_base_selected_component;
+    
 OMPI_DECLSPEC int ompi_mtl_base_open(void);
 OMPI_DECLSPEC int ompi_mtl_base_select(bool enable_progress_threads,
                                        bool enable_mpi_threads);
