75bfb1f0bec398324b16beb6bd505cd4834582ef
Brian Barrett <brbarret@open-mpi.org>
2007-03-01 17:25:21 +0000

    Make BEGIN_C_DECLS and END_C_DECLS publically available so that they can see wider use
    This commit was SVN r13869.

--- opal/class/opal_object.h
+++ opal/class/opal_object.h
@@ -124,21 +124,6 @@
 #include "opal/sys/atomic.h"
 #endif  /* OMPI_HAVE_THREAD_SUPPORT */
 
-/*
- * BEGIN_C_DECLS should be used at the beginning of your declarations,
- * so that C++ compilers don't mangle their names.  Use END_C_DECLS at
- * the end of C declarations.
- */
-#undef BEGIN_C_DECLS
-#undef END_C_DECLS
-#if defined(c_plusplus) || defined(__cplusplus)
-# define BEGIN_C_DECLS extern "C" {
-# define END_C_DECLS }
-#else
-#define BEGIN_C_DECLS          /* empty */
-#define END_C_DECLS            /* empty */
-#endif
-
 #if OMPI_ENABLE_DEBUG
 /* Any kind of unique ID should do the job */
 #define OPAL_OBJ_MAGIC_ID ((0xdeafbeedULL << 32) + 0xdeafbeedULL)
