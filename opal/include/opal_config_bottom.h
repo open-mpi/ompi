8204eddc2cabc170fc35abd95ab7f664a63ba483
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2007-03-01 17:25:21 +0000

    Make BEGIN_C_DECLS and END_C_DECLS publically available so that they can see wider use

--- opal/include/opal_config_bottom.h
+++ opal/include/opal_config_bottom.h
@@ -47,6 +47,21 @@
 #define OMPI_BUILDING 1
 #endif
 
+/*
+ * BEGIN_C_DECLS should be used at the beginning of your declarations,
+ * so that C++ compilers don't mangle their names.  Use END_C_DECLS at
+ * the end of C declarations.
+ */
+#undef BEGIN_C_DECLS
+#undef END_C_DECLS
+#if defined(c_plusplus) || defined(__cplusplus)
+# define BEGIN_C_DECLS extern "C" {
+# define END_C_DECLS }
+#else
+#define BEGIN_C_DECLS          /* empty */
+#define END_C_DECLS            /* empty */
+#endif
+
 /**
  * The attribute definition should be included before any potential
  * usage.
