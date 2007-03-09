7e5a80d42811fcc0a050eadb30d547df4eae7ded
Jeff Squyres <jsquyres@cisco.com>
2007-03-09 17:13:24 +0000

    * Submited by: Brian Barrett  * Reviewed by: George Bosilca
    Fixes trac:940
    
    Adaptation of r13967 for v1.1:
    
    Implement MPI_TYPE_CREATE_DARRAY function. Works with MPICH2
    darray-pack test, Sun's darray test, and an internal LANL test code. I
    would not assume it will work properly on other codes, as I'm still
    not sure I completely understand what the standard says this function
    is supposed to do.
    
    This commit was SVN r13997.
    
    The following SVN revision numbers were found above:
      r13967 --> open-mpi/ompi@e926bed69fa7a96a302940e0f169a1c61dd14d7a
    
    The following Trac tickets were found above:
      Ticket 940 --> https://svn.open-mpi.org/trac/ompi/ticket/940

--- ompi/datatype/datatype.h
+++ ompi/datatype/datatype.h
@@ -10,6 +10,8 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2007      Los Alamos National Security, LLC.  All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -157,14 +159,6 @@ OMPI_DECLSPEC int32_t ompi_ddt_create_indexed_block( int count, int bLength, con
 OMPI_DECLSPEC int32_t ompi_ddt_create_struct( int count, const int* pBlockLength, const long* pDisp,
                                               ompi_datatype_t* const* pTypes, ompi_datatype_t** newType );
 OMPI_DECLSPEC int32_t ompi_ddt_create_resized( const ompi_datatype_t* oldType, long lb, long extent, ompi_datatype_t** newType );
-OMPI_DECLSPEC int32_t ompi_ddt_create_subarray( int ndims, const int* pSizes,
-                                                const int* pSubSizes, const int* pStarts,
-                                                int order, const ompi_datatype_t* oldType, ompi_datatype_t** newType );
-OMPI_DECLSPEC int32_t ompi_ddt_create_darray( int size, int rank, int ndims,
-                                              const int* pGSizes, const int *pDistrib,
-                                              const int* pDArgs, const int* pPSizes,
-                                              int order, const ompi_datatype_t* oldType,
-                                              ompi_datatype_t** newType );
 
 OMPI_DECLSPEC int32_t ompi_ddt_add( ompi_datatype_t* pdtBase, const ompi_datatype_t* pdtAdd, uint32_t count,
                                     long disp, long extent );
