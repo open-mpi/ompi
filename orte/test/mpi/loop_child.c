2cbcb4abf10dd3ea717d94ee1ac8b27c5b2e23af
Jeff Squyres <jsquyres@cisco.com>
2007-04-04 15:38:23 +0000

    Remove the French and strip the tests down to essentials (no need for buffer attaching/detaching, for example).
    This commit was SVN r14216.

--- orte/test/mpi/loop_child.c
+++ orte/test/mpi/loop_child.c
@@ -1,55 +1,30 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>     /* pour sleep() */
+#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include "mpi.h"
 
-int main( int argc, char **argv ) {
-/*1)pour communiaction MPI*/
-    MPI_Comm lCommunicateur;        /*communicateur du process*/
-    MPI_Comm CommParent;            /*Communiacteur parent àépér*/
-    int lRank;                      /*rang du communicateur du process*/
-    int lRangMain;            /*rang du séenceur si lancén mode normal*/
-    int lTailleCommunicateur;       /*taille du communicateur;*/
-    long *lpBufferMpi;              /*buffer pour message*/
-    int lBufferSize;                /*taille du buffer*/
-   
- 
-    lCommunicateur   = (MPI_Comm)-1;
-    int erreur = MPI_Init( &argc, &argv );   
-    
-    if (erreur!=0){
-        printf("erreur\n");
-        free( lpBufferMpi );  
-        return -1;
-    }
-   
-   /*2) Attachement àn buffer pour le message*/
-    lBufferSize=10000 * sizeof(long);
-    lpBufferMpi = calloc( 10000, sizeof(long));
-    erreur = MPI_Buffer_attach( (void*)lpBufferMpi, lBufferSize );
-    
-    if (erreur!=0){
-        printf("erreur\n");
-        free( lpBufferMpi );  
-        return -1;
-    }
-    
-    printf( "Exe : Lance \n" );
-    MPI_Comm_get_parent(&CommParent);   
-    MPI_Intercomm_merge( CommParent, 1, &lCommunicateur );
-    MPI_Comm_rank( lCommunicateur, &lRank );
-    MPI_Comm_size( lCommunicateur, &lTailleCommunicateur );
-    lRangMain   =1-lRank;
-    printf( "Exe: lRankExe  = %d   lRankMain  = %d\n", lRank , lRangMain);
+
+int main( int argc, char **argv ) 
+{
+    MPI_Comm parent;
+    MPI_Comm merged;
+    int rank;
+    int size;
+
+    MPI_Init(&argc, &argv);   
+    printf("Child: launch\n");
+    MPI_Comm_get_parent(&parent);   
+    MPI_Intercomm_merge(parent, 1, &merged);
+    MPI_Comm_rank(merged, &rank);
+    MPI_Comm_size(merged, &size);
+    printf("Child merged rank = %d, size = %d\n", rank, size);
    
     sleep(1);
-    MPI_Buffer_detach( (void*)lpBufferMpi, &lBufferSize );
-    MPI_Comm_free( &lCommunicateur );
-    MPI_Finalize( );
-    free( lpBufferMpi );
-    printf( "Exe: Fin.\n\n\n" );
+    MPI_Comm_free(&merged);
+    MPI_Finalize();
+    printf("Child: exiting\n");
     return 0;
 }
