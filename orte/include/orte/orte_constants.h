open-mpi/ompi

commit 72767d32280a104ef80034857393e51d750c7352

Hold the RMGR in the spawn command until the application process actually launches. Previously, we returned from spawn immediately after launching the daemons - this meant that the caller had to define their own "wait until app launches". This only tells the caller that the app procs were launched, of course - it doesn't mean that they have started execution or reached any particular stage. However, for non-MPI procs, this is as far as we can go - there is no further stage gate we can provide.
Still, better than what we provided before...


git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@14554 63e3feb5-37d5-0310-a306-e8a459e722fe

orte/mca/rmgr/urm/rmgr_urm.c
@@ -34,6 +34,7 @@
 #include "opal/util/output.h"
 #include "opal/mca/base/mca_base_param.h"
 #include "opal/util/opal_environ.h"
+#include "opal/threads/condition.h"
 
 #include "orte/mca/errmgr/errmgr.h"
 #include "orte/mca/rds/rds.h"
@@ -306,7 +307,14 @@ static void orte_rmgr_urm_wireup_callback(orte_gpr_notify_data_t *data, void *cb
     opal_output(orte_rmgr_base.rmgr_output, "rmgr_urm:wireup_callback called for job %ld", (long)jobid);
     
     orte_rmgr_urm_wireup_stdin(jobid);
+    
+    /* signal that the application has indeed launched */
+    OPAL_THREAD_LOCK(&mca_rmgr_urm_component.lock);
+    mca_rmgr_urm_component.done = true;
+    opal_condition_signal(&mca_rmgr_urm_component.cond);
+    OPAL_THREAD_UNLOCK(&mca_rmgr_urm_component.lock);
 }
+
 
 /*
  *  Shortcut for the multiple steps involved in spawning a new job.
@@ -339,6 +347,11 @@ static int orte_rmgr_urm_spawn_job(
             urmstart.tv_usec = 0;
         }
     }
+    
+    /* mark that the spawn is not done */
+    OPAL_THREAD_LOCK(&mca_rmgr_urm_component.lock);
+    mca_rmgr_urm_component.done = false;
+    OPAL_THREAD_UNLOCK(&mca_rmgr_urm_component.lock);
     
     /* check for any flow directives to control what we do */
     if (NULL != (flow = orte_rmgr.find_attribute(attributes, ORTE_RMGR_SPAWN_FLOW))) {
@@ -413,7 +426,10 @@ static int orte_rmgr_urm_spawn_job(
             return rc;
         }
         
-        /** setup the subscription so we can complete the wireup when all processes reach LAUNCHED */
+        /** setup the subscription so we can complete the wireup when all processes reach LAUNCHED. This
+         * function has the dual purpose of setting the conditioned wait variable so that the RMGR
+         * can know that the app has indeed launched, and hence return to the caller
+         */
         rc = orte_smr.job_stage_gate_subscribe(*jobid, orte_rmgr_urm_wireup_callback, NULL, ORTE_PROC_STATE_LAUNCHED);
         if(ORTE_SUCCESS != rc) {
             ORTE_ERROR_LOG(rc);
@@ -487,6 +503,14 @@ static int orte_rmgr_urm_spawn_job(
         return rc;
     }
 
+     /* wait for the application to launch */
+     OPAL_THREAD_LOCK(&mca_rmgr_urm_component.lock);
+     while (!mca_rmgr_urm_component.done) {
+         opal_condition_wait(&mca_rmgr_urm_component.cond,
+                             &mca_rmgr_urm_component.lock);
+     }
+     OPAL_THREAD_UNLOCK(&mca_rmgr_urm_component.lock);
+    
      /* check for timing request - get start time if so */
      if (mca_rmgr_urm_component.timing) {
          if (0 != gettimeofday(&urmstart, NULL)) {

orte/mca/rmgr/urm/rmgr_urm.h
@@ -23,6 +23,8 @@
 #ifndef ORTE_RMGR_URM_H
 #define ORTE_RMGR_URM_H
 
+#include "opal/threads/condition.h"
+
 #include "orte/mca/rmgr/rmgr.h"
 
 #if defined(c_plusplus) || defined(__cplusplus)
@@ -40,6 +42,12 @@ struct orte_rmgr_urm_component_t {
     bool urm_rds;
     /* timing tests requested */
     bool timing;
+    /* mutex lock */
+    opal_mutex_t lock;
+    /* conditioned wait variable */
+    opal_condition_t cond;
+    /* flag for tracking completion of app launch */
+    bool done;
 };
 /** Convenience typedef */
 typedef struct orte_rmgr_urm_component_t orte_rmgr_urm_component_t;

orte/mca/rmgr/proxy/rmgr_proxy.c
@@ -27,6 +27,7 @@
 #include "opal/class/opal_list.h"
 #include "opal/util/output.h"
 #include "opal/util/trace.h"
+#include "opal/threads/condition.h"
 
 #include "orte/mca/rds/rds.h"
 #include "orte/mca/ras/ras.h"
@@ -348,6 +349,12 @@ static void orte_rmgr_proxy_wireup_callback(orte_gpr_notify_data_t *data, void *
         return;
     }
     orte_rmgr_proxy_wireup_stdin(jobid);
+    
+    /* signal that the application has indeed launched */
+    OPAL_THREAD_LOCK(&mca_rmgr_proxy_component.lock);
+    mca_rmgr_proxy_component.done = true;
+    opal_condition_signal(&mca_rmgr_proxy_component.cond);
+    OPAL_THREAD_UNLOCK(&mca_rmgr_proxy_component.lock);    
 }
 
 /*
@@ -371,6 +378,11 @@ static int orte_rmgr_proxy_spawn_job(
     uint8_t flags, *fptr;
 
     OPAL_TRACE(1);
+    
+    /* mark that the spawn is not done */
+    OPAL_THREAD_LOCK(&mca_rmgr_proxy_component.lock);
+    mca_rmgr_proxy_component.done = false;
+    OPAL_THREAD_UNLOCK(&mca_rmgr_proxy_component.lock);
     
     /* check for any flow directives to control what we do */
     if (NULL != (flow = orte_rmgr.find_attribute(attributes, ORTE_RMGR_SPAWN_FLOW))) {
@@ -492,6 +504,14 @@ static int orte_rmgr_proxy_spawn_job(
         return rc;
     }
 
+    /* wait for the application to launch */
+    OPAL_THREAD_LOCK(&mca_rmgr_proxy_component.lock);
+    while (!mca_rmgr_proxy_component.done) {
+        opal_condition_wait(&mca_rmgr_proxy_component.cond,
+                            &mca_rmgr_proxy_component.lock);
+    }
+    OPAL_THREAD_UNLOCK(&mca_rmgr_proxy_component.lock);
+    
     return ORTE_SUCCESS;
 }
 

orte/mca/rmgr/proxy/rmgr_proxy.h
@@ -21,6 +21,8 @@
 #ifndef ORTE_RMGR_URM_H
 #define ORTE_RMGR_URM_H
 
+#include "opal/threads/condition.h"
+
 #include "orte/mca/rmgr/rmgr.h"
 
 #if defined(c_plusplus) || defined(__cplusplus)
@@ -34,6 +36,12 @@ extern "C" {
 struct orte_rmgr_proxy_component_t {
     /** Base rmgr component */
     orte_rmgr_base_component_t super;
+    /* mutex lock */
+    opal_mutex_t lock;
+    /* conditioned wait variable */
+    opal_condition_t cond;
+    /* flag for tracking completion of app launch */
+    bool done;
 };
 /** Convenience typedef */
 typedef struct orte_rmgr_proxy_component_t orte_rmgr_proxy_component_t;

orte/mca/rmgr/proxy/rmgr_proxy_component.c
@@ -16,14 +16,17 @@
 
 #include "orte_config.h"
 #include "orte/orte_constants.h"
-#include "orte/util/proc_info.h"
-#include "opal/util/output.h"
-#include "orte/mca/errmgr/errmgr.h"
 
+#include "opal/threads/condition.h"
+#include "opal/util/output.h"
+
+#include "orte/util/proc_info.h"
+#include "orte/mca/errmgr/errmgr.h"
 #include "orte/mca/rds/base/base.h"
 #include "orte/mca/ras/base/base.h"
 #include "orte/mca/rmaps/base/base.h"
 #include "orte/mca/pls/base/base.h"
+
 #include "rmgr_proxy.h"
 
 /*
@@ -76,6 +79,12 @@ static int orte_rmgr_proxy_open(void)
 
 static orte_rmgr_base_module_t *orte_rmgr_proxy_init(int* priority)
 {
+    /* setup the locks - need to do this first so that we don't crash
+     * when we close the component, even if we aren't selected
+     */
+    OBJ_CONSTRUCT(&mca_rmgr_proxy_component.lock, opal_mutex_t);
+    OBJ_CONSTRUCT(&mca_rmgr_proxy_component.cond, opal_condition_t);
+    
     /* if we are an HNP, then do NOT select us */
     if (orte_process_info.seed) {
         return NULL;
@@ -94,5 +103,9 @@ static orte_rmgr_base_module_t *orte_rmgr_proxy_init(int* priority)
  */
 static int orte_rmgr_proxy_close(void)
 {
+    /* destruct locks */
+    OBJ_DESTRUCT(&mca_rmgr_proxy_component.lock);
+    OBJ_DESTRUCT(&mca_rmgr_proxy_component.cond);
+
     return ORTE_SUCCESS;
 }

orte/mca/rmgr/urm/rmgr_urm_component.c
@@ -21,6 +21,7 @@
 
 #include "opal/util/output.h"
 #include "opal/mca/base/mca_base_param.h"
+#include "opal/threads/condition.h"
 
 #include "orte/dss/dss_types.h"
 #include "orte/mca/errmgr/errmgr.h"
@@ -85,11 +86,18 @@ static orte_rmgr_base_module_t *orte_rmgr_urm_init(int* priority)
 {
     int param, value;
     
+    /* setup the locks - need to do this first so that we don't crash
+     * when we close the component, even if we aren't selected
+     */
+    OBJ_CONSTRUCT(&mca_rmgr_urm_component.lock, opal_mutex_t);
+    OBJ_CONSTRUCT(&mca_rmgr_urm_component.cond, opal_condition_t);
+    
     /* if we are NOT an HNP, then we do NOT want to be selected */
     if(!orte_process_info.seed) {
         return NULL;
     }
 
+    /* check for params */
     param = mca_base_param_reg_int_name("orte", "timing",
                                         "Request that critical timing loops be measured",
                                         false, false, 0, &value);
@@ -110,5 +118,9 @@ static orte_rmgr_base_module_t *orte_rmgr_urm_init(int* priority)
  */
 static int orte_rmgr_urm_close(void)
 {
+    /* destruct locks */
+    OBJ_DESTRUCT(&mca_rmgr_urm_component.lock);
+    OBJ_DESTRUCT(&mca_rmgr_urm_component.cond);
+    
     return ORTE_SUCCESS;
 }

commit 01e63550f5e8a384ef011ae33a5e9d58cd965819

Fix the app-failed-to-start capability that was broken by r14554 (holding the caller in rmgr.spawn until the application - as opposed to just the orteds - have started). Allow the rmgr.spawn function to return if the app terminates, correctly handling its return status code to show abnormal termination. Modify orterun to correctly handle the returned status code so it doesn't enter a conditioned wait if the app fails to start since it will never wakeup if it does.

orte/include/orte/orte_constants.h
@@ -102,7 +102,8 @@ enum {
     ORTE_ERR_WDIR_NOT_FOUND                 = (ORTE_ERR_BASE - 34),
     ORTE_ERR_EXE_NOT_FOUND                  = (ORTE_ERR_BASE - 35),
     ORTE_ERR_PIPE_READ_FAILURE              = (ORTE_ERR_BASE - 36),
-    ORTE_ERR_EXE_NOT_ACCESSIBLE             = (ORTE_ERR_BASE - 37)
+    ORTE_ERR_EXE_NOT_ACCESSIBLE             = (ORTE_ERR_BASE - 37),
+    ORTE_ERR_FAILED_TO_START                = (ORTE_ERR_BASE - 38)
 };
 
 #define ORTE_ERR_MAX                      (ORTE_ERR_BASE - 100)

orte/mca/rmgr/urm/rmgr_urm.c
@@ -307,14 +307,26 @@ static void orte_rmgr_urm_wireup_callback(orte_gpr_notify_data_t *data, void *cb
     opal_output(orte_rmgr_base.rmgr_output, "rmgr_urm:wireup_callback called for job %ld", (long)jobid);
     
     orte_rmgr_urm_wireup_stdin(jobid);
-    
-    /* signal that the application has indeed launched */
+   
+    /* signal that we can leave */
+    OPAL_THREAD_LOCK(&mca_rmgr_urm_component.lock);
+    mca_rmgr_urm_component.done = true;
+    mca_rmgr_urm_component.rc = ORTE_SUCCESS;
+    opal_condition_signal(&mca_rmgr_urm_component.cond);
+    OPAL_THREAD_UNLOCK(&mca_rmgr_urm_component.lock);    
+}
+
+/*
+ * callback that tells us when we can leave the spawn function and return to caller
+ */
+static void app_terminated(orte_gpr_notify_data_t *data, void *cbdata)
+{
+    /* signal that we can leave */
     OPAL_THREAD_LOCK(&mca_rmgr_urm_component.lock);
     mca_rmgr_urm_component.done = true;
     opal_condition_signal(&mca_rmgr_urm_component.cond);
     OPAL_THREAD_UNLOCK(&mca_rmgr_urm_component.lock);
 }
-
 
 /*
  *  Shortcut for the multiple steps involved in spawning a new job.
@@ -351,6 +363,7 @@ static int orte_rmgr_urm_spawn_job(
     /* mark that the spawn is not done */
     OPAL_THREAD_LOCK(&mca_rmgr_urm_component.lock);
     mca_rmgr_urm_component.done = false;
+    mca_rmgr_urm_component.rc = ORTE_ERR_FAILED_TO_START;
     OPAL_THREAD_UNLOCK(&mca_rmgr_urm_component.lock);
     
     /* check for any flow directives to control what we do */
@@ -426,7 +439,7 @@ static int orte_rmgr_urm_spawn_job(
             return rc;
         }
         
-        /** setup the subscription so we can complete the wireup when all processes reach LAUNCHED. This
+        /* setup the subscription so we can complete the wireup when all processes reach LAUNCHED. This
          * function has the dual purpose of setting the conditioned wait variable so that the RMGR
          * can know that the app has indeed launched, and hence return to the caller
          */
@@ -436,7 +449,14 @@ static int orte_rmgr_urm_spawn_job(
             return rc;
         }
 
-         /*
+        /* setup the subscription so we will know if things fail to launch */
+        rc = orte_smr.job_stage_gate_subscribe(*jobid, app_terminated, NULL, ORTE_PROC_STATE_TERMINATED);
+        if(ORTE_SUCCESS != rc) {
+            ORTE_ERROR_LOG(rc);
+            return rc;
+        }
+        
+        /*
           * Define the ERRMGR's callbacks as required
           */
          if (ORTE_SUCCESS != (rc = orte_errmgr.register_job(*jobid))) {
@@ -522,7 +542,8 @@ static int orte_rmgr_urm_spawn_job(
          }
      }
      
-     return ORTE_SUCCESS;
+     /* return the status code contained in the component */
+     return mca_rmgr_urm_component.rc;
 }
 
 

orte/mca/rmgr/urm/rmgr_urm.h
@@ -48,6 +48,8 @@ struct orte_rmgr_urm_component_t {
     opal_condition_t cond;
     /* flag for tracking completion of app launch */
     bool done;
+    /* return status code */
+    int rc;
 };
 /** Convenience typedef */
 typedef struct orte_rmgr_urm_component_t orte_rmgr_urm_component_t;

orte/tools/orterun/orterun.c
@@ -480,84 +480,77 @@ int orterun(int argc, char *argv[])
     while (NULL != (item = opal_list_remove_first(&attributes))) OBJ_RELEASE(item);
     OBJ_DESTRUCT(&attributes);
     
-    if (ORTE_SUCCESS != rc) {
-        /* JMS show_help unless it is ERR_SILENT */
-        if (ORTE_ERR_SILENT != rc) {
-            opal_output(0, "%s: spawn failed with errno=%d\n", orterun_basename, rc);
-        }
-    } else {
-
-        if (orterun_globals.do_not_launch) {
-            /* we are done! */
-            goto DONE;
-        }
+    if (orterun_globals.do_not_launch) {
+        /* we are done! */
+        goto DONE;
+    }
         
-        /* Wait for the app to complete */
-        if (wait_for_job_completion) {
-            OPAL_THREAD_LOCK(&orterun_globals.lock);
-            while (!orterun_globals.exit) {
-                opal_condition_wait(&orterun_globals.cond,
-                                    &orterun_globals.lock);
-            }
-            /* check to see if the job was aborted */
-            if (ORTE_JOBID_INVALID != jobid &&
-                ORTE_SUCCESS != (rc = orte_smr.get_job_state(&exit_state, jobid))) {
-                if (ORTE_SUCCESS != rc) {
-                    ORTE_ERROR_LOG(rc);
-                }
-                /* define the exit state as abnormal by default */
-                exit_state = ORTE_JOB_STATE_ABORTED;
-            }
-            if (ORTE_JOB_STATE_TERMINATED != exit_state) {
-                /* abnormal termination of some kind */
-                dump_aborted_procs(jobid, apps, exit_state);
-                /* If we showed more abort messages than were allowed,
-                show a followup message here */
-                if (num_aborted > max_display_aborted) {
-                    i = num_aborted - max_display_aborted;
-                    printf("%d additional process%s aborted (not shown)\n",
-                           i, ((i > 1) ? "es" : ""));
-                }
-                if (num_killed > 0) {
-                    printf("%d process%s killed (possibly by Open MPI)\n",
-                           num_killed, ((num_killed > 1) ? "es" : ""));
-                }
-            }
-            /* Make sure we propagate the exit code */
-            if (WIFEXITED(orterun_globals.exit_status)) {
-                rc = WEXITSTATUS(orterun_globals.exit_status);
-            }  else if (ORTE_JOB_STATE_FAILED_TO_START == exit_state) {
-                /* ensure we don't treat this like a signal */
-                rc = orterun_globals.exit_status;
-            } else {
-                /* If a process was killed by a signal, then make the
-                 * exit code of orterun be "signo + 128" so that "prog"
-                 * and "orterun prog" will both set the same status
-                 * value for the shell */
-                rc = WTERMSIG(orterun_globals.exit_status) + 128;
-            }
-            
-            /* the job is complete - now tell the orteds that it is
-             * okay to finalize and exit, we are done with them.
-             * Issue this as a "soft kill" so the daemons won't die
-             * if they are part of a virtual machine - since that is
-             * the default mode, we can just leave the attributes as NULL
-             */
-            if (ORTE_JOBID_INVALID != jobid) {
-                if (ORTE_SUCCESS != (ret = orte_pls.terminate_orteds(&orte_abort_timeout, NULL))) {
-                    opal_show_help("help-orterun.txt", "orterun:daemon-die", true,
-                                   orterun_basename, ORTE_ERROR_NAME(ret));
-                }
-            }
-            OPAL_THREAD_UNLOCK(&orterun_globals.lock);
-
-            /* If we were forcibly killed, print a warning that the
-               user may still have some manual cleanup to do. */
-            if (ORTE_JOBID_INVALID == jobid) {
-                opal_show_help("help-orterun.txt", "orterun:abnormal-exit",
-                               true, orterun_basename, orterun_basename);
-            }
+    OPAL_THREAD_LOCK(&orterun_globals.lock);
+    /* If the spawn was successful, wait for the app to complete */
+    if (ORTE_SUCCESS == rc) {
+        while (!orterun_globals.exit) {
+            opal_condition_wait(&orterun_globals.cond,
+                                &orterun_globals.lock);
         }
+    }
+    
+    /* check to see if the job was aborted */
+    if (ORTE_JOBID_INVALID != jobid &&
+        ORTE_SUCCESS != (rc = orte_smr.get_job_state(&exit_state, jobid))) {
+        if (ORTE_SUCCESS != rc) {
+            ORTE_ERROR_LOG(rc);
+        }
+        /* define the exit state as abnormal by default */
+        exit_state = ORTE_JOB_STATE_ABORTED;
+    }
+    if (ORTE_JOB_STATE_TERMINATED != exit_state) {
+        /* abnormal termination of some kind */
+        dump_aborted_procs(jobid, apps, exit_state);
+        /* If we showed more abort messages than were allowed,
+        show a followup message here */
+        if (num_aborted > max_display_aborted) {
+            i = num_aborted - max_display_aborted;
+            printf("%d additional process%s aborted (not shown)\n",
+                   i, ((i > 1) ? "es" : ""));
+        }
+        if (num_killed > 0) {
+            printf("%d process%s killed (possibly by Open MPI)\n",
+                   num_killed, ((num_killed > 1) ? "es" : ""));
+        }
+    }
+    /* Make sure we propagate the exit code */
+    if (WIFEXITED(orterun_globals.exit_status)) {
+        rc = WEXITSTATUS(orterun_globals.exit_status);
+    }  else if (ORTE_JOB_STATE_FAILED_TO_START == exit_state) {
+        /* ensure we don't treat this like a signal */
+        rc = orterun_globals.exit_status;
+    } else {
+        /* If a process was killed by a signal, then make the
+         * exit code of orterun be "signo + 128" so that "prog"
+         * and "orterun prog" will both set the same status
+         * value for the shell */
+        rc = WTERMSIG(orterun_globals.exit_status) + 128;
+    }
+    
+    /* the job is complete - now tell the orteds that it is
+     * okay to finalize and exit, we are done with them.
+     * Issue this as a "soft kill" so the daemons won't die
+     * if they are part of a virtual machine - since that is
+     * the default mode, we can just leave the attributes as NULL
+     */
+    if (ORTE_JOBID_INVALID != jobid) {
+        if (ORTE_SUCCESS != (ret = orte_pls.terminate_orteds(&orte_abort_timeout, NULL))) {
+            opal_show_help("help-orterun.txt", "orterun:daemon-die", true,
+                           orterun_basename, ORTE_ERROR_NAME(ret));
+        }
+    }
+    OPAL_THREAD_UNLOCK(&orterun_globals.lock);
+
+    /* If we were forcibly killed, print a warning that the
+       user may still have some manual cleanup to do. */
+    if (ORTE_JOBID_INVALID == jobid) {
+        opal_show_help("help-orterun.txt", "orterun:abnormal-exit",
+                       true, orterun_basename, orterun_basename);
     }
 
 DONE:

orte/util/error_strings.c
@@ -139,6 +139,9 @@ orte_err2str(int errnum)
     case ORTE_ERR_EXE_NOT_ACCESSIBLE:
         retval = "The specified executable could not be executed";
         break;
+    case ORTE_ERR_FAILED_TO_START:
+        retval = "The specified application failed to start";
+        break;
     default:
         retval = NULL;
     }