3288ce0462293ea872db0976225beb8b7f67d997
Ralph Castain <rhc@open-mpi.org>
2007-05-21 14:57:58 +0000

    Cleanup the SDS components and move some common code to the base.
    Modify the seed and singleton SDS code so it returns the right local rank and num_local_procs.
    
    This commit was SVN r14707.

--- orte/mca/sds/env/sds_env_module.c
+++ orte/mca/sds/env/sds_env_module.c
@@ -46,13 +46,7 @@ orte_sds_env_set_name(void)
 {
     int rc;
     int id;
-    int vpid_start;
-    int num_procs;
-    int local_rank;
-    int num_local_procs;
-    char* name_string = NULL;
-    char *local_daemon_uri = NULL;
-    
+    char* name_string = NULL;    
     
     id = mca_base_param_register_string("ns", "nds", "name", NULL, NULL);
     mca_base_param_lookup_string(id, &name_string);
@@ -119,48 +113,10 @@ orte_sds_env_set_name(void)
         }
     }
 
-    id = mca_base_param_register_int("ns", "nds", "vpid_start", NULL, -1);
-    mca_base_param_lookup_int(id, &vpid_start);
-    if (vpid_start < 0) {
-        ORTE_ERROR_LOG(ORTE_ERR_NOT_FOUND);
-        return ORTE_ERR_NOT_FOUND;
-    }
-    orte_process_info.vpid_start = (orte_vpid_t)vpid_start;
-
-    id = mca_base_param_register_int("ns", "nds", "num_procs", NULL, -1);
-    mca_base_param_lookup_int(id, &num_procs);
-    if (num_procs < 0) {
-        ORTE_ERROR_LOG(ORTE_ERR_NOT_FOUND);
-        return ORTE_ERR_NOT_FOUND;
-    }
-    orte_process_info.num_procs = (orte_std_cntr_t)num_procs;
-
-    /* it is okay for this param not to be found - for example, we don't bother
-     * to set it for orteds - so just set it to an invalid value which indicates
-     * it wasn't found if it isn't there
-     */
-    id = mca_base_param_register_int("ns", "nds", "local_rank", NULL, ORTE_VPID_INVALID);
-    mca_base_param_lookup_int(id, &local_rank);
-    orte_process_info.local_rank = (orte_vpid_t)local_rank;
-    
-    /* it is okay for this param not to be found - for example, we don't bother
-     * to set it for orteds - so just set it to a value which indicates
-     * it wasn't found if it isn't there
-     */
-    id = mca_base_param_register_int("ns", "nds", "num_local_procs", NULL, 0);
-    mca_base_param_lookup_int(id, &num_local_procs);
-    orte_process_info.num_local_procs = (orte_std_cntr_t)num_local_procs;
-    
-    id = mca_base_param_register_string("orte", "local_daemon", "uri", NULL, NULL);
-    mca_base_param_lookup_string(id, &local_daemon_uri);
-    if (NULL != local_daemon_uri) {
-        /* if we are a daemon, then we won't have this param set, so allow
-         * it not to be found
-         */
-        if (ORTE_SUCCESS != (rc = orte_sds_base_contact_orted(local_daemon_uri))) {
-            ORTE_ERROR_LOG(rc);
-            return(rc);
-        }
+    /* get the non-name common environmental variables */
+    if (ORTE_SUCCESS != (rc = orte_sds_env_get())) {
+        ORTE_ERROR_LOG(rc);
+        return rc;
     }
     
     return ORTE_SUCCESS;
--- orte/mca/sds/xcpu/sds_xcpu_module.c
+++ orte/mca/sds/xcpu/sds_xcpu_module.c
@@ -65,12 +65,10 @@ int orte_sds_xcpu_set_name(void)
 
         orte_cellid_t cellid;
         orte_jobid_t jobid;
-        orte_vpid_t vpid;
         orte_vpid_t vpid_start;
         char* cellid_string;
         char* jobid_string;
         char* vpid_string;
-        int num_procs, local_rank, num_local_procs;
         char *xcpu_rank_string;
         int xcpu_rank;
         int stride;
@@ -109,54 +107,11 @@ int orte_sds_xcpu_set_name(void)
 
         xcpu_rank = (int)strtol(xcpu_rank_string+1, NULL, 10);
 
-        id = mca_base_param_register_string("ns", "nds", "vpid_start", NULL, NULL);
-        mca_base_param_lookup_string(id, &vpid_string);
-        if (NULL == vpid_string) {
-            ORTE_ERROR_LOG(ORTE_ERR_NOT_FOUND);
-            return ORTE_ERR_NOT_FOUND;
-        }
-        rc = orte_ns.convert_string_to_vpid(&vpid_start, vpid_string);
-        if (ORTE_SUCCESS != rc) {
+        /* get the non-name common environmental variables */
+        if (ORTE_SUCCESS != (rc = orte_sds_env_get())) {
             ORTE_ERROR_LOG(rc);
-            return(rc);
+            return rc;
         }
-        
-        /* compute our vpid */
-        vpid = vpid_start + xcpu_rank - 1;
-
-        /* create our name */
-        if (ORTE_SUCCESS != (rc = orte_ns.create_process_name(
-           &(orte_process_info.my_name),
-           cellid,
-           jobid,
-           vpid))) {
-           ORTE_ERROR_LOG(rc);
-           return rc;
-        }
-
-        id = mca_base_param_register_int("ns", "nds", "num_procs", NULL, -1);
-        mca_base_param_lookup_int(id, &num_procs);
-        if (num_procs < 0) {
-            ORTE_ERROR_LOG(ORTE_ERR_NOT_FOUND);
-            return ORTE_ERR_NOT_FOUND;
-        }
-        orte_process_info.num_procs = (orte_std_cntr_t)num_procs;
-
-        /* it is okay for this param not to be found - for example, we don't bother
-         * to set it for orteds - so just set it to an invalid value which indicates
-         * it wasn't found if it isn't there
-         */
-        id = mca_base_param_register_int("ns", "nds", "local_rank", NULL, ORTE_VPID_INVALID);
-        mca_base_param_lookup_int(id, &local_rank);
-        orte_process_info.local_rank = (orte_vpid_t)local_rank;
-        
-        /* it is okay for this param not to be found - for example, we don't bother
-         * to set it for orteds - so just set it to a value which indicates
-         * it wasn't found if it isn't there
-         */
-        id = mca_base_param_register_int("ns", "nds", "num_local_procs", NULL, 0);
-        mca_base_param_lookup_int(id, &num_local_procs);
-        orte_process_info.num_local_procs = (orte_std_cntr_t)num_local_procs;
         
 #if 0
         id = mca_base_param_register_string("ns", "nds", "global_vpid_start", NULL, NULL);
