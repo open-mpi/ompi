open-mpi/ompi

commit d146f064d222a3a672746499503060c14723dc02

Add optional ordering to the BTL interface. This is required to tighten up the BTL semantics. Ordering is not guaranteed, but, if the BTL returns a order tag in a descriptor (other than MCA_BTL_NO_ORDER) then we may request another descriptor that will obey ordering w.r.t. to the other descriptor.
This will allow sane behavior for RDMA networks, where local completion of an
RDMA operation on the active side does not imply remote completion on the
passive side. If we send a FIN message after local completion and the FIN is
not ordered w.r.t. the RDMA operation then badness may occur as the passive
side may now try to deregister the memory and the RDMA operation may still be
pending on the passive side. 

Note that this has no impact on networks that don't suffer from this
limitation as the ORDER tag can simply always be specified as
MCA_BTL_NO_ORDER.



 


git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@14768 63e3feb5-37d5-0310-a306-e8a459e722fe

ompi/mca/btl/openib/btl_openib.c
@@ -360,12 +360,14 @@ int mca_btl_openib_register_error_cb(
  *
  * @param btl (IN)      BTL module
  * @param size (IN)     Request segment size.
+  * @param size (IN) Size of segment to allocate    
  * 
  * When allocating a segment we pull a pre-alllocated segment 
  * from one of two free lists, an eager list and a max list
  */
 mca_btl_base_descriptor_t* mca_btl_openib_alloc(
     struct mca_btl_base_module_t* btl,
+    uint8_t order,
     size_t size)
 {
     mca_btl_openib_frag_t* frag = NULL;
@@ -375,14 +377,27 @@ mca_btl_base_descriptor_t* mca_btl_openib_alloc(
     
     if(size <= mca_btl_openib_component.eager_limit){ 
         MCA_BTL_IB_FRAG_ALLOC_EAGER(btl, frag, rc);
+        if(order == MCA_BTL_NO_ORDER) {
+            order = BTL_OPENIB_HP_QP;
+        } 
+        frag->base.order = order;
+        
     } else if(size <= mca_btl_openib_component.max_send_size) { 
+        if(order == MCA_BTL_NO_ORDER) { 
+            order = BTL_OPENIB_LP_QP; 
+        } else if(order != BTL_OPENIB_LP_QP) { 
+            return NULL;
+        }
+        
         MCA_BTL_IB_FRAG_ALLOC_MAX(btl, frag, rc); 
+        frag->base.order = order;
     }
     
     if(NULL == frag)
         return NULL;
 
-    frag->segment.seg_len = size <= openib_btl->super.btl_eager_limit ? size : openib_btl->super.btl_eager_limit;  
+    frag->segment.seg_len = 
+        size <= openib_btl->super.btl_eager_limit ? size : openib_btl->super.btl_eager_limit;  
     frag->base.des_flags = 0; 
     
     return (mca_btl_base_descriptor_t*)frag;
@@ -442,6 +457,7 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
     struct mca_btl_base_endpoint_t* endpoint,
     mca_mpool_base_registration_t* registration, 
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size
 )
@@ -496,7 +512,13 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
             frag->segment.seg_len = max_data;
             frag->segment.seg_addr.pval = iov.iov_base;
             frag->segment.seg_key.key32[0] = (uint32_t)frag->sg_entry.lkey;
-
+            
+            if(MCA_BTL_NO_ORDER == order) { 
+                frag->base.order = BTL_OPENIB_LP_QP;
+            } else { 
+                frag->base.order = order;
+            }
+            
             BTL_VERBOSE(("frag->sg_entry.lkey = %lu .addr = %llu "
                         "frag->segment.seg_key.key32[0] = %lu",
                         frag->sg_entry.lkey, frag->sg_entry.addr,
@@ -509,13 +531,26 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
     if(max_data + reserve <= btl->btl_eager_limit) {
         /* the data is small enough to fit in the eager frag and
          * memory is not prepinned */
+        
         MCA_BTL_IB_FRAG_ALLOC_EAGER(btl, frag, rc);
+        if(MCA_BTL_NO_ORDER == order) { 
+            frag->base.order = BTL_OPENIB_LP_QP;
+        } else { 
+            frag->base.order = order;
+        }
     }
 
     if(NULL == frag) {
         /* the data doesn't fit into eager frag or eager frag is
          * not available */
+        if(MCA_BTL_NO_ORDER == order) { 
+            order = BTL_OPENIB_LP_QP;
+        } else if(BTL_OPENIB_HP_QP == order){ 
+            return NULL;
+        } 
         MCA_BTL_IB_FRAG_ALLOC_MAX(btl, frag, rc);
+        frag->base.order = order;
+
         if(NULL == frag) {
             return NULL;
         }
@@ -539,7 +574,9 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
     frag->base.des_dst = NULL;
     frag->base.des_dst_cnt = 0;
     frag->base.des_flags = 0;
+    
 
+    
     return &frag->base;
 }
 
@@ -562,6 +599,7 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_dst(
     struct mca_btl_base_endpoint_t* endpoint,
     mca_mpool_base_registration_t* registration,
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size)
 {
@@ -606,6 +644,13 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_dst(
     frag->base.des_src = NULL;
     frag->base.des_src_cnt = 0;
     frag->base.des_flags = 0;
+
+    if(MCA_BTL_NO_ORDER == order) { 
+        frag->base.order = BTL_OPENIB_LP_QP;
+    } else { 
+        frag->base.order = order;
+    }
+
 
     BTL_VERBOSE(("frag->sg_entry.lkey = %lu .addr = %llu "
                 "frag->segment.seg_key.key32[0] = %lu",

ompi/mca/bml/bml.h
@@ -260,8 +260,8 @@ typedef struct mca_bml_base_endpoint_t mca_bml_base_endpoint_t;
     
 OMPI_DECLSPEC OBJ_CLASS_DECLARATION(mca_bml_base_endpoint_t);
 
-static inline void mca_bml_base_alloc(mca_bml_base_btl_t* bml_btl, mca_btl_base_descriptor_t** des, size_t size) { 
-    *des = bml_btl->btl_alloc(bml_btl->btl, size);
+static inline void mca_bml_base_alloc(mca_bml_base_btl_t* bml_btl, mca_btl_base_descriptor_t** des, uint8_t order, size_t size) { 
+    *des = bml_btl->btl_alloc(bml_btl->btl, order, size);
 }
 
 static inline void mca_bml_base_free(mca_bml_base_btl_t* bml_btl, mca_btl_base_descriptor_t* des) { 
@@ -316,14 +316,16 @@ static inline int mca_bml_base_get(mca_bml_base_btl_t* bml_btl, mca_btl_base_des
 static inline void mca_bml_base_prepare_src(mca_bml_base_btl_t* bml_btl, 
                                             mca_mpool_base_registration_t* reg, 
                                             struct ompi_convertor_t* conv, 
+                                            uint8_t order,
                                             size_t reserve, 
-                                            size_t *size, 
+                                            size_t *size,
                                             mca_btl_base_descriptor_t** des) { 
     *des = bml_btl->btl_prepare_src(
                                    bml_btl->btl, 
                                    bml_btl->btl_endpoint, 
                                    reg, 
                                    conv,
+                                   order,
                                    reserve,
                                    size
                                    );
@@ -335,6 +337,7 @@ static inline void mca_bml_base_prepare_src(mca_bml_base_btl_t* bml_btl,
 static inline void mca_bml_base_prepare_dst(mca_bml_base_btl_t* bml_btl, 
                                             mca_mpool_base_registration_t* reg, 
                                             struct ompi_convertor_t* conv, 
+                                            uint8_t order,
                                             size_t reserve, 
                                             size_t *size, 
                                             mca_btl_base_descriptor_t** des) { 
@@ -343,6 +346,7 @@ static inline void mca_bml_base_prepare_dst(mca_bml_base_btl_t* bml_btl,
                                    bml_btl->btl_endpoint, 
                                    reg, 
                                    conv,
+                                   order,
                                    reserve,
                                    size
                                    );
@@ -352,16 +356,19 @@ static inline void mca_bml_base_prepare_dst(mca_bml_base_btl_t* bml_btl,
 }
 
 #if OMPI_HAVE_THREAD_SUPPORT
-#define MCA_BML_BASE_BTL_DES_ALLOC(bml_btl, des, alloc_size, seg_size)  \
+#define MCA_BML_BASE_BTL_DES_ALLOC(bml_btl, des, order,                 \
+                                   alloc_size, seg_size)                \
     do {                                                                \
-        if( NULL != (des = bml_btl->btl_cache) ) {                      \
+        if( MCA_BTL_NO_ORDER == order &&                                \
+            NULL != (des = bml_btl->btl_cache) ) {                      \
             /* atomically acquire the cached descriptor */              \
             if(opal_atomic_cmpset_ptr(&bml_btl->btl_cache,              \
                                       des, NULL) == 0) {                \
-                des = bml_btl->btl_alloc(bml_btl->btl, alloc_size);     \
+                des = bml_btl->btl_alloc(bml_btl->btl, order,           \
+                                         alloc_size);                   \
             }                                                           \
         } else {                                                        \
-            des = bml_btl->btl_alloc(bml_btl->btl, alloc_size);         \
+            des = bml_btl->btl_alloc(bml_btl->btl, order, alloc_size);  \
         }                                                               \
         if(des != NULL) {                                               \
             des->des_src->seg_len = seg_size;                           \
@@ -369,12 +376,14 @@ static inline void mca_bml_base_prepare_dst(mca_bml_base_btl_t* bml_btl,
         }                                                               \
     } while(0)
 #else
-#define MCA_BML_BASE_BTL_DES_ALLOC(bml_btl, des, alloc_size, seg_size)  \
+#define MCA_BML_BASE_BTL_DES_ALLOC(bml_btl, des, order,                 \
+                                   alloc_size, seg_size)                \
     do {                                                                \
-        if( NULL != (des = bml_btl->btl_cache) ) {                      \
+        if( MCA_BTL_NO_ORDER == order &&                                \
+            NULL != (des = bml_btl->btl_cache) ) {                      \
             bml_btl->btl_cache = NULL;                                  \
         } else {                                                        \
-            des = bml_btl->btl_alloc(bml_btl->btl, alloc_size);         \
+            des = bml_btl->btl_alloc(bml_btl->btl, order, alloc_size);  \
         }                                                               \
         if(des != NULL) {                                               \
             des->des_src->seg_len = seg_size;                           \

ompi/mca/btl/btl.h
@@ -135,6 +135,8 @@ struct ompi_proc_t;
 /* send/recv operations require tag matching */
 typedef uint8_t mca_btl_base_tag_t;
 
+#define MCA_BTL_NO_ORDER       255
+
 /* reserved tag values */
 #define MCA_BTL_TAG_BTL        0
 #define MCA_BTL_TAG_PML        1
@@ -157,6 +159,9 @@ typedef uint8_t mca_btl_base_tag_t;
 
 /* btl can report put/get completion before data hits the other side */
 #define MCA_BTL_FLAGS_FAKE_RDMA 0x40
+
+/* btl needs local rdma completion */
+#define MCA_BTL_FLAGS_RDMA_COMPLETION 0x80
 
 /* Default exclusivity levels */
 #define MCA_BTL_EXCLUSIVITY_HIGH     64*1024   /* internal loopback */
@@ -212,6 +217,7 @@ struct mca_btl_base_descriptor_t {
     void* des_cbdata;
     void* des_context; 
     int32_t des_flags;
+    uint8_t order;
 };
 typedef struct mca_btl_base_descriptor_t mca_btl_base_descriptor_t;
 
@@ -449,9 +455,12 @@ typedef int (*mca_btl_base_module_register_error_fn_t)(
  *
  * @param btl (IN)      BTL module
  * @param size (IN)     Request segment size.
+ * @param order (IN)    The ordering tag (may be MCA_BTL_NO_ORDER)
  */
+
 typedef mca_btl_base_descriptor_t* (*mca_btl_base_module_alloc_fn_t)(
     struct mca_btl_base_module_t* btl,
+    uint8_t order,
     size_t size
 );
 
@@ -486,6 +495,7 @@ typedef struct mca_btl_base_descriptor_t* (*mca_btl_base_module_prepare_fn_t)(
     struct mca_btl_base_endpoint_t* endpoint,
     mca_mpool_base_registration_t* registration,
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size
 );

ompi/mca/btl/gm/btl_gm.c
@@ -202,6 +202,7 @@ int mca_btl_gm_register_error_cb(
 
 mca_btl_base_descriptor_t* mca_btl_gm_alloc(
     struct mca_btl_base_module_t* btl,
+    uint8_t order,
     size_t size)
 {
     mca_btl_gm_module_t* gm_btl = (mca_btl_gm_module_t*) btl; 
@@ -232,6 +233,7 @@ mca_btl_base_descriptor_t* mca_btl_gm_alloc(
     frag->base.des_dst = NULL;
     frag->base.des_dst_cnt = 0;
     frag->base.des_flags = 0; 
+    frag->base.order = MCA_BTL_NO_ORDER;
     return &frag->base;
 }
 
@@ -266,6 +268,7 @@ mca_btl_base_descriptor_t* mca_btl_gm_prepare_src(
     struct mca_btl_base_endpoint_t* endpoint,
     struct mca_mpool_base_registration_t* registration,
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size
 )
@@ -316,6 +319,7 @@ mca_btl_base_descriptor_t* mca_btl_gm_prepare_src(
             frag->base.des_dst = NULL;
             frag->base.des_dst_cnt = 0;
             frag->base.des_flags = 0;
+            frag->base.order = MCA_BTL_NO_ORDER;
 
             return &frag->base;
         }
@@ -358,6 +362,7 @@ mca_btl_base_descriptor_t* mca_btl_gm_prepare_src(
     frag->base.des_dst = NULL;
     frag->base.des_dst_cnt = 0;
     frag->base.des_flags = 0;
+    frag->base.order = MCA_BTL_NO_ORDER;
 
     return &frag->base;
 }
@@ -382,6 +387,7 @@ mca_btl_base_descriptor_t* mca_btl_gm_prepare_dst(
     struct mca_btl_base_endpoint_t* endpoint,
     struct mca_mpool_base_registration_t* registration,
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size)
 {
@@ -415,6 +421,8 @@ mca_btl_base_descriptor_t* mca_btl_gm_prepare_dst(
     frag->base.des_dst = &frag->segment;
     frag->base.des_dst_cnt = 1;
     frag->base.des_flags = 0;
+    frag->base.order = MCA_BTL_NO_ORDER;
+
     if(NULL == registration) {
         rc = mpool->mpool_register( mpool,
 				    frag->segment.seg_addr.pval,

ompi/mca/btl/gm/btl_gm.h
@@ -292,6 +292,7 @@ extern int mca_btl_gm_register_error_cb(
 
 extern mca_btl_base_descriptor_t* mca_btl_gm_alloc(
     struct mca_btl_base_module_t* btl, 
+    uint8_t order,
     size_t size); 
 
 
@@ -326,6 +327,7 @@ mca_btl_base_descriptor_t* mca_btl_gm_prepare_src(
     struct mca_btl_base_endpoint_t* peer,
     struct mca_mpool_base_registration_t*,
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size
 );
@@ -335,6 +337,7 @@ extern mca_btl_base_descriptor_t* mca_btl_gm_prepare_dst(
     struct mca_btl_base_endpoint_t* peer,
     struct mca_mpool_base_registration_t*,
     struct ompi_convertor_t* convertor,
+    uint8_t order,
     size_t reserve,
     size_t* size); 
 

ompi/mca/btl/mx/btl_mx.c
@@ -163,6 +163,7 @@ int mca_btl_mx_register( struct mca_btl_base_module_t* btl,
  */
 
 mca_btl_base_descriptor_t* mca_btl_mx_alloc( struct mca_btl_base_module_t* btl,
+                                             uint8_t order,
                                              size_t size )
 {
     mca_btl_mx_module_t* mx_btl = (mca_btl_mx_module_t*) btl; 
@@ -180,6 +181,8 @@ mca_btl_base_descriptor_t* mca_btl_mx_alloc( struct mca_btl_base_module_t* btl,
     frag->base.des_src = frag->segment;
     frag->base.des_src_cnt = 1;
     frag->base.des_flags = 0;
+    frag->base.order = MCA_BTL_NO_ORDER;
+
     return (mca_btl_base_descriptor_t*)frag;
 }
 
@@ -210,6 +213,7 @@ mca_btl_mx_prepare_src( struct mca_btl_base_module_t* btl,
                         struct mca_btl_base_endpoint_t* endpoint,
                         struct mca_mpool_base_registration_t* registration,
                         struct ompi_convertor_t* convertor,
+                        uint8_t order,
                         size_t reserve,
                         size_t* size )
 {
@@ -269,6 +273,8 @@ mca_btl_mx_prepare_src( struct mca_btl_base_module_t* btl,
         frag->segment[1].seg_addr.pval = iov.iov_base;
     }
     frag->base.des_src = frag->segment;
+    frag->base.order = MCA_BTL_NO_ORDER;
+
     return &frag->base;
 }
 
@@ -290,6 +296,7 @@ mca_btl_base_descriptor_t* mca_btl_mx_prepare_dst( struct mca_btl_base_module_t*
                                                    struct mca_btl_base_endpoint_t* endpoint,
                                                    struct mca_mpool_base_registration_t* registration,
                                                    struct ompi_convertor_t* convertor,
+                                                   uint8_t order,
                                                    size_t reserve,
                                                    size_t* size)
 {
@@ -323,6 +330,7 @@ mca_btl_base_descriptor_t* mca_btl_mx_prepare_dst( struct mca_btl_base_module_t*
 
     frag->base.des_dst = frag->segment;
     frag->base.des_dst_cnt = 1;
+    frag->base.order = MCA_BTL_NO_ORDER;
 
     return &frag->base;
 }

ompi/mca/btl/mx/btl_mx.h
@@ -247,6 +247,7 @@ extern int mca_btl_mx_register(
  */
 
 mca_btl_base_descriptor_t* mca_btl_mx_alloc( struct mca_btl_base_module_t* btl, 
+                                             uint8_t order,
                                              size_t size );
 
 
@@ -279,6 +280,7 @@ mca_btl_mx_prepare_src( struct mca_btl_base_module_t* btl,
                         struct mca_btl_base_endpoint_t* peer,
                         struct mca_mpool_base_registration_t*,
                         struct ompi_convertor_t* convertor,
+                        uint8_t order,
                         size_t reserve,
                         size_t* size );
 
@@ -287,6 +289,7 @@ mca_btl_mx_prepare_dst( struct mca_btl_base_module_t* btl,
                         struct mca_btl_base_endpoint_t* peer,
                         struct mca_mpool_base_registration_t*,
                         struct ompi_convertor_t* convertor,
+                        uint8_t order,
                         size_t reserve,
                         size_t* size );
 

ompi/mca/btl/openib/btl_openib.h
@@ -393,6 +393,7 @@ extern int mca_btl_openib_get(
  */
 extern mca_btl_base_descriptor_t* mca_btl_openib_alloc(
                                                        struct mca_btl_base_module_t* btl, 
+                                                       uint8_t order,
                                                        size_t size); 
     
 
@@ -419,6 +420,7 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
                                                       struct mca_btl_base_endpoint_t* peer,
                                                       mca_mpool_base_registration_t* registration, 
                                                       struct ompi_convertor_t* convertor,
+                                                      uint8_t order,
                                                       size_t reserve,
                                                       size_t* size
                                                       );
@@ -434,6 +436,7 @@ extern mca_btl_base_descriptor_t* mca_btl_openib_prepare_dst(
                                                              struct mca_btl_base_endpoint_t* peer,
                                                              mca_mpool_base_registration_t* registration, 
                                                              struct ompi_convertor_t* convertor,
+                                                             uint8_t order,
                                                              size_t reserve,
                                                              size_t* size); 
 /**

commit 33eba04a546244b202b5e81fd0867857d417d81c

Don't dereference NULL pointer. Fix bug introduced in r14768.

ompi/mca/btl/openib/btl_openib.c
@@ -380,8 +380,6 @@ mca_btl_base_descriptor_t* mca_btl_openib_alloc(
         if(order == MCA_BTL_NO_ORDER) {
             order = BTL_OPENIB_HP_QP;
         } 
-        frag->base.order = order;
-        
     } else if(size <= mca_btl_openib_component.max_send_size) { 
         if(order == MCA_BTL_NO_ORDER) { 
             order = BTL_OPENIB_LP_QP; 
@@ -390,7 +388,6 @@ mca_btl_base_descriptor_t* mca_btl_openib_alloc(
         }
         
         MCA_BTL_IB_FRAG_ALLOC_MAX(btl, frag, rc); 
-        frag->base.order = order;
     }
     
     if(NULL == frag)
@@ -398,6 +395,7 @@ mca_btl_base_descriptor_t* mca_btl_openib_alloc(
 
     frag->segment.seg_len = 
         size <= openib_btl->super.btl_eager_limit ? size : openib_btl->super.btl_eager_limit;  
+    frag->base.order = order;
     frag->base.des_flags = 0; 
     
     return (mca_btl_base_descriptor_t*)frag;
@@ -534,9 +532,7 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
         
         MCA_BTL_IB_FRAG_ALLOC_EAGER(btl, frag, rc);
         if(MCA_BTL_NO_ORDER == order) { 
-            frag->base.order = BTL_OPENIB_LP_QP;
-        } else { 
-            frag->base.order = order;
+            order = BTL_OPENIB_LP_QP;
         }
     }
 
@@ -549,7 +545,6 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
             return NULL;
         } 
         MCA_BTL_IB_FRAG_ALLOC_MAX(btl, frag, rc);
-        frag->base.order = order;
 
         if(NULL == frag) {
             return NULL;
@@ -569,6 +564,7 @@ mca_btl_base_descriptor_t* mca_btl_openib_prepare_src(
     *size  = max_data;
     frag->segment.seg_len = max_data + reserve;
     frag->segment.seg_key.key32[0] = (uint32_t)frag->sg_entry.lkey;
+    frag->base.order = order;
     frag->base.des_src = &frag->segment;
     frag->base.des_src_cnt = 1;
     frag->base.des_dst = NULL;