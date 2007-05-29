632e36594e903ca6c6e48c74ddfbe28f13101aff
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2007-05-29 18:40:07 +0000

    The idea behind this patch is to decrease the number of strcmp used in the replica by using a small hash function before doing the strcmp. The hask key for each registry entry is computed when it is added to the registry. When we're doing a query, instead of comparing the 2 strings we first check if the hash key match, and if they do match then we compare the 2 strings in order to make sure we eliminate collisions from our answers.
    There is some benefit in terms of performance. It's hardly visible for few
    processes, but it start showing up when the number of processes increase. In fact
    the number of strcmp in the trace file drastically decrease. The main reason it
    works well, is because most of the keys start with basically the same chars
    (such as orte-blahblah) which transform the strcmp on a loop over few chars.

--- orte/mca/gpr/replica/transition_layer/gpr_replica_dict_tl.c
+++ orte/mca/gpr/replica/transition_layer/gpr_replica_dict_tl.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2007 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
@@ -27,23 +27,24 @@
 
 #include "orte_config.h"
 
-#include "orte/class/orte_pointer_array.h"
+#include "opal/hash_string.h"
 #include "opal/util/output.h"
 #include "opal/util/trace.h"
 
+#include "orte/class/orte_pointer_array.h"
 #include "orte/mca/errmgr/errmgr.h"
-
+#include "orte/mca/gpr/replica/gpr_replica.h"
 #include "orte/mca/gpr/replica/functional_layer/gpr_replica_fn.h"
-
 #include "orte/mca/gpr/replica/transition_layer/gpr_replica_tl.h"
 
 int
 orte_gpr_replica_create_itag(orte_gpr_replica_itag_t *itag,
                              orte_gpr_replica_segment_t *seg, char *name)
 {
-    char **ptr, *new_dict;
-    orte_gpr_replica_itag_t j;
-    orte_std_cntr_t i, len, len2, index;
+    orte_gpr_replica_dict_entry_t* new_dict;
+    orte_std_cntr_t index;
+    uint32_t len, hash_key;
+    int rc;
 
     OPAL_TRACE(3);
 
@@ -56,29 +57,19 @@ orte_gpr_replica_create_itag(orte_gpr_replica_itag_t *itag,
         return ORTE_ERR_BAD_PARAM;
     }
 
-    len = (orte_std_cntr_t)strlen(name);
-    
-    /* check seg's dictionary to ensure uniqueness */
-    ptr = (char**)(seg->dict)->addr;
-    for (i=0, j=0; j < seg->num_dict_entries &&
-                   i < (seg->dict)->size; i++) {
-        if (NULL != ptr[i]) {
-            j++;
-            len2 = (orte_std_cntr_t)strlen(ptr[i]);
-            if ((len == len2 && 0 == strncmp(ptr[i], name, len))) {
-                /* already present */
-                if (i < ORTE_GPR_REPLICA_ITAG_MAX) {
-                    *itag = (orte_gpr_replica_itag_t)i;
-                    return ORTE_SUCCESS;
-                }
-                /* otherwise, the itag violates the max value */
-                return ORTE_ERR_BAD_PARAM;
-            }
-        }
+    if( ORTE_ERR_NOT_FOUND != (rc = orte_gpr_replica_dict_lookup(itag, seg, name))) {
+        return rc;
     }
 
+    OPAL_HASH_STRLEN( name, hash_key, len );
+    
     /* okay, name is unique - create dictionary entry */
-    new_dict = strdup(name);
+    new_dict = (orte_gpr_replica_dict_entry_t*)malloc( sizeof(orte_gpr_replica_dict_entry_t) + len );
+    new_dict->hash_key = hash_key;
+    new_dict->length = len;
+    strncpy( new_dict->string, name, len );
+    new_dict->string[len] = '\0';
+
     if (0 > orte_pointer_array_add(&index, seg->dict, (void*)new_dict)) {
         free(new_dict);
         ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE);
@@ -93,7 +84,8 @@ orte_gpr_replica_create_itag(orte_gpr_replica_itag_t *itag,
     
     /* otherwise, the itag violates the max value */
     free(new_dict);
-    ptr[index] = NULL;
+    /* remove itag from segment dictionary */
+    orte_pointer_array_set_item(seg->dict, (orte_std_cntr_t)index, NULL);
     
     return ORTE_ERR_OUT_OF_RESOURCE;
 }
@@ -101,7 +93,7 @@ orte_gpr_replica_create_itag(orte_gpr_replica_itag_t *itag,
 
 int orte_gpr_replica_delete_itag(orte_gpr_replica_segment_t *seg, char *name)
 {
-    char **ptr;
+    orte_gpr_replica_dict_entry_t **ptr;
     orte_gpr_replica_itag_t itag;
     int rc;
 
@@ -123,19 +115,19 @@ int orte_gpr_replica_delete_itag(orte_gpr_replica_segment_t *seg, char *name)
     /* need to search this segment's registry to find all instances
      * that name & delete them
      */
-     if (ORTE_SUCCESS != (rc = orte_gpr_replica_purge_itag(seg, itag))) {
+    if (ORTE_SUCCESS != (rc = orte_gpr_replica_purge_itag(seg, itag))) {
         ORTE_ERROR_LOG(rc);
         return rc;
-     }
+    }
 
-     /* free the dictionary element data */
-     ptr = (char**)((seg->dict)->addr);
-     if (NULL == ptr[itag]) {  /* dict element no longer valid */
-         return ORTE_ERR_NOT_FOUND;
-     }
-     free(ptr[itag]);
+    /* free the dictionary element data */
+    ptr = (orte_gpr_replica_dict_entry_t**)(seg->dict->addr);
+    if (NULL == ptr[itag]) {  /* dict element no longer valid */
+        return ORTE_ERR_NOT_FOUND;
+    }
+    free(ptr[itag]);
      
-     /* remove itag from segment dictionary */
+    /* remove itag from segment dictionary */
     orte_pointer_array_set_item(seg->dict, (orte_std_cntr_t)itag, NULL);
     
     /* decrease the dict counter */
@@ -149,10 +141,10 @@ int
 orte_gpr_replica_dict_lookup(orte_gpr_replica_itag_t *itag,
                              orte_gpr_replica_segment_t *seg, char *name)
 {
-    char **ptr;
+    orte_gpr_replica_dict_entry_t **ptr;
     orte_std_cntr_t i;
     orte_gpr_replica_itag_t j;
-    orte_std_cntr_t len, len2;
+    uint32_t hash_key, len;
     
     OPAL_TRACE(3);
 
@@ -168,27 +160,26 @@ orte_gpr_replica_dict_lookup(orte_gpr_replica_itag_t *itag,
     if (NULL == name) { /* just want segment token-itag pair */
         *itag = seg->itag;
         return ORTE_SUCCESS;
-	}
+    }
 
-    len = (orte_std_cntr_t)strlen(name);
-    
-    /* want specified token-itag pair in that segment's dictionary */
-    ptr = (char**)((seg->dict)->addr);
+    OPAL_HASH_STRLEN( name, hash_key, len );
+
+    ptr = (orte_gpr_replica_dict_entry_t**)(seg->dict)->addr;
     for (i=0, j=0; j < seg->num_dict_entries &&
                    i < (seg->dict)->size; i++) {
-        if (NULL != ptr[i]) {
-            j++;
-            len2 = (orte_std_cntr_t)strlen(ptr[i]);
-	        if (len == len2 && 0 == strncmp(ptr[i], name, len)) {
-               if (i < ORTE_GPR_REPLICA_ITAG_MAX) {
-                   *itag = (orte_gpr_replica_itag_t)i;
-                   return ORTE_SUCCESS;
-               }
-               /* otherwise, the itag violates the max value */
-               return ORTE_ERR_BAD_PARAM;
+        if (NULL == ptr[i]) continue;
+        j++;
+        if( (len == ptr[i]->length) && (hash_key == ptr[i]->hash_key)
+            && (0 == strncmp(ptr[i]->string, name, len)) ) {
+            /* already present */
+            if (i < ORTE_GPR_REPLICA_ITAG_MAX) {
+                *itag = (orte_gpr_replica_itag_t)i;
+                return ORTE_SUCCESS;
             }
+            /* otherwise, the itag violates the max value */
+            return ORTE_ERR_BAD_PARAM;
         }
-	}
+    }
 
     return ORTE_ERR_NOT_FOUND; /* couldn't find the specified entry */
 }
@@ -197,7 +188,7 @@ orte_gpr_replica_dict_lookup(orte_gpr_replica_itag_t *itag,
 int orte_gpr_replica_dict_reverse_lookup(char **name,
         orte_gpr_replica_segment_t *seg, orte_gpr_replica_itag_t itag)
 {
-    char **ptr;
+    orte_gpr_replica_dict_entry_t **ptr;
     orte_gpr_replica_segment_t **segptr;
 
     OPAL_TRACE(3);
@@ -212,34 +203,34 @@ int orte_gpr_replica_dict_reverse_lookup(char **name,
     }
     
     if (NULL == seg) {
-	   /* return the segment name
-        * note that itag is the index of the segment in that array
-        */
+        /* return the segment name
+         * note that itag is the index of the segment in that array
+         */
         segptr = (orte_gpr_replica_segment_t**)(orte_gpr_replica.segments->addr);
         if (NULL == segptr[itag]) { /* this segment is no longer alive */
             return ORTE_ERR_NOT_FOUND;
         }
-	   *name = strdup(segptr[itag]->name);
-	   return ORTE_SUCCESS;
+        *name = strdup(segptr[itag]->name);
+        return ORTE_SUCCESS;
     }
 
     /* seg is provided - find the matching token for this itag
      * note again that itag is the index into this segment's
      * dictionary array
      */
-    ptr = (char**)((seg->dict)->addr);
+    ptr = (orte_gpr_replica_dict_entry_t**)((seg->dict)->addr);
     if (NULL == ptr[itag]) { /* this entry is no longer valid! */
         return ORTE_ERR_NOT_FOUND;
     }
-    *name = strdup(ptr[itag]);
+    *name = strdup(ptr[itag]->string);
     
     return ORTE_SUCCESS;
 }
 
 int
-orte_gpr_replica_get_itag_list(orte_gpr_replica_itag_t **itaglist,
-                    orte_gpr_replica_segment_t *seg, char **names,
-                    orte_std_cntr_t *num_names)
+orte_gpr_replica_get_itag_list( orte_gpr_replica_itag_t **itaglist,
+                                orte_gpr_replica_segment_t *seg, char **names,
+                                orte_std_cntr_t *num_names )
 {
     char **namptr;
     int rc;
@@ -257,15 +248,15 @@ orte_gpr_replica_get_itag_list(orte_gpr_replica_itag_t **itaglist,
     
     /* check for wild-card case */
     if (NULL == names) {
-	   return ORTE_SUCCESS;
+        return ORTE_SUCCESS;
     }
 
     if (0 >= (*num_names)) { /* NULL-terminated list - count them */
         *num_names = 0;
         namptr = names;
         while (NULL != *namptr) {
-	       *num_names = (*num_names) + 1;
-	       namptr++;
+            *num_names = (*num_names) + 1;
+            namptr++;
         }
     }
 
@@ -277,14 +268,13 @@ orte_gpr_replica_get_itag_list(orte_gpr_replica_itag_t **itaglist,
 
     namptr = names;
 
-    for (i=0; i < (*num_names); i++) {  /* traverse array of names - ignore any NULL's */
-        if (NULL != names[i]) {
-            if (ORTE_SUCCESS != (rc = orte_gpr_replica_create_itag(&((*itaglist)[i]), seg, names[i]))) {
-                ORTE_ERROR_LOG(rc);
-                free(*itaglist);
-                *itaglist = NULL;
-                return rc;
-            }
+    for( i = 0; i < (*num_names); i++ ) {  /* traverse array of names - ignore any NULL's */
+        if (NULL == names[i]) continue; 
+        if (ORTE_SUCCESS != (rc = orte_gpr_replica_create_itag(&((*itaglist)[i]), seg, names[i]))) {
+            ORTE_ERROR_LOG(rc);
+            free(*itaglist);
+            *itaglist = NULL;
+            return rc;
         }
     }
     return ORTE_SUCCESS;
