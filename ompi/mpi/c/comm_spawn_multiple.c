ba4cb28db1028840a5df01cbeab9c870ad6af33e
Shiqing Fan <shiqing@open-mpi-git-mirror.example.com>
2007-06-05 14:32:27 +0000

    The label name "ERROR" is defined as a flag in Visual Studio platformSDK. Using  "ERROR" as a label causes conflicts. Changing it into lowercase will solve the problem.

--- ompi/mpi/c/comm_spawn_multiple.c
+++ ompi/mpi/c/comm_spawn_multiple.c
@@ -110,7 +110,7 @@ int MPI_Comm_spawn_multiple(int count, char **array_of_commands, char ***array_o
         if (OMPI_SUCCESS != (rc = ompi_comm_start_processes(count, array_of_commands,
                                                             array_of_argv, array_of_maxprocs,
                                                             array_of_info, port_name))) {
-            goto ERROR;
+            goto error;
         }
         tmp_port = ompi_parse_port (port_name, &tag);
         free(tmp_port);
@@ -118,7 +118,7 @@ int MPI_Comm_spawn_multiple(int count, char **array_of_commands, char ***array_o
 
     rc = ompi_comm_connect_accept (comm, root, NULL, send_first, &newcomp, tag);
 
-ERROR:
+error:
     /* close the port again. Nothing has to be done for that at the moment.*/
 
     /* set array of errorcodes */
