fbccd9edd624501a32c21bffd7a9d8c8846ebf5e
Galen Shipman <gshipman@open-mpi-git-mirror.example.com>
2007-06-11 20:19:31 +0000

    handle MPI_STATUS_IGNORE in iprobe for the MTLs

--- ompi/mca/mtl/psm/mtl_psm_probe.c
+++ ompi/mca/mtl/psm/mtl_psm_probe.c
@@ -41,11 +41,12 @@ int ompi_mtl_psm_iprobe(struct mca_mtl_base_module_t* mtl,
     err = psm_mq_iprobe(ompi_mtl_psm.mq, mqtag, tagsel, &mqstat);
     if (err == PSM_OK) {
 	*flag = 1;
-	status->MPI_SOURCE = PSM_GET_MQRANK(mqstat.msg_tag);
-	status->MPI_TAG = PSM_GET_MQUTAG(mqstat.msg_tag);
-        status->_count = mqstat.nbytes;
+	if(MPI_STATUS_IGNORE != status) { 
+            status->MPI_SOURCE = PSM_GET_MQRANK(mqstat.msg_tag);
+            status->MPI_TAG = PSM_GET_MQUTAG(mqstat.msg_tag);
+            status->_count = mqstat.nbytes;
 
-	switch (mqstat.error_code) {
+            switch (mqstat.error_code) {
 	    case PSM_OK:
 		status->MPI_ERROR = OMPI_SUCCESS;
 		break;
@@ -54,8 +55,10 @@ int ompi_mtl_psm_iprobe(struct mca_mtl_base_module_t* mtl,
 		break;
 	    default:
 		status->MPI_ERROR = MPI_ERR_INTERN;
-	}
-	return OMPI_SUCCESS;
+            }
+        }
+        
+        return OMPI_SUCCESS;
     }
     else if (err == PSM_MQ_INCOMPLETE) {
 	*flag = 0;
