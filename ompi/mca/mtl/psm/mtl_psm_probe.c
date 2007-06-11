406b05bdc3e4aa4f852d1f94c0d1335d65624219
Galen Shipman <gshipman@lanl.gov>
2007-06-11 21:17:49 +0000

    update copyright..
    This commit was SVN r15003.

--- ompi/mca/mtl/psm/mtl_psm_probe.c
+++ ompi/mca/mtl/psm/mtl_psm_probe.c
@@ -10,6 +10,8 @@
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2006      QLogic Corporation. All rights reserved.
+ * Copyright (c) 2006-2007 Los Alamos National Security, LLC.  All rights
+ *                         reserved. 
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
