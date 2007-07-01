003d1b86659708c20a8528aec16369a5b088a524
George Bosilca <bosilca@icl.utk.edu>
2007-07-01 17:51:34 +0000

    On windows make sure we add the initial path separator only if we are creating a relative path. Use BEGIN_C_DECLS and END_C_DECLS on some header files.
    This commit was SVN r15254.

--- opal/util/basename.h
+++ opal/util/basename.h
@@ -25,85 +25,82 @@
 #ifndef OPAL_BASENAME_H
 #define OPAL_BASENAME_H
 
-#if defined(c_plusplus) || defined(__cplusplus)
-extern "C" {
-#endif
-    /**
-     * Return the basename of a filename.
-     *
-     * @param filename The filename to examine
-     *
-     * @returns A string containing the basename, or NULL if there is an error
-     *
-     * The contents of the \em filename parameter are unchanged.  This
-     * function returns a new string containing the basename of the
-     * filename (which must be eventually freed by the caller), or
-     * NULL if there is an error.  Trailing "/" characters in the
-     * filename do not count, unless it is in the only part of the
-     * filename (e.g., "/" or "C:\").
-     *
-     * This function will do the Right Things on POSIX and
-     * Windows-based operating systems.  For example:
-     *
-     * foo.txt returns "foo.txt"
-     * 
-     * /foo/bar/baz returns "baz"
-     *
-     * /yow.c returns "yow.c"
-     *
-     * / returns "/"
-     *
-     * C:\foo\bar\baz returns "baz"
-     *
-     * D:foo.txt returns "foo.txt"
-     *
-     * E:\yow.c returns "yow.c"
-     *
-     * F: returns "F:"
-     *
-     * G:\ returns "G:"
-     */
-    OPAL_DECLSPEC char *opal_basename(const char* filename);
+BEGIN_C_DECLS
 
-    /**
-     * Return the dirname of a filename.
-     *
-     * @param filename The filename to examine
-     *
-     * @returns A string containing the dirname, or NULL if there is an error
-     *
-     * The contents of the \em filename parameter are unchanged.  This
-     * function returns a new string containing the dirname of the
-     * filename (which must be eventually freed by the caller), or
-     * NULL if there is an error.  Trailing "/" characters in the
-     * filename do not count, unless it is in the only part of the
-     * filename (e.g., "/" or "C:\").
-     *
-     * This function will do the Right Things on POSIX and
-     * Windows-based operating systems.  For example:
-     *
-     * foo.txt returns "foo.txt"
-     * 
-     * /foo/bar/baz returns "/foo/bar"
-     *
-     * /yow.c returns "/"
-     *
-     * / returns ""
-     *
-     * C:\foo\bar\baz returns "C:\foo\bar"
-     *
-     * D:foo.txt returns "D:"
-     *
-     * E:\yow.c returns "E:"
-     *
-     * F: returns ""
-     *
-     * G:\ returns ""
-     */
-    OPAL_DECLSPEC char *opal_dirname(const char* filename);
+/**
+ * Return the basename of a filename.
+ *
+ * @param filename The filename to examine
+ *
+ * @returns A string containing the basename, or NULL if there is an error
+ *
+ * The contents of the \em filename parameter are unchanged.  This
+ * function returns a new string containing the basename of the
+ * filename (which must be eventually freed by the caller), or
+ * NULL if there is an error.  Trailing "/" characters in the
+ * filename do not count, unless it is in the only part of the
+ * filename (e.g., "/" or "C:\").
+ *
+ * This function will do the Right Things on POSIX and
+ * Windows-based operating systems.  For example:
+ *
+ * foo.txt returns "foo.txt"
+ * 
+ * /foo/bar/baz returns "baz"
+ *
+ * /yow.c returns "yow.c"
+ *
+ * / returns "/"
+ *
+ * C:\foo\bar\baz returns "baz"
+ *
+ * D:foo.txt returns "foo.txt"
+ *
+ * E:\yow.c returns "yow.c"
+ *
+ * F: returns "F:"
+ *
+ * G:\ returns "G:"
+ */
+OPAL_DECLSPEC char *opal_basename(const char* filename);
 
-#if defined(c_plusplus) || defined(__cplusplus)
-}
-#endif
+/**
+ * Return the dirname of a filename.
+ *
+ * @param filename The filename to examine
+ *
+ * @returns A string containing the dirname, or NULL if there is an error
+ *
+ * The contents of the \em filename parameter are unchanged.  This
+ * function returns a new string containing the dirname of the
+ * filename (which must be eventually freed by the caller), or
+ * NULL if there is an error.  Trailing "/" characters in the
+ * filename do not count, unless it is in the only part of the
+ * filename (e.g., "/" or "C:\").
+ *
+ * This function will do the Right Things on POSIX and
+ * Windows-based operating systems.  For example:
+ *
+ * foo.txt returns "foo.txt"
+ * 
+ * /foo/bar/baz returns "/foo/bar"
+ *
+ * /yow.c returns "/"
+ *
+ * / returns ""
+ *
+ * C:\foo\bar\baz returns "C:\foo\bar"
+ *
+ * D:foo.txt returns "D:"
+ *
+ * E:\yow.c returns "E:"
+ *
+ * F: returns ""
+ *
+ * G:\ returns ""
+ */
+OPAL_DECLSPEC char *opal_dirname(const char* filename);
+
+END_C_DECLS
 
 #endif /* OPAL_BASENAME_H */
