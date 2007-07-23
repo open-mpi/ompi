21a76703907649e3e435378fa85b89f38d034dcb
George Bosilca <bosilca@icl.utk.edu>
2007-07-23 19:07:13 +0000

    Update the elan BTL. Now we support the following protocols: send, put and partially get.
    This commit was SVN r15564.

--- ompi/mca/btl/elan/btl_elan_proc.c
+++ ompi/mca/btl/elan/btl_elan_proc.c
@@ -1,5 +1,6 @@
+
 /*
- * Copyright (c) 2004-2007 The University of Tennessee and The University
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * $COPYRIGHT$
@@ -11,17 +12,18 @@
 
 #include "ompi_config.h"
 #include "orte/class/orte_proc_table.h"
-#include "opal/class/opal_hash_table.h"
+#include "opal/class/opal_hash_table.h" 
 #include "ompi/runtime/ompi_module_exchange.h"
-
 #include "btl_elan.h"
 #include "btl_elan_proc.h"
+#include "orte/util/proc_info.h"
+#include "ompi/communicator/communicator.h"
 
 static void mca_btl_elan_proc_construct(mca_btl_elan_proc_t* proc);
 static void mca_btl_elan_proc_destruct(mca_btl_elan_proc_t* proc);
 
 OBJ_CLASS_INSTANCE(mca_btl_elan_proc_t, 
-                   opal_list_item_t, mca_btl_elan_proc_construct, 
+                   opal_list_item_t,mca_btl_elan_proc_construct, 
                    mca_btl_elan_proc_destruct);
 
 void mca_btl_elan_proc_construct(mca_btl_elan_proc_t* proc)
@@ -47,7 +49,6 @@ void mca_btl_elan_proc_destruct(mca_btl_elan_proc_t* proc)
     OPAL_THREAD_LOCK(&mca_btl_elan_component.elan_lock);
     opal_list_remove_item(&mca_btl_elan_component.elan_procs, &proc->super);
     OPAL_THREAD_UNLOCK(&mca_btl_elan_component.elan_lock);
-
     /* release resources */
     if(NULL != proc->proc_endpoints) {
         free(proc->proc_endpoints);
@@ -62,24 +63,18 @@ void mca_btl_elan_proc_destruct(mca_btl_elan_proc_t* proc)
 static mca_btl_elan_proc_t* mca_btl_elan_proc_lookup_ompi(ompi_proc_t* ompi_proc)
 {
     mca_btl_elan_proc_t* elan_proc;
-
     OPAL_THREAD_LOCK(&mca_btl_elan_component.elan_lock);
-
     for(elan_proc = (mca_btl_elan_proc_t*)
-            opal_list_get_first(&mca_btl_elan_component.elan_procs);
+        opal_list_get_first(&mca_btl_elan_component.elan_procs);
         elan_proc != (mca_btl_elan_proc_t*)
-            opal_list_get_end(&mca_btl_elan_component.elan_procs);
+        opal_list_get_end(&mca_btl_elan_component.elan_procs);
         elan_proc  = (mca_btl_elan_proc_t*)opal_list_get_next(elan_proc)) {
-
         if(elan_proc->proc_ompi == ompi_proc) {
             OPAL_THREAD_UNLOCK(&mca_btl_elan_component.elan_lock);
             return elan_proc;
         }
-
     }
-
     OPAL_THREAD_UNLOCK(&mca_btl_elan_component.elan_lock);
-
     return NULL;
 }
 
@@ -96,22 +91,18 @@ mca_btl_elan_proc_t* mca_btl_elan_proc_create(ompi_proc_t* ompi_proc)
     int rc;
     size_t size;
     mca_btl_elan_proc_t* module_proc = NULL;
-
     /* Check if we have already created a Elan proc
      * structure for this ompi process */
     module_proc = mca_btl_elan_proc_lookup_ompi(ompi_proc);
-
     if(module_proc != NULL) {
         /* Gotcha! */
         return module_proc;
     }
-
     /* Oops! First time, gotta create a new Elan proc
      * out of the ompi_proc ... */
-
     module_proc = OBJ_NEW(mca_btl_elan_proc_t);
-    if(NULL == module_proc)
-        return NULL;
+	if(NULL == module_proc)
+            return NULL;
     /* Initialize number of peer */
     module_proc->proc_endpoint_count = 0;
     module_proc->proc_ompi = ompi_proc;
@@ -119,33 +110,24 @@ mca_btl_elan_proc_t* mca_btl_elan_proc_create(ompi_proc_t* ompi_proc)
     /* build a unique identifier (of arbitrary
      * size) to represent the proc */
     module_proc->proc_guid = ompi_proc->proc_name;
-
-    /* Elan module doesn't have addresses exported at
-     * initialization, so the addr_count is set to one. */
-   
-    /**
-       orte_hash_table_set_proc( &mca_btl_elan_component.elan_procs,
-       &module_proc->proc_guid,
-       module_proc );*/
-
     rc = ompi_modex_recv( &mca_btl_elan_component.super.btl_version,
                           ompi_proc,
                           (void**)&module_proc->elan_vp_array,
                           &size );
     if(rc != OMPI_SUCCESS) {
-        BTL_ERROR(("mca_base_modex_recv: failed with return value=%d", rc));
-        OBJ_RELEASE(module_proc);
-        return NULL;
+            BTL_ERROR(("mca_base_modex_recv: failed with return value=%d", rc));
+            OBJ_RELEASE(module_proc);
+            return NULL;
     }
     module_proc->proc_addr_count = size / sizeof(unsigned int);;
     /* XXX: Right now, there can be only 1 peer associated
      * with a proc. Needs a little bit change in 
      * mca_btl_elan_proc_t to allow on demand increasing of
-     * number of endpoints for this proc */
+     * number of endpoints for this proc 
+    */
 
     module_proc->proc_endpoints = (mca_btl_base_endpoint_t**)
-        malloc((1+module_proc->proc_addr_count )* sizeof(mca_btl_base_endpoint_t*));
-
+    malloc((1+module_proc->proc_addr_count )* sizeof(mca_btl_base_endpoint_t*));
     if(NULL == module_proc->proc_endpoints) {
         OBJ_RELEASE(module_proc);
         return NULL;
@@ -160,16 +142,15 @@ mca_btl_elan_proc_t* mca_btl_elan_proc_create(ompi_proc_t* ompi_proc)
  * it an address.
  */
 int mca_btl_elan_proc_insert( mca_btl_elan_proc_t* module_proc, 
-                              mca_btl_base_endpoint_t* module_endpoint )
+			       mca_btl_base_endpoint_t* module_endpoint )
 {
     /* insert into endpoint array */
     size_t i;
-	
     module_endpoint->endpoint_proc = module_proc;
     module_proc->proc_endpoints[module_proc->proc_endpoint_count++] = module_endpoint;
-
     for( i = 0; i < module_proc->proc_addr_count; i++ ) {
         module_endpoint->elan_vp = module_proc->elan_vp_array[i];
+        /* module_endpoint->elan_vp = ompi_comm_rank(&ompi_mpi_comm_world);*/
         return OMPI_SUCCESS;
     }
     return OMPI_SUCCESS;
