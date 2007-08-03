e41ee17ca50830d4bee2ae09e5e573e8a1523794
George Bosilca <bosilca@icl.utk.edu>
2007-08-03 23:59:29 +0000

    Add a small comment that hopefully will enforce the correct ordering of the fields between CM and the other PML in the requests structure.
    This commit was SVN r15760.

--- ompi/mca/pml/cm/pml_cm_request.h
+++ ompi/mca/pml/cm/pml_cm_request.h
@@ -39,6 +39,8 @@ typedef enum {
  *  Base type for PML CM P2P requests 
  */
 struct mca_pml_cm_request_t { 
+
+/* START: These fields have to match the definition of the mca_pml_base_request_t */
     ompi_request_t req_ompi;              /**< base request */
     volatile bool req_pml_complete;       /**< flag indicating if the pt-2-pt layer is done with this request */
     mca_pml_cm_request_type_t req_pml_type;
@@ -46,6 +48,7 @@ struct mca_pml_cm_request_t {
     struct ompi_datatype_t *req_datatype; /**< pointer to data type */
     volatile bool req_free_called;        /**< flag indicating if the user has freed this request */
     ompi_convertor_t req_convertor;       /**< convertor that describes the memory layout */
+/* END: These fields have to match the definition of the mca_pml_base_request_t */
 };
 typedef struct mca_pml_cm_request_t mca_pml_cm_request_t;
 OMPI_DECLSPEC OBJ_CLASS_DECLARATION(mca_pml_cm_request_t);
