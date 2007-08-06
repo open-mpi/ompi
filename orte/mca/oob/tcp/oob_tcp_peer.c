0306eb747ff23663483c854a240eec51e793f907
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2007-08-06 22:55:41 +0000

    Fix issue where we set state to SHUTDOWN rather than CONNECTING when we had to switch socket types.

--- orte/mca/oob/tcp/oob_tcp_peer.c
+++ orte/mca/oob/tcp/oob_tcp_peer.c
@@ -277,7 +277,9 @@ mca_oob_tcp_peer_create_socket(mca_oob_tcp_peer_t* peer,
     if (peer->peer_current_af == af_family && peer->peer_sd > 0) {
         return ORTE_SUCCESS;
     } else if (peer->peer_sd > 0) {
+        int state = peer->peer_state;
         mca_oob_tcp_peer_shutdown(peer);
+        peer->peer_state = state;
     }
 
     peer->peer_sd = socket(af_family, SOCK_STREAM, 0);
