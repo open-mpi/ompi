39bdb032cc8f0a2cef7634e97da7e9e8eaed9372
Andrew Friedley <afriedle@open-mpi-git-mirror.example.com>
2007-08-16 20:49:27 +0000

    Fixes #1047
    Tie stdin to /dev/null to prevent stdin from being closed and thus making stdin not work in slurm allocations.

--- orte/mca/pls/slurm/pls_slurm_module.c
+++ orte/mca/pls/slurm/pls_slurm_module.c
@@ -584,10 +584,14 @@ static int pls_slurm_start_proc(int argc, char **argv, char **env,
             free(newenv);
         }
 
+        fd = open("/dev/null", O_CREAT|O_WRONLY|O_TRUNC, 0666);
+        if(fd > 0) {
+            dup2(fd, 0);
+        }
+
         /* When not in debug mode and --debug-daemons was not passed,
          * tie stdout/stderr to dev null so we don't see messages from orted */
         if (0 == mca_pls_slurm_component.debug && !orte_debug_daemons_flag) {
-            fd = open("/dev/null", O_CREAT|O_WRONLY|O_TRUNC, 0666);
             if (fd >= 0) {
                 if (fd != 1) {
                     dup2(fd,1);
@@ -595,10 +599,11 @@ static int pls_slurm_start_proc(int argc, char **argv, char **env,
                 if (fd != 2) {
                     dup2(fd,2);
                 }
-                if (fd > 2) {
-                    close(fd);
-                }
             }
+        }
+
+        if (fd > 2) {
+            close(fd);
         }
 
         /* get the srun process out of orterun's process group so that
