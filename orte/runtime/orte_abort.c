d47ea8918127b43de2940cfd964d66479590c7b8
Jeff Squyres <jsquyres@cisco.com>
2007-12-11 12:55:08 +0000

    George rightly pointed out that this should be 0600, not 0660.
    This commit was SVN r16927.

--- orte/runtime/orte_abort.c
+++ orte/runtime/orte_abort.c
@@ -94,7 +94,7 @@ void orte_abort(int status, bool report)
             ORTE_ERROR_LOG(ORTE_ERR_OUT_OF_RESOURCE);
             goto CLEANUP;
         }
-        fd = open(abort_file, O_CREAT, 0660);
+        fd = open(abort_file, O_CREAT, 0600);
         if (0 < fd) close(fd);        
     }
     
