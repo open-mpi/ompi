76b13f91b917db14bace6218a8288735935fa2b8
Aurelien Bouteiller <bouteill@icl.utk.edu>
2008-01-23 23:54:02 +0000

    fixed link:wq error in static mode
    This commit was SVN r17194.

--- ompi/mca/pml/v/pml_v.h
+++ ompi/mca/pml/v/pml_v.h
@@ -18,15 +18,15 @@
 BEGIN_C_DECLS
 
 struct mca_pml_v_t {
-    mca_pml_base_component_t            host_pml_component;
-    mca_pml_base_module_t               host_pml;
     ompi_request_fns_t                  host_request_fns;
     size_t                              host_pml_req_recv_size;
     size_t                              host_pml_req_send_size;
+    mca_pml_base_component_t            host_pml_component;
+    mca_pml_base_module_t               host_pml;
 };
 typedef struct mca_pml_v_t mca_pml_v_t;
 
-OMPI_MODULE_DECLSPEC extern mca_pml_v_t mca_pml_v;
+OMPI_DECLSPEC extern mca_pml_v_t mca_pml_v;
 OMPI_DECLSPEC extern mca_pml_base_component_1_0_0_t mca_pml_v_component;
 
 END_C_DECLS
