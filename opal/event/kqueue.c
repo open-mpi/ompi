4f104ba5d1f74004a8ff50923e124add26c33c05
Brad Penoff <penoff@cs.ubc.ca>
2008-05-03 23:07:45 +0000

    Add header for FreeBSD.
    This commit was SVN r18366.

--- opal/event/kqueue.c
+++ opal/event/kqueue.c
@@ -52,6 +52,15 @@
 #ifdef HAVE_UTIL_H
 #include <util.h>
 #endif
+#ifdef HAVE_SYS_IOCTL_H
+#include <sys/ioctl.h>
+#endif
+#ifdef HAVE_LIBUTIL_H
+#include <libutil.h>
+#endif
+#ifdef HAVE_TERMIOS_H
+#include <termios.h>
+#endif
 
 /* Some platforms apparently define the udata field of struct kevent as
  * intptr_t, whereas others define it as void*.  There doesn't seem to be an
