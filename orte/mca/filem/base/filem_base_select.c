50c909a23dc64c61282227990a129fd380d848b7
Josh Hursey <jjhursey@open-mpi.org>
2008-05-06 21:57:45 +0000

    Fix a bit of selection logic. Filem should not fail select if the user decided not to build with any filem components. This matches the logic before the mca_base_select() change.
    This commit was SVN r18389.

--- orte/mca/filem/base/filem_base_select.c
+++ orte/mca/filem/base/filem_base_select.c
@@ -85,12 +85,17 @@ int orte_filem_base_select(void)
                                    false, false,
                                    NULL, &include_list);
 
-    if(NULL != include_list && 0 == strncmp(include_list, "none", strlen("none")) ){ 
-        opal_output_verbose(10, orte_filem_base_output,
-                            "filem:select: Using %s component",
-                            include_list);
+    /* If we do not have any components to select this is ok. The user likely
+     * decided not to build with filem components. Just use the none
+     * component and move on.
+     */
+    if( 0 >= opal_list_get_size(&orte_filem_base_components_available) || 
+        (NULL != include_list && 0 == strncmp(include_list, "none", strlen("none")) ) ) { 
+        opal_output_verbose(1, orte_filem_base_output,
+                            "filem:select: Warning: Using none component. Some functionality (e.g., --preload-binary) will not work in this mode.");
         best_component = &none_component;
         best_module    = &none_module;
+
         /* JJH: Todo: Check if none is in the list */
         /* Close all components since none will be used */
         mca_base_components_close(0, /* Pass 0 to keep this from closing the output handle */
@@ -108,7 +113,7 @@ int orte_filem_base_select(void)
                                                (mca_base_component_t **) &best_component) ) ) {
         /* This will only happen if no component was selected */
         exit_status = ORTE_ERROR;
-        goto cleanup;
+        goto skip_select;
     }
 
  skip_select:
