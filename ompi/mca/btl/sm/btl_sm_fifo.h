6844ff32ba17c9b38129cfeb89160e6ea9e021de
Gleb Natapov <gleb@voltaire.com>
2008-05-12 07:15:29 +0000

    Return OMPI_ERR_RESOURCE_BUSY from sm->btl_send() function if there is no place in cb. This will prevent OB1 from doing early completion of small sends.
    This commit was SVN r18424.

--- ompi/mca/btl/sm/btl_sm_fifo.h
+++ ompi/mca/btl/sm/btl_sm_fifo.h
@@ -17,7 +17,7 @@ do {                                                                        \
     if(ompi_fifo_write_to_head(hdr, fifo, mca_btl_sm_component.sm_mpool)    \
             != OMPI_SUCCESS) {                                              \
         btl_sm_add_pending(endpoint_peer, hdr, resend);                     \
-        rc = OMPI_ERR_TEMP_OUT_OF_RESOURCE;                                 \
+        rc = OMPI_ERR_RESOURCE_BUSY;                                        \
     } else {                                                                \
         MCA_BTL_SM_SIGNAL_PEER(endpoint_peer);                              \
         rc = OMPI_SUCCESS;                                                  \
