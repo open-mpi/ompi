4c8d79d907308ca5e2f6ccd7563c77d66a883ae3
Pak Lui <pak.lui@sun.com>
2008-05-13 22:07:38 +0000

    Silence the compiler warnings/errors. There is no orte/util/show_help.h
    This commit was SVN r18436.

--- orte/mca/ras/gridengine/ras_gridengine_module.c
+++ orte/mca/ras/gridengine/ras_gridengine_module.c
@@ -29,11 +29,8 @@
 
 #include "opal/util/argv.h"
 #include "orte/util/output.h"
-#include "orte/util/show_help.h"
-
 #include "orte/mca/errmgr/errmgr.h"
 #include "orte/runtime/orte_globals.h"
-
 #include "orte/mca/ras/base/ras_private.h"
 #include "orte/mca/ras/gridengine/ras_gridengine.h"
 
