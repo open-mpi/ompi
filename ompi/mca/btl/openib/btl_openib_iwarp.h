132d6c7adf31dc71b10b5e06efc8ea49484fc29d
Jon Mason <jdmason@open-mpi-git-mirror.example.com>
2008-05-20 23:28:59 +0000

    Modify to comply with the "prefix rule" and remove "static inline" for the non-rdmacm enabled case.   This should fix Ticket #1294.

--- ompi/mca/btl/openib/btl_openib_iwarp.h
+++ ompi/mca/btl/openib/btl_openib_iwarp.h
@@ -1,4 +1,4 @@
-extern uint64_t get_iwarp_subnet_id(struct ibv_device *ib_dev);
-extern uint32_t rdma_get_ipv4addr(struct ibv_context *verbs, uint8_t port);
-extern int build_rdma_addr_list(void);
-extern void free_rdma_addr_list(void);
+extern uint64_t mca_btl_openib_get_iwarp_subnet_id(struct ibv_device *ib_dev);
+extern uint32_t mca_btl_openib_rdma_get_ipv4addr(struct ibv_context *verbs, uint8_t port);
+extern int mca_btl_openib_build_rdma_addr_list(void);
+extern void mca_btl_openib_free_rdma_addr_list(void);
