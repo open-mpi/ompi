ef6f60ed11a87a3b50cfd8c2d239d13ac3376678
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2008-06-03 16:03:36 +0000

    Add Pasha's patch as it correctly solve the issues. In fact in the current incarnation these functions do not need the inline keyword anymore.

--- ompi/mca/pml/ob1/pml_ob1_sendreq.c
+++ ompi/mca/pml/ob1/pml_ob1_sendreq.c
@@ -192,7 +192,7 @@ mca_pml_ob1_match_completion_free_request( mca_bml_base_btl_t* bml_btl,
     MCA_PML_OB1_PROGRESS_PENDING(bml_btl);
 }
 
-static inline void
+static void
 mca_pml_ob1_match_completion_free( struct mca_btl_base_module_t* btl,  
                                    struct mca_btl_base_endpoint_t* ep,
                                    struct mca_btl_base_descriptor_t* des,
@@ -235,7 +235,7 @@ mca_pml_ob1_rndv_completion_request( mca_bml_base_btl_t* bml_btl,
  *  Completion of the first fragment of a long message that 
  *  requires an acknowledgement
  */
-static inline void
+static void
 mca_pml_ob1_rndv_completion( mca_btl_base_module_t* btl,
                              struct mca_btl_base_endpoint_t* ep,
                              struct mca_btl_base_descriptor_t* des,
@@ -269,7 +269,7 @@ mca_pml_ob1_rndv_completion( mca_btl_base_module_t* btl,
  * Completion of a get request.
  */
 
-static inline void
+static void
 mca_pml_ob1_rget_completion( mca_btl_base_module_t* btl,
                              struct mca_btl_base_endpoint_t* ep,
                              struct mca_btl_base_descriptor_t* des,
@@ -295,7 +295,7 @@ mca_pml_ob1_rget_completion( mca_btl_base_module_t* btl,
  * Completion of a control message - return resources.
  */
 
-static inline void
+static void
 mca_pml_ob1_send_ctl_completion( mca_btl_base_module_t* btl,
                                  struct mca_btl_base_endpoint_t* ep,
                                  struct mca_btl_base_descriptor_t* des,
@@ -312,7 +312,7 @@ mca_pml_ob1_send_ctl_completion( mca_btl_base_module_t* btl,
  * to schedule additional fragments.
  */
 
-static inline void
+static void
 mca_pml_ob1_frag_completion( mca_btl_base_module_t* btl,
                              struct mca_btl_base_endpoint_t* ep,
                              struct mca_btl_base_descriptor_t* des,
