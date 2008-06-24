12baa7258007d5080c855e029d368e9e8c2727a9
Terry Dontje <terry.dontje@oracle.com>
2008-06-24 14:38:11 +0000

    This commit fixes trac:1306
    This commit was SVN r18718.
    
    The following Trac tickets were found above:
      Ticket 1306 --> https://svn.open-mpi.org/trac/ompi/ticket/1306

--- ompi/mca/osc/pt2pt/osc_pt2pt_buffer.c
+++ ompi/mca/osc/pt2pt/osc_pt2pt_buffer.c
@@ -7,6 +7,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2008      Sun Microsystems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -18,10 +19,14 @@
 
 #include "osc_pt2pt_mpireq.h"
 #include "osc_pt2pt_buffer.h"
+#include "opal/types.h"
 
 static void ompi_osc_pt2pt_buffer_construct(ompi_osc_pt2pt_buffer_t *buf)
 {
-    buf->payload = buf + 1;
+    /* adjust payload location to account for alignment issues */
+    buf->payload = (void* )(((char*) buf) + 
+                            sizeof(ompi_osc_pt2pt_buffer_t) + 
+                            (sizeof(ompi_osc_pt2pt_buffer_t) % sizeof(ompi_ptr_t)));
 }
 
 
