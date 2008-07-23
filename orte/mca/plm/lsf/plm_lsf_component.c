849e127ec8cacd38b55828fb3701c9878bc66290
Thomas Herault <herault@open-mpi-git-mirror.example.com>
2008-07-23 14:42:26 +0000

    Small typos for LSF compilation and update Makefile.am

--- orte/mca/plm/lsf/plm_lsf_component.c
+++ orte/mca/plm/lsf/plm_lsf_component.c
@@ -10,6 +10,8 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2006-2007 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2008      Institut National de Recherche en Informatique
+ *                         et Automatique. All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -124,7 +126,7 @@ static int orte_plm_lsf_component_query(mca_base_module_t **module, int *priorit
         opal_output_verbose(10, orte_plm_globals.output,
                             "plm:lsf: NOT available for selection");
         *module = NULL;
-        return ORTE_ERROR:
+        return ORTE_ERROR;
     }
     
     *priority = mca_plm_lsf_component.priority;
