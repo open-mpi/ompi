9cffbecdafa26ad475b59590e81e21edd5aacb19
Jeff Squyres <jsquyres@cisco.com>
2008-08-06 17:29:41 +0000

    Amend r19196 with a few extra bells and whistles:
     * use "warn_on_fork" instead of "do_not_warn_on_fork" -- i.e.,
       use positive logic instead of negative logic
     * ensure that pthread_atfork() is only called once
     * amended the error message to include the hostname, PID, and
       MPI_COMM_WORLD rank of the offender
     * ensure that the warn_fork_cb() function is only defined if
       HAVE_PTHREAD_H so that we don't get a compiler warning if it isn't
       used
    
    This commit was SVN r19204.
    
    The following SVN revision numbers were found above:
      r19196 --> open-mpi/ompi@277e4ac292de72919eed33d732df68294619055a

--- ompi/runtime/ompi_mpi_params.c
+++ ompi/runtime/ompi_mpi_params.c
@@ -275,11 +275,11 @@ int ompi_mpi_register_params(void)
                                 (int) ompi_mpi_paffinity_alone, &value);
     ompi_mpi_paffinity_alone = OPAL_INT_TO_BOOL(value);
 
-    mca_base_param_reg_int_name("mpi", "do_not_warn_on_fork",
-                                "If nonzero, do not warn if program forks under conditions that could cause memory corruption issues",
+    mca_base_param_reg_int_name("mpi", "warn_on_fork",
+                                "If nonzero, issue a warning if program forks under conditions that could cause system errors",
                                 false, false, 
-                                (int) false, &value);
-    ompi_do_not_warn_on_fork = OPAL_INT_TO_BOOL(value);
+                                (int) true, &value);
+    ompi_warn_on_fork = OPAL_INT_TO_BOOL(value);
     
     /* Sparse group storage support */
 
