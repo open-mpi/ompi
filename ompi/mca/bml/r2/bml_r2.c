d7064856bec23f774d16313daa62d4a93a68c227
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2008-08-06 18:56:16 +0000

    Correct an error output so the process names are sensible

--- ompi/mca/bml/r2/bml_r2.c
+++ ompi/mca/bml/r2/bml_r2.c
@@ -444,25 +444,12 @@ int mca_bml_r2_add_procs( size_t nprocs,
 
     if (mca_bml_r2.show_unreach_errors && 
         OMPI_ERR_UNREACH == ret) {
-        char *local, *remote;
-
-        if (ORTE_SUCCESS != 
-            orte_util_convert_process_name_to_string(&local,
-                                                     &(ompi_proc_local_proc->proc_name))) {
-            local = strdup("Unknown");
-        }
-        if (ORTE_SUCCESS != 
-            orte_util_convert_process_name_to_string(&remote,
-                                                     &(unreach_proc->proc_name))) {
-            remote = strdup("Unknown");
-        }
 
         orte_show_help("help-mca-bml-r2",
                        "unreachable proc",
-                       true, local, remote, NULL);
+                       true, ORTE_NAME_PRINT(&(ompi_proc_local_proc->proc_name)),
+                       ORTE_NAME_PRINT(&(unreach_proc->proc_name)), NULL);
 
-        free(local);
-        free(remote);
     }
 
     free(new_procs); 
