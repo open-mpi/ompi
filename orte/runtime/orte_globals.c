52d4be78dc7afa981e7eb6a0fc0e508b460e3014
Brian Barrett <brbarret@open-mpi.org>
2008-09-01 17:49:31 +0000

    Add missing header file for the full rte case caused by changes in r19471.
    This commit was SVN r19474.
    
    The following SVN revision numbers were found above:
      r19471 --> open-mpi/ompi@38eb3019195c88797087e99f9456427a020d0402

--- orte/runtime/orte_globals.c
+++ orte/runtime/orte_globals.c
@@ -28,6 +28,7 @@
 #include "opal/mca/base/mca_base_param.h"
 #include "opal/threads/mutex.h"
 #include "opal/threads/condition.h"
+#include "opal/util/argv.h"
 #include "opal/class/opal_pointer_array.h"
 #include "opal/dss/dss.h"
 
