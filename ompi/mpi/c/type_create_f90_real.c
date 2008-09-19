62f033ef56e699935ca3b79f6ebf4abdd431920a
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2008-09-19 13:27:30 +0000

    Fix CID 1117: ensure to check return values.

--- ompi/mpi/c/type_create_f90_real.c
+++ ompi/mpi/c/type_create_f90_real.c
@@ -10,6 +10,7 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2006      Sun Microsystems, Inc.  All rights reserved.
+ * Copyright (c) 2008      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -77,6 +78,7 @@ int MPI_Type_create_f90_real(int p, int r, MPI_Datatype *newtype)
     if( *newtype != &ompi_mpi_datatype_null ) {
         ompi_datatype_t* datatype;
         int* a_i[2];
+        int rc;
 
         key = (((uint64_t)p) << 32) | ((uint64_t)r);
         if( OPAL_SUCCESS == opal_hash_table_get_value_uint64( &ompi_mpi_f90_real_hashtable,
@@ -99,7 +101,10 @@ int MPI_Type_create_f90_real(int p, int r, MPI_Datatype *newtype)
         a_i[1] = &p;
         ompi_ddt_set_args( datatype, 1, a_i, 0, NULL, 0, NULL, MPI_COMBINER_F90_REAL );
 
-        opal_hash_table_set_value_uint64( &ompi_mpi_f90_real_hashtable, key, datatype );
+        rc = opal_hash_table_set_value_uint64( &ompi_mpi_f90_real_hashtable, key, datatype );
+        if (OMPI_SUCCESS != rc) {
+            return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, rc, FUNC_NAME);
+        }
         *newtype = datatype;
         return MPI_SUCCESS;
     }
