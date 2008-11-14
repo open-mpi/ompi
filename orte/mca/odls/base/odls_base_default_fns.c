68423f754423e7f2040985348b982d1557171599
Ralph Castain <rhc@open-mpi.org>
2008-11-14 20:36:18 +0000

    Partially restore the iof changes - this repairs the initial observation of inconsistent and incomplete output
    This commit was SVN r19999.

--- orte/mca/odls/base/odls_base_default_fns.c
+++ orte/mca/odls/base/odls_base_default_fns.c
@@ -1859,8 +1859,7 @@ MOVEON:
     child->alive = false;
 
     /* Release the IOF resources related to this child */
-    orte_iof.close(child->name, (ORTE_IOF_STDIN | ORTE_IOF_STDOUT |
-                                 ORTE_IOF_STDERR | ORTE_IOF_STDDIAG) );
+    orte_iof.close(child->name, ORTE_IOF_STDIN);
 
     /* Clean up the session directory as if we were the process
      * itself.  This covers the case where the process died abnormally
