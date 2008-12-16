5a63db95fd0b70d495ae8cd7334f8ac2c16cde2e
Ethan Mallove <emallove@open-mpi-git-mirror.example.com>
2008-12-16 15:30:02 +0000

    Add missing `#include <errno.h>` line (for SunStudio Solaris).

--- ompi/mca/common/mx/common_mx.c
+++ ompi/mca/common/mx/common_mx.c
@@ -10,6 +10,7 @@
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2008      Myricom. All rights reserved.
+ * Copyright (c) 2008      Sun Microsystems, Inc.  All rights reserved.
  * 
  * $COPYRIGHT$
  *
@@ -23,6 +24,7 @@
 #include "ompi/constants.h"
 #include "common_mx.h"
 
+#include <errno.h>
 #ifdef HAVE_MALLOC_H
 #include <malloc.h>
 #endif
