238b64283fa652370c8de37a8e10d06d86eae3b4
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2009-01-21 23:02:51 +0000

    Fixes #1072: allow MPI C++ constants to be used as array sizes, such as:
      char name[MPI::MAX_PORT_NAME];

--- ompi/mpi/cxx/mpicxx.cc
+++ ompi/mpi/cxx/mpicxx.cc
@@ -10,7 +10,7 @@
 //                         University of Stuttgart.  All rights reserved.
 // Copyright (c) 2004-2005 The Regents of the University of California.
 //                         All rights reserved.
-// Copyright (c) 2007-2008 Cisco Systems, Inc.  All rights reserved.
+// Copyright (c) 2007-2009 Cisco Systems, Inc.  All rights reserved.
 // Copyright (c) 2007      Sun Microsystems, Inc.  All rights reserved.
 // $COPYRIGHT$
 // 
@@ -45,73 +45,10 @@ int mpi_errno = MPI_SUCCESS;
 void* const BOTTOM = (void*) MPI_BOTTOM;
 void* const IN_PLACE = (void*) MPI_IN_PLACE;
 
-// return  codes
-const int SUCCESS = MPI_SUCCESS;
-const int ERR_BUFFER = MPI_ERR_BUFFER;
-const int ERR_COUNT = MPI_ERR_COUNT;
-const int ERR_TYPE = MPI_ERR_TYPE;
-const int ERR_TAG = MPI_ERR_TAG;
-const int ERR_COMM = MPI_ERR_COMM;
-const int ERR_RANK = MPI_ERR_RANK;
-const int ERR_REQUEST = MPI_ERR_REQUEST;
-const int ERR_ROOT = MPI_ERR_ROOT;
-const int ERR_GROUP = MPI_ERR_GROUP;
-const int ERR_OP = MPI_ERR_OP;
-const int ERR_TOPOLOGY = MPI_ERR_TOPOLOGY;
-const int ERR_DIMS = MPI_ERR_DIMS;
-const int ERR_ARG = MPI_ERR_ARG;
-const int ERR_UNKNOWN = MPI_ERR_UNKNOWN;
-const int ERR_TRUNCATE = MPI_ERR_TRUNCATE;
-const int ERR_OTHER = MPI_ERR_OTHER;
-const int ERR_INTERN = MPI_ERR_INTERN;
-
-const int ERR_BASE = MPI_ERR_BASE;
-const int ERR_INFO_VALUE = MPI_ERR_INFO_VALUE;
-const int ERR_INFO_KEY = MPI_ERR_INFO_KEY;
-const int ERR_INFO_NOKEY = MPI_ERR_INFO_NOKEY;
-const int ERR_KEYVAL = MPI_ERR_KEYVAL;
-const int ERR_NAME = MPI_ERR_NAME;
-const int ERR_NO_MEM = MPI_ERR_NO_MEM;
-const int ERR_SERVICE = MPI_ERR_SERVICE;
-const int ERR_SPAWN = MPI_ERR_SPAWN;
-const int ERR_WIN = MPI_ERR_WIN;
-
-const int ERR_PENDING = MPI_ERR_PENDING;
-const int ERR_IN_STATUS = MPI_ERR_IN_STATUS;
-const int ERR_LASTCODE = MPI_ERR_LASTCODE;
-
-// assorted constants
-const int PROC_NULL = MPI_PROC_NULL;
-const int ANY_SOURCE = MPI_ANY_SOURCE;
-const int ROOT = MPI_ROOT;
-const int ANY_TAG = MPI_ANY_TAG;
-const int UNDEFINED = MPI_UNDEFINED;
-const int BSEND_OVERHEAD = MPI_BSEND_OVERHEAD;
-const int KEYVAL_INVALID = MPI_KEYVAL_INVALID;
-const int ORDER_C = MPI_ORDER_C;
-const int ORDER_FORTRAN = MPI_ORDER_FORTRAN;
-const int DISTRIBUTE_BLOCK = MPI_DISTRIBUTE_BLOCK;
-const int DISTRIBUTE_CYCLIC = MPI_DISTRIBUTE_CYCLIC;
-const int DISTRIBUTE_NONE = MPI_DISTRIBUTE_NONE;
-const int DISTRIBUTE_DFLT_DARG = MPI_DISTRIBUTE_DFLT_DARG;
-
 // error-handling specifiers
 const Errhandler  ERRORS_ARE_FATAL(&ompi_mpi_errors_are_fatal);
 const Errhandler  ERRORS_RETURN(&ompi_mpi_errors_return);
 const Errhandler  ERRORS_THROW_EXCEPTIONS(&ompi_mpi_errors_throw_exceptions);
-
-// typeclass definitions for MPI_Type_match_size
-const int TYPECLASS_INTEGER = MPI_TYPECLASS_INTEGER;
-const int TYPECLASS_REAL = MPI_TYPECLASS_REAL;
-const int TYPECLASS_COMPLEX = MPI_TYPECLASS_COMPLEX;
-
-// maximum sizes for strings
-const int MAX_PROCESSOR_NAME = MPI_MAX_PROCESSOR_NAME;
-const int MAX_ERROR_STRING = MPI_MAX_ERROR_STRING;
-const int MAX_INFO_KEY = MPI_MAX_INFO_KEY;
-const int MAX_INFO_VAL = MPI_MAX_INFO_VAL;
-const int MAX_PORT_NAME = MPI_MAX_PORT_NAME;
-const int MAX_OBJECT_NAME = MPI_MAX_OBJECT_NAME;
 
 // elementary datatypes
 const Datatype CHAR(MPI_CHAR);
@@ -172,53 +109,9 @@ const Datatype COMPLEX(&ompi_mpi_cxx_cplex);
 const Datatype DOUBLE_COMPLEX(&ompi_mpi_cxx_dblcplex);
 const Datatype LONG_DOUBLE_COMPLEX(&ompi_mpi_cxx_ldblcplex);
 
-// datatype decoding constants
-const int COMBINER_NAMED = MPI_COMBINER_NAMED;
-const int COMBINER_DUP = MPI_COMBINER_DUP;
-const int COMBINER_CONTIGUOUS = MPI_COMBINER_CONTIGUOUS;
-const int COMBINER_VECTOR = MPI_COMBINER_VECTOR;
-const int COMBINER_HVECTOR_INTEGER =  MPI_COMBINER_HVECTOR_INTEGER;
-const int COMBINER_HVECTOR = MPI_COMBINER_HVECTOR;
-const int COMBINER_INDEXED = MPI_COMBINER_INDEXED;	
-const int COMBINER_HINDEXED_INTEGER = MPI_COMBINER_HINDEXED_INTEGER;
-const int COMBINER_HINDEXED =  MPI_COMBINER_HINDEXED;	
-const int COMBINER_INDEXED_BLOCK =  MPI_COMBINER_INDEXED_BLOCK;
-const int COMBINER_STRUCT_INTEGER =  MPI_COMBINER_STRUCT_INTEGER;
-const int COMBINER_STRUCT = MPI_COMBINER_STRUCT;
-const int COMBINER_SUBARRAY = MPI_COMBINER_SUBARRAY;
-const int COMBINER_DARRAY = MPI_COMBINER_DARRAY;
-const int COMBINER_F90_REAL = MPI_COMBINER_F90_REAL;
-const int COMBINER_F90_COMPLEX = MPI_COMBINER_F90_COMPLEX;
-const int COMBINER_F90_INTEGER = MPI_COMBINER_F90_INTEGER;
-const int COMBINER_RESIZED = MPI_COMBINER_RESIZED;
-
-// thread constants
-const int THREAD_SINGLE = MPI_THREAD_SINGLE;
-const int THREAD_FUNNELED = MPI_THREAD_FUNNELED;
-const int THREAD_SERIALIZED = MPI_THREAD_SERIALIZED;
-const int THREAD_MULTIPLE = MPI_THREAD_MULTIPLE;
-
 // reserved communicators
 Intracomm COMM_WORLD(MPI_COMM_WORLD);
 Intracomm COMM_SELF(MPI_COMM_SELF);
-
-// results of communicator and group comparisons
-const int IDENT = MPI_IDENT;
-const int CONGRUENT = MPI_CONGRUENT;
-const int SIMILAR = MPI_SIMILAR;
-const int UNEQUAL = MPI_UNEQUAL;
-
-// environmental inquiry keys
-const int TAG_UB = MPI_TAG_UB;
-const int HOST = MPI_HOST;
-const int IO = MPI_IO;
-const int WTIME_IS_GLOBAL = MPI_WTIME_IS_GLOBAL;
-const int APPNUM = MPI_APPNUM;
-const int LASTUSEDCODE = MPI_LASTUSEDCODE;
-const int UNIVERSE_SIZE = MPI_UNIVERSE_SIZE;
-const int WIN_BASE = MPI_WIN_BASE;
-const int WIN_SIZE = MPI_WIN_SIZE;
-const int WIN_DISP_UNIT = MPI_WIN_DISP_UNIT;
 
 // collective operations
 const Op MAX(MPI_MAX);
@@ -256,37 +149,6 @@ const char***      ARGVS_NULL = (const char***) MPI_ARGVS_NULL;
 
 // empty group
 const Group GROUP_EMPTY(MPI_GROUP_EMPTY);
-
-// topologies
-const int GRAPH = MPI_GRAPH;
-const int CART = MPI_CART;
-
-// MPI-2 IO
-#if OMPI_PROVIDE_MPI_FILE_INTERFACE
-const int MODE_CREATE = MPI_MODE_CREATE;
-const int MODE_RDONLY = MPI_MODE_RDONLY;
-const int MODE_WRONLY = MPI_MODE_WRONLY;
-const int MODE_RDWR = MPI_MODE_RDWR;
-const int MODE_DELETE_ON_CLOSE = MPI_MODE_DELETE_ON_CLOSE;
-const int MODE_UNIQUE_OPEN = MPI_MODE_UNIQUE_OPEN;
-const int MODE_EXCL = MPI_MODE_EXCL;
-const int MODE_APPEND = MPI_MODE_APPEND;
-const int MODE_SEQUENTIAL = MPI_MODE_SEQUENTIAL;
-
-const int DISPLACEMENT_CURRENT = MPI_DISPLACEMENT_CURRENT;
-
-const int MAX_DATAREP_STRING = MPI_MAX_DATAREP_STRING;
-#endif
-
-// one-sided constants
-const int MODE_NOCHECK = MPI_MODE_NOCHECK;
-const int MODE_NOPRECEDE = MPI_MODE_NOPRECEDE;
-const int MODE_NOPUT = MPI_MODE_NOPUT;
-const int MODE_NOSTORE = MPI_MODE_NOSTORE;
-const int MODE_NOSUCCEED = MPI_MODE_NOSUCCEED;
-
-const int LOCK_EXCLUSIVE = MPI_LOCK_EXCLUSIVE;
-const int LOCK_SHARED = MPI_LOCK_SHARED;
 
 // special datatypes for contstruction of derived datatypes
 const Datatype UB(MPI_UB);
