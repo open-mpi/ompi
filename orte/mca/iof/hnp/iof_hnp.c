104a0539e313e97488c568c3c750ce9568843494
Ralph Castain <rhc@open-mpi.org>
2009-02-02 15:47:07 +0000

    Fix a format statement to be compatible with all gcc compiler versions
    This commit was SVN r20400.

--- orte/mca/iof/hnp/iof_hnp.c
+++ orte/mca/iof/hnp/iof_hnp.c
@@ -163,7 +163,7 @@ static int hnp_push(const orte_process_name_t* dst_name, orte_iof_tag_t src_tag,
                 np = np / 10;
             }
             /* construct the filename */
-            asprintf(&outfile, "%s.%*0lu", orte_output_filename, numdigs, (unsigned long)proct->name.vpid);
+            asprintf(&outfile, "%s.%0*lu", orte_output_filename, numdigs, (unsigned long)proct->name.vpid);
             /* create the file */
             fdout = open(outfile, O_CREAT|O_RDWR|O_TRUNC, 0644);
             free(outfile);
