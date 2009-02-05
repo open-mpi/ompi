4d65eddd6b4313f371d44e4b893b0c19557e060a
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2009-02-05 06:05:28 +0000

    Add a shellscript for daemon-less launch of local slave processes. No manpage as this is totally for internal use only.

---
+++ orte/tools/orte-bootproxy/orte-bootproxy.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+# Copyright (c) 2009 Los Alamos National Security, LLC. All rights reserved
+#
+
+if (( $# < 1 )) ; then
+    echo "orte-bootproxy.sh: for OMPI internal use only"
+    exit 1
+fi
+
+# take the first arg
+var=$1
+
+# push all MCA params to the environment
+while [ "${var:0:5}" = "OMPI_" ]; do
+    export $var
+    shift 1
+    var=$1
+done
+
+# extract the application to be executed
+app=$1
+shift 1
+
+#exec the app with the remaining args
+exec "$app" "$@"
