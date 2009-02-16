63754be94fefd2c678c6792283a226f084a183a8
George Bosilca <bosilca@icl.utk.edu>
2009-02-16 20:04:55 +0000

    Allow the tools to remove the cleanly finalize without leaving the sighandler behind.
    This commit was SVN r20567.

--- orte/mca/ess/base/ess_base_std_tool.c
+++ orte/mca/ess/base/ess_base_std_tool.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2009 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
@@ -158,6 +158,8 @@ error:
 
 int orte_ess_base_tool_finalize(void)
 {
+    orte_wait_finalize();
+
     /* if I am a tool, then all I will have done is
      * a very small subset of orte_init - ensure that
      * I only back those elements out
