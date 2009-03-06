8b5e6c0425bc1c631874ace0c0af1331990e5f90
Jeff Squyres <jsquyres@cisco.com>
2009-03-06 22:02:17 +0000

    Because I could.  :-)
    Relevant MCA params:
    
     * notifier_twitter_username: Twitter username
     * notifier_twitter_password: Twitter password
    
    This commit was SVN r20750.

---
+++ orte/mca/notifier/twitter/notifier_twitter.h
@@ -0,0 +1,61 @@
+/* -*- C -*-
+ * 
+ * Copyright (c) 2004-2008 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2009 Cisco Systems, Inc. All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ *
+ */
+#ifndef NOTIFIER_TWITTER_H
+#define NOTIFIER_TWITTER_H
+
+#include "orte_config.h"
+
+#include <netdb.h>
+
+#include "orte/types.h"
+#include "orte/mca/notifier/notifier.h"
+
+BEGIN_C_DECLS
+
+typedef struct {
+    orte_notifier_base_component_t super;
+
+    /* Twitter URL, username, and password */
+    char *url;
+    char *server;
+    char *uri;
+    char *username;
+    char *password;
+
+    /* struct hostent from resolved Twitter server name */
+    struct hostent *server_hostent;
+    int port;
+
+    /* Priority of this component */
+    int priority;
+} orte_notifier_twitter_component_t;
+
+
+/*
+ * Notifier interfaces
+ */
+ORTE_MODULE_DECLSPEC extern orte_notifier_twitter_component_t 
+    mca_notifier_twitter_component;
+extern orte_notifier_base_module_t orte_notifier_twitter_module;
+
+END_C_DECLS
+
+#endif
