22bb995439318295e4936b5e979897adcd97930c
Ralph Castain <rhc@open-mpi.org>
2009-03-11 16:05:17 +0000

    Add platform files for embedded operations.
    Update the slave platform file to specify script wrapper compilers.
    
    This commit was SVN r20761.

---
+++ contrib/platform/embedded/gen_embedded.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash 
+#
+
+if (( $# < 2 )) ; then
+    echo "usage ./contrib/platform/embedded/gen_embedded.sh prefix debug|optimized"
+    exit 1
+fi
+
+prefix=$1
+shift 1
+platform=$1
+
+./autogen.sh -l -no-ompi
+./configure --prefix="${prefix}" --with-platform=contrib/platform/embedded/"${platform}"
+make clean > /dev/null
+make -j2 all > /dev/null
+make -j2 install > /dev/null
+
+# All done
+exit 0
