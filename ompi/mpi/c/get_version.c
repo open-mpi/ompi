87c35056b24ef645c5f787148534daa015483cb9
Ralph Castain <rhc@open-mpi.org>
2009-04-14 17:03:17 +0000

    Closes trac:1877: Match the version and subversion with the values in mpi.h
    Submitted by bosilca, reviewed by jsquyres, RM-approved by bbenton
    
    r20985
    	Mismatch between the reported subversion and the one in the mpi.h.
    	Thanks to Rob Egan for the report.
    
    This commit was SVN r20998.
    
    The following SVN revision numbers were found above:
      r20985 --> open-mpi/ompi@05ee4c280e5df665ebffedcf54d1bd747bfe76e8
    
    The following Trac tickets were found above:
      Ticket 1877 --> https://svn.open-mpi.org/trac/ompi/ticket/1877

--- ompi/mpi/c/get_version.c
+++ ompi/mpi/c/get_version.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2009 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
@@ -63,8 +63,8 @@ int MPI_Get_version(int *version, int *subversion)
 
     /* According to the MPI-2 specification */
 
-    *version = 2;
-    *subversion = 0;
+    *version = MPI_VERSION;
+    *subversion = MPI_SUBVERSION;
 
     return MPI_SUCCESS;
 }
