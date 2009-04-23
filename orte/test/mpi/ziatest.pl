76b6ae3b298baeb5a18df92a0fefb4d6dbd35e83
Ralph Castain <rhc@open-mpi.org>
2009-04-23 01:12:56 +0000

    Fix the ziatest to report correct times
    This commit was SVN r21059.

--- orte/test/mpi/ziatest.pl
+++ orte/test/mpi/ziatest.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 use Time::HiRes qw( gettimeofday );
 ($sec, $microsec) = gettimeofday;
-$cmd = "mpirun -npernode " . @ARGV[0] . " ./ziatest " . @ARGV[0] . " " . $sec . " " . $microsec;
+$cmd = "mpirun -npernode " . @ARGV[0] . " ./ziatest " . " $sec " . $microsec;
 system($cmd);
