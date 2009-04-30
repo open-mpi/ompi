e0ad7550918fe898e1a36465eae07509db5d5328
Shiqing Fan <shiqing@open-mpi-git-mirror.example.com>
2009-04-30 09:03:21 +0000

    Include a missing header.

--- opal/mca/installdirs/windows/opal_installdirs_windows.c
+++ opal/mca/installdirs/windows/opal_installdirs_windows.c
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2004-2007 The Trustees of the University of Tennessee.
  *                         All rights reserved.
+ * Copyright (c) 2009      High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -14,6 +16,7 @@
 #include <string.h>
 
 #include "opal/mca/installdirs/installdirs.h"
+#include "opal/constants.h"
 
 static int installdirs_windows_open(void);
 
