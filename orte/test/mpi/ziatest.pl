dfb2146430f9e3e6917fc5250763f0299ba91285
Ralph Castain <rhc@open-mpi.org>
2009-04-30 18:43:26 +0000

    Perform the ziatest as a C program instead of a script - less trouble that way.
    This commit was SVN r21132.

--- orte/test/mpi/ziatest.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-push(@INC, $ENV{HOME}."/my-perl-mods");
-require Time::HiRes;
-($s, $usec) = Time::HiRes::gettimeofday();
-$cmd = "mpirun -npernode " . @ARGV[0] . " ./ziatest " . " $s " . $usec;
-system($cmd);
