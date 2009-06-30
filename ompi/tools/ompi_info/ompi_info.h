285c6fef9368ca07784cc94cfb37c404e8c638d9
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2009-06-30 21:41:28 +0000

    Re-implement ompi_info in C, replacing the prior C++ code. This removes the last C++ code from the OMPI code base, thus relieving the requirement for C++ support unless the user requests the C++ bindings.
    Jeff will update the build system later to remove the C++ checks per the above caveat.

--- ompi/tools/ompi_info/ompi_info.h
+++ ompi/tools/ompi_info/ompi_info.h
@@ -1,130 +1,128 @@
-//
-// Copyright (c) 2004-2009 The Trustees of Indiana University and Indiana
-//                         University Research and Technology
-//                         Corporation.  All rights reserved.
-// Copyright (c) 2004-2005 The University of Tennessee and The University
-//                         of Tennessee Research Foundation.  All rights
-//                         reserved.
-// Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
-//                         University of Stuttgart.  All rights reserved.
-// Copyright (c) 2004-2005 The Regents of the University of California.
-//                         All rights reserved.
-// Copyright (c) 2007-2009 Cisco Systems, Inc.  All rights reserved.
-// $COPYRIGHT$
-// 
-// Additional copyrights may follow
-// 
-// $HEADER$
-//
+/*
+ * Copyright (c) 2004-2009 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2007-2009 Cisco Systems, Inc.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
 
-#ifndef OMPI_INFO_H
-#define OMPI_INFO_H
-
-#include <string>
-#include <vector>
-#include <map>
+#ifndef OMPI_INFO_C_H
+#define OMPI_INFO_C_H
+#include "ompi_config.h"
 
 #include "opal/class/opal_list.h"
+#include "opal/class/opal_pointer_array.h"
 #include "opal/util/cmd_line.h"
 #include "opal/mca/mca.h"
 
+BEGIN_C_DECLS
 
-namespace ompi_info {
+/*
+ * Globals
+ */
 
-  //
-  // Globals
-  //
+extern bool ompi_info_pretty;
+extern opal_cmd_line_t *ompi_info_cmd_line;
 
-  typedef std::vector<std::string> type_vector_t;
+extern const char *ompi_info_type_all;
+extern const char *ompi_info_type_ompi;
+extern const char *ompi_info_type_orte;
+extern const char *ompi_info_type_opal;
+extern const char *ompi_info_type_base;
 
-  extern bool pretty;
-  extern opal_cmd_line_t *cmd_line;
+extern opal_pointer_array_t mca_types;
 
-  extern const std::string type_all;
-  extern const std::string type_ompi;
-  extern const std::string type_orte;
-  extern const std::string type_opal;
-  extern const std::string type_base;
-  extern type_vector_t mca_types;
 
-  //
-  // Version-related strings and functions
-  //
+/*
+ * Version-related strings and functions
+ */
 
-  extern const std::string ver_full;
-  extern const std::string ver_major;
-  extern const std::string ver_minor;
-  extern const std::string ver_release;
-  extern const std::string ver_greek;
-  extern const std::string ver_svn;
+extern const char *ompi_info_ver_full;
+extern const char *ompi_info_ver_major;
+extern const char *ompi_info_ver_minor;
+extern const char *ompi_info_ver_release;
+extern const char *ompi_info_ver_greek;
+extern const char *ompi_info_ver_svn;
 
-  void do_version(bool want_all, opal_cmd_line_t *cmd_line);
-  void show_ompi_version(const std::string& scope);
-  void show_component_version(const std::string& type_name, 
-                           const std::string& component_name,
-                           const std::string& scope, 
-                           const std::string& ver_type);
+void ompi_info_do_version(bool want_all, opal_cmd_line_t *cmd_line);
+void ompi_info_show_ompi_version(const char *scope);
+void ompi_info_show_component_version(const char *type_name, 
+                                      const char *component_name,
+                                      const char *scope, 
+                                      const char *ver_type);
 
-  //
-  // Parameter/configuration-related functions
-  //
+/*
+ * Parameter/configuration-related functions
+ */
 
-  extern const std::string component_all;
-  extern const std::string param_all;
+extern const char *ompi_info_component_all;
+extern const char *ompi_info_param_all;
 
-  extern const std::string path_prefix;
-  extern const std::string path_bindir;
-  extern const std::string path_libdir;
-  extern const std::string path_incdir;
-  extern const std::string path_mandir;
-  extern const std::string path_pkglibdir;
-  extern const std::string path_sysconfdir;
-  extern const std::string path_exec_prefix;
-  extern const std::string path_sbindir;
-  extern const std::string path_libexecdir;
-  extern const std::string path_datarootdir;
-  extern const std::string path_datadir;
-  extern const std::string path_sharedstatedir;
-  extern const std::string path_localstatedir;
-  extern const std::string path_infodir;
-  extern const std::string path_pkgdatadir;
-  extern const std::string path_pkgincludedir;
+extern const char *ompi_info_path_prefix;
+extern const char *ompi_info_path_bindir;
+extern const char *ompi_info_path_libdir;
+extern const char *ompi_info_path_incdir;
+extern const char *ompi_info_path_mandir;
+extern const char *ompi_info_path_pkglibdir;
+extern const char *ompi_info_path_sysconfdir;
+extern const char *ompi_info_path_exec_prefix;
+extern const char *ompi_info_path_sbindir;
+extern const char *ompi_info_path_libexecdir;
+extern const char *ompi_info_path_datarootdir;
+extern const char *ompi_info_path_datadir;
+extern const char *ompi_info_path_sharedstatedir;
+extern const char *ompi_info_path_localstatedir;
+extern const char *ompi_info_path_infodir;
+extern const char *ompi_info_path_pkgdatadir;
+extern const char *ompi_info_path_pkgincludedir;
 
-  void do_params(bool want_all, bool want_internal);
-  void show_mca_params(opal_list_t *info,
-                       const std::string& type, const std::string& component, 
-                       bool want_internal);
+void ompi_info_do_params(bool want_all, bool want_internal);
+void ompi_info_show_mca_params(opal_list_t *info,
+                               const char *type, const char *component, 
+                               bool want_internal);
 
-  void do_path(bool want_all, opal_cmd_line_t *cmd_line);
-  void show_path(const std::string& type, const std::string& value);
+void ompi_info_do_path(bool want_all, opal_cmd_line_t *cmd_line);
+void ompi_info_show_path(const char *type, const char *value);
 
-  void do_arch();
-  void do_hostname();
-  void do_config(bool want_all);
+void ompi_info_do_arch(void);
+void ompi_info_do_hostname(void);
+void ompi_info_do_config(bool want_all);
 
-  //
-  // Output-related functions
-  //
-  void out(const std::string& pretty_message, 
-           const std::string &plain_message, 
-           int value);
-  void out(const std::string& pretty_message, 
-           const std::string &plain_message,
-           const std::string& value,
-           bool strip_leading_whitespace = true,
-           bool strip_trailing_whitespace = true);
+/*
+ * Output-related functions
+ */
+void ompi_info_out(const char *pretty_message, 
+                   const char *plain_message, 
+                   const char *value);
+void ompi_info_out_int(const char *pretty_message, 
+                       const char *plain_message, 
+                       int value);
+/*
+ * Component-related functions
+ */
+typedef struct {
+    opal_list_item_t super;
+    char *type;
+    opal_list_t *components;
+} ompi_info_component_map_t;
+OMPI_DECLSPEC OBJ_CLASS_DECLARATION(ompi_info_component_map_t);
 
-  //
-  // Component-related functions
-  //
+extern opal_pointer_array_t component_map;
 
-  typedef std::map<std::string, opal_list_t *> component_map_t;
+void ompi_info_open_components(void);
+void ompi_info_close_components(void);
 
-  extern component_map_t component_map;
-
-  void open_components();
-  void close_components();
-
-}
+END_C_DECLS
 
 #endif /* OMPI_INFO_H */
