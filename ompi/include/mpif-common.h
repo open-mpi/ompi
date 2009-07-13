4333031955f9df41c97494c9128fb9862aca89b7
Rainer Keller <rusraink@open-mpi-git-mirror.example.com>
2009-07-13 04:59:13 +0000

    - Runtime and include files missed in last commit

--- ompi/include/mpif-common.h
+++ ompi/include/mpif-common.h
@@ -11,6 +11,7 @@
 ! Copyright (c) 2004-2005 The Regents of the University of California.
 !                         All rights reserved.
 ! Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
+! Copyright (c) 2009      Oak Ridge National Labs.  All rights reserved.
 ! $COPYRIGHT$
 !
 ! Additional copyrights may follow
