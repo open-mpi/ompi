1b6db85988f7bf7c9ad3b09a67fd25c4fc17e458
Shiqing Fan <shiqing@hlrs.de>
2009-08-27 07:57:26 +0000

    Complete the support for building on UNC path.
    This commit was SVN r21897.

--- contrib/platform/win32/CMakeModules/check_mca_subdirs.cmake
+++ contrib/platform/win32/CMakeModules/check_mca_subdirs.cmake
@@ -8,27 +8,7 @@
 # $HEADER$
 #
 
-# list the sub directories of current directories
-# save the list of subdirs in OUTPUT_VARIABLE
-
-MACRO(CHECK_SUBDIRS CURRENT_DIR OUTPUT_VARIABLE)
-
-  # change the path into standard Windows format,
-  # so that we can support UNC path also.
-  STRING(REPLACE "/" "\\" DIR_FORMATTED ${CURRENT_DIR})
-
-  EXECUTE_PROCESS (COMMAND cmd /C dir /AD /B ${DIR_FORMATTED}
-                   WORKING_DIRECTORY  ${PROJECT_BINARY_DIR}
-                   OUTPUT_VARIABLE    OUTPUT
-                   RESULT_VARIABLE    RESULT
-                   ERROR_VARIABLE     ERROR)
-
-  IF(NOT "${OUTPUT}" STREQUAL "")
-    STRING (REGEX MATCHALL "[a-zA-Z1-9_]+" ${OUTPUT_VARIABLE} ${OUTPUT})
-  ENDIF(NOT "${OUTPUT}" STREQUAL "")
-
-ENDMACRO(CHECK_SUBDIRS CURRENT_DIR OUTPUT_VARIABLE)
-
+INCLUDE(list_subdirs)
 
 # there are several steps and issues for checking mca components:
 #
