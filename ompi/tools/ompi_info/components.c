d26f72db8f70f2331e23cbd6ce8d8e83a58c25df
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2009-09-29 13:14:39 +0000

    Update ompi_info to support new sensor, fddp, and rmcast frameworks if OMPI was configured to include them

--- ompi/tools/ompi_info/components.c
+++ ompi/tools/ompi_info/components.c
@@ -99,10 +99,20 @@
 #include "orte/mca/ras/base/ras_private.h"
 #include "orte/mca/rmaps/rmaps.h"
 #include "orte/mca/rmaps/base/base.h"
+#if ORTE_ENABLE_MULTICAST
+#include "orte/mca/rmcast/rmcast.h"
+#include "orte/mca/rmcast/base/base.h"
+#endif
 #include "orte/mca/rml/rml.h"
 #include "orte/mca/rml/base/base.h"
 #include "orte/mca/routed/routed.h"
 #include "orte/mca/routed/base/base.h"
+#if ORTE_ENABLE_MONITORING
+#include "orte/mca/sensor/sensor.h"
+#include "orte/mca/sensor/base/base.h"
+#include "orte/mca/fddp/fddp.h"
+#include "orte/mca/fddp/base/base.h"
+#endif
 #include "orte/mca/plm/plm.h"
 #include "orte/mca/plm/base/base.h"
 #if OPAL_ENABLE_FT == 1
@@ -401,6 +411,16 @@ void ompi_info_open_components(void)
     map->components = &orte_rmaps_base.available_components;
     opal_pointer_array_add(&component_map, map);
     
+#if ORTE_ENABLE_MULTICAST
+    if (ORTE_SUCCESS != orte_rmcast_base_open()) {
+        goto error;
+    }
+    map = OBJ_NEW(ompi_info_component_map_t);
+    map->type = strdup("rmcast");
+    map->components = &orte_rmaps_base.available_components;
+    opal_pointer_array_add(&component_map, map);
+#endif
+
     if (ORTE_SUCCESS != orte_rml_base_open()) {
         goto error;
     }
@@ -424,6 +444,24 @@ void ompi_info_open_components(void)
     map->type = strdup("plm");
     map->components = &orte_plm_base.available_components;
     opal_pointer_array_add(&component_map, map);
+    
+#if ORTE_ENABLE_MONITORING
+    if (ORTE_SUCCESS != orte_sensor_base_open()) {
+        goto error;
+    }
+    map = OBJ_NEW(ompi_info_component_map_t);
+    map->type = strdup("sensor");
+    map->components = &orte_rmaps_base.available_components;
+    opal_pointer_array_add(&component_map, map);
+    
+    if (ORTE_SUCCESS != orte_fddp_base_open()) {
+        goto error;
+    }
+    map = OBJ_NEW(ompi_info_component_map_t);
+    map->type = strdup("fddp");
+    map->components = &orte_rmaps_base.available_components;
+    opal_pointer_array_add(&component_map, map);
+#endif
     
 #if OPAL_ENABLE_FT == 1
     if (ORTE_SUCCESS != orte_snapc_base_open()) {
@@ -645,6 +683,15 @@ void ompi_info_close_components()
         (void) orte_rml_base_close();
         (void) orte_routed_base_close();
         (void) mca_oob_base_close();
+#if ORTE_ENABLE_MULTICAST
+
+        (void) orte_rmcast_base_close();
+#endif
+#if ORTE_ENABLE_MONITORING
+
+        (void) orte_sensor_base_close();
+        (void) orte_fddp_base_close();
+#endif
 #endif
         (void) orte_errmgr_base_close();
         
