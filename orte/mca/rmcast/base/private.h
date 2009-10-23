45ff2fa19587de5d7b6dc04ebd08b5b99bab1200
Tim Mattox <timattox@open-mpi-git-mirror.example.com>
2009-10-23 02:38:51 +0000

    Unfortunately, the typo's that r22129 tried to fix were not as simple as I or Ralph had hoped.  This should be the real fix, or very close to it.  I can now see both the sensor and rmcast information from ompi_info when configured with --enable-monitoring --enable_multicast

--- orte/mca/rmcast/base/private.h
+++ orte/mca/rmcast/base/private.h
@@ -41,22 +41,6 @@ BEGIN_C_DECLS
 
 
 
-/*
- * globals that might be needed
- */
-typedef struct {
-    int rmcast_output;
-    opal_list_t rmcast_opened;
-    uint32_t xmit_network;
-    char *my_group_name;
-    uint8_t my_group_number;
-    uint32_t interface;
-    uint16_t ports[256];
-} orte_rmcast_base_t;
-
-ORTE_DECLSPEC extern orte_rmcast_base_t orte_rmcast_base;
-
-
 /****    CLASS DEFINITIONS    ****/
 /*
  * Data structure for tracking assigned channels
