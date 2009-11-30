4d4bd17b9e7e7bf2474f00634fcc62d06f3b6de2
Ralph Castain <rhc@open-mpi.org>
2009-11-30 22:51:38 +0000

    Closes trac:2070: Move GROUP_INCL|EXCL and GROUP_TRANSLATE_RANKS fix to v1.4
    Submitted by jsquyres, reviewed by bosilca, RM-approved by bbenton
    
    r22144
    	Make MPI_GROUP_INCL|EXCL and MPI_GROUP_TRANSLATE_RANKS a bit more
    	social when array_size==0 is passed in. Thanks to Lisandro Dalcin for
    	pointing this out.
    
    This commit was SVN r22243.
    
    The following SVN revision numbers were found above:
      r22144 --> open-mpi/ompi@ac21b4f571602453d4b32422fd73d77fe067ecfb
    
    The following Trac tickets were found above:
      Ticket 2070 --> https://svn.open-mpi.org/trac/ompi/ticket/2070

--- ompi/mpi/c/group_incl.c
+++ ompi/mpi/c/group_incl.c
@@ -10,7 +10,7 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2006      University of Houston. All rights reserved.
- * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2006-2009 Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -53,7 +53,7 @@ int MPI_Group_incl(MPI_Group group, int n, int *ranks, MPI_Group *new_group)
            (NULL == new_group) ) {
           return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_GROUP,
                                         FUNC_NAME);
-      } else if (NULL == ranks) {
+      } else if (NULL == ranks && n > 0) {
           return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_ARG,
                                         FUNC_NAME);
       }
