7d11e0180588c8ceab4255a27b07948e5b07919c
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2009-12-03 04:30:58 +0000

    Update the multicast test. Some cleanups to the basic rmcast module

--- orte/test/system/orte_mcast.c
+++ orte/test/system/orte_mcast.c
@@ -36,6 +36,45 @@ static void cbfunc_iovec(int status,
                          orte_process_name_t *sender,
                          struct iovec *msg, int count, void* cbdata);
 
+orte_rmcast_channel_t chan=4;
+
+static void send_data(int fd, short flags, void *arg)
+{
+    opal_buffer_t buf, *bfptr;
+    int32_t i32;
+    struct iovec iovec_array[3];
+    int rc, i;
+    opal_event_t *tmp = (opal_event_t*)arg;
+    struct timeval now;
+
+    bfptr = OBJ_NEW(opal_buffer_t);
+    i32 = -1;
+    opal_dss.pack(bfptr, &i32, 1, OPAL_INT32);
+    if (ORTE_SUCCESS != (rc = orte_rmcast.send_buffer_nb(chan,
+                                                         ORTE_RMCAST_TAG_OUTPUT, bfptr,
+                                                         cbfunc_buf_snt, NULL))) {
+        ORTE_ERROR_LOG(rc);
+        OBJ_RELEASE(bfptr);
+        return;
+    }        
+    /* create an iovec array */
+    for (i=0; i < 3; i++) {
+        iovec_array[i].iov_base = (uint8_t*)malloc(30);
+        iovec_array[i].iov_len = 30;
+    }
+    /* send it out */
+    if (ORTE_SUCCESS != (rc = orte_rmcast.send(chan,
+                                               ORTE_RMCAST_TAG_OUTPUT,
+                                               iovec_array, 3))) {
+        ORTE_ERROR_LOG(rc);
+        return;
+    }
+    /* reset the timer */
+    now.tv_sec = 5;
+    now.tv_usec = 0;
+    opal_evtimer_add(tmp, &now);
+}
+
 int main(int argc, char* argv[])
 {
     int rc, i;
@@ -44,7 +83,6 @@ int main(int argc, char* argv[])
     opal_buffer_t buf, *bfptr;
     int32_t i32=1;
     struct iovec iovec_array[3];
-    orte_rmcast_channel_t chan=4;
     
     if (0 > (rc = orte_init(ORTE_PROC_NON_MPI))) {
         fprintf(stderr, "orte_nodename: couldn't init orte - error code %d\n", rc);
@@ -57,17 +95,20 @@ int main(int argc, char* argv[])
     printf("orte_mcast: Node %s Name %s Pid %ld\n",
            hostname, ORTE_NAME_PRINT(ORTE_PROC_MY_NAME), (long)pid);
     
-    
+
     
     if (0 == ORTE_PROC_MY_NAME->vpid) {
+        orte_grpcomm.barrier();
+
         /* open a new channel */
         if (ORTE_SUCCESS != (rc = orte_rmcast.open_channel(&chan, "orte_mcast", NULL, -1, NULL, ORTE_RMCAST_XMIT))) {
             ORTE_ERROR_LOG(rc);
             goto blast;
         }
-        orte_grpcomm.barrier();
         
         OBJ_CONSTRUCT(&buf, opal_buffer_t);
+        /* pass the new channel number */
+        i32 = chan;
         opal_dss.pack(&buf, &i32, 1, OPAL_INT32);
         if (ORTE_SUCCESS != (rc = orte_rmcast.send_buffer(ORTE_RMCAST_APP_PUBLIC_CHANNEL,
                                                           ORTE_RMCAST_TAG_ANNOUNCE, &buf))) {
@@ -75,60 +116,18 @@ int main(int argc, char* argv[])
             OBJ_DESTRUCT(&buf);
             goto blast;
         }
-        orte_grpcomm.barrier();
         OBJ_DESTRUCT(&buf);
-        bfptr = OBJ_NEW(opal_buffer_t);
-        i32 = 2;
-        opal_dss.pack(bfptr, &i32, 1, OPAL_INT32);
-        if (ORTE_SUCCESS != (rc = orte_rmcast.send_buffer_nb(chan,
-                                                             ORTE_RMCAST_TAG_OUTPUT, bfptr,
-                                                             cbfunc_buf_snt, NULL))) {
-            ORTE_ERROR_LOG(rc);
-            OBJ_RELEASE(bfptr);
-            goto blast;
-        }        
-        orte_grpcomm.barrier();
-        /* create an iovec array */
-        for (i=0; i < 3; i++) {
-            iovec_array[i].iov_base = (uint8_t*)malloc(30);
-            iovec_array[i].iov_len = 30;
-        }
-        /* send it out */
-        if (ORTE_SUCCESS != (rc = orte_rmcast.send(ORTE_RMCAST_APP_PUBLIC_CHANNEL,
-                                                   ORTE_RMCAST_TAG_WILDCARD,
-                                                   iovec_array, 3))) {
-            ORTE_ERROR_LOG(rc);
-            goto blast;
-        }    
-        orte_grpcomm.barrier();
-        orte_finalize();
-        return 0;
+        
+        /* wake up every 5 seconds and send something */
+        ORTE_TIMER_EVENT(5, 0, send_data);
     } else {
-        /* open a new channel */
-        if (ORTE_SUCCESS != (rc = orte_rmcast.open_channel(&chan, "orte_mcast", NULL, -1, NULL, ORTE_RMCAST_RECV))) {
-            ORTE_ERROR_LOG(rc);
-        }
         if (ORTE_SUCCESS != (rc = orte_rmcast.recv_buffer_nb(ORTE_RMCAST_APP_PUBLIC_CHANNEL,
                                                              ORTE_RMCAST_TAG_WILDCARD,
                                                              ORTE_RMCAST_PERSISTENT,
                                                              cbfunc, NULL))) {
             ORTE_ERROR_LOG(rc);
         }
-        if (ORTE_SUCCESS != (rc = orte_rmcast.recv_buffer_nb(chan,
-                                                             ORTE_RMCAST_TAG_WILDCARD,
-                                                             ORTE_RMCAST_PERSISTENT,
-                                                             cbfunc, NULL))) {
-            ORTE_ERROR_LOG(rc);
-        }
-        if (ORTE_SUCCESS != (rc = orte_rmcast.recv_nb(ORTE_RMCAST_APP_PUBLIC_CHANNEL,
-                                                      ORTE_RMCAST_TAG_WILDCARD,
-                                                      ORTE_RMCAST_PERSISTENT,
-                                                      cbfunc_iovec, NULL))) {
-            ORTE_ERROR_LOG(rc);
-        }
-        
-        orte_grpcomm.barrier();
-        
+        orte_grpcomm.barrier();  /* ensure the public recv is ready */
     }
     opal_event_dispatch();
     
@@ -153,29 +152,29 @@ static void cbfunc(int status,
             ORTE_NAME_PRINT(ORTE_PROC_MY_NAME),
             ORTE_NAME_PRINT(sender), channel, tag, i32);
 
-    orte_grpcomm.barrier();
-
-#if 0
-    opal_buffer_t *buf;
-    int32_t i32=2, rc;
-
-    buf = OBJ_NEW(opal_buffer_t);
-    opal_dss.pack(buf, &i32, 1, OPAL_INT32);
-    opal_dss.pack(buf, &i32, 1, OPAL_INT32);
-    opal_dss.pack(buf, &i32, 1, OPAL_INT32);
-
-    
-
-    if (0 != ORTE_PROC_MY_NAME->vpid) {
-        if (1 == i32) {
-            if (ORTE_SUCCESS != (rc = orte_rmcast.send_buffer(ORTE_RMCAST_APP_PUBLIC_CHANNEL,
-                                                              ORTE_RMCAST_TAG_WILDCARD, buf))) {
-                ORTE_ERROR_LOG(rc);
-            }
-        }
-        OBJ_RELEASE(buf);
+    if (i32 < 0) {
+        return;
     }
-#endif
+    
+    /* open a new channel */
+    chan = i32;
+    if (ORTE_SUCCESS != (rc = orte_rmcast.open_channel(&chan, "orte_mcast", NULL, -1, NULL, ORTE_RMCAST_RECV))) {
+        ORTE_ERROR_LOG(rc);
+    }
+    
+    /* setup to recv data on it */
+    if (ORTE_SUCCESS != (rc = orte_rmcast.recv_buffer_nb(chan,
+                                                         ORTE_RMCAST_TAG_OUTPUT,
+                                                         ORTE_RMCAST_PERSISTENT,
+                                                         cbfunc, NULL))) {
+        ORTE_ERROR_LOG(rc);
+    }
+    if (ORTE_SUCCESS != (rc = orte_rmcast.recv_nb(chan,
+                                                  ORTE_RMCAST_TAG_OUTPUT,
+                                                  ORTE_RMCAST_PERSISTENT,
+                                                  cbfunc_iovec, NULL))) {
+        ORTE_ERROR_LOG(rc);
+    }
 }
 
 static void cbfunc_iovec(int status,
@@ -189,18 +188,6 @@ static void cbfunc_iovec(int status,
     opal_output(0, "%s GOT IOVEC MESSAGE from %s of %d elements\n",
             ORTE_NAME_PRINT(ORTE_PROC_MY_NAME), ORTE_NAME_PRINT(sender), count);
 
-#if 0
-    if (0 != ORTE_PROC_MY_NAME->vpid) {
-        /* send it back */
-        if (ORTE_SUCCESS != (rc = orte_rmcast.send(ORTE_RMCAST_APP_PUBLIC_CHANNEL,
-                                                   ORTE_RMCAST_TAG_WILDCARD, msg, count))) {
-            ORTE_ERROR_LOG(rc);
-        }    
-    }
-#endif
-    orte_grpcomm.barrier();
-    orte_finalize();
-    exit(0);
 }
 
 static void cbfunc_buf_snt(int status,
