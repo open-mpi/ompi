0adb5706937215416a676e09ab090734ed94edfe
Rolf vandeVaart <rvandevaart@nvidia.com>
2010-04-07 13:51:42 +0000

    Add pml_ob1_verbose flag.  Fix the current location it is being used
    This commit was SVN r22939.

--- ompi/mca/pml/ob1/pml_ob1.h
+++ ompi/mca/pml/ob1/pml_ob1.h
@@ -9,6 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
+ * Copyright (c) 2010      Oracle and/or its affiliates.  All rights reserved
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -81,6 +82,7 @@ struct mca_pml_ob1_t {
 typedef struct mca_pml_ob1_t mca_pml_ob1_t; 
 
 extern mca_pml_ob1_t mca_pml_ob1;
+extern int mca_pml_ob1_output;
 
 /*
  * PML interface functions.
