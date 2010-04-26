c81ebedb9d5ebca89d943b9ebf0b5c59f83ca683
George Bosilca <bosilca@icl.utk.edu>
2010-04-26 14:54:17 +0000

    Fixes trac:2388: Fix v1.5 branch build caused by a merge problem
    This commit was SVN r23038.
    
    The following Trac tickets were found above:
      Ticket 2388 --> https://svn.open-mpi.org/trac/ompi/ticket/2388

--- orte/mca/ess/singleton/ess_singleton_module.c
+++ orte/mca/ess/singleton/ess_singleton_module.c
@@ -86,7 +86,6 @@ orte_ess_base_module_t orte_ess_singleton_module = {
     proc_get_node_rank,
     update_pidmap,
     update_nidmap,
-    orte_ess_base_query_sys_info,
     NULL /* ft_event */
 };
 
