e9f4c84d7eade8ca5abe95be4815a797ade64716
Ralph Castain <rhc@open-mpi.org>
2010-06-24 01:57:27 +0000

    Add another name field to the job object
    This commit was SVN r23299.

--- orte/runtime/data_type_support/orte_dt_print_fns.c
+++ orte/runtime/data_type_support/orte_dt_print_fns.c
@@ -216,8 +216,10 @@ int orte_dt_print_job(char **output, char *prefix, orte_job_t *src, opal_data_ty
         asprintf(&pfx2, "%s", prefix);
     }
 
-    asprintf(&tmp, "\n%sData for job: %s\tName: %s\n%s\tNum apps: %ld\tControls: %0x\tStdin target: %s\tState: %s\tAbort: %s", pfx2,
-             ORTE_JOBID_PRINT(src->jobid), src->name, pfx2,
+    asprintf(&tmp, "\n%sData for job: %s\tName: %s\tInstance: %s\n%s\tNum apps: %ld\tControls: %0x\tStdin target: %s\tState: %s\tAbort: %s", pfx2,
+             ORTE_JOBID_PRINT(src->jobid),
+             (NULL != src->name) ? src->name : "NULL",
+             (NULL != src->instance) ? src->instance : "NULL", pfx2,
              (long)src->num_apps, src->controls, ORTE_VPID_PRINT(src->stdin_target),
              orte_job_state_to_str(src->state), src->abort ? "True" : "False");
     asprintf(&pfx, "%s\t", pfx2);
