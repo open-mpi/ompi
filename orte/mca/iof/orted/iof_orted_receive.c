2411eb20f009e094ce45a640d7ebbddceab75f09
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2010-07-01 19:32:08 +0000

    Don't try to forward input when we already ordered shutdown. Check return codes on sends

--- orte/mca/iof/orted/iof_orted_receive.c
+++ orte/mca/iof/orted/iof_orted_receive.c
@@ -71,8 +71,10 @@ void orte_iof_orted_send_xonxoff(orte_iof_tag_t tag)
                          (ORTE_IOF_XON == tag) ? "xon" : "xoff"));
 
     /* send the buffer to the HNP */
-    orte_rml.send_buffer_nb(ORTE_PROC_MY_HNP, buf, ORTE_RML_TAG_IOF_HNP,
-                            0, send_cb, NULL);
+    if (0 > (rc = orte_rml.send_buffer_nb(ORTE_PROC_MY_HNP, buf, ORTE_RML_TAG_IOF_HNP,
+                                          0, send_cb, NULL))) {
+        ORTE_ERROR_LOG(rc);
+    }
 }
 
 /*
