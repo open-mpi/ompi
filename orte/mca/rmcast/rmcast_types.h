5d2233c95072e0e883daf39a419119b8004a53ca
Ralph Castain <rhc@open-mpi.org>
2010-07-09 17:51:28 +0000

    Add some new tags
    This commit was SVN r23369.

--- orte/mca/rmcast/rmcast_types.h
+++ orte/mca/rmcast/rmcast_types.h
@@ -59,6 +59,9 @@ typedef int32_t orte_rmcast_tag_t;
 #define ORTE_RMCAST_TAG_DATA         9
 #define ORTE_RMCAST_TAG_CMD_ACK     10
 #define ORTE_RMCAST_TAG_HEARTBEAT   11
+#define ORTE_RMCAST_TAG_COMMAND     12
+#define ORTE_RMCAST_TAG_ERRMGR      13
+
 
 /* starting value for dynamically assignable tags */
 #define ORTE_RMCAST_TAG_DYNAMIC     100
