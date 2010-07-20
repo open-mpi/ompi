4298f40eb71ec6e6dbd2a0e7507f17b767bb2074
Nadia Derby <derbeyn@open-mpi-git-mirror.example.com>
2010-07-20 06:37:17 +0000

    Wrong event_type value passed in to show_help when getting xrc async events

--- ompi/mca/btl/openib/btl_openib_async.c
+++ ompi/mca/btl/openib/btl_openib_async.c
@@ -350,13 +350,15 @@ static int btl_openib_async_deviceh(struct mca_btl_openib_async_poll *devices_po
             case IBV_EVENT_SRQ_ERR:
                 orte_show_help("help-mpi-btl-openib.txt", "of error event",
                     true,orte_process_info.nodename, orte_process_info.pid,
-                    event.event_type, openib_event_to_str(event.event_type),
+                    event_type,
+                    openib_event_to_str((enum ibv_event_type)event_type),
                     xrc_event ? "true" : "false");
                 break;
             case IBV_EVENT_PORT_ERR:
                 orte_show_help("help-mpi-btl-openib.txt", "of error event",
                     true,orte_process_info.nodename, orte_process_info.pid,
-                    event.event_type, openib_event_to_str(event.event_type),
+                    event_type,
+                    openib_event_to_str((enum ibv_event_type)event_type),
                     xrc_event ? "true" : "false");
                 /* Set the flag to indicate port error */
                 device->got_port_event = true;
@@ -385,7 +387,7 @@ static int btl_openib_async_deviceh(struct mca_btl_openib_async_poll *devices_po
             default:
                 orte_show_help("help-mpi-btl-openib.txt", "of unknown event",
                         true,orte_process_info.nodename, orte_process_info.pid,
-                        event.event_type, xrc_event ? "true" : "false");
+                        event_type, xrc_event ? "true" : "false");
         }
         ibv_ack_async_event(&event);
     } else {
