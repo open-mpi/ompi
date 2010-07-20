d65e2b9c0dec2c4aca625f396883a925de4cdaf9
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2010-07-20 19:53:49 +0000

    Add trivial functions to loop over read()'ing and write()'ing with a file descriptor (i.e., read and write complete messages, transparently handling partial reads/writes, EAGAIN, and EINTR).
    This code effectively already exists in a few places in the code base;
    this is mainly a consolidation.

---
+++ opal/util/fd.c
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2008-2009 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2009 Sandia National Laboratories. All rights reserved.
+ *
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "opal_config.h"
+
+#include <unistd.h>
+#include <errno.h>
+
+#include "opal/util/fd.h"
+#include "opal/constants.h"
+
+
+/*
+ * Simple loop over reading from a fd
+ */
+int opal_fd_read(int fd, int len, void *buffer)
+{
+    int rc;
+    char *b = buffer;
+
+    while (len > 0) {
+        rc = read(fd, b, len);
+        if (rc < 0 && (EAGAIN == errno || EINTR == errno)) {
+            continue;
+        } else if (rc > 0) {
+            len -= rc;
+            b += rc;
+        } else {
+            return OPAL_ERR_IN_ERRNO;
+        }
+    }
+    return OPAL_SUCCESS;
+}
+
+
+/*
+ * Simple loop over writing to an fd
+ */
+int opal_fd_write(int fd, int len, void *buffer)
+{
+    int rc;
+    char *b = buffer;
+
+    while (len > 0) {
+        rc = write(fd, b, len);
+        if (rc < 0 && (EAGAIN == errno || EINTR == errno)) {
+            continue;
+        } else if (rc > 0) {
+            len -= rc;
+            b += rc;
+        } else {
+            return OPAL_ERR_IN_ERRNO;
+        }
+    }
+
+    return OPAL_SUCCESS;
+}
+
+
