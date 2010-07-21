2a94e785e49b7e31d1b187a62d8c9f57755450da
George Bosilca <bosilca@icl.utk.edu>
2010-07-21 17:27:59 +0000

    Fixes trac:2500: Move one-sided 2B displacement fix to v1.5
    r23449
    Fixes trac:2472.  Use large integers to hold displacements for one-sided
    operations, not ints. 
    
    Sorry for the mid-day configure.ac change, folks...
    
    This commit was SVN r23465.
    
    The following SVN revision numbers were found above:
      r23449 --> open-mpi/ompi@35690ecad572c7fd408eb4d92614e113066dd396
    
    The following Trac tickets were found above:
      Ticket 2472 --> https://svn.open-mpi.org/trac/ompi/ticket/2472
      Ticket 2500 --> https://svn.open-mpi.org/trac/ompi/ticket/2500

--- ompi/mca/osc/pt2pt/osc_pt2pt.h
+++ ompi/mca/osc/pt2pt/osc_pt2pt.h
@@ -9,6 +9,7 @@
  *                         All rights reserved.
  * Copyright (c) 2007      Los Alamos National Security, LLC.  All rights
  *                         reserved. 
+ * Copyright (c) 2010      Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -189,7 +190,7 @@ int ompi_osc_pt2pt_module_put(void *origin_addr,
                              int origin_count,
                              struct ompi_datatype_t *origin_dt,
                              int target,
-                             int target_disp,
+                             OPAL_PTRDIFF_TYPE target_disp,
                              int target_count,
                              struct ompi_datatype_t *target_dt,
                              struct ompi_win_t *win);
@@ -198,7 +199,7 @@ int ompi_osc_pt2pt_module_accumulate(void *origin_addr,
                                     int origin_count,
                                     struct ompi_datatype_t *origin_dt,
                                     int target,
-                                    int target_disp,
+                                    OPAL_PTRDIFF_TYPE target_disp,
                                     int target_count,
                                     struct ompi_datatype_t *target_dt,
                                     struct ompi_op_t *op,
@@ -208,7 +209,7 @@ int ompi_osc_pt2pt_module_get(void *origin_addr,
                              int origin_count,
                              struct ompi_datatype_t *origin_dt,
                              int target,
-                             int target_disp,
+                             OPAL_PTRDIFF_TYPE target_disp,
                              int target_count,
                              struct ompi_datatype_t *target_dt,
                              struct ompi_win_t *win);
