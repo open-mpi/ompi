6158cab2e1de83071dd9583fdf7734066c8b372a
Ralph Castain <rhc@open-mpi.org>
2010-07-27 01:37:41 +0000

    Add missing header
    This commit was SVN r23497.

--- orte/mca/ess/generic/ess_generic_component.c
+++ orte/mca/ess/generic/ess_generic_component.c
@@ -26,6 +26,10 @@
 #include "orte_config.h"
 #include "orte/constants.h"
 
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
 #include "opal/mca/base/mca_base_param.h"
 
 #include "orte/util/proc_info.h"
