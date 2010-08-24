a987eafc900b8070916d68659a2eef7875abd0ba
Shiqing Fan <shiqing@hlrs.de>
2010-08-24 09:28:56 +0000

    Add another sm definition for ignoring posix sm on Windows, and exclude those source files.
    This commit was SVN r23640.

--- contrib/platform/win32/CMakeModules/ompi_check_Microsoft.cmake
+++ contrib/platform/win32/CMakeModules/ompi_check_Microsoft.cmake
@@ -178,8 +178,10 @@ OMPI_DEF_VAR(HAVE_INTERLOCKEDCOMPAREEXCHANGE64 "Whether we support 64 bits atomi
 OMPI_DEF_VAR(HAVE_INTERLOCKEDCOMPAREEXCHANGEACQUIRE "Whether we support 32 bits atomic operations on Windows" 0 0)
 OMPI_DEF_VAR(HAVE_INTERLOCKEDCOMPAREEXCHANGERELEASE "Whether we support 32 bits atomic operations on Windows" 0 0)
 
+# a few definitions for shared memory support
 OMPI_DEF(MCA_COMMON_SM_WINDOWS 1 "Whether we have shared memory support for Windows or not." 0 1)
-OMPI_DEF(MCA_COMMON_SM_SYSV 0 "Whether we have shared memory support for Windows or not." 0 1)
+OMPI_DEF(MCA_COMMON_SM_SYSV 0 "Whether we have shared memory support for SYSV or not." 0 1)
+OMPI_DEF(MCA_COMMON_SM_POSIX 0 "Whether we have shared memory support for POSIX or not." 0 1)
 
 OMPI_CHECK_INCLUDE_FILE (windows.h HAVE_WINDOWS_H)
 
