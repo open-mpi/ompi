open-mpi/ompi

commit cd345eea51e1081d16d396836649e3bc6168c682

corrected an array access bug in the latest libevent merge (see #2234) that was causing Solaris binaries to loop infinitely.
git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@22638 63e3feb5-37d5-0310-a306-e8a459e722fe

opal/event/devpoll.c
@@ -144,7 +144,7 @@ devpoll_init(struct event_base *base)
 
 	if (getrlimit(RLIMIT_NOFILE, &rl) == 0 &&
 	    rl.rlim_cur != RLIM_INFINITY)
-		nfiles = rl.rlim_cur;
+		nfiles = rl.rlim_cur - 1;
 
 	/* Initialize the kernel queue */
 	if ((dpfd = open("/dev/poll", O_RDWR)) == -1) {
@@ -192,12 +192,12 @@ devpoll_recalc(struct event_base *base, void *arg, int max)
 {
 	struct devpollop *devpollop = arg;
 
-	if (max >= devpollop->nfds) {
+	if (max > devpollop->nfds) {
 		struct evdevpoll *fds;
 		int nfds;
 
 		nfds = devpollop->nfds;
-		while (nfds <= max)
+		while (nfds < max)
 			nfds <<= 1;
 
 		fds = realloc(devpollop->fds, nfds * sizeof(struct evdevpoll));

opal/event/epoll.c
@@ -167,12 +167,12 @@ epoll_recalc(struct event_base *base, void *arg, int max)
 {
 	struct epollop *epollop = arg;
 
-	if (max >= epollop->nfds) {
+	if (max > epollop->nfds) {
 		struct evepoll *fds;
 		int nfds;
 
 		nfds = epollop->nfds;
-		while (nfds <= max)
+		while (nfds < max)
 			nfds <<= 1;
 
 		fds = realloc(epollop->fds, nfds * sizeof(struct evepoll));

commit 73c264f2b7e02bdf0aaa82ed9aa2a76767569362

Fixes trac:2537: Move tcp BTL hang fix to v1.5
r23626
Undo 4 of the 5 changes introduced by r22638. Leave
one of them in as it may still be needed on Solaris.

This fixes trac:2530.

This commit was SVN r23648.

The following SVN revision numbers were found above:
  r22638 --> open-mpi/ompi@2a4b1227d99bf590eec35922df66fc15be6f9884
  r23626 --> open-mpi/ompi@e71827b8fffd5028e4fc4ea0c11b593b48f91f9d

The following Trac tickets were found above:
  Ticket 2530 --> https://svn.open-mpi.org/trac/ompi/ticket/2530
  Ticket 2537 --> https://svn.open-mpi.org/trac/ompi/ticket/2537

opal/event/devpoll.c
@@ -192,12 +192,12 @@ devpoll_recalc(struct event_base *base, void *arg, int max)
 {
 	struct devpollop *devpollop = arg;
 
-	if (max > devpollop->nfds) {
+	if (max >= devpollop->nfds) {
 		struct evdevpoll *fds;
 		int nfds;
 
 		nfds = devpollop->nfds;
-		while (nfds < max)
+		while (nfds <= max)
 			nfds <<= 1;
 
 		fds = realloc(devpollop->fds, nfds * sizeof(struct evdevpoll));

opal/event/epoll.c
@@ -167,12 +167,12 @@ epoll_recalc(struct event_base *base, void *arg, int max)
 {
 	struct epollop *epollop = arg;
 
-	if (max > epollop->nfds) {
+	if (max >= epollop->nfds) {
 		struct evepoll *fds;
 		int nfds;
 
 		nfds = epollop->nfds;
-		while (nfds < max)
+		while (nfds <= max)
 			nfds <<= 1;
 
 		fds = realloc(epollop->fds, nfds * sizeof(struct evepoll));