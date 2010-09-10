3bb587937a590a57acdff2b37872e7d65a1fb09e
Rolf vandeVaart <rvandevaart@nvidia.com>
2010-09-10 21:01:52 +0000

    Just fix up some trailing spaces, tabs instead of spaces, missing periods on copyrights, extraneous spaces on blank lines.  No actual code change.
    This commit was SVN r23739.

--- ompi/mca/pml/ob1/pml_ob1_sendreq.c
+++ ompi/mca/pml/ob1/pml_ob1_sendreq.c
@@ -10,6 +10,7 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2008      UT-Battelle, LLC. All rights reserved.
+ * Copyright (c) 2010      Oracle and/or its affiliates.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -62,7 +63,7 @@ void mca_pml_ob1_send_request_process_pending(mca_bml_base_btl_t *bml_btl)
                     &sendreq->req_endpoint->btl_eager, bml_btl->btl);
             if( (NULL == send_dst) ||
                 (OPAL_SOS_GET_ERROR_CODE(mca_pml_ob1_send_request_start_btl(sendreq, send_dst)) ==
-                      OMPI_ERR_OUT_OF_RESOURCE) ) {
+                 OMPI_ERR_OUT_OF_RESOURCE) ) {
                 /* prepend to the pending list to minimize reordering in case
                  * send_dst != 0 */
                 add_request_to_send_pending(sendreq,
