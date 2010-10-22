1766bf271a1ba348a7ea597661ae60533cca1c41
Ralph Castain <rhc@open-mpi.org>
2010-10-22 05:12:12 +0000

    Correct an abstraction break that causes ompi_info to segfault if pml-v is not built. Move the definition and instantation of the mca_pml_v struct to the vprotocol base. Include the vprotocol/base/base.h file in pml_v.h. Remove the now useless pml_v.c.
    Perhaps somebody out there who cares and uses it can verify that vprotocol works?
    
    This commit was SVN r23919.

--- ompi/mca/pml/v/pml_v.h
+++ ompi/mca/pml/v/pml_v.h
@@ -15,19 +15,10 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/request/request.h"
 
+#include "ompi/mca/vprotocol/base/base.h"
+
 BEGIN_C_DECLS
 
-struct mca_pml_v_t {
-    int                                 output;
-    size_t                              host_pml_req_recv_size;
-    size_t                              host_pml_req_send_size;
-    mca_pml_base_component_t            host_pml_component;
-    mca_pml_base_module_t               host_pml;
-    ompi_request_fns_t                  host_request_fns;
-};
-typedef struct mca_pml_v_t mca_pml_v_t;
-
-OMPI_DECLSPEC extern mca_pml_v_t mca_pml_v;
 OMPI_MODULE_DECLSPEC extern mca_pml_base_component_2_0_0_t mca_pml_v_component;
 
 END_C_DECLS
