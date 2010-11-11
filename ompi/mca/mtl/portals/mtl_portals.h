open-mpi/ompi

commit 8bb331ab0236ee4f356b388d9bfec5c0f19dc76a

Update libevent to the 2.0 series, currently at 2.0.7rc. We will update to their final release when it becomes available. Currently known errors exist in unused portions of the libevent code. This revision passes the IBM test suite on a Linux machine and on a standalone Mac.
This is a fairly intrusive change, but outside of the moving of opal/event to opal/mca/event, the only changes involved (a) changing all calls to opal_event functions to reflect the new framework instead, and (b) ensuring that all opal_event_t objects are properly constructed since they are now true opal_objects.

Note: Shiqing has just returned from vacation and has not yet had a chance to complete the Windows integration. Thus, this commit almost certainly breaks Windows support on the trunk. However, I want this to have a chance to soak for as long as possible before I become less available a week from today (going to be at a class for 5 days, and thus will only be sparingly available) so we can find and fix any problems.

Biggest change is moving the libevent code from opal/event to a new opal/mca/event framework. This was done to make it much easier to update libevent in the future. New versions can be inserted as a new component and tested in parallel with the current version until validated, then we can remove the earlier version if we so choose. This is a statically built framework ala installdirs, so only one component will build at a time. There is no selection logic - the sole compiled component simply loads its function pointers into the opal_event struct.

I have gone thru the code base and converted all the libevent calls I could find. However, I cannot compile nor test every environment. It is therefore quite likely that errors remain in the system. Please keep an eye open for two things:

1. compile-time errors: these will be obvious as calls to the old functions (e.g., opal_evtimer_new) must be replaced by the new framework APIs (e.g., opal_event.evtimer_new)

ompi/mca/mtl/portals/mtl_portals.h
@@ -170,7 +170,7 @@ OBJ_CLASS_DECLARATION(ompi_mtl_portals_event_t);
 #define PTL_IS_READY_MSG(match_bits)            \
     (0 != (PTL_READY_MSG & match_bits))
 #define PTL_IS_SYNC_MSG(event) \
-    (0 != event.hdr_data)
+    (0 != opal_event.hdr_data)
 
 #define PTL_GET_TAG(match_bits) ((int)(match_bits & PTL_TAG_MASK))
 #define PTL_GET_SOURCE(match_bits) ((int)((match_bits & PTL_SOURCE_MASK) >> 32))

commit 0694a3203beb53322358dfeaf48fb3781e1ab9fc

This was a small mistake introduced in r23925 in the changes to libevent.
This commit was SVN r24043.

The following SVN revision numbers were found above:
  r23925 --> open-mpi/ompi@fceabb24983f764849f791a691c8c76378840ed8

ompi/mca/mtl/portals/mtl_portals.h
@@ -170,7 +170,7 @@ OBJ_CLASS_DECLARATION(ompi_mtl_portals_event_t);
 #define PTL_IS_READY_MSG(match_bits)            \
     (0 != (PTL_READY_MSG & match_bits))
 #define PTL_IS_SYNC_MSG(event) \
-    (0 != opal_event.hdr_data)
+    (0 != event.hdr_data)
 
 #define PTL_GET_TAG(match_bits) ((int)(match_bits & PTL_TAG_MASK))
 #define PTL_GET_SOURCE(match_bits) ((int)((match_bits & PTL_SOURCE_MASK) >> 32))