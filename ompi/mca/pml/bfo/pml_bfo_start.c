ba2dbff82da65ae8582464b2cd783b5986393034
Shiqing Fan <shiqing@hlrs.de>
2010-11-16 01:01:12 +0000

    Check for addressability in MPI_*_init, since buffer passed by the application should have been already allocated, but might be not initialized. Check in MPI_Start / MPI_Startall for defined-ness of the buffer passed into the send request(s).
    This commit was SVN r24054.

--- ompi/mca/pml/bfo/pml_bfo_start.c
+++ ompi/mca/pml/bfo/pml_bfo_start.c
@@ -113,6 +113,11 @@ int mca_pml_bfo_start(size_t count, ompi_request_t** requests)
         switch(pml_request->req_type) {
             case MCA_PML_REQUEST_SEND: 
             {
+                MEMCHECKER(
+                    memchecker_call(&opal_memchecker_base_isdefined,
+                                    pml_request->req_addr, pml_request->req_count,
+                                    pml_request->req_datatype);
+                );
                 mca_pml_bfo_send_request_t* sendreq = (mca_pml_bfo_send_request_t*)pml_request;
                 if( reuse_old_request && (sendreq->req_send.req_bytes_packed != 0) ) {
                     size_t offset = 0;
