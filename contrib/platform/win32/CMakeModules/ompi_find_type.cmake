6b3fba7370ee167ef6365ebf071729c510bf7118
Shiqing Fan <shiqing@open-mpi-git-mirror.example.com>
2010-11-17 17:27:05 +0000

    Update F77 build for Windows.

--- contrib/platform/win32/CMakeModules/ompi_find_type.cmake
+++ contrib/platform/win32/CMakeModules/ompi_find_type.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2008      High Performance Computing Center Stuttgart, 
+# Copyright (c) 2008-2010 High Performance Computing Center Stuttgart, 
 #                         University of Stuttgart.  All rights reserved.
 #
 # $COPYRIGHT$
@@ -19,32 +19,30 @@ MACRO(OMPI_FIND_TYPE TYPE TYPE_LIST ABORT TARGET_SIZE OUTPUT_VARIABLE)
 
   
   MESSAGE(STATUS "Check corresponding C type of ${TYPE}...")
-  #MESSAGE(STATUS "REALSIZE:${SIZEOF_${oft_type_name}}.TARGET SIZE: ${oft_target_size}")
   SET(oft_real_type "")
 
   IF(NOT "${oft_target_size}" STREQUAL "")
 
     FOREACH(oft_type ${TYPE_LIST})
-      IF("${oft_real_type}" STREQUAL "")
-        STRING(REGEX REPLACE "^a-zA-Z0-9_" "_" oft_type_name ${oft_type})
+        STRING(REGEX REPLACE " " "_" oft_type_name ${oft_type})
         STRING(TOUPPER ${oft_type_name} oft_type_name)
         IF(SIZEOF_${oft_type_name} EQUAL oft_target_size)
           SET(oft_real_type ${oft_type})
+          BREAK()
         ENDIF(SIZEOF_${oft_type_name} EQUAL oft_target_size)
-      ENDIF("${oft_real_type}" STREQUAL "")
     ENDFOREACH(oft_type ${TYPE_LIST})
-    
+
   ENDIF(NOT "${oft_target_size}" STREQUAL "")
-  
-  IF("${oft_real_type}" STREQUAL "0")
+
+  IF("${oft_real_type}" STREQUAL "")
     SET(OUTPUT_VARIABLE "")
     MESSAGE(STATUS "*** Did not find corresponding C type of ${TYPE}")
-    IF("${oft_abort_on_fail}" STREQUAL EQUAL "yes")
+    IF("${oft_abort_on_fail}" STREQUAL "yes")
       MESSAGE(FATAL_ERROR "Cannot continue.")
-    ENDIF("${oft_abort_on_fail}" STREQUAL EQUAL "yes")
-  ELSE("${oft_real_type}" STREQUAL "0")
+    ENDIF("${oft_abort_on_fail}" STREQUAL "yes")
+  ELSE("${oft_real_type}" STREQUAL "")
     SET(${OUTPUT_VARIABLE} "${oft_real_type}")
     MESSAGE(STATUS "Check corresponding C type of ${TYPE}... ${oft_real_type}") 
-  ENDIF("${oft_real_type}" STREQUAL "0")
+  ENDIF("${oft_real_type}" STREQUAL "")
 
 ENDMACRO(OMPI_FIND_TYPE TYPE TYPE_LIST ABORT TARGET_SIZE OUTPUT_VARIABLE)
