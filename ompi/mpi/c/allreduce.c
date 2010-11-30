1d402204b3933b0f786e02d1cb54dfa381270539
Ralph Castain <rhc@open-mpi.org>
2010-11-30 16:51:10 +0000

    Fixes trac:2631: Remove extraneous printfs from the allreduce code
    Removed printfs by hand edit
    
    This commit was SVN r24112.
    
    The following Trac tickets were found above:
      Ticket 2631 --> https://svn.open-mpi.org/trac/ompi/ticket/2631

--- ompi/mpi/c/allreduce.c
+++ ompi/mpi/c/allreduce.c
@@ -54,7 +54,6 @@ int MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
             memchecker_call(&opal_memchecker_base_isdefined, sendbuf, count, datatype);
         }
     );
-    printf("Past memchecker\n");
 
     if (MPI_PARAM_CHECK) {
         char *msg;
@@ -64,38 +63,29 @@ int MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
 
         err = MPI_SUCCESS;
         OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
-        printf("Past err_init_finalize\n");
 	if (ompi_comm_invalid(comm)) {
-            printf("Bad comm!\n");
 	    return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_COMM, 
                                           FUNC_NAME);
         } else if (MPI_OP_NULL == op) {
-            printf("Op is null!\n");
             err = MPI_ERR_OP;
         } else if (!ompi_op_is_valid(op, datatype, &msg, FUNC_NAME)) {
             int ret;
-            printf("Op is invalid!\n");
             ret = OMPI_ERRHANDLER_INVOKE(comm, MPI_ERR_OP, msg);
             free(msg);
             return ret;
         } else if( MPI_IN_PLACE == recvbuf ) {
-            printf("recvbuf = MPI_IN_PLACE!\n");
 	    return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_BUFFER, 
                                           FUNC_NAME);
         } else if( (sendbuf == recvbuf) && 
                    (MPI_BOTTOM != sendbuf) &&
                    (count > 1) ) {
-            printf("BOTTOM = bad!\n");
 	    return OMPI_ERRHANDLER_INVOKE(MPI_COMM_WORLD, MPI_ERR_BUFFER, 
                                           FUNC_NAME);
         } else {
-            printf("Checking datatype for send\n");
             OMPI_CHECK_DATATYPE_FOR_SEND(err, datatype, count);
         }
-        printf("Past all other param checking: err=%d\n", err);
         OMPI_ERRHANDLER_CHECK(err, comm, err, FUNC_NAME);
     }
-    printf("Past param check\n");
 
     /* MPI-1, p114, says that each process must supply at least
        one element.  But at least the Pallas benchmarks call
@@ -104,21 +94,16 @@ int MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
     if (0 == count) {
         return MPI_SUCCESS;
     }
-    printf("Past 0 count check\n");
 
     OPAL_CR_ENTER_LIBRARY();
-    printf("Past opal_cr_enter_library\n");
 
     /* Invoke the coll component to perform the back-end operation */
 
     OBJ_RETAIN(op);
-    printf("Past op retain\n");
     err = comm->c_coll.coll_allreduce(sendbuf, recvbuf, count,
                                       datatype, op, comm,
                                       comm->c_coll.coll_allreduce_module);
-    printf("Past coll invocation\n");
     OBJ_RELEASE(op);
-    printf("Past op release\n");
     OMPI_ERRHANDLER_RETURN(err, comm, err, FUNC_NAME);
 }
 
