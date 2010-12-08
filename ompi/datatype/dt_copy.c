5bf333608a72e140cd6eb69fa777e8194abad807
Ralph Castain <rhc@open-mpi.org>
2010-12-08 02:42:06 +0000

    Fixes trac:2628: Detect overlapping regions
    r24047
    Add a second version of the datatype copy function using memmove instead of memcpy.
    As memmove is slower than memcpy, I added the required logic to only use it when
    really necessary.
    
    No modification from developers point of view, you should always call
    opal_datatype_copy_content_same_ddt.
    
    This commit was SVN r24156.
    
    The following SVN revision numbers were found above:
      r24047 --> open-mpi/ompi@36ce319869676f912f9979ccc014511d8d27f3d2
    
    The following Trac tickets were found above:
      Ticket 2628 --> https://svn.open-mpi.org/trac/ompi/ticket/2628

--- ompi/datatype/dt_copy.c
+++ ompi/datatype/dt_copy.c
@@ -36,232 +36,62 @@ extern int ompi_copy_debug;
 #define DO_DEBUG(INST)
 #endif  /* OMPI_ENABLE_DEBUG */
 
-size_t ompi_datatype_memcpy_block_size = 128 * 1024;
+static size_t ompi_datatype_memcpy_block_size = 128 * 1024;
 
-static inline void copy_predefined_data( const dt_elem_desc_t* ELEM,
-                                         const ompi_datatype_t* DATATYPE,
-                                         unsigned char* SOURCE_BASE,
-                                         int32_t TOTAL_COUNT,
-                                         uint32_t COUNT,
-                                         unsigned char* SOURCE,
-                                         unsigned char* DESTINATION,
-                                         size_t* SPACE )
-{
-    uint32_t _copy_count = (COUNT);
-	size_t _copy_blength;
-    const ddt_elem_desc_t* _elem = &((ELEM)->elem);
-    unsigned char* _source = (SOURCE) + _elem->disp;
-    unsigned char* _destination = (DESTINATION) + _elem->disp;
+/**
+ * Non overlapping memory regions
+ */
+#undef MEM_OP_NAME
+#define MEM_OP_NAME  non_overlap
+#undef MEM_OP
+#define MEM_OP       MEMCPY
+#include "dt_copy.h"
 
-    _copy_blength = ompi_ddt_basicDatatypes[_elem->common.type]->size;
+#define MEMMOVE(d, s, l)                                  \
+    do {                                                  \
+        if( (((d) < (s)) && (((d) + (l)) > (s))) ||       \
+            (((s) < (d)) && (((s) + (l)) > (d))) ) {      \
+            memmove( (d), (s), (l) );                     \
+        } else {                                          \
+            MEMCPY( (d), (s), (l) );                      \
+        }                                                 \
+    } while (0)
 
-    if( _copy_blength == (uint32_t)_elem->extent ) {
-        _copy_blength *= _copy_count;
-        OMPI_DDT_SAFEGUARD_POINTER( _source, _copy_blength, (SOURCE_BASE),
-                                    (DATATYPE), (TOTAL_COUNT) );
-        /* the extent and the size of the basic datatype are equals */
-        DO_DEBUG( opal_output( 0, "copy 1. memcpy( %p, %p, %lu ) => space %lu\n",
-                               _destination, _source, (unsigned long)_copy_blength, (unsigned long)(*(SPACE)) ); );
-        MEMCPY( _destination, _source, _copy_blength );
-        _source      += _copy_blength;
-        _destination += _copy_blength;
-    } else {
-        uint32_t _i;
-        for( _i = 0; _i < _copy_count; _i++ ) {
-            OMPI_DDT_SAFEGUARD_POINTER( _source, _copy_blength, (SOURCE_BASE),
-                                        (DATATYPE), (TOTAL_COUNT) );
-            DO_DEBUG( opal_output( 0, "copy 2. memcpy( %p, %p, %lu ) => space %lu\n",
-                                   _destination, _source, (unsigned long)_copy_blength, (unsigned long)(*(SPACE) - (_i * _copy_blength)) ); );
-            MEMCPY( _destination, _source, _copy_blength );
-            _source      += _elem->extent;
-            _destination += _elem->extent;
-        }
-        _copy_blength *= _copy_count;
-    }
-    *(SPACE)      -= _copy_blength;
-}
-
-static inline void copy_contiguous_loop( const dt_elem_desc_t* ELEM,
-                                         const ompi_datatype_t* DATATYPE,
-                                         unsigned char* SOURCE_BASE,
-                                         int32_t TOTAL_COUNT,
-                                         uint32_t COUNT,
-                                         unsigned char* SOURCE,
-                                         unsigned char* DESTINATION,
-                                         size_t* SPACE )
-{
-    ddt_loop_desc_t *_loop = (ddt_loop_desc_t*)(ELEM);
-    ddt_endloop_desc_t* _end_loop = (ddt_endloop_desc_t*)((ELEM) + _loop->items);
-    unsigned char* _source = (SOURCE) + _end_loop->first_elem_disp;
-    unsigned char* _destination = (DESTINATION) + _end_loop->first_elem_disp;
-    size_t _copy_loops = (COUNT);
-    uint32_t _i;
-
-    if( _loop->extent == (ptrdiff_t)_end_loop->size ) {  /* the loop is contiguous */
-        _copy_loops *= _end_loop->size;
-        OMPI_DDT_SAFEGUARD_POINTER( _source, _copy_loops, (SOURCE_BASE),
-                                    (DATATYPE), (TOTAL_COUNT) );
-        MEMCPY( _destination, _source, _copy_loops );
-    } else {
-        for( _i = 0; _i < _copy_loops; _i++ ) {
-            OMPI_DDT_SAFEGUARD_POINTER( _source, _end_loop->size, (SOURCE_BASE),
-                                        (DATATYPE), (TOTAL_COUNT) );
-            DO_DEBUG( opal_output( 0, "copy 3. memcpy( %p, %p, %lu ) => space %lu\n",
-                                   _destination, _source, (unsigned long)_end_loop->size, (unsigned long)(*(SPACE) - _i * _end_loop->size) ); );
-            MEMCPY( _destination, _source, _end_loop->size );
-            _source      += _loop->extent;
-            _destination += _loop->extent;
-        }
-        _copy_loops *= _end_loop->size;
-    }
-    *(SPACE)      -= _copy_loops;
-}
-
-#define COPY_PREDEFINED_DATATYPE( ELEM, DATATYPE, SOURCE_BASE, TOTAL_COUNT, COUNT, SOURCE, DESTINATION, SPACE ) \
-    copy_predefined_data( (ELEM), (DATATYPE), (SOURCE_BASE), (TOTAL_COUNT), \
-                          (COUNT), (SOURCE), (DESTINATION), &(SPACE) )
-
-#define COPY_CONTIGUOUS_LOOP( ELEM, DATATYPE, SOURCE_BASE, TOTAL_COUNT, COUNT, SOURCE, DESTINATION, SPACE ) \
-    copy_contiguous_loop( (ELEM), (DATATYPE), (SOURCE_BASE), (TOTAL_COUNT), \
-                          (COUNT), (SOURCE), (DESTINATION), &(SPACE) )
+/**
+ * Overlapping memory regions
+ */
+#undef MEM_OP_NAME
+#define MEM_OP_NAME  overlap
+#undef MEM_OP
+#define MEM_OP       MEMMOVE
+#include "dt_copy.h"
 
 int32_t ompi_ddt_copy_content_same_ddt( const ompi_datatype_t* datatype, int32_t count,
                                         char* destination_base, char* source_base )
 {
-    dt_stack_t* pStack;       /* pointer to the position on the stack */
-    int32_t stack_pos;        /* index of the stack level */
-    uint32_t pos_desc;        /* actual position in the description of the derived datatype */
-    uint32_t count_desc;      /* the number of items already done in the actual pos_desc */
-    dt_elem_desc_t* description;
-    dt_elem_desc_t* pElem;
-    size_t iov_len_local;
-    unsigned char *source = (unsigned char*)source_base,
-                  *destination = (unsigned char*)destination_base;
+    ptrdiff_t extent;
+    int32_t (*fct)( const ompi_datatype_t*, int32_t, char*, char*);
 
-    DO_DEBUG( opal_output( 0, "ompi_ddt_copy_content_same_ddt( %p, %d, dst %p, src %p )\n",
-                           (void*)datatype, count, destination_base, source_base ); );
-    /* empty data ? then do nothing. This should normally be trapped
-     * at a higher level.
-     */
     if( 0 == count ) return 1;
 
-    iov_len_local = count * datatype->size;
-
-    /* If we have to copy a contiguous datatype then simply
-     * do a memcpy.
+    /**
+     * see discussion in coll_basic_reduce.c for the computation of extent when
+     * count != 1. Short version of the story:
+     * (true_extent + ((count - 1) * extent))
      */
-    if( datatype->flags & DT_FLAG_CONTIGUOUS ) {
-        ptrdiff_t extent = (datatype->ub - datatype->lb);
-        /* Now that we know the datatype is contiguous, we should move the 2 pointers
-         * source and destination to the correct displacement.
-         */
-        destination += datatype->true_lb;
-        source      += datatype->true_lb;
-        if( (ptrdiff_t)datatype->size == extent ) {  /* all contiguous == no gaps around */
-            size_t total_length = iov_len_local;
-            size_t memcpy_chunk = ompi_datatype_memcpy_block_size;
-            while( total_length > 0 ) {
-                if( memcpy_chunk > total_length ) memcpy_chunk = total_length;
-                OMPI_DDT_SAFEGUARD_POINTER( destination, memcpy_chunk,
-                                            (unsigned char*)destination_base, datatype, count );
-                OMPI_DDT_SAFEGUARD_POINTER( source, memcpy_chunk,
-                                            (unsigned char*)source_base, datatype, count );
-                DO_DEBUG( opal_output( 0, "copy c1. memcpy( %p, %p, %lu ) => space %lu\n",
-                                       destination, source, (unsigned long)memcpy_chunk, (unsigned long)total_length ); );
-                MEMCPY( destination, source, memcpy_chunk );
-                destination   += memcpy_chunk;
-                source        += memcpy_chunk;
-                total_length  -= memcpy_chunk;
-            }
-            return 0;  /* completed */
+    extent = (datatype->true_ub - datatype->true_lb) + (count - 1) * (datatype->ub - datatype->lb);
+    fct = non_overlap_copy_content_same_ddt;
+    if( destination_base < source_base ) {
+        if( (destination_base + extent) > source_base ) {
+            /* memmove */
+            fct = overlap_copy_content_same_ddt;
         }
-        for( pos_desc = 0; (int32_t)pos_desc < count; pos_desc++ ) {
-            OMPI_DDT_SAFEGUARD_POINTER( destination, datatype->size,
-                                        (unsigned char*)destination_base, datatype, count );
-            OMPI_DDT_SAFEGUARD_POINTER( source, datatype->size,
-                                        (unsigned char*)source_base, datatype, count );
-            DO_DEBUG( opal_output( 0, "copy c2. memcpy( %p, %p, %lu ) => space %lu\n",
-                                   destination, source, (unsigned long)datatype->size,
-                                   (unsigned long)(iov_len_local - (pos_desc * datatype->size)) ); );
-            MEMCPY( destination, source, datatype->size );
-            destination += extent;
-            source += extent;
-        }
-        return 0;  /* completed */
-    }
-
-    pStack = (dt_stack_t*)alloca( sizeof(dt_stack_t) * (datatype->btypes[DT_LOOP] + 1) );
-    pStack->count = count;
-    pStack->index   = -1;
-    pStack->disp    = 0;
-    pos_desc = 0;
-    stack_pos = 0;
-
-    if( datatype->opt_desc.desc != NULL ) {
-        description = datatype->opt_desc.desc;
     } else {
-        description = datatype->desc.desc;
-    }
-
-    if( description[0].elem.common.type == DT_LOOP )
-        count_desc = description[0].loop.loops;
-    else
-        count_desc = description[0].elem.count;
-    pElem = &(description[pos_desc]);
-
-    while( 1 ) {
-        while( OPAL_LIKELY(pElem->elem.common.flags & DT_FLAG_DATA) ) {
-            /* now here we have a basic datatype */
-            COPY_PREDEFINED_DATATYPE( pElem, datatype, (unsigned char*)source_base, count, count_desc,
-                                      source, destination, iov_len_local );
-            pos_desc++;  /* advance to the next data */
-            UPDATE_INTERNAL_COUNTERS( description, pos_desc, pElem, count_desc );
-        }
-        if( DT_END_LOOP == pElem->elem.common.type ) { /* end of the current loop */
-            DO_DEBUG( opal_output( 0, "copy end_loop count %d stack_pos %d pos_desc %d disp %ld space %lu\n",
-                                   (int)pStack->count, stack_pos, pos_desc, (long)pStack->disp, (unsigned long)iov_len_local ); );
-            if( --(pStack->count) == 0 ) { /* end of loop */
-                if( stack_pos == 0 ) {
-                    assert( iov_len_local == 0 );
-                    return 0;  /* completed */
-                }
-                stack_pos--;
-                pStack--;
-                pos_desc++;
-            } else {
-                pos_desc = pStack->index + 1;
-                if( pStack->index == -1 ) {
-                    pStack->disp += (datatype->ub - datatype->lb);
-                } else {
-                    assert( DT_LOOP == description[pStack->index].loop.common.type );
-                    pStack->disp += description[pStack->index].loop.extent;
-                }
-            }
-            source      = (unsigned char*)source_base + pStack->disp;
-            destination = (unsigned char*)destination_base + pStack->disp;
-            UPDATE_INTERNAL_COUNTERS( description, pos_desc, pElem, count_desc );
-            DO_DEBUG( opal_output( 0, "copy new_loop count %d stack_pos %d pos_desc %d disp %ld space %lu\n",
-                                   (int)pStack->count, stack_pos, pos_desc, (long)pStack->disp, (unsigned long)iov_len_local ); );
-        }
-        if( DT_LOOP == pElem->elem.common.type ) {
-            ptrdiff_t local_disp = (ptrdiff_t)source;
-            if( pElem->loop.common.flags & DT_FLAG_CONTIGUOUS ) {
-                COPY_CONTIGUOUS_LOOP( pElem, datatype, (unsigned char*)source_base, count, count_desc,
-                                      source, destination, iov_len_local );
-                pos_desc += pElem->loop.items + 1;
-                goto update_loop_description;
-            }
-            local_disp = (ptrdiff_t)source - local_disp;
-            PUSH_STACK( pStack, stack_pos, pos_desc, DT_LOOP, count_desc,
-                        pStack->disp + local_disp);
-            pos_desc++;
-        update_loop_description:  /* update the current state */
-            source      = (unsigned char*)source_base + pStack->disp;
-            destination = (unsigned char*)destination_base + pStack->disp;
-            UPDATE_INTERNAL_COUNTERS( description, pos_desc, pElem, count_desc );
-            DDT_DUMP_STACK( pStack, stack_pos, pElem, "advance loop" );
-            continue;
+        if( (source_base + extent) > destination_base ) {
+            /* memmove */
+            fct = overlap_copy_content_same_ddt;
         }
     }
+    return fct( datatype, count, destination_base, source_base );
 }
 
