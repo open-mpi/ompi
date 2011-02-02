2755a9f26175a5564a7b62d9e9cc26e2a3db25c9
Jeff Squyres <jsquyres@cisco.com>
2011-02-02 17:11:33 +0000

    As discussed here:
        http://www.open-mpi.org/community/lists/devel/2011/01/8894.php
        http://blogs.cisco.com/performance/building-3rd-party-open-mpi-components/
    
    Contribute a sample of how to build MCA components outside of the Open
    MPI source tree.
    
    This commit was SVN r24346.

---
+++ contrib/build-mca-comps-outside-of-tree/btl_tcp2_ft.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2006      Los Alamos National Security, LLC.  All rights
+ *                         reserved. 
+ *
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+#include "ompi_config.h"
+
+#include <string.h>
+
+
+#include "btl_tcp2.h"
+#include "btl_tcp2_frag.h" 
+#include "btl_tcp2_proc.h"
+#include "btl_tcp2_endpoint.h"
+
+#include "btl_tcp2_ft.h"
+
+int mca_btl_tcp2_ft_event(int state)
+{
+
+    if(OPAL_CRS_CHECKPOINT == state) {
+        ;
+    }
+    else if(OPAL_CRS_CONTINUE == state) {
+        ;
+    }
+    else if(OPAL_CRS_RESTART == state) {
+        ;
+    }
+    else if(OPAL_CRS_TERM == state ) {
+        ;
+    }
+    else {
+        ;
+    }
+
+    return OMPI_SUCCESS;
+}
+
