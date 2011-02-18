2a0f1e949ccbc84798ad652307e8c9fee55e1a1a
Shiqing Fan <shiqing@open-mpi-git-mirror.example.com>
2011-02-18 15:23:35 +0000

    Update with the version info.

--- contrib/platform/win32/CMakeModules/opal_get_version.cmake
+++ contrib/platform/win32/CMakeModules/opal_get_version.cmake
@@ -20,11 +20,11 @@ STRING(REPLACE "release=" "" RELEASE_VERSION ${VALUE})
 FILE(STRINGS ${OpenMPI_SOURCE_DIR}/VERSION VALUE REGEX "^greek=")
 STRING(REPLACE "greek=" "" GREEK_VERSION ${VALUE})
 
-FILE(STRINGS ${OpenMPI_SOURCE_DIR}/VERSION VALUE REGEX "^want_svn=")
-STRING(REPLACE "want_svn=" "" WANT_SVN ${VALUE})
+FILE(STRINGS ${OpenMPI_SOURCE_DIR}/VERSION VALUE REGEX "^want_repo_rev=")
+STRING(REPLACE "want_repo_rev=" "" WANT_REPO_REV ${VALUE})
 
-FILE(STRINGS ${OpenMPI_SOURCE_DIR}/VERSION VALUE REGEX "^svn_r=")
-STRING(REPLACE "svn_r=" "" SVN_R ${VALUE})
+FILE(STRINGS ${OpenMPI_SOURCE_DIR}/VERSION VALUE REGEX "^repo_rev=")
+STRING(REPLACE "repo_rev=" "" REPO_REV ${VALUE})
 
 FILE(STRINGS ${OpenMPI_SOURCE_DIR}/VERSION VALUE REGEX "^date=")
 STRING(REPLACE "date=" "" RELEASE_DATE ${VALUE})
@@ -37,9 +37,9 @@ ELSE(${RELEASE_VERSION} EQUAL 0)
 ENDIF(${RELEASE_VERSION} EQUAL 0)
 
 # Get the svn version
-IF("${WANT_SVN}" STREQUAL "1")
+IF("${WANT_REPO_REV}" STREQUAL "1")
 
-  IF("${SVN_R}" STREQUAL "-1")
+  IF("${REPO_REV}" STREQUAL "-1")
     # for VS.NET2003 users working with web projects, we should also check "_svn" 
     IF(EXISTS ${OpenMPI_SOURCE_DIR}/.svn OR EXISTS ${OpenMPI_SOURCE_DIR}/_svn)
       EXECUTE_PROCESS (COMMAND svnversion -n
@@ -80,17 +80,17 @@ IF("${WANT_SVN}" STREQUAL "1")
       ENDIF(${RESULT} EQUAL 0 AND NOT "${OUTPUT}" STREQUAL "exported")
     ENDIF(NOT RESULT STREQUAL "0")
 
-  ENDIF("${SVN_R}" STREQUAL "-1")
+  ENDIF("${REPO_REV}" STREQUAL "-1")
 
   SET(VERSION_STRING ${VERSION_STRING}${SVN_VERSION})
 
-ELSE("${WANT_SVN}" STREQUAL "1")
-    SET(SVN_VERSION ${SVN_R})
-ENDIF("${WANT_SVN}" STREQUAL "1")
+ELSE("${WANT_REPO_REV}" STREQUAL "1")
+    SET(SVN_VERSION ${REPO_REV})
+ENDIF("${WANT_REPO_REV}" STREQUAL "1")
 
 # Set opal versions
-SET(OPAL_WANT_SVN ${WANT_SVN})
-SET(OPAL_SVN_R ${SVN_R})
+SET(OPAL_WANT_REPO_REV ${WANT_REPO_REV})
+SET(OPAL_REPO_REV ${REPO_REV})
 OMPI_DEF(OPAL_GREEK_VERSION "${GREEK_VERSION}" "Greek - alpha, beta, etc - release number of Open Portable Access Layer." 1 1)
 OMPI_DEF(OPAL_MAJOR_VERSION ${MAJOR_VERSION} "Major release number of Open Portable Access Layer." 0 1)
 OMPI_DEF(OPAL_MINOR_VERSION ${MINOR_VERSION} "Minor release number of Open Portable Access Layer." 0 1)
@@ -99,8 +99,8 @@ OMPI_DEF(OPAL_VERSION ${VERSION_STRING} "Complete release number of Open Portabl
 OMPI_DEF(OPAL_IDENT_STRING ${VERSION_STRING} "ident string for Open MPI." 1 1)
 
 # Set OMPI versions
-SET(OMPI_WANT_SVN ${WANT_SVN})
-SET(OMPI_SVN_R ${SVN_R})
+SET(OMPI_WANT_REPO_REV ${WANT_REPO_REV})
+SET(OMPI_REPO_REV ${REPO_REV})
 OMPI_DEF(OMPI_GREEK_VERSION "${GREEK_VERSION}" "Greek - alpha, beta, etc - release number of Open Portable Access Layer." 1 1)
 OMPI_DEF(OMPI_MAJOR_VERSION ${MAJOR_VERSION} "Major release number of Open MPI." 0 1)
 OMPI_DEF(OMPI_MINOR_VERSION ${MINOR_VERSION} "Minor release number of Open MPI." 0 1)
@@ -108,8 +108,8 @@ OMPI_DEF(OMPI_RELEASE_VERSION ${RELEASE_VERSION} "Release number of Open MPI." 0
 OMPI_DEF(OMPI_VERSION ${VERSION_STRING} "Complete release number of Open MPI." 1 1)
 
 # Set ORTE versions
-SET(ORTE_WANT_SVN ${WANT_SVN})
-SET(ORTE_SVN_R ${SVN_R})
+SET(ORTE_WANT_REPO_REV ${WANT_REPO_REV})
+SET(ORTE_REPO_REV ${REPO_REV})
 OMPI_DEF(ORTE_GREEK_VERSION "${GREEK_VERSION}" "Greek - alpha, beta, etc - release number of Open Run-Time Environment." 1 1)
 OMPI_DEF(ORTE_MAJOR_VERSION ${MAJOR_VERSION} "Major release number of Open Run-Time Environment." 0 1)
 OMPI_DEF(ORTE_MINOR_VERSION ${MINOR_VERSION} "Minor release number of Open Run-Time Environment." 0 1)
