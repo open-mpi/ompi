open-mpi/ompi

commit 45fd0f972153ea745cd493284e90ace02b8c19fe

- Addendum to r24421: get mca_maffinity_libnuma to compile on linux    (with libnuma-2.0.4 / LIBNUMA_API_VERSION 2): numa_get_run_node_mask    returns a struct bitmask *.
   Whether it's a good idea to blindly pass that on to
   numa_set_membind() is another matter: one might want to match against
   the list returned by numa_get_mems_allowed(), which may be set by the
   outside environment.

   Refs #2698.



git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@24442 63e3feb5-37d5-0310-a306-e8a459e722fe

opal/mca/maffinity/libnuma/maffinity_libnuma_module.c
@@ -69,7 +69,7 @@ int opal_maffinity_libnuma_component_query(mca_base_module_t **module, int *prio
 
 static int libnuma_module_init(void)
 {
-    nodemask_t mask;
+    struct bitmask * mask;
 
     /* If we have a strict policy set, then bind all memory to this
        numa node.  Note that maffinity won't be invoked unless the
@@ -78,7 +78,7 @@ static int libnuma_module_init(void)
        numa_get_run_node_mask(). */
     if (MPOL_BIND == mca_maffinity_libnuma_component.libnuma_policy) {
         mask = numa_get_run_node_mask();
-        numa_set_membind(&mask);
+        numa_set_membind(mask);
     }
 
     /* We want libnuma to fail to alloc if it can't allocate on the

commit 5e082d68f6993fa7451f4bebf520e947cbffd468

Fix the compile error for libnuma 0.9.x introduced in r24442; hopefully, this now compiles for libnuma 0.9.x and libnuma 2.0.x.
Fixes for the strategy discussed in the commit message for r24442
(i.e., check against numa_get_mems_allowed(), which only exists in
libnuma 2.0.x) and the new new new plan on #2698 coming in a separate
commit.

This commit was SVN r24443.

The following SVN revision numbers were found above:
  r24442 --> open-mpi/ompi@90a8fe4aad3ca95824c1c58d0f72d6775e4768f1

opal/mca/maffinity/libnuma/maffinity_libnuma_module.c
@@ -69,7 +69,17 @@ int opal_maffinity_libnuma_component_query(mca_base_module_t **module, int *prio
 
 static int libnuma_module_init(void)
 {
-    struct bitmask * mask;
+    /* Libnuma didn't start defining an API version until version 2.
+       Sigh.  For convenience, ensure it's always defined. */
+#if !defined(LIBNUMA_API_VERSION)
+#define LIBNUMA_API_VERSION 1
+#endif
+
+#if LIBNUMA_API_VERSION >= 2
+    struct bitmask *mask;
+#else
+    nodemask_t mask;
+#endif
 
     /* If we have a strict policy set, then bind all memory to this
        numa node.  Note that maffinity won't be invoked unless the
@@ -78,7 +88,11 @@ static int libnuma_module_init(void)
        numa_get_run_node_mask(). */
     if (MPOL_BIND == mca_maffinity_libnuma_component.libnuma_policy) {
         mask = numa_get_run_node_mask();
+#if LIBNUMA_API_VERSION >= 2
         numa_set_membind(mask);
+#else
+        numa_set_membind(&mask);
+#endif
     }
 
     /* We want libnuma to fail to alloc if it can't allocate on the