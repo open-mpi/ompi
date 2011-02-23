e8ba72258e14d4e53469b8c2f565168b8b9f9baa
Jeff Squyres <jsquyres@cisco.com>
2011-02-23 22:43:58 +0000

    Patch for PPC64 platforms with smt=off, issue raised by Brad.  This fix will be included in hwloc 1.1.2.
    Brad -- can you verify that this fixes the issue for you?
    
    Fixes trac:2732.
    
    This commit was SVN r24450.
    
    The following Trac tickets were found above:
      Ticket 2732 --> https://svn.open-mpi.org/trac/ompi/ticket/2732

--- opal/mca/common/hwloc/hwloc/src/topology-linux.c
+++ opal/mca/common/hwloc/hwloc/src/topology-linux.c
@@ -2079,9 +2079,11 @@ look_powerpc_device_tree(struct hwloc_topology *topology)
       uint32_t nthreads = cb / sizeof(threads[0]);
 
       if (NULL != threads) {
+        unsigned int i;
         cpuset = hwloc_bitmap_alloc();
-        for (unsigned int i = 0; i < nthreads; ++i) {
-          hwloc_bitmap_set(cpuset, ntohl(threads[i]));
+        for (i = 0; i < nthreads; ++i) {
+          if (hwloc_bitmap_isset(topology->levels[0][0]->complete_cpuset, ntohl(threads[i])))
+            hwloc_bitmap_set(cpuset, ntohl(threads[i]));
         }
         free(threads);
       } else if ((unsigned int)-1 != reg) {
