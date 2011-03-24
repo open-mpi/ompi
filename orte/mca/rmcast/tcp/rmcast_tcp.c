d7e029cb40b3f7789a8d9e94da2ce61bbf7c7fa4
Ralph Castain <rhc@open-mpi.org>
2011-03-24 19:05:39 +0000

    Convert heartbeat to multicast basis
    This commit was SVN r24570.

--- orte/mca/rmcast/tcp/rmcast_tcp.c
+++ orte/mca/rmcast/tcp/rmcast_tcp.c
@@ -202,6 +202,18 @@ static int init(void)
             ORTE_ERROR_LOG(rc);
             return rc;
         }
+        /* open the app public channel so we can hear app announcements and commands */
+        if (ORTE_SUCCESS != (rc = open_channel(ORTE_RMCAST_APP_PUBLIC_CHANNEL, "app-announce",
+                                               NULL, -1, NULL, ORTE_RMCAST_BIDIR))) {
+            ORTE_ERROR_LOG(rc);
+            return rc;
+        }
+        /* open the heartbeat channel */
+        if (ORTE_SUCCESS != (rc = open_channel(ORTE_RMCAST_HEARTBEAT_CHANNEL, "heartbeat",
+                                               NULL, -1, NULL, ORTE_RMCAST_BIDIR))) {
+            ORTE_ERROR_LOG(rc);
+            return rc;
+        }
     } else if (ORTE_PROC_IS_APP) {
         /* apps open the app public and data server channels */
         if (ORTE_SUCCESS != (rc = open_channel(ORTE_RMCAST_APP_PUBLIC_CHANNEL, "app-announce",
