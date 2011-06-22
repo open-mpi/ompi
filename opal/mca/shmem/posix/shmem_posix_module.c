72716fd11ae9eb05a53f8ecd8402f28fca1f6bb3
Samuel K. Gutierrez <samuel@open-mpi-git-mirror.example.com>
2011-06-22 23:08:58 +0000

    fix posix builds on solaris.  shmem still needs more cleanup on solaris, but at least shmem will stop breaking builds (i hope).

--- opal/mca/shmem/posix/shmem_posix_module.c
+++ opal/mca/shmem/posix/shmem_posix_module.c
@@ -39,6 +39,9 @@
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif /* HAVE_STRING_H */
+#ifdef HAVE_NETDB_H
+#include <netdb.h>
+#endif /* HAVE_NETDB_H */
 
 #include "opal/constants.h"
 #include "opal_stdint.h"
