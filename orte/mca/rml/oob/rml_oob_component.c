open-mpi/ompi

commit 55fd3c5d059a0f38f3ca1eb75689efc687f64337

Fix a type cast.
git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@24888 63e3feb5-37d5-0310-a306-e8a459e722fe

orte/mca/rml/oob/rml_oob_component.c
@@ -514,7 +514,7 @@ rml_oob_recv_route_callback(int status,
     ORTE_RML_OOB_MSG_HEADER_HTON(*hdr);
 
     /* NTH: fix potential race condition. oob may modify iov before the oob send completes */
-    new_iov = calloc (count, sizeof (struct iovec));
+    new_iov = (iovec*) calloc (count, sizeof (struct iovec));
     if (NULL == new_iov) {
 	opal_output (0, "%s:route_callback malloc error!", ORTE_NAME_PRINT(ORTE_PROC_MY_NAME));
         opal_backtrace_print(stderr);

commit 3893a5a1de7b625a02d82cf9faea279f87fb6227

Fix compile error introduced in r24888.
This commit was SVN r24889.

The following SVN revision numbers were found above:
  r24888 --> open-mpi/ompi@e5253647ea15ba5af49c31cbcd5d1dcf930c3670

orte/mca/rml/oob/rml_oob_component.c
@@ -513,8 +513,9 @@ rml_oob_recv_route_callback(int status,
 
     ORTE_RML_OOB_MSG_HEADER_HTON(*hdr);
 
-    /* NTH: fix potential race condition. oob may modify iov before the oob send completes */
-    new_iov = (iovec*) calloc (count, sizeof (struct iovec));
+    /* NTH: fix potential race condition. oob may modify iov before
+       the oob send completes */
+    new_iov = (struct iovec*) calloc (count, sizeof (struct iovec));
     if (NULL == new_iov) {
 	opal_output (0, "%s:route_callback malloc error!", ORTE_NAME_PRINT(ORTE_PROC_MY_NAME));
         opal_backtrace_print(stderr);