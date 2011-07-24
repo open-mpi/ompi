3d5089e9134f1c43ec8dcd1ecc64e6249d9c5d32
George Bosilca <bosilca@icl.utk.edu>
2011-07-24 09:18:07 +0000

    Fixes trac:2674: get dynamic SL from the SA
    r24900
    Always defining OMPI_ENABLE_DYNAMIC_SL, not only when the feature enabled.
    Also, refactoring some code - make all the checks only when relevant.
    
    
    r24852
    As suggested by Jeff, checking if dynamic SL support
    was explicitly requested. If it was, but opensm-devel
    package is not found, warn and abort.
    Otherwise, doing the best effort: if opensm-devel found,
    enable dynamic SL. If it's not found, disable dynamic
    SL and build OMPI w/o it.
    
    
    r24830
    Supporting dynamic SL (#2674)
    
     - Added enable/disable configuration parameter for dynamic SL
     - All the dynamic SL code is conditionalized
     - Removed libibmad dependency
     - Using only one include - ib_types.h (part of opensm-devel package)
     - Removed all the macro and data types definitions, using the
       existing definitions from ib_types.h instead
     - general cleaning here and there
    
    The async mode is not implemented yet - stay tuned...
    
    This commit was SVN r24930.
    
    The following SVN revision numbers were found above:
      r24830 --> open-mpi/ompi@b05211148d5d1a46aa74edcf23e5fccebabbf40f
      r24852 --> open-mpi/ompi@3e43b21d41c33b0d3d774632a52497055937a0ee
      r24900 --> open-mpi/ompi@78ea8bcea2d8b26d440997f627368bf1bd50985b
    
    The following Trac tickets were found above:
      Ticket 2674 --> https://svn.open-mpi.org/trac/ompi/ticket/2674

--- ompi/mca/btl/openib/btl_openib_mca.c
+++ ompi/mca/btl/openib/btl_openib_mca.c
@@ -397,6 +397,15 @@ int btl_openib_register_mca_params(void)
     }
     mca_btl_openib_component.ib_service_level = (uint32_t) ival;
 
+#if (ENABLE_DYNAMIC_SL)
+    CHECK(reg_int("ib_path_record_service_level", NULL,
+                  "Enable getting InfiniBand service level from PathRecord "
+                  "(must be >= 0, 0 = disabled, positive = try to get the "
+                  "service level from PathRecord)",
+                  0, &ival, REGINT_GE_ZERO));
+    mca_btl_openib_component.ib_path_record_service_level = (uint32_t) ival;
+#endif
+
     CHECK(reg_int("use_eager_rdma", NULL, "Use RDMA for eager messages "
                   "(-1 = use device default, 0 = do not use eager RDMA, "
                   "1 = use eager RDMA)",
