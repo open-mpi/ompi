7cd8f21b7fbc4c74aee909fda3c9dd90bc07627a
Nathan Hjelm <hjelmn@lanl.gov>
2011-09-20 00:13:05 +0000

    add libevent 2.0.13 module
    This commit was SVN r25161.

---
+++ opal/mca/event/libevent2013/configure.m4
@@ -0,0 +1,189 @@
+# -*- shell-script -*-
+#
+# Copyright (c) 2009-2010 Cisco Systems, Inc.  All rights reserved. 
+#
+# $COPYRIGHT$
+# 
+# Additional copyrights may follow
+# 
+# $HEADER$
+#
+AC_DEFUN([MCA_opal_event_libevent2013_PRIORITY], [60])
+
+#
+# Force this component to compile in static-only mode
+#
+AC_DEFUN([MCA_opal_event_libevent2013_COMPILE_MODE], [
+    AC_MSG_CHECKING([for MCA component $2:$3 compile mode])
+    $4="static"
+    AC_MSG_RESULT([$$4])
+])
+
+# MCA_event_libevent2013_CONFIG([action-if-can-compile], 
+#                              [action-if-cant-compile])
+# ------------------------------------------------
+AC_DEFUN([MCA_opal_event_libevent2013_CONFIG],[
+    AC_CONFIG_FILES([opal/mca/event/libevent2013/Makefile])
+    basedir="opal/mca/event/libevent2013"
+
+    CFLAGS_save="$CFLAGS"
+    CFLAGS="$OMPI_CFLAGS_BEFORE_PICKY $OPAL_VISIBILITY_CFLAGS"
+    CPPFLAGS_save="$CPPFLAGS"
+    CPPFLAGS="-I$OMPI_TOP_SRCDIR -I$OMPI_TOP_BUILDDIR -I$OMPI_TOP_SRCDIR/opal/include $CPPFLAGS"
+    
+    AC_MSG_CHECKING([libevent configuration args])
+
+    str=`event_args="--disable-dns --disable-http --disable-rpc --disable-openssl --enable-hidden-symbols --includedir=$includedir/openmpi/opal/event/libevent/include"`
+    eval $str
+    unset str
+
+    AC_ARG_ENABLE(event-rtsig,
+        AC_HELP_STRING([--enable-event-rtsig],
+                       [enable support for real time signals (experimental)]))
+    if test "$enable_event_rtsig" = "yes"; then
+        event_args="$event_args --enable-rtsig"
+    fi
+
+    AC_ARG_ENABLE(event-select,
+                  AC_HELP_STRING([--disable-event-select], [disable select support]))
+    if test "$enable_event_select" = "no"; then
+        event_args="$event_args --disable-select"
+    fi
+
+    AC_ARG_ENABLE(event-poll,
+                  AC_HELP_STRING([--disable-event-poll], [disable poll support]))
+    if test "$enable_event_poll" = "no"; then
+        event_args="$event_args --disable-poll"
+    fi
+
+    AC_ARG_ENABLE(event-devpoll,
+                  AC_HELP_STRING([--disable-event-devpoll], [disable devpoll support]))
+    if test "$enable_event_devpoll" = "no"; then
+        event_args="$event_args --disable-devpoll"
+    fi
+
+    AC_ARG_ENABLE(event-kqueue,
+                  AC_HELP_STRING([--disable-event-kqueue], [disable kqueue support]))
+    if test "$enable_event_kqueue" = "no"; then
+        event_args="$event_args --disable-kqueue"
+    fi
+
+    AC_ARG_ENABLE(event-epoll,
+                  AC_HELP_STRING([--disable-event-epoll], [disable epoll support]))
+    if test "$enable_event_epoll" = "no"; then
+        event_args="$event_args --disable-epoll"
+    fi
+
+    AC_ARG_ENABLE(event-evport,
+                  AC_HELP_STRING([--enable-event-evport], [enable evport support]))
+    if test "$enable_event_evport" = "yes"; then
+        event_args="$event_args --enable-evport"
+    else
+        event_args="$event_args --disable-evport"
+    fi
+
+    AC_ARG_ENABLE(event-signal,
+                  AC_HELP_STRING([--disable-event-signal], [disable signal support]))
+    if test "$enable_event_signal" = "no"; then
+        event_args="$event_args --disable-signal"
+    fi
+
+    AC_ARG_ENABLE(event-debug,
+                  AC_HELP_STRING([--enable-event-debug], [enable event library debug output]))
+    if test "$enable_event_debug" = "no"; then
+        event_args="$event_args --disable-debug-mode"
+    fi
+
+    AC_ARG_ENABLE(event-thread-support,
+                  AC_HELP_STRING([--enable-event-thread-support], [enable event library internal thread support]))
+    if test "$enable_event_thread_support" = "yes"; then
+        AC_DEFINE_UNQUOTED(OPAL_EVENT_HAVE_THREAD_SUPPORT, 1,
+                           [Thread support was configured into the event library])
+    else
+        event_args="$event_args --disable-thread-support"
+        AC_DEFINE_UNQUOTED(OPAL_EVENT_HAVE_THREAD_SUPPORT, 0,
+                           [Thread support was not configured into the event library])
+    fi
+
+    AC_MSG_RESULT([$event_args])
+
+    OMPI_CONFIG_SUBDIR([$basedir/libevent], 
+        [$event_args $ompi_subdir_args],
+        [libevent_happy="yes"], [libevent_happy="no"])
+    if test "$libevent_happy" = "no"; then
+        AC_MSG_WARN([Event library failed to configure])
+        AC_MSG_ERROR([Cannot continue])
+    fi
+
+    CFLAGS="$CFLAGS_save"
+    CPPFLAGS="$CPPFLAGS_save"
+
+    # If we configured successfully, set OPAL_HAVE_WORKING_EVENTOPS to
+    # the value in the generated libevent/config.h (NOT
+    # libevent/include/event2/event-config.h!).  Otherwise, set it to
+    # 0.
+    file=$basedir/libevent/config.h
+    AS_IF([test "$libevent_happy" = "yes" -a -r $file], 
+          [OPAL_HAVE_WORKING_EVENTOPS=`grep HAVE_WORKING_EVENTOPS $file | awk '{print [$]3 }'`
+
+           # Build libevent/include/event2/event-config.h.  If we
+           # don't do it here, then libevent's Makefile.am will build
+           # it during "make all", which is too late for us (because
+           # other things are built before the event framework that
+           # end up including event-config.h).  The steps below were
+           # copied from libevent's Makefile.am.
+           AC_CONFIG_COMMANDS([opal/mca/event/libevent2013/libevent/include/event2/event-config.h],
+                              [basedir="opal/mca/event/libevent2013"
+                               file="$basedir/libevent/include/event2/event-config.h"
+                               rm -f "$file.new"
+                               cat > "$file.new" <<EOF
+/* event2/event-config.h
+ *
+ * This file was generated by autoconf when libevent was built, and
+ * post- processed by Open MPI's component configure.m4 (so that
+ * Libevent wouldn't build it during "make all") so that its macros
+ * would have a uniform prefix.
+ *
+ * DO NOT EDIT THIS FILE.
+ *
+ * Do not rely on macros in this file existing in later versions
+ */
+#ifndef _EVENT2_EVENT_CONFIG_H_
+#define _EVENT2_EVENT_CONFIG_H_
+EOF
+
+                               sed -e 's/#define /#define _EVENT_/' \
+                                   -e 's/#undef /#undef _EVENT_/' \
+                                   -e 's/#ifndef /#ifndef _EVENT_/' < "$basedir/libevent/config.h" >> "$file.new"
+                               echo "#endif" >> "$file.new"
+
+                               # Only make a new .h file if the
+                               # contents haven't changed
+                               diff -q $file "$file.new" > /dev/null 2> /dev/null
+                               if test "$?" = "0"; then
+                                   echo $file is unchanged
+                               else
+                                   cp "$file.new" $file
+                               fi
+                               rm -f "$file.new"])
+
+           # Must set this variable so that the framework m4 knows
+           # what file to include in opal/mca/event/event.h
+           event_base_include="libevent2013/libevent2013.h"
+
+           # We also set _cppflags so that when including
+           # libevent2013.h, it can find all the actual libevent files.
+           # Be a little friendly and only include the -I for the
+           # builddir if it's different than the srcdir.
+           file=$basedir/libevent
+           event_base_include_cppflags="-I$OMPI_TOP_SRCDIR/$file -I$OMPI_TOP_SRCDIR/$file/include"
+           AS_IF([test "$OMPI_TOP_BUILDDIR" != "$OMPI_TOP_SRCDIR"],
+                 [event_base_include_cppflags="$event_base_include_cppflags -I$OMPI_TOP_BUILDDIR/$file/include"])
+           if test "$with_devel_headers" = "yes" ; then
+               WRAPPER_EXTRA_CPPFLAGS="$WRAPPER_EXTRA_CPPFLAGS "'-I${includedir}/openmpi/opal/mca/event/libevent2013/libevent -I${includedir}/openmpi/opal/mca/event/libevent2013/libevent/include'
+           fi
+           $1],
+          [$2
+           OPAL_HAVE_WORKING_EVENTOPS=0])
+    unset file
+])
---
+++ opal/mca/event/libevent2013/libevent2013_module.c
@@ -0,0 +1,308 @@
+/*
+ * Copyright (c) 2010      Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2010      Oracle and/or its affiliates.  All rights reserved.
+ */
+#include "opal_config.h"
+#include "opal/constants.h"
+#include "config.h"
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#undef WIN32_LEAN_AND_MEAN
+#endif
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+#include <sys/queue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef WIN32
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#endif
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+
+#include "opal/class/opal_object.h"
+#include "opal/threads/mutex.h"
+#include "opal/threads/threads.h"
+#include "opal/util/output.h"
+#include "opal/util/argv.h"
+#include "opal/util/fd.h"
+#include "opal/mca/base/mca_base_param.h"
+
+#include "libevent2013.h"
+#include "opal/mca/event/base/base.h"
+
+#include "libevent/event.h"
+#include "libevent/event-internal.h"
+
+#include "opal/mca/event/event.h"
+
+/* copied from event.c */
+#if defined(_EVENT_HAVE_EVENT_PORTS) && _EVENT_HAVE_EVENT_PORTS
+extern const struct eventop evportops;
+#endif
+#if defined(_EVENT_HAVE_SELECT) && _EVENT_HAVE_SELECT
+extern const struct eventop selectops;
+#endif
+#if defined(_EVENT_HAVE_POLL) && _EVENT_HAVE_POLL
+extern const struct eventop pollops;
+#endif
+#if defined(_EVENT_HAVE_EPOLL) && _EVENT_HAVE_EPOLL
+extern const struct eventop epollops;
+#endif
+#if defined(_EVENT_HAVE_WORKING_KQUEUE) && _EVENT_HAVE_WORKING_KQUEUE
+extern const struct eventop kqops;
+#endif
+#if defined(_EVENT_HAVE_DEVPOLL) && _EVENT_HAVE_DEVPOLL
+extern const struct eventop devpollops;
+#endif
+#ifdef WIN32
+extern const struct eventop win32ops;
+#endif
+
+/* Array of backends in order of preference. */
+static const struct eventop *eventops[] = {
+#if defined(_EVENT_HAVE_EVENT_PORTS) && _EVENT_HAVE_EVENT_PORTS
+	&evportops,
+#endif
+#if defined(_EVENT_HAVE_WORKING_KQUEUE) && _EVENT_HAVE_WORKING_KQUEUE
+	&kqops,
+#endif
+#if defined(_EVENT_HAVE_EPOLL) && _EVENT_HAVE_EPOLL
+	&epollops,
+#endif
+#if defined(_EVENT_HAVE_DEVPOLL) && _EVENT_HAVE_DEVPOLL
+	&devpollops,
+#endif
+#if defined(_EVENT_HAVE_POLL) && _EVENT_HAVE_POLL
+	&pollops,
+#endif
+#if defined(_EVENT_HAVE_SELECT) && _EVENT_HAVE_SELECT
+	&selectops,
+#endif
+#ifdef WIN32
+	&win32ops,
+#endif
+	NULL
+};
+
+static struct event_config *config=NULL;
+
+static void update_event(int fd, short flags, void* arg)
+{
+    opal_event_update_t up;
+
+    /* read the event */
+    opal_fd_read(fd, sizeof(opal_event_update_t), &up);
+    if (NULL == up.ev) {
+        return;
+    }
+    if (OPAL_EVENT_ADD == up.op) {
+        event_add(up.ev, 0);
+    } else if (OPAL_EVENT_DEL == up.op) {
+        event_del(up.ev);
+    }
+    return;
+}
+
+/* Public function -- not part of the module */
+/* This includes (hopefully) a temporary change
+ * to deal with cross-base sync. Specifically,
+ * when an event in one base needs to release
+ * a condition_wait in another base, we need
+ * to "wakeup" the event base in the second base
+ * so the condition_wait can be checked
+ */
+opal_event_base_t* opal_event_base_create(void)
+{
+    struct event_base *base;
+    opal_event_base_t *evbase;
+
+    base = event_base_new_with_config(config);
+    if (NULL == base) {
+        /* there is no backend method that does what we want */
+        opal_output(0, "No event method available");
+        return NULL;
+    }
+    evbase = (opal_event_base_t*)malloc(sizeof(opal_event_base_t));
+    evbase->base = base;
+#ifndef __WINDOWS__
+    if (pipe(evbase->update_pipe) < 0) {
+        opal_output(0, "Unable to open update pipe");
+        free(evbase);
+        event_base_free(base);
+        return NULL;
+    }
+#else
+    if (create_socketpair(AF_UNIX, SOCK_STREAM, 0, evbase->update_pipe) == -1) {
+        opal_output(0, "Unable to open update socket");
+        free(evbase);
+        event_base_free(base);
+        return NULL;
+    }
+#endif
+    event_assign(&evbase->update_event, base,
+                 evbase->update_pipe[0], EV_READ | EV_PERSIST,
+                 update_event, NULL);
+    event_add(&evbase->update_event, 0);
+    return evbase;
+}
+
+void opal_event_base_finalize(opal_event_base_t *evbase)
+{
+    /* delete the wakeup event */
+    event_del(&evbase->update_event);
+#ifndef __WINDOWS__
+    /* close the pipe */
+    close(evbase->update_pipe[0]);
+    close(evbase->update_pipe[1]);
+#else
+    /* close the socket */
+    closesocket(evbase->update_pipe[0]);
+    closesocket(evbase->update_pipe[1]);
+#endif
+    /* release the base */
+    event_base_free(evbase->base);
+    /* free the storage */
+    free(evbase);
+}
+
+int opal_event_init(void)
+{
+    char* event_module_include=NULL;
+    char **modules=NULL, **includes=NULL;
+    bool dumpit=false;
+    int i, j;
+    const struct eventop** _eventop = eventops;
+    char available_eventops[1024] = "none";
+    char* help_msg = NULL;
+    int position = 0;
+
+    if (opal_output_get_verbosity(opal_event_base_output) > 4) {
+        event_enable_debug_mode();
+        event_set_debug_output(true);
+        dumpit = true;
+    }
+
+#if OPAL_EVENT_HAVE_THREAD_SUPPORT
+    /* turn on libevent thread safety */
+    evthread_use_pthreads ();
+#endif
+
+    /* Retrieve the upper level specified event system, if any.
+     * Default to select() on OS X and poll() everywhere else because
+     * various parts of OMPI / ORTE use libevent with pty's.  pty's
+     * *only* work with select on OS X (tested on Tiger and Leopard);
+     * we *know* that both select and poll works with pty's everywhere
+     * else we care about (other mechansisms such as epoll *may* work
+     * with pty's -- we have not tested comprehensively with newer
+     * versions of Linux, etc.).  So the safe thing to do is:
+     *
+     * - On OS X, default to using "select" only
+     * - Everywhere else, default to using "poll" only (because poll
+     *   is more scalable than select)
+     *
+     * An upper layer may override this setting if it knows that pty's
+     * won't be used with libevent.  For example, we currently have
+     * ompi_mpi_init() set to use "all" (to include epoll and friends)
+     * so that the TCP BTL can be a bit more scalable -- because we
+     * *know* that MPI apps don't use pty's with libevent.  
+     * Note that other tools explicitly *do* use pty's with libevent:
+     *
+     * - orted
+     * - orterun (probably only if it launches locally)
+     * - ...?
+     */
+
+    while( NULL != (*_eventop) ) {
+        opal_argv_append_nosize(&modules, (*_eventop)->name);
+        if( 0 != position ) {
+            position += snprintf( available_eventops + position,
+                                  (size_t)(1024 - position),
+                                  ", %s", (*_eventop)->name );
+        } else {
+            position += snprintf( available_eventops + position,
+                                  (size_t)(1024 - position),
+                                  "%s", (*_eventop)->name );
+        }
+        available_eventops[position] = '\0';
+        _eventop++;  /* go to the next available eventop */
+    }
+    asprintf( &help_msg, 
+              "Comma-delimited list of libevent subsystems "
+              "to use (%s -- available on your platform)",
+              available_eventops );
+    mca_base_param_reg_string_name("opal", "event_include",
+                                   help_msg, false, false, 
+#ifdef __APPLE__
+                                   "select",
+#else
+#ifdef __WINDOWS__
+                                   "win32",
+#else
+                                   "poll",
+#endif
+#endif
+                                   &event_module_include);
+
+    if (dumpit) {
+        opal_output(0, "event: available subsystems: %s", available_eventops);
+    }
+
+    free(help_msg);  /* release the help message */
+
+    if (NULL == event_module_include) {
+        /* Shouldn't happen, but... */
+        event_module_include = strdup("select");
+    }
+    includes = opal_argv_split(event_module_include,',');
+    free(event_module_include);
+
+    /* get a configuration object */
+    config = event_config_new();
+    /* cycle thru the available subsystems */
+    for (i=0; NULL != modules[i]; i++) {
+        /* if this module isn't included in the given ones,
+         * then exclude it
+         */
+        dumpit = true;
+        for (j=0; NULL != includes[j]; j++) {
+            if (0 == strcmp("all", includes[j]) ||
+                0 == strcmp(modules[i], includes[j])) {
+                dumpit = false;
+                break;
+            }
+        }
+        if (dumpit) {
+            event_config_avoid_method(config, modules[i]);
+        }
+    }
+    opal_argv_free(includes);
+    opal_argv_free(modules);
+
+    return OPAL_SUCCESS;
+}
+
+int opal_event_reinit(opal_event_base_t *evbase)
+{
+    return event_reinit(evbase->base);
+}
+
+struct timeval *opal_event_base_init_common_timeout (opal_event_base_t *evbase,
+						     struct timeval *tv_in)
+{
+    return event_base_init_common_timeout (evbase->base, tv_in);
+}
+
