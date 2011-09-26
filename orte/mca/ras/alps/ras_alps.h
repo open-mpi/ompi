45ac7f60b4c631ed141c87875004ff8e6b9f2bb0
Samuel K. Gutierrez <samuel@open-mpi-git-mirror.example.com>
2011-09-26 21:31:08 +0000

    modifications to ras alps.  this commit allows users to mpirun without having to set id environment variables (BASIL_RESERVATION_ID, OMPI_ALPS_RESID).  note, however, that we preserved the old behavior.  if an id environment variable is set, it will be obeyed and our new code path is essentially bypassed. if we missed something, please yell at us. with this commit, the use of ras-alps-command.sh is no longer needed... at least that is our hope.

--- orte/mca/ras/alps/ras_alps.h
+++ orte/mca/ras/alps/ras_alps.h
@@ -10,6 +10,8 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2008      UT-Battelle, LLC
+ * Copyright (c) 2011      Los Alamos National Security, LLC.
+ *                         All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -33,6 +35,7 @@ BEGIN_C_DECLS
 ORTE_DECLSPEC extern orte_ras_base_component_t mca_ras_alps_component;
 ORTE_DECLSPEC extern orte_ras_base_module_t orte_ras_alps_module;
 ORTE_DECLSPEC int orte_ras_alps_get_appinfo_attempts( int *attempts );
+ORTE_DECLSPEC extern unsigned long int orte_ras_alps_res_id;
 
 END_C_DECLS
 
