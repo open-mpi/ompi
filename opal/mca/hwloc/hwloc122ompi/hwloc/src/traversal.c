8c4512a99413714e1796b12ade6de675da4cd809
Ralph Castain <rhc@open-mpi.org>
2011-10-11 05:54:26 +0000

    Fix the verbose output for caches (again) so they are properly labeled, pending adoption of the upstream patch we supplied.
    This commit was SVN r25251.

--- opal/mca/hwloc/hwloc122ompi/hwloc/src/traversal.c
+++ opal/mca/hwloc/hwloc122ompi/hwloc/src/traversal.c
@@ -258,7 +258,7 @@ hwloc_obj_attr_snprintf(char * __hwloc_restrict string, size_t size, hwloc_obj_t
   switch (obj->type) {
   case HWLOC_OBJ_CACHE:
     if (verbose)
-      res = hwloc_snprintf(tmp, tmplen, "%s%lu%s%sline=%u",
+      res = hwloc_snprintf(tmp, tmplen, "%sSize=%lu%s%sLine=%u",
 			   prefix,
 			   (unsigned long) hwloc_memory_size_printf_value(obj->attr->cache.size, verbose),
 			   hwloc_memory_size_printf_unit(obj->attr->cache.size, verbose),
