663f4546f580f45ed9d0055ebe4e6f3b9aa96d58
Samuel Gutierrez <samuel@lanl.gov>
2011-10-24 18:38:12 +0000

    fix define typo in psm mtl.
    This commit was SVN r25362.

--- ompi/mca/mtl/psm/mtl_psm_types.h
+++ ompi/mca/mtl/psm/mtl_psm_types.h
@@ -10,6 +10,8 @@
  * Copyright (c) 2004-2006 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2006      QLogic Corporation. All rights reserved.
+ * Copyright (c) 2011      Los Alamos National Security, LLC.
+ *                         All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -18,7 +20,7 @@
  */
 
 #ifndef MTL_PSM_TYPES_H_HAS_BEEN_INCLUDED
-#define MTL_PSM_TYPS_H_HAS_BEEN_INCLUDED
+#define MTL_PSM_TYPES_H_HAS_BEEN_INCLUDED
 
 #include "ompi_config.h"
 #include "mtl_psm.h"
