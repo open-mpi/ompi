d603f31976accaa3b7aa411c6619344172339d4f
Nathan Hjelm <hjelmn@lanl.gov>
2011-11-08 15:44:54 +0000

    removed ptr member from seg_key union
    This commit was SVN r25460.

--- ompi/mca/btl/btl.h
+++ ompi/mca/btl/btl.h
@@ -241,7 +241,6 @@ struct mca_btl_base_segment_t {
         uint32_t  key32[4];
         uint64_t  key64[2];
         uint8_t   key8[16];
-        uintptr_t ptr[2];
     } seg_key;
 };
 typedef struct mca_btl_base_segment_t mca_btl_base_segment_t;
