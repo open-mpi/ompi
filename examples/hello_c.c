c18c9b7677a4490cad9a0b892d3eeb6d4bdab634
George Bosilca <bosilca@icl.utk.edu>
2011-11-29 19:45:06 +0000

    Fixes trac:2911: Move hello_c example fix to v1.5
    ---svn-pre-commit-ignore-below---
    r25466
    Why would Barrier be in the hello world example?  Looks like a really
    old accidental commit.  :-)
    
    This commit was SVN r25529.
    
    The following SVN revision numbers were found above:
      r25466 --> open-mpi/ompi@face13157cef4d633a12526e4f70e27898909ebc
    
    The following Trac tickets were found above:
      Ticket 2911 --> https://svn.open-mpi.org/trac/ompi/ticket/2911

--- examples/hello_c.c
+++ examples/hello_c.c
@@ -18,7 +18,6 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     printf("Hello, world, I am %d of %d\n", rank, size);
-    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
 
     return 0;
