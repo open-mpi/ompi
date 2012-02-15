3da1787c062ff366d16267b888bd5a518d788410
Ralph Castain <rhc@open-mpi.org>
2012-02-15 04:16:05 +0000

    Allow there to be no default hostfile without generating an error
    This commit was SVN r25930.

--- orte/util/hostfile/hostfile.c
+++ orte/util/hostfile/hostfile.c
@@ -419,8 +419,25 @@ static int hostfile_parse(const char *hostfile, opal_list_t* updates, opal_list_
     orte_util_hostfile_done = false;
     orte_util_hostfile_in = fopen(hostfile, "r");
     if (NULL == orte_util_hostfile_in) {
-        orte_show_help("help-hostfile.txt", "no-hostfile", true, hostfile);
-        rc = ORTE_ERR_NOT_FOUND;
+        if (NULL == orte_default_hostfile ||
+            0 != strcmp(orte_default_hostfile, hostfile)) {
+            /* not the default hostfile, so not finding it
+             * is an error
+             */
+            orte_show_help("help-hostfile.txt", "no-hostfile", true, hostfile);
+            rc = ORTE_ERR_SILENT;
+            goto unlock;
+        }
+        /* if this is the default hostfile and it was given,
+         * then it's an error
+         */
+        if (orte_default_hostfile_given) {
+            orte_show_help("help-hostfile.txt", "no-hostfile", true, hostfile);
+            rc = ORTE_ERR_NOT_FOUND;
+            goto unlock;
+        }
+        /* otherwise, not finding it is okay */
+        rc = ORTE_SUCCESS;
         goto unlock;
     }
 
