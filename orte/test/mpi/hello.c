6c2c04b09d1be63a97091ed993d908274e3dbada
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2012-02-17 16:48:01 +0000

    Add binding output to test

--- orte/test/mpi/hello.c
+++ orte/test/mpi/hello.c
@@ -8,15 +8,23 @@
 #include <stdio.h>
 #include "mpi.h"
 
+#include "opal/mca/hwloc/hwloc.h"
+
 int main(int argc, char* argv[])
 {
     int rank, size;
+    hwloc_cpuset_t cpus;
+    char *bindings;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    printf("Hello, World, I am %d of %d\n", rank, size);
+    cpus = hwloc_bitmap_alloc();
+    hwloc_get_cpubind(opal_hwloc_topology, cpus, HWLOC_CPUBIND_PROCESS);
+    hwloc_bitmap_list_asprintf(&bindings, cpus);
+
+    printf("Hello, World, I am %d of %d: bitmap %s\n", rank, size, bindings);
 
     MPI_Finalize();
     return 0;
