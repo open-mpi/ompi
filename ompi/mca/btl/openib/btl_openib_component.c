8f691880bbdf40e4d622b4abac5e8a4ecc9749d7
George Bosilca <bosilca@icl.utk.edu>
2012-03-13 12:47:53 +0000

    Fixes trac:3049: mpool ignores btl memreg error code and continues execution followed by hang
    Temporary fix until the shared memory pool make its way in.
    
    ---svn-pre-commit-ignore-below---
    r26106
    print error which is ignored on upper layer
    
    r26111
    OPENIB BTL - use orte_show_help instead of BTL_ERROR print in case ibv_reg_mr failed.
    
    This commit was SVN r26131.
    
    The following SVN revision numbers were found above:
      r26106 --> open-mpi/ompi@4e7e7d7c3f4a982f2277b0d85e11e33a0c0f09b0
      r26111 --> open-mpi/ompi@102da281c4982dbb03441051f0111af3555ba413
    
    The following Trac tickets were found above:
      Ticket 3049 --> https://svn.open-mpi.org/trac/ompi/ticket/3049

--- ompi/mca/btl/openib/btl_openib_component.c
+++ ompi/mca/btl/openib/btl_openib_component.c
@@ -557,6 +557,10 @@ static int openib_reg_mr(void *reg_data, void *base, size_t size,
     openib_reg->mr = ibv_reg_mr(device->ib_pd, base, size, access_flag);
  
     if (NULL == openib_reg->mr) {
+        orte_show_help("help-mpi-btl-openib.txt", "mem-reg-fail",
+                        orte_process_info.nodename,
+                        ibv_get_device_name(device->ib_dev),
+                        __func__, strerror(errno));
         return OMPI_ERR_OUT_OF_RESOURCE;
     }
 
