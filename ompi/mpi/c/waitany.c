189dd52784e8f35d58a6baccf190ea4226f3695d
Eugene Loh <eugene@open-mpi-git-mirror.example.com>
2012-03-16 04:59:58 +0000

    The copyright notices that should have been in r26156.

--- ompi/mpi/c/waitany.c
+++ ompi/mpi/c/waitany.c
@@ -10,6 +10,7 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012      Oracle and/or its affiliates.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
