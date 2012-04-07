c8b1a7a778fb456cd71c6c97413367feef7f12c8
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2012-04-07 01:46:09 +0000

    Fix test

--- test/class/opal_bitmap.c
+++ test/class/opal_bitmap.c
@@ -8,7 +8,6 @@
 #include "support.h"
 
 #include "opal/class/opal_bitmap.h"
-#include "opal/util/opal_sos.h"
 #include "opal/constants.h"
 
 #define BSIZE 26
@@ -63,12 +62,12 @@ int main(int argc, char *argv[])
 
     PRINT_VALID_ERR;
     err = opal_bitmap_init(NULL, 2);
-    if (OPAL_SOS_GET_ERROR_CODE(err) == OPAL_ERR_BAD_PARAM)
+    if (err == OPAL_ERR_BAD_PARAM)
 	fprintf(error_out, "ERROR: Initialization of bitmap failed\n\n");
 
     PRINT_VALID_ERR;
     err = opal_bitmap_init(&bm, -1);
-    if (OPAL_SOS_GET_ERROR_CODE(err) == OPAL_ERR_BAD_PARAM)
+    if (err == OPAL_ERR_BAD_PARAM)
 	fprintf(error_out, "ERROR: Initialization of bitmap failed \n\n");
 
     err = opal_bitmap_init(&bm, BSIZE);
