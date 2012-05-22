d346eefaf5b80af29f9e4cbdfe3a0b58dcb849fd
Nathan Hjelm <hjelmn@open-mpi-git-mirror.example.com>
2012-05-22 15:09:18 +0000

    bug fix: actually close ess components

--- orte/mca/ess/base/ess_base_close.c
+++ orte/mca/ess/base/ess_base_close.c
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2004-2008 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
@@ -10,6 +11,8 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2009      Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012      Los Alamos National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -25,7 +28,6 @@
 #include "opal/mca/mca.h"
 #include "opal/util/output.h"
 #include "opal/mca/base/base.h"
-#include "opal/mca/base/mca_base_component_repository.h"
 
 #include "orte/mca/ess/base/base.h"
 
@@ -34,23 +36,12 @@ extern opal_list_t orte_ess_base_components_available;
 int
 orte_ess_base_close(void)
 {
-    opal_list_item_t *item;
-    mca_base_component_list_item_t *cli;
-
-    /* unload all remaining components */
-    while (NULL != (item = opal_list_remove_first(&orte_ess_base_components_available))) {
-        orte_ess_base_component_t* component;
-        cli = (mca_base_component_list_item_t *) item;
-        component = (orte_ess_base_component_t *) cli->cli_component;
-        opal_output_verbose(10, 0,
-                            "orte_ess_base_close: module %s unloaded",
-                            component->base_version.mca_component_name);
-        mca_base_component_repository_release((mca_base_component_t *) component);
-        OBJ_RELEASE(item);
-    }
-    
+    /* Close all open components */
+    mca_base_components_close (orte_ess_base_output,
+			       &orte_ess_base_components_available,
+			       NULL);
     OBJ_DESTRUCT(&orte_ess_base_components_available);
-    opal_output_close(orte_ess_base_output);
+
     return ORTE_SUCCESS;
 }
 
