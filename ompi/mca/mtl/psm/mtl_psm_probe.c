2effbb1ba6f1200fd9724a1d84c6549f86bf0e70
Brian Barrett <brbarret@open-mpi.org>
2012-05-24 16:06:20 +0000

    fix copy/paste typo
    This commit was SVN r26492.

--- ompi/mca/mtl/psm/mtl_psm_probe.c
+++ ompi/mca/mtl/psm/mtl_psm_probe.c
@@ -71,7 +71,7 @@ int ompi_mtl_psm_iprobe(struct mca_mtl_base_module_t* mtl,
 
 
 int
-ompi_mtl_mxm_improbe(struct mca_mtl_base_module_t *mtl,
+ompi_mtl_psm_improbe(struct mca_mtl_base_module_t *mtl,
                      struct ompi_communicator_t *comm,
                      int src,
                      int tag,
