30455c4fb1bff5781996463154ba89d2e84fcd2c
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2012-07-28 15:33:09 +0000

    Add an intermediate state when the VM is ready so that third party tools can take action prior to mapping/launching apps

--- orte/util/error_strings.c
+++ orte/util/error_strings.c
@@ -217,6 +217,8 @@ const char *orte_job_state_to_str(orte_job_state_t state)
         return "DAEMONS LAUNCHED";
     case ORTE_JOB_STATE_DAEMONS_REPORTED:
         return "ALL DAEMONS REPORTED";
+    case ORTE_JOB_STATE_VM_READY:
+        return "VM READY";
     case ORTE_JOB_STATE_LAUNCH_APPS:
         return "PENDING APP LAUNCH";
     case ORTE_JOB_STATE_RUNNING:
