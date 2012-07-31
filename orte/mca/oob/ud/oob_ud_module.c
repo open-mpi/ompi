14c0ca928b33c81a15ab8eeaa23f81de5a7a2d3e
Nathan Hjelm <hjelmn@open-mpi-git-mirror.example.com>
2012-07-31 19:00:18 +0000

    oob/ud: don't start listening for ud requests unless we have one usable port

--- orte/mca/oob/ud/oob_ud_module.c
+++ orte/mca/oob/ud/oob_ud_module.c
@@ -349,21 +349,22 @@ int mca_oob_ud_module_init (void)
             rc = mca_oob_ud_port_recv_start (port);
             if (ORTE_SUCCESS != rc) {
                 mca_oob_ud_listen_destroy (port);
+                continue;
             }
 
             /* NTH: only supports one port for now */
+            found_one = true;
             break;
         }
-
-        mca_oob_ud_event_start_monitor (device);
-        found_one = true;
     }
 
-    if (found_one) {
-        return ORTE_SUCCESS;
-    } else {
+    if (!found_one) {
         return ORTE_ERR_NOT_FOUND;
     }
+
+    mca_oob_ud_event_start_monitor (device);
+
+    return ORTE_SUCCESS;
 }
 
 static void mca_oob_ud_cancel_all_in_list (opal_list_t *list)
