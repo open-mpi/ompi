8b595ddf8498439a042ac60518f00ca9daaf2643
Ralph Castain <rhc@open-mpi.org>
2012-08-02 16:31:02 +0000

    Complete changes specified in MPI-3 Ticket #125. Still holding on proposed change to MPI_Init as it remains under discussion (and is just plain uuuugly). No action taken on changing string arrays as that part of #125 was not included in the final pdf text.
    This commit was SVN r26939.

--- ompi/mpi/c/group_excl.c
+++ ompi/mpi/c/group_excl.c
@@ -11,6 +11,7 @@
  *                         All rights reserved.
  * Copyright (c) 2006      University of Houston. All rights reserved.
  * Copyright (c) 2006-2009 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012      Los Alamos Nat Security, LLC. All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -37,7 +38,7 @@
 static const char FUNC_NAME[] = "MPI_Group_excl";
 
 
-int MPI_Group_excl(MPI_Group group, int n, int *ranks,
+int MPI_Group_excl(MPI_Group group, int n, int ranks[],
                    MPI_Group *new_group) 
 {
     ompi_group_t *group_pointer = (ompi_group_t *)group;
