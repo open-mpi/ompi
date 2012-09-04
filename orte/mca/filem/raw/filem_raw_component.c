4d9db72e664d5a17cbc8fbf0c9005e62dec81303
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2012-09-04 17:52:12 +0000

    Ensure we don't re-link files when staging execution as we may be executing more members of the same app. Allow the user to ask that directory trees be "flattened" so that all files appear in the proc's session directory itself.

--- orte/mca/filem/raw/filem_raw_component.c
+++ orte/mca/filem/raw/filem_raw_component.c
@@ -31,6 +31,8 @@ static int filem_raw_open(void);
 static int filem_raw_close(void);
 static int filem_raw_query(mca_base_module_t **module, int *priority);
 
+bool orte_filem_raw_flatten_trees=false;
+
 orte_filem_base_component_t mca_filem_raw_component = {
     {
         ORTE_FILEM_BASE_VERSION_2_0_0,
@@ -53,6 +55,14 @@ orte_filem_base_component_t mca_filem_raw_component = {
 
 static int filem_raw_open(void) 
 {
+    int tmp;
+    mca_base_component_t *c = &mca_filem_raw_component.base_version;
+
+    mca_base_param_reg_int(c, "flatten_directory_trees",
+                           "Put all files in the working directory instead of creating their respective directory trees",
+                           false, false, (int)false, &tmp);
+    orte_filem_raw_flatten_trees = OPAL_INT_TO_BOOL(tmp);
+
     return ORTE_SUCCESS;
 }
 
@@ -74,6 +84,8 @@ static int filem_raw_query(mca_base_module_t **module, int *priority)
 #if ORTE_WANT_HADOOP_SUPPORT
     /* always use us if Hadoop is enabled */
     *priority = 1000;
+    /* always flatten directory trees for Hadoop */
+    orte_filem_raw_flatten_trees = true;
 #endif
 
     *module = (mca_base_module_t*) &mca_filem_raw_module;
