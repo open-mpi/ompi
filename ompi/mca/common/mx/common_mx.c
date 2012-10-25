a65ce495858a291713f6ea2aad2e4b84f147e671
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2012-10-25 21:50:07 +0000

    Rather than use the fake mpool for handling callbacks into the MX library, use the memory hooks interface (which does allow for multiple callbacks to be registered) directly.

--- ompi/mca/common/mx/common_mx.c
+++ ompi/mca/common/mx/common_mx.c
@@ -29,23 +29,26 @@
 #include "opal/util/output.h"
 #include "opal/mca/base/mca_base_param.h"
 #include "ompi/runtime/params.h"
-#include "ompi/mca/mpool/mpool.h"
-#include "ompi/mca/mpool/base/base.h"
-#include "ompi/mca/mpool/fake/mpool_fake.h"
 
 
 int mx__regcache_clean(void *ptr, size_t size);
 
 static int ompi_common_mx_initialize_ref_cnt = 0;
 static int ompi_common_mx_available = 0;
-static mca_mpool_base_module_t *ompi_common_mx_fake_mpool = 0;
+static int ompi_common_mx_memory_cb_registered = 0;
+
+static void
+memory_release_cb(void *buf, size_t length, void *cbdata, bool from_alloc)
+{
+  mx__regcache_clean(buf, length);
+}
+
 
 int
 ompi_common_mx_initialize(void)
 {
     mx_return_t mx_return;
-    struct mca_mpool_base_resources_t mpool_resources;
-    int index, value, ret = OMPI_SUCCESS;
+    int ret = OMPI_SUCCESS;
     
     ompi_common_mx_initialize_ref_cnt++;
     
@@ -57,31 +60,14 @@ ompi_common_mx_initialize(void)
          */
         mx_set_error_handler(MX_ERRORS_RETURN);
 	
-	/* If we have a memory manager available, and
-	   mpi_leave_pinned == -1, then set mpi_leave_pinned to 1.
-
-	   We have a memory manager if:
-	   - we have both FREE and MUNMAP support
-	   - we have MUNMAP support and the linux mallopt */
-	value = opal_mem_hooks_support_level();
-	if ((value & (OPAL_MEMORY_FREE_SUPPORT | OPAL_MEMORY_MUNMAP_SUPPORT))
-            == (OPAL_MEMORY_FREE_SUPPORT | OPAL_MEMORY_MUNMAP_SUPPORT)) {
-	  index = mca_base_param_find("mpi", NULL, "leave_pinned");
-	  if (index >= 0)
-            if ((mca_base_param_lookup_int(index, &value) == OPAL_SUCCESS) 
-		&& (value == -1)) {
-	      
-	      ompi_mpi_leave_pinned = 1;
-	      setenv("MX_RCACHE", "2", 1);
-	      ompi_common_mx_fake_mpool = 
-		mca_mpool_base_module_create("fake", NULL, &mpool_resources);
-	      if (!ompi_common_mx_fake_mpool) {
-		ompi_mpi_leave_pinned = 0;
-		setenv("MX_RCACHE", "0", 1);
-		opal_output(0, "Error creating fake mpool (error %s)\n",
-			    strerror(errno));
-	      }
-	    }
+	/* If we have a memory manager available, then tell MX to use
+	   the rcache */
+	if (0 != (OPAL_MEMORY_FREE_SUPPORT & opal_mem_hooks_support_level())) {
+	  if (OMPI_SUCCESS == 
+	      opal_mem_hooks_register_release(memory_release_cb, NULL)) {
+	    ompi_common_mx_memory_cb_registered = 1;
+	    setenv("MX_RCACHE", "2", 1);
+	  }
 	}
 	
         /* initialize the mx library */
@@ -89,10 +75,10 @@ ompi_common_mx_initialize(void)
         
         if(MX_SUCCESS != mx_return) {
             ompi_common_mx_available = -1;
-            if (ompi_common_mx_fake_mpool) {
-                mca_mpool_base_module_destroy(ompi_common_mx_fake_mpool);
-	        ompi_common_mx_fake_mpool = NULL;
-            }
+	    if (0 != ompi_common_mx_memory_cb_registered) {
+	      opal_mem_hooks_unregister_release(memory_release_cb);
+	      ompi_common_mx_memory_cb_registered = 0;
+	    }
             opal_output(0,
                         "Error in mx_init (error %s)\n",
                         mx_strerror(mx_return));
@@ -101,7 +87,6 @@ ompi_common_mx_initialize(void)
             return OMPI_ERR_NOT_AVAILABLE;
         }
         ompi_common_mx_available = 1;
-        mpool_resources.regcache_clean = mx__regcache_clean;
     } else if (ompi_common_mx_available < 0) {
         ret = OMPI_ERR_NOT_AVAILABLE;
     }
@@ -116,10 +101,10 @@ ompi_common_mx_finalize(void)
     if( 0 == ompi_common_mx_initialize_ref_cnt ) { 
         mx_return_t mx_return;
 
-        if (ompi_common_mx_fake_mpool) {
-	  mca_mpool_base_module_destroy(ompi_common_mx_fake_mpool);
-	  ompi_common_mx_fake_mpool = NULL;
-        }
+	if (0 != ompi_common_mx_memory_cb_registered) {
+	  opal_mem_hooks_unregister_release(memory_release_cb);
+	  ompi_common_mx_memory_cb_registered = 0;
+	}
         
         mx_return = mx_finalize(); 
         if(mx_return != MX_SUCCESS){ 
