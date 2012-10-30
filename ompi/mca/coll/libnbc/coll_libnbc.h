898e38c29937cb3c2f04732edc71e88ee5b20139
Ralph Castain <rhc@open-mpi.org>
2012-10-30 17:45:23 +0000

    Fixes trac:3366: Move libnbc performance / correctness fixes to v.17
    ---svn-pre-commit-ignore-below---
    
    r27498 [[BR]]
    * Lock around tag management, so that it's thread safe
    * Only register the progress function on first call to a non-blocking
      collective operation, to try to reduce overall performance impact
    * Fix tag management in roll-over case
    
    r27514 [[BR]]
    Fix warnings in nbc
    
    This commit was SVN r27520.
    
    The following SVN revision numbers were found above:
      r27498 --> open-mpi/ompi@8b40c0de9befc80812ffd9d4e69ed9204e491a98
      r27514 --> open-mpi/ompi@c0f17756200fbc4b0aac947003bfb53134ee0309
    
    The following Trac tickets were found above:
      Ticket 3366 --> https://svn.open-mpi.org/trac/ompi/ticket/3366

--- ompi/mca/coll/libnbc/coll_libnbc.h
+++ ompi/mca/coll/libnbc/coll_libnbc.h
@@ -62,7 +62,7 @@ struct ompi_coll_libnbc_component_t {
     mca_coll_base_component_2_0_0_t super;
     ompi_free_list_t requests;
     opal_list_t active_requests;
-    uint32_t active_comms;
+    int32_t active_comms;
     opal_atomic_lock_t progress_lock;
 };
 typedef struct ompi_coll_libnbc_component_t ompi_coll_libnbc_component_t;
@@ -72,6 +72,8 @@ OMPI_MODULE_DECLSPEC extern ompi_coll_libnbc_component_t mca_coll_libnbc_compone
 
 struct ompi_coll_libnbc_module_t {
     mca_coll_base_module_t super;
+    opal_mutex_t mutex;
+    bool comm_registered;
     int tag;
 #ifdef NBC_CACHE_SCHEDULE
   void *NBC_Dict[NBC_NUM_COLL]; /* this should point to a struct
@@ -128,6 +130,8 @@ typedef ompi_coll_libnbc_request_t NBC_Handle;
         OMPI_FREE_LIST_RETURN(&mca_coll_libnbc_component.requests,      \
                               (ompi_free_list_item_t*) req);            \
     } while (0)
+
+int ompi_coll_libnbc_progress(void);
 
 int NBC_Init_comm(MPI_Comm comm, ompi_coll_libnbc_module_t *module);
 int NBC_Progress(NBC_Handle *handle);
