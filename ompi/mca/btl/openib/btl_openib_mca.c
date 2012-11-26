c48a358c36a8eabf0fdbc6618d4f6ffcd5252e03
George Bosilca <bosilca@icl.utk.edu>
2012-11-26 21:34:38 +0000

    Fixes trac:3386: Please integrate IB performance fix into v1.6
    ---svn-pre-commit-ignore-below---
    
    r27559 [[BR]]
    performance optimization: process completions in the batch manner
    
    This commit was SVN r27638.
    
    The following SVN revision numbers were found above:
      r27559 --> open-mpi/ompi@d47d550dfc3ea09ae79d7628c460c7a6867a0205
    
    The following Trac tickets were found above:
      Ticket 3386 --> https://svn.open-mpi.org/trac/ompi/ticket/3386

--- ompi/mca/btl/openib/btl_openib_mca.c
+++ ompi/mca/btl/openib/btl_openib_mca.c
@@ -174,6 +174,12 @@ int btl_openib_register_mca_params(void)
                   0, &ival, 0));
     mca_btl_openib_component.abort_not_enough_reg_mem = (0 != ival);
 
+    CHECK(reg_int("poll_cq_batch", NULL,
+                  "Retrieve up to poll_cq_batch completions from CQ",
+                  MCA_BTL_OPENIB_CQ_POLL_BATCH_DEFAULT, &ival, REGINT_GE_ONE));
+
+    mca_btl_openib_component.cq_poll_batch = (ival > MCA_BTL_OPENIB_CQ_POLL_BATCH_DEFAULT)? MCA_BTL_OPENIB_CQ_POLL_BATCH_DEFAULT : ival;
+
     if (OMPI_HAVE_IBV_FORK_INIT) {
         ival2 = -1;
     } else {
