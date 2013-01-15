4c66b1babdac9b7215f640a774a809e3e5fb6095
Brian Barrett <brbarret@open-mpi-git-mirror.example.com>
2013-01-15 01:27:36 +0000

    Remove the (only two) fortran constants from OPAL.  The only places that actually care if opal_pointer_array is limited to handle_max already passes that in as the max_size during init, so don't need it there.  The arch constant was a bit more difficult, so pass that in during MPI init and leave empty otherwise.
    This is to help with the effort to allow building ompi against an external
    opal or orte.

--- opal/util/arch.h
+++ opal/util/arch.h
@@ -231,61 +231,16 @@
 
 BEGIN_C_DECLS
 
-OPAL_DECLSPEC int32_t opal_arch_compute_local_id( uint32_t *var);
+/* Local Architecture */
+OPAL_DECLSPEC extern uint32_t opal_local_arch;
+
+/* Initialize architecture and determine all but fortran logical fields */
+OPAL_DECLSPEC int opal_arch_init(void);
 
 OPAL_DECLSPEC int32_t opal_arch_checkmask ( uint32_t *var, uint32_t mask );
 
-static inline int32_t opal_arch_isbigendian ( void )
-{
-    const uint32_t value = 0x12345678;
-    const char *ptr = (char*)&value;
-    int x = 0;
-
-    /* if( sizeof(int) == 8 ) x = 4; */
-    if( ptr[x] == 0x12)  return 1; /* big endian, true */
-    if( ptr[x] == 0x78 ) return 0; /* little endian, false */
-    assert( 0 );  /* unknown architecture not little nor big endian */
-    return -1;
-}
-
-/* we must find which representation of long double is used
- * intel or sparc. Both of them represent the long doubles using a close to
- * IEEE representation (seeeeeee..emmm...m) where the mantissa look like
- * 1.????. For the intel representaion the 1 is explicit, and for the sparc
- * the first one is implicit. If we take the number 2.0 the exponent is 1
- * and the mantissa is 1.0 (the sign of course should be 0). So if we check
- * for the first one in the binary representation of the number, we will
- * find the bit from the exponent, so the next one should be the begining
- * of the mantissa. If it's 1 then we have an intel representaion, if not
- * we have a sparc one. QED
- */
-static inline int32_t opal_arch_ldisintel( void )
-{
-    long double ld = 2.0;
-    int i, j;
-    uint32_t* pui = (uint32_t*)(void*)&ld;
-
-    j = LDBL_MANT_DIG / 32;
-    i = (LDBL_MANT_DIG % 32) - 1;
-    if( opal_arch_isbigendian() ) { /* big endian */
-        j = (sizeof(long double) / sizeof(unsigned int)) - j;
-        if( i < 0 ) {
-            i = 31;
-            j = j+1;
-        }
-    } else {
-        if( i < 0 ) {
-            i = 31;
-            j = j-1;
-        }
-    }
-    return (pui[j] & (1 << i) ? 1 : 0);
-}
-
-static inline void opal_arch_setmask ( uint32_t *var, uint32_t mask)
-{
-    *var |= mask;
-}
+/* Set fortran logical fields after init, to keep fortran out of opal... */
+OPAL_DECLSPEC int opal_arch_set_fortran_logical_size(uint32_t size);
 
 END_C_DECLS
 
