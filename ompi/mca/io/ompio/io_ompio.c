626cb63acf0abc8fe6fe98b5ae23ace1e84c55e0
Ralph Castain <rhc@open-mpi.org>
2013-01-15 22:36:50 +0000

    Turn off debug
    This commit was SVN r27832.

--- ompi/mca/io/ompio/io_ompio.c
+++ ompi/mca/io/ompio/io_ompio.c
@@ -279,7 +279,7 @@ int ompi_io_ompio_decode_datatype (mca_io_ompio_file_t *fh,
 				   temp_iov,
                                    &temp_count, 
                                    &temp_data)) {
-/* #if 0 */
+#if 0
         printf ("%d: New raw extraction (iovec_count = %d, max_data = %d)\n",
                 fh->f_rank,temp_count, temp_data);
         for (i = 0; i < temp_count; i++) {
@@ -287,7 +287,7 @@ int ompi_io_ompio_decode_datatype (mca_io_ompio_file_t *fh,
 		    temp_iov[i].iov_base,
 		    temp_iov[i].iov_len);
         }
-/* #endif */
+#endif
 
         *iovec_count = *iovec_count + temp_count;
         *max_data = *max_data + temp_data;
