784bc06eeaead82068281cb7141386aa965dfb46
George Bosilca <bosilca@icl.utk.edu>
2013-01-17 11:30:27 +0000

    Fixes trac:3452: Fix to CQ TX completion moderation
    ---svn-pre-commit-ignore-below---
    
    r27767 [[BR]]
    request signal when sending over SRQ and number of SRQ sd_credits is 0
    
    This commit was SVN r27849.
    
    The following SVN revision numbers were found above:
      r27767 --> open-mpi/ompi@fe672f255f19188cb94660f3289db332ccdf7fbf
    
    The following Trac tickets were found above:
      Ticket 3452 --> https://svn.open-mpi.org/trac/ompi/ticket/3452

--- ompi/mca/btl/openib/btl_openib_endpoint.h
+++ ompi/mca/btl/openib/btl_openib_endpoint.h
@@ -303,7 +303,8 @@ static inline int qp_need_signal(mca_btl_openib_endpoint_t *ep, const int qp, si
 
     /* note that size here is payload only */
     if (ep->qps[qp].qp->sd_wqe <= 0  || 
-            size + sizeof(mca_btl_openib_header_t) + (rdma ? sizeof(mca_btl_openib_footer_t) : 0) > ep->qps[qp].ib_inline_max) {
+            size + sizeof(mca_btl_openib_header_t) + (rdma ? sizeof(mca_btl_openib_footer_t) : 0) > ep->qps[qp].ib_inline_max ||
+             !BTL_OPENIB_QP_TYPE_PP(qp) && ep->endpoint_btl->qps[qp].u.srq_qp.sd_credits <= 0) {
         ep->qps[qp].qp->wqe_count = QP_TX_BATCH_COUNT;
         return 1;
     }
