5fb7e70b17540bfcf60c98b957e73f88eee7a9af
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2013-01-19 14:27:54 +0000

    Make CMA compile and run.

--- opal/include/opal/sys/cma.h
+++ opal/include/opal/sys/cma.h
@@ -50,22 +50,24 @@
 #endif
 
 
-inline ssize_t process_vm_readv(pid_t pid, 
-                                const struct iovec  *lvec, 
-                                unsigned long liovcnt,
-                                const struct iovec *rvec,
-                                unsigned long riovcnt,
-                                unsigned long flags)
+static inline ssize_t
+process_vm_readv(pid_t pid, 
+                 const struct iovec  *lvec, 
+                 unsigned long liovcnt,
+                 const struct iovec *rvec,
+                 unsigned long riovcnt,
+                 unsigned long flags)
 {
   return syscall(__NR_process_vm_readv, pid, lvec, liovcnt, rvec, riovcnt, flags);
 }
 
-inline ssize_t process_vm_writev(pid_t pid, 
-                          const struct iovec  *lvec, 
-                          unsigned long liovcnt,
-                          const struct iovec *rvec,
-                          unsigned long riovcnt,
-                          unsigned long flags)
+static inline ssize_t
+process_vm_writev(pid_t pid, 
+                  const struct iovec  *lvec, 
+                  unsigned long liovcnt,
+                  const struct iovec *rvec,
+                  unsigned long riovcnt,
+                  unsigned long flags)
 {
   return syscall(__NR_process_vm_writev, pid, lvec, liovcnt, rvec, riovcnt, flags);
 }
