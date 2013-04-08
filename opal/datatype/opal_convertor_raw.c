c5909bffe8f55c17bab0987ce06c386ef8236ad2
George Bosilca <bosilca@icl.utk.edu>
2013-04-08 13:49:14 +0000

    Make the opal_convertor_raw similar to opal_convertor_pack and _unpack, by allowing it to handle completed convertors. In this case it will return a length of zero and an iov_count set to zero.
    This commit was SVN r28305.

--- opal/datatype/opal_convertor_raw.c
+++ opal/datatype/opal_convertor_raw.c
@@ -47,6 +47,13 @@ opal_convertor_raw( opal_convertor_t* pConvertor,
     uint32_t index = 0, i;    /* the iov index and a simple counter */
 
     assert( (*iov_count) > 0 );
+    if( OPAL_LIKELY(pConvertor->flags & CONVERTOR_COMPLETED) ) {
+        iov[0].iov_base = NULL;
+        iov[0].iov_len  = 0;
+        *iov_count      = 0;
+        *length         = iov[0].iov_len;
+        return 1;  /* We're still done */
+    }
     if( OPAL_LIKELY(pConvertor->flags & CONVERTOR_NO_OP) ) {
         /* The convertor contain minimal informations, we only use the bConverted
          * to manage the conversion. This function work even after the convertor
