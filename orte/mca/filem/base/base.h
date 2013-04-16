1a54e52da46676d33dd98f7f2a443ff5fae18dff
Ralph Castain <rhc@open-mpi.org>
2013-04-16 20:39:48 +0000

    Per conversation with Josh H., remove the stale rsh component from the filem framework. Let the "raw" component do the job.
    This commit was SVN r28338.

--- orte/mca/filem/base/base.h
+++ orte/mca/filem/base/base.h
@@ -46,19 +46,6 @@ typedef uint8_t orte_filem_cmd_flag_t;
 #define ORTE_FILEM_GET_REMOTE_PATH_CMD     2
 
 /**
- * FileM request object maintenance functions
- */
-ORTE_DECLSPEC void orte_filem_base_process_set_construct(orte_filem_base_process_set_t *obj);
-ORTE_DECLSPEC void orte_filem_base_process_set_destruct( orte_filem_base_process_set_t *obj);
-
-ORTE_DECLSPEC void orte_filem_base_file_set_construct(orte_filem_base_file_set_t *obj);
-ORTE_DECLSPEC void orte_filem_base_file_set_destruct( orte_filem_base_file_set_t *obj);
-
-ORTE_DECLSPEC void orte_filem_base_construct(orte_filem_base_request_t *obj);
-ORTE_DECLSPEC void orte_filem_base_destruct( orte_filem_base_request_t *obj);
-
-
-/**
  * Globals
  */
 ORTE_DECLSPEC extern orte_filem_base_module_t orte_filem;
@@ -69,10 +56,6 @@ ORTE_DECLSPEC extern bool orte_filem_base_is_active;
  * These are to be used when no component is selected.
  * They just return success, and empty strings as necessary.
  */
-ORTE_DECLSPEC int orte_filem_base_none_open(void);
-ORTE_DECLSPEC int orte_filem_base_none_close(void);
-ORTE_DECLSPEC int orte_filem_base_none_query(mca_base_module_t **module, int *priority);
-
 int orte_filem_base_module_init(void);
 int orte_filem_base_module_finalize(void);
 
@@ -100,17 +83,6 @@ ORTE_DECLSPEC void orte_filem_base_recv(int status, orte_process_name_t* sender,
                                         opal_buffer_t* buffer, orte_rml_tag_t tag,
                                         void* cbdata);
 
-
-/**
- * Get Node Name for an ORTE process
- */
-ORTE_DECLSPEC int orte_filem_base_get_proc_node_name(orte_process_name_t *proc, char **machine_name);
-ORTE_DECLSPEC int orte_filem_base_get_remote_path(char **remote_ref, orte_process_name_t *peer, int *flag);
-
-/**
- * Setup request structure
- */
-ORTE_DECLSPEC int orte_filem_base_prepare_request(orte_filem_base_request_t *request, int move_type);
 
 END_C_DECLS
 
