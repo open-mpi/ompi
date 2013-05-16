f24e308ce9a5e4d52dc4be17b2f0f14b376fafb9
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2013-05-16 15:15:02 +0000

    don't need the return value, but should check for error

--- orte/mca/rmaps/resilient/rmaps_resilient.c
+++ orte/mca/rmaps/resilient/rmaps_resilient.c
@@ -688,7 +688,6 @@ static int map_to_ftgrps(orte_job_t *jdata)
     orte_node_t *node, *nd=NULL;
     orte_rmaps_res_ftgrp_t *ftgrp, *target = NULL;
     orte_vpid_t totprocs, num_assigned;
-    orte_proc_t *proc;
     bool initial_map=true;
 
     OPAL_OUTPUT_VERBOSE((1, orte_rmaps_base_framework.framework_output,
@@ -827,7 +826,10 @@ static int map_to_ftgrps(orte_job_t *jdata)
                 opal_pointer_array_add(map->nodes, nd);
                 nd->mapped = true;
             }
-            proc = orte_rmaps_base_setup_proc(jdata, nd, app->idx);
+            if (NULL == orte_rmaps_base_setup_proc(jdata, nd, app->idx)) {
+                ORTE_ERROR_LOG(ORTE_ERROR);
+                return ORTE_ERROR;
+            }
             if ((nd->slots < (int)nd->num_procs) ||
                 (0 < nd->slots_max && nd->slots_max < (int)nd->num_procs)) {
                 if (ORTE_MAPPING_NO_OVERSUBSCRIBE & ORTE_GET_MAPPING_DIRECTIVE(jdata->map->mapping)) {
