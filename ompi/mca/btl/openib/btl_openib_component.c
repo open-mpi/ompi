9556310bd021db5bd5d67711c7f3e06911347e2d
Mike Dubman <miked@mellanox.com>
2013-06-12 05:58:32 +0000

    cosmetic: add comment with rationale for malloc.h include
    This commit was SVN r28614.

--- ompi/mca/btl/openib/btl_openib_component.c
+++ ompi/mca/btl/openib/btl_openib_component.c
@@ -38,6 +38,13 @@
 #include <stdlib.h>
 #include <stddef.h>
 #if BTL_OPENIB_MALLOC_HOOKS_ENABLED
+/*
+ * The include of malloc.h below breaks layers in OMPI
+ * The malloc hooks in newer glibc were deprecated, including stock malloc.h causes compilation erros
+ * So, we use internal malloc.h which does not cause compilation errors.
+ * Internally, OMPI uses built-in ptmalloc anyway
+ *
+ */
 #include "opal/mca/memory/linux/malloc.h"
 #endif
 
