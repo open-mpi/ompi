30b910b54d97bb2769567cb1c464f2bfd7b2338f
George Bosilca <bosilca@icl.utk.edu>
2013-08-06 09:08:43 +0000

    More info in the debug mode.
    This commit was SVN r29002.

--- opal/datatype/opal_datatype_pack.c
+++ opal/datatype/opal_datatype_pack.c
@@ -268,7 +268,8 @@ opal_generic_simple_pack_function( opal_convertor_t* pConvertor,
     size_t iov_len_local;
     uint32_t iov_count;
 
-    DO_DEBUG( opal_output( 0, "opal_convertor_generic_simple_pack( %p, {%p, %lu}, %d )\n", (void*)pConvertor,
+    DO_DEBUG( opal_output( 0, "opal_convertor_generic_simple_pack( %p:%p, {%p, %lu}, %d )\n",
+                           (void*)pConvertor, (void*)pConvertor->pBaseBuf,
                            iov[0].iov_base, (unsigned long)iov[0].iov_len, *out_size ); );
 
     description = pConvertor->use_desc->desc;
