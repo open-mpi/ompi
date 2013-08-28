268eb54b475727b6d775d49bbcfe308cab43e1ca
Ralph Castain <rhc@open-mpi.org>
2013-08-28 21:37:31 +0000

    Add remaining changeset (r29080) for PMI update
    This commit was SVN r29083.
    
    The following SVN revision numbers were found above:
      r29080 --> open-mpi/ompi@537e7380b11e28f35fa0292786cc16ea118ce62d

--- ompi/communicator/comm_cid.c
+++ ompi/communicator/comm_cid.c
@@ -120,6 +120,7 @@ static int ompi_comm_world_thread_level_mult=0;
 
 int ompi_comm_cid_init (void)
 {
+#if OMPI_ENABLE_THREAD_MULTIPLE
     ompi_proc_t **procs, *thisproc;
     uint8_t thread_level;
     void *tlpointer;
@@ -153,7 +154,9 @@ int ompi_comm_cid_init (void)
         }
     }
     free(procs);
-
+#else
+    ompi_comm_world_thread_level_mult = 0; // silence compiler warning if not used
+#endif
     return OMPI_SUCCESS;
 }
 
