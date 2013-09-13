433422f412dcdf6d865fc251510a230f5beb957d
Rolf Vandevaart <rolfv@open-mpi-git-mirror.example.com>
2013-09-13 14:28:26 +0000

    For debug builds, add the ability to view memory registrations and deregistrations in the openib BTL.

--- ompi/mca/btl/openib/btl_openib_component.c
+++ ompi/mca/btl/openib/btl_openib_component.c
@@ -598,6 +598,10 @@ static int openib_reg_mr(void *reg_data, void *base, size_t size,
         return OMPI_ERR_OUT_OF_RESOURCE;
     }
 
+    OPAL_OUTPUT_VERBOSE((30, mca_btl_openib_component.memory_registration_verbose,
+                         "openib_reg_mr: base=%p, bound=%p, size=%d", reg->base, reg->bound,
+                         (int) (reg->bound - reg->base + 1)));
+
 #if OMPI_CUDA_SUPPORT
     if (reg->flags & MCA_MPOOL_FLAGS_CUDA_REGISTER_MEM) {
         mca_common_cuda_register(base, size,
@@ -612,6 +616,10 @@ static int openib_dereg_mr(void *reg_data, mca_mpool_base_registration_t *reg)
 {
     mca_btl_openib_device_t *device = (mca_btl_openib_device_t*)reg_data;
     mca_btl_openib_reg_t *openib_reg = (mca_btl_openib_reg_t*)reg;
+
+    OPAL_OUTPUT_VERBOSE((30, mca_btl_openib_component.memory_registration_verbose,
+                         "openib_dereg_mr: base=%p, bound=%p, size=%d", reg->base, reg->bound,
+                         (int) (reg->bound - reg->base + 1)));
 
     if(openib_reg->mr != NULL) {
         if(ibv_dereg_mr(openib_reg->mr)) {
