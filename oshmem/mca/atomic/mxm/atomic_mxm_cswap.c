ac421fc1b99a442a9815e74077d3378b603b8712
Joshua Ladd <joshual@mellanox.com>
2013-09-24 21:48:57 +0000

    Fixing OSHMEM 64-bit compiler warnings/picky errors in MXM enabled components.
    This commit was SVN r29239.

--- oshmem/mca/atomic/mxm/atomic_mxm_cswap.c
+++ oshmem/mca/atomic/mxm/atomic_mxm_cswap.c
@@ -31,7 +31,8 @@ int mca_atomic_mxm_cswap(void *target,
 {
     unsigned my_pe;
     uint8_t nlong_order;
-    uint64_t remote_addr;
+    //uint64_t remote_addr;
+    void *remote_addr;
     int ptl_id;
     mxm_send_req_t sreq;
     mxm_error_t mxm_err;
@@ -77,7 +78,8 @@ int mca_atomic_mxm_cswap(void *target,
     }
 
     if (!mca_memheap.memheap_get_cached_mkey(pe,
-                                             (unsigned long) target,
+                                             //(unsigned long) target,
+                                             target,
                                              ptl_id,
                                              &remote_addr)) {
         ATOMIC_ERROR("[#%d] %p is not address of symmetric variable",
