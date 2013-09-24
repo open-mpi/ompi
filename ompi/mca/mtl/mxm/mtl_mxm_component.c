034f6bfee83a1775b25a73a623555b07a179abe0
Ralph Castain <rhc@open-mpi.org>
2013-09-24 18:32:26 +0000

    Fixes trac:3769: Move r29177 to v1.7 branch (enable mxm2 by default on)
    ---svn-pre-commit-ignore-below---
    
    r29177 [[BR]]
    enable mxm2 by default on np>=0
    reviewed by yossi
    cmr:v1.7.3:reviewer=jsquyres
    
    r29178 [[BR]]
    enable mxm2 from np>0
    reviewed by yossi
    cmr:v1.7.3:reviewer=jsquyres
    
    This commit was SVN r29234.
    
    The following SVN revision numbers were found above:
      r29177 --> open-mpi/ompi@44bfa9555359e7b62847b03a5d8eef4d9fe0b50f
      r29178 --> open-mpi/ompi@432c10750ac1669ee282b3588c60d26e2b58a828
    
    The following Trac tickets were found above:
      Ticket 3769 --> https://svn.open-mpi.org/trac/ompi/ticket/3769

--- ompi/mca/mtl/mxm/mtl_mxm_component.c
+++ ompi/mca/mtl/mxm/mtl_mxm_component.c
@@ -72,7 +72,11 @@ static int ompi_mtl_mxm_component_register(void)
                                            MCA_BASE_VAR_SCOPE_LOCAL,
                                            &ompi_mtl_mxm.verbose);
 
+#if MXM_API >= MXM_VERSION(2,0)
+    ompi_mtl_mxm.mxm_np = 0;
+#else
     ompi_mtl_mxm.mxm_np = 128;
+#endif
     (void) mca_base_component_var_register(c, "np",
                                            "[integer] Minimal number of MPI processes in a single job "
                                            "required to activate the MXM transport",
