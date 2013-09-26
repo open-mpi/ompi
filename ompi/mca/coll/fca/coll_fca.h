7c6ff00da5f9348e06c4e3c0a574d23bafa3f7f7
Mike Dubman <miked@mellanox.com>
2013-09-26 17:48:07 +0000

    Add caching of FCA communicators developed by Dinar, reviewed by miked/yossi. cmr:v1.7.3:reviewer=jsquyres:subject=add caching of FCA communicators.
    This commit was SVN r29256.

--- ompi/mca/coll/fca/coll_fca.h
+++ ompi/mca/coll/fca/coll_fca.h
@@ -7,6 +7,7 @@
   $HEADER$
  */
 
+
 #ifndef MCA_COLL_FCA_H
 #define MCA_COLL_FCA_H
 
@@ -20,7 +21,7 @@
 #include "ompi/mca/coll/base/coll_tags.h"
 #include "ompi/communicator/communicator.h"
 #include "ompi/op/op.h"
-
+#include "orte/runtime/orte_globals.h"
 #include "coll_fca_api.h"
 #include "coll_fca_debug.h"
 
@@ -72,7 +73,7 @@ BEGIN_C_DECLS
  * Used to load the library dynamically.
  */
 
-
+int modular_pow(uint64_t base, uint64_t exponent, uint64_t modulus);
 
 /**
  * FCA data type information
@@ -167,14 +168,66 @@ struct mca_coll_fca_component_t {
     /** MCA parameter: FCA NP */
     int   fca_np;
 
+	/** MCA parameter: Enable FCA comm cache */
+	int   fca_enable_cache;
+
+	/** MCA parameter: Enable parallel hash calc */
+	int fca_parallel_hash_calc;
+
+	/** Some statistics counters */
+	double fca_total_work_time;
+	double fca_work_time_parallel;
+	double fca_work_time_sequency;
+	int fca_cache_hit;
+	int fca_cache_miss;
+	int fca_hash_hit;
+	int fca_hash_miss;
+	int fca_max_deep_in_cache;
+
+	/** MCA parameter: Enable hash for cache */
+	int fca_enable_hash;
+
     /* FCA global stuff */
     fca_t *fca_context;                                 /* FCA context handle */
     mca_coll_fca_dtype_info_t fca_dtypes[FCA_DT_MAX_PREDEFINED]; /* FCA dtype translation */
     mca_coll_fca_op_info_t fca_reduce_ops[FCA_MAX_OPS]; /* FCA op translation */
+
+	/* communicator cache */
+	opal_list_t c_cache;
+
+	/* pointer to primes */
+	int *fca_primes;
+
+	/** MCA parameter hash table size*/
+	int fca_hash_size;
+	
+	/** MCA parameter hash table size*/
+	int fca_number_of_primes;
+
+	/* Pointer to hash  */
+	opal_list_t **fca_hash;
+
 };
 typedef struct mca_coll_fca_component_t mca_coll_fca_component_t;
 
 OMPI_MODULE_DECLSPEC extern mca_coll_fca_component_t mca_coll_fca_component;
+
+struct mca_coll_fca_comm_wrap_t {
+	opal_object_t super;
+	fca_comm_t *fca_comm;
+	int comm_id, rank;
+};
+typedef struct mca_coll_fca_comm_wrap_t mca_coll_fca_comm_wrap_t;
+OBJ_CLASS_DECLARATION(mca_coll_fca_comm_wrap_t);
+
+struct mca_coll_fca_c_cache_item_t {
+	opal_list_item_t super;
+	int size;
+	ompi_communicator_t *comm;
+	mca_coll_fca_comm_wrap_t *fca_comm_wrap;
+};
+typedef struct mca_coll_fca_c_cache_item_t mca_coll_fca_c_cache_item_t;
+OBJ_CLASS_DECLARATION(mca_coll_fca_c_cache_item_t);
 
 
 /**
