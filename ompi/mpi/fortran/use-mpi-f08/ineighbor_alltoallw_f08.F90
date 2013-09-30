66eb7ff610b1e4fd79cf2dda8a1fd5a74f3fc446
Jeff Squyres <jsquyres@cisco.com>
2013-09-30 17:54:12 +0000

    Fix the !MPI_[I]Neighbor_* mpi_f08 interfaces.
    Use the same trick for send/recvtypes as we did in r29298 for
    ![I]Alltoallw.  Also, add all prototypes for all the back-end
    ompi_neighbor_*_f functions.
    
    Refs trac:3802
    
    This commit was SVN r29299.
    
    The following SVN revision numbers were found above:
      r29298 --> open-mpi/ompi@0acebc0a1f9145bcb182e385bf9a321a2665ae41
    
    The following Trac tickets were found above:
      Ticket 3802 --> https://svn.open-mpi.org/trac/ompi/ticket/3802

--- ompi/mpi/fortran/use-mpi-f08/ineighbor_alltoallw_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/ineighbor_alltoallw_f08.F90
@@ -1,6 +1,6 @@
 ! -*- f90 -*-
 !
-! Copyright (c) 2009-2012 Cisco Systems, Inc.  All rights reserved.
+! Copyright (c) 2009-2013 Cisco Systems, Inc.  All rights reserved.
 ! Copyright (c) 2009-2013 Los Alamos National Security, LLC.
 !                         All rights reserved.
 ! $COPYRIGHT$
@@ -12,18 +12,19 @@ subroutine MPI_Ineighbor_alltoallw_f08(sendbuf,sendcounts,sdispls,sendtypes,&
    use :: mpi_f08_types, only : MPI_Datatype, MPI_Comm, MPI_Request, MPI_ADDRESS_KIND
    use :: mpi_f08, only : ompi_ineighbor_alltoallw_f
    implicit none
-   OMPI_FORTRAN_IGNORE_TKR_TYPE, INTENT(IN) :: sendbuf, recvbuf
+   OMPI_FORTRAN_IGNORE_TKR_TYPE, INTENT(IN) :: sendbuf
+   OMPI_FORTRAN_IGNORE_TKR_TYPE :: recvbuf
    INTEGER, INTENT(IN) :: sendcounts(*), recvcounts(*)
    INTEGER(MPI_ADDRESS_KIND), INTENT(IN) :: sdispls(*), rdispls(*)
-   TYPE(MPI_Datatype), INTENT(IN) :: sendtypes
-   TYPE(MPI_Datatype), INTENT(IN) :: recvtypes
+   TYPE(MPI_Datatype), INTENT(IN) :: sendtypes(*)
+   TYPE(MPI_Datatype), INTENT(IN) :: recvtypes(*)
    TYPE(MPI_Comm), INTENT(IN) :: comm
    TYPE(MPI_Request), INTENT(OUT) :: request
    INTEGER, OPTIONAL, INTENT(OUT) :: ierror
    integer :: c_ierror
 
-   call ompi_ineighbor_alltoallw_f(sendbuf,sendcounts,sdispls,sendtypes%MPI_VAL,&
-                                   recvbuf,recvcounts,rdispls,recvtypes%MPI_VAL,&
+   call ompi_ineighbor_alltoallw_f(sendbuf,sendcounts,sdispls,sendtypes(1)%MPI_VAL,&
+                                   recvbuf,recvcounts,rdispls,recvtypes(1)%MPI_VAL,&
                                    comm%MPI_VAL,request%MPI_VAL,c_ierror)
    if (present(ierror)) ierror = c_ierror
 
