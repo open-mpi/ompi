c08f97b030496eefc612d01efe2b86c96201e0a7
Jeff Squyres <jsquyres@cisco.com>
2013-10-04 22:43:07 +0000

    Fix problems with calling back-end BIND(C) ompi_*_f() functions.
    BIND(C) doesn't let us have LOGICAL parameters, so we have to be
    creative in how we invoke back-end ompi_*_f() C functions.
    Additionally, the mpi_f08 type for MPI_Status presented some
    difficulties, too.
        
    See the large comment in
    ompi/mpi/fortran/use-mpi-f08/mpi-f-interfaces-bind.h that explains
    this in much more detail.
    
    This commit was SVN r29384.

--- ompi/mpi/fortran/use-mpi-f08/profile/pinitialized_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/profile/pinitialized_f08.F90
@@ -1,18 +1,19 @@
 ! -*- f90 -*-
 !
-! Copyright (c) 2010-2012 Cisco Systems, Inc.  All rights reserved.
+! Copyright (c) 2010-2013 Cisco Systems, Inc.  All rights reserved.
 ! Copyright (c) 2009-2012 Los Alamos National Security, LLC.
 !               All Rights reserved.
 ! $COPYRIGHT$
 
 subroutine PMPI_Initialized_f08(flag,ierror)
-   use :: mpi_f08, only : ompi_initialized_f
+   ! See note in mpi-f-interfaces-bind.h for why we "use mpi" here and
+   ! call a PMPI_* subroutine below.
+   use :: mpi, only : PMPI_Initialized
    implicit none
    LOGICAL, INTENT(OUT) :: flag
    INTEGER, OPTIONAL, INTENT(OUT) :: ierror
    integer :: c_ierror
 
-   call ompi_initialized_f(flag,c_ierror)
+   call PMPI_Initialized(flag,c_ierror)
    if (present(ierror)) ierror = c_ierror
-
 end subroutine PMPI_Initialized_f08
--- ompi/mpi/fortran/use-mpi-f08/win_get_attr_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/win_get_attr_f08.F90
@@ -1,13 +1,15 @@
 ! -*- f90 -*-
 !
-! Copyright (c) 2010-2012 Cisco Systems, Inc.  All rights reserved.
+! Copyright (c) 2010-2013 Cisco Systems, Inc.  All rights reserved.
 ! Copyright (c) 2009-2012 Los Alamos National Security, LLC.
 !                         All rights reserved.
 ! $COPYRIGHT$
 
 subroutine MPI_Win_get_attr_f08(win,win_keyval,attribute_val,flag,ierror)
    use :: mpi_f08_types, only : MPI_Win, MPI_ADDRESS_KIND
-   use :: mpi_f08, only : ompi_win_get_attr_f
+   ! See note in mpi-f-interfaces-bind.h for why we "use mpi" here and
+   ! call a PMPI_* subroutine below.
+   use :: mpi, only : PMPI_Win_get_attr
    implicit none
    TYPE(MPI_Win), INTENT(IN) :: win
    INTEGER, INTENT(IN) :: win_keyval
@@ -16,7 +18,6 @@ subroutine MPI_Win_get_attr_f08(win,win_keyval,attribute_val,flag,ierror)
    INTEGER, OPTIONAL, INTENT(OUT) :: ierror
    integer :: c_ierror
 
-   call ompi_win_get_attr_f(win%MPI_VAL,win_keyval,attribute_val,flag,c_ierror)
+   call PMPI_Win_get_attr(win%MPI_VAL,win_keyval,attribute_val,flag,c_ierror)
    if (present(ierror)) ierror = c_ierror
-
 end subroutine MPI_Win_get_attr_f08
