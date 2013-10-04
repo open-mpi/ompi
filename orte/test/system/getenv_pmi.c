b83961b565c84745e1430e87788306247b113b95
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2013-10-04 15:21:40 +0000

    Add a useful test and update another

--- orte/test/system/getenv_pmi.c
+++ orte/test/system/getenv_pmi.c
@@ -26,6 +26,8 @@ int main(int argc, char **argv, char **envp)
     int rc = EXIT_SUCCESS;
     char *err = NULL;
     PMI_BOOL pmi_initialized = PMI_FALSE;
+    int pmi_vallen_max, max_length;
+    char *pmi_kvs_name;
 
     /* sanity */
     if (PMI_SUCCESS != PMI_Initialized(&pmi_initialized) ||
@@ -49,6 +51,24 @@ int main(int argc, char **argv, char **envp)
         err = "PMI_Get_clique_size failure!";
         goto done;
     }
+    if (PMI_SUCCESS != PMI_KVS_Get_value_length_max(&pmi_vallen_max)) {
+        err = "PMI_KVS_Get_value_length_max failure!";
+        goto done;
+    }
+    if (PMI_SUCCESS != PMI_KVS_Get_name_length_max(&max_length)) {
+        err = "PMI_KVS_Get_name_length_max failure!";
+        goto done;
+    }
+    pmi_kvs_name = (char*)malloc(max_length);
+    if (NULL == pmi_kvs_name) {
+        err = "malloc failure!";
+        goto done;
+    }
+    if (PMI_SUCCESS != PMI_KVS_Get_my_name(pmi_kvs_name,max_length)) {
+        err = "PMI_KVS_Get_my_name failure!";
+        goto done;
+    }
+
     if (NULL == (local_rank_ids = calloc(num_local_procs, sizeof(int)))) {
         err = "out of resources";
         goto done;
@@ -58,11 +78,11 @@ int main(int argc, char **argv, char **envp)
         goto done;
     }
     /* lowest local rank will print env info and tag its output*/
-    if (pmi_rank == local_rank_ids[0]) {
-        for (; NULL != envp && NULL != *envp; ++envp) {
-            printf("===[%d]: %s\n",  pmi_rank, *envp);
-        }
-    }
+    //    if (pmi_rank == local_rank_ids[0]) {
+    //  for (; NULL != envp && NULL != *envp; ++envp) {
+    //      printf("===[%d]: %s\n",  pmi_rank, *envp);
+    //  }
+    //}
 
 done:
     if (PMI_TRUE == pmi_initialized) {
