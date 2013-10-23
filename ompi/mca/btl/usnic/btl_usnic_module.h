d0b7d125b216b22401a1d24c2d686fd97b9be8ef
Dave Goodell <dgoodell@cisco.com>
2013-10-23 15:50:57 +0000

    usnic: refactor usnic_stats_callback
    Pull the bulk of the functionality out into a new routine,
    ompi_btl_usnic_print_stats, which can be used in other debugging
    contexts.  This also lets us eliminate the module->final_stats state
    tracking.
    
    Reviewed-by: Jeff Squyres <jsquyres@cisco.com>
    
    This commit was SVN r29482.

--- ompi/mca/btl/usnic/btl_usnic_module.h
+++ ompi/mca/btl/usnic/btl_usnic_module.h
@@ -292,6 +292,11 @@ int ompi_btl_usnic_module_init(ompi_btl_usnic_module_t* module);
  */
 void ompi_btl_usnic_module_progress_sends(ompi_btl_usnic_module_t *module);
 
+/* opal_output statistics that are useful for debugging */
+void ompi_btl_usnic_print_stats(
+    ompi_btl_usnic_module_t *module,
+    const char *prefix,
+    bool reset_stats);
 
 END_C_DECLS
 #endif
