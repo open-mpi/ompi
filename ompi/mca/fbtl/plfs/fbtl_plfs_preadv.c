10b603e995b4aabd70c18aa9e0ae60f8469e30dd
Hadi Montakhabi <hadi@open-mpi-git-mirror.example.com>
2014-01-22 21:16:15 +0000

    add support for PLFS filesystem

---
+++ ompi/mca/fbtl/plfs/fbtl_plfs_preadv.c
@@ -0,0 +1,201 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2011 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2008-2014 University of Houston. All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+#include "fbtl_plfs.h"
+
+#include "mpi.h"
+#include <unistd.h>
+#include "ompi/constants.h"
+#include "ompi/mca/fbtl/fbtl.h"
+
+size_t 
+mca_fbtl_plfs_preadv (mca_io_ompio_file_t *fh,
+                       int *sorted)
+{
+
+    Plfs_fd *pfd = NULL;
+    plfs_error_t plfs_ret;
+    pfd = fh->f_fs_ptr;
+
+    int i, block=1;
+    struct iovec *iov = NULL;
+    int iov_count = 0;
+    OMPI_MPI_OFFSET_TYPE iov_offset = 0;
+
+    if (NULL == fh->f_io_array) {
+        return OMPI_ERROR;
+    }
+
+    iov = (struct iovec *) malloc 
+        (OMPIO_IOVEC_INITIAL_SIZE * sizeof (struct iovec));
+    if (NULL == iov) {
+        opal_output(1, "OUT OF MEMORY\n");
+        return OMPI_ERR_OUT_OF_RESOURCE;
+    }
+
+    if (NULL != sorted) {
+        for (i=0 ; i<fh->f_num_of_io_entries ; i++) {
+            if (0 == iov_count) {
+                iov[iov_count].iov_base = fh->f_io_array[sorted[i]].memory_address;
+                iov[iov_count].iov_len = fh->f_io_array[sorted[i]].length;
+                iov_offset = (OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[sorted[i]].offset;
+                iov_count ++;
+            }
+
+            if (OMPIO_IOVEC_INITIAL_SIZE*block <= iov_count) {
+                block ++;
+                iov = (struct iovec *)realloc
+                    (iov, OMPIO_IOVEC_INITIAL_SIZE * block *
+                     sizeof(struct iovec));
+                if (NULL == iov) {
+                    opal_output(1, "OUT OF MEMORY\n");
+                    return OMPI_ERR_OUT_OF_RESOURCE;
+                }
+            }
+
+            if (fh->f_num_of_io_entries != i+1) {
+	      if (((OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[sorted[i]].offset + 
+                     (OPAL_PTRDIFF_TYPE)fh->f_io_array[sorted[i]].length) == 
+		  (OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[sorted[i+1]].offset) {                    
+                    iov[iov_count].iov_base = 
+                        fh->f_io_array[sorted[i+1]].memory_address;
+                    iov[iov_count].iov_len = fh->f_io_array[sorted[i+1]].length;
+                    iov_count ++;
+                    continue;
+                }
+            }
+
+	    // Find the total number of bytes to be read.
+	    size_t bytes = 0;
+	    for (int i = 0; i < iov_count; ++i) {
+		bytes += iov[i].iov_len;
+	    }
+
+	    // Allocate a temporary buffer to hold the data
+	    char *buffer;
+	    buffer = (char *) malloc (bytes);
+	    if (buffer == NULL) {
+	        return OMPI_ERROR;
+	    }
+
+	    // Read the data
+	    ssize_t bytes_read;
+
+	    plfs_ret = plfs_read( pfd, buffer, bytes, iov_offset, &bytes_read );
+	    if (PLFS_SUCCESS != plfs_ret) {
+	        opal_output(0, "fbtl_plfs_preadv: Error in plfs_read:\n%s\n", strplfserr(plfs_ret));
+		return OMPI_ERROR;
+	    }
+	    	      
+	    if (bytes_read < 0)
+	        return OMPI_ERROR;
+
+	    // Copy the data from BUFFER into the memory specified by IOV
+	    bytes = bytes_read;
+	    for (int i = 0; i < iov_count; ++i) {
+	        size_t copy = MIN (iov[i].iov_len, bytes);
+		(void) memcpy ((void *) iov[i].iov_base, (void *) buffer, copy);
+		buffer += copy;
+		bytes -= copy;
+		if (bytes == 0) {
+		    break;
+		}
+	    }
+	    iov_count = 0;
+        }
+    }
+
+    else {
+        for (i=0 ; i<fh->f_num_of_io_entries ; i++) {
+            if (0 == iov_count) {
+                iov[iov_count].iov_base = fh->f_io_array[i].memory_address;
+                iov[iov_count].iov_len = fh->f_io_array[i].length;
+                iov_offset = (OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[i].offset;
+                iov_count ++;
+            }
+
+            if (OMPIO_IOVEC_INITIAL_SIZE*block <= iov_count) {
+                block ++;
+                iov = (struct iovec *)realloc
+                    (iov, OMPIO_IOVEC_INITIAL_SIZE * block *
+		     sizeof(struct iovec));
+                if (NULL == iov) {
+                    opal_output(1, "OUT OF MEMORY\n");
+                    return OMPI_ERR_OUT_OF_RESOURCE;
+                }
+            }
+
+            if (fh->f_num_of_io_entries != i+1) {
+	      if (((OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[i].offset + 
+                     (OPAL_PTRDIFF_TYPE)fh->f_io_array[i].length) == 
+		  (OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[i+1].offset) {                    
+                    iov[iov_count].iov_base = 
+                        fh->f_io_array[i+1].memory_address;
+                    iov[iov_count].iov_len = fh->f_io_array[i+1].length;
+                    iov_count ++;
+                    continue;
+                }
+            }
+
+	    // Find the total number of bytes to be read.
+	    size_t bytes = 0;
+	    for (int i = 0; i < iov_count; ++i) {
+		bytes += iov[i].iov_len;
+	    }
+
+	    // Allocate a temporary buffer to hold the data
+	    char *buffer;
+	    buffer = (char *) malloc (bytes);
+	    if (buffer == NULL) {
+	        return OMPI_ERROR;
+	    }
+
+	    // Read the data
+	    ssize_t bytes_read;
+	    plfs_ret = plfs_read( pfd, buffer, bytes, iov_offset, &bytes_read );
+	    if (PLFS_SUCCESS != plfs_ret) {
+	        opal_output(0, "fbtl_plfs_preadv: Error in plfs_read:\n%s\n", strplfserr(plfs_ret));
+		return OMPI_ERROR;
+	    }
+
+	    if (bytes_read < 0)
+	        return OMPI_ERROR;
+
+	    // Copy the data from BUFFER into the memory specified by IOV
+	    bytes = bytes_read;
+	    for (int i = 0; i < iov_count; ++i) {
+	        size_t copy = MIN (iov[i].iov_len, bytes);
+		(void) memcpy ((void *) iov[i].iov_base, (void *) buffer, copy);
+		buffer += copy;
+		bytes -= copy;
+		if (bytes == 0) {
+		    break;
+		}
+	    }
+	    iov_count = 0;
+	}
+    }
+    if (NULL != iov) {
+        free (iov);
+        iov = NULL;
+    }
+
+    return OMPI_SUCCESS;
+}
---
+++ ompi/mca/fs/plfs/fs_plfs_component.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2008-2014 University of Houston. All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ *
+ * These symbols are in a file by themselves to provide nice linker
+ * semantics.  Since linkers generally pull in symbols by object
+ * files, keeping these symbols as the only symbols in this file
+ * prevents utility programs such as "ompi_info" from having to import
+ * entire components just to query their version and parameters.
+ */
+
+#include "ompi_config.h"
+#include "fs_plfs.h"
+#include "mpi.h"
+
+/*
+ * Public string showing the fs plfs component version number
+ */
+const char *mca_fs_plfs_component_version_string =
+  "OMPI/MPI plfs FS MCA component version " OMPI_VERSION;
+
+static int plfs_register(void);
+
+int mca_fs_plfs_priority = 20;
+int mca_fs_plfs_num_hostdir = -1;
+
+/*
+ * Instantiate the public struct with all of our public information
+ * and pointers to our public functions in it
+ */
+mca_fs_base_component_2_0_0_t mca_fs_plfs_component = {
+
+    /* First, the mca_component_t struct containing meta information
+       about the component itself */
+
+    {
+        MCA_FS_BASE_VERSION_2_0_0,
+
+        /* Component name and version */
+        "plfs",
+        OMPI_MAJOR_VERSION,
+        OMPI_MINOR_VERSION,
+        OMPI_RELEASE_VERSION,
+        .mca_register_component_params = plfs_register,
+    },
+    {
+        /* This component is checkpointable */
+      MCA_BASE_METADATA_PARAM_CHECKPOINT
+    },
+    mca_fs_plfs_component_init_query,      /* get thread level */
+    mca_fs_plfs_component_file_query,      /* get priority and actions */
+    mca_fs_plfs_component_file_unquery     /* undo what was done by previous function */
+};
+
+static int
+plfs_register(void)
+{
+    mca_fs_plfs_priority = 20;
+    (void) mca_base_component_var_register(&mca_fs_plfs_component.fsm_version,
+                                           "priority", "Priority of the plfs fs component",
+                                           MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
+                                           OPAL_INFO_LVL_9,
+                                           MCA_BASE_VAR_SCOPE_READONLY, &mca_fs_plfs_priority);
+    mca_fs_plfs_num_hostdir = -1;
+    (void) mca_base_component_var_register(&mca_fs_plfs_component.fsm_version,
+                                           "num_hostdir", "number of host directories of a file over plfs",
+                                           MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
+                                           OPAL_INFO_LVL_9,
+                                           MCA_BASE_VAR_SCOPE_READONLY, &mca_fs_plfs_num_hostdir);
+
+    return OMPI_SUCCESS;
+}
