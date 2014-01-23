19617394f0be189ea1ee51e7007ce982a8a9dc87
Jeff Squyres <jsquyres@cisco.com>
2014-01-23 01:24:54 +0000

    Add profiling versions of dist_graph functions into the library
    Also fix the interfaces that have logical parameters (the
    non-profiling versions were added/fixed a long time ago; it looks like
    the profiling versions were inadvertantly skipped).
    
    cmr=v1.7.4:ticket=4162
    
    This commit was SVN r30377.
    
    The following Trac tickets were found above:
      Ticket 4162 --> https://svn.open-mpi.org/trac/ompi/ticket/4162

--- ompi/mpi/fortran/use-mpi-f08/profile/pdist_graph_create_adjacent_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/profile/pdist_graph_create_adjacent_f08.F90
@@ -1,6 +1,6 @@
 ! -*- f90 -*-
 !
-! Copyright (c) 2010-2012 Cisco Systems, Inc.  All rights reserved.
+! Copyright (c) 2010-2013 Cisco Systems, Inc.  All rights reserved.
 ! Copyright (c) 2009-2012 Los Alamos National Security, LLC.
 !                         All rights reserved.
 ! $COPYRIGHT$
@@ -9,7 +9,9 @@ subroutine PMPI_Dist_graph_create_adjacent_f08(comm_old,indegree,sources,sourcew
                                                outdegree,destinations,destweights,info,&
                                                reorder,comm_dist_graph,ierror)
    use :: mpi_f08_types, only : MPI_Comm, MPI_Info
-   use :: mpi_f08, only : ompi_dist_graph_create_adjacent_f
+   ! See note in mpi-f-interfaces-bind.h for why we "use mpi" here and
+   ! call a PMPI_* subroutine below.
+   use :: mpi, only : PMPI_Dist_graph_create_adjacent
    implicit none
    TYPE(MPI_Comm), INTENT(IN) :: comm_old
    INTEGER, INTENT(IN) :: indegree, outdegree
@@ -20,10 +22,9 @@ subroutine PMPI_Dist_graph_create_adjacent_f08(comm_old,indegree,sources,sourcew
    INTEGER, OPTIONAL, INTENT(OUT) :: ierror
    integer :: c_ierror
 
-   call ompi_dist_graph_create_adjacent_f(comm_old%MPI_VAL,indegree,sources,&
+   call PMPI_Dist_graph_create_adjacent(comm_old%MPI_VAL,indegree,sources,&
                                           sourceweights,outdegree,destinations,&
                                           destweights,info%MPI_VAL,&
                                           reorder,comm_dist_graph%MPI_VAL,ierror)
    if (present(ierror)) ierror = c_ierror
-
 end subroutine PMPI_Dist_graph_create_adjacent_f08
