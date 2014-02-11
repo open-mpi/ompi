707e51d7869169aebe42ffa91454c40dc02702d6
Ralph Castain <rhc@open-mpi.org>
2014-02-11 02:53:53 +0000

    Check for --cpus-per-proc earlier, before the correct option can be processed. Thanks to Tetsuya Mishima for reporting it.
    Refs trac:4117
    
    This commit was SVN r30662.
    
    The following Trac tickets were found above:
      Ticket 4117 --> https://svn.open-mpi.org/trac/ompi/ticket/4117

--- orte/mca/rmaps/rmaps_types.h
+++ orte/mca/rmaps/rmaps_types.h
@@ -11,6 +11,7 @@
  * Copyright (c) 2011      Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2012      Los Alamos National Security, LLC.
  *                         All rights reserved.
+ * Copyright (c) 2014      Intel, Inc. All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
