56903f9f0b48e8e43249f2519e4fe3a658a10011
Adrian Reber <adrian@open-mpi-git-mirror.example.com>
2014-02-11 14:43:38 +0000

    CRS/CRIU: added CRIU as new CRS component
    To be able to checkpoint/restart using criu (criu.org) a new
    CRS component is added which is based on criu. This first commit
    provides the minimal set of functions and configure script options
    to enable --with-criu and link against libcriu.so.
    No actual checkpoint/restart functionality is yet implemented.
    This is only the framework which needs to be filled with the
    actual functionality.

---
+++ opal/mca/crs/criu/crs_criu_component.c
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2004-2009 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2014      Hochschule Esslingen.  All rights reserved.
+ *
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "opal_config.h"
+
+#include "opal/util/output.h"
+
+#include "opal/constants.h"
+#include "opal/mca/crs/crs.h"
+#include "opal/mca/crs/base/base.h"
+#include "crs_criu.h"
+
+/* Local functionality */
+static int crs_criu_register(void);
+static int crs_criu_open(void);
+static int crs_criu_close(void);
+
+/*
+ * Instantiate the public struct with all of our public information
+ * and pointer to our public functions in it
+ */
+opal_crs_criu_component_t mca_crs_criu_component = {
+    /* First do the base component stuff */
+    {
+        /* Handle the general mca_component_t struct containing
+         *  meta information about the component itself
+         */
+        {
+            OPAL_CRS_BASE_VERSION_2_0_0,
+
+            /* Component name and version */
+            "criu",
+            OPAL_MAJOR_VERSION,
+            OPAL_MINOR_VERSION,
+            OPAL_RELEASE_VERSION,
+
+            /* Component open and close functions */
+            crs_criu_open,
+            crs_criu_close,
+            opal_crs_criu_component_query,
+            crs_criu_register
+        },
+        {
+            /* The component is checkpoint ready */
+            MCA_BASE_METADATA_PARAM_CHECKPOINT
+        },
+
+        /* Verbosity level */
+        0,
+        /* opal_output handler */
+        -1
+    }
+};
+
+static int crs_criu_register(void)
+{
+    int ret;
+
+    mca_base_component_t *component = &mca_crs_criu_component.super.base_version;
+
+    mca_crs_criu_component.super.priority = 10;
+    ret = mca_base_component_var_register(component, "priority",
+                                          "Priority of the CRS criu component (default: 10)",
+                                          MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
+                                          OPAL_INFO_LVL_9, MCA_BASE_VAR_SCOPE_ALL_EQ,
+                                          &mca_crs_criu_component.super.priority);
+    if (0 > ret) {
+        return ret;
+    }
+
+    mca_crs_criu_component.super.verbose = 0;
+    ret = mca_base_component_var_register(component, "verbose",
+                                          "Verbose level for the CRS criu component",
+                                          MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
+                                          OPAL_INFO_LVL_9, MCA_BASE_VAR_SCOPE_LOCAL,
+                                          &mca_crs_criu_component.super.verbose);
+
+    return (0 > ret) ? ret : OPAL_SUCCESS;
+}
+
+static int crs_criu_open(void)
+{
+    /* If there is a custom verbose level for this component than use it
+     * otherwise take our parents level and output channel
+     */
+    if (0 != mca_crs_criu_component.super.verbose) {
+        mca_crs_criu_component.super.output_handle = opal_output_open(NULL);
+        opal_output_set_verbosity(mca_crs_criu_component.super.output_handle,
+                                  mca_crs_criu_component.super.verbose);
+    } else {
+        mca_crs_criu_component.super.output_handle = opal_crs_base_framework.framework_output;
+    }
+
+    /*
+     * Debug output
+     */
+    opal_output_verbose(10, mca_crs_criu_component.super.output_handle,
+                        "crs:criu: open()");
+    opal_output_verbose(20, mca_crs_criu_component.super.output_handle,
+                        "crs:criu: open: priority = %d",
+                        mca_crs_criu_component.super.priority);
+    opal_output_verbose(20, mca_crs_criu_component.super.output_handle,
+                        "crs:criu: open: verbosity = %d",
+                        mca_crs_criu_component.super.verbose);
+
+    return OPAL_SUCCESS;
+}
+
+static int crs_criu_close(void)
+{
+    opal_output_verbose(10, mca_crs_criu_component.super.output_handle,
+                        "crs:criu: close()");
+
+    return OPAL_SUCCESS;
+}
