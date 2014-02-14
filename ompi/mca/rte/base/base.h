bdff767dce8130c5d69d9e8593c080bd1c509084
Ralph Castain <rhc@open-mpi.org>
2014-02-14 20:34:21 +0000

    Ick - wonder how this ever built static? There is no "select" function anywhere in the system.
    cmr=v1.7.5:reviewer=jsquyres:subject=remove bad function declaration
    
    This commit was SVN r30729.

--- ompi/mca/rte/base/base.h
+++ ompi/mca/rte/base/base.h
@@ -2,6 +2,7 @@
  * Copyright (c) 2010      Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2012-2013 Los Alamos National Security, LLC.
  *                         All rights reserved.
+ * Copyright (c) 2014      Intel, Inc. All rights reserved
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -24,38 +25,7 @@
 
 BEGIN_C_DECLS
 
-/**
- * Select an available component.
- *
- * @return OMPI_SUCCESS Upon success.
- * @return OMPI_NOT_FOUND If no component can be selected.
- * @return OMPI_ERROR Upon other failure.
- *
- * This function invokes the selection process for rte components,
- * which works as follows:
- *
- * - If the \em rte MCA parameter is not specified, the
- *   selection set is all available rte components.
- * - If the \em rte MCA parameter is specified, the
- *   selection set is just that component.
- * - All components in the selection set are queried to see if
- *   they want to run.  All components that want to run are ranked
- *   by their priority and the highest priority component is
- *   selected.  All non-selected components have their "close"
- *   function invoked to let them know that they were not selected.
- * - The selected component will have its "init" function invoked to
- *   let it know that it was selected.
- *
- * If we fall through this entire process and no component is
- * selected, then return OMPI_NOT_FOUND (this is not a fatal
- * error).
- *
- * At the end of this process, we'll either have a single
- * component that is selected and initialized, or no component was
- * selected.  If no component was selected, subsequent invocation
- * of the rte wrapper functions will return an error.
- */
-OMPI_DECLSPEC int ompi_rte_base_select(void);
+OMPI_DECLSPEC extern mca_base_framework_t ompi_rte_base_framework;
 
 END_C_DECLS
 
