3a31869a58e0d8aaa667150f93cf80e95ce50561
Oscar Vega-Gisbert <ovega@dsic.upv.es>
2014-03-01 20:18:05 +0000

    Authorship of Java bindings
    This commit was SVN r30901.

--- ompi/mpi/java/java/File.java
+++ ompi/mpi/java/java/File.java
@@ -1,4 +1,21 @@
 /*
+ * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+/*
  * IMPLEMENTATION DETAILS
  * 
  * All methods with buffers that can be direct or non direct have
@@ -6,6 +23,7 @@
  * 
  * Checking if a buffer is direct is faster in Java than C.
  */
+
 package mpi;
 
 import java.nio.*;
