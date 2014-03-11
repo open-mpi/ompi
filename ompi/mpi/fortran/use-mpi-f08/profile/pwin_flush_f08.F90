c4de1aa1cebaa40aeae9b8983a024517464cbfc7
Nathan Hjelm <hjelmn@lanl.gov>
2014-03-11 23:02:27 +0000

    osc: add fortran bindings for new RMA function
    I have only checked that these bindings compile without warnings. They
    appear to work with both intel's compilers and gfortran.
    
    cmr=v1.7.5:reviewer=jsquyres
    
    This commit was SVN r31010.

---
+++ ompi/mpi/fortran/use-mpi-f08/profile/pwin_flush_f08.F90
@@ -0,0 +1,20 @@
+! -*- f90 -*-
+!
+! Copyright (c) 2010-2012 Cisco Systems, Inc.  All rights reserved.
+! Copyright (c) 2009-2014 Los Alamos National Security, LLC.
+!               All Rights reserved.
+! $COPYRIGHT$
+
+subroutine PMPI_Win_flush_f08(rank,win,ierror)
+   use :: mpi_f08_types, only : MPI_Win
+   use :: mpi_f08, only : ompi_win_flush_f
+   implicit none
+   INTEGER, INTENT(IN) :: rank
+   TYPE(MPI_Win), INTENT(IN) :: win
+   INTEGER, OPTIONAL, INTENT(OUT) :: ierror
+   integer :: c_ierror
+
+   call ompi_win_flush_f(rank,win%MPI_VAL,c_ierror)
+   if (present(ierror)) ierror = c_ierror
+
+end subroutine PMPI_Win_flush_f08
