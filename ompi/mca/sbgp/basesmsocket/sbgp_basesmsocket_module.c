85736941f08528d4d7f99c45d25d7253a47bad58
Nathan Hjelm <hjelmn@open-mpi-git-mirror.example.com>
2014-03-18 21:25:57 +0000

    sbgp/basesmsocket: do not recalculate process locality
    The necessary information is stored in the proc object. There is no need
    to allgather the local process data to determine if another rank is on
    the same socket.
    
    cmr=v1.7.5:reviewer=manjugv

--- ompi/mca/sbgp/basesmsocket/sbgp_basesmsocket_module.c
+++ ompi/mca/sbgp/basesmsocket/sbgp_basesmsocket_module.c
@@ -1,6 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2009-2012 Oak Ridge National Laboratory.  All rights reserved.
  * Copyright (c) 2009-2012 Mellanox Technologies.  All rights reserved.
+ * Copyright (c) 2014      Los Alamos National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -28,21 +31,5 @@
 #include "ompi/communicator/communicator.h"
 #include "ompi/mca/sbgp/basesmsocket/sbgp_basesmsocket.h"
 
-/*
- * Local functions
- */
-static void
-mca_sbgp_basesmsocket_module_construct(mca_sbgp_basesmsocket_module_t *module)
-{
-}
-
-static void
-mca_sbgp_basesmsocket_module_destruct(mca_sbgp_basesmsocket_module_t *module)
-{
-    /* done */
-}
-
 OBJ_CLASS_INSTANCE(mca_sbgp_basesmsocket_module_t,
-                   mca_sbgp_base_module_t,
-                   mca_sbgp_basesmsocket_module_construct,
-                   mca_sbgp_basesmsocket_module_destruct);
+                   mca_sbgp_base_module_t, NULL, NULL);
