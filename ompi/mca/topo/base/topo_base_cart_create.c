deb7ebffdcb2db043d68b50ecce925d6f4bb72d1
Ralph Castain <rhc@open-mpi.org>
2014-03-19 16:19:45 +0000

    Fixes trac:4425: Move r31146, r31148 to v1.7 branch (Fix comment in r31146)
    ---svn-pre-commit-ignore-below---
    
    r31146 [[BR]]
    Protect against 0-byte allocations in carte_create and cart_sub.
    
    cmr=v1.7.5:reviewer=jsquyres
    
    r31148 [[BR]]
    Fix comment in r31146
    
    cmr=v1.7.5:ticket=trac:4425
    
    This commit was SVN r31152.
    
    The following SVN revision numbers were found above:
      r31146 --> open-mpi/ompi@dca2f0027e996e6a92aac17bed1b226b32355c8b
      r31148 --> open-mpi/ompi@20fe3804b0fb28c5920bdf3b3b283c2e20e8565e
    
    The following Trac tickets were found above:
      Ticket 4425 --> https://svn.open-mpi.org/trac/ompi/ticket/4425

--- ompi/mca/topo/base/topo_base_cart_create.c
+++ ompi/mca/topo/base/topo_base_cart_create.c
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
@@ -10,6 +11,8 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2008      Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2014      Los Alamos National Security, LLC. All right
+ *                         reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -127,17 +130,23 @@ int mca_topo_base_cart_create(mca_topo_base_module_t *topo,
         }
     }
 
-    /* Copy the proc structure from the previous communicator over to
-       the new one.  The topology module is then able to work on this
-       copy and rearrange it as it deems fit. */
-    topo_procs = (ompi_proc_t**)malloc(num_procs * sizeof(ompi_proc_t *));
-    if(OMPI_GROUP_IS_DENSE(old_comm->c_local_group)) {
-        memcpy(topo_procs, 
-               old_comm->c_local_group->grp_proc_pointers,
-               num_procs * sizeof(ompi_proc_t *));
-    } else {
-        for(i = 0 ; i < num_procs; i++) {
-            topo_procs[i] = ompi_group_peer_lookup(old_comm->c_local_group,i);
+    /* JMS: This should really be refactored to use
+       comm_create_group(), because ompi_comm_allocate() still
+       complains about 0-byte mallocs in debug builds for 0-member
+       groups. */
+    if (num_procs > 0) {
+        /* Copy the proc structure from the previous communicator over to
+           the new one.  The topology module is then able to work on this
+           copy and rearrange it as it deems fit. */
+        topo_procs = (ompi_proc_t**)malloc(num_procs * sizeof(ompi_proc_t *));
+        if(OMPI_GROUP_IS_DENSE(old_comm->c_local_group)) {
+            memcpy(topo_procs,
+                   old_comm->c_local_group->grp_proc_pointers,
+                   num_procs * sizeof(ompi_proc_t *));
+        } else {
+            for(i = 0 ; i < num_procs; i++) {
+                topo_procs[i] = ompi_group_peer_lookup(old_comm->c_local_group,i);
+            }
         }
     }
 
