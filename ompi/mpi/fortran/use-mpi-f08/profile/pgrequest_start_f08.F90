c94b69635b42d6c394be624bf9d5e92c4f96b1d3
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2014-04-11 20:56:11 +0000

    mpi_f08: pass function pointers from Fortran to C properly
    Use type(c_funptr) to "cast" the fortran function pointers to
    arbitrary C pointers.  In C, we then pick up the appropriate function
    pointer type.
    
    Tested with ifort 14.0.2 and gfortran 4.9 snapshot (which is what
    identified that the previous method of passing function pointers was
    not Fortran'08-compliant).
    
    Refs #4157

--- ompi/mpi/fortran/use-mpi-f08/profile/pgrequest_start_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/profile/pgrequest_start_f08.F90
@@ -9,6 +9,7 @@
 
 subroutine PMPI_Grequest_start_f08(query_fn,free_fn,cancel_fn,&
                                   extra_state,request,ierror)
+   use, intrinsic :: iso_c_binding, only: c_funptr, c_funloc
    use :: mpi_f08_types, only : MPI_Request, MPI_ADDRESS_KIND
    use :: mpi_f08_interfaces_callbacks, only : MPI_Grequest_query_function
    use :: mpi_f08_interfaces_callbacks, only : MPI_Grequest_free_function
@@ -22,8 +23,12 @@ subroutine PMPI_Grequest_start_f08(query_fn,free_fn,cancel_fn,&
    TYPE(MPI_Request), INTENT(OUT) :: request
    INTEGER, OPTIONAL, INTENT(OUT) :: ierror
    integer :: c_ierror
+   type(c_funptr) :: fquery_fn, ffree_fn, fcancel_fn
 
-   call ompi_grequest_start_f(query_fn,free_fn,cancel_fn,&
+   fquery_fn = c_funloc(query_fn)
+   ffree_fn = c_funloc(free_fn)
+   fcancel_fn = c_funloc(cancel_fn)
+   call ompi_grequest_start_f(fquery_fn,ffree_fn,fcancel_fn,&
                               extra_state,request%MPI_VAL,c_ierror)
    if (present(ierror)) ierror = c_ierror
 
--- ompi/mpi/fortran/use-mpi-f08/profile/ptype_create_keyval_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/profile/ptype_create_keyval_f08.F90
@@ -9,6 +9,7 @@
 
 subroutine PMPI_Type_create_keyval_f08(type_copy_attr_fn,type_delete_attr_fn,&
                                        type_keyval,extra_state,ierror)
+   use, intrinsic :: iso_c_binding, only: c_funptr, c_funloc
    use :: mpi_f08_types, only : MPI_ADDRESS_KIND
    use :: mpi_f08_interfaces_callbacks, only : MPI_Type_copy_attr_function
    use :: mpi_f08_interfaces_callbacks, only : MPI_Type_delete_attr_function
@@ -20,8 +21,11 @@ subroutine PMPI_Type_create_keyval_f08(type_copy_attr_fn,type_delete_attr_fn,&
    INTEGER(MPI_ADDRESS_KIND), INTENT(IN) :: extra_state
    INTEGER, OPTIONAL, INTENT(OUT) :: ierror
    integer :: c_ierror
+   type(c_funptr) :: fcopy_fn, fdelete_fn
 
-   call ompi_type_create_keyval_f(type_copy_attr_fn,type_delete_attr_fn,&
+   fcopy_fn = c_funloc(type_copy_attr_fn)
+   fdelete_fn = c_funloc(type_delete_attr_fn)
+   call ompi_type_create_keyval_f(fcopy_fn,fdelete_fn,&
                                   type_keyval,extra_state,c_ierror)
    if (present(ierror)) ierror = c_ierror
 
