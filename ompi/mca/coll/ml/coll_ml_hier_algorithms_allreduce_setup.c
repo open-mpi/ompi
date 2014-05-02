bb33922b3b8712294059321fe32f9ae1356f9c92
Ralph Castain <rhc@open-mpi.org>
2014-05-02 17:04:00 +0000

    Fixes trac:4479: Move r31285 to v1.8 branch (coll/ml: fix some warnings identified)
    ---svn-pre-commit-ignore-below---
    
    r31285 [[BR]]
    coll/ml: fix some warnings identified by clang
    
    cmr=v1.8.1:reviewer=manjugv
    
    This commit was SVN r31610.
    
    The following SVN revision numbers were found above:
      r31285 --> open-mpi/ompi@71bdb8c4393739313fdfeb5c46b6595908748510
    
    The following Trac tickets were found above:
      Ticket 4479 --> https://svn.open-mpi.org/trac/ompi/ticket/4479

--- ompi/mca/coll/ml/coll_ml_hier_algorithms_allreduce_setup.c
+++ ompi/mca/coll/ml/coll_ml_hier_algorithms_allreduce_setup.c
@@ -164,6 +164,7 @@ static int mca_coll_ml_build_allreduce_schedule(
             bcol_module->filtered_fns_table[DATA_SRC_KNOWN][NON_BLOCKING][BCOL_REDUCE][bcol_func_index][0][0];
         if (NULL == comp_fn->bcol_function) {
             /* if there isn't a bcol function for this then we can't continue */
+            ret = OMPI_ERR_NOT_SUPPORTED;
             goto Allreduce_Setup_Error;
         }
 
@@ -193,6 +194,7 @@ static int mca_coll_ml_build_allreduce_schedule(
             bcol_module->filtered_fns_table[DATA_SRC_KNOWN][NON_BLOCKING][BCOL_ALLREDUCE][bcol_func_index][0][0];
         if (NULL == comp_fn->bcol_function) {
             /* if there isn't a bcol function for this then we can't continue */
+            ret = OMPI_ERR_NOT_SUPPORTED;
             goto Allreduce_Setup_Error;
         }
 
@@ -223,6 +225,7 @@ static int mca_coll_ml_build_allreduce_schedule(
             bcol_module->filtered_fns_table[DATA_SRC_KNOWN][NON_BLOCKING][BCOL_BCAST][bcol_func_index][0][0];
         if (NULL == comp_fn->bcol_function) {
             /* if there isn't a bcol function for this then we can't continue */
+            ret = OMPI_ERR_NOT_SUPPORTED;
             goto Allreduce_Setup_Error;
         }
 
