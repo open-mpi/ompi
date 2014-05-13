71998ad67b26e91aaff6198af903ec88dd035039
Rolf vandeVaart <rvandevaart@nvidia.com>
2014-05-13 18:55:41 +0000

    Change one argument to use const.  Matches better how function is called.
    This commit was SVN r31741.

--- opal/datatype/opal_datatype_cuda.h
+++ opal/datatype/opal_datatype_cuda.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013 NVIDIA Corporation.  All rights reserved.
+ * Copyright (c) 2011-2014 NVIDIA Corporation.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -24,7 +24,7 @@ typedef struct opal_common_cuda_function_table opal_common_cuda_function_table_t
 void mca_cuda_convertor_init(opal_convertor_t* convertor, const void *pUserBuf);
 bool opal_cuda_check_bufs(char *dest, char *src);
 void* opal_cuda_memcpy(void * dest, const void * src, size_t size, opal_convertor_t* convertor);
-void* opal_cuda_memcpy_sync(void * dest, void * src, size_t size);
+void* opal_cuda_memcpy_sync(void * dest, const void * src, size_t size);
 void* opal_cuda_memmove(void * dest, void * src, size_t size);
 void opal_cuda_add_initialization_function(int (*fptr)(opal_common_cuda_function_table_t *));
 void opal_cuda_set_copy_function_async(opal_convertor_t* convertor, void *stream);
