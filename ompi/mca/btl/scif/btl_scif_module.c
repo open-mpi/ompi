f32de8c0235fddc02860b7dfb10ca287bf2656c5
Gilles Gouaillardet <ggouaillardet@open-mpi-git-mirror.example.com>
2014-05-15 09:55:50 +0000

    btl/scif: fix hang at shutdown
    a brand new dummy connection has to be used to properly
    trigger the main thread termination and avoid timeout in
    the main thread
    
    cmr=v1.8.2:reviewer=hjelmn

--- ompi/mca/btl/scif/btl_scif_module.c
+++ ompi/mca/btl/scif/btl_scif_module.c
@@ -128,9 +128,12 @@ mca_btl_scif_module_finalize (struct mca_btl_base_module_t *btl)
     /* close the listening endpoint */
     if (-1 != mca_btl_scif_module.scif_fd) {
         /* wake up the scif thread */
-        scif_connect (mca_btl_scif_module.scif_fd, &mca_btl_scif_module.port_id);
-        scif_close (mca_btl_scif_module.scif_fd);
+        scif_epd_t tmpfd;
+        tmpfd = scif_open();
+        scif_connect (tmpfd, &mca_btl_scif_module.port_id);
         pthread_join(mca_btl_scif_module.listen_thread, NULL);
+        scif_close(tmpfd);
+        scif_close (mca_btl_scif_module.scif_fd);
     }
 
     mca_btl_scif_module.scif_fd = -1;
