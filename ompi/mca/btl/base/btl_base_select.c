127274cc935e9f7c1f4242a2af0a44468835076a
Ralph Castain <rhc@open-mpi.org>
2014-05-16 13:35:15 +0000

    Fixes trac:4640: Move r31779 to v1.8 branch (More leak fixes.)
    ---svn-pre-commit-ignore-below---
    
    r31779 [[BR]]
    More leak fixes.
    
    Two leaks are fixed in this commit:
    
     - Do not leak btl component list items.
    
     - Do not leak the nodename when decoding the pidmap.
    
    cmr=v1.8.2:reviewer=rhc
    
    This commit was SVN r31797.
    
    The following SVN revision numbers were found above:
      r31779 --> open-mpi/ompi@73bfecd650399ab0a426a4060cebf888811a997b
    
    The following Trac tickets were found above:
      Ticket 4640 --> https://svn.open-mpi.org/trac/ompi/ticket/4640

--- ompi/mca/btl/base/btl_base_select.c
+++ ompi/mca/btl/base/btl_base_select.c
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
@@ -10,8 +11,10 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2007      Sun Microsystems, Inc.  All rights reserved.
- * Copyright (c) 2012      Los Alamos National Security, LLC.
+ * Copyright (c) 2012-2014 Los Alamos National Security, LLC.
  *                         All rights reserved.
+ * Copyright (c) 2014      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -117,6 +120,7 @@ int mca_btl_base_select(bool enable_progress_threads,
                                     component->btl_version.mca_component_name);
 
                 opal_list_remove_item(&ompi_btl_base_framework.framework_components, &cli->super);
+                OBJ_RELEASE(cli);
                 mca_base_component_close((mca_base_component_t *) component,
                                          ompi_btl_base_framework.framework_output);
             } 
--- orte/util/nidmap.c
+++ orte/util/nidmap.c
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
@@ -9,7 +10,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
- * Copyright (c) 2012-2013 Los Alamos National Security, LLC.
+ * Copyright (c) 2012-2014 Los Alamos National Security, LLC.
  *                         All rights reserved.
  * Copyright (c) 2013      Intel, Inc. All rights reserved
  *
@@ -552,6 +553,7 @@ int orte_util_decode_nodemap(opal_byte_object_t *bo)
                 orte_process_info.my_hostid = hostid;
             }
         }
+        free (nodename);
     }
     if (ORTE_ERR_UNPACK_READ_PAST_END_OF_BUFFER != rc) {
         ORTE_ERROR_LOG(rc);
