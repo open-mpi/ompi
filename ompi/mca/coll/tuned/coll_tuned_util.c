542e4996a7023531095b9904c6f0993190b1f29b
George Bosilca <bosilca@icl.utk.edu>
2014-06-13 16:04:45 +0000

    Cleanup the utilities functions in tuned.
    This commit was SVN r31987.

--- ompi/mca/coll/tuned/coll_tuned_util.c
+++ ompi/mca/coll/tuned/coll_tuned_util.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2009 The University of Tennessee and The University
+ * Copyright (c) 2004-2014 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
@@ -28,69 +28,6 @@
 #include "ompi/mca/coll/base/coll_tags.h"
 #include "ompi/mca/pml/pml.h"
 #include "coll_tuned_util.h"
-
-int ompi_coll_tuned_sendrecv_actual( void* sendbuf, size_t scount, 
-                                     ompi_datatype_t* sdatatype,
-                                     int dest, int stag,
-                                     void* recvbuf, size_t rcount, 
-                                     ompi_datatype_t* rdatatype,
-                                     int source, int rtag,
-                                     struct ompi_communicator_t* comm,
-                                     ompi_status_public_t* status )
-
-{ /* post receive first, then send, then waitall... should be fast (I hope) */
-    int err, line = 0;
-    ompi_request_t* reqs[2];
-    ompi_status_public_t statuses[2];
-
-    /* post new irecv */
-    err = MCA_PML_CALL(irecv( recvbuf, rcount, rdatatype, source, rtag, 
-                              comm, &reqs[0]));
-    if (err != MPI_SUCCESS) { line = __LINE__; goto error_handler; }
-
-    /* send data to children */
-    err = MCA_PML_CALL(isend( sendbuf, scount, sdatatype, dest, stag, 
-                              MCA_PML_BASE_SEND_STANDARD, comm, &reqs[1]));
-    if (err != MPI_SUCCESS) { line = __LINE__; goto error_handler; }
-
-    err = ompi_request_wait_all( 2, reqs, statuses );
-    if (err != MPI_SUCCESS) { line = __LINE__; goto error_handler; }
-
-    if (MPI_STATUS_IGNORE != status) {
-        *status = statuses[0];
-    }
-    
-    return (MPI_SUCCESS);
-
- error_handler:
-    /* As we use wait_all we will get MPI_ERR_IN_STATUS which is not an error
-     * code that we can propagate up the stack. Instead, look for the real
-     * error code from the MPI_ERROR in the status.
-     */
-    if( MPI_ERR_IN_STATUS == err ) {
-        /* At least we know the error was detected during the wait_all */
-        int err_index = 0;
-        if( MPI_SUCCESS == statuses[0].MPI_ERROR ) {
-            err_index = 1;
-        }
-        if (MPI_STATUS_IGNORE != status) {
-            *status = statuses[err_index];
-        }
-        err = statuses[err_index].MPI_ERROR;
-        OPAL_OUTPUT ((ompi_coll_tuned_stream, "%s:%d: Error %d occurred (req index %d)\n",
-                      __FILE__, line, err, err_index));
-    } else {
-        /* Error discovered during the posting of the irecv or isend,
-         * and no status is available.
-         */
-        OPAL_OUTPUT ((ompi_coll_tuned_stream, "%s:%d: Error %d occurred\n",
-                      __FILE__, line, err));
-        if (MPI_STATUS_IGNORE != status) {
-            status->MPI_ERROR = err;
-        }
-    }
-    return (err);
-}
 
 int ompi_coll_tuned_sendrecv_nonzero_actual( void* sendbuf, size_t scount, 
                                              ompi_datatype_t* sdatatype,
@@ -133,10 +70,10 @@ int ompi_coll_tuned_sendrecv_nonzero_actual( void* sendbuf, size_t scount,
             *status = statuses[0];
         }
     } else {
-        /* FIXME this is currently unsupported but unused */
-        assert (MPI_STATUS_IGNORE == status);
+        if( MPI_STATUS_IGNORE != status )
+            *status = ompi_status_empty;
     }
-    
+
     return (MPI_SUCCESS);
 
  error_handler:
@@ -146,16 +83,17 @@ int ompi_coll_tuned_sendrecv_nonzero_actual( void* sendbuf, size_t scount,
      */
     if( MPI_ERR_IN_STATUS == err ) {
         /* At least we know the error was detected during the wait_all */
-        int err_index = 0;
+        int err_index = 1;
         if( MPI_SUCCESS == statuses[0].MPI_ERROR ) {
-            err_index = 1;
+            err_index = 0;
         }
         if (MPI_STATUS_IGNORE != status) {
             *status = statuses[err_index];
         }
         err = statuses[err_index].MPI_ERROR;
-        OPAL_OUTPUT ((ompi_coll_tuned_stream, "%s:%d: Error %d occurred (req index %d)\n",
-                      __FILE__, line, err, err_index));
+        OPAL_OUTPUT ((ompi_coll_tuned_stream, "%s:%d: Error %d occurred in the %s"
+                                              " stage of ompi_coll_tuned_sendrecv_zero\n",
+                      __FILE__, line, err, (0 == err_index ? "receive" : "send")));
     } else {
         /* Error discovered during the posting of the irecv or isend,
          * and no status is available.
@@ -166,75 +104,6 @@ int ompi_coll_tuned_sendrecv_nonzero_actual( void* sendbuf, size_t scount,
             status->MPI_ERROR = err;
         }
     }
-    return (err);
-}
-/*
- * localcompleted version that makes sure the send has completed locally 
- * Currently this is a sync call, but will change to locally completed
- * version when available
- */
-
-int ompi_coll_tuned_sendrecv_actual_localcompleted( void* sendbuf, size_t scount, 
-                                                    ompi_datatype_t* sdatatype, 
-                                                    int dest, int stag,
-                                                    void* recvbuf, size_t rcount, 
-                                                    ompi_datatype_t* rdatatype, 
-                                                    int source, int rtag, 
-                                                    struct ompi_communicator_t* comm, 
-                                                    ompi_status_public_t* status )
-
-{ /* post receive first, then [local] sync send, then wait... should be fast (I hope) */
-    int err, line = 0;
-    ompi_request_t* req[2];
-    ompi_status_public_t statuses[2];
-
-    /* post new irecv */
-    err = MCA_PML_CALL(irecv( recvbuf, rcount, rdatatype, source, rtag, 
-                              comm, &(req[0])));
-    if (err != MPI_SUCCESS) { line = __LINE__; goto error_handler; }
-
-    /* send data to children */
-    err = MCA_PML_CALL(isend( sendbuf, scount, sdatatype, dest, stag,
-                              MCA_PML_BASE_SEND_SYNCHRONOUS, comm, &(req[1])));
-    if (err != MPI_SUCCESS) { line = __LINE__; goto error_handler; }
-
-    err = ompi_request_wait_all( 2, req, statuses );
-    if (err != MPI_SUCCESS) { line = __LINE__; goto error_handler; }
-
-    if (MPI_STATUS_IGNORE != status) {
-        *status = statuses[0];
-    }
-
-    return (MPI_SUCCESS);
-
- error_handler:
-    /* As we use wait_all we will get MPI_ERR_IN_STATUS which is not an error
-     * code that we can propagate up the stack. Instead, look for the real
-     * error code from the MPI_ERROR in the status.
-     */
-    if( MPI_ERR_IN_STATUS == err ) { 
-        /* At least we know the error was detected during the wait_all */
-        int err_index = 0;
-        if( MPI_SUCCESS == statuses[0].MPI_ERROR ) {
-            err_index = 1;
-        }
-        if (MPI_STATUS_IGNORE != status) {
-            *status = statuses[err_index];
-        }
-        err = statuses[err_index].MPI_ERROR;
-        OPAL_OUTPUT ((ompi_coll_tuned_stream, "%s:%d: Error %d occurred (req index %d)\n",
-                      __FILE__,line,err, err_index));
-    } else {
-        /* Error discovered during the posting of the irecv or isend,
-         * and no status is available.
-         */
-        OPAL_OUTPUT ((ompi_coll_tuned_stream, "%s:%d: Error %d occurred\n",
-                      __FILE__, line, err));
-        if (MPI_STATUS_IGNORE != status) {
-            status->MPI_ERROR = err;
-        }
-    }
-
     return (err);
 }
 
