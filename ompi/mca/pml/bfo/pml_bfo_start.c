14b6401ccaa1c3443a13d222008077e05b009123
Gilles Gouaillardet <ggouaillardet@open-mpi-git-mirror.example.com>
2014-07-04 04:58:52 +0000

    Fix the cornercase with MPI_PROC_NULL persistent requests.
    This corner case is now handled in the pml so the same code
    is invoked for both MPI_Start and MPI_Startall.
    This also correctly report an error if MPI_Startall is invoked twice
    on a MPI_PROC_NULL persistent request.

--- ompi/mca/pml/bfo/pml_bfo_start.c
+++ ompi/mca/pml/bfo/pml_bfo_start.c
@@ -11,6 +11,8 @@
  *                         All rights reserved.
  * Copyright (c) 2006      Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2010      Oracle and/or its affiliates.  All rights reserved.
+ * Copyright (c) 2014      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -36,6 +38,20 @@ int mca_pml_bfo_start(size_t count, ompi_request_t** requests)
         mca_pml_base_request_t *pml_request = (mca_pml_base_request_t*)requests[i];
         if(NULL == pml_request) {
             continue;
+        }
+        if (OMPI_REQUEST_NOOP == requests[i]->req_type) {
+            /**
+             * We deal with a MPI_PROC_NULL request. If the request is
+             * already active, return OMPI_ERR_REQUEST err.
+             * Otherwise, mark it active so we can correctly handle it in
+             * the wait*.
+             */
+            if( OMPI_REQUEST_INACTIVE == requests[i]->req_state ) {
+                requests[i]->req_state = OMPI_REQUEST_ACTIVE;
+                continue;
+            } else {
+                return OMPI_ERR_REQUEST;
+            }
         }
         if (OMPI_REQUEST_PML != requests[i]->req_type) {
             continue;
