45b2c500a0e6a4a02d10d50692e617b1cf462318
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2014-07-10 19:10:03 +0000

    Fortran: put type(c_ptr) interfaces in a separate file in the TKR mpi module
    Older gfortran compilers (e.g., the gfortran that ships in RHEL5) do
    not support ISO_C_BINDING, and therefore do not support the
    TYPE(C_PTR) type.  As such, they cannot support the overloaded
    interfaces for MPI_WIN_ALLOCATE_SHARED and MPI_SHARED_QUERY that are
    mandated in MPI-3.
    
    So we separate those interfaces out into a separate .F90 file that is
    #include'd in the tkr mpi.F90 file.  In this separate .F90 file, we
    use an #if to determine whether the compiler supports ISO_C_BINDING or
    not.
    
    Also re-jiggered the order of testing in ompi_setup_mpi_fortran.m4: we
    now need to test whether the compiler supports ISO_C_BINDING even when
    we're only building the mpi module (not strictly when we're building
    the mpi_f08 module).
    
    Finally, tweaked the use-mpi-tkr/Makefile.am to:
    
    * Add some proper dependencies for mpi.F90
    * Allow the general AM compilation to be used instead of supplying a
      specific rule for compiling mpi.F90
    
    cmr=v1.8.2:ticket=#4736

--- ompi/mpi/fortran/use-mpi-tkr/mpi.F90
+++ ompi/mpi/fortran/use-mpi-tkr/mpi.F90
@@ -39,6 +39,12 @@ module mpi
 
   include "ompi/mpi/fortran/base/conversion-fn-null-int-interface.h"
 
+! Functions that have overloaded interfaces with TYPE(C_PTR) (which
+! this compiler may or may not support).  We use an "if" preprocessor
+! macro in this file, so we need to use the preprocessor include
+! directive, not the Fortran include.
+#include "mpi-f90-cptr-interfaces.F90"
+
 ! This file is generated, and is *huge*.  Its size is directly related
 ! to the --with-f90-max-array-dim configure parameter.
 
