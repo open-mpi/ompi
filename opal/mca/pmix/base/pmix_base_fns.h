b19aa33a7ca87f3535412707b4b189d506376f21
Elena <elena.elkina@itseez.com>
2014-07-17 18:05:23 +0300

    draft for pmix s1 put/get/fence

--- opal/mca/pmix/base/pmix_base_fns.h
+++ opal/mca/pmix/base/pmix_base_fns.h
@@ -1,7 +1,8 @@
 #include "opal/util/error.h"
 #include "opal/dss/dss_types.h"
-int pmi_commit_packed(opal_identifier_t* proc, int pmix_vallen_max, char* pmix_kvs_name);
-int pmi_store_encoded(opal_identifier_t* id, const char *key, const void *data,
-        opal_data_type_t type);
-int pmi_get_packed(opal_identifier_t* proc,
-        char **packed_data, size_t *len, int pmix_keylen_max);
+int pmi_store_encoded(const char *key, const void *data,
+        opal_data_type_t type, char** buffer, int* length);
+char* setup_key(opal_identifier_t* name, const char *key, int pmix_keylen_max);
+char *pmi_encode(const void *val, size_t vallen);
+uint8_t *pmi_decode (const char *data, size_t *retlen);
+
