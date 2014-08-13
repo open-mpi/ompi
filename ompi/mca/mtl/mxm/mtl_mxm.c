ee27eb939e6e69bdbd2e4a139e48ab24244953aa
Elena <elena.elkina@itseez.com>
2014-08-13 13:55:20 +0300

    fix for mtl mxm from trunk

--- ompi/mca/mtl/mxm/mtl_mxm.c
+++ ompi/mca/mtl/mxm/mtl_mxm.c
@@ -506,9 +506,12 @@ int ompi_mtl_mxm_del_procs(struct mca_mtl_base_module_t *mtl, size_t nprocs,
     size_t i;
 
 #if MXM_API >= MXM_VERSION(3,1)
-    if (ompi_mtl_mxm.bulk_disconnect &&
-            nprocs == ompi_comm_size(&ompi_mpi_comm_world.comm)) {
-        mxm_ep_powerdown(ompi_mtl_mxm.ep);
+    if (ompi_mtl_mxm.bulk_disconnect) {
+        size_t nprocs_world;
+        ompi_proc_world(&nprocs_world);
+        if (nprocs == nprocs_world) {
+            mxm_ep_powerdown(ompi_mtl_mxm.ep);
+        }
     }
 #endif
 
