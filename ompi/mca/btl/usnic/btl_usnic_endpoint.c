e25a424629c07c154bdf60235d3bc792371b4187
Ralph Castain <rhc@open-mpi.org>
2014-08-20 21:04:44 +0000

    Fixes trac:4866: Move r32546 to v1.8 branch (usnic: ensure to have a)
    Hand-edited to port from trunk
    
    ---svn-pre-commit-ignore-below---
    
    r32546 [[BR]]
    usnic: ensure to have a safe destruction of an opal_list_item_t
    
    It turns out that we ''can'' get to the endpoint destructor with the
    endpoint still on the "endpoints needing ACKs" list.  So if it's on
    the list, remove it first, and then DESTRUCT the opal_list_item_t.
    
    This prevents an assert() fail in debug builds.  We'd like to let this
    soak over the weekend.
    
    cmr=v1.8.2:reviewer=dgoodell
    
    This commit was SVN r32560.
    
    The following SVN revision numbers were found above:
      r32546 --> open-mpi/ompi@ac7c907f8d0fc79f15fedcf9fabc41d29255618a
    
    The following Trac tickets were found above:
      Ticket 4866 --> https://svn.open-mpi.org/trac/ompi/ticket/4866

--- ompi/mca/btl/usnic/btl_usnic_endpoint.c
+++ ompi/mca/btl/usnic/btl_usnic_endpoint.c
@@ -113,9 +113,9 @@ static void endpoint_destruct(mca_btl_base_endpoint_t* endpoint)
     int rc;
     ompi_btl_usnic_proc_t *proc;
 
-    /* We should not get here for an endpoint that is on the ACK list,
-       so it should be safe to unconditionally destruct the ack_li */
-    OBJ_DESTRUCT(&(endpoint->endpoint_ack_li));
+    if (endpoint->endpoint_ack_needed) { 
+        ompi_btl_usnic_remove_from_endpoints_needing_ack(endpoint); 
+    } 
 
     /* Remove the endpoint from the all_endpoints list */
     ompi_btl_usnic_module_t *module = endpoint->endpoint_module;
