f865ef61ab415dfbf7593c42be12dc4f3e7bd6c5
Ralph Castain <rhc@open-mpi.org>
2014-08-29 22:23:27 +0000

    Need local_size returned by the Slurm components
    This commit was SVN r32646.

--- opal/mca/pmix/s2/pmix_s2.c
+++ opal/mca/pmix/s2/pmix_s2.c
@@ -565,6 +565,15 @@ static bool s2_get_attr(const char *attr, opal_value_t **kv)
         return true;
     }
 
+    if (0 == strcmp(PMIX_LOCAL_SIZE, attr)) {
+        kp = OBJ_NEW(opal_value_t);
+        kp->key = strdup(attr);
+        kp->type = OPAL_UINT32;
+        kp->data.uint32 = s2_nlranks;
+        *kv = kp;
+        return true;
+    }
+
     if (0 == strcmp(PMIX_APPNUM, attr)) {
         kp = OBJ_NEW(opal_value_t);
         kp->key = strdup(attr);
