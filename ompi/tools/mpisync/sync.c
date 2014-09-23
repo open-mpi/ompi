f2e586980b5df8e302cfa6aef6dd49d415b18b25
Artem Polyakov <artpol84@gmail.com>
2014-09-23 12:59:54 +0000

    Fix timing framework: 1. Fixes according to (http://www.open-mpi.org/community/lists/devel/2014/09/15869.php) 2. Force mpisync:rank0 to gather results. Now sync info is written by rank0 to the output file. 3. Improve mpirun_prof: 1) adopt to the environment (SLURM/TORQUE); 2) recognize some noteset-related mpirun options.
    This commit was SVN r32772.

--- ompi/tools/mpisync/sync.c
+++ ompi/tools/mpisync/sync.c
@@ -23,8 +23,6 @@ typedef enum { Gen, Chk } prog_mode_t;
 
 char *filename = NULL;
 prog_mode_t mode = Gen;
-double orig_rtt = 0.0, orig_offs = 0.0;
-
 void print_help(char *progname);
 int parse_opts(int rank, int argc, char **argv);
 
@@ -70,14 +68,13 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     MPI_Comm comm = MPI_COMM_WORLD;
     int rank, commsize;
-    double offs, rtt;
+    double offs = 0, rtt = 0;
     char hname[1024];
 
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &commsize);
 
     int ret = parse_opts(rank, argc, argv);
-    
     if( ret < 0 ){
         // Error exit
         MPI_Finalize();
@@ -90,49 +87,58 @@ int main(int argc, char **argv)
 
     if( filename == NULL ){
         if( rank == 0 ){
-            fprintf(stderr, "The name of output file wasn't specified. Abort\n");
+            print_help(argv[0]);
         }
         MPI_Finalize();
         exit(1);
     }
-
 
     if( gethostname(hname, 1024) ){
-        perror("Cannot get hostname");
-        MPI_Finalize();
-        exit(1);
-    }
-
-    // Clear output file if it exists
-    if( rank == 0 ){
-        FILE  *fp = fopen(filename, "w");
-        if( fp == NULL ){
-            fprintf(stderr,"Cannot open output file %s for writing. Abort: %s\n",
-                     filename, strerror(errno));
-            MPI_Finalize();
-            exit(1);
-        }
-        fclose(fp);
+        perror("Cannot get hostname. Abort");
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     int rc = hpctimer_initialize("gettimeofday");
 
     if( rc == HPCTIMER_FAILURE ){
         fprintf(stderr, "Fail to initialize hpc timer. Abort\n");
-        MPI_Finalize();
-        exit(1);
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     offs = mpigclock_sync_linear(comm, 0, &rtt);
 
-    FILE *fp = fopen(filename,"a");
-    if( fp == NULL ){
-        fprintf(stderr, "Cannot open %s for appending. Abort\n", filename);
-        MPI_Finalize();
-        exit(1);
+    double send[2] = { rtt, offs };
+    if( rank == 0 ){
+        double *measure = malloc(commsize*2*sizeof(double));
+        if( measure == NULL ){
+             fprintf(stderr, "Fail to allocate memory. Abort\n");
+             MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        char *hnames = malloc(1024*commsize);
+        if( hnames == NULL ){
+             fprintf(stderr, "Fail to allocate memory. Abort\n");
+             MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        MPI_Gather(hname,1024,MPI_CHAR,hnames,1024,MPI_CHAR, 0, MPI_COMM_WORLD);
+        MPI_Gather(send,2,MPI_DOUBLE,measure,2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        char tmpname[128];
+        FILE *fp = fopen(filename,"w");
+        if( fp == NULL ){
+             fprintf(stderr, "Fail to open the file %s. Abort\n", filename);
+             MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        double (*m)[2] = (void*)measure;
+        char (*h)[1024] = (void*)hnames;
+        int i;
+        for(i=0; i<commsize;i++){
+            fprintf(fp, "%s %lf %lf\n", h[i], m[i][0], m[i][1]);
+        }
+        fclose(fp);
+    } else {
+        MPI_Gather(hname,1024, MPI_CHAR, NULL, 1024, MPI_CHAR, 0, MPI_COMM_WORLD);
+        MPI_Gather(send,2, MPI_DOUBLE, NULL, 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
-    fprintf(fp, "%s %lf %lf\n", hname, rtt, offs);
-    fclose(fp);
 
     MPI_Finalize();
     return 0;
