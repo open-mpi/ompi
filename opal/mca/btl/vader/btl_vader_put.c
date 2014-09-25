0c5b91e4147e0fabac7fd9b07b9a7f197a36c6ed
George Bosilca <bosilca@open-mpi-git-mirror.example.com>
2014-09-25 17:18:27 +0000

    Fix typo.

--- opal/mca/btl/vader/btl_vader_put.c
+++ opal/mca/btl/vader/btl_vader_put.c
@@ -74,7 +74,7 @@ int mca_btl_vader_put (struct mca_btl_base_module_t *btl,
     struct iovec dst_iov = {.iov_base = dst->seg_addr.pval, .iov_len = size};
     ssize_t ret;
 
-    ret = process_vm_writev (endpoint->seg_ds.seg_cpid, &src_iov, 1, &dst_iov, 1, 0);
+    ret = process_vm_writev (endpoint->seg_ds->seg_cpid, &src_iov, 1, &dst_iov, 1, 0);
     if (ret != (ssize_t)size) {
         opal_output(0, "Wrote %ld, expected %lu, errno = %d\n", (long)ret, (unsigned long)size, errno);
         return OPAL_ERROR;
