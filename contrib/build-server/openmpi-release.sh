6323389dfac50fbed5744e28316379d4d05ce4c9
Jeff Squyres <jsquyres@cisco.com>
2014-10-02 11:26:44 -0700

    build server: moved scripts to contrib/build-server
    Removed a redundant copy of the scripts running on the build server
    and moved the remaining copy out to a top-level directory in contrib
    (i.e., contrib/build-server vs. contrib/dist/build-server, where I
    never could remember where to find them).
    
    (cherry picked from commit eb270172a7131760f79f87e76f43026111a60905)

--- contrib/build-server/openmpi-release.sh
+++ contrib/build-server/openmpi-release.sh
@@ -1,14 +1,10 @@
 #!/bin/sh -x
 
 # The tarballs to make
-#dirs="/branches/v1.2 /branches/v1.1 /branches/v1.0 /trunk"
-
-# The tarballs to make
 if [ $# -eq 0 ] ; then
-  dirs="branches/v1.6"
+    dirs="branches/v1.8"
 else
-  dirs=$1
-  shift
+    dirs=$@
 fi
 
 # Build root - scratch space
@@ -27,7 +23,6 @@ export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
 #####
 
 # load the modules configuration
-#JJH File No longer exists: . /etc/profile.d/00-modules.sh
 . /etc/profile.d/modules.sh
 module use ~/modules
 module load sowing
