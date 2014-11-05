open-mpi/ompi

commit 1a5ff6df43562de119c868bca29470823b1595ec

Fixes trac:3687: Merge RTE framework into v1.7
Consists of patch spanning many revision numbers that are not listed

This commit was SVN r28906.

The following Trac tickets were found above:
  Ticket 3687 --> https://svn.open-mpi.org/trac/ompi/ticket/3687

ompi/mca/mtl/mxm/mtl_mxm.c
@@ -10,14 +10,10 @@
 
 #include "ompi_config.h"
 
-#include "orte/util/show_help.h"
-#include "orte/util/proc_info.h"
 #include "ompi/mca/mtl/mtl.h"
 #include "ompi/runtime/ompi_module_exchange.h"
 #include "ompi/mca/mtl/base/mtl_base_datatype.h"
 #include "ompi/proc/proc.h"
-#include "orte/mca/ess/ess.h"
-#include "orte/runtime/orte_globals.h"
 #include "ompi/communicator/communicator.h"
 #include "opal/memoryhooks/memory.h"
 
@@ -67,9 +63,9 @@ static uint32_t ompi_mtl_mxm_get_job_id(void)
     memset(uu, 0, sizeof(unique_job_key));
 
     if (!generated_key || (strlen(generated_key) != 33) || sscanf(generated_key, "%016llx-%016llx", &uu[0], &uu[1]) != 2) {
-        orte_show_help("help-mtl-mxm.txt", "no uuid present", true,
+        opal_show_help("help-mtl-mxm.txt", "no uuid present", true,
                        generated_key ? "could not be parsed from" :
-                                       "not present in", orte_process_info.nodename);
+                                       "not present in", ompi_process_info.nodename);
         return 0;
     }
 
@@ -102,7 +98,7 @@ static int ompi_mtl_mxm_get_ep_address(ompi_mtl_mxm_ep_conn_info_t *ep_info, mxm
     err = mxm_ep_address(ompi_mtl_mxm.ep, ptlid,
                          (struct sockaddr *) &ep_info->ptl_addr[ptlid], &addrlen);
     if (MXM_OK != err) {
-        orte_show_help("help-mtl-mxm.txt", "unable to extract endpoint ptl address",
+        opal_show_help("help-mtl-mxm.txt", "unable to extract endpoint address",
                        true, (int)ptlid, mxm_error_string(err));
         return OMPI_ERROR;
     }
@@ -129,7 +125,7 @@ static int ompi_mtl_mxm_get_ep_address(void **address_p, size_t *address_len_p)
 
     err = mxm_ep_get_address(ompi_mtl_mxm.ep, *address_p, address_len_p);
     if (MXM_OK != err) {
-        orte_show_help("help-mtl-mxm.txt", "unable to extract endpoint address",
+        opal_show_help("help-mtl-mxm.txt", "unable to extract endpoint address",
                        true, mxm_error_string(err));
         return OMPI_ERROR;
     }
@@ -357,11 +353,11 @@ int ompi_mtl_mxm_module_init(void)
     }
     MXM_VERBOSE(1, "MXM support enabled");
 
-    if (ORTE_NODE_RANK_INVALID == (lr = orte_process_info.my_node_rank)) {
+    if (ORTE_NODE_RANK_INVALID == (lr = ompi_process_info.my_node_rank)) {
         MXM_ERROR("Unable to obtain local node rank");
         return OMPI_ERROR;
     }
-    nlps = orte_process_info.num_local_peers + 1;
+    nlps = ompi_process_info.num_local_peers + 1;
 
     for (proc = 0; proc < totps; proc++) {
         if (OPAL_PROC_ON_LOCAL_NODE(procs[proc]->proc_flags)) {
@@ -382,7 +378,7 @@ int ompi_mtl_mxm_module_init(void)
     err = ompi_mtl_mxm_create_ep(ompi_mtl_mxm.mxm_context, &ompi_mtl_mxm.ep,
                                  ptl_bitmap, lr, jobid, mxlr, nlps);
     if (MXM_OK != err) {
-        orte_show_help("help-mtl-mxm.txt", "unable to create endpoint", true,
+        opal_show_help("help-mtl-mxm.txt", "unable to create endpoint", true,
         		mxm_error_string(err));
         return OMPI_ERROR;
     }
@@ -561,7 +557,7 @@ int ompi_mtl_mxm_add_comm(struct mca_mtl_base_module_t *mtl,
 
     err = mxm_mq_create(ompi_mtl_mxm.mxm_context, comm->c_contextid, &mq);
     if (MXM_OK != err) {
-        orte_show_help("help-mtl-mxm.txt", "mxm mq create", true, mxm_error_string(err));
+        opal_show_help("help-mtl-mxm.txt", "mxm mq create", true, mxm_error_string(err));
         return OMPI_ERROR;
     }
 
@@ -585,7 +581,7 @@ int ompi_mtl_mxm_progress(void)
 
     err = mxm_progress(ompi_mtl_mxm.mxm_context);
     if ((MXM_OK != err) && (MXM_ERR_NO_PROGRESS != err) ) {
-        orte_show_help("help-mtl-mxm.txt", "errors during mxm_progress", true, mxm_error_string(err));
+        opal_show_help("help-mtl-mxm.txt", "errors during mxm_progress", true, mxm_error_string(err));
     }
     return 1;
 }

commit ba9e77c17bd9c40638f38ab95b05676dbea8e6a5

mtl mxm: fix typo in help topic name
Fixes typo introduced in 1a5ff6df43562de119c868bca29470823b1595ec

ompi/mca/mtl/mxm/mtl_mxm.c
@@ -127,7 +127,7 @@ static int ompi_mtl_mxm_get_ep_address(void **address_p, size_t *address_len_p)
 
     err = mxm_ep_get_address(ompi_mtl_mxm.ep, *address_p, address_len_p);
     if (MXM_OK != err) {
-        opal_show_help("help-mtl-mxm.txt", "unable to extract endpoint address",
+        opal_show_help("help-mtl-mxm.txt", "unable to extract endpoint ptl address",
                        true, mxm_error_string(err));
         return OMPI_ERROR;
     }