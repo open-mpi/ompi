ff362c16ce88cedd974528d75c509c677372e46f
Howard Pritchard <hppritcha@gmail.com>
2014-11-18 10:16:11 -0700

    add/update copyrights for alps odls component

--- orte/mca/odls/alps/odls_alps_module.c
+++ orte/mca/odls/alps/odls_alps_module.c
@@ -13,7 +13,7 @@
  * Copyright (c) 2007      Evergrid, Inc. All rights reserved.
  * Copyright (c) 2008-2013 Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2010      IBM Corporation.  All rights reserved.
- * Copyright (c) 2011-2013 Los Alamos National Security, LLC.  All rights
+ * Copyright (c) 2011-2014 Los Alamos National Security, LLC.  All rights
  *                         reserved. 
  * Copyright (c) 2013-2014 Intel, Inc. All rights reserved
  *
