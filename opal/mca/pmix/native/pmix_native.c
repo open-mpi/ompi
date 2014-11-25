open-mpi/ompi

commit a6744b81777ab8247908350bd15cca49bedf5208

fix misc memory leaks specific to the master

orte/mca/grpcomm/base/grpcomm_base_stubs.c
@@ -12,6 +12,8 @@
  *                         All rights reserved.
  * Copyright (c) 2011-2012 Los Alamos National Security, LLC.
  *                         All rights reserved.
+ * Copyright (c) 2014      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -68,9 +70,18 @@ static void gccon(orte_grpcomm_caddy_t *p)
     p->cbfunc = NULL;
     p->cbdata = NULL;
 }
+static void gcdes(orte_grpcomm_caddy_t *p)
+{
+    if (NULL != p->sig) {
+        OBJ_RELEASE(p->sig);
+    }
+    p->buf = NULL;
+    p->cbfunc = NULL;
+    p->cbdata = NULL;
+}
 static OBJ_CLASS_INSTANCE(orte_grpcomm_caddy_t,
                           opal_object_t,
-                          gccon, NULL);
+                          gccon, gcdes);
 
 int orte_grpcomm_API_xcast(orte_grpcomm_signature_t *sig,
                            orte_rml_tag_t tag,

opal/class/opal_hash_table.c
@@ -891,6 +891,7 @@ int opal_proc_table_remove_all(opal_proc_table_t *pt) {
         do {
             if (NULL != vpids) {
                 opal_hash_table_remove_all(vpids);
+                OBJ_RELEASE(vpids);
             }
             rc = opal_hash_table_get_next_key_uint32 (&pt->super, &jobid,
                                                (void **) &vpids, node, &node);
@@ -940,7 +941,12 @@ int opal_proc_table_remove_value(opal_proc_table_t* pt, opal_process_name_t key)
     if (OPAL_SUCCESS != (rc=opal_hash_table_get_value_uint32(&pt->super, key.jobid, (void **)&vpids))) {
         return rc;
     }
-    rc = opal_hash_table_remove_value_uint32(vpids, key.vpid);
+    if (OPAL_SUCCESS == (rc=opal_hash_table_remove_value_uint32(vpids, key.vpid))) {
+        if (0 == vpids->ht_size) {
+            opal_hash_table_remove_value_uint32(&pt->super, key.jobid);
+            OBJ_RELEASE(vpids);
+        }
+    }
     return rc;
 }
 

opal/mca/btl/tcp/btl_tcp_proc.c
@@ -91,6 +91,9 @@ void mca_btl_tcp_proc_destruct(mca_btl_tcp_proc_t* tcp_proc)
     if(NULL != tcp_proc->proc_endpoints) {
         free(tcp_proc->proc_endpoints);
     }
+    if(NULL != tcp_proc->proc_addrs) {
+        free(tcp_proc->proc_addrs);
+    }
     OBJ_DESTRUCT(&tcp_proc->proc_lock);
 }
 

opal/mca/dstore/base/base.h
@@ -120,7 +120,7 @@ OPAL_DECLSPEC int opal_dstore_base_get_handle(int dstorehandle, void **dhdl);
 
 /* support */
 OPAL_DECLSPEC opal_dstore_proc_data_t* opal_dstore_base_lookup_proc(opal_proc_table_t *jtable,
-                                                                    opal_process_name_t id);
+                                                                    opal_process_name_t id, bool create);
 
 OPAL_DECLSPEC opal_value_t* opal_dstore_base_lookup_keyval(opal_dstore_proc_data_t *proc_data,
                                                            const char *key);

opal/mca/dstore/base/dstore_base_frame.c
@@ -2,6 +2,8 @@
  * Copyright (c) 2010      Cisco Systems, Inc.  All rights reserved. 
  * Copyright (c) 2012-2013 Los Alamos National Security, Inc.  All rights reserved. 
  * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * Copyright (c) 2014      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -131,8 +133,19 @@ OBJ_CLASS_INSTANCE(opal_dstore_proc_data_t,
                    proc_data_construct,
                    proc_data_destruct);
 
+static void attr_construct(opal_dstore_attr_t *attr)
+{
+    attr->connection_info = NULL;
+}
+
+static void attr_destruct(opal_dstore_attr_t *attr)
+{
+    if (NULL != attr->connection_info) {
+        free(attr->connection_info);
+    }
+}
 OBJ_CLASS_INSTANCE(opal_dstore_attr_t,
                    opal_list_item_t,
-                   NULL, NULL);
+                   attr_construct, attr_destruct);
 
 

opal/mca/dstore/base/dstore_base_stubs.c
@@ -256,13 +256,12 @@ opal_value_t* opal_dstore_base_lookup_keyval(opal_dstore_proc_data_t *proc_data,
  * opal_process_name_t.
  */
 opal_dstore_proc_data_t* opal_dstore_base_lookup_proc(opal_proc_table_t *ptable,
-                                                      opal_process_name_t id)
+                                                      opal_process_name_t id, bool create)
 {
     opal_dstore_proc_data_t *proc_data = NULL;
 
     opal_proc_table_get_value(ptable, id, (void**)&proc_data);
-    if (NULL == proc_data) {
-        /* The proc clearly exists, so create a data structure for it */
+    if (NULL == proc_data && create) {
         proc_data = OBJ_NEW(opal_dstore_proc_data_t);
         if (NULL == proc_data) {
             opal_output(0, "dstore:hash:lookup_opal_proc: unable to allocate proc_data_t\n");

opal/mca/dstore/hash/dstore_hash.c
@@ -116,7 +116,7 @@ static int store(struct opal_dstore_base_module_t *imod,
                         OPAL_NAME_PRINT(OPAL_PROC_MY_NAME), OPAL_NAME_PRINT(*id));
 
     /* lookup the proc data object for this proc */
-    if (NULL == (proc_data = opal_dstore_base_lookup_proc(&mod->ptable, *id))) {
+    if (NULL == (proc_data = opal_dstore_base_lookup_proc(&mod->ptable, *id, true))) {
         /* unrecoverable error */
         OPAL_OUTPUT_VERBOSE((5, opal_dstore_base_framework.framework_output,
                              "%s dstore:hash:store: storing data for proc %s unrecoverably failed",
@@ -169,7 +169,7 @@ static int fetch(struct opal_dstore_base_module_t *imod,
                          (NULL == key) ? "NULL" : key, OPAL_NAME_PRINT(*id)));
 
     /* lookup the proc data object for this proc */
-    if (NULL == (proc_data = opal_dstore_base_lookup_proc(&mod->ptable, *id))) {
+    if (NULL == (proc_data = opal_dstore_base_lookup_proc(&mod->ptable, *id, true))) {
         OPAL_OUTPUT_VERBOSE((5, opal_dstore_base_framework.framework_output,
                              "%s dstore_hash:fetch data for proc %s not found",
                              OPAL_NAME_PRINT(OPAL_PROC_MY_NAME),
@@ -228,7 +228,7 @@ static int remove_data(struct opal_dstore_base_module_t *imod,
     mod = (mca_dstore_hash_module_t*)imod;
 
     /* lookup the specified proc */
-    if (NULL == (proc_data = opal_dstore_base_lookup_proc(&mod->ptable, *id))) {
+    if (NULL == (proc_data = opal_dstore_base_lookup_proc(&mod->ptable, *id, false))) {
         /* no data for this proc */
         return OPAL_SUCCESS;
     }

orte/mca/grpcomm/direct/grpcomm_direct.c
@@ -6,6 +6,8 @@
  * Copyright (c) 2011-2013 Los Alamos National Security, LLC. All
  *                         rights reserved.
  * Copyright (c) 2014      Intel, Inc.  All rights reserved.
+ * Copyright (c) 2014      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -362,8 +364,11 @@ static void xcast_recv(int status, orte_process_name_t* sender,
                  */
                 if (ORTE_PROC_IS_HNP) {
                     /* no need - already have the info */
-                    if (NULL != bo->bytes) {
-                        free(bo->bytes);
+                    if (NULL != bo) {
+                        if (NULL != bo->bytes) {
+                            free(bo->bytes);
+                        }
+                        free(bo);
                     }
                 } else {
                     OPAL_OUTPUT_VERBOSE((5, orte_grpcomm_base_framework.framework_output,
@@ -409,6 +414,7 @@ static void xcast_recv(int status, orte_process_name_t* sender,
                     /* done with the wireup buffer - dump it */
                     OBJ_DESTRUCT(&wireup);
                 }
+                free(bo);
                 /* copy the remainder of the payload */
                 opal_dss.copy_payload(relay, buffer);
             }
@@ -430,6 +436,7 @@ static void xcast_recv(int status, orte_process_name_t* sender,
         OPAL_OUTPUT_VERBOSE((5, orte_grpcomm_base_framework.framework_output,
                              "%s grpcomm:direct:send_relay - recipient list is empty!",
                              ORTE_NAME_PRINT(ORTE_PROC_MY_NAME)));
+        OBJ_RELEASE(rly);
         goto CLEANUP;
     }
     

commit 578fe4178822f670674058261881dd50e436350a

fix hangs introduced by previous commit a6744b81777ab8247908350bd15cca49bedf5208

opal/mca/pmix/native/pmix_native.c
@@ -163,7 +163,6 @@ static int pmix_sm_attach(uint32_t jid, char *seg_info)
 
     rc = opal_dstore.update(opal_dstore_modex, &attrs);
     opal_list_remove_item(&attrs, &attr->super);
-    free(attr->connection_info);
     OBJ_RELEASE(attr);
     OPAL_LIST_DESTRUCT(&attrs);
     return rc;

orte/mca/grpcomm/base/grpcomm_base_stubs.c
@@ -12,8 +12,6 @@
  *                         All rights reserved.
  * Copyright (c) 2011-2012 Los Alamos National Security, LLC.
  *                         All rights reserved.
- * Copyright (c) 2014      Research Organization for Information Science
- *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -70,18 +68,9 @@ static void gccon(orte_grpcomm_caddy_t *p)
     p->cbfunc = NULL;
     p->cbdata = NULL;
 }
-static void gcdes(orte_grpcomm_caddy_t *p)
-{
-    if (NULL != p->sig) {
-        OBJ_RELEASE(p->sig);
-    }
-    p->buf = NULL;
-    p->cbfunc = NULL;
-    p->cbdata = NULL;
-}
 static OBJ_CLASS_INSTANCE(orte_grpcomm_caddy_t,
                           opal_object_t,
-                          gccon, gcdes);
+                          gccon, NULL);
 
 int orte_grpcomm_API_xcast(orte_grpcomm_signature_t *sig,
                            orte_rml_tag_t tag,