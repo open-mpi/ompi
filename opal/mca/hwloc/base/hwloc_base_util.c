23cfe9a3a3024626eb55954211f9d75d659e2eb7
Ralph Castain <rhc@open-mpi.org>
2014-11-30 11:47:34 -0800

    Provide tighter LSF integration by correctly handling scenarios where the user has asked LSF to assign bindings. Fix a couple of typos in lex parser definitions. Tell hostfile parser to ignore binding designations in hostfiles. Add an attribute to indicate that cpusets were provided as physical cpu ids.
    Once validated, a version of this will be backported to the v1.8.4 release.

--- opal/mca/hwloc/base/hwloc_base_util.c
+++ opal/mca/hwloc/base/hwloc_base_util.c
@@ -1215,8 +1215,13 @@ int opal_hwloc_base_slot_list_parse(const char *slot_str,
                         "slot assignment: slot_list == %s",
                         slot_str);
 
-    /* split at ';' */
-    item = opal_argv_split(slot_str, ';');
+    /* if someone used commas instead of semicolons, let them */
+    if (NULL != strchr(slot_str, ',')) {
+        item = opal_argv_split(slot_str, ',');
+    } else {
+        /* split at ';' */
+        item = opal_argv_split(slot_str, ';');
+    }
     
     /* start with a clean mask */
     hwloc_bitmap_zero(cpumask);
