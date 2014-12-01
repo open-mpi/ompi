2df98917bc1e52dab121904ab92785656410a6dd
Ralph Castain <rhc@open-mpi.org>
2014-12-01 09:35:52 -0800

    Add missing var declarations

--- orte/mca/ras/lsf/ras_lsf_module.c
+++ orte/mca/ras/lsf/ras_lsf_module.c
@@ -29,6 +29,8 @@
 
 #include "opal/util/argv.h"
 
+#include "orte/mca/rmaps/rmaps_types.h"
+#include "orte/mca/errmgr/errmgr.h"
 #include "orte/runtime/orte_globals.h"
 #include "orte/util/show_help.h"
 
@@ -53,6 +55,21 @@ orte_ras_base_module_t orte_ras_lsf_module = {
     finalize
 };
 
+static char *orte_getline(FILE *fp)
+{
+    char *ret, *buff;
+    char input[1024];
+
+    ret = fgets(input, 1024, fp);
+    if (NULL != ret) {
+	   input[strlen(input)-1] = '\0';  /* remove newline */
+	   buff = strdup(input);
+	   return buff;
+    }
+    
+    return NULL;
+}
+
 
 static int allocate(orte_job_t *jdata, opal_list_t *nodes)
 {
@@ -61,6 +78,8 @@ static int allocate(orte_job_t *jdata, opal_list_t *nodes)
     int i, num_nodes;
     char *affinity_file, *hstname;
     bool found;
+    FILE *fp;
+    orte_app_context_t *app;
     
     /* check for an affinity file */
     if (NULL != (affinity_file = getenv("LSB_AFFINITY_HOSTFILE"))) {
@@ -70,8 +89,8 @@ static int allocate(orte_job_t *jdata, opal_list_t *nodes)
         if (NULL == jdata->map) {
             jdata->map = OBJ_NEW(orte_job_map_t);
         }
-        ORTE_SET_MAPPING_POLICY(jdata->map->mapping, ORTE_MAPPING_SEQ)
-            jdata->map->req_mapper = strdup("seq"); // need sequential mapper
+        ORTE_SET_MAPPING_POLICY(jdata->map->mapping, ORTE_MAPPING_SEQ);
+        jdata->map->req_mapper = strdup("seq"); // need sequential mapper
         /* tell the sequential mapper that all cpusets are to be treated as "physical" */
         orte_set_attribute(&jdata->attributes, ORTE_JOB_PHYSICAL_CPUIDS, true, NULL, OPAL_BOOL);
         /* get the apps and set the hostfile attribute in each to point to
