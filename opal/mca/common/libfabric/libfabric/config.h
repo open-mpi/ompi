35bf5fc60c4970f988e80c7101fd5f27a22ea790
Jeff Squyres <jsquyres@cisco.com>
2014-12-19 17:32:52 -0800

    libfabric: add missing header file

---
+++ opal/mca/common/libfabric/libfabric/config.h
@@ -0,0 +1,117 @@
+/* opal/mca/common/libfabric/libfabric/config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Set to 1 to use c11 atomic functions */
+/* #undef HAVE_ATOMICS */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+/* #undef HAVE_LIBDL */
+
+/* Define to 1 if you have the `ibverbs' library (-libverbs). */
+/* #undef HAVE_LIBIBVERBS */
+
+/* Define to 1 if you have the `nl' library (-lnl). */
+#define HAVE_LIBNL 1
+
+/* Define to 1 if you have the `psm_infinipath' library (-lpsm_infinipath). */
+/* #undef HAVE_LIBPSM_INFINIPATH */
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+/* #undef HAVE_LIBPTHREAD */
+
+/* Define to 1 if you have the `rdmacm' library (-lrdmacm). */
+/* #undef HAVE_LIBRDMACM */
+
+/* Define to 1 if you have the `rt' library (-lrt). */
+/* #undef HAVE_LIBRT */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* psm provider is built */
+#define HAVE_PSM 0
+
+/* psm provider is built as DSO */
+#define HAVE_PSM_DL 0
+
+/* sockets provider is built */
+#define HAVE_SOCKETS 0
+
+/* sockets provider is built as DSO */
+#define HAVE_SOCKETS_DL 0
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* assembler has .symver support */
+/* #undef HAVE_SYMVER_SUPPORT */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* usnic provider is built */
+#define HAVE_USNIC 1
+
+/* usnic provider is built as DSO */
+#define HAVE_USNIC_DL 0
+
+/* verbs provider is built */
+#define HAVE_VERBS 0
+
+/* verbs provider is built as DSO */
+#define HAVE_VERBS_DL 0
+
+/* Define to 1 to enable valgrind annotations */
+/* #undef INCLUDE_VALGRIND */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+/* #undef PACKAGE */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.open-mpi.org/community/help/"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Open MPI"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Open MPI gitclone"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "openmpi"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "gitclone"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+/* #undef VERSION */
