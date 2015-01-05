09652335d6c64135087cd17c1790c41bc59dae66
Nathan Hjelm <hjelmn@lanl.gov>
2015-01-05 15:25:13 -0700

    btl/template: update for BTL 3.0 interface
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- opal/mca/btl/template/btl_template.c
+++ opal/mca/btl/template/btl_template.c
@@ -10,7 +10,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
- * Copyright (c) 2014      Los Alamos National Security, LLC. All rights
+ * Copyright (c) 2014-2015 Los Alamos National Security, LLC. All rights
  *                         reserved.
  * $COPYRIGHT$
  * 
@@ -43,9 +43,11 @@ mca_btl_template_module_t mca_btl_template_module = {
         .btl_alloc = mca_btl_template_alloc,
         .btl_free = mca_btl_template_free,
         .btl_prepare_src = mca_btl_template_prepare_src,
-        .btl_prepare_dst = mca_btl_template_prepare_dst,
         .btl_send = mca_btl_template_send,
         .btl_put = mca_btl_template_put,
+        .btl_get = mca_btl_template_get,
+        .btl_register_mem = mca_btl_template_register_mem,
+        .btl_deregister_mem = mca_btl_template_deregister_mem,
         .btl_ft_event = mca_btl_template_ft_event
     }
 };
@@ -206,7 +208,6 @@ int mca_btl_template_free(
 mca_btl_base_descriptor_t* mca_btl_template_prepare_src(
     struct mca_btl_base_module_t* btl,
     struct mca_btl_base_endpoint_t* endpoint,
-    struct mca_mpool_base_registration_t* registration,
     struct opal_convertor_t* convertor,
     uint8_t order,
     size_t reserve,
@@ -270,49 +271,8 @@ mca_btl_base_descriptor_t* mca_btl_template_prepare_src(
         frag->segment.seg_len = max_data + reserve;
     }
 
-    frag->base.des_local = &frag->segment;
-    frag->base.des_local_count = 1;
-    frag->base.des_flags = 0;
-    return &frag->base;
-}
-
-
-/**
- * Prepare a descriptor for send/rdma using the supplied
- * convertor. If the convertor references data that is contigous,
- * the descriptor may simply point to the user buffer. Otherwise,
- * this routine is responsible for allocating buffer space and
- * packing if required.
- *
- * @param btl (IN)          BTL module
- * @param endpoint (IN)     BTL peer addressing
- * @param convertor (IN)    Data type convertor
- * @param reserve (IN)      Additional bytes requested by upper layer to precede user data
- * @param size (IN/OUT)     Number of bytes to prepare (IN), number of bytes actually prepared (OUT)
- */
-
-mca_btl_base_descriptor_t* mca_btl_template_prepare_dst(
-    struct mca_btl_base_module_t* btl,
-    struct mca_btl_base_endpoint_t* endpoint,
-    struct mca_mpool_base_registration_t* registration,
-    struct opal_convertor_t* convertor,
-    uint8_t order,
-    size_t reserve,
-    size_t* size,
-    uint32_t flags)
-{
-    mca_btl_template_frag_t* frag;
-
-    MCA_BTL_TEMPLATE_FRAG_ALLOC_USER(btl, frag);
-    if(OPAL_UNLIKELY(NULL == frag)) {
-        return NULL;
-    }
-
-    frag->segment.seg_len = *size;
-    opal_convertor_get_current_pointer( convertor, (void**)&(frag->segment.seg_addr.pval) );
-
-    frag->base.des_local = &frag->segment;
-    frag->base.des_local_count = 1;
+    frag->base.des_segments = &frag->segment;
+    frag->base.des_segment_count = 1;
     frag->base.des_flags = 0;
     return &frag->base;
 }
@@ -350,14 +310,13 @@ int mca_btl_template_send(
  * @param descriptor (IN)  Description of the data to be transferred
  */
 
-int mca_btl_template_put( 
-    mca_btl_base_module_t* btl,
-    mca_btl_base_endpoint_t* endpoint,
-    mca_btl_base_descriptor_t* descriptor)
+int mca_btl_template_put (struct mca_btl_base_module_t *btl,
+    struct mca_btl_base_endpoint_t *endpoint, void *local_address,
+    uint64_t remote_address, struct mca_btl_base_registration_handle_t *local_handle,
+    struct mca_btl_base_registration_handle_t *remote_handle, size_t size, int flags,
+    int order, mca_btl_base_rdma_completion_fn_t cbfunc, void *cbcontext, void *cbdata)
 {
     /* mca_btl_template_module_t* template_btl = (mca_btl_template_module_t*) btl; */
-    mca_btl_template_frag_t* frag = (mca_btl_template_frag_t*) descriptor; 
-    frag->endpoint = endpoint;
     /* TODO */
     return OPAL_ERR_NOT_IMPLEMENTED; 
 }
@@ -372,16 +331,62 @@ int mca_btl_template_put(
  *
  */
 
-int mca_btl_template_get( 
-    mca_btl_base_module_t* btl,
-    mca_btl_base_endpoint_t* endpoint,
-    mca_btl_base_descriptor_t* descriptor)
+int mca_btl_template_get (struct mca_btl_base_module_t *btl,
+    struct mca_btl_base_endpoint_t *endpoint, void *local_address,
+    uint64_t remote_address, struct mca_btl_base_registration_handle_t *local_handle,
+    struct mca_btl_base_registration_handle_t *remote_handle, size_t size, int flags,
+    int order, mca_btl_base_rdma_completion_fn_t cbfunc, void *cbcontext, void *cbdata)
 {
     /* mca_btl_template_module_t* template_btl = (mca_btl_template_module_t*) btl; */
-    mca_btl_template_frag_t* frag = (mca_btl_template_frag_t*) descriptor; 
-    frag->endpoint = endpoint;
     /* TODO */
     return OPAL_ERR_NOT_IMPLEMENTED; 
+}
+
+/**
+ * @brief Register a memory region for put/get/atomic operations.
+ *
+ * @param btl (IN)         BTL module
+ * @param endpoint(IN)     BTL addressing information (or NULL for all endpoints)
+ * @param base (IN)        Pointer to start of region
+ * @param size (IN)        Size of region
+ * @param flags (IN)       Flags indicating what operation will be performed. Valid
+ *                         values are MCA_BTL_DES_FLAGS_PUT, MCA_BTL_DES_FLAGS_GET,
+ *                         and MCA_BTL_DES_FLAGS_ATOMIC
+ *
+ * @returns a memory registration handle valid for both local and remote operations
+ * @returns NULL if the region could not be registered
+ *
+ * This function registers the specified region with the hardware for use with
+ * the btl_put, btl_get, btl_atomic_cas, btl_atomic_op, and btl_atomic_fop
+ * functions. Care should be taken to not hold an excessive number of registrations
+ * as they may use limited system/NIC resources.
+ */
+struct mca_btl_base_registration_handle_t *mca_btl_template_register_mem (
+    struct mca_btl_base_module_t* btl, struct mca_btl_base_endpoint_t *endpoint, void *base,
+    size_t size, uint32_t flags)
+{
+    /* mca_btl_template_module_t* template_btl = (mca_btl_template_module_t*) btl; */
+    /* TODO */
+    return NULL;
+}
+
+/**
+ * @brief Deregister a memory region
+ *
+ * @param btl (IN)         BTL module region was registered with
+ * @param handle (IN)      BTL registration handle to deregister
+ *
+ * This function deregisters the memory region associated with the specified handle. Care
+ * should be taken to not perform any RDMA or atomic operation on this memory region
+ * after it is deregistered. It is erroneous to specify a memory handle associated with
+ * a remote node.
+ */
+int mca_btl_template_deregister_mem (struct mca_btl_base_module_t* btl,
+				     struct mca_btl_base_registration_handle_t *handle)
+{
+    /* mca_btl_template_module_t* template_btl = (mca_btl_template_module_t*) btl; */
+    /* TODO */
+    return OPAL_ERR_NOT_IMPLEMENTED;
 }
 
 
