88251a6b948a96e917750de544ae434fe17baa33
Nathan Hjelm <hjelmn@lanl.gov>
2015-01-14 13:53:30 -0700

    Combine opal_free_list_t and ompi_free_list_t
    Historically these two lists were different due to ompi_free_list_t
    dependencies in ompi (mpool). Those dependencies have since been moved
    to opal so it is safe to (finally) combine them. The combined free
    list comes in three flavors:
    
     - Single-threaded. Only to be used when it is guaranteed that no
       concurrent access will be made to the free list. Single-threaded
       functions are suffixed with _st.
    
     - Mutli-threaded. To be used when the free list may be accessed by
       multiple threads despite the setting of opal_using_threads.
       Multi-threaded functins are suffixed with _mt.
    
     - Conditionally multi-threaded. Common use case. These functions are
       thread-safe if opal_using_threads is set to true.
    
    Compatibility functions for the ompi_free_list_t and the old accessor
    functions (OPAL_FREE_LIST_*) are available while the code base is
    transitioned to the new class/functions.
    
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- opal/class/ompi_free_list.h
+++ opal/class/ompi_free_list.h
@@ -24,271 +24,105 @@
 #ifndef OMPI_FREE_LIST_H
 #define OMPI_FREE_LIST_H
 
-#include "opal_config.h"
-#include "opal/class/opal_lifo.h"
-#include "opal/prefetch.h"
-#include "opal/threads/condition.h"
-#include "opal/constants.h"
-#include "opal/runtime/opal.h"
+#include "opal_free_list.h"
 
-BEGIN_C_DECLS
+#define ompi_free_list_item_t opal_free_list_item_t
 
-struct mca_mem_pool_t;
-struct ompi_free_list_item_t;
-    
-typedef void (*ompi_free_list_item_init_fn_t) (
-        struct ompi_free_list_item_t*, void* ctx);
+typedef void (*ompi_free_list_item_init_fn_t) (ompi_free_list_item_t*, void* ctx);
 
-struct ompi_free_list_t
-{
-    opal_lifo_t super;
-    size_t fl_max_to_alloc;
-    size_t fl_num_allocated;
-    size_t fl_num_per_alloc;
-    size_t fl_num_waiting;
-    size_t fl_frag_size;                /* size of the fragment descriptor */
-    size_t fl_frag_alignment;           /* fragment descriptor alignment */
-    size_t fl_payload_buffer_size;      /* size of payload buffer */
-    size_t fl_payload_buffer_alignment; /* payload buffer alignment */
-    opal_class_t* fl_frag_class;
-    struct mca_mpool_base_module_t* fl_mpool;
-    opal_mutex_t fl_lock;
-    opal_condition_t fl_condition; 
-    opal_list_t fl_allocations;
+struct ompi_free_list_t {
+    opal_free_list_t super;
     ompi_free_list_item_init_fn_t item_init;
-    void* ctx;
+    void *ctx;
 };
 typedef struct ompi_free_list_t ompi_free_list_t;
 OPAL_DECLSPEC OBJ_CLASS_DECLARATION(ompi_free_list_t);
 
-struct mca_mpool_base_registration_t;
-struct ompi_free_list_item_t
-{ 
-    opal_list_item_t super; 
-    struct mca_mpool_base_registration_t *registration;
-    void *ptr;
-}; 
-typedef struct ompi_free_list_item_t ompi_free_list_item_t; 
-OPAL_DECLSPEC OBJ_CLASS_DECLARATION(ompi_free_list_item_t);
+#define OMPI_FREE_LIST_GET_MT(fl, item)		\
+    opal_free_list_get (&(fl)->super, &(item))
 
-/**
- * Initialize a free list.
- *
- * @param free_list (IN)           Free list.
- * @param element_size (IN)        Size of each element.
- * @param element_class (IN)       opal_class_t of element - used to initialize allocated elements.
- * @param num_elements_to_alloc    Initial number of elements to allocate.
- * @param max_elements_to_alloc    Maximum number of elements to allocate.
- * @param num_elements_per_alloc   Number of elements to grow by per allocation.
- * @param mpool                    Optional memory pool for allocation.s
- */
- 
-OPAL_DECLSPEC int ompi_free_list_init_ex(
-    ompi_free_list_t *free_list, 
-    size_t element_size,
-    size_t alignment,
-    opal_class_t* element_class,
-    int num_elements_to_alloc,
-    int max_elements_to_alloc,
-    int num_elements_per_alloc,
-    struct mca_mpool_base_module_t*,
-    ompi_free_list_item_init_fn_t item_init,
-    void *ctx
-    );
+#define OMPI_FREE_LIST_WAIT_MT(fl, item)        \
+    opal_free_list_wait (&(fl)->super, &(item))
 
-static inline int ompi_free_list_init(
-    ompi_free_list_t *free_list, 
-    size_t element_size,
-    opal_class_t* element_class,
-    int num_elements_to_alloc,
-    int max_elements_to_alloc,
-    int num_elements_per_alloc,
-    struct mca_mpool_base_module_t* mpool)
+#define OMPI_FREE_LIST_RETURN_MT(fl, item)      \
+    opal_free_list_return (&(fl)->super, item)
+
+static inline int ompi_free_list_item_init_compat (ompi_free_list_item_t *item,
+                                                   void *ctx)
 {
-    return ompi_free_list_init_ex(free_list, element_size, opal_cache_line_size,
-            element_class, num_elements_to_alloc, max_elements_to_alloc,
-                                  num_elements_per_alloc, mpool, NULL, NULL);
+    ompi_free_list_t *free_list = (ompi_free_list_t *) ctx;
+
+    free_list->item_init (item, free_list->ctx);
+    return OPAL_SUCCESS;
 }
 
-/**
- * Initialize a free list.  - this will replace ompi_free_list_init_ex
- *
- * @param free_list                (IN)  Free list.
- * @param frag_size                (IN)  Size of each element - allocated by malloc.
- * @param frag_alignment           (IN)  Fragment alignment.
- * @param frag_class               (IN)  opal_class_t of element - used to initialize allocated elements.
- * @param payload_buffer_size      (IN)  Size of payload buffer - allocated from mpool.
- * @param payload_buffer_alignment (IN)  Payload buffer alignment.
- * @param num_elements_to_alloc    (IN)  Initial number of elements to allocate.
- * @param max_elements_to_alloc    (IN)  Maximum number of elements to allocate.
- * @param num_elements_per_alloc   (IN)  Number of elements to grow by per allocation.
- * @param mpool                    (IN)  Optional memory pool for allocation.s
- * @param item_init                (IN)
- * @param ctx                      (IN)
- */
- 
-OPAL_DECLSPEC int ompi_free_list_init_ex_new(
-    ompi_free_list_t *free_list, 
-    size_t frag_size,
-    size_t frag_alignment,
-    opal_class_t* frag_class,
-    size_t payload_buffer_size,
-    size_t payload_buffer_alignment,
-    int num_elements_to_alloc,
-    int max_elements_to_alloc,
-    int num_elements_per_alloc,
-    struct mca_mpool_base_module_t*,
-    ompi_free_list_item_init_fn_t item_init,
-    void *ctx
-    );
 
-/**
- * Initialize a free list. - this will replace ompi_free_list_init
- *
- * @param free_list                (IN)  Free list.
- * @param frag_size                (IN)  Size of each element - allocated by malloc.
- * @param frag_alignment           (IN)  Fragment alignment.
- * @param frag_class               (IN)  opal_class_t of element - used to initialize allocated elements.
- * @param payload_buffer_size      (IN)  Size of payload buffer - allocated from mpool.
- * @param payload_buffer_alignment (IN)  Payload buffer alignment.
- * @param num_elements_to_alloc    (IN)  Initial number of elements to allocate.
- * @param max_elements_to_alloc    (IN)  Maximum number of elements to allocate.
- * @param num_elements_per_alloc   (IN)  Number of elements to grow by per allocation.
- * @param mpool                    (IN)  Optional memory pool for allocation.s
- */
-static inline int ompi_free_list_init_new(
-    ompi_free_list_t *free_list, 
-    size_t frag_size,
-    size_t frag_alignment,
-    opal_class_t* frag_class,
-    size_t payload_buffer_size,
-    size_t payload_buffer_alignment,
-    int num_elements_to_alloc,
-    int max_elements_to_alloc,
-    int num_elements_per_alloc,
-    struct mca_mpool_base_module_t* mpool)
+static inline int __opal_attribute_deprecated__
+ompi_free_list_init_ex_new (ompi_free_list_t *free_list,  size_t frag_size,
+                            size_t frag_alignment, opal_class_t* frag_class,
+                            size_t payload_buffer_size, size_t payload_buffer_alignment,
+                            int num_elements_to_alloc, int max_elements_to_alloc,
+                            int num_elements_per_alloc, struct mca_mpool_base_module_t *mpool,
+                            ompi_free_list_item_init_fn_t item_init, void *ctx)
 {
-    return ompi_free_list_init_ex_new(free_list,
-            frag_size, frag_alignment, frag_class,
-            payload_buffer_size, payload_buffer_alignment,
-            num_elements_to_alloc, max_elements_to_alloc,
-            num_elements_per_alloc, mpool, NULL, NULL);
+    free_list->item_init = item_init;
+    free_list->ctx = ctx;
+
+    return opal_free_list_init (&free_list->super, frag_size, frag_alignment,
+                                frag_class, payload_buffer_size, payload_buffer_alignment,
+                                num_elements_to_alloc, max_elements_to_alloc,
+                                num_elements_per_alloc, mpool, 0, NULL,
+                                ompi_free_list_item_init_compat,
+                                (void *) free_list);
 }
 
-OPAL_DECLSPEC int ompi_free_list_grow(ompi_free_list_t* flist, size_t num_elements);
+static inline int __opal_attribute_deprecated__
+ompi_free_list_init_new (ompi_free_list_t *free_list,  size_t frag_size,
+                         size_t frag_alignment, opal_class_t* frag_class,
+                         size_t payload_buffer_size, size_t payload_buffer_alignment,
+                         int num_elements_to_alloc,int max_elements_to_alloc,
+                         int num_elements_per_alloc, struct mca_mpool_base_module_t* mpool)
+{
+    return opal_free_list_init (&free_list->super, frag_size, frag_alignment,
+                                frag_class, payload_buffer_size,
+                                payload_buffer_alignment, num_elements_to_alloc,
+                                max_elements_to_alloc, num_elements_per_alloc,
+                                mpool, 0, NULL, NULL, NULL);
+}
+
+static inline int __opal_attribute_deprecated__
+ompi_free_list_init_ex (ompi_free_list_t *free_list, size_t element_size,
+                        size_t alignment, opal_class_t* element_class,
+                        int num_elements_to_alloc, int max_elements_to_alloc,
+                        int num_elements_per_alloc, struct mca_mpool_base_module_t *mpool,
+                        ompi_free_list_item_init_fn_t item_init, void *ctx)
+{
+    return ompi_free_list_init_ex_new (free_list, element_size, alignment,
+                                       element_class, 0, 0, num_elements_to_alloc,
+                                       max_elements_to_alloc, num_elements_per_alloc,
+                                       mpool, item_init, ctx);
+}
+
+static inline int __opal_attribute_deprecated__
+ompi_free_list_init (ompi_free_list_t *free_list,  size_t element_size,
+                     opal_class_t* element_class, int num_elements_to_alloc,
+                     int max_elements_to_alloc, int num_elements_per_alloc,
+                     struct mca_mpool_base_module_t* mpool)
+{
+    return opal_free_list_init (&free_list->super, element_size, opal_cache_line_size,
+                                element_class, 0, 0, num_elements_to_alloc,
+                                max_elements_to_alloc, num_elements_per_alloc,
+                                mpool, 0, NULL, NULL, NULL);
+}
 
 /* Grow the free list to be *at least* size elements.  This function
    will not shrink the list if it is already larger than size and may
    grow it past size if necessary (it will grow in
    num_elements_per_alloc chunks) */
-OPAL_DECLSPEC int ompi_free_list_resize_mt(ompi_free_list_t *flist, size_t size);
-
-/**
- * Attemp to obtain an item from a free list. 
- *
- * @param fl (IN)        Free list.
- * @param item (OUT)     Allocated item.
- *
- * If the requested item is not available the free list is grown to 
- * accomodate the request - unless the max number of allocations has 
- * been reached.  If this is the case - a NULL pointer is returned 
- * to the caller.
- */
-
-#define OMPI_FREE_LIST_GET_MT(fl, item)                                 \
-    {                                                                   \
-        item = (ompi_free_list_item_t*) opal_lifo_pop (&((fl)->super)); \
-        if( OPAL_UNLIKELY(NULL == item) ) {                             \
-            if(opal_using_threads()) {                                  \
-                opal_mutex_lock(&((fl)->fl_lock));                      \
-                ompi_free_list_grow((fl), (fl)->fl_num_per_alloc);      \
-                opal_mutex_unlock(&((fl)->fl_lock));                    \
-            } else {                                                    \
-                ompi_free_list_grow((fl), (fl)->fl_num_per_alloc);      \
-            }                                                           \
-            item = (ompi_free_list_item_t*) opal_lifo_pop(&((fl)->super)); \
-        }                                                               \
-    } 
-
-/**
- * Blocking call to obtain an item from a free list.
- *
- * @param fl (IN)        Free list.
- * @param item (OUT)     Allocated item.
- *
- * If the requested item is not available the free list is grown to 
- * accomodate the request - unless the max number of allocations has 
- * been reached. In this case the caller is blocked until an item
- * is returned to the list.
- */
-
-#define OMPI_FREE_LIST_WAIT_MT(fl, item)           \
-    __ompi_free_list_wait_mt( (fl), &(item) )
-
-static inline void __ompi_free_list_wait_mt( ompi_free_list_t* fl,
-                                             ompi_free_list_item_t** item )
+static inline int __opal_attribute_deprecated__
+ompi_free_list_resize_mt(ompi_free_list_t *flist, size_t size)
 {
-    *item = (ompi_free_list_item_t*)opal_lifo_pop(&((fl)->super));
-    while( NULL == *item ) {
-        if( !OPAL_THREAD_TRYLOCK(&((fl)->fl_lock)) ) {
-            if((fl)->fl_max_to_alloc <= (fl)->fl_num_allocated) {
-                (fl)->fl_num_waiting++;
-                opal_condition_wait(&((fl)->fl_condition), &((fl)->fl_lock));
-                (fl)->fl_num_waiting--;
-            } else {
-                if(ompi_free_list_grow((fl), (fl)->fl_num_per_alloc)
-                        == OPAL_SUCCESS) {
-                    if( 0 < (fl)->fl_num_waiting ) {
-                        if( 1 == (fl)->fl_num_waiting ) {
-                            opal_condition_signal(&((fl)->fl_condition));
-                        } else {
-                            opal_condition_broadcast(&((fl)->fl_condition));
-                        }
-                    }
-                } else {
-                    (fl)->fl_num_waiting++;
-                    opal_condition_wait(&((fl)->fl_condition), &((fl)->fl_lock));
-                    (fl)->fl_num_waiting--;
-                }
-            }
-        } else {
-            /* If I wasn't able to get the lock in the begining when I finaly grab it
-             * the one holding the lock in the begining already grow the list. I will
-             * release the lock and try to get a new element until I succeed.
-             */
-            OPAL_THREAD_LOCK(&((fl)->fl_lock));
-        }
-        OPAL_THREAD_UNLOCK(&((fl)->fl_lock));
-        *item = (ompi_free_list_item_t*)opal_lifo_pop(&((fl)->super));
-    }
-} 
+    return opal_free_list_resize_mt (&flist->super, size);
+}
 
-/**
- * Return an item to a free list. 
- *
- * @param fl (IN)        Free list.
- * @param item (OUT)     Allocated item.
- *
- */
- 
-#define OMPI_FREE_LIST_RETURN_MT(fl, item)                              \
-    do {                                                                \
-        opal_list_item_t* original;                                     \
-                                                                        \
-        original = opal_lifo_push( &(fl)->super, &(item)->super);       \
-        if( &(fl)->super.opal_lifo_ghost == original ) {                \
-            OPAL_THREAD_LOCK(&(fl)->fl_lock);                           \
-            if((fl)->fl_num_waiting > 0) {                              \
-                if( 1 == (fl)->fl_num_waiting ) {                       \
-                    opal_condition_signal(&((fl)->fl_condition));       \
-                } else {                                                \
-                    opal_condition_broadcast(&((fl)->fl_condition));    \
-                }                                                       \
-            }                                                           \
-            OPAL_THREAD_UNLOCK(&(fl)->fl_lock);                         \
-        }                                                               \
-    } while(0)
-    
-END_C_DECLS
-#endif 
-
+#endif
