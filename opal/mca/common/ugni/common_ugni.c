bc74f87e4c55049d9b06239389a2fc858f9fe9b7
Howard Pritchard <hppritcha@gmail.com>
2015-01-22 06:53:45 -0800

    btl/ugni: use PMIX_GLOBAL for modex_send in ugni
    Using PMIX_REMOTE is not the right thing for ugni
    BTL when its possible that spawned ranks end up
    on the same node as some of the spawnee ranks.

--- opal/mca/common/ugni/common_ugni.c
+++ opal/mca/common/ugni/common_ugni.c
@@ -190,7 +190,12 @@ static int opal_common_ugni_send_modex (int my_cdm_id)
         msg_offset += modex_size;
     }
 
-    OPAL_MODEX_SEND(rc, PMIX_ASYNC_RDY, PMIX_REMOTE,
+    /*
+     * need global for edge cases like MPI_Comm_spawn support with
+     * new ranks started on the same nodes as the spawnee ranks, etc.
+     */
+
+    OPAL_MODEX_SEND(rc, PMIX_ASYNC_RDY, PMIX_GLOBAL,
                     &opal_common_ugni_component,
                     modex_msg, total_msg_size);
 
