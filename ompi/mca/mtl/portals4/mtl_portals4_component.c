ed537ddca0c4fe11eb74ba331373ac31ad1e4d80
Howard Pritchard <hppritcha@gmail.com>
2015-01-31 13:50:32 -0700

    copyright updates for commit eb977de5
    I really should start using Jeff's script..

--- ompi/mca/mtl/portals4/mtl_portals4_component.c
+++ ompi/mca/mtl/portals4/mtl_portals4_component.c
@@ -11,6 +11,8 @@
  *                         All rights reserved.
  * Copyright (c) 2010-2012 Sandia National Laboratories.  All rights reserved.
  * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * Copyright (c) 2015      Los Alamos National Security, LLC.  All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
