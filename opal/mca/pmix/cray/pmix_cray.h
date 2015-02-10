42c7f72c145ef64b98a4b7a0adb92e22369d6906
Howard Pritchard <howardp@lanl.gov>
2015-02-10 10:13:38 -0700

    pmix/cray: initial kvs removal work
    Remove use of the Cray PMI KVS - which is designed for a lighweight
    MPI that exchanges only a minimimal amount of connection info
    (about 128 bytes per rank) - within cray/pmix.  Use Cray PMI
    collective extensions instead.
    
    This is the first of several steps to accelerate launch of
    Open MPI on Cray systems using either native aprun or nativized
    slurm.

--- opal/mca/pmix/cray/pmix_cray.h
+++ opal/mca/pmix/cray/pmix_cray.h
@@ -15,17 +15,29 @@
 #include "opal/mca/mca.h"
 #include "opal/mca/pmix/pmix.h"
 #include "opal/mca/pmix/base/pmix_base_fns.h"
+#include "opal/util/proc.h"
 #include "pmix_cray_pmap_parser.h"
 
 BEGIN_C_DECLS
+
+typedef struct {
+    opal_pmix_base_component_t super;
+    opal_buffer_t *cache_local;
+    opal_buffer_t *cache_global;
+} opal_pmix_cray_component_t;
 
 /*
  * Globally exported variable
  */
 
-OPAL_DECLSPEC extern const opal_pmix_base_component_t mca_pmix_cray_component;
+OPAL_DECLSPEC extern opal_pmix_cray_component_t mca_pmix_cray_component;
 
 OPAL_DECLSPEC extern const opal_pmix_base_module_t opal_pmix_cray_module;
+
+/*
+ * proto-types for cray/pmix kvs component
+ */
+
 
 END_C_DECLS
 
