16229113d8f48aa4d8bb829340029f64247dc550
Ralph Castain <rhc@open-mpi.org>
2015-02-17 14:21:23 -0800

    Protect the unload attribute function

--- orte/util/attr.c
+++ orte/util/attr.c
@@ -426,7 +426,8 @@ static int orte_attr_unload(orte_attribute_t *kv,
     if (type != kv->type) {
         return OPAL_ERR_TYPE_MISMATCH;
     }
-    if (NULL == data && OPAL_STRING != type && OPAL_BYTE_OBJECT != type) {
+    if (NULL == data  ||
+        (NULL == *data && OPAL_STRING != type && OPAL_BYTE_OBJECT != type)) {
         OPAL_ERROR_LOG(OPAL_ERR_BAD_PARAM);
         return OPAL_ERR_BAD_PARAM;
     }
