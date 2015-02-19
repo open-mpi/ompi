426d1ce146edfb299030b536d0ba3baaf6adad12
Igor Ivanov <Igor.Ivanov@itseez.com>
2015-02-19 17:42:01 +0400

    oshmem: Fix set of coverity issues
    List of CIDs (scan.coverity.com):
    1269721, 1269725, 1269787, 1269907, 1269909, 1269910, 1269911, 1269912,
    1269959, 1269960, 1269984, 1269985, 1270136, 1270157, 1269845, 1269875,
    1269876, 1269877, 1269878, 1269884, 1269885, 1270161, 1270162, 1270175,
    1269734, 1269739, 1269742, 1269743
    
    Signed-off-by: Igor Ivanov <Igor.Ivanov@itseez.com>

--- oshmem/mca/spml/ikrit/spml_ikrit.c
+++ oshmem/mca/spml/ikrit/spml_ikrit.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013      Mellanox Technologies, Inc.
+ * Copyright (c) 2013-2015 Mellanox Technologies, Inc.
  *                         All rights reserved.
  * Copyright (c) 2014      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
@@ -342,7 +342,6 @@ OBJ_CLASS_INSTANCE( mxm_peer_t,
 int mca_spml_ikrit_del_procs(oshmem_proc_t** procs, size_t nprocs)
 {
     size_t i, n;
-    opal_list_item_t *item;
     int my_rank = oshmem_my_proc_id();
 
     oshmem_shmem_barrier();
@@ -352,7 +351,7 @@ int mca_spml_ikrit_del_procs(oshmem_proc_t** procs, size_t nprocs)
     }
 #endif
 
-    while (NULL != (item = opal_list_remove_first(&mca_spml_ikrit.active_peers))) {
+    while (NULL != opal_list_remove_first(&mca_spml_ikrit.active_peers)) {
     };
     OBJ_DESTRUCT(&mca_spml_ikrit.active_peers);
 
@@ -526,11 +525,13 @@ int mca_spml_ikrit_add_procs(oshmem_proc_t** procs, size_t nprocs)
         mxm_conn_ctx_set(conn_reqs[i].conn, mca_spml_ikrit.mxm_peers[i]);
     }
 
-    if (ep_info)
-        free(ep_info);
     if (conn_reqs)
         free(conn_reqs);
 #endif
+    if (ep_info)
+        free(ep_info);
+    if (ep_hw_rdma_info)
+        free(ep_hw_rdma_info);
 
 #if MXM_API >= MXM_VERSION(2,0)
     if (mca_spml_ikrit.bulk_connect) {
@@ -559,13 +560,15 @@ int mca_spml_ikrit_add_procs(oshmem_proc_t** procs, size_t nprocs)
     SPML_VERBOSE(50, "*** ADDED PROCS ***");
     return OSHMEM_SUCCESS;
 
-    bail:
-    #if MXM_API < MXM_VERSION(2,0)
-        if (conn_reqs)
-            free(conn_reqs);
-        if (ep_info)
-            free(ep_info);
-    #endif
+bail:
+#if MXM_API < MXM_VERSION(2,0)
+	if (conn_reqs)
+		free(conn_reqs);
+#endif
+	if (ep_info)
+		free(ep_info);
+	if (ep_hw_rdma_info)
+		free(ep_hw_rdma_info);
     SPML_ERROR("add procs FAILED rc=%d", rc);
 
     return rc;
@@ -661,7 +664,7 @@ sshmem_mkey_t *mca_spml_ikrit_register(void* addr,
 error_out:
     mca_spml_ikrit_deregister(mkeys);
 
-    return NULL ;
+    return NULL;
 }
 
 int mca_spml_ikrit_deregister(sshmem_mkey_t *mkeys)
@@ -695,6 +698,8 @@ int mca_spml_ikrit_deregister(sshmem_mkey_t *mkeys)
             break;
         }
     }
+    free(mkeys);
+
     return OSHMEM_SUCCESS;
 
 }
@@ -1363,6 +1368,7 @@ int mca_spml_ikrit_fence(void)
 /* blocking receive */
 int mca_spml_ikrit_recv(void* buf, size_t size, int src)
 {
+    mxm_error_t ret = MXM_OK;
     mxm_recv_req_t req;
     char dummy_buf[1];
 
@@ -1386,9 +1392,12 @@ int mca_spml_ikrit_recv(void* buf, size_t size, int src)
     req.base.data.buffer.length = size == 0 ? sizeof(dummy_buf) : size;
     req.base.data.buffer.memh = NULL;
 
-    mxm_req_recv(&req);
+    ret = mxm_req_recv(&req);
+    if (MXM_OK != ret) {
+        return OSHMEM_ERROR;
+    }
     mca_spml_irkit_req_wait(&req.base);
-    if (req.base.error != MXM_OK) {
+    if (MXM_OK != req.base.error) {
         return OSHMEM_ERROR;
     }
     SPML_VERBOSE(100,
