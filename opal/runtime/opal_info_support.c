59e3b91278d86e9fe1666a4a54699a13ee0af5d3
Jeff Squyres <jsquyres@cisco.com>
2015-02-24 12:46:55 -0500

    opal_info_support: protect against (group->group_component==NULL)
    This was CID 1196660

--- opal/runtime/opal_info_support.c
+++ opal/runtime/opal_info_support.c
@@ -624,7 +624,7 @@ static void opal_info_show_mca_group_params(const mca_base_var_group_t *group, m
                 mca_base_component_parse_requested (value->stringval, &include_mode, &requested_components);
 
                 for (i = 0, requested = !include_mode ; requested_components[i] ; ++i) {
-                    if (0 == strcmp (requested_components[i], group->group_component)) {
+                    if (0 == strcmp (requested_components[i], group_component)) {
                         requested = include_mode;
                         break;
                     }
