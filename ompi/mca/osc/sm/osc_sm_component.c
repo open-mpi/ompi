972f7121dd00ad0bb84b45e33b7b7b6093b8a3d3
Jeff Squyres <jsquyres@cisco.com>
2015-02-24 03:36:17 -0800

    osc_sm: Fix valgrind warning
    Many thanks to Lisandro Dalcin for contributing this patch.
    
    Fixes open-mpi/ompi#202.

--- ompi/mca/osc/sm/osc_sm_component.c
+++ ompi/mca/osc/sm/osc_sm_component.c
@@ -4,6 +4,7 @@
  * Copyright (c) 2014      Los Alamos National Security, LLC. All rights
  *                         reserved.
  * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * Copyright (c) 2015 Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -331,7 +332,7 @@ component_select(struct ompi_win_t *win, void **base, size_t size, int disp_unit
             goto error;
         }
 
-        if (blocking_fence) {
+        if (flag && blocking_fence) {
             ret = pthread_mutexattr_init(&mattr);
             ret = pthread_mutexattr_setpshared(&mattr, PTHREAD_PROCESS_SHARED);
             if (ret != 0) {
