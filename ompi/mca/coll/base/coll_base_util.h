ced44e12da23e975290ec538bb7a7868c15b3571
George Bosilca <bosilca@icl.utk.edu>
2015-02-24 18:49:23 -0500

    Update copyright.

--- ompi/mca/coll/base/coll_base_util.h
+++ ompi/mca/coll/base/coll_base_util.h
@@ -2,19 +2,19 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2014 The University of Tennessee and The University
+ * Copyright (c) 2004-2015 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
- * Copyright (c) 2004-2007 High Performance Computing Center Stuttgart, 
+ * Copyright (c) 2004-2007 High Performance Computing Center Stuttgart,
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2014      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
- * 
+ *
  * Additional copyrights may follow
- * 
+ *
  * $HEADER$
  */
 
@@ -36,10 +36,10 @@ BEGIN_C_DECLS
  * If one of the communications results in a zero-byte message the
  * communication is ignored, and no message will cross to the peer.
  */
-int ompi_coll_base_sendrecv_nonzero_actual( void* sendbuf, size_t scount, 
+int ompi_coll_base_sendrecv_nonzero_actual( void* sendbuf, size_t scount,
                                              ompi_datatype_t* sdatatype,
                                              int dest, int stag,
-                                             void* recvbuf, size_t rcount, 
+                                             void* recvbuf, size_t rcount,
                                              ompi_datatype_t* rdatatype,
                                              int source, int rtag,
                                              struct ompi_communicator_t* comm,
@@ -56,21 +56,19 @@ static inline int
 ompi_coll_base_sendrecv( void* sendbuf, size_t scount, ompi_datatype_t* sdatatype,
                           int dest, int stag,
                           void* recvbuf, size_t rcount, ompi_datatype_t* rdatatype,
-                          int source, int rtag, 
+                          int source, int rtag,
                           struct ompi_communicator_t* comm,
                           ompi_status_public_t* status, int myid )
 {
     if ((dest == source) && (source == myid)) {
-        return (int) ompi_datatype_sndrcv(sendbuf, (int32_t) scount, sdatatype, 
+        return (int) ompi_datatype_sndrcv(sendbuf, (int32_t) scount, sdatatype,
                                           recvbuf, (int32_t) rcount, rdatatype);
     }
-    return ompi_coll_base_sendrecv_nonzero_actual (sendbuf, scount, sdatatype, 
-                                            dest, stag, 
+    return ompi_coll_base_sendrecv_nonzero_actual (sendbuf, scount, sdatatype,
+                                            dest, stag,
                                             recvbuf, rcount, rdatatype,
                                             source, rtag, comm, status);
 }
 
 END_C_DECLS
 #endif /* MCA_COLL_BASE_UTIL_EXPORT_H */
-
-
