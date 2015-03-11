18c6a0502bd1ed7c61423ac610b875c5e912021f
Alina Sklarevich <alinas@mellanox.com>
2015-03-11 10:58:54 +0200

    PML_YALLA: fix compilation warnings.

--- ompi/mca/pml/yalla/pml_yalla_request.c
+++ ompi/mca/pml/yalla/pml_yalla_request.c
@@ -35,7 +35,7 @@ static inline int
 mca_pml_yalla_check_request_state(mca_pml_yalla_base_request_t *req)
 {
     if (req->mxm_base->state != MXM_REQ_COMPLETED) {
-         PML_YALLA_VERBOSE(8, "request %p free called before completed", req);
+         PML_YALLA_VERBOSE(8, "request %p free called before completed", (void *)req);
          req->flags |= MCA_PML_YALLA_REQUEST_FLAG_FREE_CALLED;
          return 0;
     }
@@ -47,7 +47,7 @@ static int mca_pml_yalla_send_request_free(ompi_request_t **request)
 {
     mca_pml_yalla_base_request_t *req = (mca_pml_yalla_base_request_t*)(*request);
 
-    PML_YALLA_VERBOSE(9, "free send request *%p=%p", request, *request);
+    PML_YALLA_VERBOSE(9, "free send request *%p=%p", (void *)request, (void *)*request);
 
     OPAL_THREAD_LOCK(&ompi_request_lock);
     if (mca_pml_yalla_check_request_state(req)) {
@@ -69,18 +69,18 @@ static int mca_pml_yalla_send_request_cancel(ompi_request_t *request, int flag)
          * This might be a buffered send request which has completed anyway, so
          * we cannot cancel it anymore. Just hope for the best.
          */
-        PML_YALLA_VERBOSE(7, "not canceling a completed send request %p", request);
+        PML_YALLA_VERBOSE(7, "not canceling a completed send request %p", (void *)request);
         return OMPI_SUCCESS;
     }
 
     error = mxm_req_cancel_send(&sreq->mxm);
     if ((error != MXM_OK) && (error != MXM_ERR_NO_PROGRESS)) {
-        PML_YALLA_ERROR("failed to cancel send request %p: %s", request,
+        PML_YALLA_ERROR("failed to cancel send request %p: %s", (void *)request,
                         mxm_error_string(error));
         return OMPI_ERROR;
     }
 
-    PML_YALLA_VERBOSE(9, "canceled send request %p", request);
+    PML_YALLA_VERBOSE(9, "canceled send request %p", (void *)request);
     return OMPI_SUCCESS;
 }
 
@@ -88,7 +88,7 @@ static int mca_pml_yalla_recv_request_free(ompi_request_t **request)
 {
     mca_pml_yalla_base_request_t *req = (mca_pml_yalla_base_request_t*)(*request);
 
-    PML_YALLA_VERBOSE(9, "free receive request *%p=%p", request, *request);
+    PML_YALLA_VERBOSE(9, "free receive request *%p=%p", (void *)request, (void *)*request);
 
     OPAL_THREAD_LOCK(&ompi_request_lock);
     if (mca_pml_yalla_check_request_state(req)) {
@@ -107,12 +107,12 @@ static int mca_pml_yalla_recv_request_cancel(ompi_request_t *request, int flag)
 
     error = mxm_req_cancel_recv(&rreq->mxm);
     if ((error != MXM_OK) && (error != MXM_ERR_NO_PROGRESS)) {
-        PML_YALLA_ERROR("failed to cancel receive request %p: %s", request,
+        PML_YALLA_ERROR("failed to cancel receive request %p: %s", (void *)request,
                         mxm_error_string(error));
         return OMPI_ERROR;
     }
 
-    PML_YALLA_VERBOSE(9, "canceled receive request %p", request);
+    PML_YALLA_VERBOSE(9, "canceled receive request %p", (void *)request);
     return OMPI_SUCCESS;
 }
 
@@ -175,13 +175,13 @@ static void mca_pml_yalla_send_completion_cb(void *context)
         break;
     }
 
-    PML_YALLA_VERBOSE(8, "send request %p completed with status %s", sreq,
+    PML_YALLA_VERBOSE(8, "send request %p completed with status %s", (void *)sreq,
                    mxm_error_string(sreq->mxm.base.error));
 
     OPAL_THREAD_LOCK(&ompi_request_lock);
     ompi_request_complete(&sreq->super.ompi, true);
     if (sreq->super.flags & MCA_PML_YALLA_REQUEST_FLAG_FREE_CALLED) {
-        PML_YALLA_VERBOSE(7, "release request %p because free was already called", sreq);
+        PML_YALLA_VERBOSE(7, "release request %p because free was already called", (void *)sreq);
         mca_pml_yalla_request_release(&sreq->super, &ompi_pml_yalla.send_reqs);
     }
     OPAL_THREAD_UNLOCK(&ompi_request_lock);
@@ -191,7 +191,7 @@ static void mca_pml_yalla_bsend_completion_cb(void *context)
 {
     mca_pml_yalla_bsend_request_t *bsreq = context;
 
-    PML_YALLA_VERBOSE(8, "bsend request %p completed with status %s", bsreq,
+    PML_YALLA_VERBOSE(8, "bsend request %p completed with status %s", (void *)bsreq,
                       mxm_error_string(bsreq->mxm.base.error));
 
     mca_pml_base_bsend_request_free(bsreq->mxm.base.data.buffer.ptr);
@@ -205,8 +205,8 @@ static void mca_pml_yalla_recv_completion_cb(void *context)
     PML_YALLA_SET_RECV_STATUS(&rreq->mxm, rreq->mxm.completion.actual_len,
                               &rreq->super.ompi.req_status);
 
-    PML_YALLA_VERBOSE(8, "receive request %p completed with status %s source %d rtag %d(%d/0x%x) len %Zu",
-                      rreq, mxm_error_string(rreq->mxm.base.error),
+    PML_YALLA_VERBOSE(8, "receive request %p completed with status %s source %d rtag %d(%d/0x%x) len %zu",
+                      (void *)rreq, mxm_error_string(rreq->mxm.base.error),
                       rreq->mxm.completion.sender_imm, rreq->mxm.completion.sender_tag,
                       rreq->mxm.tag, rreq->mxm.tag_mask,
                       rreq->mxm.completion.actual_len);
@@ -214,7 +214,7 @@ static void mca_pml_yalla_recv_completion_cb(void *context)
     OPAL_THREAD_LOCK(&ompi_request_lock);
     ompi_request_complete(&rreq->super.ompi, true);
     if (rreq->super.flags & MCA_PML_YALLA_REQUEST_FLAG_FREE_CALLED) {
-        PML_YALLA_VERBOSE(7, "release request %p because free was already called", rreq);
+        PML_YALLA_VERBOSE(7, "release request %p because free was already called", (void *)rreq);
         mca_pml_yalla_request_release(&rreq->super, &ompi_pml_yalla.recv_reqs);
     }
     OPAL_THREAD_UNLOCK(&ompi_request_lock);
