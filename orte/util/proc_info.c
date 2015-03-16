5ae42c816e1f2f2ad7ca303f85601f0d66959a5c
Ralph Castain <rhc@open-mpi.org>
2015-03-16 16:25:01 -0700

    Attempt to reduce the RARP traffic during definition of allocations

--- orte/util/proc_info.c
+++ orte/util/proc_info.c
@@ -12,7 +12,7 @@
  * Copyright (c) 2009      Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2012      Los Alamos National Security, LLC.
  *                         All rights reserved.
- * Copyright (c) 2014      Intel, Inc. All rights reserved
+ * Copyright (c) 2014-2015 Intel, Inc. All rights reserved
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -32,14 +32,20 @@
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
+#if HAVE_ARPA_INET_H
+#include <arpa/inet.h>
+#endif
 #include <ctype.h>
 
 #include "opal/mca/base/base.h"
 #include "opal/mca/base/mca_base_var.h"
 #include "opal/util/argv.h"
+#include "opal/util/if.h"
 #include "opal/util/net.h"
 #include "opal/util/output.h"
 #include "opal/util/proc.h"
+
+#include "orte/util/attr.h"
 
 #include "orte/util/proc_info.h"
 
@@ -63,6 +69,7 @@ ORTE_DECLSPEC orte_proc_info_t orte_process_info = {
     .num_daemons =                     1,
     .num_nodes =                       1,
     .nodename =                        NULL,
+    .aliases =                         NULL,
     .pid =                             0,
     .proc_type =                       ORTE_PROC_TYPE_NONE,
     .sync_buf =                        NULL,
@@ -97,7 +104,8 @@ int orte_proc_info(void)
     char hostname[ORTE_MAX_HOSTNAME_SIZE];
     char **prefixes;
     bool match;
-
+    struct in_addr buf;
+    
     if (init) {
         return ORTE_SUCCESS;
     }
@@ -165,7 +173,8 @@ int orte_proc_info(void)
     gethostname(hostname, ORTE_MAX_HOSTNAME_SIZE);
     if (!orte_keep_fqdn_hostnames) {
         /* if the nodename is an IP address, do not mess with it! */
-        if (!opal_net_isaddr(hostname)) {
+        if (0 != inet_pton(AF_INET, hostname, &buf) &&
+            0 != inet_pton(AF_INET6, hostname, &buf)) {
             /* not an IP address, so remove any domain info */
             if (NULL != (ptr = strchr(hostname, '.'))) {
                 *ptr = '\0';
@@ -314,6 +323,45 @@ int orte_proc_info_finalize(void)
 
     OBJ_DESTRUCT(&orte_process_info.super);
 
+    opal_argv_free(orte_process_info.aliases);
+    
     init = false;
     return ORTE_SUCCESS;
+}
+
+bool orte_ifislocal(const char *hostname)
+{
+    int i;
+    
+    /* see if this matches our known hostname */
+    if (NULL != orte_process_info.nodename &&
+        0 == strcmp(hostname, orte_process_info.nodename)) {
+        return true;
+    }
+
+    /* see if it matches the generic "localhost" */
+    if (0 == strcmp(hostname, "localhost")) {
+        return true;
+    }
+    
+    /* see if it matches any of our known aliases */
+    if (NULL != orte_process_info.aliases) {
+        for (i=0; NULL != orte_process_info.aliases[i]; i++) {
+            if (0 == strcmp(hostname, orte_process_info.aliases[i])) {
+                return true;
+            }
+        }
+    }
+
+    /* okay, have to resolve the address - the opal_ifislocal
+     * function will not attempt to resolve the address if
+     * told not to do so */
+    if (opal_ifislocal(hostname)) {
+        /* add this to our known aliases */
+        opal_argv_append_nosize(&orte_process_info.aliases, hostname);
+        return true;
+    }
+
+    /* not me */
+    return false;
 }
