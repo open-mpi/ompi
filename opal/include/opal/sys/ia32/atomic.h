fb033e9bf0a55e5e3c20379598999a677753dafe
Nathan Hjelm <hjelmn@lanl.gov>
2015-03-25 13:39:53 -0600

    asm/ia32: do not use the = constraint modifier for input/output operands
    Also added support for the xchg instruction. The instruction is
    supported by ia32 and may benefit vader.
    
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- opal/include/opal/sys/ia32/atomic.h
+++ opal/include/opal/sys/ia32/atomic.h
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
@@ -12,6 +13,8 @@
  * Copyright (c) 2007-2010 Oracle and/or its affiliates.  All rights reserved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2015      Los Alamos National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
