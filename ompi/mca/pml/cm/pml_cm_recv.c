9d99f96d6cc3d6dd0975df68ca5663695639cf13
Jithin Jose <jithin.jose@intel.com>
2015-04-02 11:37:09 -0700

    - CM blocking send/recv optimizations
        This patch tries to do as little as possible in the PML CM blocking
        send/receive routines.  Basically, avoid creating and filling in an
        entire request object.  An OMPI-level request is still needed, but we
        can create that on the stack instead of going to a free list.
    
    Signed-off-by: Andrew Friedley <andrew.friedley@intel.com>
    Signed-off-by: Jithin Jose <jithin.jose@intel.com>

--- ompi/mca/pml/cm/pml_cm_recv.c
+++ ompi/mca/pml/cm/pml_cm_recv.c
@@ -82,6 +82,13 @@ mca_pml_cm_irecv(void *addr,
 }
 
 
+void mca_pml_cm_recv_fast_completion(struct mca_mtl_request_t *mtl_request)
+{
+    // Do nothing!
+    ompi_request_complete(mtl_request->ompi_req, true);
+    return;
+}
+
 int
 mca_pml_cm_recv(void *addr,
                 size_t count,
@@ -92,35 +99,53 @@ mca_pml_cm_recv(void *addr,
                 ompi_status_public_t * status)
 {
     int ret;
-    mca_pml_cm_thin_recv_request_t *recvreq;
-    ompi_proc_t* ompi_proc;
-    
-    MCA_PML_CM_THIN_RECV_REQUEST_ALLOC(recvreq);
-    if( OPAL_UNLIKELY(NULL == recvreq) ) return OMPI_ERR_OUT_OF_RESOURCE;
+    ompi_proc_t *ompi_proc;
+    opal_convertor_t convertor;
+    mca_pml_cm_request_t req;
+    mca_mtl_request_t *req_mtl =
+            alloca(sizeof(mca_mtl_request_t) + ompi_mtl->mtl_request_size);
 
-    MCA_PML_CM_THIN_RECV_REQUEST_INIT(recvreq,
-                                      ompi_proc,
-                                      comm, 
-                                      src,
-                                      datatype,
-                                      addr,
-                                      count);
-    
-    
-    MCA_PML_CM_THIN_RECV_REQUEST_START(recvreq, comm, tag, src, ret);
+    req_mtl->ompi_req = &req.req_ompi;
+    req_mtl->completion_callback = mca_pml_cm_recv_fast_completion;
+
+    req.req_pml_type = MCA_PML_CM_REQUEST_RECV_THIN;
+    req.req_free_called = false;
+    req.req_ompi.req_complete = false;
+    req.req_ompi.req_complete_cb = NULL;
+    req.req_ompi.req_state = OMPI_REQUEST_ACTIVE;
+    req.req_ompi.req_status.MPI_TAG = OMPI_ANY_TAG;
+    req.req_ompi.req_status.MPI_ERROR = OMPI_SUCCESS;
+    req.req_ompi.req_status._cancelled = 0;
+
+    if( MPI_ANY_SOURCE == src ) {
+        ompi_proc = ompi_proc_local_proc;
+    } else {
+        ompi_proc = ompi_comm_peer_lookup( comm, src );
+    }
+
+    opal_convertor_copy_and_prepare_for_recv(
+                                  ompi_proc->super.proc_convertor,
+                                  &(datatype->super),
+                                  count,
+                                  addr,
+                                  0,
+                                  &convertor );
+    ret = OMPI_MTL_CALL(irecv(ompi_mtl,
+                              comm,
+                              src,
+                              tag,
+                              &convertor,
+                              req_mtl));
     if( OPAL_UNLIKELY(OMPI_SUCCESS != ret) ) {
-        /* BWB - XXX - need cleanup of request here */
-        MCA_PML_CM_THIN_RECV_REQUEST_RETURN(recvreq);
         return ret;
     }
 
-    ompi_request_wait_completion(&recvreq->req_base.req_ompi);
+    ompi_request_wait_completion(&req.req_ompi);
 
     if (NULL != status) {  /* return status */
-        *status = recvreq->req_base.req_ompi.req_status;
+        *status = req.req_ompi.req_status;
     }
-    ret = recvreq->req_base.req_ompi.req_status.MPI_ERROR;
-    ompi_request_free( (ompi_request_t**)&recvreq );
+    ret = req.req_ompi.req_status.MPI_ERROR;
 
     return ret;
 }
