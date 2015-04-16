c4de46e2840f4f6ce85d1a041def61a4e5b17393
Mangala Jyothi Bhaskar <mjbhaskar@crill.cs.uh.edu>
2015-04-16 10:38:07 -0500

    Fix number of aggregators used in two phase fcoll

--- ompi/mca/fcoll/two_phase/fcoll_two_phase_file_write_all.c
+++ ompi/mca/fcoll/two_phase/fcoll_two_phase_file_write_all.c
@@ -222,8 +222,7 @@ mca_fcoll_two_phase_file_write_all (mca_io_ompio_file_t *fh,
 	    return  ret;
 	}
 	
-	two_phase_num_io_procs = 
-	    ceil((float)fh->f_size/fh->f_procs_per_group);
+	two_phase_num_io_procs = fh->f_final_num_aggrs; 
 	
     }
     
