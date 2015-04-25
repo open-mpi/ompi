d8e4b0431e2cb27e87f26353d7628cc868c1ccb3
Jeff Squyres <jsquyres@cisco.com>
2015-04-25 05:30:23 -0700

    common verbs: insert fake usnic_verbs libibverbs driver
    libibverbs will complain to stderr if it sees device entries in
    /sys/class/infiniband for which it has no userspace plugins.
    
    The Cisco usNIC device no longer exports a verbs interface, thereby
    causing libibverbs to emit this annoying stderr warning.
    
    To avoid this, use the public ibv API to register a "fake" libibverbs
    driver at run-time (right after we call ibv_fork_init(), but --
    critically -- *before* we call ibv_get_device_list()).  The purpose of
    this driver is solely to convince libibverbs that there *is* a driver
    for /sys/class/infininband/usnic_verbs devices.  ...although this
    driver will never return a valid ibv context (and therefore will never
    be used).

--- opal/mca/common/verbs/common_verbs_basics.c
+++ opal/mca/common/verbs/common_verbs_basics.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012-2015 Cisco Systems, Inc.  All rights reserved.
  *
  * $COPYRIGHT$
  * 
@@ -66,29 +66,34 @@ bool opal_common_verbs_check_basics(void)
 
 int opal_common_verbs_fork_test(void)
 {
-    /* Make sure that ibv_fork_init is called before the calls to other memory registering verbs,
-     * which will be called after this function */
+    int ret = OPAL_SUCCESS;
+
+    /* Make sure that ibv_fork_init() is the first ibv_* function to
+       be invoked in this process. */
 #ifdef HAVE_IBV_FORK_INIT
     if (0 != opal_common_verbs_want_fork_support) {
         /* Check if fork support is requested by the user */
         if (0 != ibv_fork_init()) {
-            /* If the opal_common_verbs_want_fork_support MCA parameter is >0 but
-             * the call to ibv_fork_init() failed, then return an error code.
+            /* If the opal_common_verbs_want_fork_support MCA
+             * parameter is >0 but the call to ibv_fork_init() failed,
+             * then return an error code.
              */
             if (opal_common_verbs_want_fork_support > 0) {
                 opal_show_help("help-opal-common-verbs.txt",
                                "ibv_fork_init fail", true,
                                opal_proc_local_get()->proc_hostname, errno,
                                strerror(errno));
-                return OPAL_ERROR;
+                ret = OPAL_ERROR;
             }
-        } else {
-            return OPAL_SUCCESS;
         }
-    } else {
-        return OPAL_SUCCESS;
     }
 #endif
-    return OPAL_SUCCESS;
+
+    /* Now rgister any necessary fake libibverbs drivers.  We
+       piggyback loading these fake drivers on the fork test because
+       they must be loaded before ibv_get_device_list() is invoked. */
+    opal_common_verbs_register_fake_drivers();
+
+    return ret;
 }
 
