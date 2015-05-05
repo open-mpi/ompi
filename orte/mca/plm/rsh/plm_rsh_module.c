f5405c7ce598a0c98e45d4e951378957c10658a6
Ralph Castain <rhc@open-mpi.org>
2015-05-05 15:00:31 -0700

    ckpt

--- orte/mca/plm/rsh/plm_rsh_module.c
+++ orte/mca/plm/rsh/plm_rsh_module.c
@@ -1520,25 +1520,43 @@ static int setup_shell(orte_plm_rsh_shell_t *rshell,
     orte_plm_rsh_shell_t remote_shell, local_shell;
     char *param;
     int rc;
+    FILE *fp;
+    char path[1035];
 
     /* What is our local shell? */
     local_shell = ORTE_PLM_RSH_SHELL_UNKNOWN;
 
-#if OPAL_ENABLE_GETPWUID
-    {
-        struct passwd *p;
 
-        p = getpwuid(getuid());
-        if( NULL == p ) {
-            /* This user is unknown to the system. Therefore, there is no reason we
-             * spawn whatsoever in his name. Give up with a HUGE error message.
-             */
-            orte_show_help( "help-plm-rsh.txt", "unknown-user", true, (int)getuid() );
-            return ORTE_ERR_FATAL;
-        }
-        param = p->pw_shell;
-        local_shell = find_shell(p->pw_shell);
+    /* Open the command for reading. */
+    fp = popen("ps -hp $$|awk '{echo $5}'", "r");
+    if (fp == NULL) {
+        orte_show_help( "help-plm-rsh.txt", "unknown-user", true, (int)getuid() );
+        printf("Failed to run command\n" );
+        return ORTE_ERR_OUT_OF_RESOURCE;
     }
+
+    /* Read the output a line at a time - output it. */
+    while (fgets(path, sizeof(path)-1, fp) != NULL) {
+        printf("%s", path);
+    }
+    /* close */
+    pclose(fp);
+
+#if OPAL_ENABLE_GETPWUID
+        {
+            struct passwd *p;
+
+            p = getpwuid(getuid());
+            if( NULL == p ) {
+                /* This user is unknown to the system. Therefore, there is no reason we
+                 * spawn whatsoever in his name. Give up with a HUGE error message.
+                 */
+                orte_show_help( "help-plm-rsh.txt", "unknown-user", true, (int)getuid() );
+                return ORTE_ERR_FATAL;
+            }
+            param = p->pw_shell;
+            local_shell = find_shell(p->pw_shell);
+        }
 #endif
 
     /* If we didn't find it in getpwuid(), try looking at the $SHELL
