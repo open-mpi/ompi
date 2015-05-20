88c54c12634560154933d75b8fb3c5dbf6a2d04e
Jeff Squyres <jsquyres@cisco.com>
2015-05-20 17:24:21 -0400

    oob_tcp: do not set KEEPALIVE on listening sockets
    (cherry picked from commit open-mpi/ompi@c95215dfc2a199fc626fa89e614559ca5e7959f7)

--- orte/mca/oob/tcp/oob_tcp_listener.c
+++ orte/mca/oob/tcp/oob_tcp_listener.c
@@ -11,7 +11,7 @@
  *                         All rights reserved.
  * Copyright (c) 2006-2013 Los Alamos National Security, LLC. 
  *                         All rights reserved.
- * Copyright (c) 2009-2014 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2009-2015 Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2011      Oak Ridge National Labs.  All rights reserved.
  * Copyright (c) 2013-2014 Intel, Inc.  All rights reserved.
  * $COPYRIGHT$
@@ -304,9 +304,6 @@ static int create_listen(void)
             return ORTE_ERR_IN_ERRNO;
         }
 
-        /* setup socket options */
-        orte_oob_tcp_set_socket_options(sd);
-
         /* Enable/disable reusing ports */
         if (orte_static_ports) {
             flags = 1;
@@ -556,10 +553,6 @@ static int create_listen6(void)
             opal_argv_free(ports);
             return ORTE_ERROR;
         }
-
-
-        /* setup socket options */
-        orte_oob_tcp_set_socket_options(sd);
 
         /* Enable/disable reusing ports */
         if (orte_static_ports) {
