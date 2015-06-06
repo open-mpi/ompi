cddc8945e056eb05e5ad49de4578fe36f1c198a6
Jeff Squyres <jsquyres@cisco.com>
2015-06-06 10:09:43 -0700

    btl_tcp_proc.c: add missing "continue"
    Also add another (superflous but symmetric) continue statement.
    
    This missing "continue" statement allows IPv4 "private network"
    matches to fall through and allow IPv6 matches to be made -- thereby
    overriding the IPv4 match that was already made.
    
    Fixes #585 (although several of the other issues identified on #585
    still exist, the primary / initial bug that was reported there is now
    fixed).

--- opal/mca/btl/tcp/btl_tcp_proc.c
+++ opal/mca/btl/tcp/btl_tcp_proc.c
@@ -14,6 +14,7 @@
  * Copyright (c) 2013-2014 Intel, Inc. All rights reserved
  * Copyright (c) 2014-2015 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2015 Cisco Systems, Inc.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -557,6 +558,7 @@ int mca_btl_tcp_proc_insert( mca_btl_tcp_proc_t* btl_proc,
                         weights[i][j] = CQ_PRIVATE_DIFFERENT_NETWORK;
                     }
                     best_addr[i][j] = peer_interfaces[j]->ipv4_endpoint_addr;
+                    continue;
                 }
             }
 
@@ -584,6 +586,7 @@ int mca_btl_tcp_proc_insert( mca_btl_tcp_proc_t* btl_proc,
                     weights[i][j] = CQ_PUBLIC_DIFFERENT_NETWORK;
                 }
                 best_addr[i][j] = peer_interfaces[j]->ipv6_endpoint_addr;
+                continue;
             } 
 
         } /* for each peer interface */
