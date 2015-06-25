bureddy/ompi-release

powerpc: update mem barrier instructions
       - added isync interface.
       - define ocoms_atomic_wmb() to lwsync as it is recommend over eieio
         on cache enabled storage.
        (http://www.ibm.com/developerworks/systems/articles/powerpc.html).

opal/include/opal/sys/amd64/atomic.h
@@ -66,6 +66,10 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void ocoms_atomic_isync(void)
+{
+}
+
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 
 

opal/include/opal/sys/ia32/atomic.h
@@ -75,6 +75,10 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void ocoms_atomic_isync(void)
+{
+}
+
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 
 

opal/include/opal/sys/ia64/atomic.h
@@ -61,6 +61,9 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void ocoms_atomic_isync(void)
+{
+}
 
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 

opal/include/opal/sys/osx/atomic.h
@@ -67,6 +67,10 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void ocoms_atomic_isync(void)
+{
+}
+
 /**********************************************************************
  *
  * Atomic math operations

opal/include/opal/sys/sparcv9/atomic.h
@@ -65,6 +65,11 @@ static inline void opal_atomic_wmb(void)
     MEMBAR("#StoreStore");
 }
 
+static inline void ocoms_atomic_isync(void)
+{
+}
+
+
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 
 

open-mpi/ompi

       - define opal_atomic_wmb() to lwsync as it is recommend over eieio

opal/include/opal/sys/amd64/atomic.h
@@ -66,6 +66,10 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void opal_atomic_isync(void)
+{
+}
+
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 
 

opal/include/opal/sys/ia32/atomic.h
@@ -75,6 +75,10 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void opal_atomic_isync(void)
+{
+}
+
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 
 

opal/include/opal/sys/ia64/atomic.h
@@ -61,6 +61,9 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void opal_atomic_isync(void)
+{
+}
 
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 

opal/include/opal/sys/osx/atomic.h
@@ -67,6 +67,10 @@ static inline void opal_atomic_wmb(void)
     MB();
 }
 
+static inline void opal_atomic_isync(void)
+{
+}
+
 /**********************************************************************
  *
  * Atomic math operations

opal/include/opal/sys/sparcv9/atomic.h
@@ -65,6 +65,11 @@ static inline void opal_atomic_wmb(void)
     MEMBAR("#StoreStore");
 }
 
+static inline void opal_atomic_isync(void)
+{
+}
+
+
 #endif /* OPAL_GCC_INLINE_ASSEMBLY */
 
 