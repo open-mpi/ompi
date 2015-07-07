68c69d07ff6f118d1d9990563c8888d6f4d4f832
Howard Pritchard <howardp@lanl.gov>
2015-07-07 14:35:36 -0600

    add copyrights to changed java related files
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- ompi/mpi/java/java/Win.java
+++ ompi/mpi/java/java/Win.java
@@ -11,6 +11,8 @@
  *                         All rights reserved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2015      Los Alamos National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
