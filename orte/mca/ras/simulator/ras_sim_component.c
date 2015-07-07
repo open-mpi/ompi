a09d1677b41a8aa604d5e9501cffecba0f85bf90
Ralph Castain <rhc@open-mpi.org>
2015-07-07 12:52:16 -0700

    Fix hetero operations. An error in the hwloc utilities only allocated memory for the first display of a binding map, and then assumed that all nodes had the same number of cores in them. This resulted in memory corruption whenever someone displayed a binding pattern for a hetero cluster, and a smaller node was first in line.
    (cherry picked from commit open-mpi/ompi@ed93154e43ed7c7fdf09205c42922e1790a8a67c)

--- orte/mca/ras/simulator/ras_sim_component.c
+++ orte/mca/ras/simulator/ras_sim_component.c
@@ -12,6 +12,7 @@
  *                         All rights reserved.
  * Copyright (c) 2015      Los Alamos National Security, LLC.  All rights
  *                         reserved.
+ * Copyright (c) 2015      Intel, Inc. All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -95,6 +96,13 @@ static int ras_sim_register(void)
                                             OPAL_INFO_LVL_9,
                                             MCA_BASE_VAR_SCOPE_READONLY,
                                             &mca_ras_simulator_component.topofiles);
+    mca_ras_simulator_component.topologies = NULL;
+    (void) mca_base_component_var_register (component, "topologies",
+                                            "Comma-separated list of topology descriptions for simulated nodes",
+                                            MCA_BASE_VAR_TYPE_STRING, NULL, 0, 0,
+                                            OPAL_INFO_LVL_9,
+                                            MCA_BASE_VAR_SCOPE_READONLY,
+                                            &mca_ras_simulator_component.topologies);
     mca_ras_simulator_component.have_cpubind = true;
     (void) mca_base_component_var_register (component, "have_cpubind",
                                             "Topology supports binding to cpus",
