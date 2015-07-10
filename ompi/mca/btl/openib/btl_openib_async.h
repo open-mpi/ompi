bccbbb90edb5ed8a768046405a5432462592797b
Gilles Gouaillardet <gilles@rist.or.jp>
2015-07-10 16:05:24 +0900

    configury: fix (again) XRC detection on OFED < 3.12 since ibv_create_xrc_rcv_qp is now deprecated, and in order to be "future-proof", we have to consider the case in which only XRC Domains are supported. also, correctly handle distro that ship broken ibverbs devel headers.
    Thanks Paul Hargrove for the detailled report.
    
    (back-ported from commit open-mpi/ompi@f7cf7d50708e997be77ee320661790af7fe34f97)

--- ompi/mca/btl/openib/btl_openib_async.h
+++ ompi/mca/btl/openib/btl_openib_async.h
@@ -19,7 +19,7 @@
 int        start_async_event_thread(void);
 void       mca_btl_openib_load_apm(struct ibv_qp *qp, mca_btl_openib_endpoint_t *ep);
 int        btl_openib_async_command_done(int exp);
-#if HAVE_XRC && ! OMPI_HAVE_CONNECTX_XRC_DOMAINS
+#if OMPI_HAVE_CONNECTX_XRC
 void       mca_btl_openib_load_apm_xrc_rcv(uint32_t qp_num, mca_btl_openib_endpoint_t *ep);
 #endif
 
