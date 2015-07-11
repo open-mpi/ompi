2c896c5a2d51fc2348cf95015005b7f8baa8fc83
Ralph Castain <rhc@open-mpi.org>
2015-07-11 10:43:11 -0700

    Default allocated nodes to the UP state

--- orte/mca/ras/alps/ras_alps_module.c
+++ orte/mca/ras/alps/ras_alps_module.c
@@ -543,6 +543,7 @@ orte_ras_alps_read_appinfo_file(opal_list_t *nodes, char *filename,
                 node->slots_inuse = 0;
                 node->slots_max = 0;
                 node->slots = 1;
+                node->state = ORTE_NODE_STATE_UP;
                 /* need to order these node ids so the regex generator
                  * can properly function
                  */
@@ -579,6 +580,7 @@ orte_ras_alps_read_appinfo_file(opal_list_t *nodes, char *filename,
             node->slots_inuse = 0;
             node->slots_max = 0;
             node->slots = apNodes[ix].numPEs;
+            node->state = ORTE_NODE_STATE_UP;
             /* need to order these node ids so the regex generator
              * can properly function
              */
