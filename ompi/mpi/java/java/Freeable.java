88ca3026e5af88efb3e6360f93e93accb44ab990
Nathaniel Graham <ngraham@lanl.gov>
2015-07-24 11:02:13 -0600

    Formatting fixes for Java files.
    Fixed tab indenting as well as some of the block comments
    at the top of a few of the files.
    
    Signed-off-by: Nathaniel Graham <ngraham@lanl.gov>
    
    Conflicts:
    	ompi/mpi/java/java/CartComm.java
    	ompi/mpi/java/java/Comm.java
    	ompi/mpi/java/java/Constant.java
    	ompi/mpi/java/java/Errhandler.java
    	ompi/mpi/java/java/Intercomm.java
    	ompi/mpi/java/java/Intracomm.java
    	ompi/mpi/java/java/MPI.java
    	ompi/mpi/java/java/Request.java
    	ompi/mpi/java/java/Struct.java
    	ompi/mpi/java/java/UserFunction.java

--- ompi/mpi/java/java/Freeable.java
+++ ompi/mpi/java/java/Freeable.java
@@ -16,26 +16,26 @@
  * Additional copyrights may follow
  * 
  * $HEADER$
- */
-/*
+ *
+ *
  * This file is almost a complete re-write for Open MPI compared to the
  * original mpiJava package. Its license and copyright are listed below.
  * See <path to ompi/mpi/java/README> for more information.
- */
-/*
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-/*
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
  * File         : Freeable.java
  * Author       : Bryan Carpenter
  * Created      : Wed Jan 15 23:14:43 EST 2003
@@ -50,10 +50,10 @@ package mpi;
  */
 public interface Freeable
 {
-    /**
-     * Frees a freeable object.
-     * @throws MPIException Signals that an MPI exception of some sort has occurred.
-     */
-    void free() throws MPIException;
+	/**
+	 * Frees a freeable object.
+	 * @throws MPIException Signals that an MPI exception of some sort has occurred.
+	 */
+	void free() throws MPIException;
 }
 
--- ompi/mpi/java/java/LongInt.java
+++ ompi/mpi/java/java/LongInt.java
@@ -25,110 +25,110 @@ package mpi;
  */
 public final class LongInt extends Struct
 {
-private final int lSize, iOff, iSize;
+	private final int lSize, iOff, iSize;
 
-/**
- * The struct object will be created only in MPI class.
- * @param longSize	size of long
- * @param intOff	int offset
- * @param intSize	int size
- * @see MPI#longInt
- */
-protected LongInt(int longSize, int intOff, int intSize)
-{
-    lSize = longSize;
-    iSize = intSize;
-    int lOff;
+	/**
+	 * The struct object will be created only in MPI class.
+	 * @param longSize	size of long
+	 * @param intOff	int offset
+	 * @param intSize	int size
+	 * @see MPI#longInt
+	 */
+	protected LongInt(int longSize, int intOff, int intSize)
+	{
+		lSize = longSize;
+		iSize = intSize;
+		int lOff;
 
-    switch(lSize)
-    {
-        case 4: lOff = addInt();  break;
-        case 8: lOff = addLong(); break;
-        default: throw new AssertionError("Unsupported long size: "+ lSize);
-    }
+		switch(lSize)
+		{
+		case 4: lOff = addInt();  break;
+		case 8: lOff = addLong(); break;
+		default: throw new AssertionError("Unsupported long size: "+ lSize);
+		}
 
-    assert lOff == 0;
-    setOffset(intOff);
+		assert lOff == 0;
+		setOffset(intOff);
 
-    switch(iSize)
-    {
-        case 4: iOff = addInt();  break;
-        case 8: iOff = addLong(); break;
-        default: throw new AssertionError("Unsupported int size: "+ iSize);
-    }
+		switch(iSize)
+		{
+		case 4: iOff = addInt();  break;
+		case 8: iOff = addLong(); break;
+		default: throw new AssertionError("Unsupported int size: "+ iSize);
+		}
 
-    assert(intOff == iOff);
-}
+		assert(intOff == iOff);
+	}
 
-/**
- * Creates a Data object.
- * @return new Data object.
- */
-@Override protected LongInt.Data newData()
-{
-    return new LongInt.Data();
-}
+	/**
+	 * Creates a Data object.
+	 * @return new Data object.
+	 */
+	@Override protected LongInt.Data newData()
+	{
+		return new LongInt.Data();
+	}
 
-/**
- * Class for reading/writing data in a struct stored in a byte buffer.
- */
-public final class Data extends Struct.Data
-{
-    /**
-     * Gets the long value.
-     * @return long value
-     */
-    public long getValue()
-    {
-        switch(lSize)
-        {
-            case 8: return getLong(0);
-            case 4: return getInt(0);
-            default: throw new AssertionError();
-        }
-    }
+	/**
+	 * Class for reading/writing data in a struct stored in a byte buffer.
+	 */
+	public final class Data extends Struct.Data
+	{
+		/**
+		 * Gets the long value.
+		 * @return long value
+		 */
+		public long getValue()
+		{
+			switch(lSize)
+			{
+			case 8: return getLong(0);
+			case 4: return getInt(0);
+			default: throw new AssertionError();
+			}
+		}
 
-    /**
-     * Gets the int value.
-     * @return int value
-     */
-    public int getIndex()
-    {
-        switch(iSize)
-        {
-            case 4: return getInt(iOff);
-            case 8: return (int)getLong(iOff);
-            default: throw new AssertionError();
-        }
-    }
+		/**
+		 * Gets the int value.
+		 * @return int value
+		 */
+		public int getIndex()
+		{
+			switch(iSize)
+			{
+			case 4: return getInt(iOff);
+			case 8: return (int)getLong(iOff);
+			default: throw new AssertionError();
+			}
+		}
 
-    /**
-     * Puts the long value.
-     * @param v long value
-     */
-    public void putValue(long v)
-    {
-        switch(lSize)
-        {
-            case 8: putLong(0, v);     break;
-            case 4: putInt(0, (int)v); break;
-            default: throw new AssertionError();
-        }
-    }
+		/**
+		 * Puts the long value.
+		 * @param v long value
+		 */
+		public void putValue(long v)
+		{
+			switch(lSize)
+			{
+			case 8: putLong(0, v);     break;
+			case 4: putInt(0, (int)v); break;
+			default: throw new AssertionError();
+			}
+		}
 
-    /**
-     * Puts the int value.
-     * @param v int value
-     */
-    public void putIndex(int v)
-    {
-        switch(iSize)
-        {
-            case 4: putInt(iOff, v);  break;
-            case 8: putLong(iOff, v); break;
-            default: throw new AssertionError();
-        }
-    }
-} // Data
+		/**
+		 * Puts the int value.
+		 * @param v int value
+		 */
+		public void putIndex(int v)
+		{
+			switch(iSize)
+			{
+			case 4: putInt(iOff, v);  break;
+			case 8: putLong(iOff, v); break;
+			default: throw new AssertionError();
+			}
+		}
+	} // Data
 
 } // LongInt
