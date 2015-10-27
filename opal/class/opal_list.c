02cdd046bda2feaa7e269ef0e8c1f16980c7da49
Ralph Castain <rhc@open-mpi.org>
2015-10-27 15:24:48 -0700

    Revert "  Releasing the list items when list destructor is called"
    This reverts commit 7579ae3086836af348b0564f032be709d24e097f.

--- opal/class/opal_list.c
+++ opal/class/opal_list.c
@@ -97,17 +97,11 @@ static void opal_list_construct(opal_list_t *list)
 
 
 /*
- * Release the list items in the list.
- * Reset list pointers to be NULL
+ * Reset all the pointers to be NULL -- do not actually destroy
+ * anything.
  */
 static void opal_list_destruct(opal_list_t *list)
 {
-    opal_list_item_t *it;
-
-    while (NULL != (it = opal_list_remove_first(list))) {
-        OBJ_RELEASE(it);
-    }
-
     opal_list_construct(list);
 }
 
