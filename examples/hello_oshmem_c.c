af6ce46f6b89277306f4d08b991ee4b30bf9e546
Igor Ivanov <Igor.Ivanov@itseez.com>
2015-11-19 16:34:15 +0200

    oshmem: Align OSHMEM API with spec v1.2 (change examples)

--- examples/hello_oshmem_c.c
+++ examples/hello_oshmem_c.c
@@ -15,13 +15,18 @@
 int main(int argc, char* argv[])
 {
     int proc, nproc;
+    char name[SHMEM_MAX_NAME_LEN];
+    int major, minor;
 
-    start_pes(0);
-    nproc = _num_pes();
-    proc = _my_pe();
+    shmem_init();
+    nproc = shmem_n_pes();
+    proc = shmem_my_pe();
+    shmem_info_get_name(name);
+    shmem_info_get_version(&major, &minor);
 
-    printf("Hello, world, I am %d of %d\n",
-           proc, nproc);
+    printf("Hello, world, I am %d of %d: %s (version: %d.%d)\n",
+           proc, nproc, name, major, minor);
+    shmem_finalize();
 
     return 0;
 }
