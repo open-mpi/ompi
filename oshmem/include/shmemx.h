456d32716ab75eefe0b061e62eb2715f5a06ab0b
Igor Ivanov <Igor.Ivanov@itseez.com>
2015-11-24 17:41:24 +0200

    oshmem: Align OSHMEM API with spec v1.2 (backward compatibility for extensions)

--- oshmem/include/shmemx.h
+++ oshmem/include/shmemx.h
@@ -126,6 +126,70 @@ OSHMEM_DECLSPEC void shmemx_int16_prod_to_all(int16_t *target, int16_t *source, 
 OSHMEM_DECLSPEC void shmemx_int32_prod_to_all(int32_t *target, int32_t *source, int nreduce, int PE_start, int logPE_stride, int PE_size, int32_t *pWrk, long *pSync);
 OSHMEM_DECLSPEC void shmemx_int64_prod_to_all(int64_t *target, int64_t *source, int nreduce, int PE_start, int logPE_stride, int PE_size, int64_t *pWrk, long *pSync);
 
+/*
+ * Backward compatibility section
+ */
+#define shmem_int16_p               shmemx_int16_p
+#define shmem_int32_p               shmemx_int32_p
+#define shmem_int64_p               shmemx_int64_p
+
+#define shmem_put16                 shmemx_put16
+#define shmem_iput16                shmemx_iput16
+
+#define shmem_int16_g               shmemx_int16_g
+#define shmem_int32_g               shmemx_int32_g
+#define shmem_int64_g               shmemx_int64_g
+
+#define shmem_get16                 shmemx_get16
+#define shmem_iget16                shmemx_iget16
+
+#define shmem_int32_swap            shmemx_int32_swap
+#define shmem_int64_swap            shmemx_int64_swap
+#define shmem_int32_cswap           shmemx_int32_cswap
+#define shmem_int64_cswap           shmemx_int64_cswap
+
+#define shmem_int32_fadd            shmemx_int32_fadd
+#define shmem_int64_fadd            shmemx_int64_fadd
+#define shmem_int32_finc            shmemx_int32_finc
+#define shmem_int64_finc            shmemx_int64_finc
+#define shmem_int32_add             shmemx_int32_add
+#define shmem_int64_add             shmemx_int64_add
+#define shmem_int32_inc             shmemx_int32_inc
+#define shmem_int64_inc             shmemx_int64_inc
+
+#define shmem_int32_wait            shmemx_int32_wait
+#define shmem_int64_wait            shmemx_int64_wait
+#define shmem_int32_wait_until      shmemx_int32_wait_until
+#define shmem_int64_wait_until      shmemx_int64_wait_until
+
+#define shmem_int16_and_to_all      shmemx_int16_and_to_all
+#define shmem_int32_and_to_all      shmemx_int32_and_to_all
+#define shmem_int64_and_to_all      shmemx_int64_and_to_all
+
+#define shmem_int16_or_to_all       shmemx_int16_or_to_all
+#define shmem_int32_or_to_all       shmemx_int32_or_to_all
+#define shmem_int64_or_to_all       shmemx_int64_or_to_all
+
+#define shmem_int16_xor_to_all      shmemx_int16_xor_to_all
+#define shmem_int32_xor_to_all      shmemx_int32_xor_to_all
+#define shmem_int64_xor_to_all      shmemx_int64_xor_to_all
+
+#define shmem_int16_max_to_all      shmemx_int16_max_to_all
+#define shmem_int32_max_to_all      shmemx_int32_max_to_all
+#define shmem_int64_max_to_all      shmemx_int64_max_to_all
+
+#define shmem_int16_min_to_all      shmemx_int16_min_to_all
+#define shmem_int32_min_to_all      shmemx_int32_min_to_all
+#define shmem_int64_min_to_all      shmemx_int64_min_to_all
+
+#define shmem_int16_sum_to_all      shmemx_int16_sum_to_all
+#define shmem_int32_sum_to_all      shmemx_int32_sum_to_all
+#define shmem_int64_sum_to_all      shmemx_int64_sum_to_all
+
+#define shmem_int16_prod_to_all     shmemx_int16_prod_to_all
+#define shmem_int32_prod_to_all     shmemx_int32_prod_to_all
+#define shmem_int64_prod_to_all     shmemx_int64_prod_to_all
+
 #if defined(c_plusplus) || defined(__cplusplus)
 }
 #endif
