ab70ca6d16d9a018c1e77f45a8880e74485096a4
Igor Ivanov <Igor.Ivanov@itseez.com>
2015-11-25 13:27:34 +0200

    Revert "oshmem: Add new mca variables oshmem_abort_delay and oshmem_abort_print_stack"
    This commit is replaced with more suitable.
    
    This reverts commit f288cd7254ef4b35387848394ec16b6ec4f9674d.

--- oshmem/runtime/oshmem_shmem_params.c
+++ oshmem/runtime/oshmem_shmem_params.c
@@ -13,37 +13,12 @@
 #include "oshmem/constants.h"
 
 
-bool oshmem_shmem_abort_print_stack = false;
-int oshmem_shmem_abort_delay = 0;
 int oshmem_shmem_lock_recursive = 0;
 int oshmem_shmem_api_verbose = 0;
 int oshmem_preconnect_all = 0;
 
 int oshmem_shmem_register_params(void)
 {
-    oshmem_shmem_abort_delay = 0;
-    (void) mca_base_var_register("oshmem",
-                                 "oshmem",
-                                 NULL,
-                                 "abort_delay",
-                                "If nonzero, print out an identifying message when abort is invoked (hostname, PID of the process that called abort operation) and delay for that many seconds before exiting (a negative delay value means to never abort).  This allows attaching of a debugger before quitting the job.",
-                                 MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
-                                 OPAL_INFO_LVL_9,
-                                 MCA_BASE_VAR_SCOPE_READONLY,
-                                 &oshmem_shmem_abort_delay);
-
-    oshmem_shmem_abort_print_stack = false;
-    (void) mca_base_var_register("oshmem",
-                                 "oshmem",
-                                 NULL,
-                                 "abort_print_stack",
-                                 "If nonzero, print out a stack trace when abort is invoked",
-                                 MCA_BASE_VAR_TYPE_BOOL, NULL, 0,
-                                 0,
-                                 OPAL_INFO_LVL_9,
-                                 MCA_BASE_VAR_SCOPE_READONLY,
-                                 &oshmem_shmem_abort_print_stack);
-
     (void) mca_base_var_register("oshmem",
                                  "oshmem",
                                  NULL,
