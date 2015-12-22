99d046d060ff53d4748d2749eebae4d9a2c4cff5
Gilles Gouaillardet <gilles@rist.or.jp>
2015-12-22 10:17:36 +0900

    scoll/fca: add missing #include <alloca.h>

--- oshmem/mca/scoll/fca/scoll_fca_ops.c
+++ oshmem/mca/scoll/fca/scoll_fca_ops.c
@@ -1,12 +1,19 @@
 /*
  * Copyright (c) 2013      Mellanox Technologies, Inc.
  *                         All rights reserved.
+ * Copyright (c) 2015      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
  *
  * $HEADER$
  */
+
+#ifdef HAVE_ALLOCA_H
+#include <alloca.h>
+#endif
+
 #include "oshmem_config.h"
 #include "oshmem/constants.h"
 #include "scoll_fca.h"
