7d7898d67ddaa4ebef57f9162de9316a95d12340
Edgar Gabriel <gabriel@cs.uh.edu>
2016-01-05 08:14:59 -0600

    revise the logic in the fbtl plfs avoiding the memcpy operation

--- ompi/mca/fbtl/plfs/fbtl_plfs_preadv.c
+++ ompi/mca/fbtl/plfs/fbtl_plfs_preadv.c
@@ -28,74 +28,19 @@
 ssize_t  mca_fbtl_plfs_preadv (mca_io_ompio_file_t *fh )
 {
 
-    Plfs_fd *pfd = NULL;
+    Plfs_fd *pfd = fh->f_fs_ptr;
     plfs_error_t plfs_ret;
-    pfd = fh->f_fs_ptr;
     ssize_t total_bytes_read=0;
-
-    int i, block=1;
-    struct iovec *iov = NULL;
-    int iov_count = 0;
-    OMPI_MPI_OFFSET_TYPE iov_offset = 0;
+    int i;
+    ssize_t bytes_read;
 
     if (NULL == fh->f_io_array) {
         return OMPI_ERROR;
     }
 
-    iov = (struct iovec *) malloc
-        (OMPIO_IOVEC_INITIAL_SIZE * sizeof (struct iovec));
-    if (NULL == iov) {
-        opal_output(1, "OUT OF MEMORY\n");
-        return OMPI_ERR_OUT_OF_RESOURCE;
-    }
-
     for (i=0 ; i<fh->f_num_of_io_entries ; i++) {
-	if (0 == iov_count) {
-	    iov[iov_count].iov_base = fh->f_io_array[i].memory_address;
-	    iov[iov_count].iov_len = fh->f_io_array[i].length;
-	    iov_offset = (OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[i].offset;
-	    iov_count ++;
-	}
-
-	if (OMPIO_IOVEC_INITIAL_SIZE*block <= iov_count) {
-	    block ++;
-	    iov = (struct iovec *)realloc
-		(iov, OMPIO_IOVEC_INITIAL_SIZE * block *
-		 sizeof(struct iovec));
-	    if (NULL == iov) {
-		opal_output(1, "OUT OF MEMORY\n");
-		return OMPI_ERR_OUT_OF_RESOURCE;
-	    }
-	}
-
-	if (fh->f_num_of_io_entries != i+1) {
-	    if (((OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[i].offset +
-		 (OPAL_PTRDIFF_TYPE)fh->f_io_array[i].length) ==
-		(OMPI_MPI_OFFSET_TYPE)(intptr_t)fh->f_io_array[i+1].offset) {
-		iov[iov_count].iov_base =
-		    fh->f_io_array[i+1].memory_address;
-		iov[iov_count].iov_len = fh->f_io_array[i+1].length;
-		iov_count ++;
-		continue;
-	    }
-	}
-
-	// Find the total number of bytes to be read.
-	size_t bytes = 0;
-	for (int i = 0; i < iov_count; ++i) {
-	    bytes += iov[i].iov_len;
-	}
-
-	// Allocate a temporary buffer to hold the data
-	char *buffer;
-	buffer = (char *) malloc (bytes);
-	if (buffer == NULL) {
-	    return OMPI_ERROR;
-	}
-
-	// Read the data
-	ssize_t bytes_read;
-	plfs_ret = plfs_read( pfd, buffer, bytes, iov_offset, &bytes_read );
+	plfs_ret = plfs_read( pfd, fh->f_io_array[i].memory_address, fh->f_io_array[i].length,
+                              (off_t )fh->f_io_array[i].offset, &bytes_read );
 	if (PLFS_SUCCESS != plfs_ret) {
 	    opal_output(0, "fbtl_plfs_preadv: Error in plfs_read:\n%s\n", strplfserr(plfs_ret));
 	    return OMPI_ERROR;
@@ -104,27 +49,6 @@ ssize_t  mca_fbtl_plfs_preadv (mca_io_ompio_file_t *fh )
 	if (bytes_read < 0)
 	    return OMPI_ERROR;
 	total_bytes_read += bytes_read;
-	// Copy the data from BUFFER into the memory specified by IOV
-	bytes = bytes_read;
-	for (int i = 0; i < iov_count; ++i) {
-	    size_t copy = MIN (iov[i].iov_len, bytes);
-	    (void) memcpy ((void *) iov[i].iov_base, (void *) buffer, copy);
-	    buffer += copy;
-	    bytes -= copy;
-	    if (bytes == 0) {
-		break;
-	    }
-	}
-	iov_count = 0;
-	if ( NULL != buffer ) {
-	    free (buffer);
-	    buffer=NULL;
-	}
-    }
-
-    if (NULL != iov) {
-	free (iov);
-	iov = NULL;
     }
 
     return total_bytes_read;
