ad496b8cc72ccad88a57cc9e16865ac2332aca66
Howard Pritchard <howardp@lanl.gov>
2016-01-21 09:12:27 -0700

    mca/io: fix os-x regression
    PR #896 broke os-x build. This commit fixes the problem.
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- ompi/mca/io/ompio/io_ompio_component.c
+++ ompi/mca/io/ompio/io_ompio_component.c
@@ -11,7 +11,7 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2008-2016 University of Houston. All rights reserved.
- * Copyright (c) 2015      Los Alamos National Security, LLC. All rights
+ * Copyright (c) 2015-2016 Los Alamos National Security, LLC. All rights
  *                         reserved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
@@ -34,6 +34,12 @@
 
 #ifdef HAVE_SYS_STATFS_H
 #include <sys/statfs.h> /* or <sys/vfs.h> */
+#endif
+#ifdef HAVE_SYS_MOUNT_H
+#include <sys/mount.h>
+#endif
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>
 #endif
 
 
