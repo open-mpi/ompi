908cf4e800e1516528fa05eb2ab7ad34a0a58bb7
Alex Mikheev <alexm@mellanox.com>
2016-01-21 16:02:28 +0200

    OSHMEM/UCX: implements atomic support
    ucx atomic component has a real code now.
    fixes bug in spml ucx add_procs
    removes redundant parameter checks from atomic components.
    (cherry picked from commit f627608e4215f2fdef8a3561fbe0242e2ca14f55)

--- oshmem/mca/atomic/ucx/atomic_ucx_fadd.c
+++ oshmem/mca/atomic/ucx/atomic_ucx_fadd.c
@@ -13,13 +13,8 @@
 #include <stdlib.h>
 
 #include "oshmem/constants.h"
-#include "oshmem/op/op.h"
-#include "oshmem/mca/spml/spml.h"
 #include "oshmem/mca/atomic/atomic.h"
 #include "oshmem/mca/atomic/base/base.h"
-#include "oshmem/mca/memheap/memheap.h"
-#include "oshmem/mca/memheap/base/base.h"
-#include "oshmem/runtime/runtime.h"
 
 #include "atomic_ucx.h"
 
@@ -30,6 +25,44 @@ int mca_atomic_ucx_fadd(void *target,
                         int pe,
                         struct oshmem_op_t *op)
 {
-    /* TODO: actual code */
-    return OSHMEM_SUCCESS;
+    ucs_status_t status;
+    spml_ucx_mkey_t *ucx_mkey;
+    uint64_t rva;
+
+    ucx_mkey = mca_spml_ucx_get_mkey(pe, target, (void *)&rva);
+
+    if (NULL == prev) {
+        switch (nlong) {
+            case 4:
+                status = ucp_atomic_add32(mca_spml_self->ucp_peers[pe].ucp_conn, 
+                        *(uint32_t *)value, rva, ucx_mkey->rkey);
+                break;
+            case 8:
+                status = ucp_atomic_add64(mca_spml_self->ucp_peers[pe].ucp_conn, 
+                        *(uint64_t *)value, rva, ucx_mkey->rkey);
+                break;
+            default:
+                goto err_size;
+        }
+    }
+    else {
+        switch (nlong) {
+            case 4:
+                status = ucp_atomic_fadd32(mca_spml_self->ucp_peers[pe].ucp_conn, 
+                        *(uint32_t *)value, rva, ucx_mkey->rkey, prev);
+                break;
+            case 8:
+                status = ucp_atomic_fadd64(mca_spml_self->ucp_peers[pe].ucp_conn, 
+                        *(uint64_t *)value, rva, ucx_mkey->rkey, prev);
+                break;
+            default:
+                goto err_size;
+        }
+    }
+
+    return ucx_status_to_oshmem(status);
+
+err_size:
+    ATOMIC_ERROR("[#%d] Type size must be 1/2/4 or 8 bytes.", my_pe);
+    return OSHMEM_ERROR;
 }
