51240394a7dbfe3cbd68da41351bad400f086034
Jeff Squyres <jsquyres@cisco.com>
2016-02-06 03:28:17 -0800

    usnic: ensure to init module->av_eq_num

--- opal/mca/btl/usnic/btl_usnic_module.c
+++ opal/mca/btl/usnic/btl_usnic_module.c
@@ -1895,6 +1895,7 @@ static void init_queue_lengths(opal_btl_usnic_module_t *module)
     } else {
         module->cq_num = mca_btl_usnic_component.cq_num;
     }
+    module->av_eq_num = mca_btl_usnic_component.av_eq_num;
 
     /*
      * Queue sizes for priority channel scale with # of endpoint. A
