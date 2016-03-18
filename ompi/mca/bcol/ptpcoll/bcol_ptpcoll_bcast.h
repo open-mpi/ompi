3540b65f7d81a91212a6b4e134eafe1d36776e7a
Nathan Hjelm <hjelmn@lanl.gov>
2016-03-18 10:59:46 -0600

    bcol: fix coverity issues
    Fix CID 1269976 (#1 of 1): Unused value (UNUSED_VALUE):
    Fix CID 1269979 (#1 of 1): Unused value (UNUSED_VALUE):
    
    Removed unused variables k_temp1 and k_temp2.
    
    Fix CID 1269981 (#1 of 1): Unused value (UNUSED_VALUE):
    Fix CID 1269974 (#1 of 1): Unused value (UNUSED_VALUE):
    
    Removed gotos and use the matched flags to decide whether to return.
    
    Fix CID 715755 (#1 of 1): Dereference null return value (NULL_RETURNS):
    
    This was also a leak. The items on cs->ctl_structures are allocated using OBJ_NEW so they mist be released using OBJ_RELEASE not OBJ_DESTRUCT. Replaced the loop with OPAL_LIST_DESTRUCT().
    
    Fix CID 715776 (#1 of 1): Dereference before null check (REVERSE_INULL):
    
    Rework error path to remove REVERSE_INULL. Also added a free to an error path where it was missing.
    
    Fix CID 1196603 (#1 of 1): Bad bit shift operation (BAD_SHIFT):
    Fix CID 1196601 (#1 of 1): Bad bit shift operation (BAD_SHIFT):
    
    Both of these are false positives but it is still worthwhile to fix so they no longer appear. The loop conditional has been updated to use radix_mask_pow instead of radix_mask to quiet these issues.
    
    Fix CID 1269804 (#1 of 1): Argument cannot be negative (NEGATIVE_RETURNS):
    
    In general close (-1) is safe but coverity doesn’t like it. Reworked the error path for open to not try to close (-1).
    
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- ompi/mca/bcol/ptpcoll/bcol_ptpcoll_bcast.h
+++ ompi/mca/bcol/ptpcoll/bcol_ptpcoll_bcast.h
@@ -1,6 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2009-2012 Oak Ridge National Laboratory.  All rights reserved.
  * Copyright (c) 2009-2012 Mellanox Technologies.  All rights reserved.
+ * Copyright (c) 2016      Los Alamos National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -66,7 +69,7 @@ do {                                                                            
     int dst_boundary_rank;                                                                                          \
     int radix_mask = radix_mask_pow >= 0 ? 1 << radix_mask_pow : 0;                                                 \
                                                                                                                     \
-    while(radix_mask > 0) {                                                                                         \
+    while(radix_mask_pow >= 0) {                                                                                    \
         /* For each level of tree, do sends */                                                                      \
         dst = my_group_index ^ radix_mask;                                                                          \
         comm_dst = group_list[dst];                                                                                 \
