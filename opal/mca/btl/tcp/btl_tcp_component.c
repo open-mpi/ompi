7eca2f96504f4bc10eae82a0559b70145a775a9c
Ralph Castain <rhc@open-mpi.org>
2016-03-30 01:34:01 -0700

    Add missing include

--- opal/mca/btl/tcp/btl_tcp_component.c
+++ opal/mca/btl/tcp/btl_tcp_component.c
@@ -70,6 +70,7 @@
 #include "opal/mca/mpool/base/base.h"
 #include "opal/mca/btl/base/btl_base_error.h"
 #include "opal/mca/pmix/pmix.h"
+#include "opal/threads/threads.h"
 
 #include "opal/constants.h"
 #include "opal/mca/btl/btl.h"
