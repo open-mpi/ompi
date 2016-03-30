1d5d4f480c4e285345df09a296423a5c7c84dcce
Gilles Gouaillardet <gilles@rist.or.jp>
2016-03-30 11:40:08 +0900

    Add the datatype checks to the pack/unpack functions. The datatype must satisfy the same constraints as for the corresponding communication function (send for pack and recv for unpack).
    (cherry picked from commit 221e6e2eabe60b7be5902cb8ef05966dcc077f4b)

--- ompi/mpi/c/unpack.c
+++ ompi/mpi/c/unpack.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -43,7 +43,7 @@ int MPI_Unpack(const void *inbuf, int insize, int *position,
                void *outbuf, int outcount, MPI_Datatype datatype,
                MPI_Comm comm)
 {
-    int rc = 1;
+    int rc = MPI_SUCCESS;
     opal_convertor_t local_convertor;
     struct iovec outvec;
     unsigned int iov_count;
@@ -70,9 +70,10 @@ int MPI_Unpack(const void *inbuf, int insize, int *position,
             return OMPI_ERRHANDLER_INVOKE(comm, MPI_ERR_COUNT, FUNC_NAME);
         }
 
-        if (MPI_DATATYPE_NULL == datatype || NULL == datatype) {
-            return OMPI_ERRHANDLER_INVOKE(comm, MPI_ERR_TYPE, FUNC_NAME);
-        }
+        OMPI_CHECK_DATATYPE_FOR_RECV(rc, datatype, outcount);
+        OMPI_ERRHANDLER_CHECK(rc, comm, rc, FUNC_NAME);
+        OMPI_CHECK_USER_BUFFER(rc, outbuf, datatype, outcount);
+        OMPI_ERRHANDLER_CHECK(rc, comm, rc, FUNC_NAME);
     }
 
 
@@ -101,10 +102,9 @@ int MPI_Unpack(const void *inbuf, int insize, int *position,
 
         /* All done.  Note that the convertor returns 1 upon success, not
            OMPI_SUCCESS. */
-
+        rc = (1 == rc) ? OMPI_SUCCESS : OMPI_ERROR;
     }
 
     OMPI_ERRHANDLER_RETURN((rc == 1) ? OMPI_SUCCESS : OMPI_ERROR,
                            comm, MPI_ERR_UNKNOWN, FUNC_NAME);
-
 }
