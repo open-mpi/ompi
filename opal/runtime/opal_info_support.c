open-mpi/ompi

commit a3229c3a1f855ecf31b379705ce46937ef4c0654

opal_info_support: add component to param pretty output
When listing available parameters, add the component name to the MCA
framework field. Parsable option is already doing this, makes sense
for the pretty print option to do it as well.

Signed-off-by: Karol Mroz <mroz.karol@gmail.com>

opal/runtime/opal_info_support.c
@@ -639,12 +639,20 @@ static void opal_info_show_mca_group_params(const mca_base_var_group_t *group, m
         }
     }
 
+    const mca_base_var_group_t *curr_group = NULL;
+    char *component_msg = NULL;
     for (i = 0 ; i < count ; ++i) {
         ret = mca_base_var_get(variables[i], &var);
         if (OPAL_SUCCESS != ret || ((var->mbv_flags & MCA_BASE_VAR_FLAG_INTERNAL) &&
                                     !want_internal) ||
             max_level < var->mbv_info_lvl) {
             continue;
+        }
+
+        if (opal_info_pretty && curr_group != group) {
+            free(component_msg);
+            asprintf(&component_msg, " %s", group_component);
+            curr_group = group;
         }
 
         ret = mca_base_var_dump(variables[i], &strings, !opal_info_pretty ? MCA_BASE_VAR_DUMP_PARSABLE : MCA_BASE_VAR_DUMP_READABLE);
@@ -654,7 +662,9 @@ static void opal_info_show_mca_group_params(const mca_base_var_group_t *group, m
 
         for (j = 0 ; strings[j] ; ++j) {
             if (0 == j && opal_info_pretty) {
-                asprintf (&message, "MCA%s %s", requested ? "" : " (disabled)", group->group_framework);
+                asprintf (&message, "MCA%s %s%s", requested ? "" : " (disabled)",
+                          group->group_framework,
+                          component_msg ? component_msg : "");
                 opal_info_out(message, message, strings[j]);
                 free(message);
             } else {

commit 43254391a8088abd1ee5d009e5b41fa14b751cf7

opal_info_support: fix memory leak
Fixing a memory leak I introduced with a3229c3a1f855ecf31b379705ce46937ef4c0654.

Signed-off-by: Karol Mroz <mroz.karol@gmail.com>

opal/runtime/opal_info_support.c
@@ -740,6 +740,7 @@ static void opal_info_show_mca_group_params(const mca_base_var_group_t *group, m
         }
         opal_info_show_mca_group_params(group, max_level, want_internal);
     }
+    free(component_msg);
 }
 
 void opal_info_show_mca_params(const char *type, const char *component,