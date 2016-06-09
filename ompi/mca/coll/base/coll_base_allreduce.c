779c0d2093a2625a386071b21fb988bcdef61529
Gilles Gouaillardet <gilles@rist.or.jp>
2016-06-09 13:12:25 +0900

    coll/base: fix memory free in ompi_coll_base_allreduce_intra_recursivedoubling err handler
    Fix CID 1362630
    
    Fixes open-mpi/ompi@0e393195d9f2373ffa9d59a240092f643117cd39
    
    (back-ported from commit open-mpi/ompi@80e362de521409897da64f5f267da33946135d35)

--- ompi/mca/coll/base/coll_base_allreduce.c
+++ ompi/mca/coll/base/coll_base_allreduce.c
@@ -270,7 +270,8 @@ ompi_coll_base_allreduce_intra_recursivedoubling(const void *sbuf, void *rbuf,
  error_hndl:
     OPAL_OUTPUT((ompi_coll_base_framework.framework_output, "%s:%4d\tRank %d Error occurred %d\n",
                  __FILE__, line, rank, ret));
-    if (NULL != inplacebuf) free(inplacebuf);
+    (void)line;  // silence compiler warning
+    if (NULL != inplacebuf_free) free(inplacebuf_free);
     return ret;
 }
 
