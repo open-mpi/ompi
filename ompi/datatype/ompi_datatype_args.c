open-mpi/ompi

commit 113b45b4e6dd1337701026a3256bc720f5a91f1d

Add support for the new MPI_Type_create_hindexed_block (MPI 3.0).
This commit was SVN r26962.

ompi/datatype/ompi_datatype_args.c
@@ -216,6 +216,12 @@ int32_t ompi_datatype_set_args( ompi_datatype_t* pData,
     case MPI_COMBINER_RESIZED:
         break;
 
+    case MPI_COMBINER_HINDEXED_BLOCK:
+        pArgs->i[0] = i[0][0];
+        pArgs->i[1] = i[1][0];
+        memcpy( pArgs->i + 2, i[2], i[0][0] * sizeof(int) );
+        break;
+
     default:
         break;
     }
@@ -744,7 +750,15 @@ static ompi_datatype_t* __ompi_datatype_create_from_args( int32_t* i, MPI_Aint* 
         /*ompi_datatype_set_args( datatype, 0, NULL, 2, a, 1, d, MPI_COMBINER_RESIZED );*/
         break;
         /******************************************************************/
-    default:
+    case MPI_COMBINER_HINDEXED_BLOCK:
+        ompi_datatype_create_hindexed_block( i[0], i[1], a, d[0], &datatype );
+        {
+            int* a_i[2]; a_i[0] = &i[0]; a_i[1] = &i[1];
+            ompi_datatype_set_args( datatype, 2 + i[0], a_i, i[0], a, 1, d, MPI_COMBINER_HINDEXED_BLOCK );
+        }
+        break;
+        /******************************************************************/
+     default:
         break;
     }
 

ompi/datatype/ompi_datatype.h
@@ -210,6 +210,8 @@ OMPI_DECLSPEC int32_t ompi_datatype_create_hindexed( int count, const int* pBloc
                                                      const ompi_datatype_t* oldType, ompi_datatype_t** newType );
 OMPI_DECLSPEC int32_t ompi_datatype_create_indexed_block( int count, int bLength, const int* pDisp,
                                                           const ompi_datatype_t* oldType, ompi_datatype_t** newType );
+OMPI_DECLSPEC int32_t ompi_datatype_create_hindexed_block( int count, int bLength, const OPAL_PTRDIFF_TYPE* pDisp,
+                                                           const ompi_datatype_t* oldType, ompi_datatype_t** newType );
 OMPI_DECLSPEC int32_t ompi_datatype_create_struct( int count, const int* pBlockLength, const OPAL_PTRDIFF_TYPE* pDisp,
                                                    ompi_datatype_t* const* pTypes, ompi_datatype_t** newType );
 OMPI_DECLSPEC int32_t ompi_datatype_create_darray( int size, int rank, int ndims, int const* gsize_array,

ompi/datatype/ompi_datatype_create_indexed.c
@@ -142,3 +142,41 @@ int32_t ompi_datatype_create_indexed_block( int count, int bLength, const int* p
     *newType = pdt;
     return OMPI_SUCCESS;
 }
+
+int32_t ompi_datatype_create_hindexed_block( int count, int bLength, const OPAL_PTRDIFF_TYPE* pDisp,
+                                             const ompi_datatype_t* oldType, ompi_datatype_t** newType )
+{
+    ompi_datatype_t* pdt;
+    int i, dLength;
+    OPAL_PTRDIFF_TYPE extent, disp, endat;
+
+    ompi_datatype_type_extent( oldType, &extent );
+    if( (count == 0) || (bLength == 0) ) {
+        *newType = ompi_datatype_create(1);
+        if( 0 == count )
+            ompi_datatype_add( *newType, &ompi_mpi_datatype_null.dt, 0, 0, 0 );
+        else
+            ompi_datatype_add( *newType, oldType, 0, pDisp[0] * extent, extent );
+        return OMPI_SUCCESS;
+    }
+    pdt = ompi_datatype_create( count * (2 + oldType->super.desc.used) );
+    disp = pDisp[0];
+    dLength = bLength;
+    endat = disp + dLength;
+    for( i = 1; i < count; i++ ) {
+        if( endat == pDisp[i] ) {
+            /* contiguous with the previsious */
+            dLength += bLength;
+            endat += bLength;
+        } else {
+            ompi_datatype_add( pdt, oldType, dLength, disp, extent );
+            disp = pDisp[i];
+            dLength = bLength;
+            endat = disp + bLength;
+        }
+    }
+    ompi_datatype_add( pdt, oldType, dLength, disp, extent );
+
+    *newType = pdt;
+    return OMPI_SUCCESS;
+}

ompi/include/mpi.h.in
@@ -613,7 +613,8 @@ enum {
   MPI_COMBINER_F90_REAL,
   MPI_COMBINER_F90_COMPLEX,
   MPI_COMBINER_F90_INTEGER,
-  MPI_COMBINER_RESIZED
+  MPI_COMBINER_RESIZED,
+  MPI_COMBINER_HINDEXED_BLOCK
 };
 
 /* 
@@ -1501,6 +1502,10 @@ OMPI_DECLSPEC  int MPI_Type_create_darray(int size, int rank, int ndims,
 OMPI_DECLSPEC  int MPI_Type_create_f90_complex(int p, int r, MPI_Datatype *newtype);
 OMPI_DECLSPEC  int MPI_Type_create_f90_integer(int r, MPI_Datatype *newtype);
 OMPI_DECLSPEC  int MPI_Type_create_f90_real(int p, int r, MPI_Datatype *newtype);
+OMPI_DECLSPEC  int MPI_Type_create_hindexed_block(int count, int blocklength,
+                                                  MPI_Aint array_of_displacements[],
+                                                  MPI_Datatype oldtype,
+                                                  MPI_Datatype *newtype);
 OMPI_DECLSPEC  int MPI_Type_create_hindexed(int count, int array_of_blocklengths[],
                                             MPI_Aint array_of_displacements[],
                                             MPI_Datatype oldtype,
@@ -2096,6 +2101,10 @@ OMPI_DECLSPEC  int PMPI_Type_create_hvector(int count, int blocklength, MPI_Aint
 OMPI_DECLSPEC  int PMPI_Type_create_keyval(MPI_Type_copy_attr_function *type_copy_attr_fn,
                                            MPI_Type_delete_attr_function *type_delete_attr_fn,
                                            int *type_keyval, void *extra_state);
+OMPI_DECLSPEC  int PMPI_Type_create_hindexed_block(int count, int blocklength,
+                                                   MPI_Aint array_of_displacements[],
+                                                   MPI_Datatype oldtype,
+                                                   MPI_Datatype *newtype);
 OMPI_DECLSPEC  int PMPI_Type_create_indexed_block(int count, int blocklength,
                                                   int array_of_displacements[],
                                                   MPI_Datatype oldtype,

ompi/include/mpif-common.h.fin
@@ -354,6 +354,7 @@
       integer MPI_COMBINER_F90_COMPLEX
       integer MPI_COMBINER_F90_INTEGER
       integer MPI_COMBINER_RESIZED
+      integer MPI_COMBINER_HINDEXED_BLOCK
 
       parameter (MPI_COMBINER_NAMED=0)
       parameter (MPI_COMBINER_DUP=1)
@@ -373,6 +374,7 @@
       parameter (MPI_COMBINER_F90_COMPLEX=15)
       parameter (MPI_COMBINER_F90_INTEGER=16)
       parameter (MPI_COMBINER_RESIZED=17)
+      parameter (MPI_COMBINER_INDEXED_BLOCK=18)
 
 ! 
 ! Communicator split type constants.

ompi/mpi/c/Makefile.am
@@ -272,6 +272,7 @@ libmpi_c_mpi_la_SOURCES = \
         type_create_hindexed.c \
         type_create_hvector.c \
         type_create_indexed_block.c \
+        type_create_hindexed_block.c \
         type_create_keyval.c \
         type_create_resized.c \
         type_create_struct.c \

ompi/mpi/c/profile/Makefile.am
@@ -254,6 +254,7 @@ nodist_libmpi_c_pmpi_la_SOURCES = \
         ptype_create_hindexed.c \
         ptype_create_hvector.c \
         ptype_create_indexed_block.c \
+        ptype_create_hindexed_block.c \
         ptype_create_keyval.c \
         ptype_create_resized.c \
         ptype_create_struct.c \

ompi/mpi/c/profile/defines.h
@@ -302,6 +302,7 @@
 #define MPI_Type_create_hindexed PMPI_Type_create_hindexed
 #define MPI_Type_create_hvector PMPI_Type_create_hvector
 #define MPI_Type_create_indexed_block PMPI_Type_create_indexed_block
+#define MPI_Type_create_hindexed_block PMPI_Type_create_hindexed_block
 #define MPI_Type_create_keyval PMPI_Type_create_keyval
 #define MPI_Type_create_resized PMPI_Type_create_resized
 #define MPI_Type_create_struct PMPI_Type_create_struct

commit 84b110a1f2e1952459aed5dd788261fe2e68a87b

ompi/datatype: Fix args of HINDEXED_BLOCK
According to MPI-3.1 P.121, `ni` for `MPI_COMBINER_HINDEXED_BLOCK`
should be `2`, not `2 + count`.

This bug was introduced in 113b45b4 (when `MPI_Type_create_hindexed_block`
support is added in Open MPI) and fixed partially in 7f5314ee and 8de93982.
This commit fixes the remaining part.

Probably this bug has no user impact. It only consumes a bit more memory.

ompi/datatype/ompi_datatype_args.c
@@ -788,7 +788,7 @@ static ompi_datatype_t* __ompi_datatype_create_from_args( int32_t* i, MPI_Aint* 
         ompi_datatype_create_hindexed_block( i[0], i[1], a, d[0], &datatype );
         {
             const int* a_i[2] = {&i[0], &i[1]};
-            ompi_datatype_set_args( datatype, 2 + i[0], a_i, i[0], a, 1, d, MPI_COMBINER_HINDEXED_BLOCK );
+            ompi_datatype_set_args( datatype, 2, a_i, i[0], a, 1, d, MPI_COMBINER_HINDEXED_BLOCK );
         }
         break;
         /******************************************************************/