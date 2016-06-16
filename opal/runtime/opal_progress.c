dbd83694851f1d1b990e70fc29216abc7ceed092
Nathan Hjelm <hjelmn@lanl.gov>
2016-06-16 09:01:19 -0600

    opal/progress: use 32-bit atomics for call counter
    This commit fixes a compile error on 32-bit platforms. The
    low-priority call counter was always using 64-bit atomics which will
    not work if 64-bit atomic math is not available. Updated to use 32-bit
    instead.
    
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- opal/runtime/opal_progress.c
+++ opal/runtime/opal_progress.c
@@ -182,7 +182,7 @@ opal_progress_finalize(void)
 void
 opal_progress(void)
 {
-    static volatile uint64_t num_calls = 0;
+    static volatile uint32_t num_calls = 0;
     size_t i;
     int events = 0;
 
@@ -221,7 +221,7 @@ opal_progress(void)
         events += (callbacks[i])();
     }
 
-    if ((OPAL_THREAD_ADD64((volatile int64_t *) &num_calls, 1) & 0x7) == 0) {
+    if ((OPAL_THREAD_ADD32((volatile int32_t *) &num_calls, 1) & 0x7) == 0) {
         /* run low priority callbacks once every 8 calls to opal_progress() */
         for (i = 0 ; i < callbacks_lp_len ; ++i) {
             events += (callbacks_lp[i])();
