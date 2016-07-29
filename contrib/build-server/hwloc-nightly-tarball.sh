c4c04f6a2c7c37afc8d19f15ef9f8f6f2e5dd851
Ralph Castain <rhc@open-mpi.org>
2016-07-29 16:20:10 -0700

    Cleanup minor typo

--- contrib/build-server/hwloc-nightly-tarball.sh
+++ contrib/build-server/hwloc-nightly-tarball.sh
@@ -19,6 +19,9 @@ outputroot=$HOME/hwloc/nightly
 # where to find the build script
 script_uri=contrib/nightly/make_snapshot_tarball
 
+# helper scripts dir
+script_dir=$HOME/scripts
+
 # The tarballs to make
 if [ $# -eq 0 ] ; then
     # Branches v1.6 and earlier were not updated to build nightly
@@ -54,16 +57,18 @@ cd $build_root
 pending_coverity=$build_root/tarballs-to-run-through-coverity.txt
 rm -f $pending_coverity
 touch $pending_coverity
-debug=100
 
 # Loop making them
+module unload autotools
 for branch in $branches; do
+    echo "=== Branch: $branch"
     # Get the last tarball version that was made
     prev_snapshot=`cat $outputroot/$branch/latest_snapshot.txt`
 
     # Form a URL-specific script name
     script=$branch-`basename $script_uri`
 
+    echo "=== Getting script from: $raw_uri"
     wget --quiet --no-check-certificate --tries=10 $raw_uri/$branch/$script_uri -O $script
     if test ! $? -eq 0 ; then
         echo "wget of hwloc nightly tarball create script failed."
@@ -79,6 +84,7 @@ for branch in $branches; do
     module load "autotools/hwloc-$branch"
    # module load "tex-live/hwloc-$branch"
 
+   echo "=== Running script..."
     ./$script \
         $build_root/$branch \
         $results_addr \
@@ -87,25 +93,37 @@ for branch in $branches; do
         $branch \
         >/dev/null 2>&1
 
+    module unload autotools
+    echo "=== Done running script"
+
     # Did the script generate a new tarball?  If so, save it so that we can
     # spawn the coverity checker on it afterwards.  Only for this for the
     # master (for now).
     latest_snapshot=`cat $outputroot/$branch/latest_snapshot.txt`
+    echo "=== Latest snapshot: $latest_snapshot"
     if test "$prev_snapshot" != "$latest_snapshot" && \
         test "$branch" = "master"; then
+        echo "=== Saving output for a Coverity run"
         echo "$outputroot/$branch/hwloc-$latest_snapshot.tar.bz2" >> $pending_coverity
         echo "=== Posting tarball to open-mpi.org"
         # tell the web server to cleanup old nightly tarballs
         ssh -p 2222 ompiteam@192.185.39.252 "git/ompi/contrib/build-server/remove-old.pl 7 public_html/software/hwloc/nightly/$branch"
         # upload the new ones
         scp -P 2222 $outputroot/$branch/hwloc-$latest_snapshot.tar.* ompiteam@192.185.39.252:public_html/software/hwloc/nightly/$branch/
-        scp -P 2222 $outputroot/$branch/$latest_snapshot.txt ompiteam@192.185.39.252:public_html/software/hwloc/nightly/$branch/
+        scp -P 2222 $outputroot/$branch/latest_snapshot.txt ompiteam@192.185.39.252:public_html/software/hwloc/nightly/$branch/
         # direct the web server to regenerate the checksums
         ssh -p 2222 ompiteam@192.185.39.252 "cd public_html/software/hwloc/nightly/$branch && md5sum hwloc* > md5sums.txt"
         ssh -p 2222 ompiteam@192.185.39.252 "cd public_html/software/hwloc/nightly/$branch && sha1sum hwloc* > sha1sums.txt"
+    else
+        echo "=== NOT saving output for a Coverity run"
     fi
 
-    module unload autotools
+    # Failed builds are not removed.  But if a human forgets to come
+    # in here and clean up the old failed builds, we can accumulate
+    # many over time.  So remove any old failed builds that are over
+    # 4 weeks old.
+    ${script_dir}/remove-old.pl 7 $build_root/$branch
+
 done
 
 # If we had any new snapshots to send to coverity, process them now
