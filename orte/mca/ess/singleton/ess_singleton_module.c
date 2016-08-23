a9227f6d81879c6aa9e6fa4694c31369e83a327f
Gilles Gouaillardet <gilles@rist.or.jp>
2016-08-23 09:29:17 +0900

    ess/singleton: expects 4 PMIX_* environment variables or more
    (cherry picked from commit a1e8e58a8a9a06d2e4297c4dc479c8b359ca8d05)

--- orte/mca/ess/singleton/ess_singleton_module.c
+++ orte/mca/ess/singleton/ess_singleton_module.c
@@ -15,6 +15,8 @@
  * Copyright (c) 2013-2015 Intel, Inc.  All rights reserved.
  * Copyright (c) 2015      Los Alamos National Security, LLC. All rights
  *                         reserved.
+ * Copyright (c) 2016      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -546,6 +548,8 @@ static int fork_hnp(void)
         exit(1);
 
     } else {
+        int count;
+
         free(cmd);
         /* I am the parent - wait to hear something back and
          * report results
@@ -612,12 +616,13 @@ static int fork_hnp(void)
 
         /* split the pmix_uri into its parts */
         argv = opal_argv_split(cptr, ',');
-        if (4 != opal_argv_count(argv)) {
+        count = opal_argv_count(argv);
+        if (4 > count) {
             opal_argv_free(argv);
             return ORTE_ERR_BAD_PARAM;
         }
         /* push each piece into the environment */
-        for (i=0; i < 4; i++) {
+        for (i=0; i < count; i++) {
             pmixenvars[i] = strdup(argv[i]);
             putenv(pmixenvars[i]);
         }
