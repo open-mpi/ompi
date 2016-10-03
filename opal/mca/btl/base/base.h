8dc867c14341a7db82acdba68c7c07cc7ca1eb91
Nathan Hjelm <hjelmn@me.com>
2016-10-02 22:04:49 -0600

    btl/base: add missing include
    Signed-off-by: Nathan Hjelm <hjelmn@me.com>

--- opal/mca/btl/base/base.h
+++ opal/mca/btl/base/base.h
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
@@ -11,7 +12,7 @@
  *                         All rights reserved.
  * Copyright (c) 2006      Sun Microsystems, Inc.  All rights reserved.
  * Copyright (c) 2008      Cisco Systems, Inc.  All rights reserved.
- * Copyright (c) 2015      Los Alamos National Security, LLC. All rights
+ * Copyright (c) 2015-2016 Los Alamos National Security, LLC. All rights
  *                         reserved.
  * $COPYRIGHT$
  *
@@ -30,6 +31,7 @@
 #include "opal/mca/mca.h"
 #include "opal/mca/base/mca_base_framework.h"
 #include "opal/mca/btl/btl.h"
+#include "opal/mca/base/mca_base_var_enum.h"
 
 BEGIN_C_DECLS
 
