974dd6429322bc3b6254189d024aff63c5af18fb
Gilles Gouaillardet <gilles@rist.or.jp>
2016-10-12 14:49:31 +0900

    ompi/errhandler: remove useless include file
    and avoid #includ'ing stuff that breaks C++ compilers

--- ompi/errhandler/errhandler.h
+++ ompi/errhandler/errhandler.h
@@ -37,7 +37,6 @@
 #include "opal/class/opal_pointer_array.h"
 #include "opal/mca/pmix/pmix.h"
 
-#include "ompi/mca/rte/rte.h"
 #include "ompi/runtime/mpiruntime.h"
 #include "ompi/errhandler/errhandler_predefined.h"
 #include "ompi/errhandler/errcode-internal.h"
