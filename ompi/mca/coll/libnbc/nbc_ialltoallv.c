1a0dfe1dc4781ba30accec67cd49c0d568cd577f
Gilles Gouaillardet <gilles@rist.or.jp>
2016-10-18 14:42:16 +0900

    coll/libnbc: implement support for MPI_IN_PLACE in MPI_Ialltoall*
    Thanks Chris Ward for the report
    
    Many thanks to George for the guidance
    
    (back-ported from commit open-mpi/ompi@1e0f591811f422175200b7ae2ba76c825d713489)

--- ompi/mca/coll/libnbc/nbc_ialltoallv.c
+++ ompi/mca/coll/libnbc/nbc_ialltoallv.c
@@ -1,14 +1,32 @@
 /*
- * Copyright (c) 2006 The Trustees of Indiana University and Indiana
- *                    University Research and Technology
- *                    Corporation.  All rights reserved.
- * Copyright (c) 2006 The Technical University of Chemnitz. All 
- *                    rights reserved.
+ * Copyright (c) 2006      The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2006      The Technical University of Chemnitz. All
+ *                         rights reserved.
+ * Copyright (c) 2016      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  *
  * Author(s): Torsten Hoefler <htor@cs.indiana.edu>
  *
  */
 #include "nbc_internal.h"
+
+static inline int a2av_sched_linear(int rank, int p, NBC_Schedule *schedule,
+                                    const void *sendbuf, const int *sendcounts,
+                                    const int *sdispls, MPI_Aint sndext, MPI_Datatype sendtype,
+                                    void *recvbuf, const int *recvcounts,
+                                    const int *rdispls, MPI_Aint rcvext, MPI_Datatype recvtype);
+
+static inline int a2av_sched_pairwise(int rank, int p, NBC_Schedule *schedule,
+                                      const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                      MPI_Aint sndext, MPI_Datatype sendtype,
+                                      void *recvbuf, const int *recvcounts, const int *rdispls,
+                                      MPI_Aint rcvext, MPI_Datatype recvtype);
+
+static inline int a2av_sched_inplace(int rank, int p, NBC_Schedule *schedule,
+                                    void *buf, const int *counts, const int *displs,
+                                    MPI_Aint ext, MPI_Datatype type, ptrdiff_t gap);
 
 /* an alltoallv schedule can not be cached easily because the contents
  * ot the recvcounts array may change, so a comparison of the address
@@ -20,10 +38,11 @@ int ompi_coll_libnbc_ialltoallv(void* sendbuf, int *sendcounts, int *sdispls,
                                 MPI_Datatype recvtype, struct ompi_communicator_t *comm, ompi_request_t ** request,
                                struct mca_coll_base_module_2_0_0_t *module)
 {
-  int rank, p, res, i;
+  int rank, p, res;
   MPI_Aint sndext, rcvext;
   NBC_Schedule *schedule;
   char *rbuf, *sbuf, inplace;
+  ptrdiff_t gap, span;
   NBC_Handle *handle;
   ompi_coll_libnbc_request_t **coll_req = (ompi_coll_libnbc_request_t**) request;
   ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
@@ -43,7 +62,7 @@ int ompi_coll_libnbc_ialltoallv(void* sendbuf, int *sendcounts, int *sdispls,
   if (MPI_SUCCESS != res) { printf("MPI Error in MPI_Type_extent() (%i)\n", res); return res; }
 
   schedule = (NBC_Schedule*)malloc(sizeof(NBC_Schedule));
-  if (NULL == schedule) { printf("Error in malloc() (%i)\n", res); return res; }
+  if (NULL == schedule) { printf("Error in malloc()\n"); return NBC_OOR; }
 
   handle->tmpbuf=NULL;
   
@@ -51,32 +70,36 @@ int ompi_coll_libnbc_ialltoallv(void* sendbuf, int *sendcounts, int *sdispls,
   if(res != NBC_OK) { printf("Error in NBC_Sched_create (%i)\n", res); return res; }
 
   /* copy data to receivbuffer */
-  if((sendcounts[rank] != 0) && !inplace) {
-    rbuf = ((char *) recvbuf) + (rdispls[rank] * rcvext);
-    sbuf = ((char *) sendbuf) + (sdispls[rank] * sndext);
-    res = NBC_Copy(sbuf, sendcounts[rank], sendtype, rbuf, recvcounts[rank], recvtype, comm);
+  if (inplace) {
+    int count = 0;
+    for (int i = 0; i < p; i++) {
+      if (recvcounts[i] > count) {
+        count = recvcounts[i];
+      }
+    }
+    span = opal_datatype_span(&recvtype->super, count, &gap);
+    handle->tmpbuf = malloc(span);
+    if (OPAL_UNLIKELY(NULL == handle->tmpbuf)) { printf("Error in malloc()\n"); return NBC_OOR; }
+    sendcounts = recvcounts;
+    sdispls = rdispls;
+  } else if (sendcounts[rank] != 0) {
+    rbuf = (char *) recvbuf + rdispls[rank] * rcvext;
+    sbuf = (char *) sendbuf + sdispls[rank] * sndext;
+    res = NBC_Copy (sbuf, sendcounts[rank], sendtype, rbuf, recvcounts[rank], recvtype, comm);
     if (NBC_OK != res) { printf("Error in NBC_Copy() (%i)\n", res); return res; }
   }
 
-  for (i = 0; i < p; i++) {
-    if (i == rank) { continue; }
-    /* post all sends */
-    if(sendcounts[i] != 0) {
-      sbuf = ((char *) sendbuf) + (sdispls[i] * sndext);
-      res = NBC_Sched_send(sbuf, false, sendcounts[i], sendtype, i, schedule);
-      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
-    }
-    /* post all receives */
-    if(recvcounts[i] != 0) {
-      rbuf = ((char *) recvbuf) + (rdispls[i] * rcvext);
-      res = NBC_Sched_recv(rbuf, false, recvcounts[i], recvtype, i, schedule);
-      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
-    }
+  if (inplace) {
+    res = a2av_sched_inplace(rank, p, schedule, recvbuf, recvcounts,
+                                 rdispls, rcvext, recvtype, gap);
+  } else {
+    res = a2av_sched_linear(rank, p, schedule,
+                            sendbuf, sendcounts, sdispls, sndext, sendtype,
+                            recvbuf, recvcounts, rdispls, rcvext, recvtype);
   }
+  if (OPAL_UNLIKELY(NBC_OK != res)) { return res; }
 
-  /*NBC_PRINT_SCHED(*schedule);*/
-
-  res = NBC_Sched_commit(schedule);
+  res = NBC_Sched_commit (schedule);
   if (NBC_OK != res) { printf("Error in NBC_Sched_commit() (%i)\n", res); return res; }
 
   res = NBC_Start(handle, schedule);
@@ -94,7 +117,6 @@ int ompi_coll_libnbc_ialltoallv_inter (void* sendbuf, int *sendcounts, int *sdis
   int rank, res, i, rsize;
   MPI_Aint sndext, rcvext;
   NBC_Schedule *schedule;
-  char *rbuf, *sbuf;
   NBC_Handle *handle;
   ompi_coll_libnbc_request_t **coll_req = (ompi_coll_libnbc_request_t**) request;
   ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
@@ -112,7 +134,7 @@ int ompi_coll_libnbc_ialltoallv_inter (void* sendbuf, int *sendcounts, int *sdis
   MPI_Comm_remote_size (comm, &rsize);
 
   schedule = (NBC_Schedule*)malloc(sizeof(NBC_Schedule));
-  if (NULL == schedule) { printf("Error in malloc() (%i)\n", res); return res; }
+  if (NULL == schedule) { printf("Error in malloc()\n"); return NBC_OOR; }
 
   handle->tmpbuf=NULL;
 
@@ -122,13 +144,13 @@ int ompi_coll_libnbc_ialltoallv_inter (void* sendbuf, int *sendcounts, int *sdis
   for (i = 0; i < rsize; i++) {
     /* post all sends */
     if(sendcounts[i] != 0) {
-      sbuf = ((char *) sendbuf) + (sdispls[i] * sndext);
+      char *sbuf = ((char *) sendbuf) + (sdispls[i] * sndext);
       res = NBC_Sched_send(sbuf, false, sendcounts[i], sendtype, i, schedule);
       if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
     }
     /* post all receives */
     if(recvcounts[i] != 0) {
-      rbuf = ((char *) recvbuf) + (rdispls[i] * rcvext);
+      char *rbuf = ((char *) recvbuf) + (rdispls[i] * rcvext);
       res = NBC_Sched_recv(rbuf, false, recvcounts[i], recvtype, i, schedule);
       if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
     }
@@ -141,6 +163,125 @@ int ompi_coll_libnbc_ialltoallv_inter (void* sendbuf, int *sendcounts, int *sdis
 
   res = NBC_Start(handle, schedule);
   if (NBC_OK != res) { printf("Error in NBC_Start() (%i)\n", res); return res; }
+
+  return NBC_OK;
+}
+
+static inline int a2av_sched_linear(int rank, int p, NBC_Schedule *schedule,
+                                    const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                    MPI_Aint sndext, MPI_Datatype sendtype,
+                                    void *recvbuf, const int *recvcounts, const int *rdispls,
+                                    MPI_Aint rcvext, MPI_Datatype recvtype) {
+  int res;
+
+  for (int i = 0 ; i < p ; ++i) {
+    if (i == rank) {
+      continue;
+    }
+
+    /* post send */
+    if (sendcounts[i] != 0) {
+      char *sbuf = ((char *) sendbuf) + (sdispls[i] * sndext);
+      res = NBC_Sched_send(sbuf, false, sendcounts[i], sendtype, i, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+
+    /* post receive */
+    if (recvcounts[i] != 0) {
+      char *rbuf = ((char *) recvbuf) + (rdispls[i] * rcvext);
+      res = NBC_Sched_recv(rbuf, false, recvcounts[i], recvtype, i, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+    }
+  }
+
+  return NBC_OK;
+}
+
+static inline int a2av_sched_pairwise(int rank, int p, NBC_Schedule *schedule,
+                                      const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                      MPI_Aint sndext, MPI_Datatype sendtype,
+                                      void *recvbuf, const int *recvcounts, const int *rdispls,
+                                      MPI_Aint rcvext, MPI_Datatype recvtype) {
+  int res;
+
+  for (int i = 1 ; i < p ; ++i) {
+    int sndpeer = (rank + i) % p;
+    int rcvpeer = (rank + p - i) %p;
+
+    /* post send */
+    if (sendcounts[sndpeer] != 0) {
+      char *sbuf = ((char *) sendbuf) + (sdispls[sndpeer] * sndext);
+      res = NBC_Sched_send(sbuf, false, sendcounts[sndpeer], sendtype, sndpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+
+    /* post receive */
+    if (recvcounts[rcvpeer] != 0) {
+      char *rbuf = ((char *) recvbuf) + (rdispls[rcvpeer] * rcvext);
+      res = NBC_Sched_recv(rbuf, false, recvcounts[rcvpeer], recvtype, rcvpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+  }
+
+  return NBC_OK;
+}
+
+static inline int a2av_sched_inplace(int rank, int p, NBC_Schedule *schedule,
+                                    void *buf, const int *counts, const int *displs,
+                                    MPI_Aint ext, MPI_Datatype type, ptrdiff_t gap) {
+  int res;
+
+  for (int i = 1; i < (p+1)/2; i++) {
+    int speer = (rank + i) % p;
+    int rpeer = (rank + p - i) % p;
+    char *sbuf = (char *) buf + displs[speer] * ext;
+    char *rbuf = (char *) buf + displs[rpeer] * ext;
+
+    if (0 != counts[rpeer]) {
+      res = NBC_Sched_copy (rbuf, false, counts[rpeer], type, (void *)(-gap), true, counts[rpeer], type, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Copy() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+    if (0 != counts[speer]) {
+      res = NBC_Sched_send (sbuf, false , counts[speer], type, speer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+    if (0 != counts[rpeer]) {
+      res = NBC_Sched_recv (rbuf, false , counts[rpeer], type, rpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+
+    if (0 != counts[rpeer]) {
+      res = NBC_Sched_send ((void *)(-gap), true, counts[rpeer], type, rpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+    if (0 != counts[speer]) {
+      res = NBC_Sched_recv (sbuf, false, counts[speer], type, speer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+  }
+  if (0 == (p%2)) {
+    int peer = (rank + p/2) % p;
+
+    char *tbuf = (char *) buf + displs[peer] * ext;
+    res = NBC_Sched_copy (tbuf, false, counts[peer], type, (void *)(-gap), true, counts[peer], type, schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Copy() (%i)\n", res); return res; }
+    res = NBC_Sched_barrier(schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    res = NBC_Sched_send ((void *)(-gap), true , counts[peer], type, peer, schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    res = NBC_Sched_recv (tbuf, false , counts[peer], type, peer, schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+    res = NBC_Sched_barrier(schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+  }
 
   return NBC_OK;
 }
--- ompi/mca/coll/libnbc/nbc_ialltoallw.c
+++ ompi/mca/coll/libnbc/nbc_ialltoallw.c
@@ -1,14 +1,32 @@
 /*
- * Copyright (c) 2006 The Trustees of Indiana University and Indiana
- *                    University Research and Technology
- *                    Corporation.  All rights reserved.
- * Copyright (c) 2006 The Technical University of Chemnitz. All
- *                    rights reserved.
+ * Copyright (c) 2006      The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2006      The Technical University of Chemnitz. All
+ *                         rights reserved.
+ * Copyright (c) 2016      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
  *
  * Author(s): Torsten Hoefler <htor@cs.indiana.edu>
  *
  */
 #include "nbc_internal.h"
+
+static inline int a2aw_sched_linear(int rank, int p, NBC_Schedule *schedule,
+                                    const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                    struct ompi_datatype_t * const * sendtypes,
+                                    void *recvbuf, const int *recvcounts, const int *rdispls,
+                                    struct ompi_datatype_t * const * recvtypes);
+
+static inline int a2aw_sched_pairwise(int rank, int p, NBC_Schedule *schedule,
+                                      const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                      struct ompi_datatype_t * const * sendtypes,
+                                      void *recvbuf, const int *recvcounts, const int *rdispls,
+                                      struct ompi_datatype_t * const * recvtypes);
+
+static inline int a2aw_sched_inplace(int rank, int p, NBC_Schedule *schedule,
+                                    void *buf, const int *counts, const int *displs,
+                                    struct ompi_datatype_t * const * types);
 
 /* an alltoallw schedule can not be cached easily because the contents
  * ot the recvcounts array may change, so a comparison of the address
@@ -20,9 +38,10 @@ int ompi_coll_libnbc_ialltoallw(void* sendbuf, int *sendcounts, int *sdispls,
                                 MPI_Datatype recvtypes[], struct ompi_communicator_t *comm, ompi_request_t ** request,
 				struct mca_coll_base_module_2_0_0_t *module)
 {
-  int rank, p, res, i;
+  int rank, p, res;
   NBC_Schedule *schedule;
   char *rbuf, *sbuf, inplace;
+  ptrdiff_t span=0;
   NBC_Handle *handle;
   ompi_coll_libnbc_request_t **coll_req = (ompi_coll_libnbc_request_t**) request;
   ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
@@ -46,32 +65,37 @@ int ompi_coll_libnbc_ialltoallw(void* sendbuf, int *sendcounts, int *sdispls,
   if(res != NBC_OK) { printf("Error in NBC_Sched_create (%i)\n", res); return res; }
 
   /* copy data to receivbuffer */
-  if((sendcounts[rank] != 0) && !inplace) {
-    rbuf = ((char *) recvbuf) + rdispls[rank];
-    sbuf = ((char *) sendbuf) + sdispls[rank];
+  if (inplace) {
+    ptrdiff_t lgap, lspan;
+    for (int i = 0; i < p; i++) {
+      lspan = opal_datatype_span(&recvtypes[i]->super, recvcounts[i], &lgap);
+      if (lspan > span) {
+        span = lspan;
+      }
+    }
+    handle->tmpbuf = malloc(span);
+    if (OPAL_UNLIKELY(NULL == handle->tmpbuf)) { printf("Error in malloc()\n"); return NBC_OOR; }
+    sendcounts = recvcounts;
+    sdispls = rdispls;
+    sendtypes = recvtypes;
+  } else if (sendcounts[rank] != 0) {
+    rbuf = (char *) recvbuf + rdispls[rank];
+    sbuf = (char *) sendbuf + sdispls[rank];
     res = NBC_Copy(sbuf, sendcounts[rank], sendtypes[rank], rbuf, recvcounts[rank], recvtypes[rank], comm);
     if (NBC_OK != res) { printf("Error in NBC_Copy() (%i)\n", res); return res; }
   }
 
-  for (i = 0; i < p; i++) {
-    if (i == rank) { continue; }
-    /* post all sends */
-    if(sendcounts[i] != 0) {
-      sbuf = ((char *) sendbuf) + sdispls[i];
-      res = NBC_Sched_send(sbuf, false, sendcounts[i], sendtypes[i], i, schedule);
-      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
-    }
-    /* post all receives */
-    if(recvcounts[i] != 0) {
-      rbuf = ((char *) recvbuf) + rdispls[i];
-      res = NBC_Sched_recv(rbuf, false, recvcounts[i], recvtypes[i], i, schedule);
-      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
-    }
+  if (inplace) {
+    res = a2aw_sched_inplace(rank, p, schedule, recvbuf,
+                                 recvcounts, rdispls, recvtypes);
+  } else {
+    res = a2aw_sched_linear(rank, p, schedule,
+                            sendbuf, sendcounts, sdispls, sendtypes,
+                            recvbuf, recvcounts, rdispls, recvtypes);
   }
+  if (OPAL_UNLIKELY(NBC_OK != res)) { return res; }
 
-  /*NBC_PRINT_SCHED(*schedule);*/
-
-  res = NBC_Sched_commit(schedule);
+  res = NBC_Sched_commit (schedule);
   if (NBC_OK != res) { printf("Error in NBC_Sched_commit() (%i)\n", res); return res; }
 
   res = NBC_Start(handle, schedule);
@@ -131,6 +155,126 @@ int ompi_coll_libnbc_ialltoallw_inter (void* sendbuf, int *sendcounts, int *sdis
 
   res = NBC_Start(handle, schedule);
   if (NBC_OK != res) { printf("Error in NBC_Start() (%i)\n", res); return res; }
+
+  return NBC_OK;
+}
+
+static inline int a2aw_sched_linear(int rank, int p, NBC_Schedule *schedule,
+                                    const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                    struct ompi_datatype_t * const * sendtypes,
+                                    void *recvbuf, const int *recvcounts, const int *rdispls,
+                                    struct ompi_datatype_t * const * recvtypes) {
+  int res;
+
+  for (int i = 0; i < p; i++) {
+    if (i == rank) {
+      continue;
+    }
+
+    /* post send */
+    if (sendcounts[i] != 0) {
+      char *sbuf = (char *) sendbuf + sdispls[i];
+      res = NBC_Sched_send (sbuf, false, sendcounts[i], sendtypes[i], i, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+    /* post receive */
+    if (recvcounts[i] != 0) {
+      char *rbuf = (char *) recvbuf + rdispls[i];
+      res = NBC_Sched_recv (rbuf, false, recvcounts[i], recvtypes[i], i, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+    }
+  }
+
+  return NBC_OK;
+}
+
+static inline int a2aw_sched_pairwise(int rank, int p, NBC_Schedule *schedule,
+                                      const void *sendbuf, const int *sendcounts, const int *sdispls,
+                                      struct ompi_datatype_t * const * sendtypes,
+                                      void *recvbuf, const int *recvcounts, const int *rdispls,
+                                      struct ompi_datatype_t * const * recvtypes) {
+  int res;
+
+  for (int i = 1; i < p; i++) {
+    int sndpeer = (rank + i) % p;
+    int rcvpeer = (rank + p - i) % p;
+
+    /* post send */
+    if (sendcounts[sndpeer] != 0) {
+      char *sbuf = (char *) sendbuf + sdispls[sndpeer];
+      res = NBC_Sched_send (sbuf, false, sendcounts[sndpeer], sendtypes[sndpeer], sndpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+    /* post receive */
+    if (recvcounts[rcvpeer] != 0) {
+      char *rbuf = (char *) recvbuf + rdispls[rcvpeer];
+      res = NBC_Sched_recv (rbuf, false, recvcounts[rcvpeer], recvtypes[rcvpeer], rcvpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+  }
+
+  return NBC_OK;
+}
+
+static inline int a2aw_sched_inplace(int rank, int p, NBC_Schedule *schedule,
+                                     void *buf, const int *counts, const int *displs,
+                                     struct ompi_datatype_t * const * types) {
+  ptrdiff_t gap;
+  int res;
+
+  for (int i = 1; i < (p+1)/2; i++) {
+    int speer = (rank + i) % p;
+    int rpeer = (rank + p - i) % p;
+    char *sbuf = (char *) buf + displs[speer];
+    char *rbuf = (char *) buf + displs[rpeer];
+
+    if (0 != counts[rpeer]) {
+      (void)opal_datatype_span(&types[rpeer]->super, counts[rpeer], &gap);
+      res = NBC_Sched_copy (rbuf, false, counts[rpeer], types[rpeer], (void *)(-gap), true, counts[rpeer], types[rpeer], schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_copy() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+    if (0 != counts[speer]) {
+      res = NBC_Sched_send (sbuf, false , counts[speer], types[speer], speer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+    if (0 != counts[rpeer]) {
+      res = NBC_Sched_recv (rbuf, false , counts[rpeer], types[rpeer], rpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+
+    if (0 != counts[rpeer]) {
+      res = NBC_Sched_send ((void *)(-gap), true, counts[rpeer], types[rpeer], rpeer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    }
+    if (0 != counts[speer]) {
+      res = NBC_Sched_recv (sbuf, false, counts[speer], types[speer], speer, schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+      res = NBC_Sched_barrier(schedule);
+      if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    }
+  }
+  if (0 == (p%2)) {
+    int peer = (rank + p/2) % p;
+
+    char *tbuf = (char *) buf + displs[peer];
+    (void)opal_datatype_span(&types[peer]->super, counts[peer], &gap);
+    res = NBC_Sched_copy (tbuf, false, counts[peer], types[peer], (void *)(-gap), true, counts[peer], types[peer], schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_copy() (%i)\n", res); return res; }
+    res = NBC_Sched_barrier(schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+    res = NBC_Sched_send ((void *)(-gap), true , counts[peer], types[peer], peer, schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_send() (%i)\n", res); return res; }
+    res = NBC_Sched_recv (tbuf, false , counts[peer], types[peer], peer, schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_recv() (%i)\n", res); return res; }
+    res = NBC_Sched_barrier(schedule);
+    if (NBC_OK != res) { printf("Error in NBC_Sched_barr() (%i)\n", res); return res; }
+  }
 
   return NBC_OK;
 }
