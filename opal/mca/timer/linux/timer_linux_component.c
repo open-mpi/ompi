dee73d96cd499ef27a3055c1b135da29338cc8ff
Joshua Hursey <jhursey@us.ibm.com>
2017-01-11 10:30:55 -0600

    timer/linux: Add missing header
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- opal/mca/timer/linux/timer_linux_component.c
+++ opal/mca/timer/linux/timer_linux_component.c
@@ -16,6 +16,7 @@
  *                         reserved.
  * Copyright (c) 2015 Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2016 Broadcom Limited. All rights reserved.
+ * Copyright (c) 2017      IBM Corporation.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -31,6 +32,7 @@
 #include "opal/mca/timer/base/base.h"
 #include "opal/mca/timer/linux/timer_linux.h"
 #include "opal/constants.h"
+#include "opal/util/show_help.h"
 
 static opal_timer_t opal_timer_base_get_cycles_sys_timer(void);
 static opal_timer_t opal_timer_base_get_usec_sys_timer(void);
