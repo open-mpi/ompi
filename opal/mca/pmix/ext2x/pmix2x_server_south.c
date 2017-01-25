7a3d39f079fcb592540c4c177b3f01f5673641b9
Gilles Gouaillardet <gilles@rist.or.jp>
2017-01-25 16:14:21 +0900

    pmix/ext2x: plug a memory leak in _reg_nspace()
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- opal/mca/pmix/ext2x/pmix2x_server_south.c
+++ opal/mca/pmix/ext2x/pmix2x_server_south.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2014-2016 Intel, Inc.  All rights reserved.
- * Copyright (c) 2014-2016 Research Organization for Information Science
+ * Copyright (c) 2014-2017 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  * Copyright (c) 2014-2016 Intel, Inc.  All rights reserved.
  * Copyright (c) 2014      Mellanox Technologies, Inc.
@@ -239,6 +239,7 @@ static void _reg_nspace(int sd, short args, void *cbdata)
                     pmix2x_value_load(&pmap[m].value, k2);
                     ++m;
                 }
+                OPAL_LIST_RELEASE(pmapinfo);
             } else {
                 pmix2x_value_load(&pinfo[n].value, kv);
             }
