6d4198bdcb7a23f049d5364fad3e5fff38cd44cb
Joshua Hursey <jhursey@us.ibm.com>
2017-02-10 16:18:04 -0500

    ess: Make sure orted and HNP clean up their session directories
     * Cleans up some litter that can occur when the HNP and orted are
       in finalize. The HNP, in particular, was leaving the job session
       directory structure in place.
    
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- orte/mca/ess/hnp/ess_hnp_module.c
+++ orte/mca/ess/hnp/ess_hnp_module.c
@@ -17,6 +17,7 @@
  * Copyright (c) 2013-2017 Intel, Inc.  All rights reserved.
  * Copyright (c) 2017      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2017      IBM Corporation. All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -755,6 +756,18 @@ static int rte_init(void)
                        true, error, ORTE_ERROR_NAME(ret), ret);
     }
 
+    /* remove my contact info file, if we have session directories */
+    if (NULL != orte_process_info.job_session_dir) {
+        jobfam_dir = opal_dirname(orte_process_info.job_session_dir);
+        contact_path = opal_os_path(false, jobfam_dir, "contact.txt", NULL);
+        free(jobfam_dir);
+        unlink(contact_path);
+        free(contact_path);
+    }
+    /* remove our use of the session directory tree */
+    orte_session_dir_finalize(ORTE_PROC_MY_NAME);
+    /* ensure we scrub the session directory tree */
+    orte_session_dir_cleanup(ORTE_JOBID_WILDCARD);
     return ORTE_ERR_SILENT;
 }
 
@@ -820,6 +833,8 @@ static int rte_finalize(void)
     (void) mca_base_framework_close(&orte_rml_base_framework);
     (void) mca_base_framework_close(&orte_oob_base_framework);
 
+    /* remove our use of the session directory tree */
+    orte_session_dir_finalize(ORTE_PROC_MY_NAME);
     /* ensure we scrub the session directory tree */
     orte_session_dir_cleanup(ORTE_JOBID_WILDCARD);
 
