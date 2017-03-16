194728086588a24890c3e2d5543cde7e1d9e31b7
Jeff Squyres <jsquyres@cisco.com>
2017-03-16 05:44:26 -0700

    topo/treematch: squash some compiler warnings
    Only define MIN/MAX if they are not already defined.
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- ompi/mca/topo/treematch/treematch/tm_tree.c
+++ ompi/mca/topo/treematch/treematch/tm_tree.c
@@ -12,8 +12,12 @@
 #include "tm_thread_pool.h"
 
 
+#if !defined(MIN)
 #define MIN(a,b) ((a)<(b)?(a):(b))
+#endif
+#if !defined(MAX)
 #define MAX(a,b) ((a)>(b)?(a):(b))
+#endif
 
 #ifndef __CHARMC__
 #define __CHARMC__ 0
