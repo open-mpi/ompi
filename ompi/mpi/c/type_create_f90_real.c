89436e70227cc080b5bf48414883eb31aecd2220
Joshua Hursey <jhursey@us.ibm.com>
2017-05-23 17:54:33 -0400

    ompi/mpi: Fix parameter order in mpi_type_create_f90_(real|complex)
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>
    (cherry picked from commit 5e302f52794f080d7e03da3933f20ed0a5c4a9ef)
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- ompi/mpi/c/type_create_f90_real.c
+++ ompi/mpi/c/type_create_f90_real.c
@@ -14,6 +14,7 @@
  * Copyright (c) 2008      Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2013      Los Alamos National Security, LLC.  All rights
  *                         reserved.
+ * Copyright (c) 2017      IBM Corporation.  All rights reserved.
  * $COPYRIGHT$
  * 
  * Additional copyrights may follow
@@ -82,7 +83,7 @@ int MPI_Type_create_f90_real(int p, int r, MPI_Datatype *newtype)
 
     if( *newtype != &ompi_mpi_datatype_null.dt ) {
         ompi_datatype_t* datatype;
-        const int* a_i[2] = {&r, &p};
+        const int* a_i[2] = {&p, &r};
         int rc;
 
         key = (((uint64_t)p) << 32) | ((uint64_t)r);
