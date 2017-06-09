ca3d7cca1408f7f53dccbcaa2c919fb37b7586cb
Joshua Hursey <jhursey@us.ibm.com>
2017-06-09 11:22:28 -0400

    mpi/c: Protect some IO functions not widely implemented
     * Protects us from segv when ROMIO 314 is selected and one of the
       following operations is called:
       - MPI_File_iread_at_all
       - MPI_File_iwrite_at_all
       - MPI_File_iread_all
       - MPI_File_iwrite_all
    
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>
    (cherry picked from commit 29609631a246ddbd4cb375651c4afa5ec72692bc)
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- ompi/mpi/c/file_iwrite_at_all.c
+++ ompi/mpi/c/file_iwrite_at_all.c
@@ -16,6 +16,7 @@
  * Copyright (c) 2015      University of Houston.  All rights reserved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2017      IBM Corporation.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -76,9 +77,14 @@ int MPI_File_iwrite_at_all(MPI_File fh, MPI_Offset offset, const void *buf,
     /* Call the back-end io component function */
     switch (fh->f_io_version) {
     case MCA_IO_BASE_V_2_0_0:
-        rc = fh->f_io_selected_module.v2_0_0.
-            io_module_file_iwrite_at_all(fh, offset, buf, count, datatype,
-                                         request);
+        if( OPAL_UNLIKELY(NULL == fh->f_io_selected_module.v2_0_0.io_module_file_iwrite_at_all) ) {
+            rc = MPI_ERR_UNSUPPORTED_OPERATION;
+        }
+        else {
+            rc = fh->f_io_selected_module.v2_0_0.
+                io_module_file_iwrite_at_all(fh, offset, buf, count, datatype,
+                                             request);
+        }
         break;
 
     default:
