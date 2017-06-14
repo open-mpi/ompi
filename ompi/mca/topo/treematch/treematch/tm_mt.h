c2927d7e9107cd381413c352c538f0b4ff344837
George Bosilca <bosilca@icl.utk.edu>
2017-06-13 22:15:09 -0400

    Update to the latest version provided by Guillaume.
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>

--- ompi/mca/topo/treematch/treematch/tm_mt.h
+++ ompi/mca/topo/treematch/treematch/tm_mt.h
@@ -2,8 +2,7 @@ void init_genrand(unsigned long s);
 void init_by_array(unsigned long init_key[], int key_length);
 
 /* generates a random number on the interval [0,0x7fffffff] */
-unsigned long genrand_int32(void);
-
+unsigned long genrand_int32(void); 
 long genrand_int31(void);
 double genrand_real1(void);
 double genrand_real2(void);
--- ompi/mca/topo/treematch/treematch/tm_thread_pool.h
+++ ompi/mca/topo/treematch/treematch/tm_thread_pool.h
@@ -2,17 +2,18 @@
 #define THREAD_POOL_H
 
 #include <pthread.h>
-#include "opal/mca/hwloc/hwloc-internal.h"
+#include <hwloc.h>
 
 
 typedef struct _work_t{
   int nb_args;
-  void (*task)(int nb_args, void **args);
+  void (*task)(int nb_args, void **args, int thread_id);
   void **args;
   struct _work_t *next;
   pthread_cond_t work_done;
   pthread_mutex_t mutex;
   int done;
+  int thread_id;
 }work_t;
 
 typedef struct {
@@ -38,8 +39,10 @@ int get_nb_threads(void);
 int submit_work(work_t *work, int thread_id);
 void wait_work_completion(work_t *work);
 void terminate_thread_pool(void);
-work_t *create_work(int nb_args, void **args, void (int, void **));
+work_t *create_work(int nb_args, void **args, void (int, void **, int));
 int test_main(void);
+
+
 
 
 #endif /* THREAD_POOL_H */
