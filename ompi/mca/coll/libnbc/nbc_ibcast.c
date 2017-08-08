sebastian-steiner/ompi_pmbs

coll/libnbc: revisit NBC_Handle usage
make NBC_Handle (almost) an internal structure created
by NBC_Schedule_request()
use a local variable instead of what was previously handle->tmpbuf

Refs open-mpi/ompi#3487

Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

ompi/mca/coll/libnbc/nbc_ibcast.c
@@ -55,7 +55,6 @@ int ompi_coll_libnbc_ibcast(void *buffer, int count, MPI_Datatype datatype, int 
   NBC_Bcast_args *args, *found, search;
 #endif
   enum { NBC_BCAST_LINEAR, NBC_BCAST_BINOMIAL, NBC_BCAST_CHAIN } alg;
-  NBC_Handle *handle;
   ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
 
   rank = ompi_comm_rank (comm);
@@ -163,19 +162,11 @@ int ompi_coll_libnbc_ibcast(void *buffer, int count, MPI_Datatype datatype, int 
   }
 #endif
 
-  res = NBC_Init_handle (comm, &handle, libnbc_module);
+  res = NBC_Schedule_request(schedule, comm, libnbc_module, request, NULL);
   if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
     OBJ_RELEASE(schedule);
     return res;
   }
-
-  res = NBC_Start (handle, schedule);
-  if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
-    NBC_Return_handle (handle);
-    return res;
-  }
-
-  *request = (ompi_request_t *) handle;
 
   return OMPI_SUCCESS;
 }
@@ -327,6 +318,58 @@ static inline int bcast_sched_chain(int rank, int p, int root, NBC_Schedule *sch
         }
       }
     }
+  }
+
+  return OMPI_SUCCESS;
+}
+
+int ompi_coll_libnbc_ibcast_inter(void *buffer, int count, MPI_Datatype datatype, int root,
+                                  struct ompi_communicator_t *comm, ompi_request_t ** request,
+                                  struct mca_coll_base_module_2_2_0_t *module) {
+  int res;
+  NBC_Schedule *schedule;
+  ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
+
+  schedule = OBJ_NEW(NBC_Schedule);
+  if (OPAL_UNLIKELY(NULL == schedule)) {
+    return OMPI_ERR_OUT_OF_RESOURCE;
+  }
+
+  if (root != MPI_PROC_NULL) {
+    /* send to all others */
+    if (root == MPI_ROOT) {
+      int remsize;
+
+      remsize = ompi_comm_remote_size (comm);
+
+      for (int peer = 0 ; peer < remsize ; ++peer) {
+        /* send msg to peer */
+        res = NBC_Sched_send (buffer, false, count, datatype, peer, schedule, false);
+        if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+          OBJ_RELEASE(schedule);
+          return res;
+        }
+      }
+    } else {
+      /* recv msg from root */
+      res = NBC_Sched_recv (buffer, false, count, datatype, root, schedule, false);
+      if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+        OBJ_RELEASE(schedule);
+        return res;
+      }
+    }
+  }
+
+  res = NBC_Sched_commit (schedule);
+  if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+    OBJ_RELEASE(schedule);
+    return res;
+  }
+
+  res = NBC_Schedule_request(schedule, comm, libnbc_module, request, NULL);
+  if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+    OBJ_RELEASE(schedule);
+    return res;
   }
 
   return OMPI_SUCCESS;

open-mpi/ompi

(back-ported from commit open-mpi/ompi@9ba85b85e1fdc9cb6860a48d74f5c7216cbe42cd)

ompi/mca/coll/libnbc/nbc_ibcast.c
@@ -52,7 +52,6 @@ int ompi_coll_libnbc_ibcast(void *buffer, int count, MPI_Datatype datatype, int 
   NBC_Bcast_args *args, *found, search;
 #endif
   enum { NBC_BCAST_LINEAR, NBC_BCAST_BINOMIAL, NBC_BCAST_CHAIN } alg;
-  NBC_Handle *handle;
   ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
 
   rank = ompi_comm_rank (comm);
@@ -155,19 +154,11 @@ int ompi_coll_libnbc_ibcast(void *buffer, int count, MPI_Datatype datatype, int 
   }
 #endif
 
-  res = NBC_Init_handle (comm, &handle, libnbc_module);
+  res = NBC_Schedule_request(schedule, comm, libnbc_module, request, NULL);
   if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
     OBJ_RELEASE(schedule);
     return res;
   }
-
-  res = NBC_Start (handle, schedule);
-  if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
-    NBC_Return_handle (handle);
-    return res;
-  }
-
-  *request = (ompi_request_t *) handle;
 
   return OMPI_SUCCESS;
 }
@@ -319,6 +310,58 @@ static inline int bcast_sched_chain(int rank, int p, int root, NBC_Schedule *sch
         }
       }
     }
+  }
+
+  return OMPI_SUCCESS;
+}
+
+int ompi_coll_libnbc_ibcast_inter(void *buffer, int count, MPI_Datatype datatype, int root,
+                                  struct ompi_communicator_t *comm, ompi_request_t ** request,
+                                  struct mca_coll_base_module_2_1_0_t *module) {
+  int res;
+  NBC_Schedule *schedule;
+  ompi_coll_libnbc_module_t *libnbc_module = (ompi_coll_libnbc_module_t*) module;
+
+  schedule = OBJ_NEW(NBC_Schedule);
+  if (OPAL_UNLIKELY(NULL == schedule)) {
+    return OMPI_ERR_OUT_OF_RESOURCE;
+  }
+
+  if (root != MPI_PROC_NULL) {
+    /* send to all others */
+    if (root == MPI_ROOT) {
+      int remsize;
+
+      remsize = ompi_comm_remote_size (comm);
+
+      for (int peer = 0 ; peer < remsize ; ++peer) {
+        /* send msg to peer */
+        res = NBC_Sched_send (buffer, false, count, datatype, peer, schedule, false);
+        if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+          OBJ_RELEASE(schedule);
+          return res;
+        }
+      }
+    } else {
+      /* recv msg from root */
+      res = NBC_Sched_recv (buffer, false, count, datatype, root, schedule, false);
+      if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+        OBJ_RELEASE(schedule);
+        return res;
+      }
+    }
+  }
+
+  res = NBC_Sched_commit (schedule);
+  if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+    OBJ_RELEASE(schedule);
+    return res;
+  }
+
+  res = NBC_Schedule_request(schedule, comm, libnbc_module, request, NULL);
+  if (OPAL_UNLIKELY(OMPI_SUCCESS != res)) {
+    OBJ_RELEASE(schedule);
+    return res;
   }
 
   return OMPI_SUCCESS;