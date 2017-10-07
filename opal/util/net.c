b3438cf98670db7f47d2e87f6ee2d68a392d56c5
Jeff Squyres <jsquyres@cisco.com>
2017-10-07 06:01:35 -0700

    opal/util/net.c: fix compiler warning
    This variable is only used when IPv6 support is enabled; ensure to
    protect it in an #if OPAL_ENABLE_IPv6 check.
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- opal/util/net.c
+++ opal/util/net.c
@@ -11,7 +11,7 @@
  *                         All rights reserved.
  * Copyright (c) 2007      Los Alamos National Security, LLC.  All rights
  *                         reserved.
- * Copyright (c) 2009-2015 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2009-2017 Cisco Systems, Inc.  All rights reserved
  * Copyright (c) 2013      Intel, Inc.  All rights reserved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
