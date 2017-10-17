1fff9cc0ea180136aaea720035c569f4aa218ebe
Matias Cabral <matias.a.cabral@intel.com>
2017-10-17 06:13:04 -0700

    MTL PSM2: add a thread lock while peeking and completing the psm2 requests. Reviewed-by: Gopalakrishnan, Aravind <aravind.gopalakrishnan@intel.com> Signed-off-by: Matias Cabral <matias.a.cabral@intel.com>
    (cherry picked from commit b81bcd4b0d71ad02637d469b356ba2e7134bb1ef)
    Signed-off-by: Matias Cabral <matias.a.cabral@intel.com>

--- ompi/mca/mtl/psm2/mtl_psm2_component.c
+++ ompi/mca/mtl/psm2/mtl_psm2_component.c
@@ -40,6 +40,8 @@
 #include <glob.h>
 
 static int param_priority;
+/* MPI_THREAD_MULTIPLE_SUPPORT */
+opal_mutex_t mtl_psm2_mq_mutex = OPAL_MUTEX_STATIC_INIT;
 
 static int ompi_mtl_psm2_component_open(void);
 static int ompi_mtl_psm2_component_close(void);
