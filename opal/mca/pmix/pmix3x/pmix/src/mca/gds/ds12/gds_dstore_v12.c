c26a49f0b2a5e8a6840ff87046929ebcb3d4f7f0
Ralph Castain <rhc@open-mpi.org>
2017-12-14 06:47:59 -0800

    Restore the v12 module, remove all inlining
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore_v12.c
+++ opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore_v12.c
@@ -34,7 +34,7 @@ dstore_mod_t dstore_v12_module = {
     _ds12_put_key
 };
 
-static inline size_t _ds12_kv_size(uint8_t *addr) {
+static size_t _ds12_kv_size(uint8_t *addr) {
     size_t sz;
     memcpy(&sz, addr + _ds12_key_len(_ds12_key_name_ptr(addr)),
         sizeof(size_t));
@@ -42,28 +42,28 @@ static inline size_t _ds12_kv_size(uint8_t *addr) {
     return sz;
 }
 
-static inline size_t _ds12_key_size(const char *key, size_t data_size) {
+static size_t _ds12_key_size(const char *key, size_t data_size) {
     size_t len = strlen(key) + 1 + sizeof(size_t) + data_size;
     return len;
 }
 
-static inline char* _ds12_key_name_ptr(uint8_t *addr) {
+static char* _ds12_key_name_ptr(uint8_t *addr) {
     char *name_ptr = (char *)addr;
     return name_ptr;
 }
 
-static inline size_t _ds12_key_len(const char *key) {
+static size_t _ds12_key_len(const char *key) {
     size_t len = strlen((char*)key) + 1;
     return len;
 }
 
-static inline uint8_t* _ds12_data_ptr(uint8_t *addr) {
+static uint8_t* _ds12_data_ptr(uint8_t *addr) {
     uint8_t *data_ptr = addr + sizeof(size_t) +
         _ds12_key_len(_ds12_key_name_ptr(addr));
     return data_ptr;
 }
 
-static inline size_t _ds12_data_size(uint8_t *addr, uint8_t *data_ptr) {
+static size_t _ds12_data_size(uint8_t *addr, uint8_t *data_ptr) {
     size_t data_size;
     memcpy(&data_size,
         addr + _ds12_key_len(_ds12_key_name_ptr(addr)),
@@ -71,11 +71,11 @@ static inline size_t _ds12_data_size(uint8_t *addr, uint8_t *data_ptr) {
     return data_size;
 }
 
-static inline size_t _ds12_slot_size(void) {
+static size_t _ds12_slot_size(void) {
     return _ds12_key_size(ESH_REGION_EXTENSION, sizeof(size_t));
 }
 
-static inline void _ds12_put_key(uint8_t *data, char *key, void *buf, size_t size) {
+static void _ds12_put_key(uint8_t *data, char *key, void *buf, size_t size) {
     size_t sz = size;
     memset(data, 0, _ds12_key_len(key));
     strncpy((char *)data, key, _ds12_key_len(key));
--- opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore_v20.c
+++ opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore_v20.c
@@ -34,47 +34,47 @@ dstore_mod_t dstore_v20_module = {
     _ds20_put_key
 };
 
-static inline size_t _ds20_kv_size(uint8_t *addr) {
+static size_t _ds20_kv_size(uint8_t *addr) {
     size_t sz;
     memcpy(&sz, addr, sizeof(size_t));
     return sz;
 }
 
-static inline size_t _ds20_key_size(const char *key, size_t data_size) {
+static size_t _ds20_key_size(const char *key, size_t data_size) {
     size_t len = sizeof(size_t) + _ds20_key_len(key) + data_size;
     return len;
 }
 
-static inline char* _ds20_key_name_ptr(uint8_t *addr) {
+static char* _ds20_key_name_ptr(uint8_t *addr) {
     char *name_ptr = (char *)addr + sizeof(size_t);
     return name_ptr;
 }
 
-static inline size_t _ds20_key_len(const char *key) {
+static size_t _ds20_key_len(const char *key) {
     size_t kname_len = strlen(key) + 1;
     size_t len = (kname_len < ESH_MIN_KEY_LEN) ?
     ESH_MIN_KEY_LEN : kname_len;
     return len;
 }
 
-static inline uint8_t* _ds20_data_ptr(uint8_t *addr) {
+static uint8_t* _ds20_data_ptr(uint8_t *addr) {
     size_t kname_len =
         _ds20_key_len(_ds20_key_name_ptr(addr));
     uint8_t *data_ptr = addr + sizeof(size_t) + kname_len;
     return data_ptr;
 }
 
-static inline size_t _ds20_data_size(uint8_t *addr, uint8_t *data_ptr) {
+static size_t _ds20_data_size(uint8_t *addr, uint8_t *data_ptr) {
     size_t sz = _ds20_kv_size(addr);
     size_t data_size = sz - (data_ptr - addr);
     return data_size;
 }
 
-static inline size_t _ds20_slot_size(void) {
+static size_t _ds20_slot_size(void) {
     return _ds20_key_size(ESH_REGION_EXTENSION, sizeof(size_t));
 }
 
-static inline void _ds20_put_key(uint8_t *data, char *key, void *buf, size_t size) {
+static void _ds20_put_key(uint8_t *data, char *key, void *buf, size_t size) {
     size_t sz = _ds20_key_size(key, size);
     memcpy(data, &sz, sizeof(size_t));
     memset(data + sizeof(size_t), 0, _ds20_key_len(key));
