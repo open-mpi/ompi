ef772f3b50ab8f2e1c9add31c37a425b49689067
Ralph Castain <rhc@open-mpi.org>
2017-12-13 21:10:29 -0800

    Update with macro removals
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore.c
+++ opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore.c
@@ -52,75 +52,76 @@
 #include "src/mca/pshmem/base/base.h"
 
 #ifdef ESH_PTHREAD_LOCK
-#define _ESH_LOCK(rwlock, func)                             \
-__extension__ ({                                            \
-    pmix_status_t ret = PMIX_SUCCESS;                       \
-    int rc;                                                 \
-    rc = pthread_rwlock_##func(rwlock);                     \
-    if (0 != rc) {                                          \
-        switch (errno) {                                    \
-            case EINVAL:                                    \
-                ret = PMIX_ERR_INIT;                        \
-                break;                                      \
-            case EPERM:                                     \
-                ret = PMIX_ERR_NO_PERMISSIONS;              \
-                break;                                      \
-        }                                                   \
-    }                                                       \
-    if (ret) {                                              \
-        pmix_output(0, "%s %d:%s lock failed: %s",          \
-            __FILE__, __LINE__, __func__, strerror(errno)); \
-    }                                                       \
-    ret;                                                    \
-})
+static int inline _esh_pthread_lock(pthread_rwlock_t *rwlock, int rwlock_func(pthread_rwlock_t *rwlock));
 
-#define _ESH_WRLOCK(rwlock) _ESH_LOCK(rwlock, wrlock)
-#define _ESH_RDLOCK(rwlock) _ESH_LOCK(rwlock, rdlock)
-#define _ESH_UNLOCK(rwlock) _ESH_LOCK(rwlock, unlock)
+static int inline _esh_pthread_lock(pthread_rwlock_t *rwlock, int rwlock_func(pthread_rwlock_t *rwlock)) {
+    pmix_status_t ret = PMIX_SUCCESS;
+    int rc;
+    rc = rwlock_func(rwlock);
+    if (0 != rc) {
+        switch (errno) {
+            case EINVAL:
+                ret = PMIX_ERR_INIT;
+                break;
+            case EPERM:
+                ret = PMIX_ERR_NO_PERMISSIONS;
+                break;
+        }
+    }
+    if (ret) {
+        pmix_output(0, "%s %d:%s lock failed: %s",
+            __FILE__, __LINE__, __func__, strerror(errno));
+    }
+    return ret;
+}
+
+#define _ESH_WRLOCK(rwlock) _esh_pthread_lock(rwlock, pthread_rwlock_wrlock)
+#define _ESH_RDLOCK(rwlock) _esh_pthread_lock(rwlock, pthread_rwlock_rdlock)
+#define _ESH_UNLOCK(rwlock) _esh_pthread_lock(rwlock, pthread_rwlock_unlock)
 #endif
 
 #ifdef ESH_FCNTL_LOCK
-#define _ESH_LOCK(lockfd, operation)                        \
-__extension__ ({                                            \
-    pmix_status_t ret = PMIX_SUCCESS;                       \
-    int i;                                                  \
-    struct flock fl = {0};                                  \
-    fl.l_type = operation;                                  \
-    fl.l_whence = SEEK_SET;                                 \
-    for(i = 0; i < 10; i++) {                               \
-        if( 0 > fcntl(lockfd, F_SETLKW, &fl) ) {            \
-            switch( errno ){                                \
-                case EINTR:                                 \
-                    continue;                               \
-                case ENOENT:                                \
-                case EINVAL:                                \
-                    ret = PMIX_ERR_NOT_FOUND;               \
-                    break;                                  \
-                case EBADF:                                 \
-                    ret = PMIX_ERR_BAD_PARAM;               \
-                    break;                                  \
-                case EDEADLK:                               \
-                case EFAULT:                                \
-                case ENOLCK:                                \
-                    ret = PMIX_ERR_RESOURCE_BUSY;           \
-                    break;                                  \
-                default:                                    \
-                    ret = PMIX_ERROR;                       \
-                    break;                                  \
-            }                                               \
-        }                                                   \
-        break;                                              \
-    }                                                       \
-    if (ret) {                                              \
-        pmix_output(0, "%s %d:%s lock failed: %s",          \
-            __FILE__, __LINE__, __func__, strerror(errno)); \
-    }                                                       \
-    ret;                                                    \
-})
+static int inline _esh_fcntl_lock(int lockfd, int operation);
 
-#define _ESH_WRLOCK(lock) _ESH_LOCK(lock, F_WRLCK)
-#define _ESH_RDLOCK(lock) _ESH_LOCK(lock, F_RDLCK)
-#define _ESH_UNLOCK(lock) _ESH_LOCK(lock, F_UNLCK)
+static int inline _esh_fcntl_lock(int lockfd, int operation) {
+    pmix_status_t ret = PMIX_SUCCESS;
+    int i;
+    struct flock fl = {0};
+    fl.l_type = operation;
+    fl.l_whence = SEEK_SET;
+    for(i = 0; i < 10; i++) {
+        if( 0 > fcntl(lockfd, F_SETLKW, &fl) ) {
+            switch( errno ){
+                case EINTR:
+                    continue;
+                case ENOENT:
+                case EINVAL:
+                    ret = PMIX_ERR_NOT_FOUND;
+                    break;
+                case EBADF:
+                    ret = PMIX_ERR_BAD_PARAM;
+                    break;
+                case EDEADLK:
+                case EFAULT:
+                case ENOLCK:
+                    ret = PMIX_ERR_RESOURCE_BUSY;
+                    break;
+                default:
+                    ret = PMIX_ERROR;
+                    break;
+            }
+        }
+        break;
+    }
+    if (ret) {
+        pmix_output(0, "%s %d:%s lock failed: %s",
+            __FILE__, __LINE__, __func__, strerror(errno));
+    }
+    return ret;
+}
+#define _ESH_WRLOCK(lock) _esh_fcntl_lock(lock, F_WRLCK)
+#define _ESH_RDLOCK(lock) _esh_fcntl_lock(lock, F_RDLCK)
+#define _ESH_UNLOCK(lock) _esh_fcntl_lock(lock, F_UNLCK)
 #endif
 
 #define ESH_INIT_SESSION_TBL_SIZE 2
--- opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore_v12.c
+++ opal/mca/pmix/pmix3x/pmix/src/mca/gds/ds12/gds_dstore_v12.c
@@ -13,71 +13,6 @@
 
 #include "gds_dstore.h"
 
-#define ESH_KEY_SIZE(key, size)                             \
-__extension__ ({                                            \
-    size_t len = strlen(key) + 1 + sizeof(size_t) + size;   \
-    len;                                                    \
-})
-
-/* in ext slot new offset will be stored in case if
- * new data were added for the same process during
- * next commit
- */
-#define EXT_SLOT_SIZE()                                     \
-    (ESH_KEY_SIZE(ESH_REGION_EXTENSION, sizeof(size_t)))
-
-#define ESH_KV_SIZE(addr)                                   \
-__extension__ ({                                            \
-    size_t sz;                                              \
-    memcpy(&sz, addr +                                      \
-        ESH_KNAME_LEN(ESH_KNAME_PTR(addr)),                 \
-        sizeof(size_t));                                    \
-    sz += ESH_KNAME_LEN(ESH_KNAME_PTR(addr)) +              \
-        sizeof(size_t);                                     \
-    sz;                                                     \
-})
-
-#define ESH_KNAME_PTR(addr)                                 \
-__extension__ ({                                            \
-    char *name_ptr = (char *)addr;                          \
-    name_ptr;                                               \
-})
-
-#define ESH_KNAME_LEN(key)                                  \
-__extension__ ({                                            \
-    size_t len = strlen((char*)key) + 1;                    \
-    len;                                                    \
-})
-
-#define ESH_DATA_PTR(addr)                                  \
-__extension__ ({                                            \
-    uint8_t *data_ptr =                                     \
-        addr +                                              \
-        sizeof(size_t) +                                    \
-        ESH_KNAME_LEN(ESH_KNAME_PTR(addr));                 \
-    data_ptr;                                               \
-})
-
-#define ESH_DATA_SIZE(addr)                                 \
-__extension__ ({                                            \
-    size_t data_size;                                       \
-    memcpy(&data_size,                                      \
-        addr + ESH_KNAME_LEN(ESH_KNAME_PTR(addr)),          \
-        sizeof(size_t));                                    \
-    data_size;                                              \
-})
-
-#define ESH_PUT_KEY(addr, key, buffer, size)                \
-__extension__ ({                                            \
-    size_t sz = size;                                       \
-    memset(addr, 0, ESH_KNAME_LEN(key));                    \
-    strncpy((char *)addr, key, ESH_KNAME_LEN(key));         \
-    memcpy(addr + ESH_KNAME_LEN(key), &sz,                  \
-        sizeof(size_t));                                    \
-    memcpy(addr + ESH_KNAME_LEN(key) + sizeof(size_t),      \
-            buffer, size);                                  \
-})
-
 static size_t _ds12_kv_size(uint8_t *addr);
 static size_t _ds12_key_size(const char *key, size_t data_size);
 static char* _ds12_key_name_ptr(uint8_t *addr);
@@ -99,34 +34,51 @@ dstore_mod_t dstore_v12_module = {
     _ds12_put_key
 };
 
-static size_t _ds12_kv_size(uint8_t *addr) {
-    return ESH_KV_SIZE(addr);
+static inline size_t _ds12_kv_size(uint8_t *addr) {
+    size_t sz;
+    memcpy(&sz, addr + _ds12_key_len(_ds12_key_name_ptr(addr)),
+        sizeof(size_t));
+    sz += _ds12_key_len(_ds12_key_name_ptr(addr)) + sizeof(size_t);
+    return sz;
 }
 
-static size_t _ds12_key_size(const char *key, size_t data_size) {
-    return ESH_KEY_SIZE(key, data_size);
+static inline size_t _ds12_key_size(const char *key, size_t data_size) {
+    size_t len = strlen(key) + 1 + sizeof(size_t) + data_size;
+    return len;
 }
 
-static char* _ds12_key_name_ptr(uint8_t *addr) {
-    return ESH_KNAME_PTR(addr);
+static inline char* _ds12_key_name_ptr(uint8_t *addr) {
+    char *name_ptr = (char *)addr;
+    return name_ptr;
 }
 
-static size_t _ds12_key_len(const char *key) {
-    return ESH_KNAME_LEN(key);
+static inline size_t _ds12_key_len(const char *key) {
+    size_t len = strlen((char*)key) + 1;
+    return len;
 }
 
-static uint8_t* _ds12_data_ptr(uint8_t *addr) {
-    return ESH_DATA_PTR(addr);
+static inline uint8_t* _ds12_data_ptr(uint8_t *addr) {
+    uint8_t *data_ptr = addr + sizeof(size_t) +
+        _ds12_key_len(_ds12_key_name_ptr(addr));
+    return data_ptr;
 }
 
-static size_t _ds12_data_size(uint8_t *addr, uint8_t *data_ptr) {
-    return ESH_DATA_SIZE(addr);
+static inline size_t _ds12_data_size(uint8_t *addr, uint8_t *data_ptr) {
+    size_t data_size;
+    memcpy(&data_size,
+        addr + _ds12_key_len(_ds12_key_name_ptr(addr)),
+        sizeof(size_t));
+    return data_size;
 }
 
-static size_t _ds12_slot_size(void) {
-    return EXT_SLOT_SIZE();
+static inline size_t _ds12_slot_size(void) {
+    return _ds12_key_size(ESH_REGION_EXTENSION, sizeof(size_t));
 }
 
-static void _ds12_put_key(uint8_t *data, char *key, void *buf, size_t size) {
-    ESH_PUT_KEY(data, key, buf, size);
+static inline void _ds12_put_key(uint8_t *data, char *key, void *buf, size_t size) {
+    size_t sz = size;
+    memset(data, 0, _ds12_key_len(key));
+    strncpy((char *)data, key, _ds12_key_len(key));
+    memcpy(data + _ds12_key_len(key), &sz, sizeof(size_t));
+    memcpy(data + _ds12_key_len(key) + sizeof(size_t), buf, size);
 }
