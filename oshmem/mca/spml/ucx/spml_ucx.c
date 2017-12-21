4fa3628646bbc0a7f58fc4b4a1e6e03f3b68debb
Yossi Itigin <yosefe@mellanox.com>
2017-12-21 18:40:20 +0200

    spml_ucx: fix rkey leak
    Signed-off-by: Yossi Itigin <yosefe@mellanox.com>
    (cherry-picked from commit 1193e1e)

--- oshmem/mca/spml/ucx/spml_ucx.c
+++ oshmem/mca/spml/ucx/spml_ucx.c
@@ -528,12 +528,13 @@ int mca_spml_ucx_deregister(sshmem_mkey_t *mkeys)
     if (!mkeys[0].spml_context) 
         return OSHMEM_SUCCESS;
 
-    mem_seg = memheap_find_va(mkeys[0].va_base);
+    mem_seg  = memheap_find_va(mkeys[0].va_base);
+    ucx_mkey = (spml_ucx_mkey_t*)mkeys[0].spml_context;
     
     if (MAP_SEGMENT_ALLOC_UCX != mem_seg->type) {
-        ucx_mkey = (spml_ucx_mkey_t *)mkeys[0].spml_context;
         ucp_mem_unmap(mca_spml_ucx.ucp_context, ucx_mkey->mem_h);
     }
+    ucp_rkey_destroy(ucx_mkey->rkey);
 
     if (0 < mkeys[0].len) {
         ucp_rkey_buffer_release(mkeys[0].u.data);
