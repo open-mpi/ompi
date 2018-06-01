6efb8069ec905efb3b9ffac98757baf98de8399a
Thananon Patinyasakdikul <thananon.patinyasakdikul@intel.com>
2018-06-01 13:53:53 -0700

    new btl/ofi: RDMA only btl using libfabric.
    This commit added new transport layer to be used with osc rdma module.
    This BTL provides put, get, atomic and fetch atomic operations. It can
    be used with multiple hardware vendors as long as they have their
    provider under Libfabric and have the right capabilities.
    
    Signed-off-by: Thananon Patinyasakdikul <thananon.patinyasakdikul@intel.com>

---
+++ opal/mca/btl/ofi/btl_ofi_endpoint.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2006 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2017-2018 Los Alamos National Security, LLC. All rights
+ *                         reserved.
+ * Copyright (c) 2018      Intel, Inc, All rights reserved
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef MCA_BTL_OFI_ENDPOINT_H
+#define MCA_BTL_OFI_ENDPOINT_H
+
+#include "opal/class/opal_list.h"
+#include "opal/mca/event/event.h"
+
+#include "btl_ofi.h"
+
+BEGIN_C_DECLS
+
+struct mca_btl_base_endpoint_t {
+    opal_list_item_t super;
+
+    struct fid_ep *ofi_endpoint;
+    fi_addr_t peer_addr;
+
+    /** endpoint proc */
+    opal_proc_t *ep_proc;
+
+    /** mutex to protect this structure */
+    opal_mutex_t ep_lock;
+};
+
+typedef struct mca_btl_base_endpoint_t mca_btl_base_endpoint_t;
+typedef mca_btl_base_endpoint_t mca_btl_ofi_endpoint_t;
+OBJ_CLASS_DECLARATION(mca_btl_ofi_endpoint_t);
+
+mca_btl_base_endpoint_t *mca_btl_ofi_endpoint_create (opal_proc_t *proc, struct fid_ep *ep);
+
+END_C_DECLS
+#endif
