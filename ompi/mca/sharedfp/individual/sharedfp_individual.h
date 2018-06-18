9986a15b57ef21012e7b22514aeb333a88883191
Edgar Gabriel <egabriel@central.uh.edu>
2018-06-18 18:25:29 -0500

    sharedfp/individual: only complain about fseek if sharedfp operations are really in use
    this component can only be used in very specific scenarios. However, since some file systems do not support file locking and processes might be distributed over multiple nodes (hence the sm sharedfp component is also inelligible), the component might be selected in some scenarios, even if an application does not intend to use shared file pointers.
    
    Since the fseek_shared function is involved as part of the File_set_view operation, only complain about the inability to perform the seek_shared operation if actual shared file pointer operations are being used. This avoid spurious error values being returned.
    
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- ompi/mca/sharedfp/individual/sharedfp_individual.h
+++ ompi/mca/sharedfp/individual/sharedfp_individual.h
@@ -42,6 +42,7 @@ int mca_sharedfp_individual_module_finalize (ompio_file_t *file);
 
 extern int mca_sharedfp_individual_priority;
 extern int mca_sharedfp_individual_verbose;
+extern int mca_sharedfp_individual_usage_counter;
 
 OMPI_MODULE_DECLSPEC extern mca_sharedfp_base_component_2_0_0_t mca_sharedfp_individual_component;
 /*
