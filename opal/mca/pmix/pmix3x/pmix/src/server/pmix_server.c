17c4cf0db8bc46abd83843961e6932bcf3e38ee1
Ralph Castain <rhc@open-mpi.org>
2018-07-06 06:38:02 -0700

    Install PMIx v3.0.0 release
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- opal/mca/pmix/pmix3x/pmix/src/server/pmix_server.c
+++ opal/mca/pmix/pmix3x/pmix/src/server/pmix_server.c
@@ -1821,10 +1821,10 @@ static void clct_complete(pmix_status_t status,
 static void clct(int sd, short args, void *cbdata)
 {
     pmix_inventory_rollup_t *cd = (pmix_inventory_rollup_t*)cbdata;
+    pmix_status_t rc;
 
 #if PMIX_HAVE_HWLOC
     /* if we don't know our topology, we better get it now */
-    pmix_status_t rc;
     if (NULL == pmix_hwloc_topology) {
         if (PMIX_SUCCESS != (rc = pmix_hwloc_get_topology(NULL, 0))) {
             PMIX_ERROR_LOG(rc);
