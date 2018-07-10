open-mpi/ompi

commit b90c83840f472de3219b87cd7e1a364eec5c5a29

Refactor the request completion (#1422)
* Remodel the request.
Added the wait sync primitive and integrate it into the PML and MTL
infrastructure. The multi-threaded requests are now significantly
less heavy and less noisy (only the threads associated with completed
requests are signaled).

* Fix the condition to release the request.

ompi/mca/osc/rdma/osc_rdma_request.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2011-2012 Sandia National Laboratories.  All rights reserved.
  * Copyright (c) 2014-2015 Los Alamos National Security, LLC. All rights
  *                         reserved.
+ * Copyright (c) 2016      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -30,7 +33,7 @@ static int request_free(struct ompi_request_t **ompi_req)
     ompi_osc_rdma_request_t *request =
         (ompi_osc_rdma_request_t*) *ompi_req;
 
-    if (true != request->super.req_complete) {
+    if( REQUEST_COMPLETE(&request->super) ) {
         return MPI_ERR_REQUEST;
     }
 

ompi/communicator/comm_request.c
@@ -4,6 +4,9 @@
  *                         reseved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -114,7 +117,7 @@ static int ompi_comm_request_progress (void)
             /* don't call ompi_request_test_all as it causes a recursive call into opal_progress */
             while (request_item->subreq_count) {
                 ompi_request_t *subreq = request_item->subreqs[request_item->subreq_count-1];
-                if (true == subreq->req_complete) {
+                if( REQUEST_COMPLETE(subreq) ) {
                     ompi_request_free (&subreq);
                     request_item->subreq_count--;
                 } else {
@@ -204,7 +207,7 @@ static int ompi_comm_request_free (struct ompi_request_t **ompi_req)
 {
     ompi_comm_request_t *request = (ompi_comm_request_t *) *ompi_req;
 
-    if (!(*ompi_req)->req_complete) {
+    if( !REQUEST_COMPLETE(*ompi_req) ) {
         return MPI_ERR_REQUEST;
     }
 

ompi/mca/coll/libnbc/coll_libnbc_component.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -300,7 +300,7 @@ request_free(struct ompi_request_t **ompi_req)
     ompi_coll_libnbc_request_t *request =
         (ompi_coll_libnbc_request_t*) *ompi_req;
 
-    if (true != request->super.req_complete) {
+    if( !REQUEST_COMPLETE(&request->super) ) {
         return MPI_ERR_REQUEST;
     }
 

ompi/mca/io/ompio/io_ompio_request.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2007 The University of Tennessee and The University
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -29,7 +29,7 @@ static int mca_io_ompio_request_free ( struct ompi_request_t **req)
 {
     mca_ompio_request_t *ompio_req = ( mca_ompio_request_t *)*req;
     if ( NULL != ompio_req->req_free_fn ) {
-	ompio_req->req_free_fn (ompio_req );
+        ompio_req->req_free_fn (ompio_req );
     }
     opal_list_remove_item (&mca_io_ompio_pending_requests, &ompio_req->req_item);
 
@@ -65,7 +65,7 @@ void mca_io_ompio_request_destruct(mca_ompio_request_t* req)
     OMPI_REQUEST_FINI ( &(req->req_ompi));
     OBJ_DESTRUCT (&req->req_item);
     if ( NULL != req->req_data ) {
-	free (req->req_data);
+        free (req->req_data);
     }
 
     return;
@@ -79,16 +79,16 @@ int mca_io_ompio_component_progress ( void )
 
     OPAL_LIST_FOREACH(litem, &mca_io_ompio_pending_requests, opal_list_item_t) {
         req = GET_OMPIO_REQ_FROM_ITEM(litem);
-	if ( true == req->req_ompi.req_complete ) {
-	    continue;
-	}
+        if( REQUEST_COMPLETE(&req->req_ompi) ) {
+            continue;
+        }
         if ( NULL != req->req_progress_fn ) {
             if ( req->req_progress_fn(req) ) {
                 completed++;
                 ompi_request_complete (&req->req_ompi, 1);
                 /* The fbtl progress function is expected to set the
-		** status elements
-		*/
+                 * status elements
+                 */
             }
         }
 

ompi/mca/osc/pt2pt/osc_pt2pt_request.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2011-2012 Sandia National Laboratories.  All rights reserved.
  * Copyright (c) 2014      Los Alamos National Security, LLC. All rights
  *                         reserved.
+ * Copyright (c) 2016      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -32,7 +35,7 @@ request_free(struct ompi_request_t **ompi_req)
     ompi_osc_pt2pt_request_t *request =
         (ompi_osc_pt2pt_request_t*) *ompi_req;
 
-    if (true != request->super.req_complete) {
+    if (REQUEST_COMPLETED != request->super.req_complete) {
         return MPI_ERR_REQUEST;
     }
 

ompi/mca/pml/base/pml_base_recvreq.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2010 The University of Tennessee and The University
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -78,7 +78,7 @@ OMPI_DECLSPEC OBJ_CLASS_DECLARATION(mca_pml_base_recv_request_t);
     (request)->req_base.req_sequence = 0;                                \
     (request)->req_base.req_datatype = datatype;                         \
     /* What about req_type ? */                                          \
-    (request)->req_base.req_pml_complete = OPAL_INT_TO_BOOL(persistent); \
+    (request)->req_base.req_pml_complete = false;                        \
     (request)->req_base.req_free_called = false;                         \
 }
 /**
@@ -99,7 +99,7 @@ OMPI_DECLSPEC OBJ_CLASS_DECLARATION(mca_pml_base_recv_request_t);
         (request)->req_ompi.req_status._ucount = 0;                             \
         (request)->req_ompi.req_status._cancelled = 0;                          \
                                                                                 \
-        (request)->req_ompi.req_complete = false;                               \
+        (request)->req_ompi.req_complete = REQUEST_PENDING;                     \
         (request)->req_ompi.req_state = OMPI_REQUEST_ACTIVE;                    \
     } while (0)
 

ompi/mca/pml/base/pml_base_request.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2007 The University of Tennessee and The University
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -62,11 +62,11 @@ struct mca_pml_base_request_t {
 
 /* START: These fields have to match the definition of the mca_pml_cm_request_t */
     ompi_request_t req_ompi;              /**< base request */
-    volatile bool req_pml_complete;       /**< flag indicating if the pt-2-pt layer is done with this request */
+    volatile int32_t req_pml_complete;    /**< flag indicating if the pt-2-pt layer is done with this request */
+    volatile int32_t req_free_called;     /**< flag indicating if the user has freed this request */
     mca_pml_base_request_type_t req_type; /**< MPI request type - used for test */
     struct ompi_communicator_t *req_comm; /**< communicator pointer */
     struct ompi_datatype_t *req_datatype; /**< pointer to data type */
-    volatile bool req_free_called;        /**< flag indicating if the user has freed this request */
     opal_convertor_t req_convertor;       /**< always need the convertor */
 /* END: These field have to match the definition of the mca_pml_cm_request_t */
 

ompi/mca/pml/base/pml_base_sendreq.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2007 The University of Tennessee and The University
+ * Copyright (c) 2004-2016 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -88,7 +88,7 @@ OMPI_DECLSPEC OBJ_CLASS_DECLARATION( mca_pml_base_send_request_t );
       (request)->req_base.req_tag = (int32_t)tag;                         \
       (request)->req_base.req_comm = comm;                                \
       /* (request)->req_base.req_proc is set on request allocation */     \
-      (request)->req_base.req_pml_complete = OPAL_INT_TO_BOOL(persistent); \
+      (request)->req_base.req_pml_complete = false;                       \
       (request)->req_base.req_free_called = false;                        \
       (request)->req_base.req_ompi.req_status._cancelled = 0;             \
       (request)->req_bytes_packed = 0;                                    \
@@ -119,7 +119,7 @@ OMPI_DECLSPEC OBJ_CLASS_DECLARATION( mca_pml_base_send_request_t );
 #define MCA_PML_BASE_SEND_START( request )                    \
     do {                                                      \
         (request)->req_pml_complete = false;                  \
-        (request)->req_ompi.req_complete = false;             \
+        (request)->req_ompi.req_complete = REQUEST_PENDING;   \
         (request)->req_ompi.req_state = OMPI_REQUEST_ACTIVE;  \
         (request)->req_ompi.req_status._cancelled = 0;        \
     } while (0)

commit 0a67867e514fc6268a254e4a61d1938e3d4eebdb

osc/rdma: fix bug introduced in b90c838
This commit fixes an bug that was introduced back in 2016 which
impacts request-based RMA in some cases.

Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

ompi/mca/osc/rdma/osc_rdma_request.c
@@ -33,7 +33,7 @@ static int request_free(struct ompi_request_t **ompi_req)
     ompi_osc_rdma_request_t *request =
         (ompi_osc_rdma_request_t*) *ompi_req;
 
-    if( REQUEST_COMPLETE(&request->super) ) {
+    if (!REQUEST_COMPLETE(&request->super)) {
         return MPI_ERR_REQUEST;
     }
 