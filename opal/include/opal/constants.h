e27e945d9afbfcd43708d5a102168cfb8072dde3
Ralph Castain <rhc@open-mpi.org>
2018-08-20 16:08:54 -0700

    Complete job control integration
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- opal/include/opal/constants.h
+++ opal/include/opal/constants.h
@@ -98,7 +98,8 @@ enum {
     OPAL_ERR_HEARTBEAT_ALERT                = (OPAL_ERR_BASE - 67),
     OPAL_ERR_FILE_ALERT                     = (OPAL_ERR_BASE - 68),
     OPAL_ERR_MODEL_DECLARED                 = (OPAL_ERR_BASE - 69),
-    OPAL_PMIX_LAUNCH_DIRECTIVE              = (OPAL_ERR_BASE - 70)
+    OPAL_PMIX_LAUNCH_DIRECTIVE              = (OPAL_ERR_BASE - 70),
+    OPAL_OPERATION_SUCCEEDED                = (OPAL_ERR_BASE - 71)
 };
 
 #define OPAL_ERR_MAX                (OPAL_ERR_BASE - 100)
