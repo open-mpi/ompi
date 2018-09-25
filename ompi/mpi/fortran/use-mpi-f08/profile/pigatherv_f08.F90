6df6e1a93fd647b944463b20827db90e91ed750f
Gilles Gouaillardet <gilles@rist.or.jp>
2018-09-25 10:09:34 +0900

    fortran/use-mpi-f08: Corrections to PMPI signatures of collectives
    Corrected the signatures of the collectives used by the Fortran 2008
    interface to state correct intent for inout arguments and use the
    ASYNCHRONOUS attribute in non-blocking collective calls.
    
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>
    
    (cherry picked from commit open-mpi/ompi@f750c6932c845b65d059f05aae3a3b6205853aa4)

--- ompi/mpi/fortran/use-mpi-f08/profile/pigatherv_f08.F90
+++ ompi/mpi/fortran/use-mpi-f08/profile/pigatherv_f08.F90
@@ -12,9 +12,10 @@ subroutine PMPI_Igatherv_f08(sendbuf,sendcount,sendtype,recvbuf,recvcounts,&
    use :: mpi_f08_types, only : MPI_Datatype, MPI_Comm, MPI_Request
    use :: mpi_f08, only : ompi_igatherv_f
    implicit none
-   OMPI_FORTRAN_IGNORE_TKR_TYPE, INTENT(IN) :: sendbuf, recvbuf
+   OMPI_FORTRAN_IGNORE_TKR_TYPE, INTENT(IN), ASYNCHRONOUS :: sendbuf
+   OMPI_FORTRAN_IGNORE_TKR_TYPE, ASYNCHRONOUS :: recvbuf
    INTEGER, INTENT(IN) :: sendcount, root
-   INTEGER, INTENT(IN) :: recvcounts(*), displs(*)
+   INTEGER, INTENT(IN), ASYNCHRONOUS :: recvcounts(*), displs(*)
    TYPE(MPI_Datatype), INTENT(IN) :: sendtype
    TYPE(MPI_Datatype), INTENT(IN) :: recvtype
    TYPE(MPI_Comm), INTENT(IN) :: comm
