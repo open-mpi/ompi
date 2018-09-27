c087365ead22b647e77d549021b8256234e7b9e6
Brian Barrett <bbarrett@amazon.com>
2018-09-27 15:26:50 -0700

    opal/util: Always support BSD behavior of asprintf
    Open MPI's developers like to assume that asprintf() always sets
    the ptr to NULL on error, but the standard (and Linux glibc) do
    not guarantee this.  As a result, we're making opal_asprintf()
    always available for developers, which will guarantee that
    ptr is set to NULL on error.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- opal/util/printf.h
+++ opal/util/printf.h
@@ -95,7 +95,12 @@ OPAL_DECLSPEC int  opal_vsnprintf(char *str, size_t size, const char *fmt, va_li
  *
  * Returns the number of characters printed.
  *
- * THIS IS A PORTABILITY FEATURE: USE asprintf() in CODE.
+ * Unlike opal_snprintf and opal_vsnprintf, opal_asprintf() is always
+ * available and guarantees that *ptr is NULL when the underlying
+ * asprintf fails.  The standard does not require *ptr be set to NULL
+ * on error and some implementations (modern Linux) do not guarantee
+ * such behavior.
+ *
  */
 OPAL_DECLSPEC int  opal_asprintf(char **ptr, const char *fmt, ...) __opal_attribute_format__(__printf__, 2, 3);
 
@@ -119,7 +124,12 @@ OPAL_DECLSPEC int  opal_asprintf(char **ptr, const char *fmt, ...) __opal_attrib
  *
  * Returns the number of characters printed.
  *
- * THIS IS A PORTABILITY FEATURE: USE vasprintf() in CODE.
+ * Unlike opal_snprintf and opal_vsnprintf, opal_vasprintf() is always
+ * available and guarantees that *ptr is NULL when the underlying
+ * asprintf fails.  The standard does not require *ptr be set to NULL
+ * on error and some implementations (modern Linux) do not guarantee
+ * such behavior.
+ *
  */
 OPAL_DECLSPEC int  opal_vasprintf(char **ptr, const char *fmt, va_list ap) __opal_attribute_format__(__printf__, 2, 0);
 
