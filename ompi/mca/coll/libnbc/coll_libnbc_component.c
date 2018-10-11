91a4b4c799795d6f5e66e91762e7414fb8a1a9ee
Mikhail Kurnosov <mkurnosov@gmail.com>
2018-10-11 21:43:13 +0700

    coll/libnbc: add recursive doubling algorithm for MPI_Iallgather
    Implements recursive doubling algorithm for MPI_Iallgather.
    The algorithm can be used only for power-of-two number of processes.
    
    Signed-off-by: Mikhail Kurnosov <mkurnosov@gmail.com>
    (cherry picked from commit a7386c1e09fb274991ca5b50d9d418a0d6b77b6c)
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- ompi/mca/coll/libnbc/coll_libnbc_component.c
+++ ompi/mca/coll/libnbc/coll_libnbc_component.c
@@ -46,6 +46,14 @@ static int libnbc_priority = 10;
 static bool libnbc_in_progress = false;     /* protect from recursive calls */
 bool libnbc_ibcast_skip_dt_decision = true;
 
+int libnbc_iallgather_algorithm = 0;             /* iallgather user forced algorithm */
+static mca_base_var_enum_value_t iallgather_algorithms[] = {
+    {0, "ignore"},
+    {1, "linear"},
+    {2, "recursive_doubling"},
+    {0, NULL}
+};
+
 int libnbc_ibcast_algorithm = 0;             /* ibcast user forced algorithm */
 int libnbc_ibcast_knomial_radix = 4;
 static mca_base_var_enum_value_t ibcast_algorithms[] = {
@@ -194,6 +202,16 @@ libnbc_register(void)
                                            OPAL_INFO_LVL_9,
                                            MCA_BASE_VAR_SCOPE_READONLY,
                                            &libnbc_ibcast_skip_dt_decision);
+
+    libnbc_iallgather_algorithm = 0;
+    (void) mca_base_var_enum_create("coll_libnbc_iallgather_algorithms", iallgather_algorithms, &new_enum);
+    mca_base_component_var_register(&mca_coll_libnbc_component.super.collm_version,
+                                    "iallgather_algorithm",
+                                    "Which iallgather algorithm is used: 0 ignore, 1 linear, 2 recursive_doubling",
+                                    MCA_BASE_VAR_TYPE_INT, new_enum, 0, MCA_BASE_VAR_FLAG_SETTABLE,
+                                    OPAL_INFO_LVL_5, MCA_BASE_VAR_SCOPE_ALL,
+                                    &libnbc_iallgather_algorithm);
+    OBJ_RELEASE(new_enum);
 
     libnbc_ibcast_algorithm = 0;
     (void) mca_base_var_enum_create("coll_libnbc_ibcast_algorithms", ibcast_algorithms, &new_enum);
