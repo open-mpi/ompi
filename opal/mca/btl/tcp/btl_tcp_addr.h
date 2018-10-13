457f058e73326fe94ad37ae0a8604a1382b74b1f
Brian Barrett <bbarrett@amazon.com>
2018-10-13 04:14:01 +0000

    btl tcp: Simplify modex address selection
    Simplify selection of the address to publish for a given BTL TCP
    module in the module exchange code.  Rather than looping through
    all IP addresses associated with a node, looking for one that
    matches the kindex of a module, loop over the modules and
    use the address stored in the module structure.  This also
    happens to be the address that the source will use to bind()
    in a connect() call, so this should eliminate any confusion
    (read: bugs) when an interface has multiple IPs associated with
    it.
    
    Refs #5818
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- opal/mca/btl/tcp/btl_tcp_addr.h
+++ opal/mca/btl/tcp/btl_tcp_addr.h
@@ -33,40 +33,52 @@
 
 
 /**
- * Structure used to publish TCP connection information to peers.
+ * Modex address structure.
+ *
+ * One of these structures will be sent for every btl module in use by
+ * the local BTL TCP component.
  */
+struct mca_btl_tcp_modex_addr_t {
+    uint8_t     addr[16];       /* endpoint address.  for addr_family
+                                   of MCA_BTL_TCP_AF_INET, only the
+                                   first 4 bytes have meaning. */
+    uint32_t    addr_ifkindex;  /* endpoint kernel index */
+    uint16_t    addr_port;      /* endpoint listen port */
+    uint8_t     addr_family;    /* endpoint address family.  Note that
+                                   this is
+                                   MCA_BTL_TCP_AF_{INET,INET6}, not
+                                   the traditional
+                                   AF_INET/AF_INET6. */
+    uint8_t     padding[1];     /* padd out to an 8-byte word */
+};
+typedef struct mca_btl_tcp_modex_addr_t mca_btl_tcp_modex_addr_t;
 
+
+/**
+ * Remote peer address structure
+ *
+ * One of these structures will be allocated for every remote endpoint
+ * associated with a remote proc.  The data is pulled from the
+ * mca_btl_tcp_modex_addr_t structure, except for the addr_inuse
+ * field, which is local.
+ */
 struct mca_btl_tcp_addr_t {
-    /* the following information is exchanged between different
-       machines (read: byte order), so use network byte order
-       for everything and don't add padding
-    */
+    union {
+        struct in_addr  addr_inet;      /* IPv6 listen address */
 #if OPAL_ENABLE_IPV6
-    struct in6_addr addr_inet;    /**< IPv4/IPv6 listen address > */
-#else
-    /* Bug, FIXME: needs testing */
-    struct my_in6_addr {
-        union {
-            uint32_t u6_addr32[4];
-            struct _my_in6_addr {
-                struct in_addr _addr_inet;
-                uint32_t _pad[3];
-            } _addr__inet;
-        } _union_inet;
-    } addr_inet;
+        struct in6_addr addr_inet6;     /* IPv6 listen address */
 #endif
-    in_port_t      addr_port;     /**< listen port */
-    uint16_t       addr_ifkindex; /**< remote interface index assigned with
+    };
+    in_port_t   addr_port;     /**< listen port */
+    int         addr_ifkindex; /**< remote interface index assigned with
                                     this address */
-    unsigned short addr_inuse;    /**< local meaning only */
-    uint8_t        addr_family;   /**< AF_INET or AF_INET6 */
+    uint8_t     addr_family;   /**< AF_INET or AF_INET6 */
+    bool        addr_inuse;    /**< local meaning only */
 };
 typedef struct mca_btl_tcp_addr_t mca_btl_tcp_addr_t;
 
 #define MCA_BTL_TCP_AF_INET     0
-#if OPAL_ENABLE_IPV6
-# define MCA_BTL_TCP_AF_INET6   1
-#endif
+#define MCA_BTL_TCP_AF_INET6    1
 
 #endif
 
