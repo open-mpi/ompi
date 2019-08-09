a3752b681bd02ca41917878ac971cdcfb423f33a
guserav <erik.zeiske@web.de>
2019-08-09 09:42:12 -0700

    Revert "Remove opal/mca/common/ofi."
    This reverts commit dd20174532928e0c9cdbe7b206868e6e4bea9d0b.
    
    Signed-off-by: guserav <erik.zeiske@web.de>
    (cherry picked from commit 4ad78aaa156f118c907451b4e72d13dfebcbccf2)
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

---
+++ opal/mca/common/ofi/common_ofi.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015      Intel, Inc.  All rights reserved.
+ * Copyright (c) 2017      Los Alamos National Security, LLC.  All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "opal_config.h"
+#include "opal/constants.h"
+
+#include <errno.h>
+#include <unistd.h>
+
+#include "common_ofi.h"
+
+int mca_common_ofi_register_mca_variables(void)
+{
+    return OPAL_SUCCESS;
+}
