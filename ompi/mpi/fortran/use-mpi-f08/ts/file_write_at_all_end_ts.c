bd9536be8d447d5979ee9b8c2edfedff2a651226
Gilles Gouaillardet <gilles@rist.or.jp>
2019-08-20 16:40:26 +0900

    fortran/use-mpi-f08: add CFI support for MPI-IO operations
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

---
+++ ompi/mpi/fortran/use-mpi-f08/ts/file_write_at_all_end_ts.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2011-2012 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012      Oracle and/or its affiliates.  All rights reserved.
+ * Copyright (c) 2015-2019 Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include "ompi/file/file.h"
+#include "ompi/errhandler/errhandler.h"
+#include "ompi/mpi/fortran/use-mpi-f08/ts/bindings.h"
+#include "ompi/mpi/fortran/mpif-h/status-conversion.h"
+#include "ompi/mpi/fortran/base/constants.h"
+
+void ompi_file_write_at_all_end_ts(MPI_Fint *fh, CFI_cdesc_t *x,
+                                   MPI_Fint *status, MPI_Fint *ierr)
+{
+    int c_ierr;
+    MPI_File c_fh = PMPI_File_f2c(*fh);
+    void *buf = x->base_addr;
+    OMPI_FORTRAN_STATUS_DECLARATION(c_status,c_status2)
+
+    OMPI_FORTRAN_STATUS_SET_POINTER(c_status,c_status2,status)
+
+    c_ierr = PMPI_File_write_at_all_end(c_fh, buf, c_status);
+
+    if (NULL != ierr) *ierr = OMPI_INT_2_FINT(c_ierr);
+
+    OMPI_FORTRAN_STATUS_RETURN(c_status,c_status2,status,c_ierr)
+}
