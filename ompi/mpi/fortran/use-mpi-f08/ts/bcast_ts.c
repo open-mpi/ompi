0edc5cb01fced55d26c6d8981e00334dd973d2da
Gilles Gouaillardet <gilles@rist.or.jp>
2019-09-07 04:32:14 +0900

    checkpoint

--- ompi/mpi/fortran/use-mpi-f08/ts/bcast_ts.c
+++ ompi/mpi/fortran/use-mpi-f08/ts/bcast_ts.c
@@ -35,13 +35,17 @@ void ompi_bcast_ts(CFI_cdesc_t* x, MPI_Fint *count, MPI_Fint *datatype,
     MPI_Comm c_comm = PMPI_Comm_f2c(*comm);
     MPI_Datatype c_datatype = NULL, c_type = NULL;
     int c_root = OMPI_FINT_2_INT(*root);
-    void *buffer = x->base_addr;
+    void *buffer = NULL;
     int c_count = 0;
 
     if (OMPI_COMM_IS_INTRA(c_comm) || MPI_PROC_NULL != c_root) {
         c_type = PMPI_Type_f2c(*datatype);
         c_count = OMPI_FINT_2_INT(*count);
-        OMPI_CFI_2_C(x, c_count, c_type, c_datatype, c_ierr);
+        if ((OMPI_COMM_IS_INTER(c_comm) && MPI_PROC_NULL == c_root) || (ompi_comm_rank(c_comm) == c_root)) {
+            OMPI_CFI_2_C_COPY(x, buffer, c_count, c_type, c_datatype, c_ierr);
+        } else {
+            OMPI_CFI_2_C_ALLOC(x, buffer, c_count, c_type, c_datatype, c_ierr);
+        }
         if (MPI_SUCCESS != c_ierr) {
             if (NULL != ierr) *ierr = OMPI_INT_2_FINT(c_ierr);
             OMPI_ERRHANDLER_INVOKE(c_comm, c_ierr, FUNC_NAME)
@@ -54,8 +58,12 @@ void ompi_bcast_ts(CFI_cdesc_t* x, MPI_Fint *count, MPI_Fint *datatype,
                        c_datatype,
                        c_root,
                        c_comm);
-    if (c_datatype != c_type) {
-        ompi_datatype_destroy(&c_datatype);
-    }
     if (NULL != ierr) *ierr = OMPI_INT_2_FINT(c_ierr);
+    if (OMPI_COMM_IS_INTRA(c_comm) || MPI_PROC_NULL != c_root) {
+        if ((OMPI_COMM_IS_INTER(c_comm) && MPI_ROOT == c_root) || (ompi_comm_rank(c_comm) == c_root)) {
+            OMPI_C_2_CFI_FREE(x, buffer, c_count, c_type, c_datatype, c_ierr);
+        } else {
+            OMPI_C_2_CFI_COPY(x, buffer, c_count, c_type, c_datatype, c_ierr);
+        }
+    }
 }
