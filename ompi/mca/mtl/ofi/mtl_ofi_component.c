3aca4af548a3d781b6b52f89f4d6c7e66d379609
Michael Heinz <michael.william.heinz@intel.com>
2019-09-23 13:09:09 -0400

    REF6976 Silent failure of OMPI over OFI with large messages sizes
    INTERNAL: STL-59403
    
    The OFI (libfabric) MTL does not respect the maximum message size
    parameter that OFI provides in the fi_info data.
    
    This patch adds this missing max_msg_size field to the mca_ofi_module_t
    structure and adds a length check to the low-level send routines.
    
    Change-Id: I05aa71d332f2df897133b30c28bf37d98f061996
    Signed-off-by: Michael Heinz <michael.william.heinz@intel.com>
    Reviewed-by: Adam Goldman <adam.goldman@intel.com>
    Reviewed-by: Brendan Cunningham <brendan.cunningham@intel.com>

--- ompi/mca/mtl/ofi/mtl_ofi_component.c
+++ ompi/mca/mtl/ofi/mtl_ofi_component.c
@@ -881,9 +881,10 @@ ompi_mtl_ofi_component_init(bool enable_progress_threads,
     }
 
     /**
-     * Save the maximum inject size.
+     * Save the maximum sizes.
      */
     ompi_mtl_ofi.max_inject_size = prov->tx_attr->inject_size;
+    ompi_mtl_ofi.max_msg_size = prov->ep_attr->max_msg_size;
 
     /**
      * The user is not allowed to exceed MTL_OFI_MAX_PROG_EVENT_COUNT.
