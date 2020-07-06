229724799e51029d479b792a47be499cb2ff8ac5
tomhers <tom.herschberg@gmail.com>
2020-07-06 16:22:49 -0400

    BTL/OFI: Fix missing include file.
    The missing include file causes an error when using an external version of LibEvent.
    
    Signed-off-by: tomhers <tom.herschberg@gmail.com>
    (cherry picked from commit 88f9d2c90f1730f2e0b5bc4951893f60ff5a1332)

--- opal/mca/btl/ofi/btl_ofi_component.c
+++ opal/mca/btl/ofi/btl_ofi_component.c
@@ -27,6 +27,9 @@
 
 #include "opal_config.h"
 
+#include "opal/util/printf.h"
+#include "opal/util/argv.h"
+
 #include "opal/mca/btl/btl.h"
 #include "opal/mca/btl/base/base.h"
 #include "opal/mca/hwloc/base/base.h"
