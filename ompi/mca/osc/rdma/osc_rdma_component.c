776a603ec48506cd0a3ff04b4f408c20a58eaee3
Howard Pritchard <howardp@lanl.gov>
2021-02-12 09:56:03 -0700

    OSC/RDMA: fix typo in btl selection logic
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>
    (cherry picked from commit 1d4f6bf4ac902eadade9864517a17c95437308cc)

--- ompi/mca/osc/rdma/osc_rdma_component.c
+++ ompi/mca/osc/rdma/osc_rdma_component.c
@@ -21,6 +21,8 @@
  * Copyright (c) 2019      Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
  * Copyright (c) 2020-2021 Google, LLC. All rights reserved.
+ * Copyright (c) 2019-2021 Triad National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -937,7 +939,7 @@ static int ompi_osc_rdma_query_btls (ompi_communicator_t *comm, struct mca_btl_b
         }
 
         /* any non-local rank must have a usable btl */
-        if (!found_btl && comm_rank == rank) {
+        if (!found_btl && comm_rank != rank) {
             /* no btl = no rdma/atomics */
             rc = OMPI_ERR_UNREACH;
             break;
