b31e43a58c570bcb81cf548f4eb8412e1ad143ac
Nathan Hjelm <hjelmn@google.com>
2021-03-03 13:14:27 -0700

    Add a .clang_format file for Open MPI
    This file works with clang-format --style=file to reformat code to match the
    style used in Open MPI. This type includes:
    
     - No tabs. They are not recommended for Open MPI and can often screw up the
       formatting.
    
     - Tab depth: 4. This is what is used throughout the Open MPI code base.
    
     - Max column width: 100. There currently is no standard for Open MPI but we
       should aim to use something reasonable.
    
     - Braces following function definitions occur un-indented on the following
       line.
    
     - Braces following other control statements occur on the same line as the
       control statement.
    
     - Spaces before open parentheses for control statements. No spaces otherwise.
       This is common accross the code base but not consistent.
    
     - Sort headers. Put _config headers before any other header. Followed by
       internal ("" headers), then system headers (<> headers).
    
     - Align consecutive macro definitions.
    
    Signed-off-by: Nathan Hjelm <hjelmn@google.com>
    (cherry picked from commit c352fba5090ed82cf699779cead5f8532c2f7cc3)

---
+++ contrib/clang-format-ompi.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Running clang-format on code base..."
+
+files=($(git ls-tree -r master --name-only | grep -v '3rd-party/' | grep -v 'contrib' | grep -e '.*\.[ch]$'))
+
+for file in "${files[@]}" ; do
+    if test "$1" = "-d" ; then
+	echo "Would have formatted: ${file}"
+    else
+	clang-format --style=file --verbose -i "${file}"
+    fi
+done
+
+echo "Done"
