open-mpi/ompi

commit 38e53f331c22e0059552101f5b1513eccd5a56cc

No longer require a C++ compiler to build Open MPI
Fix several places where old code assumptions effectively required a
C++ compiler to build Open MPI.  Most of these were a holdover from
early days when we (deliberately) assumed that there was always a C++
compiler available.  In most cases this is true, but at least in some
CI cases that have come up recently, some Linux distros do not install
a C++ compiler by default.  The list of changes in this commit include
the following:

- AC_PROG_CXX will set $CXX to "g++" even if no C++ compiler was found
  (!!).  Change that to set $CXX to "no" if no C++ compiler was found.
- Change the C++ configury to only test for C/C++ linker compatibility
  when there's actually a C++ compiler.
- Remove duplicate C++ configury checks (in OPAL and OMPI)
- Remove C++ "bool" alignment and sizeof checks in configury; the
  results of these tests aren't used in the code anywhere.
- Remove assumptions in opal/mca/threads/pthreads/configure.m4 that we
  always have a C++ compiler (and stop using the undocumented
  AC_PROVIDE_IFELSE macro; this Autoconf list post sums it up well:
  https://lists.gnu.org/archive/html/autoconf/2012-11/msg00038.html).
- Do not install any C++ wrapper sym links or data files if there is
  no C++ compiler

Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

opal/mca/threads/pthreads/configure.m4
@@ -348,18 +348,19 @@ AC_DEFUN([OPAL_INTL_POSIX_THREADS_PLAIN], [
 # why take chances?
 #
 
-# Only run C++ and Fortran if those compilers already configured
-AC_PROVIDE_IFELSE([AC_PROG_CC],
-                  [OPAL_INTL_POSIX_THREADS_PLAIN_C],
-                  [opal_pthread_c_success=1])
+# Only run C++ and Fortran if those compilers were found
 
-AC_PROVIDE_IFELSE([AC_PROG_CXX],
-                  [OPAL_INTL_POSIX_THREADS_PLAIN_CXX],
-                  [opal_pthread_cxx_success=1])
+OPAL_INTL_POSIX_THREADS_PLAIN_C
 
-AC_PROVIDE_IFELSE([AC_PROG_FC],
-                  [OPAL_INTL_POSIX_THREADS_PLAIN_FC],
-                  [opal_pthread_fortran_success=1])
+AS_IF([test "$CXX" != "no"],
+      [OPAL_INTL_POSIX_THREADS_PLAIN_CXX
+       opal_pthread_cxx_success=1],
+      [opal_pthread_cxx_success=0])
+
+AS_IF([test "$FC" != "no"],
+      [OPAL_INTL_POSIX_THREADS_PLAIN_FC
+       opal_pthread_fortran_success=1],
+      [opal_pthread_fortran_success=0])
 
 # End: OPAL_INTL_POSIX_THREADS_PLAIN
 ])dnl
@@ -477,18 +478,19 @@ case "${host_cpu}-${host_os}" in
   ;;
 esac
 
-# Only run C++ and Fortran if those compilers already configured
-AC_PROVIDE_IFELSE([AC_PROG_CC],
-                  [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_C],
-                  [opal_pthread_c_success=1])
+# Only run C++ and Fortran if those compilers were found
 
-AC_PROVIDE_IFELSE([AC_PROG_CXX],
-                  [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_CXX],
-                  [opal_pthread_cxx_success=1])
+OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_C
 
-AC_PROVIDE_IFELSE([AC_PROG_FC],
-                  [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_FC],
-                  [opal_pthread_fortran_success=1])
+AS_IF([test "$CXX" != "no"],
+      [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_CXX
+       opal_pthread_cxx_success=1],
+      [opal_pthread_cxx_success=0])
+
+AS_IF([test "$FC" != "no"],
+      [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_FC
+       opal_pthread_fortran_success=1],
+      [opal_pthread_fortran_success=0])
 
 OPAL_VAR_SCOPE_POP
 # End: OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS
@@ -668,18 +670,19 @@ AC_DEFUN([OPAL_INTL_POSIX_THREADS_LIBS],[
 OPAL_VAR_SCOPE_PUSH([plibs])
 plibs="-lpthreads -llthread -lpthread"
 
-# Only run C++ and Fortran if those compilers already configured
-AC_PROVIDE_IFELSE([AC_PROG_CC],
-                  [OPAL_INTL_POSIX_THREADS_LIBS_C],
-                  [opal_pthread_c_success=1])
+# Only run C++ and Fortran if those compilers were found
 
-AC_PROVIDE_IFELSE([AC_PROG_CXX],
-                  [OPAL_INTL_POSIX_THREADS_LIBS_CXX],
-                  [opal_pthread_cxx_success=1])
+OPAL_INTL_POSIX_THREADS_LIBS_C
 
-AC_PROVIDE_IFELSE([AC_PROG_FC],
-                  [OPAL_INTL_POSIX_THREADS_LIBS_FC],
-                  [opal_pthread_fortran_success=1])
+AS_IF([test "$CXX" != "no"],
+      [OPAL_INTL_POSIX_THREADS_LIBS_CXX
+       opal_pthread_cxx_success=1],
+      [opal_pthread_cxx_success=0])
+
+AS_IF([test "$FC" != "no"],
+      [OPAL_INTL_POSIX_THREADS_LIBS_FC
+       opal_pthread_fortran_success=1],
+      [opal_pthread_fortran_success=0])
 
 OPAL_VAR_SCOPE_POP
 # End: OPAL_INTL_POSIX_THREADS_LIBS]
@@ -772,6 +775,8 @@ THREAD_LDFLAGS="$TPKG_LDFLAGS"
 THREAD_LIBS="$TPKG_LIBS"
 
 OPAL_CHECK_PTHREAD_PIDS
+
+AS_IF([test "$CXX" = "no"], [opal_pthread_cxx_success=1])
 
 if test "$OMPI_TRY_FORTRAN_BINDINGS" = "$OMPI_FORTRAN_NO_BINDINGS" || \
    test $ompi_fortran_happy -ne 1; then

config/ompi_setup_cxx.m4
@@ -29,11 +29,33 @@ AC_DEFUN([OMPI_SETUP_CXX_BANNER],[
 ])
 
 AC_DEFUN([OMPI_PROG_CXX],[
-    OPAL_VAR_SCOPE_PUSH([ompi_cxxflags_save])
+    OPAL_VAR_SCOPE_PUSH([ompi_cxxflags_save ompi_cxx_argv0])
+
     ompi_cxxflags_save="$CXXFLAGS"
     AC_PROG_CXX
     AC_PROG_CXXCPP
     CXXFLAGS="$ompi_cxxflags_save"
+
+    # Note: according to the Autoconf docs, if no C++ compiler is
+    # found, $CXX is still set to "g++" (!!).  So make sure that we
+    # actually found a C++ compiler; if not, set CXX to "no", per
+    # thread at
+    # https://www.open-mpi.org/community/lists/users/2013/02/21356.php,
+    # which advises us to set Libtool precious variables to "no" if we
+    # don't want Libtool to setup that language at all.
+    AS_IF([test "x$CXX" = "x"], [CXX=no])
+    set dummy $CXX
+    ompi_cxx_argv0=[$]2
+    OPAL_WHICH([$ompi_cxx_argv0], [OMPI_CXX_ABSOLUTE])
+    AS_IF([test "x$OMPI_CXX_ABSOLUTE" = "x"],
+          [CXX=no
+           OMPI_CXX_ABSOLUTE=no])
+
+    AC_DEFINE_UNQUOTED(OMPI_CXX, "$CXX", [OMPI underlying C++ compiler])
+    AC_SUBST(OMPI_CXX_ABSOLUTE)
+
+    AM_CONDITIONAL([OMPI_HAVE_CXX_COMPILER], [test "$CXX" != "no"])
+
     OPAL_VAR_SCOPE_POP
 ])
 
@@ -44,8 +66,6 @@ dnl wrapper compiler (there is no C++ code in Open MPI, so we do not
 dnl need to setup for internal C++ compilations).  Safe to AC_REQUIRE
 dnl this macro.
 AC_DEFUN([OMPI_SETUP_CXX],[
-    OPAL_VAR_SCOPE_PUSH([ompi_cxx_argv0])
-
     # Do a little tomfoolery to get the subsection title printed first
     AC_REQUIRE([OMPI_SETUP_CXX_BANNER])
 
@@ -54,20 +74,11 @@ AC_DEFUN([OMPI_SETUP_CXX],[
     # see the docs for AC PROG_CC for details.
     AC_REQUIRE([OMPI_PROG_CXX])
 
-    BASECXX="`basename $CXX`"
-
-    AS_IF([test "x$CXX" = "x"], [CXX=none])
-    set dummy $CXX
-    ompi_cxx_argv0=[$]2
-    OPAL_WHICH([$ompi_cxx_argv0], [OMPI_CXX_ABSOLUTE])
-    AS_IF([test "x$OMPI_CXX_ABSOLUTE" = "x"], [OMPI_CXX_ABSOLUTE=none])
-
-    AC_DEFINE_UNQUOTED(OMPI_CXX, "$CXX", [OMPI underlying C++ compiler])
-    AC_SUBST(OMPI_CXX_ABSOLUTE)
-
-    # Make sure we can link with the C compiler
-    OPAL_LANG_LINK_WITH_C([C++], [],
-        [cat <<EOF >&2
+    # If we have a C++ compiler, do some additional tests
+    AS_IF([test "$CXX" != "no"],
+        [ # Make sure we can link with the C compiler
+         OPAL_LANG_LINK_WITH_C([C++], [],
+            [cat <<EOF >&2
 **********************************************************************
 * It appears that your C++ compiler is unable to link against object
 * files created by your C compiler.  This generally indicates either
@@ -78,13 +89,6 @@ AC_DEFUN([OMPI_SETUP_CXX],[
 * available in the config.log file in this directory.
 **********************************************************************
 EOF
-         AC_MSG_ERROR([C and C++ compilers are not link compatible.  Can not continue.])])
-
-    # bool type size and alignment
-    AC_LANG_PUSH(C++)
-    AC_CHECK_SIZEOF(bool)
-    OPAL_C_GET_ALIGNMENT(bool, OPAL_ALIGNMENT_CXX_BOOL)
-    AC_LANG_POP(C++)
-
-    OPAL_VAR_SCOPE_POP
+             AC_MSG_ERROR([C and C++ compilers are not link compatible.  Can not continue.])])
+        ])
 ])

config/opal_case_sensitive_fs_setup.m4
@@ -63,6 +63,8 @@ AC_ARG_WITH([cs_fs],
             [AS_HELP_STRING([--with-cs-fs],
                            [Destination FS is case sensitive (default: set to value of the build FS's case sensitivity)])])
 
+dnl Stupid emacs syntax hilighting: '
+
 if test "$with_cs_fs" = "yes"; then
     OPAL_WANT_CS_FS=1
 elif test -z "$with_cs_fs"; then
@@ -78,6 +80,12 @@ else
 fi
 
 AM_CONDITIONAL(CASE_SENSITIVE_FS, test "$OPAL_WANT_CS_FS" = "1")
+# There is a case in the ompi/tools/wrappers/Makefile.am where we need
+# to know if there is a case sensitive filesystem *and* if we have a
+# C++ compiler.  Since we can't use operators like "&&" or "and" to
+# join together AM CONDITIONALs in a Makefile.am, effectively make a
+# combo CONDITIONAL here.
+AM_CONDITIONAL([CASE_SENSITIVE_FS_AND_HAVE_CXX_COMPILER], [test "$OPAL_WANT_CS_FS" = "1" && test "$CXX" != "no"])
 
 if test "$OPAL_WANT_CS_FS" = "0"; then
 	cat <<EOF

config/opal_config_asm.m4
@@ -1087,7 +1087,6 @@ dnl
 dnl #################################################################
 AC_DEFUN([OPAL_CONFIG_ASM],[
     AC_REQUIRE([OPAL_SETUP_CC])
-    AC_REQUIRE([OPAL_SETUP_CXX])
     AC_REQUIRE([AM_PROG_AS])
 
     AC_ARG_ENABLE([c11-atomics],[AS_HELP_STRING([--enable-c11-atomics],

config/opal_setup_cxx.m4
@@ -1,218 +1,0 @@
-dnl -*- shell-script -*-
-dnl
-dnl Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
-dnl                         University Research and Technology
-dnl                         Corporation.  All rights reserved.
-dnl Copyright (c) 2004-2006 The University of Tennessee and The University
-dnl                         of Tennessee Research Foundation.  All rights
-dnl                         reserved.
-dnl Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
-dnl                         University of Stuttgart.  All rights reserved.
-dnl Copyright (c) 2004-2006 The Regents of the University of California.
-dnl                         All rights reserved.
-dnl Copyright (c) 2006      Los Alamos National Security, LLC.  All rights
-dnl                         reserved.
-dnl Copyright (c) 2007-2009 Sun Microsystems, Inc.  All rights reserved.
-dnl Copyright (c) 2008-2013 Cisco Systems, Inc.  All rights reserved.
-dnl Copyright (c) 2015-2016 Research Organization for Information Science
-dnl                         and Technology (RIST). All rights reserved.
-dnl Copyright (c) 2020      Triad National Security, LLC. All rights
-dnl                         reserved.
-dnl Copyright (c) 2021      IBM Corporation.  All rights reserved.
-dnl
-dnl $COPYRIGHT$
-dnl
-dnl Additional copyrights may follow
-dnl
-dnl $HEADER$
-dnl
-
-# This macro is necessary to get the title to be displayed first.  :-)
-AC_DEFUN([OPAL_SETUP_CXX_BANNER],[
-    opal_show_subtitle "C++ compiler and preprocessor"
-])
-
-# This macro is necessary because PROG_CXX* is REQUIREd by multiple
-# places in SETUP_CXX.
-AC_DEFUN([OPAL_PROG_CXX],[
-    OPAL_VAR_SCOPE_PUSH([opal_cxxflags_save])
-    opal_cxxflags_save="$CXXFLAGS"
-    AC_PROG_CXX
-    AC_PROG_CXXCPP
-    CXXFLAGS="$opal_cxxflags_save"
-    OPAL_VAR_SCOPE_POP
-])
-
-# OPAL_SETUP_CXX()
-# ----------------
-# Do everything required to setup the C++ compiler.  Safe to AC_REQUIRE
-# this macro.
-AC_DEFUN([OPAL_SETUP_CXX],[
-    AC_REQUIRE([OPAL_SETUP_CXX_BANNER])
-
-    _OPAL_SETUP_CXX_COMPILER
-
-    OPAL_CXX_COMPILER_VENDOR([opal_cxx_vendor])
-
-    _OPAL_SETUP_CXX_COMPILER_BACKEND
-])
-
-# _OPAL_SETUP_CXX_COMPILER()
-# --------------------------
-# Setup the CXX compiler
-AC_DEFUN([_OPAL_SETUP_CXX_COMPILER],[
-    OPAL_VAR_SCOPE_PUSH(opal_cxx_compiler_works)
-
-    # Must REQUIRE the PROG_CXX macro and not call it directly here for
-    # reasons well-described in the AC2.64 (and beyond) docs.
-    AC_REQUIRE([OPAL_PROG_CXX])
-    BASECXX="`basename $CXX`"
-
-    AS_IF([test "x$CXX" = "x"], [CXX=none])
-    set dummy $CXX
-    opal_cxx_argv0=[$]2
-    OPAL_WHICH([$opal_cxx_argv0], [OPAL_CXX_ABSOLUTE])
-    AS_IF([test "x$OPAL_CXX_ABSOLUTE" = "x"], [OPAL_CXX_ABSOLUTE=none])
-
-    AC_DEFINE_UNQUOTED(OPAL_CXX, "$CXX", [OPAL underlying C++ compiler])
-    AC_SUBST(OPAL_CXX_ABSOLUTE)
-
-    OPAL_VAR_SCOPE_POP
-])
-
-# OPAL_CHECK_CXX_IQUOTE()
-# ----------------------
-# Check if the compiler supports the -iquote option. This options
-# removes the specified directory from the search path when using
-# #include <>. This check works around an issue caused by C++20
-# which added a <version> header. This conflicts with the
-# VERSION file at the base of our source directory on case-
-# insensitive filesystems.
-AC_DEFUN([OPAL_CHECK_CXX_IQUOTE],[
-    OPAL_VAR_SCOPE_PUSH([opal_check_cxx_iquote_CXXFLAGS_save])
-    opal_check_cxx_iquote_CXXFLAGS_save=${CXXFLAGS}
-    CXXFLAGS="${CXXFLAGS} -iquote ."
-    AC_MSG_CHECKING([for $CXX option to add a directory only to the search path for the quote form of include])
-    AC_LANG_PUSH(C++)
-    AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[]],[])],
-		      [opal_cxx_iquote="-iquote"],
-		      [opal_cxx_iquote="-I"])
-    CXXFLAGS=${opal_check_cxx_iquote_CXXFLAGS_save}
-    AC_LANG_POP(C++)
-    OPAL_VAR_SCOPE_POP
-    AC_MSG_RESULT([$opal_cxx_iquote])
-])
-
-# _OPAL_SETUP_CXX_COMPILER_BACKEND()
-# ----------------------------------
-# Back end of _OPAL_SETUP_CXX_COMPILER_BACKEND()
-AC_DEFUN([_OPAL_SETUP_CXX_COMPILER_BACKEND],[
-    AC_LANG_PUSH(C++)
-    OPAL_CHECK_CXX_IQUOTE
-
-    # Do we want code coverage
-    if test "$WANT_COVERAGE" = "1"; then
-        # For compilers > gcc-4.x, use --coverage for
-        # compiling and linking to circumvent trouble with
-        # libgcov.
-        OPAL_COVERAGE_FLAGS=
-
-        _OPAL_CHECK_SPECIFIC_CXXFLAGS(--coverage, coverage)
-        if test "$opal_cv_cxx_coverage" = "1" ; then
-            OPAL_COVERAGE_FLAGS="--coverage"
-            CLEANFILES="*.gcno ${CLEANFILES}"
-            CONFIG_CLEAN_FILES="*.gcda *.gcov ${CONFIG_CLEAN_FILES}"
-            AC_MSG_WARN([$OPAL_COVERAGE_FLAGS has been added to CXXFLAGS (--enable-coverage)])
-        else
-            _OPAL_CHECK_SPECIFIC_CXXFLAGS(-ftest-coverage, ftest_coverage)
-            _OPAL_CHECK_SPECIFIC_CXXFLAGS(-fprofile-arcs, fprofile_arcs)
-            if test "$opal_cv_cxx_ftest_coverage" = "0" || test "opal_cv_cxx_fprofile_arcs" = "0" ; then
-                AC_MSG_WARN([Code coverage functionality is not currently available with $CXX])
-                AC_MSG_ERROR([Configure: Cannot continue])
-            fi
-            CLEANFILES="*.bb *.bbg ${CLEANFILES}"
-            OPAL_COVERAGE_FLAGS="-ftest-coverage -fprofile-arcs"
-        fi
-        OPAL_FLAGS_UNIQ(CXXFLAGS)
-        WANT_DEBUG=1
-   fi
-
-    # Do we want debugging?
-    if test "$WANT_DEBUG" = "1" && test "$enable_debug_symbols" != "no" ; then
-        CXXFLAGS="$CXXFLAGS -g"
-        AC_MSG_WARN([-g has been added to CXXFLAGS (--enable-debug)])
-    fi
-
-    if test "$WANT_DEBUG"  = "0" ; then
-        OPAL_ENSURE_CONTAINS_OPTFLAGS(["$CXXFLAGS"])
-    fi
-
-    # These flags are generally g++-specific; even the g++-impersonating
-    # compilers won't accept them.
-    OPAL_CXXFLAGS_BEFORE_PICKY="$CXXFLAGS"
-    if test "$WANT_PICKY_COMPILER" = 1; then
-        _OPAL_CHECK_SPECIFIC_CXXFLAGS(-Wundef, Wundef)
-        _OPAL_CHECK_SPECIFIC_CXXFLAGS(-Wno-long-long, Wno_long_long, int main() { long long x; } )
-        _OPAL_CHECK_SPECIFIC_CXXFLAGS(-Wno-long-double, Wno_long_double, int main () { long double x; })
-        _OPAL_CHECK_SPECIFIC_CXXFLAGS(-fstrict-prototype, fstrict_prototype)
-        _OPAL_CHECK_SPECIFIC_CXXFLAGS(-Wall, Wall)
-    fi
-
-    _OPAL_CHECK_SPECIFIC_CXXFLAGS(-finline-functions, finline_functions)
-
-    # Make sure we can link with the C compiler
-    if test "$opal_cv_cxx_compiler_vendor" != "microsoft"; then
-      OPAL_LANG_LINK_WITH_C([C++], [],
-        [cat <<EOF >&2
-**********************************************************************
-* It appears that your C++ compiler is unable to link against object
-* files created by your C compiler.  This generally indicates either
-* a conflict between the options specified in CFLAGS and CXXFLAGS
-* or a problem with the local compiler installation.  More
-* information (including exactly what command was given to the
-* compilers and what error resulted when the commands were executed) is
-* available in the config.log file in this directory.
-**********************************************************************
-EOF
-         AC_MSG_ERROR([C and C++ compilers are not link compatible.  Can not continue.])])
-    fi
-
-    # If we are on HP-UX, ensure that we're using aCC
-    case "$host" in
-    *hpux*)
-        if test "$BASECXX" = "CC"; then
-            AC_MSG_WARN([*** You will probably have problems compiling the MPI 2])
-            AC_MSG_WARN([*** C++ bindings with the HP-UX CC compiler.  You should])
-            AC_MSG_WARN([*** probably be using the aCC compiler.  Re-run configure])
-            AC_MSG_WARN([*** with the environment variable "CXX=aCC".])
-        fi
-        ;;
-    esac
-
-    # Note: gcc-imperonating compilers accept -O3
-    if test "$WANT_DEBUG" = "1"; then
-        OPTFLAGS=
-    else
-        if test "$GXX" = yes; then
-            OPTFLAGS="-O3"
-        else
-            OPTFLAGS="-O"
-        fi
-    fi
-
-
-    # bool type size and alignment
-    AC_CHECK_SIZEOF(bool)
-    OPAL_C_GET_ALIGNMENT(bool, OPAL_ALIGNMENT_CXX_BOOL)
-
-    OPAL_ENSURE_CONTAINS_OPTFLAGS("$OPAL_CXXFLAGS_BEFORE_PICKY")
-    OPAL_CXXFLAGS_BEFORE_PICKY="$co_result"
-
-    AC_MSG_CHECKING([for CXX optimization flags])
-    OPAL_ENSURE_CONTAINS_OPTFLAGS(["$CXXFLAGS"])
-    AC_MSG_RESULT([$co_result])
-    CXXFLAGS="$co_result"
-    OPAL_FLAGS_UNIQ([CXXFLAGS])
-    AC_MSG_RESULT(CXXFLAGS result: $CXXFLAGS)
-    AC_LANG_POP(C++)
-])

ompi/tools/wrappers/Makefile.am
@@ -24,7 +24,11 @@
 
 include $(top_srcdir)/Makefile.ompi-rules
 
-generated_man_pages = mpicc.1 mpic++.1 mpicxx.1 mpifort.1 mpif77.1 mpif90.1
+generated_man_pages = mpicc.1 mpifort.1 mpif77.1 mpif90.1
+
+if OMPI_HAVE_CXX_COMPILER
+generated_man_pages += mpic++.1 mpicxx.1
+endif
 
 if OMPI_WANT_JAVA_BINDINGS
 generated_man_pages += mpijavac.1
@@ -45,8 +49,10 @@ CLEANFILES += $(bin_SCRIPTS)
 install-exec-hook-always:
 	test -z "$(bindir)" || $(MKDIR_P) "$(DESTDIR)$(bindir)"
 	(cd $(DESTDIR)$(bindir); rm -f mpicc; $(LN_S) ompi_wrapper_script mpicc)
+if OMPI_HAVE_CXX_COMPILER
 	(cd $(DESTDIR)$(bindir); rm -f mpic++; $(LN_S) ompi_wrapper_script mpic++)
 	(cd $(DESTDIR)$(bindir); rm -f mpicxx; $(LN_S) ompi_wrapper_script mpicxx)
+endif
 	(cd $(DESTDIR)$(bindir); rm -f mpifort; $(LN_S) ompi_wrapper_script mpifort)
 	(cd $(DESTDIR)$(bindir); rm -f mpif77; $(LN_S) ompi_wrapper_script mpif77)
 	(cd $(DESTDIR)$(bindir); rm -f mpif90; $(LN_S) ompi_wrapper_script mpif90)
@@ -64,7 +70,7 @@ uninstall-local-always:
 	$(DESTDIR)$(bindir)/mpif90 \
 	$(DESTDIR)$(bindir)/mpijavac
 
-if CASE_SENSITIVE_FS
+if CASE_SENSITIVE_FS_AND_HAVE_CXX_COMPILER
 install-exec-hook: install-exec-hook-always
 	(cd $(DESTDIR)$(bindir); rm -f mpiCC; $(LN_S) ompi_wrapper_script mpiCC)
 uninstall-local: uninstall-local-always
@@ -79,7 +85,11 @@ else # OPAL_WANT_SCRIPT_WRAPPER_COMPILERS
 if OPAL_INSTALL_BINARIES
 
 pkgconfigdir = $(libdir)/pkgconfig
-pkgconfig_DATA = ompi.pc ompi-c.pc ompi-cxx.pc ompi-fort.pc
+pkgconfig_DATA = ompi.pc ompi-c.pc ompi-fort.pc
+
+if OMPI_HAVE_CXX_COMPILER
+pkgconfig_DATA += ompi-cxx.pc
+endif
 
 if OMPI_WANT_JAVA_BINDINGS
 bin_SCRIPTS = mpijavac.pl
@@ -89,14 +99,19 @@ man_MANS = $(man_pages)
 
 nodist_ompidata_DATA = \
         mpicc-wrapper-data.txt \
-	mpic++-wrapper-data.txt \
 	mpifort-wrapper-data.txt
+
+if OMPI_HAVE_CXX_COMPILER
+nodist_ompidata_DATA += mpic++-wrapper-data.txt
+endif
 
 install-exec-hook-always:
 	test -z "$(bindir)" || $(MKDIR_P) "$(DESTDIR)$(bindir)"
 	(cd $(DESTDIR)$(bindir); rm -f mpicc$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpicc$(EXEEXT))
+if OMPI_HAVE_CXX_COMPILER
 	(cd $(DESTDIR)$(bindir); rm -f mpic++$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpic++$(EXEEXT))
 	(cd $(DESTDIR)$(bindir); rm -f mpicxx$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpicxx$(EXEEXT))
+endif
 	(cd $(DESTDIR)$(bindir); rm -f mpifort$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpifort$(EXEEXT))
 	(cd $(DESTDIR)$(bindir); rm -f mpif77$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpif77$(EXEEXT))
 	(cd $(DESTDIR)$(bindir); rm -f mpif90$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpif90$(EXEEXT))
@@ -106,7 +121,9 @@ if OMPI_WANT_JAVA_BINDINGS
 endif
 
 install-data-hook-always:
+if OMPI_HAVE_CXX_COMPILER
 	(cd $(DESTDIR)$(pkgdatadir); rm -f mpicxx-wrapper-data.txt; $(LN_S) mpic++-wrapper-data.txt mpicxx-wrapper-data.txt)
+endif
 	(cd $(DESTDIR)$(pkgdatadir); rm -f mpif77-wrapper-data.txt; $(LN_S) mpifort-wrapper-data.txt mpif77-wrapper-data.txt)
 	(cd $(DESTDIR)$(pkgdatadir); rm -f mpif90-wrapper-data.txt; $(LN_S) mpifort-wrapper-data.txt mpif90-wrapper-data.txt)
 	(cd $(DESTDIR)$(pkgconfigdir); rm -f ompi-f77.pc; $(LN_S) ompi-fort.pc ompi-f77.pc)
@@ -128,7 +145,7 @@ uninstall-local-always:
 	$(DESTDIR)$(bindir)/mpijavac \
 	$(DESTDIR)$(bindir)/mpijavac.pl
 
-if CASE_SENSITIVE_FS
+if CASE_SENSITIVE_FS_AND_HAVE_CXX_COMPILER
 install-exec-hook: install-exec-hook-always
 	(cd $(DESTDIR)$(bindir); rm -f mpiCC$(EXEEXT); $(LN_S) opal_wrapper$(EXEEXT) mpiCC$(EXEEXT))
 
@@ -172,9 +189,11 @@ mpic++.1: $(top_builddir)/opal/tools/wrappers/generic_wrapper.1
 	rm -f mpic++.1
 	sed -e 's/#COMMAND#/mpic++/g' -e 's/#PROJECT#/Open MPI/g' -e 's/#PROJECT_SHORT#/OMPI/g' -e 's/#LANGUAGE#/C++/g' < $(top_builddir)/opal/tools/wrappers/generic_wrapper.1 > mpic++.1
 
+if OMPI_HAVE_CXX_COMPILER
 mpicxx.1: $(top_builddir)/opal/tools/wrappers/generic_wrapper.1
 	rm -f mpicxx.1
 	sed -e 's/#COMMAND#/mpicxx/g' -e 's/#PROJECT#/Open MPI/g' -e 's/#PROJECT_SHORT#/OMPI/g' -e 's/#LANGUAGE#/C++/g' < $(top_builddir)/opal/tools/wrappers/generic_wrapper.1 > mpicxx.1
+endif
 
 mpifort.1: $(top_builddir)/opal/tools/wrappers/generic_wrapper.1
 	rm -f mpifort.1

commit cc985b10cd0f239119e9536d81a2f12f8bc0e367

threads: Fix C++/Fortran pthread tests
Commit 38e53f3 introduced a bug in the checks for pthreads when
using C++ or Fortran, resulting in -pthread not being added
to the wrapper compiler flags.  This (on some platforms) lead
to link errors in multi-threaded applications.  Fix the bug by
not declaring success after the check for whether or not pthreads
works without additional libraries; this was overriding the
value set in the plain check.

Signed-off-by: Brian Barrett <bbarrett@amazon.com>

opal/mca/threads/pthreads/configure.m4
@@ -353,13 +353,11 @@ AC_DEFUN([OPAL_INTL_POSIX_THREADS_PLAIN], [
 OPAL_INTL_POSIX_THREADS_PLAIN_C
 
 AS_IF([test "$CXX" != "no"],
-      [OPAL_INTL_POSIX_THREADS_PLAIN_CXX
-       opal_pthread_cxx_success=1],
+      [OPAL_INTL_POSIX_THREADS_PLAIN_CXX],
       [opal_pthread_cxx_success=0])
 
 AS_IF([test "$FC" != "no"],
-      [OPAL_INTL_POSIX_THREADS_PLAIN_FC
-       opal_pthread_fortran_success=1],
+      [OPAL_INTL_POSIX_THREADS_PLAIN_FC],
       [opal_pthread_fortran_success=0])
 
 # End: OPAL_INTL_POSIX_THREADS_PLAIN
@@ -483,13 +481,11 @@ esac
 OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_C
 
 AS_IF([test "$CXX" != "no"],
-      [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_CXX
-       opal_pthread_cxx_success=1],
+      [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_CXX],
       [opal_pthread_cxx_success=0])
 
 AS_IF([test "$FC" != "no"],
-      [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_FC
-       opal_pthread_fortran_success=1],
+      [OPAL_INTL_POSIX_THREADS_SPECIAL_FLAGS_FC],
       [opal_pthread_fortran_success=0])
 
 OPAL_VAR_SCOPE_POP
@@ -675,13 +671,11 @@ plibs="-lpthreads -llthread -lpthread"
 OPAL_INTL_POSIX_THREADS_LIBS_C
 
 AS_IF([test "$CXX" != "no"],
-      [OPAL_INTL_POSIX_THREADS_LIBS_CXX
-       opal_pthread_cxx_success=1],
+      [OPAL_INTL_POSIX_THREADS_LIBS_CXX],
       [opal_pthread_cxx_success=0])
 
 AS_IF([test "$FC" != "no"],
-      [OPAL_INTL_POSIX_THREADS_LIBS_FC
-       opal_pthread_fortran_success=1],
+      [OPAL_INTL_POSIX_THREADS_LIBS_FC],
       [opal_pthread_fortran_success=0])
 
 OPAL_VAR_SCOPE_POP