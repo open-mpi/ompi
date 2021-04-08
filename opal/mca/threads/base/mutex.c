bcd376a1158b0960746e592080e430446b5f2218
Shintaro Iwasaki <siwasaki@anl.gov>
2021-04-08 16:50:04 -0500

    opal/mca/threads/pthreads: remove opal_pthread_mutex_t
    opal_pthread_mutex_t is not used.  This patch removes it.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/qthreads,argobots: remove m_recursive
    
    m_recursive is not used.  This patch removes it.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/qthreads,argobots: update wait_sync implementation
    
    This patch is corresponding to changes to the following commit for
    Pthreads: 6a406fb3c9ea74aa371bdff82e8eb01bf9820293
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/pthreads: implement opal_thread_internal_mutex/cond_t
    
    opal_thread_internal_mutex_t and opal_thread_internal_cond_t are new
    objects that are directly mapped to raw mutex/condition variable
    implementations (e.g., pthread_mutex_t for Pthreads).  This abstraction
    is different from opal_mutex_t, which has more functionalities.
    
    Introducing this abstraction helps removal of code duplication among
    other threading layers.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/pthreads: use opal_thread_internal_mutex/cond_t
    
    opal_thread_internal_mutex/cond routines are static inline functions, so
    there should be no additional overhead.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/argobots: implement opal_thread_internal_mutex/cond_t
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/argobots: use opal_thread_internal_mutex/cond_t
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/qthreads: implement opal_thread_internal_mutex/cond_t
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads/qthreads: use opal_thread_internal_mutex/cond_t
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads: move mutex implementation to threads/base
    
    Now implementations of opal_mutex_xxx() and opal_cond_xxx() are the
    same.  This patch moves them to base/mutex.c and mutex.h and removes the
    duplication.
    
    Note that those implementations are copied from
    pthread/threads_pthreads_mutex.c and pthread/threads_pthreads_mutex.h.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads: use opal_thread_yield() in busy loop if requested
    
    This commit is prerequisite for the next commit that unifies the
    implementation of wait_sync.  Nonpreemptive threads like Qthreads and
    Argobots need a yield in a busy loop to avoid a deadlock, while Pthreads
    does not.
    
    To unify the implementation in the next commit, this patch adds
    opal_thread_yield() if opal_progress_yield_when_idle is true.
    
    At the same time, this patch removes a yield operation after
    opal_progress() since opal_progress() internally yields when it does not
    make any progress.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads: move wait_sync implementation to threads/base
    
    Now implementations of wait_sync_xxx() functions and macros are the
    same.  This patch moves them to base/wait_sync.c and wait_sync.h and
    removes the duplication.
    
    Note that those implementations are copied from
    pthread/threads_pthreads_wait_sync.c and
    pthread/threads_pthreads_wait_sync.h.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads: change Pthread-specific names
    
    This patch renames variables and functions that are no longer specific
    to Pthread.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads: remove MCA_threads_wait_sync_base_include_xxx
    
    MCA_threads_wait_sync_base_include_xxx is no longer used.  This patch
    removes them.
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>
    
    opal/mca/threads: minor cleanup
    
    - Remove unnecessary "include"
    - Remove weird empty lines
    - Clean up parentheses for macros
    - Remove EDEADLK check for trylock() and unlock()
    
    Signed-off-by: Shintaro Iwasaki <siwasaki@anl.gov>

--- opal/mca/threads/base/mutex.c
+++ opal/mca/threads/base/mutex.c
@@ -25,13 +25,8 @@
  */
 
 #include "opal_config.h"
-#include "opal/mca/threads/argobots/threads_argobots.h"
-
-#include <errno.h>
-#include <string.h>
 
 #include "opal/constants.h"
-#include "opal/mca/threads/argobots/threads_argobots_mutex.h"
 #include "opal/mca/threads/mutex.h"
 
 /*
@@ -40,68 +35,75 @@
  */
 bool opal_uses_threads = false;
 
-static void mca_threads_argobots_mutex_constructor(opal_mutex_t *p_mutex)
+static void mca_threads_mutex_constructor(opal_mutex_t *p_mutex)
 {
-    opal_threads_argobots_ensure_init();
-    const ABT_mutex_memory init_mutex = ABT_MUTEX_INITIALIZER;
-    memcpy(&p_mutex->m_lock_argobots, &init_mutex, sizeof(ABT_mutex_memory));
-    p_mutex->m_recursive = 0;
 #if OPAL_ENABLE_DEBUG
+    int ret = opal_thread_internal_mutex_init(&p_mutex->m_lock, false);
+    assert(0 == ret);
     p_mutex->m_lock_debug = 0;
     p_mutex->m_lock_file = NULL;
     p_mutex->m_lock_line = 0;
+#else
+    opal_thread_internal_mutex_init(&p_mutex->m_lock, false);
 #endif
     opal_atomic_lock_init(&p_mutex->m_lock_atomic, 0);
 }
 
-static void mca_threads_argobots_recursive_mutex_constructor(opal_recursive_mutex_t *p_mutex)
+static void mca_threads_mutex_destructor(opal_mutex_t *p_mutex)
 {
-    opal_threads_argobots_ensure_init();
-    const ABT_mutex_memory init_mutex = ABT_RECURSIVE_MUTEX_INITIALIZER;
-    memcpy(&p_mutex->m_lock_argobots, &init_mutex, sizeof(ABT_mutex_memory));
-    p_mutex->m_recursive = 1;
+    opal_thread_internal_mutex_destroy(&p_mutex->m_lock);
+}
+
+static void mca_threads_recursive_mutex_constructor(opal_recursive_mutex_t *p_mutex)
+{
 #if OPAL_ENABLE_DEBUG
+    int ret = opal_thread_internal_mutex_init(&p_mutex->m_lock, true);
+    assert(0 == ret);
     p_mutex->m_lock_debug = 0;
     p_mutex->m_lock_file = NULL;
     p_mutex->m_lock_line = 0;
+#else
+    opal_thread_internal_mutex_init(&p_mutex->m_lock, true);
 #endif
     opal_atomic_lock_init(&p_mutex->m_lock_atomic, 0);
 }
 
-OBJ_CLASS_INSTANCE(opal_mutex_t, opal_object_t, mca_threads_argobots_mutex_constructor, NULL);
-OBJ_CLASS_INSTANCE(opal_recursive_mutex_t, opal_object_t,
-                   mca_threads_argobots_recursive_mutex_constructor, NULL);
+static void mca_threads_recursive_mutex_destructor(opal_recursive_mutex_t *p_mutex)
+{
+    opal_thread_internal_mutex_destroy(&p_mutex->m_lock);
+}
+
+OBJ_CLASS_INSTANCE(opal_mutex_t, opal_object_t, mca_threads_mutex_constructor,
+                   mca_threads_mutex_destructor);
+
+OBJ_CLASS_INSTANCE(opal_recursive_mutex_t, opal_object_t, mca_threads_recursive_mutex_constructor,
+                   mca_threads_recursive_mutex_destructor);
 
 int opal_cond_init(opal_cond_t *cond)
 {
-    const ABT_cond_memory init_cond = ABT_COND_INITIALIZER;
-    memcpy(cond, &init_cond, sizeof(ABT_cond_memory));
-    return OPAL_SUCCESS;
+    return opal_thread_internal_cond_init(cond);
 }
 
 int opal_cond_wait(opal_cond_t *cond, opal_mutex_t *lock)
 {
-    ABT_mutex abt_mutex = ABT_MUTEX_MEMORY_GET_HANDLE(&lock->m_lock_argobots);
-    ABT_cond abt_cond = ABT_COND_MEMORY_GET_HANDLE(cond);
-    int ret = ABT_cond_wait(abt_cond, abt_mutex);
-    return ABT_SUCCESS == ret ? OPAL_SUCCESS : OPAL_ERROR;
+    opal_thread_internal_cond_wait(cond, &lock->m_lock);
+    return OPAL_SUCCESS;
 }
 
 int opal_cond_broadcast(opal_cond_t *cond)
 {
-    ABT_cond abt_cond = ABT_COND_MEMORY_GET_HANDLE(cond);
-    int ret = ABT_cond_broadcast(abt_cond);
-    return ABT_SUCCESS == ret ? OPAL_SUCCESS : OPAL_ERROR;
+    opal_thread_internal_cond_broadcast(cond);
+    return OPAL_SUCCESS;
 }
 
 int opal_cond_signal(opal_cond_t *cond)
 {
-    ABT_cond abt_cond = ABT_COND_MEMORY_GET_HANDLE(cond);
-    int ret = ABT_cond_signal(abt_cond);
-    return ABT_SUCCESS == ret ? OPAL_SUCCESS : OPAL_ERROR;
+    opal_thread_internal_cond_signal(cond);
+    return OPAL_SUCCESS;
 }
 
 int opal_cond_destroy(opal_cond_t *cond)
 {
+    opal_thread_internal_cond_destroy(cond);
     return OPAL_SUCCESS;
 }
--- opal/mca/threads/base/wait_sync.c
+++ opal/mca/threads/base/wait_sync.c
@@ -1,12 +1,13 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
- * Copyright (c) 2014-2016 The University of Tennessee and The University
+ * Copyright (c) 2014-2020 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2016      Los Alamos National Security, LLC. All rights
  *                         reserved.
  * Copyright (c) 2017      IBM Corporation. All rights reserved.
  * Copyright (c) 2019      Sandia National Laboratories.  All rights reserved.
+ * Copyright (c) 2021      Argonne National Laboratory.  All rights reserved.
  *
  * $COPYRIGHT$
  *
@@ -15,19 +16,44 @@
  * $HEADER$
  */
 
-#include "opal/mca/threads/qthreads/threads_qthreads.h"
 #include "opal/mca/threads/wait_sync.h"
 
 static opal_mutex_t wait_sync_lock = OPAL_MUTEX_STATIC_INIT;
-static ompi_wait_sync_t *wait_sync_list = NULL;
+ompi_wait_sync_t *wait_sync_list = NULL; /* not static for inline "wait_sync_st" */
+
+void wait_sync_global_wakeup_st(int status)
+{
+    ompi_wait_sync_t *sync;
+    for (sync = wait_sync_list; sync != NULL; sync = sync->next) {
+        wait_sync_update(sync, 0, status);
+    }
+}
+
+void wait_sync_global_wakeup_mt(int status)
+{
+    ompi_wait_sync_t *sync;
+    opal_mutex_lock(&wait_sync_lock);
+    for (sync = wait_sync_list; sync != NULL; sync = sync->next) {
+        /* sync_update is going to  take the sync->lock from within
+         * the wait_sync_lock. Thread lightly here: Idealy we should
+         * find a way to not take a lock in a lock as this is deadlock prone,
+         * but as of today we are the only place doing this so it is safe.
+         */
+        wait_sync_update(sync, 0, status);
+        if (sync->next == wait_sync_list) {
+            break; /* special case for rings */
+        }
+    }
+    opal_mutex_unlock(&wait_sync_lock);
+}
 
 static opal_atomic_int32_t num_thread_in_progress = 0;
 
-#define WAIT_SYNC_PASS_OWNERSHIP(who)        \
-    do {                                     \
-        opal_mutex_lock(&(who)->lock);       \
-        opal_cond_signal(&(who)->condition); \
-        opal_mutex_unlock(&(who)->lock);     \
+#define WAIT_SYNC_PASS_OWNERSHIP(who)                        \
+    do {                                                     \
+        opal_thread_internal_mutex_lock(&(who)->lock);       \
+        opal_thread_internal_cond_signal(&(who)->condition); \
+        opal_thread_internal_mutex_unlock(&(who)->lock);     \
     } while (0)
 
 int ompi_sync_wait_mt(ompi_wait_sync_t *sync)
@@ -41,13 +67,13 @@ int ompi_sync_wait_mt(ompi_wait_sync_t *sync)
     }
 
     /* lock so nobody can signal us during the list updating */
-    opal_mutex_lock(&sync->lock);
+    opal_thread_internal_mutex_lock(&sync->lock);
 
     /* Now that we hold the lock make sure another thread has not already
      * call cond_signal.
      */
     if (sync->count <= 0) {
-        opal_mutex_unlock(&sync->lock);
+        opal_thread_internal_mutex_unlock(&sync->lock);
         return (0 == sync->status) ? OPAL_SUCCESS : OPAL_ERROR;
     }
 
@@ -72,7 +98,7 @@ int ompi_sync_wait_mt(ompi_wait_sync_t *sync)
      */
 check_status:
     if (sync != wait_sync_list && num_thread_in_progress >= opal_max_thread_in_progress) {
-        opal_cond_wait(&sync->condition, &sync->lock);
+        opal_thread_internal_cond_wait(&sync->condition, &sync->lock);
 
         /**
          * At this point either the sync was completed in which case
@@ -81,19 +107,18 @@ check_status:
          */
 
         if (sync->count <= 0) { /* Completed? */
-            opal_mutex_unlock(&sync->lock);
+            opal_thread_internal_mutex_unlock(&sync->lock);
             goto i_am_done;
         }
         /* either promoted, or spurious wakeup ! */
         goto check_status;
     }
-    opal_mutex_unlock(&sync->lock);
+    opal_thread_internal_mutex_unlock(&sync->lock);
 
     OPAL_THREAD_ADD_FETCH32(&num_thread_in_progress, 1);
     while (sync->count > 0) { /* progress till completion */
         /* don't progress with the sync lock locked or you'll deadlock */
         opal_progress();
-        qthread_yield();
     }
     OPAL_THREAD_ADD_FETCH32(&num_thread_in_progress, -1);
 
