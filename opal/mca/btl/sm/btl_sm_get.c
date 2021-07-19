95eb83f90ca6b1d115ad37e9e090136747a80ec1
Nathan Hjelm <hjelmn@google.com>
2021-07-19 11:55:33 -0700

    btl/sm: use the shared-memory single-copy framework for single-copy
    This commit removes the dedicated shared-memory single-copy support from
    btl/sm. This support is now provided by the shared-memory single-copy
    (smsc) framework. The btl_sm_single_copy_mechanism MCA variable has
    been removed. Use either the component priority parameters (ex.
    scsm_xpmem_priority) or component selection (--mca smsc) to select the
    single-copy mechanism.
    
    Signed-off-by: Nathan Hjelm <hjelmn@google.com>

--- opal/mca/btl/sm/btl_sm_get.c
+++ opal/mca/btl/sm/btl_sm_get.c
@@ -4,7 +4,7 @@
  *                         reserved.
  * Copyright (c) 2018      Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
- * Copyright (c) 2019      Google, Inc. All rights reserved.
+ * Copyright (c) 2019-2021 Google, Inc. All rights reserved.
  * Copyright (c) 2020      The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
@@ -20,16 +20,7 @@
 #include "opal/mca/btl/sm/btl_sm.h"
 #include "opal/mca/btl/sm/btl_sm_endpoint.h"
 #include "opal/mca/btl/sm/btl_sm_frag.h"
-#include "opal/mca/btl/sm/btl_sm_xpmem.h"
-
-#if OPAL_BTL_SM_HAVE_CMA
-#    include <sys/uio.h>
-
-#    if OPAL_CMA_NEED_SYSCALL_DEFS
-#        include "opal/sys/cma.h"
-#    endif /* OPAL_CMA_NEED_SYSCALL_DEFS */
-
-#endif
+#include "opal/mca/smsc/smsc.h"
 
 /**
  * Initiate an synchronous get.
@@ -38,131 +29,18 @@
  * @param endpoint (IN)    BTL addressing information
  * @param descriptor (IN)  Description of the data to be transferred
  */
-#if OPAL_BTL_SM_HAVE_XPMEM
-int mca_btl_sm_get_xpmem(mca_btl_base_module_t *btl, mca_btl_base_endpoint_t *endpoint,
-                         void *local_address, uint64_t remote_address,
-                         mca_btl_base_registration_handle_t *local_handle,
-                         mca_btl_base_registration_handle_t *remote_handle, size_t size, int flags,
-                         int order, mca_btl_base_rdma_completion_fn_t cbfunc, void *cbcontext,
-                         void *cbdata)
+
+int mca_btl_sm_get(mca_btl_base_module_t *btl, mca_btl_base_endpoint_t *endpoint,
+                   void *local_address, uint64_t remote_address,
+                   mca_btl_base_registration_handle_t *local_handle,
+                   mca_btl_base_registration_handle_t *remote_handle, size_t size, int flags,
+                   int order, mca_btl_base_rdma_completion_fn_t cbfunc, void *cbcontext,
+                   void *cbdata)
 {
-    mca_rcache_base_registration_t *reg;
-    void *rem_ptr;
-
-    /* silence warning about unused arguments */
-    (void) local_handle;
-    (void) remote_handle;
-
-    reg = sm_get_registation(endpoint, (void *) (intptr_t) remote_address, size, 0, &rem_ptr);
-    if (OPAL_UNLIKELY(NULL == rem_ptr)) {
-        return OPAL_ERROR;
-    }
-
-    sm_memmove(local_address, rem_ptr, size);
-
-    sm_return_registration(reg, endpoint);
-
-    /* always call the callback function */
-    cbfunc(btl, endpoint, local_address, local_handle, cbcontext, cbdata, OPAL_SUCCESS);
-
-    return OPAL_SUCCESS;
-}
-#endif
-
-#if OPAL_BTL_SM_HAVE_CMA
-int mca_btl_sm_get_cma(mca_btl_base_module_t *btl, mca_btl_base_endpoint_t *endpoint,
-                       void *local_address, uint64_t remote_address,
-                       mca_btl_base_registration_handle_t *local_handle,
-                       mca_btl_base_registration_handle_t *remote_handle, size_t size, int flags,
-                       int order, mca_btl_base_rdma_completion_fn_t cbfunc, void *cbcontext,
-                       void *cbdata)
-{
-    struct iovec src_iov = {.iov_base = (void *) (intptr_t) remote_address, .iov_len = size};
-    struct iovec dst_iov = {.iov_base = local_address, .iov_len = size};
-    ssize_t ret;
-
-    /*
-     * According to the man page :
-     * "On success, process_vm_readv() returns the number of bytes read and
-     * process_vm_writev() returns the number of bytes written.  This return
-     * value may be less than the total number of requested bytes, if a
-     * partial read/write occurred.  (Partial transfers apply at the
-     * granularity of iovec elements.  These system calls won't perform a
-     * partial transfer that splits a single iovec element.)".
-     * So since we use a single iovec element, the returned size should either
-     * be 0 or size, and the do loop should not be needed here.
-     * We tried on various Linux kernels with size > 2 GB, and surprisingly,
-     * the returned value is always 0x7ffff000 (fwiw, it happens to be the size
-     * of the larger number of pages that fits a signed 32 bits integer).
-     * We do not know whether this is a bug from the kernel, the libc or even
-     * the man page, but for the time being, we do as is process_vm_readv() could
-     * return any value.
-     */
-    do {
-        ret = process_vm_readv(endpoint->segment_data.other.seg_ds->seg_cpid, &dst_iov, 1, &src_iov,
-                               1, 0);
-        if (0 > ret) {
-            if (ESRCH == errno) {
-                BTL_PEER_ERROR(NULL, ("CMA read %ld, expected %lu, errno = %d\n", (long) ret,
-                                      (unsigned long) size, errno));
-                return OPAL_ERROR;
-            }
-            BTL_ERROR(("CMA read %ld, expected %lu, errno = %d\n", (long) ret, (unsigned long) size,
-                       errno));
-            return OPAL_ERROR;
-        }
-        src_iov.iov_base = (void *) ((char *) src_iov.iov_base + ret);
-        src_iov.iov_len -= ret;
-        dst_iov.iov_base = (void *) ((char *) dst_iov.iov_base + ret);
-        dst_iov.iov_len -= ret;
-    } while (0 < src_iov.iov_len);
-
-    /* always call the callback function */
-    cbfunc(btl, endpoint, local_address, local_handle, cbcontext, cbdata, OPAL_SUCCESS);
-
-    return OPAL_SUCCESS;
-}
-#endif
-
-#if OPAL_BTL_SM_HAVE_KNEM
-int mca_btl_sm_get_knem(mca_btl_base_module_t *btl, mca_btl_base_endpoint_t *endpoint,
-                        void *local_address, uint64_t remote_address,
-                        mca_btl_base_registration_handle_t *local_handle,
-                        mca_btl_base_registration_handle_t *remote_handle, size_t size, int flags,
-                        int order, mca_btl_base_rdma_completion_fn_t cbfunc, void *cbcontext,
-                        void *cbdata)
-{
-    struct knem_cmd_param_iovec recv_iovec;
-    struct knem_cmd_inline_copy icopy;
-
-    /* Fill in the ioctl data fields.  There's no async completion, so
-       we don't need to worry about getting a slot, etc. */
-    recv_iovec.base = (uintptr_t) local_address;
-    recv_iovec.len = size;
-    icopy.local_iovec_array = (uintptr_t) &recv_iovec;
-    icopy.local_iovec_nr = 1;
-    icopy.remote_cookie = remote_handle->cookie;
-    icopy.remote_offset = remote_address - remote_handle->base_addr;
-    icopy.write = 0;
-    icopy.flags = 0;
-
-    /* Use the DMA flag if knem supports it *and* the segment length
-     * is greater than the cutoff. Not that if DMA is not supported
-     * or the user specified 0 for knem_dma_min the knem_dma_min was
-     * set to UINT_MAX in mca_btl_sm_knem_init. */
-    if (mca_btl_sm_component.knem_dma_min <= size) {
-        icopy.flags = KNEM_FLAG_DMA;
-    }
-    /* synchronous flags only, no need to specify icopy.async_status_index */
-
-    /* When the ioctl returns, the transfer is done and we can invoke
-       the btl callback and return the frag */
-    if (OPAL_UNLIKELY(0 != ioctl(mca_btl_sm.knem_fd, KNEM_CMD_INLINE_COPY, &icopy))) {
-        return OPAL_ERROR;
-    }
-
-    if (KNEM_STATUS_FAILED == icopy.current_status) {
-        return OPAL_ERROR;
+    int ret = MCA_SMSC_CALL(copy_from, endpoint->smsc_endpoint, local_address,
+                            (void *) (intptr_t) remote_address, size, remote_handle);
+    if (OPAL_UNLIKELY(OPAL_SUCCESS != ret)) {
+        return ret;
     }
 
     /* always call the callback function */
@@ -170,4 +48,3 @@ int mca_btl_sm_get_knem(mca_btl_base_module_t *btl, mca_btl_base_endpoint_t *end
 
     return OPAL_SUCCESS;
 }
-#endif
--- opal/mca/btl/sm/configure.m4
+++ opal/mca/btl/sm/configure.m4
@@ -21,25 +21,6 @@
 AC_DEFUN([MCA_opal_btl_sm_CONFIG],[
     AC_CONFIG_FILES([opal/mca/btl/sm/Makefile])
 
-    OPAL_VAR_SCOPE_PUSH([btl_sm_xpmem_happy btl_sm_cma_happy btl_sm_knem_happy])
-
-    # Check for single-copy APIs
-
-    OPAL_CHECK_XPMEM([btl_sm], [btl_sm_xpmem_happy=1], [btl_sm_xpmem_happy=0])
-    OPAL_CHECK_KNEM([btl_sm], [btl_sm_knem_happy=1],[btl_sm_knem_happy=0])
-    OPAL_CHECK_CMA([btl_sm], [AC_CHECK_HEADER([sys/prctl.h]) btl_sm_cma_happy=1], [btl_sm_cma_happy=0])
-
-    AC_DEFINE_UNQUOTED([OPAL_BTL_SM_HAVE_XPMEM], [$btl_sm_xpmem_happy],
-        [If XPMEM support can be enabled within sm])
-
-    AC_DEFINE_UNQUOTED([OPAL_BTL_SM_HAVE_CMA], [$btl_sm_cma_happy],
-        [If CMA support can be enabled within sm])
-
-    AC_DEFINE_UNQUOTED([OPAL_BTL_SM_HAVE_KNEM], [$btl_sm_knem_happy],
-	[If KNEM support can be enabled within sm])
-
-    OPAL_VAR_SCOPE_POP
-
     # always happy
     [$1]
 
