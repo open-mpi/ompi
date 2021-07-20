63bab277ce94ef229fcf4f31100fb6d677d42305
Boris Karasev <karasev.b@gmail.com>
2021-07-20 13:10:03 +0300

    pml/ucx: fix datatype refcount
    Fixes #9138
    
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>

--- ompi/mca/pml/ucx/pml_ucx_request.c
+++ ompi/mca/pml/ucx/pml_ucx_request.c
@@ -218,7 +218,9 @@ static int mca_pml_ucx_persistent_request_free(ompi_request_t **rptr)
     }
     if ((preq->flags & MCA_PML_UCX_REQUEST_FLAG_SEND) &&
          (MCA_PML_BASE_SEND_BUFFERED == preq->send.mode)) {
-        OBJ_RELEASE(preq->datatype.ompi_datatype);
+        OBJ_RELEASE(preq->ompi_datatype);
+    } else {
+        OMPI_DATATYPE_RELEASE(preq->ompi_datatype);
     }
     PML_UCX_FREELIST_RETURN(&ompi_pml_ucx.persistent_reqs, &preq->ompi.super);
     *rptr = MPI_REQUEST_NULL;
