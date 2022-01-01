e72b4e6630e44264d9da05b6bbbc014163e96dd1
Jeff Squyres <jsquyres@cisco.com>
2022-01-01 07:25:45 -0800

    MPI_Wtick/MPI_Wtime: use opal_clock_get*() wrappers
    Update MPI_Wtick and MPI_Wtime to use the portable wrappers
    opal_clock_gettime() and opal_clock_getres().
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- ompi/mpi/c/wtick.c
+++ ompi/mpi/c/wtick.c
@@ -9,7 +9,7 @@
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
- * Copyright (c) 2007-2014 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2007-2022 Cisco Systems, Inc.  All rights reserved
  * Copyright (c) 2015-2016 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  * Copyright (c) 2017      IBM Corporation.  All rights reserved.
@@ -31,9 +31,10 @@
 #include <time.h>
 #endif
 
-#include MCA_timer_IMPLEMENTATION_HEADER
 #include "ompi/mpi/c/bindings.h"
 #include "ompi/runtime/mpiruntime.h"
+
+#include "opal/util/clock_gettime.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -44,38 +45,15 @@
 
 double MPI_Wtick(void)
 {
-    /*
-     * See https://github.com/open-mpi/ompi/issues/3003
-     * to get an idea what's going on here.
-     */
-#if 0
-#if OPAL_TIMER_CYCLE_NATIVE
-    {
-        opal_timer_t freq = opal_timer_base_get_freq();
-        if (0 == freq) {
-            /* That should never happen, but if it does, return a bogus value
-             * rather than crashing with a division by zero */
-            return (double)0.0;
-        }
-        return (double)1.0 / (double)freq;
-    }
-#elif OPAL_TIMER_USEC_NATIVE
-    return 0.000001;
-#endif
-#else
-#if defined(__linux__) && OPAL_HAVE_CLOCK_GETTIME
+    // We intentionally don't use the OPAL timer framework here.  See
+    // https://github.com/open-mpi/ompi/issues/3003 for more details.
     struct timespec spec;
     double wtick = 0.0;
-    if (0 == clock_getres(CLOCK_MONOTONIC, &spec)){
+    if (0 == opal_clock_getres(&spec)){
         wtick =  spec.tv_sec + spec.tv_nsec * 1.0e-09;
     } else {
         /* guess */
         wtick = 1.0e-09;
     }
     return wtick;
-#else
-    /* Otherwise, we already return usec precision. */
-    return 0.000001;
-#endif
-#endif
 }
