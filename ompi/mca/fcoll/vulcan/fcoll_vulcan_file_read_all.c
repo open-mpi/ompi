b4d21e0099bbc878f33c97ca39779956c0b1350d
Edgar Gabriel <edgar.gabriel1@outlook.com>
2022-01-30 08:29:32 -0600

    ompio file_read_all: relocate implementation from base to common
    Move the default implementation of the read_all function used by some components
    from fcoll/base to common/ompio, in order to resolve dependency problems with fcoll/base.
    Re-introduce per-component read_all functions, which are however only stubs calling
    the common/ompio implementation.
    
    Tested with static and dynamic builds. Also ensured that the --disable-io-ompio flag
    still works correctly.
    
    Signed-off-by: Edgar Gabriel <edgar.gabriel1@outlook.com>

---
+++ ompi/mca/fcoll/vulcan/fcoll_vulcan_file_read_all.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2017 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2008-2021 University of Houston. All rights reserved.
+ * Copyright (c) 2015-2018 Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+#include "fcoll_vulcan.h"
+
+#include "mpi.h"
+#include "ompi/mca/common/ompio/common_ompio.h"
+
+
+int mca_fcoll_vulcan_file_read_all (struct ompio_file_t *fh,
+                                    void *buf,
+                                    int count,
+                                    struct ompi_datatype_t *datatype,
+                                    ompi_status_public_t *status)
+{
+    return mca_common_ompio_base_file_read_all (fh, buf, count, datatype, status);
+}
+
+
