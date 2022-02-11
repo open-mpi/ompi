ef7dcbdc00cecb92907bdb02ed612a7379177bb2
Howard Pritchard <howardp@lanl.gov>
2022-02-11 14:13:12 -0700

    sessions:move opal_sma_base_f to ompi_framework
    dependencies array.
    
    If this is not done, then when using a non-ob1 PML, the stages of finalization
    have the SMSC framework closed before the btl framework is closed or del_procs invoked
    for the btl(s).
    
    When using the --enable-mca_dso this results in a segfault as the code section for the SMSC
    framework/components has been dlclosed.
    
    Moving the opal_smc_base_framework from the opal_init_frameworks to the
    ompi_framework_dependencies fixes this problem.
    
    Related to #9097
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>
    (cherry picked from commit ec21585ec1940f79d887beec5c48f53e9fc728f6)

--- opal/runtime/opal_init.c
+++ opal/runtime/opal_init.c
@@ -622,7 +622,7 @@ static mca_base_framework_t *opal_init_frameworks[] = {
     &opal_memcpy_base_framework, &opal_memchecker_base_framework,
     &opal_backtrace_base_framework, &opal_timer_base_framework,
     &opal_shmem_base_framework, &opal_reachable_base_framework,
-    &opal_pmix_base_framework, &opal_smsc_base_framework,
+    &opal_pmix_base_framework,
     NULL,
 };
 
