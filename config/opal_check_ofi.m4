open-mpi/ompi

commit b542a9ce6aaebc3b47a366bbd863af5b334ad2d8

build/ofi: Update checks to use cache file
Update the ofi checks to use OAC_CHECK_PACKAGE and cache checks,
rather than a big if statement.  This required a bit of trickery
to get the cache to work with the version check macro, but as
an added bonus, the print for version checking now is in the
form <major>.<minor> instead of <major>,<minor>.

Signed-off-by: Brian Barrett <bbarrett@amazon.com>

config/opal_check_ofi.m4
@@ -1,4 +1,4 @@
-dnl -*- shell-script -*-
+dnl -*- autoconf -*-
 dnl
 dnl Copyright (c) 2015-2020 Cisco Systems, Inc.  All rights reserved.
 dnl Copyright (c) 2016-2017 Los Alamos National Security, LLC. All rights
@@ -24,42 +24,55 @@ dnl $3: action if OFI API version is < $1
 AC_DEFUN([OPAL_CHECK_OFI_VERSION_GE],[
     OPAL_VAR_SCOPE_PUSH([opal_ofi_ver_ge_save_CPPFLAGS opal_ofi_ver_ge_happy])
 
-    AC_MSG_CHECKING([if OFI API version number is >= $1])
-    opal_ofi_ver_ge_save_CPPFLAGS=$CPPFLAGS
-    CPPFLAGS=$opal_ofi_CPPFLAGS
+    AS_LITERAL_WORD_IF([$1], [], [m4_fatal([OPAL_CHECK_OFI_VERSION_GE called with non-literal first argument])])dnl
+    AS_VAR_PUSHDEF([version_cache_var], [opal_ofi_ver_ge_cv_$1])dnl
+    m4_pushdef([version_pretty_print], [m4_translit([$1], [,], [.])])dnl
 
-    AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[#include <rdma/fabric.h>]],
-[[
+    AC_CACHE_CHECK([if OFI API version number is >= version_pretty_print],
+        [version_cache_var],
+        [opal_ofi_ver_ge_save_CPPFLAGS=$CPPFLAGS
+         CPPFLAGS=$opal_ofi_CPPFLAGS
+
+         AC_COMPILE_IFELSE([AC_LANG_PROGRAM(
+                [[#include <rdma/fabric.h>
+]], [[
 #if !defined(FI_MAJOR_VERSION)
 #error "we cannot check the version -- sad panda"
 #elif FI_VERSION_LT(FI_VERSION(FI_MAJOR_VERSION, FI_MINOR_VERSION), FI_VERSION($1))
 #error "version is too low -- nopes"
 #endif
 ]])],
-                      [opal_ofi_ver_ge_happy=1],
-                      [opal_ofi_ver_ge_happy=0])
+                      [version_cache_var=yes],
+                      [version_cache_var=no])])
 
-    AS_IF([test $opal_ofi_ver_ge_happy -eq 1],
-          [AC_MSG_RESULT([yes])
-           $2],
-          [AC_MSG_RESULT([no])
-           $3])
+    AS_IF([test "${version_cache_var}" = "yes"],
+          [$2],
+          [$3])
 
     CPPFLAGS=$opal_ofi_ver_ge_save_CPPFLAGS
 
+    m4_popdef([version_pretty_print])
+    AS_VAR_POPDEF([version_cache_var])
     OPAL_VAR_SCOPE_POP
 ])dnl
 
-dnl
-dnl _OPAL_CHECK_OFI
+
+dnl OPAL_CHECK_OFI(prefix, [action if found], [action if not found])
 dnl --------------------------------------------------------
 dnl Do the real work of checking for OFI libfabric.
 dnl Upon return:
 dnl
-dnl - opal_ofi_happy: will be "yes" or "no"
-dnl - opal_ofi_{CPPFLAGS|LDFLAGS|LIBS} will be loaded (if relevant)
+dnl opal_ofi_{CPPFLAGS, LDFLAGS, LIBS} and prefix_{CPPFLAGS, LDFLAGS,
+dnl LIBS} will be set as needed.
 dnl
-AC_DEFUN([_OPAL_CHECK_OFI],[
+dnl This macro intentionally leaks opal_ofi_happy = yes/no as well as
+dnl evaluating the action if found / action if not found
+dnl
+AC_DEFUN([OPAL_CHECK_OFI],[
+    OPAL_VAR_SCOPE_PUSH([opal_check_ofi_save_CPPFLAGS opal_check_ofi_save_LDFLAGS opal_check_ofi_save_LIBS opal_check_fi_info_pci])
+
+    m4_ifblank([$1], [m4_fatal([First argument must be set for call to OPAL_CHECK_OFI])])
+
     # Add --with options
     AC_ARG_WITH([libfabric],
                 [AS_HELP_STRING([--with-libfabric=DIR],
@@ -71,58 +84,32 @@ AC_DEFUN([_OPAL_CHECK_OFI],[
     AC_ARG_WITH([ofi],
                 [AS_HELP_STRING([--with-ofi=DIR],
                                 [Specify location of OFI libfabric installation, adding DIR/include to the default search location for libfabric headers, and DIR/lib or DIR/lib64 to the default search location for libfabric libraries.  Error if libfabric support cannot be found.])])
-
     AC_ARG_WITH([ofi-libdir],
                 [AS_HELP_STRING([--with-ofi-libdir=DIR],
                                 [Search for OFI libfabric libraries in DIR])])
 
-    if test "$with_ofi" = ""; then
-         with_ofi=$with_libfabric
-    fi
+    AS_IF([test -z "${with_ofi}"], [with_ofi=${with_libfabric}])
+    AS_IF([test -z "${with_ofi_libdir}"], [with_ofi_libdir=${with_libfabric_libdir}])
 
-    if test "$with_ofi_libdir" = ""; then
-         with_ofi_libdir=$with_libfabric_libdir
-    fi
+    opal_check_ofi_save_CPPFLAGS=${CPPFLAGS}
+    opal_check_ofi_save_LDFLAGS=${LDFLAGS}
+    opal_check_ofi_save_LIBS=${LIBS}
 
-    # Sanity check the --with values
-    OPAL_CHECK_WITHDIR([ofi], [$with_ofi],
-                       [include/rdma/fabric.h])
-    OPAL_CHECK_WITHDIR([ofi-libdir], [$with_ofi_libdir],
-                       [libfabric.*])
-
-    OPAL_VAR_SCOPE_PUSH([opal_check_ofi_save_CPPFLAGS opal_check_ofi_save_LDFLAGS opal_check_ofi_save_LIBS opal_check_fi_info_pci])
-    opal_check_ofi_save_CPPFLAGS=$CPPFLAGS
-    opal_check_ofi_save_LDFLAGS=$LDFLAGS
-    opal_check_ofi_save_LIBS=$LIBS
     opal_check_fi_info_pci=0
 
-    opal_ofi_happy=yes
-    AS_IF([test "$with_ofi" = "no"],
-          [opal_ofi_happy=no])
+    dnl OMPI has used ofi everywhere for some time, but the pkg-config
+    dnl module name is libfabric.  Easier to set the pkg-config module
+    dnl name explicitly than change everything in OMPI.
+    m4_define([ofi_pkgconfig_module], [libfabric])
+    OAC_CHECK_PACKAGE([ofi],
+                      [$1],
+                      [rdma/fabric.h],
+                      [fabric],
+                      [fi_getinfo],
+                      [opal_ofi_happy=yes],
+                      [opal_ofi_happy=no])
 
-    AS_IF([test $opal_ofi_happy = yes],
-          [AC_MSG_CHECKING([looking for OFI libfabric in])
-           AS_IF([test "$with_ofi" != "yes"],
-                 [opal_ofi_dir=$with_ofi
-                  AC_MSG_RESULT([($opal_ofi_dir)])],
-                 [AC_MSG_RESULT([(default search paths)])])
-           AS_IF([test ! -z "$with_ofi_libdir" && \
-                         test "$with_ofi_libdir" != "yes"],
-                 [opal_ofi_libdir=$with_ofi_libdir])
-          ])
-
-    AS_IF([test $opal_ofi_happy = yes],
-          [OPAL_CHECK_PACKAGE([opal_ofi],
-                              [rdma/fabric.h],
-                              [fabric],
-                              [fi_getinfo],
-                              [],
-                              [$opal_ofi_dir],
-                              [$opal_ofi_libdir],
-                              [],
-                              [opal_ofi_happy=no])])
-
-    CPPFLAGS="$CPPFLAGS $opal_ofi_CPPFLAGS"
+    OPAL_FLAGS_APPEND_UNIQ([CPPFLAGS], [${$1_CPPFLAGS}])
 
     AS_IF([test $opal_ofi_happy = yes],
           [AC_CHECK_HEADERS([rdma/fi_ext.h])
@@ -133,7 +120,7 @@ AC_DEFUN([_OPAL_CHECK_OFI],[
                            [[#include <rdma/fabric.h>]])
 
            AC_DEFINE_UNQUOTED([OPAL_OFI_PCI_DATA_AVAILABLE],
-                              [$opal_check_fi_info_pci],
+                              [${opal_check_fi_info_pci}],
                               [check if pci data is available in ofi])
 
            AC_CHECK_DECLS([FI_OPT_FI_HMEM_P2P],
@@ -147,51 +134,28 @@ AC_DEFUN([_OPAL_CHECK_OFI],[
                            ])
 
            OPAL_FLAGS_APPEND_UNIQ([CPPFLAGS], [${opal_pmix_CPPFLAGS}])
-
            AC_CHECK_DECLS([PMIX_PACKAGE_RANK],
                           [],
                           [],
                           [#include <pmix.h>])])
 
-    CPPFLAGS=$opal_check_ofi_save_CPPFLAGS
-    LDFLAGS=$opal_check_ofi_save_LDFLAGS
-    LIBS=$opal_check_ofi_save_LIBS
+    CPPFLAGS=${opal_check_ofi_save_CPPFLAGS}
+    LDFLAGS=${opal_check_ofi_save_LDFLAGS}
+    LIBS=${opal_check_ofi_save_LIBS}
 
-    AC_SUBST([opal_ofi_CPPFLAGS])
-    AC_SUBST([opal_ofi_LDFLAGS])
-    AC_SUBST([opal_ofi_LIBS])
+    dnl for backwards compatibility reasons
+    opal_ofi_CPPFLAGS="${$1_CPPFLAGS}"
+    opal_ofi_LDFLAGS="${$1_LDFLAGS}"
+    opal_ofi_LIBS="${$1_LIBS}"
 
-    OPAL_SUMMARY_ADD([Transports], [OpenFabrics OFI Libfabric], [], [$opal_ofi_happy])
+    OPAL_SUMMARY_ADD([Transports], [OpenFabrics OFI Libfabric], [], [${$1_SUMMARY}])
 
-    OPAL_VAR_SCOPE_POP
-
-    AS_IF([test $opal_ofi_happy = no],
-          [AS_IF([test -n "$with_ofi" && test "$with_ofi" != "no"],
+    AS_IF([test "${opal_ofi_happy}" = "yes"],
+          [$2],
+          [AS_IF([test -n "${with_ofi}" && test "${with_ofi}" != "no"],
                  [AC_MSG_WARN([OFI libfabric support requested (via --with-ofi or --with-libfabric), but not found.])
                   AC_MSG_ERROR([Cannot continue.])])
-           ])
+           $3])
+
+    OPAL_VAR_SCOPE_POP
 ])dnl
-
-
-dnl
-dnl OPAL_CHECK_OFI
-dnl --------------------------------------------------------
-dnl Check to see if OFI libfabric is available.
-dnl
-dnl This is a simple wrapper around _OPAL_CHECK_OFI that just
-dnl ensures to only run the checks once.  We do not use AC_REQUIRE
-dnl because that re-orders the texts and makes ordering in stdout
-dnl quite confusing / difficult to grok.
-dnl
-AC_DEFUN([OPAL_CHECK_OFI],[
-    # Check for OFI libfabric.  Note that $opal_ofi_happy is used in
-    # other configure.m4's to know if OFI/libfabric configured
-    # successfully.  We only need to run the back-end checks once, but
-    # at least emit a "checking..." statement each subsequent time
-    # this macro is invoked so that configure's stdout has
-    # sensible/logical output.
-    AS_IF([test -z "$opal_ofi_happy"],
-        [_OPAL_CHECK_OFI],
-        [AC_MSG_CHECKING([if OFI libfabric is available])
-         AC_MSG_RESULT([$opal_ofi_happy])])
-])

opal/mca/btl/usnic/configure.m4
@@ -98,7 +98,7 @@ AC_DEFUN([_OPAL_BTL_USNIC_DO_CONFIG],[
 
     AS_IF([test "$opal_btl_usnic_happy" = "yes"],
           [ # The usnic BTL requires OFI libfabric support
-           OPAL_CHECK_OFI
+           OPAL_CHECK_OFI([btl_usnic])
            opal_btl_usnic_happy=$opal_ofi_happy])
 
     # The usnic BTL requires at least OFI libfabric v1.3.

ompi/mca/mtl/ofi/Makefile.am
@@ -9,6 +9,7 @@
 #                         and Technology (RIST).  All rights reserved.
 # Copyright (c) 2020      Triad National Security, LLC. All rights
 #                         reserved.
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -22,7 +23,7 @@ EXTRA_DIST = post_configure.sh \
 MAINTAINERCLEANFILES = \
     $(generated_sources)
 
-AM_CPPFLAGS = $(opal_ofi_CPPFLAGS)
+AM_CPPFLAGS = $(mtl_ofi_CPPFLAGS)
 
 dist_ompidata_DATA = help-mtl-ofi.txt
 
@@ -77,15 +78,15 @@ mcacomponentdir = $(ompilibdir)
 mcacomponent_LTLIBRARIES = $(component_install)
 mca_mtl_ofi_la_SOURCES = $(mtl_ofi_sources)
 mca_mtl_ofi_la_LDFLAGS = \
-        $(opal_ofi_LDFLAGS) \
+        $(mtl_ofi_LDFLAGS) \
         -module -avoid-version
 mca_mtl_ofi_la_LIBADD = $(top_builddir)/ompi/lib@OMPI_LIBMPI_NAME@.la \
         $(OPAL_TOP_BUILDDIR)/opal/mca/common/ofi/lib@OPAL_LIB_NAME@mca_common_ofi.la \
-        $(opal_ofi_LIBS)
+        $(mtl_ofi_LIBS)
 
 noinst_LTLIBRARIES = $(component_noinst)
 libmca_mtl_ofi_la_SOURCES = $(mtl_ofi_sources)
 libmca_mtl_ofi_la_LDFLAGS = \
-        $(opal_ofi_LDFLAGS) \
+        $(mtl_ofi_LDFLAGS) \
         -module -avoid-version
-libmca_mtl_ofi_la_LIBADD = $(opal_ofi_LIBS)
+libmca_mtl_ofi_la_LIBADD = $(mtl_ofi_LIBS)

ompi/mca/mtl/ofi/configure.m4
@@ -1,10 +1,11 @@
-# -*- shell-script -*-
+# -*- autoconf -*-
 #
 # Copyright (c) 2013-2014 Intel, Inc. All rights reserved
 #
 # Copyright (c) 2014-2020 Cisco Systems, Inc.  All rights reserved
 # Copyright (c) 2017      Los Alamos National Security, LLC.  All rights
 #                         reserved.
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -23,29 +24,40 @@ AC_DEFUN([MCA_ompi_mtl_ofi_POST_CONFIG], [
 #                    [action-if-cant-compile])
 # ------------------------------------------------
 AC_DEFUN([MCA_ompi_mtl_ofi_CONFIG],[
+    OPAL_VAR_SCOPE_PUSH([mtl_ofi_happy])
+
     AC_CONFIG_FILES([ompi/mca/mtl/ofi/Makefile])
 
-    # Check for OFI
-    OPAL_CHECK_OFI
+    OPAL_CHECK_OFI([mtl_ofi],
+                   [mtl_ofi_happy=1],
+                   [mtl_ofi_happy=0])
 
-    # Check for CUDA
-    OPAL_CHECK_CUDA
-
-    # Check for cuda support. If so, we require a minimum libfabric version
-    # of 1.9. FI_HMEM capabilities are only available starting from v1.9
-    opal_ofi_happy="yes"
-    AS_IF([test "$opal_check_cuda_happy" = "yes"],
-          [OPAL_CHECK_OFI_VERSION_GE([1,9],
-                                     [],
-                                     [opal_ofi_happy=no])])
-
-    # The OFI MTL requires at least OFI libfabric v1.5.
-    AS_IF([test "$opal_ofi_happy" = "yes"],
+    dnl The OFI MTL requires at least OFI libfabric v1.5.
+    AS_IF([test ${mtl_ofi_happy} -eq 1],
           [OPAL_CHECK_OFI_VERSION_GE([1,5],
                                      [],
-                                     [opal_ofi_happy=no])])
+                                     [mtl_ofi_happy=0])])
 
-    AS_IF([test "$opal_ofi_happy" = "yes"],
+    AS_IF([test ${mtl_ofi_happy} -eq 1],
+          [OPAL_CHECK_CUDA
+
+           dnl Check for cuda support. If so, we require a minimum libfabric version
+           dnl of 1.9. FI_HMEM capabilities are only available starting from v1.9
+           AS_IF([test "${opal_check_cuda_happy}" = "yes"],
+                 [OPAL_CHECK_OFI_VERSION_GE([1,9],
+                                            [],
+                                            [mtl_ofi_happy=0])])])
+
+    AS_IF([test ${mtl_ofi_happy} -eq 1],
           [$1],
-          [$2])
+          [AS_IF([test -n "${with_ofi}" -a "${with_ofi}" != "no"],
+                 [AC_MSG_WARN([OFI libfabric support requested (via --with-ofi or --with-libfabric), but not found.])
+                  AC_MSG_ERROR([Cannot continue.])])
+           $2])
+
+    AC_SUBST([mtl_ofi_CPPFLAGS])
+    AC_SUBST([mtl_ofi_LDFLAGS])
+    AC_SUBST([mtl_ofi_LIBS])
+
+    OPAL_VAR_SCOPE_POP
 ])dnl

opal/mca/btl/ofi/Makefile.am
@@ -15,6 +15,7 @@
 # Copyright (c) 2018      Intel, inc. All rights reserved
 # Copyright (c) 2020      Triad National Security, LLC. All rights
 #                         reserved.
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -24,7 +25,7 @@
 
 #dist_opaldata_DATA = help-mpi-btl-ofi.txt
 
-AM_CPPFLAGS = $(opal_ofi_CPPFLAGS)
+AM_CPPFLAGS = $(btl_ofi_CPPFLAGS)
 sources = \
     btl_ofi.h \
     btl_ofi_component.c \
@@ -58,12 +59,12 @@ mcacomponentdir = $(opallibdir)
 mcacomponent_LTLIBRARIES = $(component)
 mca_btl_ofi_la_SOURCES = $(component_sources)
 mca_btl_ofi_la_LDFLAGS = -module -avoid-version \
-                         $(opal_ofi_LDFLAGS)
+                         $(btl_ofi_LDFLAGS)
 mca_btl_ofi_la_LIBADD = $(top_builddir)/opal/lib@OPAL_LIB_NAME@.la \
     $(OPAL_TOP_BUILDDIR)/opal/mca/common/ofi/lib@OPAL_LIB_NAME@mca_common_ofi.la \
-    $(opal_ofi_LIBS)
+    $(btl_ofi_LIBS)
 
 noinst_LTLIBRARIES = $(lib)
 libmca_btl_ofi_la_SOURCES = $(lib_sources)
-libmca_btl_ofi_la_LDFLAGS = -module -avoid-version $(opal_ofi_LDFLAGS)
-libmca_btl_ofi_la_LIBS = $(opal_ofi_LIBS)
+libmca_btl_ofi_la_LDFLAGS = -module -avoid-version $(btl_ofi_LDFLAGS)
+libmca_btl_ofi_la_LIBS = $(btl_ofi_LIBS)

opal/mca/btl/ofi/configure.m4
@@ -1,4 +1,4 @@
-# -*- shell-script -*-
+# -*- autoconf -*-
 #
 # Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
 #                         University Research and Technology
@@ -16,6 +16,7 @@
 #                         All rights reserved.
 # Copyright (c) 2018      Intel, inc. All rights reserved
 #
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -30,23 +31,30 @@
 # support, otherwise executes action-if-not-found
 
 AC_DEFUN([MCA_opal_btl_ofi_CONFIG],[
-    OPAL_VAR_SCOPE_PUSH([opal_btl_ofi_happy CPPFLAGS_save])
+    OPAL_VAR_SCOPE_PUSH([btl_ofi_happy CPPFLAGS_save])
 
     AC_CONFIG_FILES([opal/mca/btl/ofi/Makefile])
 
     # Check for OFI
-    OPAL_CHECK_OFI
+    OPAL_CHECK_OFI([btl_ofi], [btl_ofi_happy=1], [btl_ofi_happy=0])
 
-    opal_btl_ofi_happy=0
-    AS_IF([test "$opal_ofi_happy" = "yes"],
-          [CPPFLAGS_save=$CPPFLAGS
-           CPPFLAGS="$opal_ofi_CPPFLAGS $CPPFLAGS"
-           AC_CHECK_DECL([FI_MR_VIRT_ADDR], [opal_btl_ofi_happy=1], [],
+    AS_IF([test ${btl_ofi_happy} -eq 1],
+          [CPPFLAGS_save=${CPPFLAGS}
+           OPAL_FLAGS_APPEND_UNIQ([CPPFLAGS], [${btl_ofi_CPPFLAGS}])
+           AC_CHECK_DECL([FI_MR_VIRT_ADDR], [], [btl_ofi_happy=0],
                          [#include <rdma/fabric.h>])
-           CPPFLAGS=$CPPFLAGS_save])
-    AS_IF([test $opal_btl_ofi_happy -eq 1],
+           CPPFLAGS=${CPPFLAGS_save}])
+
+    AS_IF([test ${btl_ofi_happy} -eq 1],
           [$1],
-          [$2])
+          [AS_IF([test -n "${with_ofi}" -a "${with_ofi}" != "no"],
+                 [AC_MSG_WARN([OFI libfabric support requested (via --with-ofi or --with-libfabric), but not found.])
+                  AC_MSG_ERROR([Cannot continue.])])
+           $2])
+
+    AC_SUBST([btl_ofi_CPPFLAGS])
+    AC_SUBST([btl_ofi_LDFLAGS])
+    AC_SUBST([btl_ofi_LIBS])
 
     OPAL_VAR_SCOPE_POP
 ])dnl

opal/mca/common/ofi/Makefile.am
@@ -15,6 +15,7 @@
 # Copyright (c) 2017      Los Alamos National Security, LLC.  All rights
 #                         reserved.
 # Copyright (c) 2019      Hewlett Packard Enterprise. All rights reserved.
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -29,7 +30,7 @@
 # Note that building this common component statically and linking
 # against other dynamic components is *not* supported!
 
-AM_CPPFLAGS = $(opal_ofi_CPPFLAGS)
+AM_CPPFLAGS = $(common_ofi_CPPFLAGS)
 
 dist_opaldata_DATA = help-common-ofi.txt
 
@@ -72,13 +73,13 @@ endif
 
 lib@OPAL_LIB_NAME@mca_common_ofi_la_SOURCES = $(headers) $(sources)
 lib@OPAL_LIB_NAME@mca_common_ofi_la_LDFLAGS = \
-        $(opal_ofi_LDFLAGS) \
+        $(common_ofi_LDFLAGS) \
         -version-info $(libmca_opal_common_ofi_so_version)
-lib@OPAL_LIB_NAME@mca_common_ofi_la_LIBADD = $(opal_ofi_LIBS)
+lib@OPAL_LIB_NAME@mca_common_ofi_la_LIBADD = $(common_ofi_LIBS)
 
 lib@OPAL_LIB_NAME@mca_common_ofi_noinst_la_SOURCES = $(headers) $(sources)
-lib@OPAL_LIB_NAME@mca_common_ofi_noinst_la_LDFLAGS = $(opal_ofi_LDFLAGS)
-lib@OPAL_LIB_NAME@mca_common_ofi_noinst_la_LIBADD = $(opal_ofi_LIBS)
+lib@OPAL_LIB_NAME@mca_common_ofi_noinst_la_LDFLAGS = $(common_ofi_LDFLAGS)
+lib@OPAL_LIB_NAME@mca_common_ofi_noinst_la_LIBADD = $(common_ofi_LIBS)
 
 # Conditionally install the header files
 

opal/mca/common/ofi/configure.m4
@@ -1,4 +1,4 @@
-# -*- shell-script -*-
+# -*- autoconf -*-
 #
 # Copyright (c) 2011-2013 NVIDIA Corporation.  All rights reserved.
 # Copyright (c) 2013      The University of Tennessee and The University
@@ -9,6 +9,7 @@
 # Copyright (c) 2017       Los Alamos National Security, LLC.  All rights
 #                         reserved.
 # Copyright (c) 2019      Hewlett Packard Enterprise. All rights reserved.
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -17,19 +18,19 @@
 #
 
 AC_DEFUN([MCA_opal_common_ofi_CONFIG],[
+    OPAL_VAR_SCOPE_PUSH([common_ofi_happy])
+
     AC_CONFIG_FILES([opal/mca/common/ofi/Makefile])
 
-    OPAL_CHECK_OFI
+    OPAL_CHECK_OFI([common_ofi], [common_ofi_happy=1], [common_ofi_happy=0])
 
-    # Note that $opal_common_ofi_happy is
-    # used in other configure.m4's to know if ofi configured
-    # successfully.
-    AS_IF([test "$opal_ofi_happy" = "yes"],
-          [opal_common_ofi_happy=yes
-           common_ofi_WRAPPER_EXTRA_LDFLAGS=$opal_ofi_LDFLAGS
-           common_ofi_WRAPPER_EXTRA_LIBS=$opal_ofi_LIBS
-           $1],
-          [opal_common_ofi_happy=no
-           $2])
+    AS_IF([test ${common_ofi_happy} -eq 1],
+          [$1],
+          [$2])
 
+    AC_SUBST([common_ofi_CPPFLAGS])
+    AC_SUBST([common_ofi_LDFLAGS])
+    AC_SUBST([common_ofi_LIBS])
+
+    OPAL_VAR_SCOPE_POP
 ])dnl

commit 7d8d130f5ef7b839824dbb24ef3dbbc1220022e8

btl/usnic: Fix broken build
b542a9c broke building usnic (at least when Libfabric isn't in default
search paths), because I removed the AC_SUBST of the legacy opal_ofi_*
flags.  I should have just deleted them and fixed up the last remaining
user, the usnic btl, so do that here.

Signed-off-by: Brian Barrett <bbarrett@amazon.com>

config/opal_check_ofi.m4
@@ -31,7 +31,7 @@ AC_DEFUN([OPAL_CHECK_OFI_VERSION_GE],[
     AC_CACHE_CHECK([if OFI API version number is >= version_pretty_print],
         [version_cache_var],
         [opal_ofi_ver_ge_save_CPPFLAGS=$CPPFLAGS
-         CPPFLAGS=$opal_ofi_CPPFLAGS
+         CPPFLAGS=$opal_ofi_internal_CPPFLAGS
 
          AC_COMPILE_IFELSE([AC_LANG_PROGRAM(
                 [[#include <rdma/fabric.h>
@@ -143,10 +143,8 @@ AC_DEFUN([OPAL_CHECK_OFI],[
     LDFLAGS=${opal_check_ofi_save_LDFLAGS}
     LIBS=${opal_check_ofi_save_LIBS}
 
-    dnl for backwards compatibility reasons
-    opal_ofi_CPPFLAGS="${$1_CPPFLAGS}"
-    opal_ofi_LDFLAGS="${$1_LDFLAGS}"
-    opal_ofi_LIBS="${$1_LIBS}"
+    dnl for version compare tests
+    opal_ofi_internal_CPPFLAGS="${$1_CPPFLAGS}"
 
     OPAL_SUMMARY_ADD([Transports], [OpenFabrics OFI Libfabric], [], [${$1_SUMMARY}])
 

opal/mca/btl/usnic/Makefile.am
@@ -18,6 +18,7 @@
 #                         reserved.
 # Copyright (c) 2019      Research Organization for Information Science
 #                         and Technology (RIST).  All rights reserved.
+# Copyright (c) 2022      Amazon.com, Inc. or its affiliates.  All Rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -25,7 +26,7 @@
 # $HEADER$
 #
 
-AM_CPPFLAGS = $(opal_ofi_CPPFLAGS) -DOMPI_LIBMPI_NAME=\"$(OMPI_LIBMPI_NAME)\"
+AM_CPPFLAGS = $(btl_usnic_CPPFLAGS) -DOMPI_LIBMPI_NAME=\"$(OMPI_LIBMPI_NAME)\"
 
 EXTRA_DIST = README.md README.test
 
@@ -90,18 +91,18 @@ mcacomponent_LTLIBRARIES = $(component)
 mca_btl_usnic_la_SOURCES = $(component_sources)
 mca_btl_usnic_la_LDFLAGS = \
         $(opal_btl_usnic_LDFLAGS) \
-        $(opal_ofi_LDFLAGS) \
+        $(btl_usnic_LDFLAGS) \
         -module -avoid-version
 mca_btl_usnic_la_LIBADD = $(top_builddir)/opal/lib@OPAL_LIB_NAME@.la \
-        $(opal_ofi_LIBS)
+        $(btl_usnic_LIBS)
 
 noinst_LTLIBRARIES = $(lib)
 libmca_btl_usnic_la_SOURCES = $(lib_sources)
 libmca_btl_usnic_la_LDFLAGS = \
         $(opal_btl_usnic_LDFLAGS) \
-        $(opal_ofi_LDFLAGS) \
+        $(btl_usnic_LDFLAGS) \
         -module -avoid-version
-libmca_btl_usnic_la_LIBADD = $(opal_ofi_LIBS)
+libmca_btl_usnic_la_LIBADD = $(btl_usnic_LIBS)
 
 if OPAL_BTL_USNIC_BUILD_UNIT_TESTS
 usnic_btl_run_tests_CPPFLAGS = $(AM_CPPFLAGS) \

opal/mca/btl/usnic/configure.m4
@@ -110,12 +110,16 @@ AC_DEFUN([_OPAL_BTL_USNIC_DO_CONFIG],[
     # Make sure we can find the OFI libfabric usnic extensions header
     AS_IF([test "$opal_btl_usnic_happy" = "yes" ],
           [opal_btl_usnic_CPPFLAGS_save=$CPPFLAGS
-           CPPFLAGS="$opal_ofi_CPPFLAGS $CPPFLAGS"
+           CPPFLAGS="$btl_usnic_CPPFLAGS $CPPFLAGS"
            AC_CHECK_HEADER([rdma/fi_ext_usnic.h],
                             [],
                             [opal_btl_usnic_happy=no])
            CPPFLAGS=$opal_btl_usnic_CPPFLAGS_save
           ])
+
+    AC_SUBST([btl_usnic_CPPFLAGS])
+    AC_SUBST([btl_usnic_LDFLAGS])
+    AC_SUBST([btl_usnic_LIBS])
 
     # All done
     AS_IF([test "$opal_btl_usnic_happy" = "yes"],