8fe26b48743a8147f9f4ec6919564a2aab47965b
Aurelien Bouteiller <bouteill@icl.utk.edu>
2022-06-22 19:36:10 -0400

    ulfm: new functions comm_get_failed, comm_ack_failed C bindings
    Add error management to ack_failed_internal
    
    Use comm->num_acked to decide if any_source should be re-enabled
    
    More compatibility with mixed use of failure_ack and ack_failed
    
    Additional shortcut: bypass computing num_acked when no new faults
    
    Recompute num_acked when the v1 interface got used inbetween
    
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>
    
    Copyrights
    
    The group needs to be initialized before the first goto.
    
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>
    
    Add #defines for the new API.
    
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>

--- ompi/mpiext/ftmpi/c/mpiext_ftmpi_c.h
+++ ompi/mpiext/ftmpi/c/mpiext_ftmpi_c.h
@@ -27,6 +27,8 @@ OMPI_DECLSPEC int MPIX_Comm_is_revoked(MPI_Comm comm, int *flag);
 OMPI_DECLSPEC int MPIX_Comm_shrink(MPI_Comm comm, MPI_Comm *newcomm);
 OMPI_DECLSPEC int MPIX_Comm_failure_ack(MPI_Comm comm);
 OMPI_DECLSPEC int MPIX_Comm_failure_get_acked(MPI_Comm comm, MPI_Group *failedgrp);
+OMPI_DECLSPEC int MPIX_Comm_get_failed(MPI_Comm comm, MPI_Group *failedgroup);
+OMPI_DECLSPEC int MPIX_Comm_ack_failed(MPI_Comm comm, int num_to_ack, int *num_acked);
 OMPI_DECLSPEC int MPIX_Comm_agree(MPI_Comm comm, int *flag);
 OMPI_DECLSPEC int MPIX_Comm_iagree(MPI_Comm comm, int *flag, MPI_Request *request);
 
@@ -35,9 +37,14 @@ OMPI_DECLSPEC int PMPIX_Comm_is_revoked(MPI_Comm comm, int *flag);
 OMPI_DECLSPEC int PMPIX_Comm_shrink(MPI_Comm comm, MPI_Comm *newcomm);
 OMPI_DECLSPEC int PMPIX_Comm_failure_ack(MPI_Comm comm);
 OMPI_DECLSPEC int PMPIX_Comm_failure_get_acked(MPI_Comm comm, MPI_Group *failedgrp);
+OMPI_DECLSPEC int PMPIX_Comm_get_failed(MPI_Comm comm, MPI_Group *failedgroup);
+OMPI_DECLSPEC int PMPIX_Comm_ack_failed(MPI_Comm comm, int num_to_ack, int *num_acked);
 OMPI_DECLSPEC int PMPIX_Comm_agree(MPI_Comm comm, int *flag);
 OMPI_DECLSPEC int PMPIX_Comm_iagree(MPI_Comm comm, int *flag, MPI_Request *request);
 
 #include <stdbool.h>
 OMPI_DECLSPEC int OMPI_Comm_failure_inject(MPI_Comm comm, bool notify);
+/* Provide defines to facilitate the detection of the new API */
+#define OMPI_HAVE_MPIX_COMM_GET_FAILED 1
+#define OMPI_HAVE_MPIX_COMM_ACK_FAILED 1
 
