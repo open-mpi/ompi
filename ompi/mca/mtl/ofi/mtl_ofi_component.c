29532368ecbb4dc337b6dd671ff829d2bc8bbe0b
William Zhang <wilzhang@amazon.com>
2022-11-07 12:16:33 -0800

    mtl/ofi: Remove trailing whitespaces
    Signed-off-by: William Zhang <wilzhang@amazon.com>

--- ompi/mca/mtl/ofi/mtl_ofi_component.c
+++ ompi/mca/mtl/ofi/mtl_ofi_component.c
@@ -1066,7 +1066,7 @@ select_prov:
     ompi_mtl_ofi.any_addr = FI_ADDR_UNSPEC;
     ompi_mtl_ofi.is_initialized = false;
     ompi_mtl_ofi.has_posted_initial_buffer = false;
-    
+
     ompi_mtl_ofi.base.mtl_flags |= MCA_MTL_BASE_FLAG_SUPPORTS_EXT_CID;
 
     return &ompi_mtl_ofi.base;
