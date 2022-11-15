a74f31df1cab9fd98790f722de38b34c1fc344f0
Howard Pritchard <howardp@lanl.gov>
2022-11-15 11:41:01 -0800

    OFI/MTL: bypass HMEM mem reg for CXI provider
    The code in the OFI MTL to register acclerator memory doesn't work well with providers like CXI which require the libfabric
    consumer to specify the requested_key field in the fi_mr_attr arg to fi_mr_regattr.  As it turns out there's a simpe workaround
    though with this provider as it actually doesn't need accelerator memory to be registered for FI_MSG/FI_TAGGED type transfers.
    
    This patch restores support for transfers to/from accelerator memory using the OFI MTL on SS11 (CXI provider).
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- ompi/mca/mtl/ofi/mtl_ofi_types.h
+++ ompi/mca/mtl/ofi/mtl_ofi_types.h
@@ -2,6 +2,8 @@
  * Copyright (c) 2013-2018 Intel, Inc. All rights reserved
  *
  * Copyright (c) 2015 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2022 Triad National Security, LLC. All rights
+ *                    reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -97,6 +99,7 @@ typedef struct mca_mtl_ofi_module_t {
 
     bool is_initialized;
     bool has_posted_initial_buffer;
+    bool hmem_needs_reg;
 
 } mca_mtl_ofi_module_t;
 
