88f4074648c214e89cd66a5e00d3922fcc732031
Edgar Gabriel <edgar.gabriel@amd.com>
2022-12-04 11:23:59 -0600

    fs/lustre: fix assignment of info objects to lustre args
    It seems that commit 1631f387704641dd7939cbce156a47ea8eda39eb
    accidentally reverted the two info objects setting stripe size and stripe
    width for lustre file systems.
    
    Signed-off-by: Edgar Gabriel <edgar.gabriel@amd.com>
    (cherry picked from commit 2778e26869955bfd90ab8cac64613cfb6ad380b5)

--- ompi/mca/fs/lustre/fs_lustre_file_open.c
+++ ompi/mca/fs/lustre/fs_lustre_file_open.c
@@ -76,7 +76,7 @@ mca_fs_lustre_file_open (struct ompi_communicator_t *comm,
     perm = mca_fs_base_get_file_perm(fh);
     amode = mca_fs_base_get_file_amode(fh->f_rank, access_mode);
 
-    opal_info_get (info, "striping_factor", MPI_MAX_INFO_VAL, char_stripe, &flag);
+    opal_info_get (info, "striping_unit", MPI_MAX_INFO_VAL, char_stripe, &flag);
     if ( flag ) {
         sscanf ( char_stripe, "%d", &fs_lustre_stripe_size );
     }
@@ -88,7 +88,7 @@ mca_fs_lustre_file_open (struct ompi_communicator_t *comm,
         }
     }
     
-    opal_info_get (info, "striping_unit", MPI_MAX_INFO_VAL, char_stripe, &flag);
+    opal_info_get (info, "striping_factor", MPI_MAX_INFO_VAL, char_stripe, &flag);
     if ( flag ) {
         sscanf ( char_stripe, "%d", &fs_lustre_stripe_width );
     }
