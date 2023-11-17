b52cbc880a3f06aa5d45b2364e860169115f5f36
Wenduo Wang <wenduwan@amazon.com>
2023-11-17 11:22:43 -0800

    Revert "opal/accelerator: add destroy_event API"
    This reverts commit 32333ab6f25a2c403f9f051f4789ff84716358b0.

--- opal/mca/accelerator/accelerator.h
+++ opal/mca/accelerator/accelerator.h
@@ -200,8 +200,7 @@ typedef int (*opal_accelerator_base_module_stream_wait_event_fn_t)(
  * Creates an event. An event is a synchronization marker that can be
  * appended to a stream to monitor device progress or synchronize the
  * corresponding stream. This function will allocate memory for the object.
- * To release the object memory and associated resources
- * call opal_accelerator_base_module_destroy_event_fn_t
+ * To release the memory, call OBJ_RELEASE(*event);
  *
  * @param[IN] dev_id         Associated device for the event or
  *                           MCA_ACCELERATOR_NO_DEVICE_ID
@@ -211,18 +210,6 @@ typedef int (*opal_accelerator_base_module_stream_wait_event_fn_t)(
  */
 typedef int (*opal_accelerator_base_module_create_event_fn_t)(
     int dev_id, opal_accelerator_event_t **event);
-
-/**
- * Destroys an event and release the object memory.
- * This function should return immediately, but the event may complete
- * and associated resources are released later.
- *
- * @param[IN] event          Event to destroy
- *
- * @return                   OPAL_SUCCESS or error status on failure.
- */
-typedef int (*opal_accelerator_base_module_destroy_event_fn_t)(
-    opal_accelerator_event_t *event);
 
 /**
  * Records an event on a stream. An event recorded on the stream is
@@ -454,7 +441,6 @@ typedef struct {
     opal_accelerator_base_module_synchronize_stream_fn_t synchronize_stream;
     opal_accelerator_base_module_stream_wait_event_fn_t stream_wait_event;
     opal_accelerator_base_module_create_event_fn_t create_event;
-    opal_accelerator_base_module_destroy_event_fn_t destroy_event;
     opal_accelerator_base_module_record_event_fn_t record_event;
     opal_accelerator_base_module_query_event_fn_t query_event;
 
