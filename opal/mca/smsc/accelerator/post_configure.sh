ed363c1a538ccde8822c3cf0edb231455f9b9872
Edgar Gabriel <Edgar.Gabriel@amd.com>
2024-03-12 07:18:18 -0700

    smsc/accelerator: add new smsc component
    - add smsc component based on the accelerator API IPC functionality.
    - adjust smsc base selection logic to enable a pass-through mechanism
      with the new smsc/accelerator component
    
    Signed-off-by: Edgar Gabriel <Edgar.Gabriel@amd.com>

---
+++ opal/mca/smsc/accelerator/post_configure.sh
@@ -0,0 +1,1 @@
+DIRECT_CALL_HEADER="opal/mca/smsc/accelerator/smsc_accelerator.h"
--- opal/mca/smsc/base/base.h
+++ opal/mca/smsc/base/base.h
@@ -12,9 +12,18 @@
 #ifndef OPAL_MCA_SMSC_BASE_BASE_H
 #define OPAL_MCA_SMSC_BASE_BASE_H
 
+#include "ompi_config.h"
+
 #include "opal/mca/smsc/smsc.h"
 
 extern mca_base_framework_t opal_smsc_base_framework;
+
+struct mca_smsc_base_component_t {
+    opal_list_item_t super;
+    mca_smsc_component_t *smsc_component;
+};
+typedef struct mca_smsc_base_component_t mca_smsc_base_component_t;
+OMPI_DECLSPEC OBJ_CLASS_DECLARATION(mca_smsc_base_component_t);
 
 int mca_smsc_base_select(void);
 void mca_smsc_base_register_default_params(mca_smsc_component_t *component, int default_priority);
