2ea6f8c59a02fc1e0a55ea8e6d60803d24a3be83
Jake Tronge <jtronge@lanl.gov>
2024-08-12 09:51:15 -0600

    Generate mpi_f08 bindings and add CFI support
    This updates fortran/use-mpi-f08 to generate most of the Fortran
    bindings from a script and template files. It also adds support for
    Fortran TS 29113 when possible, allowing for better Fortran array
    handling that matches the standard.
    
    The C files were imported from PR #10302 and converted to templates to
    be fed into the binding script.
    
    Co-authored-by: Gilles Gouaillardet <gilles@rist.or.jp>
    Co-authored-by: Howard Pritchard <howardp@lanl.gov>
    Signed-off-by: Jake Tronge <jtronge@lanl.gov>

---
+++ ompi/mpi/fortran/use-mpi-f08/base/ompi_type_get_envelope_f_c.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2011-2012 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2015      Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2024      Triad National Security, LLC. All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+
+#include "ompi_config.h"
+
+#include "ompi/datatype/ompi_datatype.h"
+#include "ompi/mpi/fortran/base/fint_2_int.h"
+
+void ompi_type_get_envelope_f_c(MPI_Fint *type, MPI_Count *num_integers,
+			     MPI_Count *num_addresses,
+			     MPI_Count *num_large_counts,
+			     MPI_Count *num_datatypes, MPI_Fint *combiner,
+			     MPI_Fint *ierr);
+void ompi_type_get_envelope_f_c(MPI_Fint *type, MPI_Count *num_integers,
+			     MPI_Count *num_addresses,
+			     MPI_Count *num_large_counts,
+			     MPI_Count *num_datatypes, MPI_Fint *combiner,
+			     MPI_Fint *ierr)
+{
+    int c_ierr;
+    MPI_Datatype c_type = PMPI_Type_f2c(*type);
+    OMPI_SINGLE_NAME_DECL(num_integers);
+    OMPI_SINGLE_NAME_DECL(num_addresses);
+    OMPI_SINGLE_NAME_DECL(num_datatypes);
+    OMPI_SINGLE_NAME_DECL(combiner);
+
+    c_ierr = PMPI_Type_get_envelope_c(c_type,
+                                   OMPI_SINGLE_NAME_CONVERT(num_integers),
+                                   OMPI_SINGLE_NAME_CONVERT(num_addresses),
+                                   OMPI_SINGLE_NAME_CONVERT(num_large_counts),
+                                   OMPI_SINGLE_NAME_CONVERT(num_datatypes),
+                                   OMPI_SINGLE_NAME_CONVERT(combiner));
+    if (NULL != ierr) *ierr = OMPI_INT_2_FINT(c_ierr);
+
+    if (MPI_SUCCESS == c_ierr) {
+        OMPI_SINGLE_INT_2_FINT(num_integers);
+        OMPI_SINGLE_INT_2_FINT(num_addresses);
+        OMPI_SINGLE_INT_2_FINT(num_datatypes);
+        OMPI_SINGLE_INT_2_FINT(combiner);
+    }
+}
