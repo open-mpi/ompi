b10df4756e3401b366898f2d10f157e201bedd65
Jake Tronge <jtronge@lanl.gov>
2024-08-12 09:49:34 -0600

    Generate interfaces for C with bigcount
    This adds scripts for generating the C API bindings from template files,
    while also generating bigcount interfaces for those that require them.
    The binding script also include initial support for the mpi_f08 Fortran
    bindings, but doesn't yet make any changes to fortran/use-mpi-f08
    
    Python >=3.6 is required for running these scripts, which is only
    necessary when the binding files have not already been generated.
    Users of the distribution tarball should not need to generate these
    files and thus should not require Python.
    
    Note that there are additional changes needed to Open MPI for bigcount support.
    One is that the datatype system needs to be embiggened.  This
    PR includes workarounds for this lack of support that will need to
    be changed once the datatype system is embiggened.  These areas
    of code are marked with TODO:BIGCOUNT.
    
    Co-authored-by: mphinney1100 <mphinney@lanl.gov>
    Co-authored-by: Howard Pritchard <hppritcha@gmail.com>
    Signed-off-by: Jake Tronge <jtronge@lanl.gov>

---
+++ ompi/mpi/bindings/ompi_bindings/util.py
@@ -0,0 +1,146 @@
+# Copyright (c) 2024      Triad National Security, LLC. All rights
+#                         reserved.
+#
+# $COPYRIGHT$
+#
+# Additional copyrights may follow
+#
+# $HEADER$
+"""Utility code for OMPI binding generation."""
+import textwrap
+
+
+class OutputFile:
+    """Output file of script."""
+
+    def __init__(self, fp):
+        self.fp = fp
+
+    def dump(self, *pargs, **kwargs):
+        print(*pargs, **kwargs, file=self.fp)
+
+
+def prepare_text(text):
+    """Prepare text to be output, removing extra lines and whitespace."""
+    text = textwrap.dedent(text)
+    lines = text.split('\n')
+    out_lines = []
+    new_lines = 0
+    for line in lines:
+        line = line.rstrip()
+        # Only allow one blank line
+        if not line:
+            new_lines += 1
+        else:
+            new_lines = 0
+        if new_lines > 1:
+            continue
+        out_lines.append(line)
+    return '\n'.join(line for line in lines if line.strip())
+
+
+class BindingError(Exception):
+    """Thrown when a binding error is encountered."""
+
+
+def validate_allowed_keys(keys, req_keys, type_name, param_name):
+    """Validate allowed keys for a type, raising an error on failure."""
+    missing_keys = [key for key in req_keys if key not in keys]
+    invalid_keys = [key for key in keys if key not in req_keys]
+    init_message = f'Param {param_name} with type {type_name}'
+    if missing_keys and invalid_keys:
+        raise BindingError(f'{init_message} has missing keys ({missing_keys}) and invalid keys ({invalid_keys})')
+    elif missing_keys:
+        raise BindingError(f'{init_message} has missing keys: {missing_keys}')
+    elif invalid_keys:
+        raise BindingError(f'{init_message} has invalid keys: {invalid_keys}')
+
+
+def ext_api_func_name(fn_name, bigcount=False):
+    """Produce the external MPI API function name."""
+    suffix = '_c' if bigcount else ''
+    return f'MPI_{fn_name.capitalize()}{suffix}'
+
+
+def ext_api_func_name_profile(fn_name, bigcount=False):
+    """Produce the external PMPI API function name."""
+    return f'P{ext_api_func_name(fn_name, bigcount)}'
+
+
+def fortran_f08_name(fn_name, bigcount=False):
+    """Produce the final f08 name from base_name."""
+    suffix = '_c' if bigcount else ''
+    return f'MPI_{fn_name.capitalize()}_f08{suffix}'
+
+
+def break_param_lines_fortran(start, params, end):
+    """Break paramters for a fortran call onto multiple lines.
+
+    This is often necessary to avoid going over the max line length of 132
+    characters.
+    """
+    assert len(params) > 1, 'expected more than one parameter'
+    indent = len(start) * ' '
+    lines = [f'{start}{params[0]},']
+    for param in params[1:-1]:
+        lines.append(f'{indent}{param},')
+    last_line = f'{indent}{params[-1]}{end}'
+    max_len = max(len(line) for line in lines)
+    max_len = max(max_len, len(last_line))
+    result_lines = []
+    for line in lines:
+        spaces = (max_len - len(line) + 1) * ' '
+        result_lines.append(f'{line}{spaces}&')
+    result_lines.append(last_line)
+    return result_lines
+
+
+def indent_lines(lines, tab, start=0):
+    """Crude pretty-printing function."""
+    new_lines = []
+    indent_count = start
+    for line in lines:
+        # Closing bracket
+        if '}' in line:
+            indent_count -= 1
+
+        prefix = indent_count * tab
+        new_lines.append(f'{prefix}{line}')
+
+        # Opening bracket
+        if '{' in line:
+            indent_count += 1
+    return new_lines
+
+
+def mpi_fn_name_from_base_fn_name(name):
+    """Convert from a base name to the standard 'MPI_*' name."""
+    return f'MPI_{name.capitalize()}'
+
+
+def abi_internal_name(extname):
+    """Convert from the ABI external name to an internal name.
+
+    Used to avoid conflicts with existing MPI names.
+    """
+    return f'{extname}_ABI_INTERNAL'
+
+
+BIGCOUNT_TYPE_NAMES = [
+    'COUNT',
+    'COUNT_ARRAY',
+    'DISP',
+    'DISP_ARRAY',
+    'COUNT_OUT',
+    'AINT_COUNT',
+    'AINT_COUNT_ARRAY',
+    'AINT_COUNT_OUT',
+    'INT_AINT_OUT',
+    'USER_FUNCTION',
+    'DATAREP_CONVERSION_FUNCTION',
+]
+
+
+def prototype_has_bigcount(prototype):
+    """Should this prototype have a bigcount version?"""
+    return any(param.type_name in BIGCOUNT_TYPE_NAMES for param in prototype.params)
