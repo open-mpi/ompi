4c7d82814f7c15fba4882bc5b8b028dfa69f36f2
Wenduo Wang <wenduwan@amazon.com>
2024-09-05 22:19:08 +0000

    Revert "hook comm_method: fix compiler warnings"
    This reverts commit 3e2da6eb41522b7cb64785ecc5debd95543ccdd2.

--- ompi/mca/hook/comm_method/hook_comm_method_fns.c
+++ ompi/mca/hook/comm_method/hook_comm_method_fns.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2016-2022 IBM Corporation. All rights reserved.
- * Copyright (c) 2024      Jeffrey M. Squyres.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -16,7 +15,6 @@
 #include <dlfcn.h>
 #endif
 
-#include "opal/util/string_copy.h"
 #include "ompi/communicator/communicator.h"
 #include "ompi/mca/pml/pml.h"
 #include "opal/mca/btl/btl.h"
@@ -107,21 +105,21 @@ comm_method_string(MPI_Comm comm, int rank, int *comm_mode) {
             if (comm_mode) { *comm_mode = MODE_IS_BTL; }
             btl = lookup_btl_name_for_send(comm, rank);
             if (NULL == btl) {
-                opal_string_copy(string, "n/a", COMM_METHOD_STRING_SIZE);
+                strncpy(string, "n/a", COMM_METHOD_STRING_SIZE);
             } else {
-                opal_string_copy(string, btl, COMM_METHOD_STRING_SIZE);
+                strncpy(string, btl, COMM_METHOD_STRING_SIZE);
             }
         }
         else if (p && 0==strncmp("cm", p, 3)) {  // MTL
             if (comm_mode) { *comm_mode = MODE_IS_MTL; }
-            opal_string_copy(string, lookup_mtl_name(), COMM_METHOD_STRING_SIZE);
+            strncpy(string, lookup_mtl_name(), COMM_METHOD_STRING_SIZE);
         } else {                        // PML
             if (comm_mode) { *comm_mode = MODE_IS_PML; }
             if (p) {
-                opal_string_copy(string, p, COMM_METHOD_STRING_SIZE);
+                strncpy(string, p, COMM_METHOD_STRING_SIZE);
             }
             else {
-                opal_string_copy(string, "n/a", COMM_METHOD_STRING_SIZE);
+                strncpy(string, "n/a", COMM_METHOD_STRING_SIZE);
             }
         }
     }
@@ -693,7 +691,7 @@ ompi_report_comm_methods(int called_from_location)
                 p = str;
                 for (k=0; k<nleaderranks; ++k) {
                     char *method_string;
-                    char ucx_label[20];
+                    char ucx_label[10];
                     
                     method_string = comm_method_to_string(method[i * nleaderranks + k]);
                     if (0 == strncmp(method_string, UCX_TAG, strlen(UCX_TAG))) {
