c6d6bf0c4e6783b75cea326721a687421da48aa5
Axel Schneewind <axel.schneewind@hlrs.de>
2025-01-10 11:54:11 +0100

    implement a simple aggregation scheme
    This aggregation scheme is intended to allow OpenMPI to transfer larger messages
    if the user-reported partitions are too small or too high in number.
    This is achieved by using an internal partitioning where each internal (transfer)
    partition corresponds to one or multiple user-reported partitions.
    
    The scheme provides a method to mark a user partition as ready, that optionally
    returns a transfer partition that is ready.
    
    This is implemented by associating each transfer partition with an atomic counter,
    tracking the number of corresponding pready-calls already made.
    If a counter reaches the number of corresponding user-partitions, the corresponding
    transfer partition is returned.
    
    This implementation is thread-safe.
    
    Signed-off-by: Axel Schneewind <axel.schneewind@hlrs.de>

---
+++ ompi/mca/part/persist_aggregated/schemes/part_persist_aggregated_scheme_regular.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024      High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
+#include "part_persist_aggregated_scheme_regular.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+// converts the index of a public partition to the index of its corresponding internal partition
+static int internal_partition(struct part_persist_aggregation_state *state, int public_part)
+{
+    return public_part / state->aggregation_count;
+}
+
+void part_persist_aggregate_regular_init(struct part_persist_aggregation_state *state,
+                                         int internal_partition_count, int factor,
+                                         int last_internal_partition_size)
+{
+    state->public_partition_count = (internal_partition_count - 1) * factor
+                                    + last_internal_partition_size;
+    state->internal_partition_count = internal_partition_count;
+
+    // number of user-partitions per internal partition (except for the last one)
+    state->aggregation_count = factor;
+    // number of user-partitions corresponding to the last internal partition
+    state->last_internal_partition_size = last_internal_partition_size;
+
+    // initialize counters
+    state->public_parts_ready = (opal_atomic_int32_t *) calloc(state->internal_partition_count,
+                                                               sizeof(opal_atomic_uint32_t));
+}
+
+void part_persist_aggregate_regular_reset(struct part_persist_aggregation_state *state)
+{
+    // reset flags
+    if (NULL != state->public_parts_ready) {
+        memset((void *) state->public_parts_ready, 0,
+               state->internal_partition_count * sizeof(opal_atomic_uint32_t));
+    }
+}
+
+static inline int is_last_partition(struct part_persist_aggregation_state *state, int partition)
+{
+    return (partition == state->internal_partition_count - 1);
+}
+
+static inline int num_public_parts(struct part_persist_aggregation_state *state, int partition)
+{
+    return is_last_partition(state, partition) ? state->last_internal_partition_size
+                                               : state->aggregation_count;
+}
+
+void part_persist_aggregate_regular_pready(struct part_persist_aggregation_state *state,
+                                           int partition, int *available_partition)
+{
+    int internal_part = internal_partition(state, partition);
+
+    // this is the new value (after adding)
+    int count = opal_atomic_add_fetch_32(&state->public_parts_ready[internal_part], 1);
+
+    // push to buffer if internal partition is ready
+    if (count == num_public_parts(state, internal_part)) {
+        *available_partition = internal_part;
+    } else {
+        *available_partition = -1;
+    }
+}
+
+void part_persist_aggregate_regular_free(struct part_persist_aggregation_state *state)
+{
+    if (state->public_parts_ready != NULL)
+        free((void *) state->public_parts_ready);
+    state->public_parts_ready = NULL;
+}
