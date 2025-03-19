5384920ceab7c589b2896f80ce287d48a6ada7ad
George Bosilca <gbosilca@nvidia.com>
2025-03-19 17:29:05 -0400

    Better prefix extraction
    For the libdir it now matched the libdir prefix from the installdirs.h
    in order to find the real prefix.
    
    It does the same for executable allowing us to execute our compiler
    wrappers and other tools from a relocated directory. The problem here
    is that all our internal executbales are statically linked against
    libopal_core, we we will find the basic symbols not in a shared library
    but directly in the executable.
    
    This works fine, but we need a way to prevent this mechanism from
    triggering on a statically build user application.
    
    Signed-off-by: George Bosilca <gbosilca@nvidia.com>

--- opal/mca/installdirs/runtime/opal_installdirs_runtime.c
+++ opal/mca/installdirs/runtime/opal_installdirs_runtime.c
@@ -41,6 +41,7 @@ opal_installdirs_base_component_t mca_installdirs_runtime_component = {
 
 #include <dlfcn.h>
 #include "opal/util/basename.h"
+#include "opal/mca/installdirs/install_dirs.h"
 
 /**
  * We are trying to solve a particular use case here, when the entire install tree
@@ -51,42 +52,60 @@ static int installdirs_runtime_open(void)
 {
     Dl_info info;
     void* opal_fct;
+    char* libname = NULL;
+    const char* base_prefix_path = OPAL_LIBDIR;
 
     /* Casting from void* to fct pointer according to POSIX.1-2001 and POSIX.1-2008 */
     *(void **)&opal_fct = dlsym(RTLD_DEFAULT, "opal_init_util");
 
     if( 0 == dladdr(opal_fct, &info) ) {
-        /* Can't find the symbol */
-        return OPAL_ERROR;
+        goto bail_out_with_no_data;
     }
 
     /* If this build was both static and shared then this compoenent will be build and will exists
      * even in the static library. We need to prevent setting a prefix for the OMPI library that
      * is actually the application path. Check, if the name points to a library.
      */
-    char* libname = opal_basename(info.dli_fname);
+    libname = opal_basename(info.dli_fname);
     if( strncmp(libname, "lib", 3)) {  /* not a shared library */
-        free(libname);
-        return OPAL_ERROR;
-    }
+        base_prefix_path = OPAL_BINDIR;
+    } else {
 #if defined(OPAL_LIB_NAME)
-    /* Extra check using the installed name of the OPAL library */
-    if( strncmp(libname+3, OPAL_LIB_NAME, strlen(OPAL_LIB_NAME)) ) {  /* not a shared library */
-        free(libname);
-        return OPAL_ERROR;
-    }
+        /* Extra check using the installed name of the OPAL library */
+        if( strncmp(libname+3, OPAL_LIB_NAME, strlen(OPAL_LIB_NAME)) ) {  /* not a shared library */
+            goto bail_out_with_no_data;
+        }
 #endif  /* defined(OPAL_LIB_NAME) */
+    }
     /* Remove the shared library name and it's first dirname to obtain a prefix. This
      * is true in most cases, especially when the install directory was just moved
      * moved around, but it is not necessarily always true.
      */
-    char* dname = opal_dirname(info.dli_fname);
-    char* prefix = opal_dirname(dname);
+    char *prefix = NULL, *dname = opal_dirname(info.dli_fname);
+    int dname_idx = strlen(dname), dname_token = dname_idx;
+    for( int i = strlen(base_prefix_path); (i > 0) && (dname_idx > 0); i-- ) {
+        if( dname[dname_idx] != base_prefix_path[i] ) {
+            dname[dname_token] = '\0';
+            prefix = dname;
+            dname = NULL;  /* the string is now attached to the component, prevent it from being freed */
+            break;
+        }
+        if( dname[dname_idx] == OPAL_PATH_SEP[0] )
+            dname_token = dname_idx;
+        dname_idx--;
+    }
+
+    mca_installdirs_runtime_component.install_dirs_data.prefix = prefix;
+
+    /* If we goto here, there is some error. Unfortunately, we can't return an error from
+     * this function, the MCA infrastructure is not yet completely setup, and a call to
+     * mca_base_component_close will break.
+     * So, return success but provide no meaningfull data in the component.
+     */
+  bail_out_with_no_data:
 
     free(libname);
     free(dname);
-
-    mca_installdirs_runtime_component.install_dirs_data.prefix = prefix;
 
     return OPAL_SUCCESS;
 }
