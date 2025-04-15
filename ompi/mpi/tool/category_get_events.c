59e4e4e31d52d5cedacf1d4dbc008e4ae5b351ee
Howard Pritchard <howardp@lanl.gov>
2025-04-15 12:48:54 -0600

    MPI_T_Events: but wait there is more!
    Turns out I forgot to run the pcvs checker when adding MPI_T event stubs
    in PR #13086 and missed a couple of events related functions.  Also it
    looks like these were not included in PR #8057.
    
    With this patch, the PCVS MPI API checker passes for MPI 4.0 standard.
    
    The PCVS MPI API checker is described here https://dl.acm.org/doi/abs/10.1145/3615318.3615329
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

---
+++ ompi/mpi/tool/category_get_events.c
@@ -0,0 +1,40 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2012-2013 Los Alamos National Security, LLC. All rights
+ *                         reserved.
+ * Copyright (c) 2014 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2017      IBM Corporation. All rights reserved.
+ * Copyright (c) 2020      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2021      Amazon.com, Inc. or its affiliates.  All Rights
+ *                         reserved.
+ * Copyright (c) 2025      Triad National Security, LLC. All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include "ompi/mpi/tool/mpit-internal.h"
+
+#if OMPI_BUILD_MPI_PROFILING
+#if OPAL_HAVE_WEAK_SYMBOLS
+#pragma weak MPI_T_category_get_events = PMPI_T_category_get_events
+#endif
+#define MPI_T_category_get_events PMPI_T_category_get_events
+#endif
+
+int MPI_T_category_get_events(int cat_index, int len, int indices[])
+{
+    int rc = MPI_SUCCESS;
+
+    if (!mpit_is_initialized ()) {
+        return MPI_T_ERR_NOT_INITIALIZED;
+    }
+    return rc;
+}
---
+++ ompi/mpi/tool/category_get_num_events.c
@@ -0,0 +1,45 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2012-2013 Los Alamos National Security, LLC. All rights
+ *                         reserved.
+ * Copyright (c) 2014 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2017      IBM Corporation. All rights reserved.
+ * Copyright (c) 2020      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2021      Amazon.com, Inc. or its affiliates.  All Rights
+ *                         reserved.
+ * Copyright (c) 2025      Triad National Security, LLC. All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include "ompi/mpi/tool/mpit-internal.h"
+
+#if OMPI_BUILD_MPI_PROFILING
+#if OPAL_HAVE_WEAK_SYMBOLS
+#pragma weak MPI_T_category_get_num_events = PMPI_T_category_get_num_events
+#endif
+#define MPI_T_category_get_num_events PMPI_T_category_get_num_events
+#endif
+
+int MPI_T_category_get_num_events (int cat_index, int *num_events)
+{
+    if (!mpit_is_initialized ()) {
+        return MPI_T_ERR_NOT_INITIALIZED;
+    }
+
+    if (MPI_PARAM_CHECK && NULL == num_events) {
+        return MPI_T_ERR_INVALID;
+    }
+
+    *num_events = 0;
+
+    return MPI_SUCCESS;
+}
