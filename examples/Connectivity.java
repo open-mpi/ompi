448388be7202935aeefbc6b9342d91413df03006
Vasista Dhyasani <vasi.dhyasani@gmail.com>
2025-04-18 15:57:05 -0700

    Create Connectivity.java and be able to compile/run java files
    Signed-off-by: vdhyasani17 <vasi.dhyasani@gmail.com>

---
+++ examples/Connectivity.java
@@ -0,0 +1,13 @@
+/*
+ * File for testing connectivity for the Java interface
+ * WIP
+ */
+
+import mpi.*;
+
+class Connectivity {
+    static public void main(String args[]) {
+        System.out.println("Java Connectivity Test");
+    }
+
+}
