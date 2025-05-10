03a77110be0b72b97f1b34addd03dd852a37a8df
Howard Pritchard <howardp@lanl.gov>
2025-05-10 16:06:15 -0600

    ABI: Move ABI support into big count binding code
    Implement lots of missing functionality in the original
    api.py script.  The functionality is now part of the
    bindings generation framework used for Big Count.
    
    Number of todos still to do, in particular provide
    support for wrapping user supplied callback functions.
    Also, the sendrecv_replace, etc. code needs to be
    refactored to work with the bindings framework.
    
    to heed the --enable-abi-standard config option
    
    Additional initial fixes to merging ABI support into big count
    framework.
    
    Co-authored-by: Jake Tronge <jtronge3@gmail.com>
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- ompi/mpi/bindings/ompi_bindings/consts.py
+++ ompi/mpi/bindings/ompi_bindings/consts.py
@@ -160,6 +160,10 @@ COMMUNICATOR_SPLIT_TYPES = [
     'MPI_COMM_TYPE_RESOURCE_GUIDED',
 ]
 
+RESERVED_GROUPS = [
+    'MPI_GROUP_NULL',
+]
+
 RESERVED_WINDOWS = [
     'MPI_WIN_NULL',
 ]
@@ -175,6 +179,23 @@ RESERVED_INFOS = [
 
 RESERVED_FILES = [
     'MPI_FILE_NULL',
+]
+
+RESERVED_OPS = [
+    'MPI_OP_NULL',
+]
+
+RESERVED_MESSAGES = [
+     'MPI_MESSAGE_NULL',
+     'MPI_MESSAGE_NO_PROC',
+]
+
+RESERVED_SESSIONS = [
+    'MPI_SESSION_NULL',
+]
+
+RESERVED_ERRHANDLERS = [
+    'MPI_ERRHANDLER_NULL',
 ]
 
 IGNORED_STATUS_HANDLES = [
@@ -230,15 +251,28 @@ C_HANDLES = [
     'MPI_Win',
 ]
 
+#
+# C objects that can have attributes cached on them
+#
+C_ATTRIBUTE_OBJS = [
+    'MPI_Comm',
+    'MPI_Type',
+    'MPI_Win',
+]
+
 class ConvertFuncs:
     """Names of conversion functions (between standard ABI and OMPI ABI)."""
 
-    ERROR_CLASS = 'ompi_convert_intern_error_abi_error'
+    ERROR_CLASS = 'ompi_convert_abi_error_intern_error'
+    ERRHANDLER = 'ompi_convert_abi_errorhandler_intern_errorhandler'
     COMM = 'ompi_convert_abi_comm_intern_comm'
     DATATYPE = 'ompi_convert_abi_datatype_intern_datatype'
+    GROUP = 'ompi_convert_abi_group_intern_group'
     REQUEST = 'ompi_convert_abi_request_intern_request'
-    STATUS = 'ompi_convert_intern_status_abi_status'
+    STATUS = 'ompi_convert_abi_status_intern_status'
+    MESSAGE = 'ompi_convert_abi_message_intern_message'
     OP = 'ompi_convert_abi_op_intern_op'
+    SESSION = 'ompi_convert_abi_session_intern_session'
     WIN = 'ompi_convert_abi_win_intern_win'
     INFO = 'ompi_convert_abi_info_intern_info'
     FILE = 'ompi_convert_abi_file_intern_file'
@@ -248,6 +282,18 @@ class ConvertOMPIToStandard:
     """Generated function for converting from OMPI to standard ABI."""
 
     COMM = 'ompi_convert_comm_ompi_to_standard'
+    ERROR_CLASS = 'ompi_convert_intern_error_abi_error'
+    ERRHANDLER = 'ompi_convert_intern_errorhandler_abi_errorhandler'
+    GROUP = 'ompi_convert_group_ompi_to_standard'
+    DATATYPE = 'ompi_convert_datatype_ompi_to_standard'
+    FILE = 'ompi_convert_file_ompi_to_standard'
+    MESSAGE = 'ompi_convert_message_ompi_to_standard'
+    OP = 'ompi_convert_op_ompi_to_standard'
+    SESSION = 'ompi_convert_session_ompi_to_standard'
+    STATUS = 'ompi_convert_intern_status_abi_status'
+    WIN = 'ompi_convert_win_ompi_to_standard'
+    REQUEST = 'ompi_convert_ompi_request_abi_request'
+    INFO = 'ompi_convert_info_ompi_to_standard'
 
 
 # Inline function attributes
--- ompi/mpi/c/keyval_free.c
+++ ompi/mpi/c/keyval_free.c
@@ -1,57 +1,52 @@
-/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/* THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT BY HAND. */
 /*
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2007 The University of Tennessee and The University
+ * Copyright (c) 2004-2020 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
  *                         University of Stuttgart.  All rights reserved.
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
- * Copyright (c) 2006-2012 Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2024      Triad National Security, LLC. All rights
+ *                         reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
  *
  * $HEADER$
  */
+
 #include "ompi_config.h"
 
-#include "ompi/win/win.h"
 #include "ompi/mpi/c/bindings.h"
 #include "ompi/runtime/params.h"
 #include "ompi/errhandler/errhandler.h"
-#include "ompi/mpi/fortran/base/fint_2_int.h"
+#include "ompi/attribute/attribute.h"
+#include "ompi/communicator/communicator.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
-#pragma weak MPI_Win_f2c = PMPI_Win_f2c
+#pragma weak MPI_Keyval_free = PMPI_Keyval_free
 #endif
-#define MPI_Win_f2c PMPI_Win_f2c
+#define MPI_Keyval_free PMPI_Keyval_free
 #endif
-
-static const char FUNC_NAME[] = "MPI_Win_f2c";
-
-MPI_Win MPI_Win_f2c(MPI_Fint win)
+int MPI_Keyval_free(int *keyval)
 {
-    int o_index= OMPI_FINT_2_INT(win);
+    int ret;
 
+    /* Check for valid key pointer */
     if (MPI_PARAM_CHECK) {
-        OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
+        if (NULL == keyval) {
+            return OMPI_ERRHANDLER_NOHANDLE_INVOKE(MPI_ERR_KEYVAL,
+                                          "MPI_Keyval_free");
+        }
     }
 
-    /* Per MPI-2:4.12.4, do not invoke an error handler if we get an
-       invalid fortran handle.  If we get an invalid fortran handle,
-       return an invalid C handle. */
-
-    if ( 0 > o_index ||
-         o_index >= opal_pointer_array_get_size(&ompi_mpi_windows)) {
-        return NULL;
-    }
-
-    return (MPI_Win)opal_pointer_array_get_item(&ompi_mpi_windows, o_index);
+    ret = ompi_attr_free_keyval(COMM_ATTR, keyval, 0);
+    OMPI_ERRHANDLER_NOHANDLE_RETURN(ret, MPI_ERR_OTHER, "MPI_Keyval_free");
 }
