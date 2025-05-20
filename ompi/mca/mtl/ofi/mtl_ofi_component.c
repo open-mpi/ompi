b097ff7901561fb6946332f8fff25137d400afb2
Shi Jin <sjina@amazon.com>
2025-05-20 04:01:42 +0000

    mtl/ofi: Remove special patch for gni provider
    There should not be any Cray XC systems in production now
    - which is where the GNI provider nominally functioned.
    
    Meanwhile FI_MR_BASIC is deprecated since Libfabric 1.5
    and will be droppped in future Libfabric 2.x versions.
    
    Remove this special handler for gni provider due to
    unnecessity and build warnings.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>
    (cherry picked from commit 1c67d904e87b14d9abd75de2c2708f1a644371d7)

--- ompi/mca/mtl/ofi/mtl_ofi_component.c
+++ ompi/mca/mtl/ofi/mtl_ofi_component.c
@@ -897,17 +897,6 @@ select_prov:
         goto error;
     }
 
-    /**
-     * Unfortunately the attempt to implement FI_MR_SCALABLE in the GNI provider
-     * doesn't work, at least not well.  Since we're asking for the 1.5 libfabric
-     * API now, we have to tell GNI we want to use Mr. Basic.  Using FI_MR_BASIC
-     * rather than FI_MR_VIRT_ADDR | FI_MR_ALLOCATED | FI_MR_PROV_KEY to stay
-     * compatible with older libfabrics.
-     */
-    if (!strncmp(prov->fabric_attr->prov_name,"gni",3)) {
-         prov->domain_attr->mr_mode = FI_MR_BASIC;
-    }
-
 #if OPAL_CUDA_SUPPORT
     /**
      * Some providers do not require the use of the CUDA convertor
