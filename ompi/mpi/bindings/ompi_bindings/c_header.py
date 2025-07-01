7d79681b51087a937c1271f60e82e51823e6f090
Joe Downs <joe@dwns.dev>
2025-07-01 10:55:11 -0600

    WIP: create ABI header file from template with categories

--- ompi/mpi/bindings/ompi_bindings/c_header.py
+++ ompi/mpi/bindings/ompi_bindings/c_header.py
@@ -1,20 +1,84 @@
+import re
+import textwrap
 import consts
+from consts import Lang
 
-lines = []
+categories = {}
 
-for value in consts.consts.values():
-    name = value["name"]
-    abi_value = value["abi_value"]
-    c_type = value["handle_types"]["c"]["type"]
+def comment(message, lang=Lang.C, indentation=0):
+    """
+    Take in a message and return a commented version of it.
+    """
+    comment_lines = []
+    width = 80
+    #words = message.split()
+    prefix = ""
+    spaces = (indentation + 1) * ' '
+    if lang == Lang.C or lang == Lang.CPP:
+        comment_lines.append(spaces = "/*")
+        prefix = " *"
+    elif lang == Lang.FORTRAN:
+        prefix ="!"
+
+    prefix = spaces + prefix + ' '
+    comment_lines.append(textwrap.wrap(message,
+                                       width=width,
+                                       initial_indent=prefix,
+                                       subsequent_indent=prefix))
+    # for word in words:
+    #     line = [prefix]
+    #     cols_left = width - len(prefix)
+    #     len(word) <= cols_left:
+    #         line.append(word)
+    #     comment_lines.append()
+
+    if lang == Lang.C or lang == Lang.CPP:
+        comment_lines.append(spaces + "*/")
+    return '\n'.join(comment_lines)
+
+for category in consts.categories.values():
+    name = category["name"]
+    categories[name] = []
+    for value in consts.consts.values():
+        if value["category"] == name:
+            categories[name].append(value)
+
+def output_constant(const):
+    # TODO: Use enums for ints? (ask Howard)
+    name = const["name"]
+    abi_value = const["abi_value"]
+    c_type = const["handle_types"]["c"]["type"]
     if c_type is None:
-        continue
+        return None
     def_name = f"#define {name}"
     if c_type == "int":
-        value = f"{abi_value}\n"
+        value = f"{abi_value}"
     else:
-        value = f"(({c_type}) {abi_value})\n"
-    line = def_name + " " * (45 - len(def_name)) + value
-    lines.append(line)
+        value = f"(({c_type}) {abi_value})"
+    return def_name + " " * (45 - len(def_name)) + value + "\n"
 
-with open(consts.DIR / "mpi_abi.h", 'wt') as header:
-    header.writelines(lines)
+lines = []
+with open(consts.DIR / "abi.h.in", 'r') as header_in:
+    lines = header_in.readlines()
+
+# Match lines that start with `$CATEGORY:`. Any amount of whitespace is allowed
+# in the case of indenting. However, only preceding whitespace is allowed so
+# that commented-out lines are NOT included.
+category_pattern = re.compile(r"^[\s]*\$CATEGORY:([A-Z_0-9]+)\$$")
+output = []
+
+for line in lines:
+    category = category_pattern.search(line)
+    # If there's no match, category is None. Then we just want to preserve the
+    # line as-is.
+    if category:
+        category = category.group(1)
+        for constant in categories[category]:
+            line = output_constant(constant)
+            if line is not None:
+                output.append(line)
+    else:
+        output.append(line)
+
+with open(consts.DIR / "abi.h", 'tw') as header_out:
+    header_out.writelines(output)
--- ompi/mpi/bindings/ompi_bindings/consts.py
+++ ompi/mpi/bindings/ompi_bindings/consts.py
@@ -9,224 +9,33 @@
 #
 """Constants used for generating bindings."""
 import re
+import json
+from enum import Enum
+
+# TODO: remove?
+from pathlib import Path
+
 
 FORTRAN_ERROR_NAME = 'ierror'
 C_ERROR_NAME = 'ierr'
 C_ERROR_TMP_NAME = 'c_ierr'
 GENERATED_MESSAGE = 'THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT BY HAND.'
 
+DIR = Path("/mnt/")
+
+with open(DIR / "abi.json") as f:
+    abi = json.load(f)
+    consts = abi["constants"]
+    categories = abi["categories"]
+
+class Lang(Enum):
+    C = 1
+    CPP = 2
+    FORTRAN = 3
 #
 # C and ABI constants
 #
 # C type: const int
-ERROR_CLASSES = [
-    'MPI_SUCCESS',
-    'MPI_ERR_BUFFER',
-    'MPI_ERR_COUNT',
-    'MPI_ERR_TYPE',
-    'MPI_ERR_TAG',
-    'MPI_ERR_COMM',
-    'MPI_ERR_RANK',
-    'MPI_ERR_REQUEST',
-    'MPI_ERR_ROOT',
-    'MPI_ERR_GROUP',
-    'MPI_ERR_OP',
-    'MPI_ERR_TOPOLOGY',
-    'MPI_ERR_DIMS',
-    'MPI_ERR_ARG',
-    'MPI_ERR_UNKNOWN',
-    'MPI_ERR_TRUNCATE',
-    'MPI_ERR_OTHER',
-    'MPI_ERR_INTERN',
-    'MPI_ERR_PENDING',
-    'MPI_ERR_IN_STATUS',
-    'MPI_ERR_ACCESS',
-    'MPI_ERR_AMODE',
-    'MPI_ERR_ASSERT',
-    'MPI_ERR_BAD_FILE',
-    'MPI_ERR_BASE',
-    'MPI_ERR_CONVERSION',
-    'MPI_ERR_DISP',
-    'MPI_ERR_DUP_DATAREP',
-    'MPI_ERR_FILE_EXISTS',
-    'MPI_ERR_FILE_IN_USE',
-    'MPI_ERR_FILE',
-    'MPI_ERR_INFO_KEY',
-    'MPI_ERR_INFO_NOKEY',
-    'MPI_ERR_INFO_VALUE',
-    'MPI_ERR_INFO',
-    'MPI_ERR_IO',
-    'MPI_ERR_KEYVAL',
-    'MPI_ERR_LOCKTYPE',
-    'MPI_ERR_NAME',
-    'MPI_ERR_NO_MEM',
-    'MPI_ERR_NOT_SAME',
-    'MPI_ERR_NO_SPACE',
-    'MPI_ERR_NO_SUCH_FILE',
-    'MPI_ERR_PORT',
-    'MPI_ERR_PROC_ABORTED',
-    'MPI_ERR_QUOTA',
-    'MPI_ERR_READ_ONLY',
-    'MPI_ERR_RMA_ATTACH',
-    'MPI_ERR_RMA_CONFLICT',
-    'MPI_ERR_RMA_RANGE',
-    'MPI_ERR_RMA_SHARED',
-    'MPI_ERR_RMA_SYNC',
-    'MPI_ERR_RMA_FLAVOR',
-    'MPI_ERR_SERVICE',
-    'MPI_ERR_SESSION',
-    'MPI_ERR_SIZE',
-    'MPI_ERR_SPAWN',
-    'MPI_ERR_UNSUPPORTED_DATAREP',
-    'MPI_ERR_UNSUPPORTED_OPERATION',
-    'MPI_ERR_WIN',
-    'MPI_T_ERR_CANNOT_INIT',
-    'MPI_T_ERR_NOT_INITIALIZED',
-    'MPI_T_ERR_MEMORY',
-    'MPI_T_ERR_INVALID',
-    'MPI_T_ERR_INVALID_INDEX',
-    'MPI_T_ERR_INVALID_ITEM',
-    'MPI_T_ERR_INVALID_SESSION',
-    'MPI_T_ERR_INVALID_HANDLE',
-    'MPI_T_ERR_INVALID_NAME',
-    'MPI_T_ERR_OUT_OF_HANDLES',
-    'MPI_T_ERR_OUT_OF_SESSIONS',
-    'MPI_T_ERR_CVAR_SET_NOT_NOW',
-    'MPI_T_ERR_CVAR_SET_NEVER',
-    'MPI_T_ERR_PVAR_NO_WRITE',
-    'MPI_T_ERR_PVAR_NO_STARTSTOP',
-    'MPI_T_ERR_PVAR_NO_ATOMIC',
-    'MPI_ERR_LASTCODE',
-]
-
-PREDEFINED_DATATYPES = [
-    'MPI_CHAR',
-    'MPI_SHORT',
-    'MPI_INT',
-    'MPI_LONG',
-    'MPI_LONG_LONG_INT',
-    'MPI_LONG_LONG',
-    'MPI_SIGNED_CHAR',
-    'MPI_UNSIGNED_CHAR',
-    'MPI_UNSIGNED_SHORT',
-    'MPI_UNSIGNED',
-    'MPI_UNSIGNED_LONG',
-    'MPI_UNSIGNED_LONG_LONG',
-    'MPI_FLOAT',
-    'MPI_DOUBLE',
-    'MPI_LONG_DOUBLE',
-    'MPI_WCHAR',
-    'MPI_C_BOOL',
-    'MPI_INT8_T',
-    'MPI_INT16_T',
-    'MPI_INT32_T',
-    'MPI_INT64_T',
-    'MPI_UINT8_T',
-    'MPI_UINT16_T',
-    'MPI_UINT32_T',
-    'MPI_UINT64_T',
-    'MPI_AINT',
-    'MPI_COUNT',
-    'MPI_OFFSET',
-    'MPI_C_COMPLEX',
-    'MPI_C_FLOAT_COMPLEX',
-    'MPI_C_DOUBLE_COMPLEX',
-    'MPI_C_LONG_DOUBLE_COMPLEX',
-    'MPI_BYTE',
-    'MPI_PACKED',
-    'MPI_CXX_BOOL',
-    'MPI_CXX_FLOAT_COMPLEX',
-    'MPI_CXX_DOUBLE_COMPLEX',
-    'MPI_CXX_LONG_DOUBLE_COMPLEX',
-    'MPI_FLOAT_INT',
-    'MPI_DOUBLE_INT',
-    'MPI_LONG_INT',
-    'MPI_2INT',
-    'MPI_SHORT_INT',
-    'MPI_LONG_DOUBLE_INT',
-]
-
-# C type: MPI_Comm
-RESERVED_COMMUNICATORS = [
-    'MPI_COMM_NULL',
-    'MPI_COMM_WORLD',
-    'MPI_COMM_SELF',
-]
-
-COMMUNICATOR_SPLIT_TYPES = [
-    'MPI_COMM_TYPE_SHARED',
-    'MPI_COMM_TYPE_HW_UNGUIDED',
-    'MPI_COMM_TYPE_HW_GUIDED',
-]
-
-RESERVED_GROUPS = [
-    'MPI_GROUP_NULL',
-]
-
-RESERVED_WINDOWS = [
-    'MPI_WIN_NULL',
-]
-
-RESERVED_REQUESTS = [
-    'MPI_REQUEST_NULL',
-]
-
-RESERVED_INFOS = [
-    'MPI_INFO_ENV',
-    'MPI_INFO_NULL',
-]
-
-RESERVED_FILES = [
-    'MPI_FILE_NULL',
-]
-
-RESERVED_OPS = [
-    'MPI_OP_NULL',
-]
-
-RESERVED_MESSAGES = [
-     'MPI_MESSAGE_NULL',
-     'MPI_MESSAGE_NO_PROC',
-]
-
-RESERVED_SESSIONS = [
-    'MPI_SESSION_NULL',
-]
-
-RESERVED_ERRHANDLERS = [
-    'MPI_ERRHANDLER_NULL',
-]
-
-IGNORED_STATUS_HANDLES = [
-    'MPI_STATUSES_IGNORE',
-    'MPI_STATUS_IGNORE',
-]
-
-COLLECTIVE_OPERATIONS = [
-    'MPI_MAX',
-    'MPI_MIN',
-    'MPI_SUM',
-    'MPI_PROD',
-    'MPI_MAXLOC',
-    'MPI_MINLOC',
-    'MPI_BAND',
-    'MPI_BOR',
-    'MPI_BXOR',
-    'MPI_LAND',
-    'MPI_LOR',
-    'MPI_LXOR',
-    'MPI_REPLACE',
-    'MPI_NO_OP',
-]
-
-VARIOUS_CONSTANTS = {
-    # Just setting this to the same as ompi ABI for right now, but will need to
-    # match the standard ABI value when defined
-    'MPI_MAX_LIBRARY_VERSION_STRING': 256,
-    'MPI_MAX_PROCESSOR_NAME': 256,
-}
-
-# Types
 
 C_OPAQUE_TYPES = {
     'MPI_Aint': 'intptr_t',
