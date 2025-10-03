9eba64857d949fed2a1680eb4125111ff6d11c5a
Howard Pritchard <howardp@lanl.gov>
2025-10-03 11:19:06 -0600

    some fixes to comm attributes wrappers
    also see about fixing an issue with some of the ompi
    mca components when using --enable-mca-dso.
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

---
+++ ompi/mpi/c/attr_fn_abi.c
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2018      Research Organization for Information Science
+ *                         and Technology (RIST).  All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+
+#include "ompi_config.h"
+
+#include "ompi/mpi/c/bindings.h"
+#include "ompi/mpi/c/abi.h"
+#include "ompi/mpi/c/abi_converters.h"
+
+/*
+ * Comment to circumvent error-msg of weak-check:
+ *   We do not need #pragma weak in here, as these functions
+ *   do not have a function for the profiling interface.
+ */
+
+/*
+ * Note that these are the back-end functions for MPI_DUP_FN (and
+ * friends).  They have an ABI_C_* prefix because of weird reasons
+ * listed in a lengthy comment in mpi.h.
+ *
+ * Specifically:
+ *
+ *   MPI_NULL_DELETE_FN -> ABI_C_MPI_NULL_DELETE_FN
+ *   MPI_NULL_COPY_FN -> ABI_C_MPI_NULL_COPY_FN
+ *   MPI_DUP_FN -> ABI_C_MPI_DUP_FN
+ *
+ *   MPI_TYPE_NULL_DELETE_FN -> ABI_C_MPI_TYPE_NULL_DELETE_FN
+ *   MPI_TYPE_NULL_COPY_FN -> ABI_C_MPI_TYPE_NULL_COPY_FN
+ *   MPI_TYPE_DUP_FN -> ABI_C_MPI_TYPE_DUP_FN
+ *
+ *   MPI_COMM_NULL_DELETE_FN -> ABI_C_MPI_COMM_NULL_DELETE_FN
+ *   MPI_COMM_NULL_COPY_FN -> ABI_C_MPI_COMM_NULL_COPY_FN
+ *   MPI_COMM_DUP_FN -> ABI_C_MPI_COMM_DUP_FN
+ *
+ *   MPI_WIN_NULL_DELETE_FN -> ABI_C_MPI_WIN_NULL_DELETE_FN
+ *   MPI_WIN_NULL_COPY_FN -> ABI_C_MPI_WIN_NULL_COPY_FN
+ *   MPI_WIN_DUP_FN -> ABI_C_MPI_WIN_DUP_FN
+ */
+
+#if 0
+int ABI_C_MPI_TYPE_NULL_DELETE_FN( MPI_Datataype_ABI_INTERNAL datatype, int type_keyval,
+                                    void* attribute_val_out,
+                                    void* extra_state )
+{
+   /* Why not all MPI functions are like this ?? */
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_TYPE_NULL_COPY_FN( MPI_Datataype_ABI_INTERNAL datatype, int type_keyval,
+                                  void* extra_state,
+                                  void* attribute_val_in,
+                                  void* attribute_val_out,
+                                  int* flag )
+{
+   *flag = 0;
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_TYPE_DUP_FN( MPI_Datataype_ABI_INTERNAL datatype, int type_keyval,
+                            void* extra_state,
+                            void* attribute_val_in, void* attribute_val_out,
+                            int* flag )
+{
+   *flag = 1;
+   *(void**)attribute_val_out = attribute_val_in;
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_WIN_NULL_DELETE_FN( MPI_Win_ABI_INTERNAL window, int win_keyval,
+                                   void* attribute_val_out,
+                                   void* extra_state )
+{
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_WIN_NULL_COPY_FN( MPI_Win_ABI_INTERNAL window, int win_keyval,
+                                 void* extra_state,
+                                 void* attribute_val_in,
+                                 void* attribute_val_out, int* flag )
+{
+   *flag= 0;
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_WIN_DUP_FN( MPI_Win_ABI_INTERNAL window, int win_keyval, void* extra_state,
+                           void* attribute_val_in, void* attribute_val_out,
+                           int* flag )
+{
+   *flag = 1;
+   *(void**)attribute_val_out = attribute_val_in;
+   return MPI_SUCCESS;
+}
+
+#endif
+
+int ABI_C_MPI_COMM_NULL_DELETE_FN( MPI_Comm_ABI_INTERNAL comm, int comm_keyval,
+                                    void* attribute_val_out,
+                                    void* extra_state )
+{
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_COMM_NULL_COPY_FN( MPI_Comm_ABI_INTERNAL comm, int comm_keyval,
+                                  void* extra_state,
+                                  void* attribute_val_in,
+                                  void* attribute_val_out, int* flag )
+{
+   *flag= 0;
+   return MPI_SUCCESS;
+}
+
+int ABI_C_MPI_COMM_DUP_FN( MPI_Comm_ABI_INTERNAL comm, int comm_keyval, void* extra_state,
+                     void* attribute_val_in, void* attribute_val_out,
+                     int* flag )
+{
+   *flag = 1;
+   *(void**)attribute_val_out = attribute_val_in;
+   return MPI_SUCCESS;
+}
