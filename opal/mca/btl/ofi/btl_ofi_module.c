97ccdd627f5a1d39c8329192dd65912a851c5950
Matthew Whitlock <mwhitlo@sandia.gov>
2025-10-09 14:34:52 -0500

    btl/ofi: fault tolerance
    Signed-off-by: Matthew Whitlock <mwhitlo@sandia.gov>

--- opal/mca/btl/ofi/btl_ofi_module.c
+++ opal/mca/btl/ofi/btl_ofi_module.c
@@ -143,6 +143,14 @@ static int mca_btl_ofi_del_procs(mca_btl_base_module_t *btl, size_t nprocs, opal
     return OPAL_SUCCESS;
 }
 
+static int mca_btl_ofi_register_error(mca_btl_base_module_t *btl,
+                                      mca_btl_base_module_error_cb_fn_t cb)
+{
+    mca_btl_ofi_module_t *ofi_btl = (mca_btl_ofi_module_t *) btl;
+    ofi_btl->ofi_error_cb = cb;
+    return OPAL_SUCCESS;
+}
+
 void mca_btl_ofi_rcache_init(mca_btl_ofi_module_t *module)
 {
     if (!module->initialized) {
@@ -515,4 +523,5 @@ mca_btl_ofi_module_t mca_btl_ofi_module_template = {
         .btl_add_procs = mca_btl_ofi_add_procs,
         .btl_del_procs = mca_btl_ofi_del_procs,
         .btl_finalize = mca_btl_ofi_finalize,
+        .btl_register_error = mca_btl_ofi_register_error,
     }};
