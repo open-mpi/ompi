7dbd91702f7cbaf69b1bb16d335733c48ceeddac
geokoko <71934918+geokoko@users.noreply.github.com>
2026-03-19 01:40:03 +0200

    Change src and dst buffer sizes from 64 to INET_ADDRSTRLEN in btl_tcp2_endpoint.c
    Signed-off-by: geokoko <71934918+geokoko@users.noreply.github.com>

--- contrib/build-mca-comps-outside-of-tree/btl_tcp2_endpoint.c
+++ contrib/build-mca-comps-outside-of-tree/btl_tcp2_endpoint.c
@@ -122,7 +122,7 @@ static void mca_btl_tcp2_endpoint_send_handler(int sd, short flags, void* user);
 
 void mca_btl_tcp_endpoint_dump(mca_btl_base_endpoint_t* btl_endpoint, const char* msg)
 {
-    char src[64], dst[64], *status;
+    char src[INET_ADDRSTRLEN], dst[INET_ADDRSTRLEN], *status;
     int sndbuf, rcvbuf, nodelay, flags = -1;
 #if OPAL_ENABLE_IPV6
     struct sockaddr_storage inaddr;
