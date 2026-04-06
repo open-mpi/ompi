6ec3b937ab45fa496bb412bd8d0a91b1b52d5990
George Bosilca <gbosilca@nvidia.com>
2026-04-06 13:23:07 -0700

    We can't have OPAL cleanups in modules
    Modules are unloaded before the cleanup sequence triggers, and
    then the cleanup will call function pointers from unloaded objects.
    
    Signed-off-by: George Bosilca <gbosilca@nvidia.com>

--- opal/mca/btl/uct/btl_uct_component.c
+++ opal/mca/btl/uct/btl_uct_component.c
@@ -146,23 +146,6 @@ static int mca_btl_uct_component_register(void)
         MCA_BASE_VAR_TYPE_UNSIGNED_INT, NULL, 0, MCA_BASE_VAR_FLAG_SETTABLE, OPAL_INFO_LVL_4,
         MCA_BASE_VAR_SCOPE_LOCAL, &mca_btl_uct_component.connection_retry_timeout);
 
-    OBJ_CONSTRUCT(&mca_btl_uct_component.md_list, opal_list_t);
-    OBJ_CONSTRUCT(&mca_btl_uct_component.memory_domain_list, mca_btl_uct_include_list_t);
-    OBJ_CONSTRUCT(&mca_btl_uct_component.connection_domain_list, mca_btl_uct_include_list_t);
-
-    int rc = mca_btl_uct_component_discover_mds();
-    if (OPAL_SUCCESS != rc) {
-        return rc;
-    }
-
-    rc = mca_btl_uct_component_generate_modules(&mca_btl_uct_component.md_list);
-    if (OPAL_SUCCESS != rc) {
-        return rc;
-    }
-
-    mca_btl_uct_component.initialized = true;
-    opal_finalize_register_cleanup(mca_btl_uct_cleanup);
-
     return OPAL_SUCCESS;
 }
 
@@ -205,6 +188,22 @@ static int mca_btl_uct_component_open(void)
         ucm_set_external_event(UCM_EVENT_VM_UNMAPPED);
         opal_mem_hooks_register_release(mca_btl_uct_mem_release_cb, NULL);
     }
+
+    OBJ_CONSTRUCT(&mca_btl_uct_component.md_list, opal_list_t);
+    OBJ_CONSTRUCT(&mca_btl_uct_component.memory_domain_list, mca_btl_uct_include_list_t);
+    OBJ_CONSTRUCT(&mca_btl_uct_component.connection_domain_list, mca_btl_uct_include_list_t);
+
+    int rc = mca_btl_uct_component_discover_mds();
+    if (OPAL_SUCCESS != rc) {
+        return rc;
+    }
+
+    rc = mca_btl_uct_component_generate_modules(&mca_btl_uct_component.md_list);
+    if (OPAL_SUCCESS != rc) {
+        return rc;
+    }
+
+    mca_btl_uct_component.initialized = true;
 
     return OPAL_SUCCESS;
 }
