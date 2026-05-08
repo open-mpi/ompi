df918fcf0f5fd4d2177d04f02de3e6817e8165bc
Axel Schneewind <axel.schneewind@protonmail.com>
2026-05-08 10:11:51 +0200

    implement message aggregation for part/persist
    This aggregation scheme is intended to allow OpenMPI to transfer larger messages
    if the user-reported partitions are too small or too many.
    This is achieved by using an internal partitioning where each internal (transfer)
    partition corresponds to one or multiple user-reported partitions.
    
    The implementation provides an interface for insertion of user partitions,
    that optionally outputs a transfer partition that is ready.
    
    Internally, each transfer partition is associated with an atomic counter,
    tracking the number of corresponding pready-calls. As soon as a counter reaches
    the number of corresponding user-partitions, the transfer partition is returned
    in the respective insertion call.
    
    Signed-off-by: Axel Schneewind <axel.schneewind@hlrs.de>

---
+++ ompi/mca/part/base/aggregation_schemes/aggregation_scheme_regular.c
@@ -0,0 +1,83 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2024      High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
+#include "aggregation_scheme_regular.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+// converts the index of a public partition to the index of its corresponding internal partition
+static inline int internal_partition(struct part_persist_aggregation_state *state, int public_part)
+{
+    return public_part / state->factor;
+}
+
+void aggregation_scheme_regular_psend_init(struct part_persist_aggregation_state *state,
+                                        int internal_partition_count, int factor, int last_internal_partition_size)
+{
+    state->public_partition_count = (internal_partition_count - 1) * factor + last_internal_partition_size;
+    state->internal_partition_count = internal_partition_count;
+
+    // number of user-partitions per internal partition (except for the last one)
+    state->factor = factor;
+    // number of user-partitions corresponding to the last internal partition
+    state->last_internal_partition_size = last_internal_partition_size;
+
+    // initialize counters
+    state->public_parts_ready = (opal_atomic_uint32_t *) calloc(state->internal_partition_count,
+                                                                 sizeof(opal_atomic_uint32_t));
+}
+
+void aggregation_scheme_regular_reset(struct part_persist_aggregation_state *state)
+{
+    // reset flags
+    if (NULL != state->public_parts_ready) {
+        memset((void*)state->public_parts_ready, 0, state->internal_partition_count * sizeof(opal_atomic_uint32_t));
+    }
+}
+
+static inline int is_last_partition(struct part_persist_aggregation_state *state, int partition) {
+    return (partition == state->internal_partition_count - 1);
+}
+
+static inline int num_public_parts(struct part_persist_aggregation_state *state, int partition) {
+    return is_last_partition(state, partition) ? state->last_internal_partition_size : state->factor;
+}
+
+void aggregation_scheme_regular_pready(struct part_persist_aggregation_state *state, int partition, int* available_partition)
+{
+    int internal_part = internal_partition(state, partition);
+    int corresponding_public_parts = num_public_parts(state, internal_part);
+
+    // this is the new value (after incrementing)
+    int count = opal_atomic_add_fetch_32(&state->public_parts_ready[internal_part], 1);
+
+    // output internal partition if ready
+    if (count == corresponding_public_parts) {
+        *available_partition = internal_part;
+    } else {
+        *available_partition = -1;
+    }
+}
+
+int aggregation_scheme_regular_internal_part(struct part_persist_aggregation_state *state,
+                                                      int partition)
+{
+    return internal_partition(state, partition);
+}
+
+void aggregation_scheme_regular_free(struct part_persist_aggregation_state *state)
+{
+    if (NULL != state->public_parts_ready)
+        free((void*)state->public_parts_ready);
+    state->public_parts_ready = NULL;
+}
