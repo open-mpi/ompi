0499e44259cee63a7548c61affba10754eccee48
Howard Pritchard <howardp@lanl.gov>
2026-06-23 14:41:49 -0600

    mpi/tool: Add missing MPI_T_ERR_NOT_ACCESSIBLE and MPI_T_ERR_NOT_SUPPORTED error constants
    MPI-5.0 §15.3 requires all MPI_T_ERR_* constants to be defined in mpi.h.
    Two constants were missing: MPI_T_ERR_NOT_ACCESSIBLE and
    MPI_T_ERR_NOT_SUPPORTED (listed in Table 15.7, p.765).
    
    This commit adds both constants with values 81 and 82 respectively,
    satisfying the standard's requirement that error codes be in the range
    0 < code <= MPI_ERR_LASTCODE (92). The constants are now:
    
    - Defined in ompi/include/mpi.h.in (C bindings)
    - Added to ompi/include/mpif-values.py (Fortran bindings)
    - Registered in ompi/errhandler/errcode.c for MPI_Error_string support
    - Listed in ompi/mpi/bindings/ompi_bindings/consts.py for ABI generation
    
    Fixes #14018
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- ompi/errhandler/errcode.c
+++ ompi/errhandler/errcode.c
@@ -129,6 +129,8 @@ static ompi_mpi_errcode_t ompi_err_revoked;
 static ompi_mpi_errcode_t ompi_err_session;
 static ompi_mpi_errcode_t ompi_err_value_too_large;
 static ompi_mpi_errcode_t ompi_err_errhandler;
+static ompi_mpi_errcode_t ompi_t_err_not_accessible;
+static ompi_mpi_errcode_t ompi_t_err_not_supported;
 
 static void ompi_mpi_errcode_construct(ompi_mpi_errcode_t* errcode);
 static void ompi_mpi_errcode_destruct(ompi_mpi_errcode_t* errcode);
@@ -248,6 +250,8 @@ int ompi_mpi_errcode_init (void)
     CONSTRUCT_ERRCODE( ompi_err_session,  MPI_ERR_SESSION,  "MPI_ERR_SESSION: Invalid session handle" );
     CONSTRUCT_ERRCODE( ompi_err_value_too_large,  MPI_ERR_VALUE_TOO_LARGE,  "MPI_ERR_VALUE_TOO_LARGE: Value is too large to store" );
     CONSTRUCT_ERRCODE( ompi_err_errhandler,  MPI_ERR_ERRHANDLER,  "MPI_ERR_ERRHANDLER: Invalid error handler handle" );
+    CONSTRUCT_ERRCODE( ompi_t_err_not_accessible, MPI_T_ERR_NOT_ACCESSIBLE, "MPI_T_ERR_NOT_ACCESSIBLE: Requested functionality is not accessible" );
+    CONSTRUCT_ERRCODE( ompi_t_err_not_supported, MPI_T_ERR_NOT_SUPPORTED, "MPI_T_ERR_NOT_SUPPORTED: Requested functionality not supported" );
 
     /* Per MPI-3 p353:27-32, MPI_LASTUSEDCODE must be >=
        MPI_ERR_LASTCODE.  So just start it as == MPI_ERR_LASTCODE. */
@@ -367,6 +371,8 @@ int ompi_mpi_errcode_finalize (void)
     OBJ_DESTRUCT(&ompi_err_session);
     OBJ_DESTRUCT(&ompi_err_value_too_large);
     OBJ_DESTRUCT(&ompi_err_errhandler);
+    OBJ_DESTRUCT(&ompi_t_err_not_accessible);
+    OBJ_DESTRUCT(&ompi_t_err_not_supported);
     OBJ_DESTRUCT(&ompi_mpi_errcodes);
     ompi_mpi_errcode_lastpredefined = 0;
     opal_mutex_unlock(&errcode_lock);
