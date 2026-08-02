00b9793fb8d0718e8a77a141523bae3681a0b6d2
Jeff Squyres <jeff@squyres.com>
2026-08-02 09:45:25 -0400

    coll/tuned: handle EOF when reading the rules-file version
    CodeQL (GitHub code scanning alert 29) flagged the fscanf() call that
    consumes the optional rule-file version header: its result was
    compared only against 0, but fscanf() returns EOF (not 0) if the file
    is empty, in which case version was left uninitialized instead of
    defaulting to 1.  Check for exactly one converted item so both the
    no-match and EOF cases fall back to the default version.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- ompi/mca/coll/tuned/coll_tuned_dynamic_file.c
+++ ompi/mca/coll/tuned/coll_tuned_dynamic_file.c
@@ -447,8 +447,9 @@ static int ompi_coll_tuned_read_rules_config_file_classic (char *fname, ompi_col
         goto on_file_error;
     }
 
-    /* consume the optional version identifier */
-    if (0 == fscanf(fptr, "rule-file-version-%d", &version)) {
+    /* consume the optional version identifier; fscanf() returns EOF
+       (not 0) if the file is empty */
+    if (1 != fscanf(fptr, "rule-file-version-%d", &version)) {
         version = 1;
     }
 
