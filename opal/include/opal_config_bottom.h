25c916812daf838011f7f18235453a645350ae9f
Brian Barrett <bbarrett@amazon.com>
2026-08-31 20:22:28 -0700

    opal: Remove qsort workaround
    We no longer support Solaris, so no reason to have an option to work
    around its occasionally broken qsort() implementation.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- opal/include/opal_config_bottom.h
+++ opal/include/opal_config_bottom.h
@@ -18,6 +18,7 @@
  * Copyright (c) 2015-2017 Intel, Inc. All rights reserved.
  * Copyright (c) 2021      FUJITSU LIMITED.  All rights reserved.
  * Copyright (c) 2023      NVIDIA Corporation.  All rights reserved.
+ * Copyright (c) 2016      Amazon.com, Inc. or its affiliates.  All Rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -409,19 +410,6 @@
 
 #    ifndef HAVE_VSNPRINTF
 #        define vsnprintf opal_vsnprintf
-#    endif
-
-/*
- * Some platforms (Solaris) have a broken qsort implementation.  Work
- * around by using our own.
- */
-#    if OPAL_HAVE_BROKEN_QSORT
-#        ifdef qsort
-#            undef qsort
-#        endif
-
-#        include "opal/util/qsort.h"
-#        define qsort opal_qsort
 #    endif
 
 /*
